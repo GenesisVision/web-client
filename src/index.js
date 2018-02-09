0431cc8e08b06594870e4dffbbdd64d4868dbe26
nkrivous <kolsln@list.ru>
2018-02-09 18:43:55 +0300

    Add fonts and colors

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/core/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
+import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
