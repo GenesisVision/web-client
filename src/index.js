b74dcc4261274ff5d4173decc0aa273d72ec9ab4
nkrivous <kolsln@list.ru>
2018-03-02 18:48:23 +0300

    Reorder styles

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from "react-dom";
 import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
 
-import "./index.css";
 import "./styles/gv-bootstrap/gv-bootstrap.css";
 import "./styles/gv-styles.css";
+import "./index.css";
 
 import App from "./components/app";
 
