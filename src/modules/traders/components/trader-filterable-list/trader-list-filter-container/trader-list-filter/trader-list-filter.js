db051c1af7ef8d2abe50aba3b063bbfeb130837e
nkrivous <kolsln@list.ru>
2018-03-13 18:29:59 +0300

    Add combobox filter

--- src/modules/traders/components/trader-filterable-list/trader-list-filter-container/trader-list-filter/trader-list-filter.js
+++ src/modules/traders/components/trader-filterable-list/trader-list-filter-container/trader-list-filter/trader-list-filter.js
@@ -2,10 +2,23 @@ import { withFormik, Field } from "formik";
 import React from "react";
 
 import GVInputRange from "../../../../../../shared/components/form/gv-input-range/gv-input-range";
+import GVSelect from "../../../../../../shared/components/form/gv-select/gv-select";
 
 import "./trader-list-filter.css";
-
-const TraderListFilter = ({ values, setFieldValue, onChangeComplete }) => {
+const sortingOptions = [
+  { value: "Name", label: "Name" },
+  { value: "DaysLeft", label: "Days Left" }
+];
+const sortingDirectionOptions = [
+  { value: "Asc", label: "Ascending" },
+  { value: "Desc", label: "Descending" }
+];
+const TraderListFilter = ({
+  values,
+  setFieldValue,
+  setFieldTouched,
+  onChangeComplete
+}) => {
   return (
     <form className="trader-list-filter">
       <div className="filter-item">
@@ -39,6 +52,36 @@ const TraderListFilter = ({ values, setFieldValue, onChangeComplete }) => {
           />
         </div>
       </div>
+      <div className="filter-item">
+        <div className="filter-item__title">Sorting</div>
+        <div className="filter-item__description">Select column</div>
+        <div className="filter-item__component">
+          <Field
+            name="sorting"
+            value={values.sorting}
+            onChange={onChangeComplete("sorting")}
+            setFieldValue={setFieldValue}
+            onBlur={setFieldTouched}
+            component={GVSelect}
+            options={sortingOptions}
+          />
+        </div>
+      </div>
+      <div className="filter-item">
+        <div className="filter-item__title">Sorting direction</div>
+        <div className="filter-item__description">Select sorting direction</div>
+        <div className="filter-item__component">
+          <Field
+            name="sortingDirection"
+            value={values.sortingDirection}
+            onChange={onChangeComplete("sortingDirection")}
+            setFieldValue={setFieldValue}
+            onBlur={setFieldTouched}
+            component={GVSelect}
+            options={sortingDirectionOptions}
+          />
+        </div>
+      </div>
     </form>
   );
 };
@@ -47,6 +90,7 @@ export default withFormik({
   displayName: "traderListFilterForm",
   mapPropsToValues: () => ({
     traderLevel: { min: 1, max: 7 },
-    profitAvg: 20
+    profitAvg: 20,
+    sorting: ""
   })
 })(TraderListFilter);
---
+++ src/shared/components/form/gv-select/gv-select.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Select from "react-select";
+import "./gv-select.css";
+const GVSelect = ({ field, setFieldValue, onChange, onBlur, ...other }) => {
+  const handleChange = value => {
+    setFieldValue(field.name, value);
+    const newValue = value ? value.value : "";
+    onChange(newValue);
+  };
+
+  const handleBlur = () => {
+    onBlur(field.name, true);
+  };
+
+  return (
+    <Select
+      id={field.name}
+      name={field.name}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      {...other}
+    />
+  );
+};
+
+export default GVSelect;
