e0a25c653685c9daae4b6a771468a2a966ba182a
nkrivous <kolsln@list.ru>
2018-03-14 17:11:24 +0300

    Update wallet chart

--- src/modules/wallet/components/wallet/wallet-chart-container/wallet-chart/wallet-chart.js
+++ src/modules/wallet/components/wallet/wallet-chart-container/wallet-chart/wallet-chart.js
@@ -1,17 +1,20 @@
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import moment from "moment";
 import React from "react";
-import { LineChart, Line } from "recharts";
+
 import "./wallet-chart.css";
 
 const WalletChart = ({ data }) => {
-  const dataX = data.map(x => ({ x: x }));
+  const data1 = data.map(x => ({
+    amount: x.amount,
+    date: moment(x.date).format("MMMM Do HH:mm")
+  }));
   return (
-    <LineChart width={300} height={100} data={dataX} className="wallet-chart">
-      <Line
-        type="monotone"
-        dataKey="x"
-        dot={false}
-        className="wallet-chart__line"
-      />
+    <LineChart width={500} height={100} data={data1} className="wallet-chart">
+      <XAxis dataKey="date" type="category" axisLine={false} interval={7} />
+      <YAxis dataKey="amount" axisLine={false} tickCount={2} />
+      <Tooltip />
+      <Line type="monotone" dataKey="amount" stroke="#03bdaf" dot={false} />
     </LineChart>
   );
 };
