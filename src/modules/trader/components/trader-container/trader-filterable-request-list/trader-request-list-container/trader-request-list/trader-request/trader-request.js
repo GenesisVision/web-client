0f19f82d237265339a6bcf9b034b1ddffc652a0e
nkrivous <kolsln@list.ru>
2018-03-30 17:52:37 +0300

    Update currency in the My request grid

--- src/modules/trader/components/trader-container/trader-filterable-request-list/trader-request-list-container/trader-request-list/trader-request/trader-request.js
+++ src/modules/trader/components/trader-container/trader-filterable-request-list/trader-request-list-container/trader-request-list/trader-request/trader-request.js
@@ -1,29 +1,39 @@
-import React from "react";
 import moment from "moment";
+import React from "react";
 
 import "./trader-request.css";
 
 const TraderRequest = ({ request, token, cancelRequest }) => {
+  const getRequestCurrency = () => {
+    if (request.type === "Withdrawal") return token.tokenSymbol;
+    return "GVT";
+  };
+
   return (
     <div className="trader-request__row">
-      <div className="trader-request__type">{request.type}</div>
-      <div className="trader-request__amount">
-        {request.amount} {token.tokenSymbol}
+      <div className="trader-request__type trader-request__cell">
+        {request.type}
       </div>
-      <div className="trader-request__status">{request.status}</div>
-      <div className="trader-request__date">
+      <div className="trader-request__amount trader-request__cell">
+        {request.amount} {getRequestCurrency()}
+      </div>
+      <div className="trader-request__status trader-request__cell">
+        {request.status}
+      </div>
+      <div className="trader-request__date trader-request__cell">
         {moment(request.date).format("L")}
       </div>
-      {request.status === "New" ? (
-        <div className="trader-request__cancel">
+
+      <div className="trader-request__cancel trader-request__cell">
+        {request.status === "New" ? (
           <button
             className="gv-btn gv-btn-primary"
             onClick={cancelRequest(request.id)}
           >
             Cancel
           </button>
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   );
 };
