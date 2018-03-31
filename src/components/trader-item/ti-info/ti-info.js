b0224d23eef6d0e27e8efd6c09c62b6962e71d76
nkrivous <kolsln@list.ru>
2018-03-31 12:31:18 +0300

    Open program detail by clicking on program name

--- src/components/trader-item/ti-info/ti-info.js
+++ src/components/trader-item/ti-info/ti-info.js
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import React from "react";
 
 import DaysLeftWidget from "../../days-left-widget/days-left-widget";
+import replaceParams from "../../../utils/replace-params";
 import TraderAvatar from "../../trader-avatar/trader-avatar";
 
 import "./ti-info.css";
+import { TRADER_ROUTE } from "../../../modules/trader/trader.constants";
 
 const TIInfo = ({ idx, trader }) => {
   const renderDaysLeft = () => {
@@ -18,6 +21,10 @@ const TIInfo = ({ idx, trader }) => {
 
     return <div>The program is not enabled</div>;
   };
+
+  const traderRoute = replaceParams(TRADER_ROUTE, {
+    ":traderId": trader.id
+  });
   return (
     <div className="ti-info">
       <div className="ti-info__order">{idx}</div>
@@ -25,7 +32,9 @@ const TIInfo = ({ idx, trader }) => {
         <TraderAvatar imgUrl={trader.logo} level={trader.level} />
       </div>
       <div className="ti-info__name ti-name">
-        <div className="ti-name__title">{trader.title}</div>
+        <Link className="ti-name__title" to={traderRoute}>
+          {trader.title}
+        </Link>
         <div className="ti-name__description">{trader.description}</div>
         <div className="ti-name__eop">{renderDaysLeft()}</div>
       </div>
