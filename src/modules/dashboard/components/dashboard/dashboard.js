d9fc89eef1297edb7bf07e4c5a4ba287e6f5c5b5
Евгений Гребенщиков <evgenijgrebensikov@MacBook-Pro-Evgenij.local>
2018-04-25 14:06:16 +0300

    add dashboard filters

--- src/modules/dashboard/components/dashboard/dashboard.js
+++ src/modules/dashboard/components/dashboard/dashboard.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import DashboardChartContainer from "./dashboard-chart-container/dashboard-chart-container";
 import DashboardContainer from "./dashboard-container/dashboard-container";
+import DashboardFilters from './dashboard-filters/dashboard-filters';
 
 const Dashboard = () => {
   return (
     <div>
       <DashboardChartContainer />
+      <DashboardFilters />
       <DashboardContainer />
     </div>
   );
