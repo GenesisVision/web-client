96962fe7449a973912a9b17e290ffd5dc65e190b
Евгений Гребенщиков <evgenijgrebensikov@MacBook-Pro-Evgenij.local>
2018-04-26 16:01:36 +0300

    git add remove reducer and action

--- src/modules/dashboard/components/dashboard/dashboard.js
+++ src/modules/dashboard/components/dashboard/dashboard.js
@@ -4,18 +4,20 @@ import { Route } from "react-router-dom";
 import DashboardChartContainer from "./dashboard-chart-container/dashboard-chart-container";
 import DashboardContainer from "./dashboard-container/dashboard-container";
 import DashboardFilters from "./dashboard-filters/dashboard-filters";
-import FavoritePrograms from './dashboard-favorite-programs/dashboard-favorite-programs';
+import FavoritePrograms from "./dashboard-favorite-programs/dashboard-favorite-programs";
 
 const Dashboard = () => {
   return (
     <div>
-      <DashboardChartContainer />
       <DashboardFilters />
-      <Route exact path={`/dashboard`} component={DashboardContainer} />
       <Route
-        path={"/dashboard/favorite"}
-        component={FavoritePrograms}
+        exact
+        path={`/dashboard`}
+        component={() => {
+          return [<DashboardChartContainer />, <DashboardContainer />];
+        }}
       />
+      <Route path={"/dashboard/favorite"} component={FavoritePrograms} />
     </div>
   );
 };
