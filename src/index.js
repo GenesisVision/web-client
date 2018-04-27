732d6d0e397e676e9e66e1746c7a570af241b257
Евгений Гребенщиков <evgenijgrebensikov@MacBook-Pro-Evgenij.local>
2018-04-27 16:29:17 +0300

    add i18n

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './i18n';
 
 import "@fortawesome/fontawesome-free-webfonts/css/fa-solid.css";
 import "@fortawesome/fontawesome-free-webfonts/css/fontawesome.css";
