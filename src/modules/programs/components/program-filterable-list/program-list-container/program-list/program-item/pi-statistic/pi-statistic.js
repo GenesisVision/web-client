42e1b1ae1c66b6682793ec16fc19f7c0d8c62466
Евгений Гребенщиков <evgenijgrebensikov@MacBook-Pro-Evgenij.local>
2018-04-28 11:30:16 +0300

    program list i18n

--- src/modules/programs/components/program-filterable-list/program-list-container/program-list/program-item/pi-statistic/pi-statistic.js
+++ src/modules/programs/components/program-filterable-list/program-list-container/program-list/program-item/pi-statistic/pi-statistic.js
@@ -40,19 +40,51 @@ const TIStatistic = ({ t, trader }) => {
               displayType="text"
             />
           </div>
-          <div className="metric__description">Avg Profit</div>
+          <div className="metric__description">
+            <span id={`avgProfit_${trader.id}`}>
+              {t("program-statistic.avg-profit.text")}
+            </span>
+            <UncontrolledTooltip
+              placement="bottom"
+              target={`avgProfit_${trader.id}`}
+            >
+              {t("program-statistic.avg-profit.tooltip")}
+            </UncontrolledTooltip>
+          </div>
         </div>
       </div>
       <div className="pis-item">
         <div className="metric">
           <div className="metric__value">{trader.tradesCount}</div>
-          <div className="metric__description">Trades</div>
+          <div className="metric__description">
+            <span id={`trades_${trader.id}`}>
+              {t("program-statistic.trades.text")}
+            </span>
+            <UncontrolledTooltip
+              placement="bottom"
+              target={`trades_${trader.id}`}
+            >
+              {t("program-statistic.trades.tooltip")}
+            </UncontrolledTooltip>
+          </div>
         </div>
       </div>
       <div className="pis-item">
         <div className="metric">
           <div className="metric__value">{trader.investorsCount}</div>
-          <div className="metric__description">Investors</div>
+          <div className="metric__description">
+            <span id={`investors_${trader.id}`}>
+              {t("program-statistic.investors.text", {
+                count: trader.investorsCount
+              })}
+            </span>
+            <UncontrolledTooltip
+              placement="bottom"
+              target={`investors_${trader.id}`}
+            >
+              {t("program-statistic.investors.tooltip")}
+            </UncontrolledTooltip>
+          </div>
         </div>
       </div>
       <div className="pis-item">
@@ -88,7 +120,17 @@ const TIStatistic = ({ t, trader }) => {
             />
             <div className="metric__bubble">GVT</div>
           </div>
-          <div className="metric__description">Total Profit</div>
+          <div className="metric__description">
+            <span id={`totalProfit_${trader.id}`}>
+              {t("program-statistic.total-profit.text")}
+            </span>
+            <UncontrolledTooltip
+              placement="bottom"
+              target={`totalProfit_${trader.id}`}
+            >
+              {t("program-statistic.total-profit.tooltip")}
+            </UncontrolledTooltip>
+          </div>
         </div>
       </div>
     </div>
