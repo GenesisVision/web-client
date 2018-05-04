126c16f0d040230624dabb6d7c22d40efc9afa37
Евгений Гребенщиков <evgenijgrebensikov@MacBook-Pro-Evgenij.local>
2018-05-04 14:37:23 +0300

    add links in transactions

--- src/modules/wallet/helpers/transaction-helper.js
+++ src/modules/wallet/helpers/transaction-helper.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { PROGRAMS_ROUTE } from '../../programs/programs.constants';
 
 export const getTransactionTypeText = type => {
   var regex = /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g;
@@ -9,7 +11,7 @@ export const getProgramName = transaction => {
   if (transaction.investmentProgram) {
     return (
       <div className="wp-transaction__program">
-        {transaction.investmentProgram.title}
+        <Link to={`${PROGRAMS_ROUTE}/${transaction.investmentProgram.id}`}>{transaction.investmentProgram.title}</Link>
       </div>
     );
   }
