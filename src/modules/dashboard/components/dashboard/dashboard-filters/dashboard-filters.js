32d7d26b06b0d99e4b898d2dadb2f40f5e9c07cf
nkrivous <kolsln@list.ru>
2018-05-04 17:02:09 +0300

    Refactoring

--- src/modules/dashboard/components/dashboard/dashboard-filters/dashboard-filters.js
+++ src/modules/dashboard/components/dashboard/dashboard-filters/dashboard-filters.js
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import { translate } from "react-i18next";
 import { NavLink } from "react-router-dom";
-
-import "./dashboard-filters.css";
 import {
-  DASHBOARD_PORTFOLIO_ROUTE,
-  DASHBOARD_FAVOURITES_ROUTE
+  DASHBOARD_FAVOURITES_ROUTE,
+  DASHBOARD_PORTFOLIO_ROUTE
 } from "../../../dashboard.constants";
+import "./dashboard-filters.css";
 
-class DashboardFilters extends Component {
-  render() {
-    return (
-      <div className="dashboard-program-list-tabs">
-        <NavLink
-          to={DASHBOARD_PORTFOLIO_ROUTE}
-          activeClassName="dashboard-program-tab--active"
-          className={"dashboard-program-tab"}
-        >
-          Portfolio
-        </NavLink>
-        <NavLink
-          to={DASHBOARD_FAVOURITES_ROUTE}
-          activeClassName="dashboard-program-tab--active"
-          className={"dashboard-program-tab"}
-        >
-          Favorite programs
-        </NavLink>
-      </div>
-    );
-  }
-}
+const DashboardFilters = ({ t }) => (
+  <div className="dashboard-program-list-tabs">
+    <NavLink
+      to={DASHBOARD_PORTFOLIO_ROUTE}
+      activeClassName="dashboard-program-tab--active"
+      className={"dashboard-program-tab"}
+    >
+      {t("dashboard.portfolio")}
+    </NavLink>
+    <NavLink
+      to={DASHBOARD_FAVOURITES_ROUTE}
+      activeClassName="dashboard-program-tab--active"
+      className={"dashboard-program-tab"}
+    >
+      {t("dashboard.favorite-programs")}
+    </NavLink>
+  </div>
+);
 
-export default DashboardFilters;
+export default translate()(DashboardFilters);
