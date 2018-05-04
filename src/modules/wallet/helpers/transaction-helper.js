68af0d07cf170d1757a96375804423734e9aed87
evgreb <evgreb@gmail.com>
2018-05-04 17:03:39 +0300

    add links in transactions (#8)
    Fix misprint, add link to program detail in the wallet

--- src/modules/wallet/helpers/transaction-helper.js
+++ src/modules/wallet/helpers/transaction-helper.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import replaceParams from "../../../utils/replace-params";
+import { PROGRAM_ROUTE } from "../../program/program.constants";
 
 export const getTransactionTypeText = type => {
   var regex = /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g;
@@ -8,9 +11,14 @@ export const getTransactionTypeText = type => {
 export const getProgramName = transaction => {
   if (transaction.investmentProgram) {
     return (
-      <div className="wp-transaction__program">
+      <Link
+        className="wp-transaction__program"
+        to={replaceParams(PROGRAM_ROUTE, {
+          ":programId": transaction.investmentProgram.id
+        })}
+      >
         {transaction.investmentProgram.title}
-      </div>
+      </Link>
     );
   }
   return null;
