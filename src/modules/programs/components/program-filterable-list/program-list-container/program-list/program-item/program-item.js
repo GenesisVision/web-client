9b5d2b0309cd92296cc179cadb216c2d728dc0c3
Гребенщиков Евгений <evgreb@gmail.com>
2018-05-08 18:12:16 +0300

    program-item styles

--- src/modules/programs/components/program-filterable-list/program-list-container/program-list/program-item/program-item.js
+++ src/modules/programs/components/program-filterable-list/program-list-container/program-list/program-item/program-item.js
@@ -1,14 +1,19 @@
 import classnames from "classnames";
 import React from "react";
 
-import PTIButtons from "../../../../../../../components/program-item/pi-buttons/pi-buttons";
+// import PTIButtons from "../../../../../../../components/program-item/pi-buttons/pi-buttons";
 import PIChart from "../../../../../../../components/program-item/pi-chart/pi-chart";
 import PIInfo from "../../../../../../../components/program-item/pi-info/pi-info";
 import PIStatistic from "./pi-statistic/pi-statistic";
 import { Link } from "react-router-dom";
 import PIBookmark from "../../../../../../../components/program-item/pi-bookmark/pi-bookmark";
-
+import { PROGRAM_ROUTE } from "../../../../../../program/program.constants";
+import { LOGIN_ROUTE } from "../../../../../../login/login.constants";
+import replaceParams from "../../../../../../../utils/replace-params";
+import Button from "../../../../../../../components/button/button";
 import "./program-item.css";
+import DaysLeft from "../../../../../../../components/program-item/pi-days-left/pi-days-left";
+import PIAvatar from "../../../../../../../components/program-item/pi-avatar/pi-avatar";
 
 const ProgramItem = ({
   program,
@@ -16,42 +21,63 @@ const ProgramItem = ({
   openInvestPopup,
   toggleFavoriteProgram
 }) => {
+  const programRoute = replaceParams(PROGRAM_ROUTE, {
+    ":programId": program.id
+  });
+
   return (
     <div
       className={classnames("program-item", {
         "program-item--inactive": !program.isEnabled
       })}
     >
-      <PIInfo
-        order={program.order}
-        program={program}
-        isAuthenticated={isAuthenticated}
-        toggleFavoriteProgram={toggleFavoriteProgram}
-      />
+      <div className="program-item__order">{program.order}</div>
+      <Link to={programRoute}>
+        <PIAvatar
+          className="program-item__avatar"
+          url={program.logo}
+          level={program.level}
+          isTournament={program.isTournament}
+        />
+      </Link>
       <div className="program-item__content">
-        <div className="pi-info__name pi-name">
-          <div className="pi-name__title">
+        <div className="program-item__info">
+          <div className="program-item__title">
             {isAuthenticated && (
               <PIBookmark
+                className="program-item__bookmark"
                 isFavorite={program.isFavorite}
                 onClick={toggleFavoriteProgram(program)}
               />
             )}
-            <Link className="pi-name__link" to="">
+            <Link className="program-item__link" to={programRoute}>
               {program.title}
             </Link>
           </div>
-          {/* <div className="pi-name__eop">{renderDaysLeft()}</div> */}
+          <DaysLeft
+            className="program-item__days-left"
+            isEnabled={program.isEnabled}
+            startOfPeriod={program.startOfPeriod}
+            periodDuration={program.periodDuration}
+          />
         </div>
-        <PTIButtons
-          programId={program.id}
-          isInvestEnable={program.isInvestEnable}
-          isAuthenticated={isAuthenticated}
-          openInvestPopup={openInvestPopup}
-        />
-        <hr style={{ width: "100%" }} />
-        <PIChart data={program.equityChart} />
-        <PIStatistic trader={program} />
+
+        <div className="program-item__buttons">
+          <Button
+            primary
+            href={programRoute}
+            className="program-item__button"
+            label="View Profile"
+          />
+          <Button
+            primary
+            className="program-item__button"
+            label="Invest"
+            onClick={openInvestPopup(program.id)}
+            href={isAuthenticated ? "" : LOGIN_ROUTE}
+          />
+        </div>
+        <PIStatistic className="program-item__stats" trader={program} />
       </div>
     </div>
   );
