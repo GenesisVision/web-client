d3ac93132d142c016b3a511106cc2a0b4538dcb1
nkrivous <kolsln@list.ru>
2018-05-16 15:20:56 +0300

    Apply logarithmic range

--- src/modules/programs/components/program-filterable-list/program-list-filter-container/program-list-filter/program-list-filter.js
+++ src/modules/programs/components/program-filterable-list/program-list-filter-container/program-list-filter/program-list-filter.js
@@ -8,7 +8,9 @@ import {
   LEVEL_MAX_FILTER_VALUE,
   LEVEL_MIN_FILTER_VALUE,
   LEVEL_FILTER_NAME,
-  AVG_PROFIT_FILTER_NAME
+  AVG_PROFIT_FILTER_NAME,
+  AVG_PROFIT_MIN_FILTER_VALUE,
+  AVG_PROFIT_MAX_FILTER_VALUE
 } from "../../../../programs.constants";
 import FilterItem from "../../../../../filter-pane/components/filter-item/filter-item";
 
@@ -26,13 +28,45 @@ export const PROFIT_AVG_FILTER_FORM = "profitAvg";
 //   { value: "Desc", label: "Descending" }
 // ];
 
-const marks = {
-  "-10": "-10%",
-  "0": "0%",
-  "10": "10%",
-  "100": "100%",
-  "1000": "1k%"
+const pointsCount = 4;
+const sPoints = new Array(pointsCount).fill(0).map((x, idx) => idx);
+const mPointsReflection = x => x - 1;
+const sPointsReflection = x => x + 1;
+const mPoints = sPoints.map(mPointsReflection);
+const mPointsFormula = x => {
+  if (x === 0) return 0;
+  const value = Math.pow(10, Math.abs(x));
+  return x > 0 ? value : -value;
 };
+const mPointsReverseFormula = x => {
+  if (x === 0) return 0;
+  const value = Math.log10(Math.abs(x));
+  return x > 0 ? value : -value;
+};
+const sPointsFormula = x => {
+  const gap = 100 / (pointsCount - 1);
+  return x * gap;
+};
+const sPointsReverseFormula = x => {
+  const gap = 100 / (pointsCount - 1);
+  return x / gap;
+};
+const mPointsValues = mPoints.reduce((prev, curr, idx) => {
+  const gap = 100 / (pointsCount - 1);
+  const markValue = mPointsFormula(curr);
+  let markFormattedValue = markValue;
+  if (markValue / Math.pow(10, 3) > 1) {
+    markFormattedValue = markValue / Math.pow(10, 3) + "k";
+  }
+
+  prev[idx * gap] = `${markFormattedValue}%`;
+  return prev;
+}, {});
+
+const getSValue = mValue =>
+  sPointsFormula(sPointsReflection(mPointsReverseFormula(mValue)));
+const getMValue = sValue =>
+  mPointsFormula(mPointsReflection(sPointsReverseFormula(sValue)));
 
 const ProgramListFilter = ({ t, filtering, onChangeComplete }) => {
   return (
@@ -44,8 +78,8 @@ const ProgramListFilter = ({ t, filtering, onChangeComplete }) => {
         </div>
       </div>
       <FilterItem
-        name={t("programs-filtering.LEVEL_FILTER_NAME.name")}
-        description={t("programs-filtering.LEVEL_FILTER_NAME.description")}
+        name={t(`programs-filtering.${LEVEL_FILTER_NAME}.name`)}
+        description={t(`programs-filtering.${LEVEL_FILTER_NAME}.description`)}
         value={filtering.filters[LEVEL_FILTER_NAME]}
         defaultValue={filtering.defaultFilters[LEVEL_FILTER_NAME]}
         onFilterChange={onChangeComplete(LEVEL_FILTER_NAME)}
@@ -69,17 +103,20 @@ const ProgramListFilter = ({ t, filtering, onChangeComplete }) => {
       </FilterItem>
 
       <FilterItem
-        name={t("programs-filtering.AVG_PROFIT_FILTER_NAME.name")}
-        description={t("programs-filtering.AVG_PROFIT_FILTER_NAME.description")}
-        value={filtering.filters[AVG_PROFIT_FILTER_NAME]}
-        defaultValue={[-10, 1000]}
+        name={t(`programs-filtering.${AVG_PROFIT_FILTER_NAME}.name`)}
+        description={t(
+          `programs-filtering.${AVG_PROFIT_FILTER_NAME}.description`
+        )}
+        value={getSValue(filtering.filters[AVG_PROFIT_FILTER_NAME])}
+        defaultValue={[
+          getSValue(AVG_PROFIT_MIN_FILTER_VALUE),
+          getSValue(AVG_PROFIT_MAX_FILTER_VALUE)
+        ]}
         onFilterChange={onChangeComplete(AVG_PROFIT_FILTER_NAME)}
       >
         {(value, onChange) => (
           <Range
-            min={-10}
-            max={1000}
-            marks={marks}
+            marks={mPointsValues}
             value={value}
             onChange={onChange}
             pushable
@@ -91,7 +128,7 @@ const ProgramListFilter = ({ t, filtering, onChangeComplete }) => {
                     left: `${props.offset - 5}%`
                   }}
                 >
-                  {props.value}
+                  {+getMValue(props.value).toFixed(2)}
                 </span>
                 <Handle {...props} dragging="false" />
               </div>
