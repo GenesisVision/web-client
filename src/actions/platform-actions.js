894e251ecc85b22adcb71fccc630a29f80e7bc45
Гребенщиков Евгений <evgreb@gmail.com>
2018-07-25 16:14:03 +0300

    GVP-249 Login page - BLL
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/actions/platform-actions.js
+++ src/actions/platform-actions.js
@@ -3,8 +3,8 @@ import SwaggerInvestorApi from "../services/api-client/swagger-investor-api";
 export const PLATFORM_SETTINGS = "PLATFORM_SETTINGS";
 
 const fetchPlatformSettings = {
-  type: PLATFORM_SETTINGS,
-  payload: SwaggerInvestorApi.apiInvestorPlatformStatusGet()
+  type: PLATFORM_SETTINGS
+  // payload: SwaggerInvestorApi.apiInvestorPlatformStatusGet()
 };
 
 const platformActions = {
