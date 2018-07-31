799102fec4eb5aded97a1a097aa9b8fc24995270
Гребенщиков Евгений <evgreb@gmail.com>
2018-07-31 16:32:33 +0300

    GVP-252 Authorize service / new api
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/services/api-client/swagger-custom-client.js
+++ src/services/api-client/swagger-custom-client.js
@@ -1,10 +1,6 @@
 import { ApiClient } from "gv-api-web";
-import { API_VERSION } from "utils/constants";
 
 const apiClient = ApiClient.instance;
 apiClient.basePath = process.env.REACT_APP_API_URL;
-apiClient.defaultHeaders = {
-  "api-version": API_VERSION
-};
 
 export default apiClient;
