01459760b1dbf3609dfd29d2404bc947e57d66b0
Гребенщиков Евгений <evgreb@gmail.com>
2018-08-01 19:19:40 +0300

    GVP-256 Navigation bar
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,14 @@
 import "./header.scss";
 
 import GVLogo from "components/gv-logo/gv-logo";
+import Navigation from "components/navigation/navigation";
 import React from "react";
 
 const Header = () => {
   return (
     <div className={"header"}>
       <GVLogo />
+      <Navigation className={"header__navigation"} />
     </div>
   );
 };
