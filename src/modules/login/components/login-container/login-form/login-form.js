0b55c7c15bebeadfaf699eeba87129ca136978c3
Гребенщиков Евгений <evgreb@gmail.com>
2018-08-02 16:08:32 +0300

    GVP-250 Login page - UI
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/modules/login/components/login-container/login-form/login-form.js
+++ src/modules/login/components/login-container/login-form/login-form.js
@@ -1,24 +1,14 @@
 import Form from "components/form/form";
-import Input from "components/form/input/input";
 import { withFormik } from "formik";
-import { GVButton } from "gv-react-components";
+import { GVButton, GVFormikField, GVTextField } from "gv-react-components";
 import { FORGOT_PASSWORD_ROUTE } from "modules/password-reset/password-reset.constants";
 import React from "react";
+import { translate } from "react-i18next";
+import { compose } from "redux";
 
 import validationSchema from "./login-form.validators";
 
-const LoginForm = ({
-  values,
-  touched,
-  errors,
-  isSubmitting,
-  handleChange,
-  handleBlur,
-  handleSubmit,
-  handleReset,
-  dirty,
-  error
-}) => {
+const LoginForm = ({ t, isSubmitting, handleSubmit }) => {
   return (
     <Form
       className={"login-form"}
@@ -26,48 +16,52 @@ const LoginForm = ({
       onSubmit={handleSubmit}
       noValidate
     >
-      <Input
+      <GVFormikField
         id={"loginEmail"}
         type="email"
         name="email"
-        onChange={handleChange}
-        placeholder="Email address"
-        value={values.email}
+        placeholder={t("login-form.placeholder.email")}
         autoComplete="email"
+        className={"login-input gv-text-field"}
+        CustomComponent={GVTextField}
       />
-      <Input
+      <GVFormikField
         id={"loginPassword"}
         type="password"
         name="password"
-        onChange={handleChange}
-        placeholder="Password"
-        value={values.password}
+        placeholder={t("login-form.placeholder.password")}
         autoComplete="current-password"
+        className={"login-input gv-text-field"}
+        adornment={<a href={FORGOT_PASSWORD_ROUTE}>{t("login-form.forgot")}</a>}
+        CustomComponent={GVTextField}
       />
       <GVButton
+        className="login-button"
         id="loginSubmit"
         title="submit login form"
         color="primary"
         variant="contained"
         disabled={isSubmitting}
       >
-        login
-      </GVButton>
-      <GVButton variant="text" color="secondary" href={FORGOT_PASSWORD_ROUTE}>
-        Forgot?
+        {t("login-form.login")}
       </GVButton>
     </Form>
   );
 };
 
-export default withFormik({
-  displayName: "loginForm",
-  mapPropsToValues: () => ({
-    email: "",
-    password: ""
-  }),
-  validationSchema: validationSchema,
-  handleSubmit: (values, { props, setSubmitting }) => {
-    props.onSubmit(values, setSubmitting);
-  }
-})(LoginForm);
+const withTranslationAndFormik = compose(
+  translate(),
+  withFormik({
+    displayName: "loginForm",
+    mapPropsToValues: () => ({
+      email: "",
+      password: ""
+    }),
+    validationSchema: validationSchema,
+    handleSubmit: (values, { props, setSubmitting }) => {
+      props.onSubmit(values, setSubmitting);
+    }
+  })
+)(LoginForm);
+
+export default withTranslationAndFormik;
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -6,8 +6,9 @@ import Page from "components/page/page";
 import { GVButton, GVTab, GVTabs } from "gv-react-components";
 import LoginRoutes from "pages/login/login.routes";
 import React from "react";
+import { translate } from "react-i18next";
 
-const Login = () => {
+const Login = ({ t }) => {
   return (
     <Page className={"login"} title={"Login"}>
       <div className="login__left">
@@ -15,38 +16,37 @@ const Login = () => {
           <GvLogo />
           <GvBrand />
         </div>
-        <h2 className="login__description">
-          A Brief History
-          <br /> Of Creation
-        </h2>
+        <h2 className="login__description">{t("login-page.desc")}</h2>
       </div>
       <div className="login__right">
-        <h1 className="login__header">Login</h1>
-        <div className="login__tabs">
-          <GVTabs value={"investor"}>
-            <GVTab value={"investor"} label={"Investor"} />
-            <GVTab
-              value={"manager"}
-              label={
-                <a href={process.env.REACT_APP_MANAGER_PORTAL_URL}>Manager</a>
-              }
-            />
-          </GVTabs>
-        </div>
+        <div className="login__content">
+          <div className="login__header">
+            <h1>{t("login-page.header")}</h1>
+            <GVTabs value={"investor"}>
+              <GVTab value={"investor"} label={t("login-page.tabs.investor")} />
+              <GVTab
+                value={"manager"}
+                label={
+                  <a href={process.env.REACT_APP_MANAGER_PORTAL_URL}>
+                    {t("login-page.tabs.manager")}
+                  </a>
+                }
+              />
+            </GVTabs>
+          </div>
 
-        <LoginRoutes />
+          <LoginRoutes />
 
-        <div className="login__buttons">
-          <GVButton variant="text" color="secondary">
-            Don’t have an account?
-          </GVButton>
-          <GVButton variant="outlined" color="secondary">
-            Sign up
-          </GVButton>
+          <div className="sign-up">
+            <span className="sign-up__desc">{t("login-page.signup.desc")}</span>
+            <GVButton variant="outlined" color="secondary">
+              {t("login-page.signup.title")}
+            </GVButton>
+          </div>
         </div>
       </div>
     </Page>
   );
 };
 
-export default Login;
+export default translate()(Login);
