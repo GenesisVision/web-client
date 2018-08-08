46e69a5baefad235be456873be6622295cda35c5
Гребенщиков Евгений <evgreb@gmail.com>
2018-08-08 16:21:58 +0300

    GVP-250 Login page - UI / fix routes
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/pages/login/login-page.js
+++ src/pages/login/login-page.js
@@ -4,7 +4,7 @@ import LoginTabs from "components/login-tabs/login-tabs";
 import { GVButton, GVTab, GVTabs } from "gv-react-components";
 import LoginContainer from "modules/login/components/login-container/login-container";
 import { HOME_ROUTE } from "pages/root.routes";
-import { REGISTER_ROUTE } from "pages/signup/signup";
+import { REGISTER_ROUTE } from "pages/signup/signup.routes";
 import React, { Fragment } from "react";
 import { translate } from "react-i18next";
 import { Link } from "react-router-dom";
