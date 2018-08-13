381598908dae691f9b3ce6f215ea5c26ef9f82da
d.korobskoy@genesis.vision <d.korobskoy@genesis.vision>
2018-08-13 15:45:18 +0300

    [GVT 263] stylize pending page

--- src/pages/forgot-password/pending/pending.js
+++ src/pages/forgot-password/pending/pending.js
@@ -1,13 +1,15 @@
 import React from "react";
+import "./pending.scss";
 
 const ResetPasswordPending = () => {
   return (
     <div>
-      <h1>Restore password</h1>
-      <p>
+      <h1 className="password-pending__title">Restore password</h1>
+      <p className="password-pending__text">
         Please check your inbox for a confirmation email. Click the link in the
         email to create new password.
       </p>
+      <p className="password-pending__text">After you confirm click Continue</p>
     </div>
   );
 };
