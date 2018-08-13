a7f8475944995d1b15f52cf1c3f674e723f7dac1
Гребенщиков Евгений <evgreb@gmail.com>
2018-08-13 21:20:29 +0300

    GVP-254 Account Info - UI / fix navigation routes
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,9 +4,9 @@ import classnames from "classnames";
 import GVLogo from "components/gv-logo/gv-logo";
 import { DashboardIcon, ProgramIcon } from "components/icon/icon";
 import NavigationItem from "components/navigation/navigation-item";
-import { DASHBOARD_ROUTE } from "modules/dashboard/dashboard.constants";
-import { PROGRAMS_ROUTE } from "modules/programs/programs.constants";
-import { HOME_ROUTE } from "pages/root.constants";
+import { DASHBOARD_ROUTE } from "pages/dashboard/dashboard.routes";
+import { PROGRAMS_ROUTE } from "pages/programs/programs.routes";
+import { HOME_ROUTE } from "pages/root.routes";
 import React from "react";
 
 const Navigation = ({ className }) => {
