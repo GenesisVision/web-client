a01e741eef718fc80c041d5c31f844da48d1ea34
Гребенщиков Евгений <evgreb@gmail.com>
2018-08-15 17:21:36 +0300

    GVP-254 Account Info - UI / add popovers
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/modules/profile-header/components/profile-header.js
+++ src/modules/profile-header/components/profile-header.js
@@ -1,21 +1,58 @@
 import "./profile-header.scss";
 
-import Avatar from "components/avatar/avatar";
-import NotificationWidget from "components/notifications-widget/notification-widget";
+import classnames from "classnames";
+import { RingIcon } from "components/icon/icon";
+import ProfileWidget from "components/profile-widget/profile-widget";
 import WalletWidget from "components/wallet-widget/wallet-widget";
+import PropTypes from "prop-types";
 import React from "react";
 
-const ProfileHeader = props => {
-  return (
-    <div className={"profile-header"}>
-      <WalletWidget {...props.info} />
-      <NotificationWidget
-        notificationsAmount={props.info.notificationsAmount}
-        className={"profile-header__notifications"}
-      />
-      <Avatar url={props.info.avatar} />
-    </div>
-  );
+class ProfileHeader extends React.Component {
+  render() {
+    const {
+      availableGvt,
+      investedGvt,
+      totalBalanceGvt,
+      notificationsAmount,
+      avatar,
+      name
+    } = this.props;
+    const hasNotifications = notificationsAmount > 0;
+    return (
+      <div className={"profile-header"}>
+        <WalletWidget
+          availableGvt={availableGvt}
+          investedGvt={investedGvt}
+          totalBalanceGvt={totalBalanceGvt}
+        />
+        <button className={"profile-header__add profile-header__label"}>
+          +
+        </button>
+        <div
+          className={classnames("notifications-widget", {
+            "notifications-widget--has": hasNotifications
+          })}
+        >
+          <RingIcon />
+          <span className="notifications-widget__count profile-header__label">
+            {notificationsAmount}
+          </span>
+        </div>
+        <ProfileWidget avatar={avatar} name={name}/>
+      </div>
+    );
+  }
+}
+
+ProfileHeader.propTypes = {
+  availableGvt: PropTypes.number,
+  avatar: PropTypes.string,
+  favoritesCount: PropTypes.number,
+  id: PropTypes.string,
+  investedGvt: PropTypes.number,
+  name: PropTypes.string,
+  notificationsAmount: PropTypes.number,
+  totalBalanceGvt: PropTypes.number
 };
 
 export default ProfileHeader;
