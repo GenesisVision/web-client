a7ac5043492cb39bd22d74885c1b208987b25d2c
nkrivous <kolsln@list.ru>
2018-08-20 17:52:04 +0300

    Wrap dashboard in the page.

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,18 +1,19 @@
 import PropTypes from "prop-types";
-import React, { Fragment } from "react";
+import React from "react";
 import DocumentTitle from "react-document-title";
 import { translate } from "react-i18next";
 
 const Page = ({ t, title, children }) => {
   return (
-    <DocumentTitle title={t("app.title") + title}>
-      <Fragment>{children}</Fragment>
-    </DocumentTitle>
+    <DocumentTitle title={t("app.title") + title}>{children}</DocumentTitle>
   );
 };
 
 Page.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
   title: PropTypes.string.isRequired
 };
 
--- src/pages/dashboard/dashboard.page.js
+++ src/pages/dashboard/dashboard.page.js
@@ -1,27 +1,31 @@
 import "./dashboard.scss";
 
 import React from "react";
+import { translate } from "react-i18next";
 
+import Page from "../../components/page/page";
 import DashboardAssetsContainer from "./components/dashboard-assets/dashboard-assets-container";
 import DashboardChartContainer from "./components/dashboard-chart/dashboard-chart-container";
 import DashboardPortfolioEventsContainer from "./components/dashboard-portfolio-events/dashboard-portfolio-events-container";
 
-const DashboardPage = () => {
+const DashboardPage = ({ t }) => {
   return (
-    <div className="dashboard">
-      <div className="dashboard__row">
-        <div className="dashboard__chart">
-          <DashboardChartContainer />
+    <Page title={t("dashboard-page.title")}>
+      <div className="dashboard">
+        <div className="dashboard__row">
+          <div className="dashboard__chart">
+            <DashboardChartContainer />
+          </div>
+          <div className="dashboard__portfolio-events">
+            <DashboardPortfolioEventsContainer />
+          </div>
         </div>
-        <div className="dashboard__portfolio-events">
-          <DashboardPortfolioEventsContainer />
+        <div className="dashboard__assets">
+          <DashboardAssetsContainer />
         </div>
       </div>
-      <div className="dashboard__assets">
-        <DashboardAssetsContainer />
-      </div>
-    </div>
+    </Page>
   );
 };
 
-export default DashboardPage;
+export default translate()(DashboardPage);
