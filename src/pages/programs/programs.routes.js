f19bd09df6cef549c593a6b887ff00a8e46c4972
IntensNow <d.korobskoy@mail.ru>
2018-08-29 13:01:58 +0300

    GVP-274 add guid regexp

--- src/pages/programs/programs.routes.js
+++ src/pages/programs/programs.routes.js
@@ -2,6 +2,7 @@ import NotFoundPage from "pages/not-found/not-found.routes";
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
+import { GUID_REGEXP } from "../../utils/constants";
 import PrivateRoute from "../private-route";
 import ProgramDetailsPage from "./program-details/program-details.page";
 import ProgramsFacetPage from "./programs-facet/programs-facet.page";
@@ -12,6 +13,7 @@ export const PROGRAMS_EXPLORE_TAB_NAME = "explore";
 
 export const PROGRAMS_ROUTE = "/programs";
 export const PROGRAM_DETAILS_ROUTE = `${PROGRAMS_ROUTE}/:programId`;
+export const PROGRAM_DETAILS_ROUTE_REGEX = `${PROGRAMS_ROUTE}/:programId${GUID_REGEXP}`;
 export const PROGRAMS_FACET_ROUTE = `${PROGRAMS_ROUTE}/facet/:facetId`;
 export const PROGRAMS_FACET_ROUTE_REGEX = `${PROGRAMS_ROUTE}/facet/:facetId([0-9])`;
 export const PROGRAMS_TAB_ROUTE = `${PROGRAMS_ROUTE}/:tab`;
@@ -26,7 +28,7 @@ const ProgramsRoutes = () => (
       component={ProgramsPage}
     />
     <Route path={PROGRAMS_FACET_ROUTE_REGEX} component={ProgramsFacetPage} />
-    <Route path={PROGRAM_DETAILS_ROUTE} component={ProgramDetailsPage} />
+    <Route path={PROGRAM_DETAILS_ROUTE_REGEX} component={ProgramDetailsPage} />
     <Redirect
       exact
       from={PROGRAMS_ROUTE}
