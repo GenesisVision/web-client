29058e93b4009aac3407d9c157cfba47bcc3e173
nkrivous <kolsln@list.ru>
2018-09-18 16:41:52 +0300

    GVW-44 Api proxy

---
+++ src/services/api-client/api-proxy.js
@@ -0,0 +1,36 @@
+import { hideLoading, showLoading } from "react-redux-loading-bar";
+import store from "store";
+import handleErrorMessage from "utils/handle-error-message";
+
+const apiProxy = (api, dispatch) => {
+  return new Proxy(api, {
+    get(target, property) {
+      const origianlMethod = target[property];
+      if (typeof origianlMethod !== "function") return origianlMethod;
+      return (...args) => {
+        dispatch(showLoading());
+        return origianlMethod
+          .apply(target, args)
+          .then(result => {
+            return {
+              isPending: true,
+              data: result
+            };
+          })
+          .catch(ex => {
+            return {
+              isPending: false,
+              errorMessage: handleErrorMessage(ex.response)
+            };
+          })
+          .finally(() => {
+            dispatch(hideLoading());
+          });
+      };
+    }
+  });
+};
+
+const withApiProxy = api => apiProxy(api, store.dispatch);
+
+export default withApiProxy;
