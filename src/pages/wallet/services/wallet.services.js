9f3aa887ff117e0cbb94d22ce41817bb61f2f5bb
IntensNow <d.korobskoy@mail.ru>
2018-09-19 16:58:54 +0300

    GVW-23 add wallet service

--- src/pages/wallet/services/wallet.services.js
+++ src/pages/wallet/services/wallet.services.js
@@ -1,3 +1,4 @@
+import walletApi from "services/api-client/wallet-api";
 import authService from "services/auth-service";
 
 import * as actions from "../actions/wallet.actions";
@@ -7,4 +8,13 @@ export const fetchWalletBalance = () => (dispatch, getState) => {
   const { currentCurrency } = getState().accountSettings;
 
   dispatch(actions.fetchWalletBalance(currentCurrency, authorization));
+};
+
+export const fetchWalletTransactions = filters => {
+  const authorization = authService.getAuthArg();
+
+  return walletApi.v10WalletTransactionsGet(authorization).then(data => ({
+    items: data.transactions,
+    total: data.total
+  }));
 };
