248a1db724f1bbc93d7a9092cbec5da35a086deb
pavelzubov <pasha224@yandex.ru>
2018-09-21 09:31:58 +0300

    Add icons to component Icon

---
+++ src/components/icon/table-icon.js
@@ -0,0 +1,40 @@
+import { Icon } from "components/icon/icon";
+import React from "react";
+
+export const TableIcon = props => {
+  return (
+    <Icon type={"table"} {...props}>
+      <svg width="14" height="15" viewBox="0 0 14 15">
+        <g id="ui" fill="none" fillRule="evenodd" strokeLinejoin="round">
+          <g
+            id="1600_0_1_dashboard"
+            transform="translate(-1406 -963)"
+            stroke="#fff"
+            strokeWidth="1.5"
+          >
+            <g id="Group-22" transform="translate(120 880)">
+              <g id="Group-78" transform="translate(32 40)">
+                <g id="Group-21-Copy-2" transform="translate(1254 43)">
+                  <g id="icon_table">
+                    <path
+                      d="M0.388888889,1.24444444 L14,1.24444444"
+                      id="Stroke-3"
+                    />
+                    <path
+                      d="M0.388888889,7.54444444 L14,7.54444444"
+                      id="Stroke-5"
+                    />
+                    <path
+                      d="M0.388888889,13.8444444 L14,13.8444444"
+                      id="Stroke-7"
+                    />
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+    </Icon>
+  );
+};
