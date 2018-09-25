9b67d440711c8718897083409ae95281c13e6e2c
Гребенщиков Евгений <evgreb@gmail.com>
2018-09-25 16:43:27 +0300

    GVW-25 Wallet: withdraw popup / style fix
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/modules/program-deposit/components/program-deposit-form.js
+++ src/modules/program-deposit/components/program-deposit-form.js
@@ -40,22 +40,26 @@ const ProgramDepositForm = ({
         displayType="text"
       />
     </div>
-    <div className="invest-popup__entry">
-      {t("deposit-program.entry-fee")}
-      <span>
-        {info.entryFee} %{" "}
-        <NumberFormat
-          value={convertToCurrency(
-            calculateValueOfEntryFee(values.amount, info.entryFee),
-            info.rate
-          )}
-          prefix="("
-          suffix={` ${currency})`}
-          decimalScale={8}
-          displayType="text"
-        />
-      </span>
-    </div>
+    <ul className="dialog-list">
+      <li className="dialog-list__item">
+        <span className="dialog-list__title">
+          {t("deposit-program.entry-fee")}
+        </span>
+        <span className="dialog-list__value">
+          {info.entryFee} %{" "}
+          <NumberFormat
+            value={convertToCurrency(
+              calculateValueOfEntryFee(values.amount, info.entryFee),
+              info.rate
+            )}
+            prefix="("
+            suffix={` ${currency})`}
+            decimalScale={8}
+            displayType="text"
+          />
+        </span>
+      </li>
+    </ul>
     <div className="form-error">{errorMessage}</div>
     <div className="dialog__buttons">
       <GVButton
@@ -67,7 +71,7 @@ const ProgramDepositForm = ({
         {t("deposit-program.confirm")}
       </GVButton>
     </div>
-    <div className="invest-popup__period-ends">
+    <div className="dialog__info">
       {`${t("deposit-program.period")} ${moment(info.periodEnds).format(
         "DD.MM.YYYY, HH:mm"
       )}`}
