66127ee654e6c44ddba86d328d547e7134ff9693
pavelzubov <pasha224@yandex.ru>
2018-09-26 14:54:59 +0300

    Svg to jsx

--- src/components/icon/cards-icon.js
+++ src/components/icon/cards-icon.js
@@ -4,32 +4,18 @@ import React from "react";
 export const CardsIcon = props => {
   return (
     <Icon type={"cards"} {...props}>
-      <svg width="15px" height="15px" viewBox="0 0 15 15">
-        <g
-          id="ui"
-          stroke="none"
-          stroke-width="1"
-          fill="none"
-          fill-rule="evenodd"
-          opacity="1"
-          stroke-linejoin="round"
-        >
+      <svg width="15" height="15" viewBox="0 0 15 15">
+        <g id="ui" fill="none" fillRule="evenodd" strokeLinejoin="round">
           <g
             id="1600_0_1_dashboard"
-            transform="translate(-1434.000000, -963.000000)"
+            transform="translate(-1434 -963)"
             stroke="#fff"
-            stroke-width="1.5"
+            strokeWidth="1.5"
           >
-            <g id="Group-22" transform="translate(120.000000, 880.000000)">
-              <g id="Group-78" transform="translate(32.000000, 40.000000)">
-                <g
-                  id="Group-21-Copy-2"
-                  transform="translate(1254.000000, 43.000000)"
-                >
-                  <g
-                    id="icon_cards"
-                    transform="translate(29.000000, 1.000000)"
-                  >
+            <g id="Group-22" transform="translate(120 880)">
+              <g id="Group-78" transform="translate(32 40)">
+                <g id="Group-21-Copy-2" transform="translate(1254 43)">
+                  <g id="icon_cards" transform="translate(29 1)">
                     <polygon
                       id="Stroke-1"
                       points="0 3.54545455 3.54545455 3.54545455 3.54545455 0 0 0"
