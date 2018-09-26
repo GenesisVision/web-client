d8ffafec882acf3b47b1b2a3ae1fe7ee0dc60e97
nkrivous <kolsln@list.ru>
2018-09-26 13:49:32 +0300

    Refactor Invested section

--- src/pages/programs/program-details/components/program-details-description-section/program-details-description-section.js
+++ src/pages/programs/program-details/components/program-details-description-section/program-details-description-section.js
@@ -1,17 +1,7 @@
-import "./program-details-description.scss";
-
-import { RingIcon } from "components/icon/icon";
-import { GVButton, GVProgramAvatar } from "gv-react-components";
-import ProgramDepositContainer from "modules/program-deposit/program-deposit-container";
-import ProgramReinvestingWidget from "modules/program-reinvesting/components/program-reinvesting-widget";
 import { toggleReinvesting } from "modules/program-reinvesting/services/program-reinvesting.service";
-import { MANAGER_DETAILS_ROUTE } from "pages/manager/manager.page";
-import React, { Component, PureComponent } from "react";
-import { translate } from "react-i18next";
-import NumberFormat from "react-number-format";
-import { Link } from "react-router-dom";
-import replaceParams from "utils/replace-params";
+import React, { Fragment, PureComponent } from "react";
 
+import ProgramDetailsDescription from "./program-details-description/program-details-description";
 import ProgramDetailsInvestment from "./program-details-investment/program-details-investment";
 
 const composeInvestmentData = programDetails => {
@@ -28,13 +18,12 @@ const composeInvestmentData = programDetails => {
     status: personalProgramDetails.investmentProgramStatus
   };
 };
-
-class ProgramDetailsDescriptionSection extends Component {
+class ProgramDetailsDescriptionSection extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       ui: {
-        isOpenInvestmentPopup: false,
+        isReinvestPending: false,
         isPending: false
       },
       programDescription: null,
@@ -45,7 +34,7 @@ class ProgramDetailsDescriptionSection extends Component {
   static getDerivedStateFromProps(props, state) {
     let newState = {};
     if (state.prevProps !== props.programDescriptionData) {
-      newState.prevProps = props;
+      newState.prevProps = props.programDescriptionData;
       newState.programDescription = props.programDescriptionData.data;
       newState.ui = { isPending: props.programDescriptionData.isPending };
     }
@@ -54,139 +43,54 @@ class ProgramDetailsDescriptionSection extends Component {
   }
 
   handleOnReinvestingClick = () => {
-    const { programDescription } = this.state;
+    const { ui, programDescription } = this.state;
     const { id, isReinvesting } = programDescription;
     this.setState({
+      ui: { ...ui, isReinvestPending: true },
       programDescription: {
         ...programDescription,
         isReinvesting: !isReinvesting
       }
     });
-    //toggleReinvesting(id, isReinvesting);
-  };
-
-  handleOpenInvestmentPopup = () => {
-    this.setState({ isOpenInvestToProgramPopup: true });
-  };
-
-  composeManagerUrl = managerId => {
-    return replaceParams(MANAGER_DETAILS_ROUTE, {
-      ":managerId": managerId
-    });
+    toggleReinvesting(id, isReinvesting)
+      .catch(e => {
+        this.setState({
+          programDescription: {
+            ...programDescription,
+            isReinvesting: isReinvesting
+          }
+        });
+      })
+      .finally(() => {
+        this.setState({
+          ui: { ...ui, isReinvestPending: false }
+        });
+      });
   };
 
   render() {
-    const { t } = this.props;
     const { programDescription, ui } = this.state;
     if (!programDescription || ui.isPending) return null;
     const isInvested =
       programDescription.personalProgramDetails &&
       programDescription.personalProgramDetails.isInvested;
     return (
-      <div className="program-details-description">
-        <div className="program-details-description__left">
-          <GVProgramAvatar
-            url={programDescription.logo}
-            level={programDescription.level}
-            alt={programDescription.title}
-            size="big"
+      <Fragment>
+        <ProgramDetailsDescription
+          isInvested={isInvested}
+          programDescription={programDescription}
+          onReinvestingClick={this.handleOnReinvestingClick}
+          isReinvestPending={ui.isReinvestPending}
+        />
+        {isInvested && (
+          <ProgramDetailsInvestment
+            className={"program-details-description__your-investment"}
+            {...composeInvestmentData(programDescription)}
           />
-        </div>
-        <div className="program-details-description__main">
-          <h1 className="program-details-description__heading">
-            {programDescription.title}
-          </h1>
-          <Link to={this.composeManagerUrl(programDescription.manager.id)}>
-            <GVButton
-              variant="text"
-              className="program-details-description__author-btn"
-            >
-              {programDescription.manager.username}
-            </GVButton>
-          </Link>
-
-          <div className="program-details-description__info">
-            <h2 className="program-details-description__subheading">
-              {t("program-details-page.description.strategy")}
-            </h2>
-            <p className="program-details-description__text">
-              {programDescription.description}
-            </p>
-            <div className="program-details-description__short-statistic">
-              <div className="program-details-description__short-statistic-item">
-                <span className="program-details-description__short-statistic-subheading">
-                  {t("program-details-page.description.avToInvest")}
-                </span>
-                <NumberFormat
-                  value={programDescription.availableInvestment}
-                  displayType="text"
-                  suffix={` ${programDescription.currency}`}
-                />
-              </div>
-              <div className="program-details-description__short-statistic-item">
-                <span className="program-details-description__short-statistic-subheading">
-                  {t("program-details-page.description.entryFee")}
-                </span>
-                <NumberFormat
-                  value={programDescription.entryFee}
-                  displayType="text"
-                  suffix=" %"
-                />
-              </div>
-              <div className="program-details-description__short-statistic-item">
-                <span className="program-details-description__short-statistic-subheading">
-                  {t("program-details-page.description.successFee")}
-                </span>
-                <NumberFormat
-                  value={programDescription.successFee}
-                  displayType="text"
-                  suffix=" %"
-                />
-              </div>
-            </div>
-            <GVButton
-              className="program-details-description__invest-btn"
-              onClick={this.handleOpenInvestmentPopup}
-            >
-              {t("program-details-page.description.invest")}
-            </GVButton>
-
-            <ProgramDepositContainer
-              open={this.state.isOpenInvestToProgramPopup}
-              id={programDescription.id}
-              onClose={() =>
-                this.setState({ isOpenInvestToProgramPopup: false })
-              }
-            />
-
-            {isInvested && (
-              <ProgramReinvestingWidget
-                className="program-details-description__reinvest"
-                toggleReinvesting={this.handleOnReinvestingClick}
-                isReinvesting={programDescription.isReinvesting}
-              />
-            )}
-          </div>
-          {isInvested && (
-            <ProgramDetailsInvestment
-              className={"program-details-description__your-investment"}
-              {...composeInvestmentData(programDescription)}
-            />
-          )}
-        </div>
-
-        <div className="program-details-description__right">
-          <GVButton variant="text" color="secondary">
-            {t("program-details-page.description.addToFavorites")}
-          </GVButton>
-          <GVButton variant="text" color="secondary">
-            {t("program-details-page.description.notifications")}{" "}
-            <RingIcon className="program-details-description__notification-icon" />
-          </GVButton>
-        </div>
-      </div>
+        )}
+      </Fragment>
     );
   }
 }
 
-export default translate()(ProgramDetailsDescriptionSection);
+export default ProgramDetailsDescriptionSection;
