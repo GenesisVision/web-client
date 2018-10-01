8fc66dd52f463461ecceff19b8091ea1081136b3
Гребенщиков Евгений <evgreb@gmail.com>
2018-10-01 12:34:24 +0300

    GVW-28 Profile: settings / fix gv-qr
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/components/gv-qr/gv-qr.js
+++ src/components/gv-qr/gv-qr.js
@@ -1,26 +1,27 @@
+import { GVColors } from "gv-react-components";
 import PropTypes from "prop-types";
 import QRCode from "qrcode.react";
 import React from "react";
-
-const GVqr = ({ value, size }) => {
-  return (
-    <QRCode
-      className="gv-qr"
-      value={value}
-      bgColor={"transparent"}
-      fgColor={"white"}
-      size={size}
-    />
-  );
-};
-
+const GVqr = ({ value, size, figureColor, backgroundColor }) => (
+  <QRCode
+    className="gv-qr"
+    value={value}
+    bgColor={backgroundColor}
+    fgColor={figureColor}
+    size={size}
+  />
+);
 GVqr.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  size: PropTypes.number
+  size: PropTypes.number,
+  figureColor: PropTypes.string,
+  backgroundColor: PropTypes.string
 };
 
 GVqr.defaultProps = {
-  size: 150
+  size: 150,
+  figureColor: GVColors.$textAccentColor,
+  backgroundColor: "transparent"
 };
 
 export default GVqr;
