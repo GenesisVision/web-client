bdb0d57ed95ded3f25cc287b3778ccb44f320c7b
pavelzubov <pasha224@yandex.ru>
2018-10-03 10:41:39 +0300

    Change program-card component for dropdown

--- src/modules/programs-table/components/programs-table/program-card.js
+++ src/modules/programs-table/components/programs-table/program-card.js
@@ -1,102 +1,181 @@
 import ProgramPeriodPie from "components/program-period/program-period-pie/program-period-pie";
 import ProgramSimpleChart from "components/program-simple-chart/program-simple-chart";
-import React from "react";
+import React, { Component } from "react";
 import NumberFormat from "react-number-format";
-import { ActionsIcon } from "../../../../components/icon/actions-icon";
 import ProgramAvatar from "../../../../components/program-avatar/program-avatar";
+import Popover from "../../../../components/popover/popover";
+import { GVButton } from "gv-react-components";
+import { Link } from "react-router-dom";
+import replaceParams from "utils/replace-params";
+import { PROGRAM_DETAILS_ROUTE } from "pages/programs/programs.routes";
+import ProgramDepositContainer from "modules/program-deposit/program-deposit-container";
+import ProgramWithdrawContainer from "modules/program-withdraw/program-withdraw-container";
+import { ActionsCircleIcon } from "../../../../components/icon/actions-circle-icon";
 
-const ProgramCard = ({
-  program,
-  isAuthenticated,
-  toggleFavorite,
-  onExpandClick
-}) => {
-  return (
-    <div onClick={onExpandClick} className="programs-cards__card">
-      <div className="programs-cards__row">
-        <div className="programs-cards__avatar">
-          <ProgramAvatar
-            url={program.logo}
-            level={program.level}
-            alt={program.title}
-            size="medium"
-          />
-        </div>
-        <div className="programs-cards__names">
-          <div className="programs-cards__title">{program.title}</div>
-          <div className="programs-cards__name">Eddie Carr</div>
-        </div>
-        <div className="programs-cards__actions">
-          <ActionsIcon />
-        </div>
-      </div>
-      <div className="programs-cards__row">
-        <div className="programs-cards__chart">
-          <ProgramSimpleChart
-            data={program.chart}
-            isPositive={program.statistic.profitPercent >= 0}
-          />
-        </div>
-        <div className="programs-cards__chart-info">
-          <div className="programs-cards__chart-info--profit">
-            <NumberFormat
-              value={program.statistic.profitPercent}
-              suffix="%"
-              decimalScale={2}
-              displayType="text"
+class ProgramCard extends Component {
+  state = {
+    anchor: null,
+    isOpenInvestmentPopup: false,
+    isOpenWithdrawalPopup: false
+  };
+  handleOpenWithdrawalPopup = () => {
+    this.handleCloseDropdown();
+    this.setState({ isOpenWithdrawalPopup: true });
+  };
+  handleCloseWithdrawalPopup = () => {
+    this.setState({ isOpenWithdrawalPopup: false });
+  };
+  handleOpenInvestmentPopup = () => {
+    this.handleCloseDropdown();
+    this.setState({ isOpenInvestmentPopup: true });
+  };
+  handleCloseInvestmentPopup = () =>
+    this.setState({ isOpenInvestmentPopup: false });
+  handleOpenDropdown = event => this.setState({ anchor: event.currentTarget });
+  handleCloseDropdown = () => this.setState({ anchor: null });
+  render() {
+    const programDetailsUrl = replaceParams(PROGRAM_DETAILS_ROUTE, {
+      ":programId": this.props.program.id
+    });
+    return (
+      <div onClick={this.props.onExpandClick} className="programs-cards__card">
+        <div className="programs-cards__row">
+          <div className="programs-cards__avatar">
+            <ProgramAvatar
+              url={this.props.program.logo}
+              level={this.props.program.level}
+              alt={this.props.program.title}
+              size="medium"
             />
           </div>
-          <div className="programs-cards__chart-info--balance">
-            {program.statistic.balanceGVT.amount} GVT
+          <div className="programs-cards__names">
+            <div className="programs-cards__title">
+              {this.props.program.title}
+            </div>
+            <div className="programs-cards__name">
+              {this.props.program.manager.username}
+            </div>
+          </div>
+          <div className="programs-cards__actions">
+            <ActionsCircleIcon
+              primary={!!this.state.anchor}
+              onClick={this.handleOpenDropdown}
+            />
+            <Popover
+              horizontal="right"
+              vertical="bottom"
+              anchorEl={this.state.anchor}
+              noPadding
+              onClose={this.handleCloseDropdown}
+            >
+              <div className="popover-list">
+                {this.props.program.personalProgramDetails.isInvested && (
+                  <GVButton
+                    variant="text"
+                    color="secondary"
+                    onClick={this.handleOpenWithdrawalPopup}
+                  >
+                    Withdraw
+                  </GVButton>
+                )}
+                <GVButton
+                  variant="text"
+                  color="secondary"
+                  onClick={this.handleOpenInvestmentPopup}
+                >
+                  Invest
+                </GVButton>
+                <Link to={programDetailsUrl}>
+                  <GVButton
+                    variant="text"
+                    color="secondary"
+                    onClick={this.handleCloseDropdown}
+                  >
+                    Details
+                  </GVButton>
+                </Link>
+              </div>
+            </Popover>
           </div>
         </div>
+        <div className="programs-cards__row">
+          <div className="programs-cards__chart">
+            <ProgramSimpleChart
+              data={this.props.program.chart}
+              isPositive={this.props.program.statistic.profitPercent >= 0}
+            />
+          </div>
+          <div className="programs-cards__chart-info">
+            <div className="programs-cards__chart-info--profit">
+              <NumberFormat
+                value={this.props.program.statistic.profitPercent}
+                suffix="%"
+                decimalScale={2}
+                displayType="text"
+              />
+            </div>
+            <div className="programs-cards__chart-info--balance">
+              {this.props.program.statistic.balanceGVT.amount} GVT
+            </div>
+          </div>
+        </div>
+        <div className="programs-cards__table">
+          <table>
+            <tbody>
+              <tr>
+                <th className="programs-cards__table--title">Balance</th>
+                <th className="programs-cards__table--title">Investors</th>
+                <th className="programs-cards__table--title">Av. to invest</th>
+              </tr>
+              <tr>
+                <td>{this.props.program.statistic.balanceGVT.amount}</td>
+                <td>{this.props.program.statistic.investorsCount}</td>
+                <td>{this.props.program.availableInvestment}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="programs-cards__table">
+          <table>
+            <tbody>
+              <tr>
+                <th className="programs-cards__table--title">Trades</th>
+                <th className="programs-cards__table--title">Period</th>
+                <th className="programs-cards__table--title">D.down</th>
+              </tr>
+              <tr>
+                <td>{this.props.program.statistic.tradesCount}</td>
+                <td>
+                  <ProgramPeriodPie
+                    start={this.props.program.periodStarts}
+                    end={this.props.program.periodEnds}
+                  />
+                </td>
+                <td>
+                  <NumberFormat
+                    value={this.props.program.statistic.drawdownPercent}
+                    suffix="%"
+                    decimalScale={2}
+                    displayType="text"
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <ProgramDepositContainer
+          open={this.state.isOpenInvestmentPopup}
+          id={this.props.program.id}
+          onClose={this.handleCloseInvestmentPopup}
+        />
+        <ProgramWithdrawContainer
+          open={this.state.isOpenWithdrawalPopup}
+          id={this.props.program.id}
+          onClose={this.handleCloseWithdrawalPopup}
+        />
       </div>
-      <div className="programs-cards__table">
-        <table>
-          <tbody>
-            <tr>
-              <th className="programs-cards__table--title">Balance </th>
-              <th className="programs-cards__table--title">Investors</th>
-              <th className="programs-cards__table--title">Av. to invest</th>
-            </tr>
-            <tr>
-              <td>{program.statistic.balanceGVT.amount}</td>
-              <td>{program.statistic.investorsCount}</td>
-              <td>{program.availableInvestment}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="programs-cards__table">
-        <table>
-          <tbody>
-            <tr>
-              <th className="programs-cards__table--title">Trades</th>
-              <th className="programs-cards__table--title">Period</th>
-              <th className="programs-cards__table--title">D.down</th>
-            </tr>
-            <tr>
-              <td>{program.statistic.tradesCount}</td>
-              <td>
-                <ProgramPeriodPie
-                  start={program.periodStarts}
-                  end={program.periodEnds}
-                />
-              </td>
-              <td>
-                <NumberFormat
-                  value={program.statistic.drawdownPercent}
-                  suffix="%"
-                  decimalScale={2}
-                  displayType="text"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProgramCard;
