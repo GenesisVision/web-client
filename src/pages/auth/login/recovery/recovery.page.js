ce51d5c4f06ca38e95ef85aadfaf0745250e3707
IntensNow <d.korobskoy@mail.ru>
2018-10-04 12:31:29 +0300

    GVW-53 update login recovery page

--- src/pages/auth/login/recovery/recovery.page.js
+++ src/pages/auth/login/recovery/recovery.page.js
@@ -7,13 +7,7 @@ import RecoveryCodeContainer from "./recovery-code/recovery-code-container";
 
 const RecoveryPage = ({ t }) => {
   return (
-    <div className="login-recovery-page">
-      <h3 className="login-recovery-page__header">
-        {t("auth.login.recovery.title")}
-      </h3>
-      <p className="login-recovery-page__text">
-        {t("auth.login.recovery.text")}
-      </p>
+    <div className="recovery-page">
       <RecoveryCodeContainer />
     </div>
   );
