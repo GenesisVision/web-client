e1eb738bfa840de7c4e7bd438342de2c666aa9ec
IntensNow <d.korobskoy@mail.ru>
2018-10-05 15:23:30 +0300

    GVW-55 add notify message to copy on clipboard in the wallet-deposit

--- src/modules/wallet-add-funds/components/wallet-add-funds-container.js
+++ src/modules/wallet-add-funds/components/wallet-add-funds-container.js
@@ -2,8 +2,10 @@ import "./wallet-add-funds-form.scss";
 
 import WalletAddFundsForm from "modules/wallet-add-funds/components/wallet-add-funds-form";
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { walletApiProxy } from "services/api-client/wallet-api";
 import authService from "services/auth-service";
+import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 
 class WalletAddFundsContainer extends Component {
   state = {
@@ -21,8 +23,22 @@ class WalletAddFundsContainer extends Component {
   render() {
     if (!this.state.data) return null;
     const { wallets } = this.state.data;
-    return <WalletAddFundsForm wallets={wallets} />;
+    return (
+      <WalletAddFundsForm
+        wallets={wallets}
+        notifySuccess={this.props.notifySuccess}
+        notifyError={this.props.notifyError}
+      />
+    );
   }
 }
 
-export default WalletAddFundsContainer;
+const mapDispatchToProps = dispatch => ({
+  notifySuccess: text => dispatch(alertMessageActions.success(text)),
+  notifyError: text => dispatch(alertMessageActions.error(text))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(WalletAddFundsContainer);
