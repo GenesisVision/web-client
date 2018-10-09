826408dac42bf9df613de1ae84683c96586cc7a4
Гребенщиков Евгений <evgreb@gmail.com>
2018-10-09 21:03:38 +0300

    GVW-29 Profile: change password
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- src/pages/profile/profile.routes.js
+++ src/pages/profile/profile.routes.js
@@ -1,3 +1,5 @@
+import PasswordPage from "pages/profile/password/password.page";
+import { PASSWORD_ROUTE } from "pages/profile/password/password.page";
 import ProfilePage, { PROFILE_ROUTE } from "pages/profile/profile/profile.page";
 import { SETTINGS_ROUTE } from "pages/profile/settings/settings.page";
 import SettingsPage from "pages/profile/settings/settings.page";
@@ -6,6 +8,7 @@ import { Route, Switch } from "react-router-dom";
 
 const ProfileRoutes = () => (
   <Switch>
+    <Route path={PASSWORD_ROUTE} component={PasswordPage} />
     <Route path={SETTINGS_ROUTE} component={SettingsPage} />
     <Route path={PROFILE_ROUTE} component={ProfilePage} />
   </Switch>
