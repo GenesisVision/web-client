9dbcd0863e86f3426f70f15c02a1088e9c610674
nkrivous <kolsln@list.ru>
2018-10-12 10:28:10 +0300

    Fix dashboard events service

--- src/pages/dashboard/services/dashboard-events.services.js
+++ src/pages/dashboard/services/dashboard-events.services.js
@@ -2,7 +2,7 @@ import { investorApiProxy } from "services/api-client/investor-api";
 import authService from "services/auth-service";
 
 export const fetchPortfolioEvents = filters => {
-  const authorization = authService.getAuthArg() + 1;
+  const authorization = authService.getAuthArg();
 
   return investorApiProxy
     .v10InvestorPortfolioEventsGet(authorization, filters)
