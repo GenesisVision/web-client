adc54b3fb2d1964fc186169f49acf04a85e2c9f5
nkrivous <kolsln@list.ru>
2018-10-25 20:07:44 +0300

    Fix program url regex

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,4 +2,4 @@ export const AUTH_TOKEN = "gvInvestorAuthToken";
 export const GUID_REGEXP =
   "[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}";
 
-export const SLUG_URL_REGEXP = "^[-a-z0-9]{1,99}$";
+export const SLUG_URL_REGEXP = "[-a-z0-9]{1,99}";
