5ff44f36aa30c6e5013e7f5efca6be13475dc8df
nkrivous <kolsln@list.ru>
2018-10-29 16:04:24 +0300

    rollback routes

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,4 +1,4 @@
 import { createBrowserHistory as createHistory } from "history";
 
-const history = createHistory({ basename: "/investor" });
+const history = createHistory();
 export default history;
