9cad9480e2181bb4b44fbc9f1c4f6549592aa50f
nkrivous <kolsln@list.ru>
2018-10-29 16:00:15 +0300

    edit routes

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
-import { reactI18nextModule } from "react-i18next";
-import Backend from "i18next-xhr-backend";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-xhr-backend";
+import { reactI18nextModule } from "react-i18next";
 
 i18n
   .use(Backend)
@@ -25,6 +25,7 @@ i18n
       wait: true
     },
     backend: {
+      loadPath: "locales/{{lng}}/{{ns}}.json",
       queryStringParams: { v: "0.0.3" }
     }
   });
