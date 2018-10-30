433ec7a32adeb81f9f08096ab193fd365a839d1f
nkrivous <kolsln@list.ru>
2018-10-30 12:03:07 +0300

    Update period calculation

--- src/components/chart/chart-period/chart-period.helpers.js
+++ src/components/chart/chart-period/chart-period.helpers.js
@@ -16,7 +16,6 @@ export const getPeriodStartDate = periodType => {
     default:
       return moment()
         .subtract(1, `${periodType}s`)
-        .startOf("day")
         .toDate();
   }
 };
@@ -24,8 +23,5 @@ export const getPeriodStartDate = periodType => {
 export const DEFAULT_PERIOD = {
   type: ChartPeriodType.week,
   start: getPeriodStartDate(ChartPeriodType.week),
-  end: moment()
-    .add(1, "d")
-    .startOf("day")
-    .toDate()
+  end: moment().toDate()
 };
