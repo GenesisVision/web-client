64a3abb4424f44b3693ba78a50ff0d2606ed3353
pavelzubov <pasha224@yandex.ru>
2018-11-01 17:25:43 +0300

    Changed formatting values and styles in program-details

--- src/pages/programs/program-details/components/program-details-description-section/program-details-description/program-details-description.js
+++ src/pages/programs/program-details/components/program-details-description-section/program-details-description/program-details-description.js
@@ -104,7 +104,10 @@ class ProgramDetailsDescription extends PureComponent {
                 </div>
                 <div className="popover-levels__balance">
                   <NumberFormat
-                    value={formatValue(programDescription.availableInvestment)}
+                    value={formatValue(
+                      programDescription.availableInvestment,
+                      5
+                    )}
                     displayType="text"
                     suffix={` GVT`}
                   />
@@ -156,7 +159,7 @@ class ProgramDetailsDescription extends PureComponent {
                   {t("program-details-page.description.avToInvest")}
                 </span>
                 <NumberFormat
-                  value={formatValue(programDescription.availableInvestment)}
+                  value={formatValue(programDescription.availableInvestment, 2)}
                   displayType="text"
                   suffix={` GVT`}
                 />
@@ -166,7 +169,7 @@ class ProgramDetailsDescription extends PureComponent {
                   {t("program-details-page.description.entryFee")}
                 </span>
                 <NumberFormat
-                  value={programDescription.entryFee}
+                  value={formatValue(programDescription.entryFee, 2)}
                   displayType="text"
                   suffix=" %"
                 />
@@ -176,7 +179,7 @@ class ProgramDetailsDescription extends PureComponent {
                   {t("program-details-page.description.successFee")}
                 </span>
                 <NumberFormat
-                  value={programDescription.successFee}
+                  value={formatValue(programDescription.successFee, 2)}
                   displayType="text"
                   suffix=" %"
                 />
