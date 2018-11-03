718c26b3a89e7e344f76b48ddb9ce377e975b2ad
pavelzubov <pasha224@yandex.ru>
2018-11-03 15:58:10 +0300

    Fixed table view memoring

--- src/modules/table/components/table.js
+++ src/modules/table/components/table.js
@@ -15,8 +15,13 @@ export const PROGRAMS_VIEW = "programs_view";
 
 class Table extends Component {
   state = {
-    view: (this.isViewSwitchEnabled && loadData(PROGRAMS_VIEW)) || TABLE_VIEW
+    view: TABLE_VIEW
   };
+
+  componentDidMount() {
+    if (this.isViewSwitchEnabled)
+      this.setState({ view: loadData(PROGRAMS_VIEW) || TABLE_VIEW });
+  }
 
   changeView = view => {
     saveData(PROGRAMS_VIEW, view);
@@ -31,6 +36,7 @@ class Table extends Component {
 
   render() {
     const { view } = this.state;
+    console.log(view);
     return (
       <div className="table-wrapper">
         <TableToolbar
