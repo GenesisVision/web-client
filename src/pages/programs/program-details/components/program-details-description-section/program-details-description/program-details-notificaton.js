03500c5542387d5400472959b391d039d987e50e
pavelzubov <pasha224@yandex.ru>
2018-11-04 11:30:20 +0300

    Added self path for ProgramDetailsNotificaton

--- src/pages/programs/program-details/components/program-details-description-section/program-details-description/program-details-notificaton.js
+++ src/pages/programs/program-details/components/program-details-description-section/program-details-description/program-details-notificaton.js
@@ -4,10 +4,16 @@ import { translate } from "react-i18next";
 import { Link } from "react-router-dom";
 import { compose } from "redux";
 import isAuthenticated from "shared/decorators/is-authenticated";
+import connect from "react-redux/es/connect/connect";
 
-const ProgramDetailsNotificaton = ({ t, url }) => {
+const ProgramDetailsNotificaton = ({ t, url, pathname }) => {
   return (
-    <Link to={url}>
+    <Link
+      to={{
+        pathname: url,
+        state: pathname
+      }}
+    >
       <div className="program-details-description__control">
         <RingIcon className="program-details-description__control-icon" />
         <div className="program-details-description__control-text">
@@ -18,7 +24,13 @@ const ProgramDetailsNotificaton = ({ t, url }) => {
   );
 };
 
+const mapStateToProps = state => {
+  const { pathname } = state.routing.location;
+  return { pathname };
+};
+
 export default compose(
   translate(),
-  isAuthenticated
+  isAuthenticated,
+  connect(mapStateToProps)
 )(ProgramDetailsNotificaton);
