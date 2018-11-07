2d08ef5bd4938c219b10e4205cbaef64abebe838
pavelzubov <pasha224@yandex.ru>
2018-11-07 11:51:34 +0300

    Removed back button in search and notifications settings pages

--- src/pages/global-search/global-search.page.js
+++ src/pages/global-search/global-search.page.js
@@ -3,30 +3,11 @@ import React from "react";
 import { translate } from "react-i18next";
 
 import GlobalSearchResultConatiner from "./components/global-search-result/global-search-result-conatiner";
-import BackButton from "components/back-button/back-button";
-import { goBack } from "react-router-redux";
-import { bindActionCreators, compose } from "redux";
-import connect from "react-redux/es/connect/connect";
 
-const GlobalSearchPage = ({ t, backPath, service }) => (
+const GlobalSearchPage = ({ t }) => (
   <Page title={t("global-search-page.title")}>
-    {backPath && <BackButton backPath={backPath} goBack={service.goBack} />}
     <GlobalSearchResultConatiner />
   </Page>
 );
 
-const mapStateToProps = state => ({
-  backPath: state.routing.location.state
-});
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ goBack }, dispatch)
-});
-
-export default compose(
-  translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(GlobalSearchPage);
+export default translate()(GlobalSearchPage);
--- src/pages/notifications/general-settings/general-settings.page.js
+++ src/pages/notifications/general-settings/general-settings.page.js
@@ -2,33 +2,14 @@ import Page from "components/page/page";
 import NotificationSettingsContainer from "modules/notification-settings/notification-settings-container";
 import React from "react";
 import { translate } from "react-i18next";
-import { connect } from "react-redux";
-import { goBack } from "react-router-redux";
-import { bindActionCreators, compose } from "redux";
-import BackButton from "components/back-button/back-button";
 
-const NotificationsPage = ({ t, service, backPath }) => {
+const NotificationsPage = ({ t }) => {
   return (
     <Page title={t("notifications.title")}>
-      {backPath && <BackButton backPath={backPath} goBack={service.goBack} />}
       <h1>{t("notifications.title")}</h1>
       <NotificationSettingsContainer />
     </Page>
   );
 };
 
-const mapStateToProps = state => ({
-  backPath: state.routing.location.state
-});
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ goBack }, dispatch)
-});
-
-export default compose(
-  translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(NotificationsPage);
+export default translate()(NotificationsPage);
