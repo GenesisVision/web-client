aa2f463b284753cd4624fab5a948a91fadb69115
pavelzubov <pasha224@yandex.ru>
2018-11-07 22:02:36 +0300

    Removed backPath and goBack from PasswordPage

--- src/pages/profile/password/password.page.js
+++ src/pages/profile/password/password.page.js
@@ -2,33 +2,16 @@ import Page from "components/page/page";
 import PasswordChange from "modules/password-change/password-change";
 import React from "react";
 import { translate } from "react-i18next";
-import { bindActionCreators, compose } from "redux";
-import { connect } from "react-redux";
-import { goBack } from "react-router-redux";
 import BackButton from "components/back-button/back-button";
 
-const PasswordPage = ({ t, backPath, service }) => {
+const PasswordPage = ({ t }) => {
   return (
     <Page title={t("password-page.title")}>
-      <BackButton backPath={backPath} goBack={service.goBack} />
+      <BackButton />
       <h1>{t("password-page.title")}</h1>
       <PasswordChange />
     </Page>
   );
 };
 
-const mapStateToProps = state => ({
-  backPath: state.routing.location.state
-});
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ goBack }, dispatch)
-});
-
-export default compose(
-  translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(PasswordPage);
+export default translate()(PasswordPage);
