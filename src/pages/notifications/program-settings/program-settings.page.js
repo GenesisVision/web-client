189add286da35053ad6e0f96fbbd116464272240
pavelzubov <pasha224@yandex.ru>
2018-11-07 22:02:20 +0300

    Removed backPath and goBack from ProgramNotificationPage

--- src/pages/notifications/program-settings/program-settings.page.js
+++ src/pages/notifications/program-settings/program-settings.page.js
@@ -2,34 +2,17 @@ import Page from "components/page/page";
 import ProgramNotificationsContainer from "modules/program-notifications/program-notifications-container";
 import React from "react";
 import { translate } from "react-i18next";
-import { connect } from "react-redux";
-import { goBack } from "react-router-redux";
-import { bindActionCreators, compose } from "redux";
 import BackButton from "components/back-button/back-button";
 
-const ProgramNotificationPage = ({ t, match, service, backPath }) => {
+const ProgramNotificationPage = ({ t, match }) => {
   const { id } = match.params;
   return (
     <Page title={t("notifications.program.title")}>
-      <BackButton backPath={backPath} goBack={service.goBack} />
+      <BackButton />
       <h1>{t("notifications.program.title")}</h1>
       <ProgramNotificationsContainer id={id} />
     </Page>
   );
 };
 
-const mapStateToProps = state => ({
-  backPath: state.routing.location.state
-});
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ goBack }, dispatch)
-});
-
-export default compose(
-  translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(ProgramNotificationPage);
+export default translate()(ProgramNotificationPage);
