c09496a3bfc6724806fd381ba451770f18744855
pavelzubov <pasha224@yandex.ru>
2018-11-07 21:59:22 +0300

    Removed backPath and goBack from PortfolioEventsAll

--- src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.js
+++ src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.js
@@ -4,19 +4,16 @@ import Page from "components/page/page";
 import Surface from "components/surface/surface";
 import React from "react";
 import { translate } from "react-i18next";
-import { connect } from "react-redux";
-import { goBack } from "react-router-redux";
-import { bindActionCreators, compose } from "redux";
 
 import PortfolioEventsTableComponent from "./dashboard-portfolio-events-table/dashboard-portfolio-events-all-table";
 import BackButton from "components/back-button/back-button";
 
 export const PORTFOLIO_EVENTS_ALL_PAGE_ROUTE = "portfolio-events";
-const PortfolioEventsAllComponent = ({ service, t, backPath }) => {
+const PortfolioEventsAllComponent = ({ service, t }) => {
   return (
     <Page title={t("dashboard.portfolio-events.title")}>
       <div className="dashboard-portfolio-events-all">
-        <BackButton backPath={backPath} goBack={service.goBack} />
+        <BackButton />
         <h1 className="dashboard-portfolio-events-all__heading">
           {t("dashboard.portfolio-events.title")}
         </h1>
@@ -32,17 +29,4 @@ const PortfolioEventsAllComponent = ({ service, t, backPath }) => {
   );
 };
 
-const mapStateToProps = state => ({
-  backPath: state.routing.location.state
-});
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ goBack }, dispatch)
-});
-
-export default compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(translate()(PortfolioEventsAllComponent));
+export default translate()(PortfolioEventsAllComponent);
