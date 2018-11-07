428d42c66673e1c348ddc3ca1e9eacb722014f02
pavelzubov <pasha224@yandex.ru>
2018-11-07 12:10:15 +0300

    Corrected back button style

--- src/components/back-button/back-button.js
+++ src/components/back-button/back-button.js
@@ -7,9 +7,9 @@ import { translate } from "react-i18next";
 const BackButton = ({ t, goBack, backPath }) => {
   return (
     <div className="back-button">
-      <GVButton variant="text" onClick={goBack} color="secondary">
+      <GVButton variant="text" onClick={goBack} color="secondary" className="back-button__container">
         <span className="back-button__back-arrow">&larr;</span>
-        {t("buttons.back")}
+        <span className="back-button__back">{t("buttons.back")}</span>
       </GVButton>
       <div className="back-button__path">{backPath}</div>
     </div>
