7b466f1d796a1d9a4b96d93c7e51e508fc7b1138
pavelzubov <pasha224@yandex.ru>
2018-11-09 15:56:45 +0300

    Corrected validator

--- packages/manager-web-portal/src/shared/utils/validators/validators.js
+++ packages/manager-web-portal/src/shared/utils/validators/validators.js
@@ -1,13 +1,13 @@
-import Yup from "yup";
+import { string } from "yup";
 
-export const emailValidator = Yup.string()
+export const emailValidator = string()
   .email("Invalid email address.")
   .required("Email is required.");
 
-export const passwordValidator = Yup.string()
+export const passwordValidator = string()
   .min(8, "Password is weak.")
   .required("Password is required.");
 
-export const ethWalletValidator = Yup.string()
+export const ethWalletValidator = string()
   .matches(/^0x[a-fA-F0-9]{40}$/, "Invalid eth wallet address")
   .required("Wallet is required.");
