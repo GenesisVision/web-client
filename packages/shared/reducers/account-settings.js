f6bae5f107be5e78d7ed821dc8b92a6ed785c98a
pavelzubov <pasha224@yandex.ru>
2018-11-10 08:03:16 +0300

    Moved reducers to shared package

--- packages/shared/reducers/account-settings.js
+++ packages/shared/reducers/account-settings.js
@@ -1,5 +1,5 @@
 import { UPDATE_ACCOUNT_SETTINGS } from "shared/actions/account-settings-actions";
-import twoFactorReducer from "reducers/2fa-reducer";
+import twoFactorReducer from "shared/reducers/2fa-reducer";
 import { combineReducers } from "redux";
 import { ACCOUNT_CURRENCY_KEY } from "shared/middlewares/update-account-settings-middleware/update-account-settings-middleware";
 import { loadData } from "shared/utils/localstorage";
