e021dd03f18ea8331af06dbdef8249cf4960ab86
pavelzubov <pasha224@yandex.ru>
2018-11-11 11:15:42 +0300

    Added program withdraw to shared package

---
+++ packages/shared/components/program-withdraw/program-withdraw-popup.js
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+import React, { Component, Fragment } from "react";
+import ProgramWithdrawForm from "./program-withdraw-form";
+import ProgramWithdrawTop from "./program-withdraw-top";
+
+class ProgramWithdrawPopup extends Component {
+  state = {
+    data: undefined,
+    isPending: false,
+    errorMessage: null
+  };
+
+  componentDidMount() {
+    this.setState({ isPending: true });
+    this.props
+      .fetchInfo()
+      .then(data => {
+        this.setState({ ...data });
+      })
+      .catch(data => this.setState({ ...data }));
+  }
+
+  handleSumbit = amount => {
+    this.setState({ isPending: true });
+    return this.props
+      .withdraw(amount)
+      .then(data => this.setState({ ...data }))
+      .catch(data => this.setState({ ...data }));
+  };
+
+  render() {
+    if (!this.state.data) return null;
+    const { programCurrency, accountCurrency, error } = this.props;
+    const { title, availableToWithdraw, periodEnds, rate } = this.state.data;
+    return (
+      <Fragment>
+        <ProgramWithdrawTop
+          title={title}
+          availableToWithdraw={availableToWithdraw}
+          programCurrency={programCurrency}
+        />
+        <ProgramWithdrawForm
+          programCurrency={programCurrency}
+          accountCurrency={accountCurrency}
+          availableToWithdraw={availableToWithdraw}
+          periodEnds={periodEnds}
+          rate={rate}
+          onSubmit={this.handleSumbit}
+          errorMessage={error}
+          disabled={this.state.isPending}
+        />
+      </Fragment>
+    );
+  }
+}
+
+ProgramWithdrawPopup.propTypes = {
+  fetchInfo: PropTypes.func,
+  withdraw: PropTypes.func,
+  accountCurrency: PropTypes.string.isRequired,
+  programCurrency: PropTypes.string.isRequired
+};
+
+export default ProgramWithdrawPopup;
