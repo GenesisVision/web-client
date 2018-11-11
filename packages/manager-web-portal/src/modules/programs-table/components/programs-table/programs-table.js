6f7eef661b1fded4aba11cc2b77e32b32213e03e
pavelzubov <pasha224@yandex.ru>
2018-11-11 11:20:08 +0300

    Removed console.log

--- packages/manager-web-portal/src/modules/programs-table/components/programs-table/programs-table.js
+++ packages/manager-web-portal/src/modules/programs-table/components/programs-table/programs-table.js
@@ -36,7 +36,6 @@ const ProgramsTable = ({
     { value: undefined, label: "All" },
     ...currencies.map(x => ({ value: x, label: x }))
   ];
-  console.log(selectFilterValues)
   return (
     <Table
       title={title}
