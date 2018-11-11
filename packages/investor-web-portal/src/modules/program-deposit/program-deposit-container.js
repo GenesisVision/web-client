0590e76d9031694c2d7c6792600900c7b2c2b336
pavelzubov <pasha224@yandex.ru>
2018-11-11 09:37:34 +0300

    Changed ways to  program deposit to shared in investor-platform

--- packages/investor-web-portal/src/modules/program-deposit/program-deposit-container.js
+++ packages/investor-web-portal/src/modules/program-deposit/program-deposit-container.js
@@ -3,7 +3,7 @@ import {
   clearDepositProgramInfo,
   clearInvestSubmit
 } from "modules/program-deposit/actions/program-deposit.actions";
-import ProgramDepositPopup from "modules/program-deposit/components/program-deposit-popup";
+import ProgramDepositPopup from "shared/components/program-deposit/program-deposit-popup";
 import {
   getDepositFundInfoById,
   getDepositProgramInfoById,
@@ -37,6 +37,7 @@ const ProgramDepositContainer = props => {
   return (
     <Dialog open={props.open} onClose={handleClose}>
       <ProgramDepositPopup
+        entryFee
         submitInfo={props.submitInfo}
         currency={props.currency}
         info={props.info.data}
