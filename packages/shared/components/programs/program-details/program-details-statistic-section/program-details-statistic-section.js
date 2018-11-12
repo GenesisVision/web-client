5a54c662937ed7d543601b5043f18ae816f4ed50
pavelzubov <pasha224@yandex.ru>
2018-11-12 19:08:48 +0300

    -I Add program-details-statistic-section to shared

---
+++ packages/shared/components/programs/program-details/program-details-statistic-section/program-details-statistic-section.js
@@ -0,0 +1,66 @@
+import "./program-details-statistic-section.scss";
+
+import React, { PureComponent } from "react";
+
+import { DEFAULT_PERIOD } from "shared/components/chart/chart-period/chart-period.helpers";
+
+import ProgramDetailsChartSection from "./program-details-chart-section/program-details-chart-section";
+import ProgramDetailsStatistic from "./program-details-statistics/program-details-statistics";
+
+class ProgramDetailsStatisticSection extends PureComponent {
+  state = {
+    statisticData: { data: null, isPending: true },
+    profitChartData: { data: null, isPending: true },
+    balanceChartData: { data: null, isPending: true },
+    period: DEFAULT_PERIOD,
+    prevProps: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    let newState = {};
+    if (state.prevProps !== props) {
+      newState.prevProps = props;
+      newState.statisticData = props.statisticData;
+      newState.profitChartData = props.profitChartData;
+      newState.balanceChartData = props.balanceChartData;
+      return newState;
+    }
+
+    return state;
+  }
+
+  handlePeriodChange = period => {
+    const { programId, currency, getProgramStatistic } = this.props;
+
+    getProgramStatistic(programId, currency, period).then(data => {
+      this.setState({ period, ...data });
+    });
+  };
+
+  render() {
+    const { statisticData, profitChartData, balanceChartData } = this.state;
+    const { period } = this.state;
+    if (!profitChartData.data) return null;
+    return (
+      <div className="program-details-statistic-section">
+        <div className="program-details-statistic-section__statistic">
+          <ProgramDetailsStatistic
+            statisticData={statisticData}
+            profitChartData={profitChartData}
+            period={period}
+          />
+        </div>
+        <div className="program-details-statistic-section__chart">
+          <ProgramDetailsChartSection
+            profitChartData={profitChartData}
+            balanceChartData={balanceChartData}
+            period={period}
+            onPeriodChange={this.handlePeriodChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProgramDetailsStatisticSection;
