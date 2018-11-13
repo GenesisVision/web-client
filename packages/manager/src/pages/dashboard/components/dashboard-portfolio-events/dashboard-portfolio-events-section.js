ed862f60a39ad99abe35e80edd342faacfe0b98d
pavelzubov <pasha224@yandex.ru>
2018-11-13 10:49:33 +0300

    -M change DashboardPortfolioEventsContainer to DashboardPortfolioEventsSection in Dashboard page

---
+++ packages/manager/src/pages/dashboard/components/dashboard-portfolio-events/dashboard-portfolio-events-section.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { compose } from "redux";
+
+import DashboardPortfolioEventsContainer from "shared/components/dashboard/dashboard-portfolio-events/dashboard-portfolio-events-container";
+
+import { DASHBOARD_EVENTS_ROUTE } from "../../dashboard.routes";
+import { getPortfolioEvents } from "../../services/dashboard.service";
+
+class DashboardPortfolioEventsSection extends Component {
+  componentDidMount() {
+    const { service } = this.props;
+    service.getPortfolioEvents();
+  }
+
+  render() {
+    const { title, isPending, data } = this.props;
+    return (
+      <DashboardPortfolioEventsContainer
+        fullEventsUrl={DASHBOARD_EVENTS_ROUTE}
+        title={title}
+        isPending={isPending}
+        data={data}
+        manager
+      />
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  service: bindActionCreators({ getPortfolioEvents }, dispatch)
+});
+const mapStateToProps = state => {
+  const { isPending, data } = state.dashboard.eventsData;
+  return { isPending, data };
+};
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(DashboardPortfolioEventsSection);
