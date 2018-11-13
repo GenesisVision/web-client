802fb0fd545e948b8a34ea469fac45c9948c399d
Гребенщиков Евгений <evgreb@gmail.com>
2018-11-13 16:44:33 +0300

    GVW-172 fix prettier
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- packages/investor/src/index.js
+++ packages/investor/src/index.js
@@ -1,7 +1,8 @@
-import "./i18n";
-import "./setupMoment";
-import "./index.scss";
 import "shared/styles/gv-styles.scss";
+
+import "./setupMoment";
+import "./i18n";
+import "./index.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
