01f7cf22ff634a8d2e83e5e1d1391fe2ac659914
pavelzubov <pasha224@yandex.ru>
2018-11-13 13:27:32 +0300

    Add currency-select to shared

---
+++ packages/shared/modules/currency-select/currency-select.constants.js
@@ -0,0 +1,17 @@
+export const HEADER_CURRENCY_VALUES = {
+  USDT: "USD Tether",
+  BTC: "Bitcoin",
+  ETH: "Ethereum",
+  ADA: "Cardano",
+  USD: "US Dollar",
+  EUR: "Euro",
+  XRP: "Ripple",
+  BCH: "Bitcoin Cash",
+  LTC: "Litecoin",
+  DOGE: "Dogecoin"
+};
+
+export const CURRENCY_VALUES = {
+  GVT: "Genesis Vision Token",
+  ...HEADER_CURRENCY_VALUES
+};
