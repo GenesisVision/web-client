f78b5cd0650776576e266164c58a2c60a2ffadc4
nkrivous <kolsln@list.ru>
2018-11-13 15:18:45 +0300

    Table refactoring

--- packages/investor/src/modules/table/actions/table.actions.js
+++ packages/investor/src/modules/table/actions/table.actions.js
@@ -1,36 +1,9 @@
-import { composeFilteringActionType } from "shared/components/table/helpers/filtering.helpers";
-import { composePaingActionType } from "shared/components/table/helpers/paging.helpers";
-import { composeSortingActionType } from "shared/components/table/helpers/sorting.helpers";
+import { composeFiltersActionType } from "../reducers/table-filters.reducer";
 
-export const tableActionsFactory = actionType => {
-  const tablePagingActionType = composePaingActionType(actionType);
-  const tableFilteringActionType = composeFilteringActionType(actionType);
-  const tableSortingActionType = composeSortingActionType(actionType);
-
-  const updatePaging = paging => {
-    return {
-      type: tablePagingActionType,
-      paging
-    };
-  };
-
-  const updateSorting = sorting => {
-    return {
-      type: tableSortingActionType,
-      payload: sorting
-    };
-  };
-
-  const updateFilter = filter => {
-    return {
-      type: tableFilteringActionType,
-      payload: filter
-    };
-  };
-
+export const updateFilters = (filters, actionType) => {
+  const filterActionType = composeFiltersActionType(actionType);
   return {
-    updateSorting,
-    updatePaging,
-    updateFilter
+    type: filterActionType,
+    filters
   };
 };
--- packages/investor/src/modules/table/reducers/table.reducer.js
+++ packages/investor/src/modules/table/reducers/table.reducer.js
@@ -1,26 +1,41 @@
 import { combineReducers } from "redux";
 import apiReducerFactory from "shared/reducers/api-reducer/api-reducer";
 
+import clearableReducer from "shared/reducers/clearable.reducer";
+
 import tableFiltersReducer from "./table-filters.reducer";
 
-const tableReducerFactory = ({ type, paging, sorting, filtering }) => {
-  const tableReducer = {
-    itemsData: apiReducerFactory({
-      apiType: type
-    }),
-    filters: tableFiltersReducer({
-      type: type + "_FILTERS",
-      filters: {
-        paging,
-        sorting,
-        filtering
-      }
-    })
-  };
+const tableReducerFactory = ({
+  type,
+  paging,
+  sorting,
+  filtering,
+  defaultFilters,
+  clearable = false,
+  clearableAction
+}) => {
+  const clearableWrapper = clearable ? clearableReducer : f => f;
 
-  return combineReducers({
-    ...tableReducer
-  });
+  return clearableWrapper(
+    combineReducers({
+      itemsData: apiReducerFactory({
+        apiType: type
+      }),
+      filters: tableFiltersReducer({
+        type,
+        filters: {
+          paging,
+          sorting,
+          filtering
+        }
+      }),
+      defaults: () => ({
+        defaultFilters,
+        type
+      })
+    }),
+    clearableAction
+  );
 };
 
 export default tableReducerFactory;
