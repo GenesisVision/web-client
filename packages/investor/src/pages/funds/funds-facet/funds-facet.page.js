ea1e9b74fd08009e8d28269e65e4fa1d64211d40
pavelzubov <pasha224@yandex.ru>
2018-11-14 16:26:53 +0300

    -I Change way to mount FundsFacetContainer

--- packages/investor/src/pages/funds/funds-facet/funds-facet.page.js
+++ packages/investor/src/pages/funds/funds-facet/funds-facet.page.js
@@ -2,11 +2,18 @@ import Page from "shared/components/page/page";
 import React from "react";
 import { translate } from "react-i18next";
 
-import FundsFacetContainer from "./components/funds-facet/funds-facet-container";
+import FundsFacetContainer from "shared/components/funds/funds-facet/funds-facet-container";
+import FundsTableContainer from "modules/funds-table/components/funds-table/funds-table-container";
+import { getPrograms } from "modules/programs-table/services/programs-table.service";
+import { getCurrentFacet } from "./services/funds-facet.service";
 
 const FundsFacetPage = ({ t }) => (
   <Page title={t("funds-page.title")}>
-    <FundsFacetContainer />
+    <FundsFacetContainer
+      getPrograms={getPrograms}
+      getCurrentFacet={getCurrentFacet}
+      FundsTableContainer={FundsTableContainer}
+    />
   </Page>
 );
 export default translate()(FundsFacetPage);
