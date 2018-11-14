4cf2529667a289a7f4f18808838833ef5ee3391c
pavelzubov <pasha224@yandex.ru>
2018-11-14 13:21:21 +0300

    -M Change ways to all-events table

--- packages/manager/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.js
+++ packages/manager/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.js
@@ -5,8 +5,8 @@ import Surface from "shared/components/surface/surface";
 import React from "react";
 import { translate } from "react-i18next";
 
-import PortfolioEventsTableComponent from "./dashboard-portfolio-events-table/dashboard-portfolio-events-all-table";
-
+import { fetchPortfolioEvents } from "../../services/dashboard-events.services";
+import PortfolioEventsTableComponent from "shared/components/dashboard/dashboard-portfolio-events-all/dashboard-portfolio-events-table/dashboard-portfolio-events-all-table";
 export const PORTFOLIO_EVENTS_ALL_PAGE_ROUTE = "portfolio-events";
 const PortfolioEventsAllComponent = ({ t }) => {
   return (
@@ -17,6 +17,7 @@ const PortfolioEventsAllComponent = ({ t }) => {
         </h1>
         <Surface>
           <PortfolioEventsTableComponent
+            fetchPortfolioEvents={fetchPortfolioEvents}
             title={t("dashboard.portfolio-events.table-title")}
             className="portfolio-events-all-table"
             dateRangeStartLabel={t("filters.date-range.account-creation")}
