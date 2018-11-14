c01d95ac032e62b1fe516b534f8285f4753e457e
pavelzubov <pasha224@yandex.ru>
2018-11-14 16:11:32 +0300

    -M Change way to ProgramsFacetContainer to shared

--- packages/investor/src/pages/programs/programs-facet/programs-facet.page.js
+++ packages/investor/src/pages/programs/programs-facet/programs-facet.page.js
@@ -2,7 +2,7 @@ import Page from "shared/components/page/page";
 import React from "react";
 import { translate } from "react-i18next";
 
-import ProgramsFacetContainer from "./components/programs-facet/programs-facet-container";
+import ProgramsFacetContainer from "shared/components/programs/programs-facet/programs-facet-container";
 import ProgramsContainer from "modules/programs-table/components/programs-table/programs-table-container";
 import { getPrograms } from "modules/programs-table/services/programs-table.service";
 import { getCurrentFacet } from "./services/programs-facet.service";
