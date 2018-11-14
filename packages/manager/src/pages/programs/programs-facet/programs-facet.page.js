ea45f1c58ce0968bb968671b6118af7cf35c3ee6
pavelzubov <pasha224@yandex.ru>
2018-11-14 16:02:09 +0300

    -M Change way to mount ProgramsFacetContainer

--- packages/manager/src/pages/programs/programs-facet/programs-facet.page.js
+++ packages/manager/src/pages/programs/programs-facet/programs-facet.page.js
@@ -3,10 +3,17 @@ import React from "react";
 import { translate } from "react-i18next";
 
 import ProgramsFacetContainer from "./components/programs-facet/programs-facet-container";
+import ProgramsContainer from "modules/programs-table/components/programs-table/programs-table-container";
+import { getPrograms } from "modules/programs-table/services/programs-table.service";
+import { getCurrentFacet } from "./services/programs-facet.service";
 
 const ProgramsFacetPage = ({ t }) => (
   <Page title={t("programs-page.title")}>
-    <ProgramsFacetContainer />
+    <ProgramsFacetContainer
+      ProgramsContainer={ProgramsContainer}
+      getPrograms={getPrograms}
+      getCurrentFacet={getCurrentFacet}
+    />
   </Page>
 );
 export default translate()(ProgramsFacetPage);
