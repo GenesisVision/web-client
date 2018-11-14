28d4270305931aed7e55fac7756b7b80982aba5a
pavelzubov <pasha224@yandex.ru>
2018-11-14 14:47:28 +0300

    -M remove unused dashboard-programs

--- packages/manager/src/pages/dashboard/components/dashboard-assets/dashboard-programs/dashboard-programs.selector.js
+++
@@ -1,9 +0,0 @@
-import { tableSelectorCreator } from "shared/components/table/helpers/table.selector";
-
-const dashboardProgramsSelector = state => state.dashboard.programs;
-const dashboardProgramsTableSelector = tableSelectorCreator(
-  dashboardProgramsSelector,
-  "programs"
-);
-
-export default dashboardProgramsTableSelector;
