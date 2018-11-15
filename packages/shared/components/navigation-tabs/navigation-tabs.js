56fb3ac14057a5b9a9d985c2561899d5e1c587f1
pavelzubov <pasha224@yandex.ru>
2018-11-15 11:21:22 +0300

    Add NavigationTabsContainer to shared

---
+++ packages/shared/components/navigation-tabs/navigation-tabs.js
@@ -0,0 +1,45 @@
+import { GVTab, GVTabs } from "gv-react-components";
+import React from "react";
+import { translate } from "react-i18next";
+import { Link } from "react-router-dom";
+
+import replaceParams from "shared/utils/replace-params";
+
+const NavigationTabs = ({
+  t,
+  exploreTabName,
+  fundsTabRoute,
+  fundsFavoritesTabName,
+  tab = exploreTabName
+}) => {
+  return (
+    <GVTabs value={tab}>
+      <GVTab
+        value={exploreTabName}
+        label={
+          <Link
+            to={replaceParams(fundsTabRoute, {
+              ":tab": exploreTabName
+            })}
+          >
+            {t("funds-page.tabs.explore")}
+          </Link>
+        }
+      />
+      <GVTab
+        value={fundsFavoritesTabName}
+        label={
+          <Link
+            to={replaceParams(fundsTabRoute, {
+              ":tab": fundsFavoritesTabName
+            })}
+          >
+            {t("funds-page.tabs.favorites")}
+          </Link>
+        }
+      />
+    </GVTabs>
+  );
+};
+
+export default translate()(NavigationTabs);
