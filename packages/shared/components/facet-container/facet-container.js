5b2c299d333850dd00e88a51ac1122e53dd6c755
pavelzubov <pasha224@yandex.ru>
2018-11-15 09:39:43 +0300

    Create common facet container

---
+++ packages/shared/components/facet-container/facet-container.js
@@ -0,0 +1,59 @@
+import NotFoundPage from "shared/components/not-found/not-found.routes";
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { bindActionCreators, compose } from "redux";
+
+class FacetContainer extends Component {
+  state = {
+    facetData: null
+  };
+
+  componentDidMount() {
+    const { service, facets } = this.props;
+    if (facets !== null) {
+      this.setState({ facetData: service.getCurrentFacet() });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { service, facets } = this.props;
+    if (prevProps.facets !== facets) {
+      this.setState({ facetData: service.getCurrentFacet() });
+    }
+  }
+
+  render() {
+    const { TableContainer } = this.props;
+    const { facetData } = this.state;
+    if (!facetData || facetData.isPending) return null;
+    if (facetData.notFound) return <NotFoundPage />;
+    return (
+      <Fragment>
+        <TableContainer title={facetData.facet.title} />
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  const { data } = state.platformData;
+  let facets = null;
+  if (data) facets = data[props.asset];
+  return { facets };
+};
+
+const mapDispatchToProps = (dispatch, props) => {
+  const { getCurrentFacet, getPrograms } = props;
+  return {
+    service: bindActionCreators({ getCurrentFacet, getPrograms }, dispatch)
+  };
+};
+
+export default compose(
+  withRouter,
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(FacetContainer);
