ae05683525b23603ee17859486a19cd249b113d7
pavelzubov <pasha224@yandex.ru>
2018-11-15 09:17:52 +0300

    -I Change ways to not-found to shared

--- packages/investor/src/pages/funds/funds.routes.js
+++ packages/investor/src/pages/funds/funds.routes.js
@@ -1,4 +1,4 @@
-import NotFoundPage from "pages/not-found/not-found.routes";
+import NotFoundPage from "shared/components/not-found/not-found.routes";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
