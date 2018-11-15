860e2104d06c27cf41c864b8df5456d190be444a
pavelzubov <pasha224@yandex.ru>
2018-11-15 09:42:03 +0300

    -M Change ways to FacetContainer

--- packages/manager/src/pages/programs/programs-facet/programs-facet.page.js
+++ packages/manager/src/pages/programs/programs-facet/programs-facet.page.js
@@ -2,15 +2,16 @@ import Page from "shared/components/page/page";
 import React from "react";
 import { translate } from "react-i18next";
 
-import ProgramsFacetContainer from "shared/components/programs/programs-facet/programs-facet-container";
+import FacetContainer from "shared/components/facet-container/facet-container";
 import ProgramsContainer from "modules/programs-table/components/programs-table/programs-table-container";
 import { getPrograms } from "modules/programs-table/services/programs-table.service";
 import { getCurrentFacet } from "./services/programs-facet.service";
 
 const ProgramsFacetPage = ({ t }) => (
   <Page title={t("programs-page.title")}>
-    <ProgramsFacetContainer
-      ProgramsContainer={ProgramsContainer}
+    <FacetContainer
+      asset={"programsFacets"}
+      TableContainer={ProgramsContainer}
       getPrograms={getPrograms}
       getCurrentFacet={getCurrentFacet}
     />
