5f184f0e4406f62573128ad9ff7bc3773c3fc1a4
pavelzubov <pasha224@yandex.ru>
2018-11-15 11:26:52 +0300

    Change ways to assets tabs to shared

--- packages/investor/src/pages/funds/funds/funds.page.js
+++ packages/investor/src/pages/funds/funds/funds.page.js
@@ -4,13 +4,18 @@ import React from "react";
 import { translate } from "react-i18next";
 
 import FacetCardsContainer from "shared/components/facet-cards/faset-cards-container";
-import FundsNavigationTabsContainer from "./components/funds-navigation-tabs/funds-navigation-tabs-container";
+import NavigationTabsContainer from "shared/components/navigation-tabs/navigation-tabs-container";
+import * as routes from "../funds.routes";
 
 const FundsPage = ({ t }) => {
   const title = t("funds-page.title");
   return (
     <Page title={title}>
-      <FundsNavigationTabsContainer />
+      <NavigationTabsContainer
+        exploreTabName={routes.FUNDS_EXPLORE_TAB_NAME}
+        fundsTabRoute={routes.FUNDS_TAB_ROUTE}
+        fundsFavoritesTabName={routes.FUNDS_FAVORITES_TAB_NAME}
+      />
       <FacetCardsContainer title={title} assetsFacets={"fundsFacets"} />
       <FundsTableContainer title={"All funds"} />
     </Page>
--- packages/manager/src/pages/funds/funds/funds.page.js
+++ packages/manager/src/pages/funds/funds/funds.page.js
@@ -4,10 +4,17 @@ import React from "react";
 import { translate } from "react-i18next";
 
 import FacetCardsContainer from "shared/components/facet-cards/faset-cards-container";
+import NavigationTabsContainer from "shared/components/navigation-tabs/navigation-tabs-container";
+import * as routes from "../funds.routes";
 
 const FundsPage = ({ t }) => {
   return (
     <Page title={t("funds-page.title")}>
+      <NavigationTabsContainer
+        exploreTabName={routes.FUNDS_EXPLORE_TAB_NAME}
+        fundsTabRoute={routes.FUNDS_TAB_ROUTE}
+        fundsFavoritesTabName={routes.FUNDS_FAVORITES_TAB_NAME}
+      />
       <FacetCardsContainer title={t("funds-page.title")} assetsFacets={"fundsFacets"} />
       <FundsTableContainer title={"All funds"} />
     </Page>
