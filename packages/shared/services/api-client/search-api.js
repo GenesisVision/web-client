0b35edebe4ec03a068cb75be60df24df39c05e43
nkrivous <kolsln@list.ru>
2018-11-16 13:03:57 +0300

    Update global search api

--- packages/shared/services/api-client/search-api.js
+++ packages/shared/services/api-client/search-api.js
@@ -3,7 +3,6 @@ import { SearchApi } from "gv-api-web";
 import withApiProxy from "./api-proxy";
 import apiClient from "./swagger-custom-client";
 
-const searchApi = new SearchApi(apiClient);
+const searchApi = withApiProxy(new SearchApi(apiClient));
 
-export const searchApiProxy = withApiProxy(searchApi);
 export default searchApi;
