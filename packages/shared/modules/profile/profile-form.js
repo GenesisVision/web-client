ab40e94f6690bbbf743ee4ba6b16cfbb2f5c2aba
pavelzubov <pasha224@yandex.ru>
2018-11-16 11:02:27 +0300

    Update profile form

--- packages/shared/modules/profile/profile-form.js
+++ packages/shared/modules/profile/profile-form.js
@@ -1,17 +1,15 @@
 import "./profile.scss";
 
-import Dialog from "shared/components/dialog/dialog";
-import FileLabel from "shared/components/file-label/file-label";
-import GVDatePicker from "shared/components/gv-datepicker/gv-datepicker";
-import VerificationStatus from "shared/components/verification-status/verification-status";
 import { withFormik } from "formik";
-import { GVButton, GVFormikField, GVTextField } from "gv-react-components";
-import PhoneVerification from "shared/components/phone-verification/phone-verification";
-import UploadButton from "shared/modules/upload-button/upload-button";
+import { GVFormikField, GVTextField } from "gv-react-components";
 import moment from "moment";
 import PropTypes from "prop-types";
 import React, { Component, Fragment } from "react";
+import Scrollbars from "react-custom-scrollbars";
 import { translate } from "react-i18next";
+
+import VerificationStatus from "shared/components/verification-status/verification-status";
+import About from "shared/modules/about/about";
 
 class Profile extends Component {
   state = {
@@ -42,186 +40,57 @@ class Profile extends Component {
     const { t, info, handleSubmit } = this.props;
     return (
       <Fragment>
-        {info.phone && (
-          <Dialog
-            open={this.state.isOpenVerify}
-            onClose={this.handleCloseVerify}
-          >
-            <PhoneVerification
-              phoneNumber={info.phone}
-              onVerify={this.handleVerify}
-            />
-          </Dialog>
-        )}
+        <About about={info.about} userName={info.userName} />
         <form
           id="profile-form"
           className="profile__container"
           onSubmit={handleSubmit}
         >
-          <table className={"profile"}>
-            <tbody>
-              <tr className="profile__title">
-                <td className="profile__left">
-                  <h4>01</h4>
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <h4>{t("profile.contacts")}</h4>
-                  <VerificationStatus checked={info.phoneNumberConfirmed} />
-                </td>
-              </tr>
-              <tr className="profile__content">
-                <td className="profile__left">
-                  <span className="profile__stick" />
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <div className="profile__row">
-                    <div className="profile__phone">
+          <Scrollbars autoHeight autoHeightMax={14000}>
+            <table className={"profile"}>
+              <tbody>
+                <tr className="profile__title">
+                  <td className="profile__left">
+                    <h4>01</h4>
+                  </td>
+                  <td className="profile__center" />
+                  <td className="profile__right">
+                    <h4>{t("profile.contacts")}</h4>
+                    <VerificationStatus checked={info.phoneNumberConfirmed} />
+                  </td>
+                </tr>
+                <tr className="profile__content">
+                  <td className="profile__left">
+                    <span className="profile__stick" />
+                  </td>
+                  <td className="profile__center" />
+                  <td className="profile__right">
+                    <div className="profile__row">
                       <GVFormikField
-                        label={t("profile.phone-number")}
-                        name="phoneNumber"
+                        disabled
+                        label={t("profile.email")}
+                        value={info.email}
                         component={GVTextField}
+                        name="email"
                       />
-                      {info.phoneNumberConfirmed ||
-                        (info.phone && (
-                          <GVButton
-                            variant="text"
-                            onClick={this.handleOpenVerify}
-                          >
-                            {t("buttons.verify")}
-                          </GVButton>
-                        ))}
                     </div>
-                    <GVFormikField
-                      label={t("profile.email")}
-                      value={info.email}
-                      component={GVTextField}
-                      name="email"
+                  </td>
+                </tr>
+                <tr className="profile__title">
+                  <td className="profile__left">
+                    <h4>02</h4>
+                  </td>
+                  <td className="profile__center" />
+                  <td className="profile__right">
+                    <h4>{t("profile.personal-info")}</h4>
+                    <VerificationStatus
+                      verificationStatus={info.verificationStatus}
                     />
-                  </div>
-                </td>
-              </tr>
-              <tr className="profile__title">
-                <td className="profile__left">
-                  <h4>02</h4>
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <h4>{t("profile.personal-info")}</h4>
-                  <VerificationStatus
-                    verificationStatus={info.verificationStatus}
-                  />
-                </td>
-              </tr>
-              <tr className="profile__content">
-                <td className="profile__left">
-                  <span className="profile__stick" />
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <div>
-                    <GVFormikField
-                      label={t("profile.forename")}
-                      name="firstName"
-                      component={GVTextField}
-                    />
-                    <GVFormikField
-                      label={t("profile.family-name")}
-                      name="lastName"
-                      component={GVTextField}
-                    />
-                  </div>
-                  <div>
-                    <GVFormikField
-                      label={t("profile.birthday")}
-                      name="birthday"
-                      component={GVTextField}
-                      InputComponent={GVDatePicker}
-                      maxDate={new Date()}
-                    />
-                    <GVFormikField
-                      label={t("profile.citizen")}
-                      name="citizenship"
-                      component={GVTextField}
-                    />
-                  </div>
-                  {this.state.file && (
-                    <FileLabel
-                      className="profile__file"
-                      file={this.state.file}
-                      onClick={this.handleDelete}
-                    />
-                  )}
-                  <div>
-                    <GVFormikField
-                      name="documentId"
-                      component={props => (
-                        <input
-                          type="hidden"
-                          name={props.name}
-                          value={props.value}
-                        />
-                      )}
-                    />
-                    <UploadButton onLoad={this.handleLoad} />
-                  </div>
-                </td>
-              </tr>
-              <tr className="profile__title">
-                <td className="profile__left">
-                  <h4>03</h4>
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <h4>{t("profile.residential-address")}</h4>
-                </td>
-              </tr>
-              <tr className="profile__content">
-                <td className="profile__left" />
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <div className="profile__row">
-                    <GVFormikField
-                      label={t("profile.country")}
-                      name="country"
-                      component={GVTextField}
-                    />
-                    <GVFormikField
-                      label={t("profile.city")}
-                      name="city"
-                      component={GVTextField}
-                    />
-                  </div>
-                  <div className="profile__row">
-                    <GVFormikField
-                      label={t("profile.address")}
-                      name="address"
-                      component={GVTextField}
-                    />
-                  </div>
-                  <div className="profile__row">
-                    <GVFormikField
-                      label={t("profile.index")}
-                      name="index"
-                      component={GVTextField}
-                    />
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td />
-                <td />
-                <td>
-                  <span className="profile__edit-link">
-                    <GVButton type="submit">
-                      {this.props.t("buttons.save")}
-                    </GVButton>
-                  </span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </Scrollbars>
         </form>
       </Fragment>
     );
