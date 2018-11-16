473c735d88eb254fe2b49f53884a13e5da46f83b
nkrivous <kolsln@list.ru>
2018-11-16 13:35:12 +0300

    Update file api

--- packages/shared/services/api-client/file-api.js
+++ packages/shared/services/api-client/file-api.js
@@ -3,7 +3,6 @@ import { FileApi } from "gv-api-web";
 import withApiProxy from "./api-proxy";
 import apiClient from "./swagger-custom-client";
 
-const fileApi = new FileApi(apiClient);
+const fileApi = withApiProxy(new FileApi(apiClient));
 
-export const fileApiProxy = withApiProxy(fileApi);
 export default fileApi;
