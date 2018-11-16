9cc528843db62be841303631e7c3a652469970d4
nkrivous <kolsln@list.ru>
2018-11-16 13:31:14 +0300

    Update platform api

--- packages/shared/services/api-client/platform-api.js
+++ packages/shared/services/api-client/platform-api.js
@@ -1,7 +1,8 @@
 import { PlatformApi } from "gv-api-web";
 
+import withApiProxy from "./api-proxy";
 import apiClient from "./swagger-custom-client";
 
-const platformApi = new PlatformApi(apiClient);
+const platformApi = withApiProxy(new PlatformApi(apiClient));
 
 export default platformApi;
