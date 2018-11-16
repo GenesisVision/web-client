456d952c3571c9bc50c1bb07d54adee81acb96bc
pavelzubov <pasha224@yandex.ru>
2018-11-16 11:28:38 +0300

    Add header actions and constants to shared

---
+++ packages/shared/components/header/actions/header-actions.js
@@ -0,0 +1,8 @@
+import { PROFILE_HEADER } from "shared/components/header/header.constants";
+import profileApi from "shared/services/api-client/profile-api";
+import authService from "shared/services/auth-service";
+
+export const fetchProfileHeaderInfo = () => ({
+  type: PROFILE_HEADER,
+  payload: profileApi.v10ProfileHeaderGet(authService.getAuthArg())
+});
