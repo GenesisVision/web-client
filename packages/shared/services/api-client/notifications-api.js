0e672413e23ebed1d6efc80b476254a91758e840
nkrivous <kolsln@list.ru>
2018-11-16 13:32:46 +0300

    Update notifications api

--- packages/shared/services/api-client/notifications-api.js
+++ packages/shared/services/api-client/notifications-api.js
@@ -3,7 +3,6 @@ import { NotificationsApi } from "gv-api-web";
 import withApiProxy from "./api-proxy";
 import apiClient from "./swagger-custom-client";
 
-const notificationsApi = new NotificationsApi(apiClient);
+const notificationsApi = withApiProxy(new NotificationsApi(apiClient));
 
-export const notificationsProxyApi = withApiProxy(notificationsApi);
 export default notificationsApi;
