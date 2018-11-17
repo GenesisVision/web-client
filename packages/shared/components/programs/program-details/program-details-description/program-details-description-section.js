0689b71e6ca03d48c2a707654e4353fd540e938e
pavelzubov <pasha224@yandex.ru>
2018-11-17 18:59:36 +0300

    Add ProgramDetailsDescriptionSection to shared

---
+++ packages/shared/components/programs/program-details/program-details-description/program-details-description-section.js
@@ -0,0 +1,127 @@
+import { toggleFavoriteProgram } from "shared/modules/favorite-asset/services/favorite-program.service";
+import React, { Fragment, PureComponent } from "react";
+
+import ProgramDetailsDescription from "shared/components/programs/program-details/program-details-description/program-details-description";
+
+const composeInvestmentData = programDetails => {
+  const { statistic, personalProgramDetails } = programDetails;
+
+  const { balanceBase, profitPercent } = statistic;
+
+  return {
+    pendingInput: personalProgramDetails.pendingInput,
+    pendingOutput: personalProgramDetails.pendingOutput,
+    id: programDetails.id,
+    investedAmount: personalProgramDetails.value,
+    balanceAmount: balanceBase.amount,
+    balanceCurrency: balanceBase.currency,
+    profitPercent,
+    status: personalProgramDetails.status,
+    value: personalProgramDetails.value
+  };
+};
+class ProgramDetailsDescriptionSection extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ui: {
+        isFavoritePending: false,
+        isReinvestPending: false,
+        isPending: false
+      },
+      programDescription: null,
+      prevProps: null
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    let newState = {};
+    if (state.prevProps !== props.programDescriptionData) {
+      newState.prevProps = props.programDescriptionData;
+      newState.programDescription = props.programDescriptionData.data;
+      newState.ui = { isPending: props.programDescriptionData.isPending };
+    }
+
+    return newState;
+  }
+
+  handleOnReinvestingClick = () => {
+    const { ui, programDescription } = this.state;
+    const { id, personalProgramDetails } = programDescription;
+    const { isReinvest } = personalProgramDetails;
+    const { toggleReinvesting } = this.props;
+    if (!toggleReinvesting) return null;
+    const composeNewReinvestState = newState => ({
+      ...programDescription,
+      personalProgramDetails: {
+        ...personalProgramDetails,
+        isReinvest: !isReinvest
+      }
+    });
+
+    this.setState({
+      ui: { ...ui, isReinvestPending: true },
+      programDescription: composeNewReinvestState(!isReinvest)
+    });
+    toggleReinvesting(id, isReinvest)
+      .then(() => this.setState({ ui: { ...ui, isReinvestPending: false } }))
+      .catch(e => {
+        this.setState({
+          programDescription: composeNewReinvestState(isReinvest),
+          ui: { ...ui, isReinvestPending: false }
+        });
+      });
+  };
+
+  handleOnFavoriteClick = () => {
+    const { ui, programDescription } = this.state;
+    const { id, personalProgramDetails } = programDescription;
+    const { isFavorite } = personalProgramDetails;
+
+    const composeNewFavoriteState = newState => ({
+      ...programDescription,
+      personalProgramDetails: {
+        ...personalProgramDetails,
+        isFavorite: !isFavorite
+      }
+    });
+
+    this.setState({
+      ui: { ...ui, isFavoritePending: true },
+      programDescription: composeNewFavoriteState(!isFavorite)
+    });
+    toggleFavoriteProgram(id, isFavorite)
+      .then(() => {
+        this.setState({
+          ui: { ...ui, isFavoritePending: false }
+        });
+      })
+      .catch(e => {
+        this.setState({
+          programDescription: composeNewFavoriteState(isFavorite),
+          ui: { ...ui, isFavoritePending: false }
+        });
+      });
+  };
+
+  render() {
+    const { programDescription, ui } = this.state;
+    if (!programDescription) return null;
+    return (
+      <Fragment>
+        <ProgramDetailsDescription
+          onReinvestingClick={this.handleOnReinvestingClick}
+          programDescription={programDescription}
+          isReinvestPending={ui.isReinvestPending}
+          onFavoriteClick={this.handleOnFavoriteClick}
+          isFavoritePending={ui.isFavoritePending}
+          investmentData={composeInvestmentData(programDescription)}
+          {...programDescription.personalProgramDetails}
+          {...this.props}
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default ProgramDetailsDescriptionSection;
