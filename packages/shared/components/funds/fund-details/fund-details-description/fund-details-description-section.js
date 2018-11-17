d1486a26a3e948df5a610362705746662cf625a0
pavelzubov <pasha224@yandex.ru>
2018-11-17 18:59:14 +0300

    Added FundDetailsDescriptionSection to shared

---
+++ packages/shared/components/funds/fund-details/fund-details-description/fund-details-description-section.js
@@ -0,0 +1,86 @@
+import React, { Fragment, PureComponent } from "react";
+
+import FundDetailsDescription from "shared/components/funds/fund-details/fund-details-description/fund-details-description";
+import { toggleFavoriteFund } from "shared/modules/favorite-asset/services/favorite-fund.service";
+
+const composeInvestmentData = fundDetails => {
+  const { statistic, personalFundDetails } = fundDetails;
+  const { balanceGVT, profitPercent } = statistic;
+  return {
+    id: fundDetails.id,
+    balanceAmount: balanceGVT.amount,
+    balanceCurrency: balanceGVT.currency,
+    profitPercent,
+    ...personalFundDetails
+  };
+};
+class FundDetailsDescriptionSection extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ui: {
+        isFavoritePending: false,
+        isReinvestPending: false,
+        isPending: false
+      },
+      fundDescription: null,
+      prevProps: null
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    let newState = {};
+    if (state.prevProps !== props.fundDescriptionData) {
+      newState.prevProps = props.fundDescriptionData;
+      newState.fundDescription = props.fundDescriptionData.data;
+      newState.ui = { isPending: props.fundDescriptionData.isPending };
+    }
+
+    return newState;
+  }
+
+  handleOnFavoriteClick = () => {
+    const { ui, fundDescription } = this.state;
+    const { id, personalFundDetails } = fundDescription;
+    const { isFavorite } = personalFundDetails;
+    const composeNewFavoriteState = () => ({
+      ...fundDescription,
+      personalFundDetails: {
+        ...personalFundDetails,
+        isFavorite: !isFavorite
+      }
+    });
+
+    this.setState({
+      ui: { ...ui, isFavoritePending: true },
+      fundDescription: composeNewFavoriteState()
+    });
+    toggleFavoriteFund(id, isFavorite)
+      .then(() => this.setState({ ui: { ...ui, isFavoritePending: false } }))
+      .catch(e =>
+        this.setState({
+          fundDescription: composeNewFavoriteState(isFavorite),
+          ui: { ...ui, isFavoritePending: false }
+        })
+      );
+  };
+
+  render() {
+    const { fundDescription, ui } = this.state;
+    if (!fundDescription) return null;
+    return (
+      <Fragment>
+        <FundDetailsDescription
+          fundDescription={fundDescription}
+          onFavoriteClick={this.handleOnFavoriteClick}
+          isFavoritePending={ui.isFavoritePending}
+          investmentData={composeInvestmentData(fundDescription)}
+          {...fundDescription.personalFundDetails}
+          {...this.props}
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default FundDetailsDescriptionSection;
