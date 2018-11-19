593b6b583b614ab1f026c29bee88e1f2dba1d160
pavelzubov <pasha224@yandex.ru>
2018-11-19 12:11:18 +0300

    -M Remove unused program tournament

--- packages/manager/src/modules/program-tournament/components/program-tournament-create-container/program-tournament-create-form/program-tournament-create-form.validators.js
+++
@@ -1,8 +0,0 @@
-import { object, string } from "yup";
-
-const validateSchema = object().shape({
-  title: string().required("Title is required"),
-  description: string()
-});
-
-export default validateSchema;
