7dcd9b33b5d37f15f941650d37ef8aacc0cf4cc2
pavelzubov <pasha224@yandex.ru>
2018-11-20 19:45:18 +0300

    Change statistic-item to details-statistic-item in DashboardPortfolioChartStat

--- packages/investor/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-chart/dashboard-portfolio-chart-stat.js
+++ packages/investor/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-chart/dashboard-portfolio-chart-stat.js
@@ -1,10 +1,10 @@
-import Profitability from "shared/components/profitability/profitability";
-import StatisticItem from "shared/components/statistic-item/statistic-item";
 import React from "react";
 import { translate } from "react-i18next";
 import NumberFormat from "react-number-format";
 
 import { formatValue } from "shared/utils/formatter";
+import DetailsStatisticItem from "shared/components/details-statistic-item/details-statistic-item";
+import Profitability from "shared/components/profitability/profitability";
 
 const DashboardPortfolioChartStat = ({
   t,
@@ -15,38 +15,59 @@ const DashboardPortfolioChartStat = ({
   changeValue,
   changeValueCurrency
 }) => {
+  const renderValue = () => (
+    <div className="dashboard-portfolio-chart-stat__value">
+      <NumberFormat
+        value={formatValue(value)}
+        thousandSeparator={" "}
+        displayType="text"
+        suffix={" GVT"}
+      />
+    </div>
+  );
+
+  const renderChange = () => (
+    <div className="dashboard-portfolio-chart-stat__value">
+      <NumberFormat
+        value={formatValue(changeValue)}
+        thousandSeparator={" "}
+        displayType="text"
+        suffix={" GVT"}
+      />
+      <Profitability
+        prefix="arrow"
+        variant="chips"
+        value={changePercent && formatValue(changePercent, 2)}
+        className="dashboard-portfolio-chart-stat__adornment"
+      >
+        <NumberFormat
+          value={formatValue(changePercent, 2, true)}
+          suffix="%"
+          allowNegative={false}
+          decimalScale={2}
+          displayType="text"
+        />
+      </Profitability>
+    </div>
+  );
+
   return (
     <div className="dashboard-portfolio-chart-stat">
-      <StatisticItem
-        heading={"Value"}
-        value={formatValue(value)}
-        equivalent={valueCurrency}
-        currency={currency}
-        className="dashboard-portfolio-chart-stat__statistic-item"
-      />
-      <StatisticItem
-        heading={"Change"}
-        value={formatValue(changeValue)}
-        adornment={
-          <Profitability
-            prefix="arrow"
-            variant="chips"
-            value={changePercent && formatValue(changePercent, 2)}
-            className="dashboard-portfolio-chart-stat__adornment"
-          >
-            <NumberFormat
-              value={formatValue(changePercent, 2, true)}
-              suffix="%"
-              allowNegative={false}
-              decimalScale={2}
-              displayType="text"
-            />
-          </Profitability>
-        }
-        equivalent={changeValueCurrency}
-        currency={currency}
-        className="dashboard-portfolio-chart-stat__statistic-item"
-      />
+      <DetailsStatisticItem
+        accent
+        label={"Value"}
+        equivalent={formatValue(valueCurrency)}
+        equivalentCurrency={currency}
+      >
+        {renderValue()}
+      </DetailsStatisticItem>
+      <DetailsStatisticItem
+        label={"Change"}
+        equivalent={formatValue(changeValueCurrency)}
+        equivalentCurrency={currency}
+      >
+        {renderChange()}
+      </DetailsStatisticItem>
     </div>
   );
 };
