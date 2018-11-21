cb54aaa435d0637247c06a7a401cba9bc19ceedf
pavelzubov <pasha224@yandex.ru>
2018-11-21 17:05:55 +0300

    Add confirmWithdraw to shared

---
+++ packages/shared/components/wallet-withdraw-confirm/services/wallet-withdraw-confirm.services.js
@@ -0,0 +1,26 @@
+import { PROGRAMS_ROUTE } from "pages/programs/programs.routes";
+import { push } from "react-router-redux";
+import { walletApiProxy } from "shared/services/api-client/wallet-api";
+import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
+
+export const confirmWithdraw = (requestId, code) => dispatch => {
+  return walletApiProxy
+    .v10WalletWithdrawRequestConfirmPost({
+      requestId,
+      code
+    })
+    .then(response => {
+      dispatch(push(PROGRAMS_ROUTE));
+      dispatch(
+        alertMessageActions.success(
+          "wallet-withdraw.confirmation.success",
+          true
+        )
+      );
+      return response;
+    })
+    .catch(error => {
+      dispatch(push(PROGRAMS_ROUTE));
+      dispatch(alertMessageActions.error(error.errorMessage));
+    });
+};
