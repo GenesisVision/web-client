0ea01eedda87daac92d0fb9d2cfabb9650a64e39
pavelzubov <pasha224@yandex.ru>
2018-11-23 10:52:36 +0300

    Change way getting funds and programs to fetching in ManagerPage

--- packages/investor/src/pages/manager/manager.page.js
+++ packages/investor/src/pages/manager/manager.page.js
@@ -6,10 +6,9 @@ import { translate } from "react-i18next";
 import { connect } from "react-redux";
 import { goBack } from "react-router-redux";
 import { bindActionCreators } from "redux";
-import replaceParams from "shared/utils/replace-params";
 
 import { SLUG_URL_REGEXP } from "shared/utils/constants";
-import ManagerDescriptionContainer from "shared/components/manager/manager-description/manager-description";
+import ManagerDescription from "shared/components/manager/manager-description/manager-description";
 import ManagerHistorySection from "shared/components/manager/manager-history/manager-history-section";
 import * as managerService from "./services/manager.service";
 
@@ -19,40 +18,56 @@ export const MANAGERS_ROUTE = "/managers";
 export const MANAGER_DETAILS_ROUTE = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}`;
 export const MANAGER_DETAILS_ROUTE_REGEXP = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}(${SLUG_URL_REGEXP})`;
 
-export const composeManagerDetailsUrl = slugUrl =>
-  replaceParams(MANAGER_DETAILS_ROUTE, {
-    [`:${MANAGER_SLUG_URL_PARAM_NAME}`]: slugUrl
-  });
-
 class ManagerPage extends Component {
   state = {
     managerProfile: {},
-    funds: [],
-    programs: [],
+    funds: null,
+    programs: null,
     isPending: true
   };
+
   componentDidMount() {
     const { service } = this.props;
-    service.fetchManagerProfile().then(profile => {
-      this.setState({ managerProfile: profile, isPending: false });
-    });
+    service
+      .fetchManagerProfile()
+      .then(profile => {
+        this.setState({ managerProfile: profile, isPending: false });
+        return this.getFunds();
+      })
+      .then(funds => {
+        this.setState({ funds: funds });
+        return this.getPrograms();
+      })
+      .then(programs => {
+        this.setState({ programs: programs, isPending: false });
+      });
   }
+
+  getFunds = filters => {
+    return managerService.getFunds(this.props.managerId, filters);
+  };
+
+  getPrograms = filters => {
+    return managerService.getPrograms(this.props.managerId, filters);
+  };
+
   render() {
-    const { t, service } = this.props;
-    const { managerProfile, isPending } = this.state;
+    const { t } = this.props;
+    const { managerProfile, isPending, funds, programs } = this.state;
 
     return (
       !isPending && (
         <Page title={`${t("manager.title")} ${managerProfile.username}`}>
           <div className="manager">
             <div className="manager__description">
-              <ManagerDescriptionContainer
-                managerProfile={managerProfile}
-                managerService={service}
-              />
+              <ManagerDescription managerProfile={managerProfile} />
             </div>
             <div className="manager__history">
               <ManagerHistorySection
+                programs={programs}
+                funds={funds}
+                getPrograms={this.getPrograms}
+                getFunds={this.getFunds}
                 managerId={managerProfile.id}
                 title={managerProfile.username}
               />
@@ -63,6 +78,7 @@ class ManagerPage extends Component {
     );
   }
 }
+
 const mapStateToProps = state => {
   return {
     managerProfile: state.manager.data
--- packages/manager/src/pages/manager/manager.page.js
+++ packages/manager/src/pages/manager/manager.page.js
@@ -5,10 +5,9 @@ import React, { Component } from "react";
 import { translate } from "react-i18next";
 import connect from "react-redux/es/connect/connect";
 import { bindActionCreators } from "redux";
-import replaceParams from "shared/utils/replace-params";
 
 import { SLUG_URL_REGEXP } from "shared/utils/constants";
-import ManagerDescriptionContainer from "shared/components/manager/manager-description/manager-description";
+import ManagerDescription from "shared/components/manager/manager-description/manager-description";
 import ManagerHistorySection from "shared/components/manager/manager-history/manager-history-section";
 import * as managerService from "./services/manager.service";
 
@@ -18,40 +17,55 @@ export const MANAGERS_ROUTE = "/managers";
 export const MANAGER_DETAILS_ROUTE = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}`;
 export const MANAGER_DETAILS_ROUTE_REGEXP = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}(${SLUG_URL_REGEXP})`;
 
-export const composeManagerDetailsUrl = slugUrl =>
-  replaceParams(MANAGER_DETAILS_ROUTE, {
-    [`:${MANAGER_SLUG_URL_PARAM_NAME}`]: slugUrl
-  });
-
 class ManagerPage extends Component {
   state = {
     managerProfile: {},
-    funds: [],
-    programs: [],
+    funds: null,
+    programs: null,
     isPending: true
   };
+
   componentDidMount() {
     const { service } = this.props;
-    service.fetchManagerProfile().then(profile => {
-      this.setState({ managerProfile: profile, isPending: false });
-    });
+    service
+      .fetchManagerProfile()
+      .then(profile => {
+        this.setState({ managerProfile: profile, isPending: false });
+        return this.getFunds();
+      })
+      .then(funds => {
+        this.setState({ funds: funds });
+        return this.getPrograms();
+      })
+      .then(programs => {
+        this.setState({ programs: programs, isPending: false });
+      });
   }
-  render() {
-    const { t, service } = this.props;
-    const { managerProfile, isPending } = this.state;
 
+  getFunds = filters => {
+    return managerService.getFunds(this.props.managerId, filters);
+  };
+
+  getPrograms = filters => {
+    return managerService.getPrograms(this.props.managerId, filters);
+  };
+
+  render() {
+    const { t } = this.props;
+    const { managerProfile, isPending, funds, programs } = this.state;
     return (
       !isPending && (
         <Page title={`${t("manager.title")} ${managerProfile.username}`}>
           <div className="manager">
             <div className="manager__description">
-              <ManagerDescriptionContainer
-                managerProfile={managerProfile}
-                managerService={service}
-              />
+              <ManagerDescription managerProfile={managerProfile} />
             </div>
             <div className="manager__history">
               <ManagerHistorySection
+                programs={programs}
+                funds={funds}
+                getPrograms={this.getPrograms}
+                getFunds={this.getFunds}
                 managerId={managerProfile.id}
                 title={managerProfile.username}
               />
