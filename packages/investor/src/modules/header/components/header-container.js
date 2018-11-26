9dedeec67e49f929a1853f8d8a7fdab610caac0a
Гребенщиков Евгений <evgreb@gmail.com>
2018-11-26 15:29:17 +0300

    GV-15 fix routes
    Signed-off-by: Гребенщиков Евгений <evgreb@gmail.com>

--- packages/investor/src/modules/header/components/header-container.js
+++ packages/investor/src/modules/header/components/header-container.js
@@ -1,13 +1,13 @@
-import { notificationsToggle } from "pages/app/components/notifications/actions/notifications.actions";
-import { LOGIN_ROUTE } from "pages/auth/login/login.routes";
-import { SIGNUP_ROUTE } from "pages/auth/signup/signup.routes";
+import { notificationsToggle } from "../../../pages/app/components/notifications/actions/notifications.actions";
+import { LOGIN_ROUTE } from "../../../pages/auth/login/login.routes";
+import { SIGNUP_ROUTE } from "../../../pages/auth/signup/signup.routes";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchTwoFactor } from "shared/actions/2fa-actions";
 import { GLOBAL_SEARCH_ROUTE } from "shared/components/global-search/global-search.routes";
 import Header from "shared/components/header/header";
 
-import { logout } from "pages/auth/login/services/login.service";
+import { logout } from "../../../pages/auth/login/services/login.service";
 
 class HeaderContainer extends Component {
   componentDidMount() {
