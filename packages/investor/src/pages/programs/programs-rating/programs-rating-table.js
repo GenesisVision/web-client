096937b2ef875a70eee70cee0e31b01c8d678c99
pavelzubov <pasha224@yandex.ru>
2018-11-28 10:32:57 +0300

    Add paging to ProgramsRatingTable

--- packages/investor/src/pages/programs/programs-rating/programs-rating-table.js
+++ packages/investor/src/pages/programs/programs-rating/programs-rating-table.js
@@ -3,36 +3,54 @@ import { translate } from "react-i18next";
 import { compose } from "redux";
 import { fetchPrograms } from "shared/modules/programs-table/services/programs-table.service";
 import ProgramsTableModule from "shared/modules/programs-table/components/programs-table/programs-table-module";
+import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
 
 class ProgramsRatingTable extends Component {
   state = {
     programs: null,
-    isPending: true
+    isPending: true,
+    paging: {
+      currentPage: 1,
+      itemsOnPage: 10,
+      totalPages: 0
+    }
   };
 
   componentDidMount() {
-    const { tab } = this.props;
-    this.updatePrograms(tab);
+    // const { tab } = this.props;
+    this.updatePrograms();
   }
 
-  updateSorting = e => {};
+  updatePaging = e => {
+    console.log(e);
+    this.setState({ paging: { ...this.paging, currentPage: 2 } });
+    this.updatePrograms({ page: 2 });
+  };
 
-  updatePrograms = tab => {
-    const { managerId } = this.props;
-    fetchPrograms({ managerId, tab }).then(programs => {
-      this.setState({ programs, isPending: false });
+  updatePrograms = filters => {
+    const { managerId, tab } = this.props;
+    const { paging } = this.state;
+    fetchPrograms({ managerId, tab, ...filters }).then(programs => {
+      const totalPages = Math.round(programs.total / paging.itemsOnPage);
+      this.setState({
+        programs,
+        isPending: false,
+        paging: { ...paging, totalPages }
+      });
     });
   };
 
   render() {
     const { title } = this.props;
-    const { programs, isPending } = this.state;
+    const { programs, isPending, paging } = this.state;
     if (isPending) return null;
     return (
       <ProgramsTableModule
         title={title}
         data={programs}
         isPending={isPending}
+        paging={paging}
+        updatePaging={this.updatePaging}
       />
     );
   }
