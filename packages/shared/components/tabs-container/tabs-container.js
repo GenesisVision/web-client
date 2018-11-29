12160665eae09770e2b5aef971ad47553253c819
a.popova <bob.m@mail.ru>
2018-11-29 13:25:26 +0300

    added scrollbars in the rating tabs

--- packages/shared/components/tabs-container/tabs-container.js
+++ packages/shared/components/tabs-container/tabs-container.js
@@ -5,27 +5,30 @@ import replaceParams from "shared/utils/replace-params";
 import { translate } from "react-i18next";
 
 import { GVTab, GVTabs } from "gv-react-components";
+import Scrollbars from "react-custom-scrollbars";
 
 class TabsContainer extends Component {
   render() {
     const { t, tabs, programFacetRoute, tab, handleTabChange } = this.props;
     return (
-      <GVTabs value={tab} onChange={handleTabChange}>
-        {tabs.map(tab => (
-          <GVTab
-            key={tab}
-            value={tab}
-            label={tab}
-            /*<Link
-                to={replaceParams(programFacetRoute, {
-                  ":tab": tab
-                })}
-              >
-                {t(tab)}
-              </Link>*/
-          />
-        ))}
-      </GVTabs>
+      <Scrollbars autoHeight autoHeightMax={40}>
+        <GVTabs value={tab} onChange={handleTabChange}>
+          {tabs.map(tab => (
+            <GVTab
+              key={tab}
+              value={tab}
+              label={tab}
+              /*<Link
+                  to={replaceParams(programFacetRoute, {
+                    ":tab": tab
+                  })}
+                >
+                  {t(tab)}
+                </Link>*/
+            />
+          ))}
+        </GVTabs>
+      </Scrollbars>
     );
   }
 }
