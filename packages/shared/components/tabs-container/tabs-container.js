46f3467b47e6552c0dab81bac849be63213dd723
pavelzubov <pasha224@yandex.ru>
2018-11-29 14:09:30 +0300

    Remove unused code from TabsContainer

--- packages/shared/components/tabs-container/tabs-container.js
+++ packages/shared/components/tabs-container/tabs-container.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { compose } from "redux";
-import replaceParams from "shared/utils/replace-params";
 import { translate } from "react-i18next";
 
 import { GVTab, GVTabs } from "gv-react-components";
@@ -9,23 +8,12 @@ import Scrollbars from "react-custom-scrollbars";
 
 class TabsContainer extends Component {
   render() {
-    const { t, tabs, programFacetRoute, tab, handleTabChange } = this.props;
+    const { tabs, tab, handleTabChange } = this.props;
     return (
       <Scrollbars autoHeight autoHeightMax={40}>
         <GVTabs value={tab} onChange={handleTabChange}>
           {tabs.map(tab => (
-            <GVTab
-              key={tab}
-              value={tab}
-              label={tab}
-              /*<Link
-                  to={replaceParams(programFacetRoute, {
-                    ":tab": tab
-                  })}
-                >
-                  {t(tab)}
-                </Link>*/
-            />
+            <GVTab key={tab} value={tab} label={tab} />
           ))}
         </GVTabs>
       </Scrollbars>
