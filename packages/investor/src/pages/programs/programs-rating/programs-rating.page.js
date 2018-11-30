df6e0b1005df53c1d5873a5bd0032d31713f6435
pavelzubov <pasha224@yandex.ru>
2018-11-30 09:53:50 +0300

    Remove unused code and mount ProgramsRatingContainer

--- packages/investor/src/pages/programs/programs-rating/programs-rating.page.js
+++ packages/investor/src/pages/programs/programs-rating/programs-rating.page.js
@@ -3,74 +3,19 @@ import "shared/components/programs-rating/programs-rating.scss";
 import React, { Component } from "react";
 import { translate } from "react-i18next";
 import Page from "shared/components/page/page";
-
-import TabsContainer from "shared/components/tabs-container/tabs-container";
 import * as routes from "../programs.routes";
-import { bindActionCreators, compose } from "redux";
-import ProgramsRatingTables from "shared/components/programs-rating/programs-rating-tables";
-import { getLevelUpSummary } from "shared/components/programs-rating/services/program-rating-service";
-import Surface from "shared/components/surface/surface";
-import connect from "react-redux/es/connect/connect";
-import { LEVELS } from "shared/components/programs-rating/programs-rating-table";
-
-const TABS = ["1 > 2", "2 > 3", "3 > 4", "5 > 6", "7 > 8"];
+import { compose } from "redux";
+import ProgramsRatingContainer from "shared/components/programs-rating/programs-rating-container";
 
 class ProgramsRatingPage extends Component {
-  state = {
-    tab: TABS[0],
-    level: LEVELS[TABS[0]]
-  };
-
-  componentDidMount() {
-    const { match, service } = this.props;
-    service.getLevelUpSummary();
-    if (match.params.tab) this.setState({ tab: match.params.tab });
-  }
-  handleTabChange = (e, tab) => {
-    this.setState({ tab, level: LEVELS[tab] });
-  };
   render() {
-    const { t, levelData } = this.props;
-    const { tab, level } = this.state;
-    const currentLevelData = levelData ? levelData[level] : null;
-    if (!tab) return null;
+    const { t } = this.props;
     return (
       <Page title={t("programs-page.title")}>
-        <Surface className="programs-rating">
-          <div className="programs-rating__tabs">
-            <TabsContainer
-              programFacetRoute={routes.PROGRAMS_RATING_TAB_ROUTE}
-              tabs={TABS}
-              handleTabChange={this.handleTabChange}
-              tab={tab}
-              levelData={levelData}
-            />
-          </div>
-          <ProgramsRatingTables
-            key={tab}
-            tab={tab}
-            levelData={currentLevelData}
-          />
-        </Surface>
+        <ProgramsRatingContainer routes={routes} />
       </Page>
     );
   }
 }
-const mapStateToProps = state => {
-  const levelData = state.programsRating.levelupSummary.data
-    ? state.programsRating.levelupSummary.data.levelData
-    : {};
-  return { levelData };
-};
 
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ getLevelUpSummary }, dispatch)
-});
-
-export default compose(
-  translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(ProgramsRatingPage);
+export default compose(translate())(ProgramsRatingPage);
--- packages/manager/src/pages/programs/programs-rating/programs-rating.page.js
+++ packages/manager/src/pages/programs/programs-rating/programs-rating.page.js
@@ -3,56 +3,18 @@ import "shared/components/programs-rating/programs-rating.scss";
 import React, { Component } from "react";
 import { translate } from "react-i18next";
 import Page from "shared/components/page/page";
-
-import TabsContainer from "shared/components/tabs-container/tabs-container";
 import * as routes from "../programs.routes";
-import { bindActionCreators, compose } from "redux";
+import { compose } from "redux";
 import connect from "react-redux/es/connect/connect";
-import ProgramsRatingTables from "shared/components/programs-rating/programs-rating-tables";
-import Surface from "shared/components/surface/surface";
-import { getLevelUpSummary } from "shared/components/programs-rating/services/program-rating-service";
-import {
-  LEVELS,
-  TABS
-} from "shared/components/programs-rating/programs-rating-table";
+import ProgramsRatingContainer from "shared/components/programs-rating/programs-rating-container";
 
 class ProgramsRatingPage extends Component {
-  state = {
-    tab: TABS[0],
-    level: LEVELS[TABS[0]]
-  };
-
-  componentDidMount() {
-    const { service } = this.props;
-    service.getLevelUpSummary();
-  }
-  handleTabChange = (e, tab) => {
-    this.setState({ tab, level: LEVELS[tab] });
-  };
   render() {
-    const { t, id, levelData } = this.props;
-    const { tab, level } = this.state;
-    const currentLevelData = levelData ? levelData[level] : null;
-    if (!tab) return null;
+    const { t, id } = this.props;
+    if (!id) return null;
     return (
       <Page title={t("programs-page.title")}>
-        <Surface className="programs-rating">
-          <div className="programs-rating__tabs">
-            <TabsContainer
-              programFacetRoute={routes.PROGRAMS_RATING_TAB_ROUTE}
-              tabs={TABS}
-              handleTabChange={this.handleTabChange}
-              tab={tab}
-              levelData={levelData}
-            />
-          </div>
-          <ProgramsRatingTables
-            key={tab}
-            tab={tab}
-            id={id}
-            levelData={currentLevelData}
-          />
-        </Surface>
+        <ProgramsRatingContainer routes={routes} id={id} />
       </Page>
     );
   }
@@ -60,19 +22,11 @@ class ProgramsRatingPage extends Component {
 
 const mapStateToProps = state => {
   const { data } = state.profileHeader.info;
-  const { levelupSummary } = state.programsRating;
-  if (!levelupSummary.data || !data) return {};
-  return { id: data.id, levelData: levelupSummary.data.levelData };
+  if (!data) return {};
+  return { id: data.id };
 };
-
-const mapDispatchToProps = dispatch => ({
-  service: bindActionCreators({ getLevelUpSummary }, dispatch)
-});
 
 export default compose(
   translate(),
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )
+  connect(mapStateToProps)
 )(ProgramsRatingPage);
