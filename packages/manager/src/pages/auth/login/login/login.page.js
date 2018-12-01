783530534b0ea948b584ab8ed172b484801ef00e
pavelzubov <pasha224@yandex.ru>
2018-12-01 12:53:53 +0300

    Change ways to LoginFormContainer to shared

--- packages/manager/src/pages/auth/login/login/login.page.js
+++ packages/manager/src/pages/auth/login/login/login.page.js
@@ -1,17 +1,18 @@
-import "./login.scss";
+import "shared/components/auth/login/login/login.scss";
 
 import { HOME_ROUTE } from "pages/app/app.routes";
 import React from "react";
 
 import AuthTabs from "../../components/auth-tabs/auth-tabs";
-import LoginFormContainer from "./components/login-form-container";
+import LoginFormContainer from "shared/components/auth/login/login/login-form-container";
+import * as loginService from "../services/login.service";
 
 const LoginPage = ({ location }) => {
   const { from } = location.state || { from: { pathname: HOME_ROUTE } };
   return (
     <div className="login">
       <AuthTabs />
-      <LoginFormContainer from={from} />
+      <LoginFormContainer from={from} loginService={loginService} />
     </div>
   );
 };
