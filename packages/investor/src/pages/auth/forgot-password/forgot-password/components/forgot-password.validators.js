8945758092b5d4dfa86f6300bdaaa25cf7a6db36
pavelzubov <pasha224@yandex.ru>
2018-12-01 12:36:41 +0300

    Remove unused forgot-password files

--- packages/investor/src/pages/auth/forgot-password/forgot-password/components/forgot-password.validators.js
+++
@@ -1,8 +0,0 @@
-import { emailValidator } from "shared/utils/validators/validators";
-import { object } from "yup";
-
-const validationSchema = object().shape({
-  email: emailValidator
-});
-
-export default validationSchema;
