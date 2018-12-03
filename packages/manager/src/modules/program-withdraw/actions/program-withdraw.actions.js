312e568a84d3b0382af42fdea86ef230914a42d4
nkrivous <kolsln@list.ru>
2018-12-03 17:06:39 +0300

    Update investor api

--- packages/manager/src/modules/program-withdraw/actions/program-withdraw.actions.js
+++ packages/manager/src/modules/program-withdraw/actions/program-withdraw.actions.js
@@ -1,23 +1,12 @@
 import { FETCH_WITHDRAW_PROGRAM_INFO } from "modules/program-withdraw/program-withdraw-constants";
-import investorApi, {
-  investorApiProxy
-} from "shared/services/api-client/investor-api";
+import managerApi from "shared/services/api-client/manager-api";
 import authService from "shared/services/auth-service";
 
 export const fetchWithdrawInfoById = (id, currency) => ({
   type: FETCH_WITHDRAW_PROGRAM_INFO,
-  payload: investorApiProxy.v10InvestorProgramsByIdWithdrawInfoByCurrencyGet(
+  payload: managerApi.v10InvestorProgramsByIdWithdrawInfoByCurrencyGet(
     id,
     currency,
     authService.getAuthArg()
   )
 });
-
-// export const withdrawProgramById = (id, amount) => ({
-//   type: FETCH_WITHDRAW_PROGRAM_INFO,
-//   payload: investorApiProxy.v10InvestorProgramsByIdWithdrawByAmountPost(
-//     id,
-//     amount,
-//     authService.getAuthArg()
-//   )
-// });
