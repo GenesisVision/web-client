39cc95bbbe9810bf9492837a96d471544207ee2f
nkrivous <kolsln@list.ru>
2018-12-03 18:02:37 +0300

    Fix managers programs

--- packages/manager/src/pages/programs/program-details/program-details.page.js
+++ packages/manager/src/pages/programs/program-details/program-details.page.js
@@ -13,17 +13,17 @@ import NotFoundPage from "shared/components/not-found/not-found.routes";
 import Page from "shared/components/page/page";
 import ProgramDetailsDescriptionSection from "shared/components/programs/program-details/program-details-description/program-details-description-section";
 import ProgramDetailsStatisticSection from "shared/components/programs/program-details/program-details-statistic-section/program-details-statistic-section";
+import ProgramDetailsHistorySection from "shared/components/programs/program-details/program-trades/program-details-history-section";
 
 import { LOGIN_ROUTE } from "../../auth/login/login.routes";
+import { fetchPortfolioEvents } from "../../dashboard/services/dashboard-events.services";
 import ClosePeriodContainer from "./close-period/close-period-container";
 import CloseProgramContainer from "./close-program/close-program-container";
-import ProgramDetailsHistorySection from "shared/components/programs/program-details/program-trades/program-details-history-section";
 import {
+  fetchProgramTrades,
   getProgramDescription,
-  getProgramStatistic,
-  fetchProgramTrades
+  getProgramStatistic
 } from "./services/program-details.service";
-import { fetchPortfolioEvents } from "../../dashboard/services/dashboard-events.services";
 
 export const ProgramDetailContext = React.createContext({
   updateDetails: () => {}
@@ -37,11 +37,11 @@ class ProgramDetailsPage extends PureComponent {
 
   constructor(props) {
     super(props);
-    this.description = { data: null, isPending: true };
-    this.profitChart = { data: null, isPending: true };
-    this.balanceChart = { data: null, isPending: true };
-    this.statistic = { data: null, isPending: true };
-    this.trades = { data: null, isPending: true };
+    this.description = null;
+    this.profitChart = null;
+    this.balanceChart = null;
+    this.statistic = null;
+    this.trades = null;
   }
 
   componentDidMount() {
@@ -63,12 +63,12 @@ class ProgramDetailsPage extends PureComponent {
       })
       .then(() => {
         this.setState({ isPending: true });
-        return getProgramStatistic(this.description.data.id);
+        return getProgramStatistic(this.description.id);
       })
       .then(data => {
-        this.profitChart = data.profitChartData;
-        this.balanceChart = data.balanceChartData;
-        this.statistic = data.statisticData;
+        this.profitChart = data.profitChart;
+        this.balanceChart = data.balanceChart;
+        this.statistic = data.statistic;
         this.setState({ isPending: false });
       })
       .catch(e => {
@@ -78,12 +78,10 @@ class ProgramDetailsPage extends PureComponent {
 
   changeInvestmentStatus = () => {
     this.setState({ isPending: true });
-    this.props.service
-      .getProgramDescription(this.description.data.id)
-      .then(data => {
-        this.description = data;
-        this.setState({ isPending: false });
-      });
+    this.props.service.getProgramDescription(this.description.id).then(data => {
+      this.description = data;
+      this.setState({ isPending: false });
+    });
   };
 
   render() {
@@ -93,12 +91,12 @@ class ProgramDetailsPage extends PureComponent {
       return <NotFoundPage />;
     }
 
-    if (!this.description.data) return null;
+    if (!this.description) return null;
     const isInvested =
-      this.description.data.personalProgramDetails &&
-      this.description.data.personalProgramDetails.isInvested;
+      this.description.personalProgramDetails &&
+      this.description.personalProgramDetails.isInvested;
     return (
-      <Page title={this.description.data.title}>
+      <Page title={this.description.title}>
         <ProgramDetailContext.Provider
           value={{ updateDetails: this.updateDetails }}
         >
@@ -113,7 +111,7 @@ class ProgramDetailsPage extends PureComponent {
                 ClosePeriodContainer={ClosePeriodContainer}
                 CloseProgramContainer={CloseProgramContainer}
                 ProgramDetailContext={ProgramDetailContext}
-                programDescriptionData={this.description}
+                programDescription={this.description}
                 isAuthenticated={isAuthenticated}
                 redirectToLogin={service.redirectToLogin}
                 onChangeInvestmentStatus={this.changeInvestmentStatus}
@@ -122,11 +120,11 @@ class ProgramDetailsPage extends PureComponent {
             <div className="details__section">
               <ProgramDetailsStatisticSection
                 getProgramStatistic={getProgramStatistic}
-                programId={this.description.data.id}
+                programId={this.description.id}
                 currency={currency}
-                statisticData={this.statistic}
-                profitChartData={this.profitChart}
-                balanceChartData={this.balanceChart}
+                statistic={this.statistic}
+                profitChart={this.profitChart}
+                balanceChart={this.balanceChart}
               />
             </div>
             <div className="details__history">
@@ -135,10 +133,10 @@ class ProgramDetailsPage extends PureComponent {
                 fetchPortfolioEvents={filters =>
                   fetchPortfolioEvents({
                     ...filters,
-                    assetId: this.description.data.id
+                    assetId: this.description.id
                   })
                 }
-                programId={this.description.data.id}
+                programId={this.description.id}
                 currency={currency}
                 isInvested={isInvested}
               />
