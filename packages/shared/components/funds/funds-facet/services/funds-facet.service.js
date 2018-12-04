f767cf134f9ee3a084720042ad05b08c586b1be2
pavelzubov <pasha224@yandex.ru>
2018-12-04 15:04:52 +0300

    Add funds-facet.service to shared

---
+++ packages/shared/components/funds/funds-facet/services/funds-facet.service.js
@@ -0,0 +1,20 @@
+import getParams from "shared/utils/get-params";
+
+import {
+  FUNDS_FACET_ROUTE,
+  FUNDS_SLUG_URL_PARAM_NAME
+} from "pages/funds/funds.routes";
+
+export const getCurrentFacet = () => (dispatch, getState) => {
+  const { routing, platformData } = getState();
+
+  if (!platformData.data) return { isPending: true };
+
+  const { fundsFacets } = platformData.data;
+  const facetUrl = getParams(routing.location.pathname, FUNDS_FACET_ROUTE)[
+    FUNDS_SLUG_URL_PARAM_NAME
+  ];
+  const facet = fundsFacets.find(x => x.url === facetUrl);
+  if (!facet) return { notFound: true };
+  return { facet };
+};
