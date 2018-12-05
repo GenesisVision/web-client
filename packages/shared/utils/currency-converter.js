917291fcda90fd1881042d85f97d1a94f1dd24b2
pavelzubov <pasha224@yandex.ru>
2018-12-05 18:34:00 +0300

    Add CURRENCY_FRACTIONS to constants

--- packages/shared/utils/currency-converter.js
+++ packages/shared/utils/currency-converter.js
@@ -9,3 +9,16 @@ export const convertToCurrency = (value = 0, rate) => {
 export const calculateValueOfEntryFee = (value = 0, percentage) => {
   return (value * percentage) / 100;
 };
+
+export const CURRENCY_FRACTIONS = currency => {
+  switch (currency) {
+    case "BTC":
+    case "ETH":
+      return 8;
+    case "USD":
+    case "EUR":
+      return 2;
+    default:
+      return 4;
+  }
+};
