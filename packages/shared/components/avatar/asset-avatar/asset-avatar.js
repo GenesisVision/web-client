98d1957932ed2b320ea3b044c33968789840b179
nkrivous <kolsln@list.ru>
2018-12-07 17:27:42 +0300

    Program level

--- packages/shared/components/avatar/asset-avatar/asset-avatar.js
+++ packages/shared/components/avatar/asset-avatar/asset-avatar.js
@@ -1,10 +1,42 @@
 import { GVProgramAvatar } from "gv-react-components";
-import React from "react";
+import React, { Fragment, PureComponent } from "react";
+import Popover from "shared/components/popover/popover";
 import withUrl from "shared/decorators/with-url";
 
-let AssetAvatar = props => {
-  return <GVProgramAvatar {...props} />;
-};
+class AssetAvatar extends PureComponent {
+  state = {
+    anchor: null
+  };
+
+  handleMouseEnter = event => {
+    this.setState({ anchor: event.currentTarget });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ anchor: null });
+  };
+  render() {
+    return (
+      <Fragment>
+        <GVProgramAvatar
+          onMouseEnterLevel={this.handleMouseEnter}
+          onMouseLeaveLevel={this.handleMouseLeave}
+          {...this.props}
+        />
+        <Popover
+          disableBackdropClick
+          noPadding
+          anchorEl={this.state.anchor}
+          className="tooltip__popover"
+          vertical={this.props.vertical}
+          horizontal={this.props.horizontal}
+        >
+          {"title || component || render()"}
+        </Popover>
+      </Fragment>
+    );
+  }
+}
 
 AssetAvatar = withUrl("url")(AssetAvatar);
 export default AssetAvatar;
