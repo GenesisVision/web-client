a6db206f252c00b2062c83c84f2e1562971d691f
nkrivous <kolsln@list.ru>
2018-12-19 12:53:22 +0300

    Add items count to investor dashboard

--- packages/investor/src/pages/dashboard/services/dashboard.service.js
+++ packages/investor/src/pages/dashboard/services/dashboard.service.js
@@ -1,4 +1,5 @@
 import authService from "shared/services/auth-service";
+import investorApi from "shared/services/api-client/investor-api";
 
 import * as actions from "../actions/dashboard.actions";
 
@@ -6,4 +7,16 @@ export const getPortfolioChart = () => (dispatch, getState) => {
   const authorization = authService.getAuthArg();
 
   dispatch(actions.fetchPortfolioChart(authorization));
+};
+
+export const fetchAssetsCount = () => {
+  const authorization = authService.getAuthArg();
+  const filtering = { take: 0 };
+  return Promise.all([
+    investorApi.v10InvestorProgramsGet(authorization, filtering),
+    investorApi.v10InvestorFundsGet(authorization, filtering)
+  ]).then(([programsData, fundsData]) => ({
+    programsCount: programsData.total,
+    fundsCount: fundsData.total
+  }));
 };
