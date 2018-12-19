5f8cbf40dafe2c0134938a640bebc3fa3dd43bcb
nkrivous <kolsln@list.ru>
2018-12-19 12:35:19 +0300

    Refactor programs&funds api

--- packages/shared/services/api-client/programs-api.js
+++ packages/shared/services/api-client/programs-api.js
@@ -1,9 +1,8 @@
 import { ProgramsApi } from "gv-api-web";
 
-import withApiProxy from "./api-proxy";
 import apiClient from "./swagger-custom-client";
+import withApiProxy from "./api-proxy";
 
-const programsApi = new ProgramsApi(apiClient);
+const programsApi = withApiProxy(new ProgramsApi(apiClient));
 
-export const programsApiProxy = withApiProxy(programsApi);
 export default programsApi;
