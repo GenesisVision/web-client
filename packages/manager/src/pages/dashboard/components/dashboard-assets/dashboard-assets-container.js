6aa25bcb9ff1c07040ed93d877f03ac0cae12058
nkrivous <kolsln@list.ru>
2018-12-19 16:37:33 +0300

    Attempt 8

--- packages/manager/src/pages/dashboard/components/dashboard-assets/dashboard-assets-container.js
+++ packages/manager/src/pages/dashboard/components/dashboard-assets/dashboard-assets-container.js
@@ -1,18 +1,19 @@
+import { GVButton } from "gv-react-components";
 import { CREATE_FUND_PAGE_ROUTE } from "pages/create-fund/create-fund.constants";
 import { CREATE_PROGRAM_PAGE_ROUTE } from "pages/create-program/create-program.constants";
+import React, { Component } from "react";
 import { translate } from "react-i18next";
 import { connect } from "react-redux";
-
-import { bindActionCreators, compose } from "redux";
 import { Link } from "react-router-dom";
-import { getDashboardPrograms } from "../../services/dashboard-programs.service";
-import { fetchAssetsCount } from "../../services/dashboard.service";
-import React, { Component } from "react";
-import { GVButton } from "gv-react-components";
-import { getDashboardFunds } from "../../services/dashboard-funds.service";
-import { MANAGER } from "shared/constants/constants";
+import { bindActionCreators, compose } from "redux";
 import DashboardAssets from "shared/components/dashboard/dashboard-assets/dashboard-assets";
 import { ChartIcon } from "shared/components/icon/chart-icon";
+import { MANAGER } from "shared/constants/constants";
+
+import { getDashboardFunds } from "../../services/dashboard-funds.service";
+import { getDashboardPrograms } from "../../services/dashboard-programs.service";
+import { fetchAssetsCount } from "../../services/dashboard.service";
+
 class DashboardAssetsContainer extends Component {
   getAssets = () => {
     const { service } = this.props;
