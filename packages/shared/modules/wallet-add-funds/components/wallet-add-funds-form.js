f0725db320ca7fa4691ccbcf3182c5763d30dd55
a.popova <bob.m@mail.ru>
2018-12-21 14:53:47 +0300

    GV-294 Reset the amount in the input when changing the currency in the add-funds form

--- packages/shared/modules/wallet-add-funds/components/wallet-add-funds-form.js
+++ packages/shared/modules/wallet-add-funds/components/wallet-add-funds-form.js
@@ -2,7 +2,7 @@ import copy from "copy-to-clipboard";
 import { withFormik } from "formik";
 import { GVButton, GVFormikField, GVTextField } from "gv-react-components";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 import { translate } from "react-i18next";
 import NumberFormat from "react-number-format";
 import { compose } from "redux";
@@ -14,18 +14,15 @@ import { convertToCurrency } from "shared/utils/currency-converter";
 import { formatCurrencyValue, validateFraction } from "shared/utils/formatter";
 
 const MAX_AMOUNT_GVT = 4436644;
+const MAX_AMOUNT_DEFAULT = 1000000;
 
-const WalletAddFundsForm = ({
-  t,
-  notifySuccess,
-  notifyError,
-  values,
-  wallets
-}) => {
-  const selected = wallets.find(w => w.currency === values.currency) || {};
-  const { address = "", currency = null, rateToGVT = null } = selected;
+class WalletAddFundsForm extends Component {
+  state = {
+    currentAmount: ""
+  };
 
-  const onCopy = () => {
+  onCopy = () => {
+    const { t, notifySuccess, notifyError } = this.props;
     try {
       copy(address);
       notifySuccess(t("wallet-add-funds.copy-to-clipboard-success"));
@@ -34,86 +31,107 @@ const WalletAddFundsForm = ({
     }
   };
 
-  const isAllow = values => {
+  isAllow = (values, currency) => {
     const { formattedValue } = values;
+    const maxAmount = currency === "GVT" ? MAX_AMOUNT_GVT : MAX_AMOUNT_DEFAULT;
     return formattedValue === "" ||
       validateFraction(formattedValue, currency) &&
-      convertToCurrency(formattedValue, rateToGVT) <= MAX_AMOUNT_GVT;
+      formattedValue <= maxAmount;
   };
 
-  return (
-    <form id="add-funds" className="wallet-add-funds-popup">
-      <div className="dialog__top">
-        <div className="dialog__header">
-          <h2>{t("wallet-add-funds.title")}</h2>
-        </div>
-        <div className="dialog-field">
-          <GVFormikField
-            name="currency"
-            component={GVTextField}
-            label={t("wallet-add-funds.select-currency")}
-            InputComponent={Select}
-          >
-            {wallets.map(wallet => {
-              const { description, currency } = wallet;
-              return (
-                <option
-                  value={currency}
-                  key={currency}
-                >{`${description} | ${currency}`}</option>
-              );
-            })}
-          </GVFormikField>
-        </div>
-        <GVFormikField
-          name="amount"
-          label={t("wallet-add-funds.will-send")}
-          component={GVTextField}
-          adornment={currency}
-          autoComplete="off"
-          autoFocus
-          InputComponent={NumberFormat}
-          allowNegative={false}
-          isAllowed={isAllow}
-        />
-        {currency !== "GVT" && <div className="gv-text-field__wrapper">
-          <StatisticItem
-            big
-            label={t("wallet-add-funds.will-get")}
-          >
-            <NumberFormat
-              value={formatCurrencyValue(
-                convertToCurrency(values.amount, rateToGVT),
-                "GVT"
-              )}
-              suffix=" GVT"
-              displayType="text"
-            />
-          </StatisticItem>
-        </div>}
-      </div>
-      <div className="dialog__bottom wallet-add-funds-popup__bottom">
-        <GVqr className="wallet-add-funds-popup__qr" value={address} />
-        <StatisticItem
-          className="wallet-add-funds-popup__address"
-          label={t("wallet-add-funds.deposit-address")}
-        >
-          {address}
-        </StatisticItem>
-        <GVButton color="secondary" onClick={onCopy} disabled={!address}>
-          <CopyIcon />
-          &nbsp;
-          {t("buttons.copy")}
-        </GVButton>
-        {currency !== "GVT" && currency !== null && (
-          <div className="dialog__info">
-            {t("wallet-add-funds.disclaimer", { currency })}
+  onChangeCurrency = (name, target) => {
+    const { setFieldValue } = this.props;
+    setFieldValue("currency", target.props.value);
+    this.setState({ currentAmount: "" });
+  };
+
+  onChangeAmount = (event) => {
+    this.setState({ currentAmount: event.target.value });
+  };
+
+  render() {
+    const { t, values, wallets } = this.props;
+    const { currentAmount } = this.state;
+    const selected = wallets.find(w => w.currency === values.currency) || {};
+    const { address = "", currency = null, rateToGVT = null } = selected;
+    return (
+      <form id="add-funds" className="wallet-add-funds-popup">
+        <div className="dialog__top">
+          <div className="dialog__header">
+            <h2>{t("wallet-add-funds.title")}</h2>
           </div>
-        )}
-      </div>
-    </form>
-  );
-};
+          <div className="dialog-field">
+            <GVFormikField
+              name="currency"
+              component={GVTextField}
+              label={t("wallet-add-funds.select-currency")}
+              InputComponent={Select}
+              onChange={this.onChangeCurrency}
+            >
+              {wallets.map(wallet => {
+                const { description, currency } = wallet;
+                return (
+                  <option
+                    value={currency}
+                    key={currency}
+                  >{`${description} | ${currency}`}</option>
+                );
+              })}
+            </GVFormikField>
+          </div>
+          <GVFormikField
+            name="amount"
+            label={t("wallet-add-funds.will-send")}
+            component={GVTextField}
+            adornment={currency}
+            autoComplete="off"
+            autoFocus
+            InputComponent={NumberFormat}
+            allowNegative={false}
+            isAllowed={this.isAllow}
+            onChange={this.onChangeAmount}
+            value={currentAmount}
+          />
+          {currency !== "GVT" && (
+            <div className="gv-text-field__wrapper">
+            <StatisticItem
+              big
+              label={t("wallet-add-funds.will-get")}
+            >
+              <NumberFormat
+                value={formatCurrencyValue(
+                  convertToCurrency(currentAmount, rateToGVT),
+                  "GVT"
+                )}
+                suffix=" GVT"
+                displayType="text"
+              />
+            </StatisticItem>
+          </div>)}
+        </div>
+        <div className="dialog__bottom wallet-add-funds-popup__bottom">
+          <GVqr className="wallet-add-funds-popup__qr" value={address} />
+          <StatisticItem
+            className="wallet-add-funds-popup__address"
+            label={t("wallet-add-funds.deposit-address")}
+          >
+            {address}
+          </StatisticItem>
+          <GVButton color="secondary" onClick={this.onCopy} disabled={!address}>
+            <CopyIcon />
+            &nbsp;
+            {t("buttons.copy")}
+          </GVButton>
+          {currency !== "GVT" && currency !== null && (
+            <div className="dialog__info">
+              {t("wallet-add-funds.disclaimer", { currency })}
+            </div>
+          )}
+        </div>
+      </form>
+    );
+  }
+}
 
 WalletAddFundsForm.propTypes = {
   wallets: PropTypes.arrayOf(
