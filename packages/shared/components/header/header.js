03f19b80cee6d1d2a86ee19292222bde7f32cac1
Гребенщиков Евгений <evgreb@gmail.com>
2018-12-24 19:52:03 +0300

    GV-305 redirect to prev page after login -I

--- packages/shared/components/header/header.js
+++ packages/shared/components/header/header.js
@@ -76,7 +76,12 @@ class Header extends Component {
             </Fragment>
           ) : (
             <div className="header__buttons">
-              <Link to={LOGIN_ROUTE}>
+              <Link
+                to={{
+                  pathname: LOGIN_ROUTE,
+                  state: this.props.backPath
+                }}
+              >
                 <GVButton variant="outlined" color="secondary">
                   {t("auth.login.title")}
                 </GVButton>
@@ -90,6 +95,7 @@ class Header extends Component {
           )}
         </div>
         <NavigationMobile
+          backPath={this.props.backPath}
           logout={logout}
           isOpenNavigation={this.state.isOpenNavigation}
           email={email}
