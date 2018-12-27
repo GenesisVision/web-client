c12097b75213d7f6e4a89ae728c75a388ec42728
nkrivous <kolsln@list.ru>
2018-12-27 14:58:09 +0300

    Add specific auth part url

--- packages/investor/src/pages/auth/login/login/login.page.js
+++ packages/investor/src/pages/auth/login/login/login.page.js
@@ -6,13 +6,14 @@ import LoginFormContainer from "shared/components/auth/login/login/login-form-co
 
 import AuthTabs from "../../components/auth-tabs/auth-tabs";
 import { FORGOT_PASSWORD_ROUTE } from "../../forgot-password/forgot-password.routes";
+import { LOGIN_ROUTE } from "../login.routes";
 import * as loginService from "../services/login.service";
 
 const LoginPage = ({ location }) => {
   const from = location.state || HOME_ROUTE;
   return (
     <div className="login">
-      <AuthTabs />
+      <AuthTabs authPartUrl={LOGIN_ROUTE} />
       <LoginFormContainer
         from={from}
         loginService={loginService}
