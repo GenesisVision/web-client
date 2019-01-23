2c3278bc9f9c834a65ee781bac293c51404df20f
a.popova <bob.m@mail.ru>
2019-01-23 18:15:41 +0300

    GV-454 deleted console.log

--- packages/shared/components/programs/program-details/program-trades/program-details-history-section.js
+++ packages/shared/components/programs/program-details/program-trades/program-details-history-section.js
@@ -46,7 +46,6 @@ class ProgramDetailsHistorySection extends PureComponent {
       isInvested,
       eventTypeFilterValues
     } = this.props;
-    console.log(eventTypeFilterValues);
     return (
       <Surface className="details-history">
         <div className="details-history__header">
