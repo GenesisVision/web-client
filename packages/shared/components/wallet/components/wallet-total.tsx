91d7418ea6cf6953608d7cf96dd22b4fd5187fd0
Гребенщиков Евгений <evgreb@gmail.com>
2019-01-25 10:30:10 +0300

    GV-454 add loader

--- packages/shared/components/wallet/components/wallet-total.tsx
+++ packages/shared/components/wallet/components/wallet-total.tsx
@@ -8,6 +8,7 @@ import { compose } from "redux";
 import Page from "../../page/page";
 import { INVESTOR_EVENT_TYPE_FILTER_VALUES } from "../../table/components/filtering/event-type-filter/event-type-filter.constants";
 import WalletBalanceElements from "./wallet-balance/wallet-balance-elements";
+import WalletBalanceLoader from "./wallet-balance/wallet-balance-loader";
 import WalletContainerTotal from "./wallet-container/wallet-container-total";
 
 interface IWalletProps {
@@ -21,16 +22,16 @@ interface IWalletState {
 
 class WalletTotal extends React.Component<IWalletProps, IWalletState> {
   render() {
-    const { t, info } = this.props;
-    if (!this.props.info) return <p>...loading</p>;
+    const { t, info, wallets } = this.props;
+    if (!info) return <WalletBalanceLoader />;
     return (
       <Page title={t("wallet-page.title")}>
         <h1>{t("wallet-page.title")}</h1>
         <div className="wallet-balance">
-          <WalletBalanceElements walletBalanceData={this.props.info} />
+          <WalletBalanceElements walletBalanceData={info} />
         </div>
         <WalletContainerTotal
-          wallets={this.props.wallets}
+          wallets={wallets}
           eventTypeFilterValues={INVESTOR_EVENT_TYPE_FILTER_VALUES}
         />
       </Page>
