b7fc4a0be5362978379318f32d01bbe734c181d9
Гребенщиков Евгений <evgreb@gmail.com>
2019-01-25 10:47:28 +0300

    GV-454 add currency page

--- packages/shared/components/wallet/components/wallet-currency.tsx
+++ packages/shared/components/wallet/components/wallet-currency.tsx
@@ -1,4 +1,15 @@
+import { IState } from "manager-web-portal/src/reducers";
 import * as React from "react";
+import { translate } from "react-i18next";
+import { connect } from "react-redux";
+import { compose } from "redux";
+
+import NotFoundPage from "../../not-found/not-found.routes";
+import Page from "../../page/page";
+import { INVESTOR_EVENT_TYPE_FILTER_VALUES } from "../../table/components/filtering/event-type-filter/event-type-filter.constants";
+import WalletBalanceElements from "./wallet-balance/wallet-balance-elements";
+import WalletBalanceLoader from "./wallet-balance/wallet-balance-loader";
+import WalletContainerTotal from "./wallet-container/wallet-container-total";
 
 interface IWalletProps {
   currency: string;
@@ -9,16 +20,35 @@ interface IWalletState {
 }
 
 class WalletCurrency extends React.Component<IWalletProps, IWalletState> {
-  state = {
-    info: null
-  };
-  componentDidMount() {
-    const currency = this.props.currency;
-  }
-
   render() {
-    return this.state.info;
+    const { t, info, isPending } = this.props;
+    if (!info && isPending) return <WalletBalanceLoader />;
+    if (!info) return <NotFoundPage />;
+    return (
+      <Page title={info.title}>
+        <h1>{info.title}</h1>
+        <div className="wallet-balance">
+          <WalletBalanceElements walletBalanceData={info} />
+        </div>
+      </Page>
+    );
   }
 }
 
-export default WalletCurrency;
+const mapStateToProps = (state: IState, ownProps) => {
+  const isPending = state.wallet.info.isPending;
+  const info = state.wallet.info.data
+    ? state.wallet.info.data.wallets.find(
+        wallet => wallet.currency.toLowerCase() === ownProps.currency
+      )
+    : null;
+  return {
+    info,
+    isPending
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  translate()
+)(WalletCurrency);
