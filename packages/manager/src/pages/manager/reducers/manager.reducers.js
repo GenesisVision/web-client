79fb7968e2ca1bd79d46dbd2365049196853c907
Гребенщиков Евгений <evgreb@gmail.com>
2019-01-31 11:47:37 +0300

    types for manager reducer -M

--- packages/manager/src/pages/manager/reducers/manager.reducers.js
+++
@@ -1,7 +0,0 @@
-import apiReducerFactory from "shared/reducers/api-reducer/api-reducer";
-
-import { MANAGER_PROFILE } from "../actions/manager.actions";
-
-const managerReducer = apiReducerFactory({ apiType: MANAGER_PROFILE });
-
-export default managerReducer;
