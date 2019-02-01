68ca94538dc8b0c9828e88a248ea975b306cd31a
Гребенщиков Евгений <evgreb@gmail.com>
2019-02-01 17:13:18 +0300

    remove redux for transactions table

--- packages/shared/components/wallet/components/wallet-transactions/wallet-transactions.js
+++ packages/shared/components/wallet/components/wallet-transactions/wallet-transactions.js
@@ -7,74 +7,78 @@ import NumberFormat from "react-number-format";
 import { PORTFOLIO_EVENTS_DEFAULT_FILTERING } from "shared/components/portfolio-events-table/portfolio-events-table.constants";
 import Surface from "shared/components/surface/surface";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
-import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
+import {
+  DATE_RANGE_FILTER_NAME,
+  DEFAULT_DATE_RANGE_FILTER_VALUE
+} from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
+import { composeDefaultDateRangeFilter } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.helpers";
 import SelectFilter from "shared/components/table/components/filtering/select-filter/select-filter";
 import TableCell from "shared/components/table/components/table-cell";
-import TableContainer from "shared/components/table/components/table-container";
+import TableModule from "shared/components/table/components/table-module";
 import TableRow from "shared/components/table/components/table-row";
+import { FilterType } from "shared/components/table/helpers/filtering.helpers";
 import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
 import { WALLET_TRANSACTION_ACTIONS_VALUES } from "shared/components/wallet/components/wallet-list/wallet-list.constants";
-import EmptyTransactionsIcon from "shared/media/empty-wallet.svg";
 import SuccessTransactionsIcon from "shared/media/transactions/success.svg";
+import { walletApi } from "shared/services/api-client/wallet-api";
+import authService from "shared/services/auth-service";
 import { formatValue } from "shared/utils/formatter";
 
 import Profitability from "../../../profitability/profitability";
-import { fetchWalletTransactions } from "../../services/wallet.services";
 import { WALLET_TRANSACTIONS_COLUMNS } from "./wallet-transactions.constants";
-import { walletTableTransactionsSelector } from "./wallet-transactions.selector";
 
-const emptyTransactions = t => (
-  <div className="empty-transactions">
-    <div className="empty-transactions__subtitle">
-      {t("wallet-page.transactions.title")}
-    </div>
-    <div className="empty-transactions__disclaimer">
-      <div className="empty-transactions__icon">
-        <img src={EmptyTransactionsIcon} alt="" />
-      </div>
-      <div className="empty-transactions__text">
-        {t("wallet-page.transactions.empty-transactions")}
-      </div>
-    </div>
-  </div>
-);
+const TRANSACTIONS_FILTERS = {
+  dateRange: DEFAULT_DATE_RANGE_FILTER_VALUE
+};
+
+const DEFAULT_FILTERS = [
+  { ...composeDefaultDateRangeFilter() },
+  {
+    name: "txAction",
+    type: FilterType.general
+  }
+];
 
 class WalletTransactions extends Component {
   fetch = filters => {
     const { currency } = this.props;
-    return fetchWalletTransactions({
-      wallet: currency,
-      ...filters
-    });
+    return walletApi
+      .v10WalletTransactionsGet(authService.getAuthArg(), {
+        wallet: currency,
+        ...filters
+      })
+      .then(data => ({ total: data.total, items: data.transactions }));
   };
 
   render() {
     const { t, createButtonToolbar } = this.props;
     return (
       <Surface className="wallet-transactions">
-        <TableContainer
-          isFetchOnMount
+        <TableModule
+          defaultFilters={DEFAULT_FILTERS}
           paging={DEFAULT_PAGING}
+          filtering={TRANSACTIONS_FILTERS}
           createButtonToolbar={createButtonToolbar}
           getItems={this.fetch}
-          dataSelector={walletTableTransactionsSelector}
-          renderFilters={(updateFilter, filtering) => (
-            <Fragment>
-              <SelectFilter
-                name={"txAction"}
-                label="Action"
-                value={filtering["txAction"]}
-                values={WALLET_TRANSACTION_ACTIONS_VALUES}
-                onChange={updateFilter}
-              />
-              <DateRangeFilter
-                name={DATE_RANGE_FILTER_NAME}
-                value={filtering["dateRange"]}
-                onChange={updateFilter}
-                startLabel={t("filters.date-range.account-creation")}
-              />
-            </Fragment>
-          )}
+          renderFilters={(updateFilter, filtering) => {
+            return (
+              <Fragment>
+                <SelectFilter
+                  name={"txAction"}
+                  label="Action"
+                  value={filtering["txAction"]}
+                  values={WALLET_TRANSACTION_ACTIONS_VALUES}
+                  onChange={updateFilter}
+                />
+                <DateRangeFilter
+                  name={DATE_RANGE_FILTER_NAME}
+                  value={filtering["dateRange"]}
+                  onChange={updateFilter}
+                  startLabel={t("filters.date-range.account-creation")}
+                />
+              </Fragment>
+            );
+          }}
           columns={WALLET_TRANSACTIONS_COLUMNS}
           renderHeader={column => (
             <span
