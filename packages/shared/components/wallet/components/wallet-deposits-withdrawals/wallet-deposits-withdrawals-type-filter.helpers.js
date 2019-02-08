d476084e1613bc7c49ebc62f0e6de81e85d1eacb
pavelzubov <pasha224@yandex.ru>
2019-02-08 10:00:43 +0300

    Remove unused code in WalletDepositsWithdrawals Constants

--- packages/shared/components/wallet/components/wallet-deposits-withdrawals/wallet-deposits-withdrawals-type-filter.helpers.js
+++
@@ -1,8 +0,0 @@
-import { FilterType } from "shared/components/table/helpers/filtering.helpers";
-
-export const composeDefaultDepositsWithdrawalsTypeFilter = () => ({
-  name: "txAction",
-  composeRequestValue: value => value,
-  defaultValue: undefined,
-  type: FilterType.general
-});
--- packages/shared/components/wallet/components/wallet-deposits-withdrawals/wallet-deposits-withdrawals.selector.js
+++
@@ -1,8 +0,0 @@
-import { tableSelectorCreator } from "shared/components/table/helpers/table.selector";
-
-const walletDepositsWithdrawalsSelector = state =>
-  state.wallet.depositsWithdrawals;
-export const walletTableDepositsWithdrawalsSelector = tableSelectorCreator(
-  walletDepositsWithdrawalsSelector,
-  "transactions"
-);
