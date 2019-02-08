81ec4e646a09c27da400603e9d5af27547ace890
pavelzubov <pasha224@yandex.ru>
2019-02-08 09:41:36 +0300

    Upgrade AllDepositsWithdrawalsRow

--- packages/shared/components/wallet/components/wallet-deposits-withdrawals/all-deposits-withdrawals-row.tsx
+++ packages/shared/components/wallet/components/wallet-deposits-withdrawals/all-deposits-withdrawals-row.tsx
@@ -3,11 +3,11 @@ import moment from "moment";
 import * as React from "react";
 import NumberFormat from "react-number-format";
 
-import TransactionDetailsPopup from "../../../../modules/transaction-details/transaction-details-popup";
-import { formatCurrencyValue } from "../../../../utils/formatter";
-import Profitability from "../../../profitability/profitability";
-import TableCell from "../../../table/components/table-cell";
-import TableRow from "../../../table/components/table-row";
+import TransactionDetailsPopup from "shared/modules/transaction-details/transaction-details-popup";
+import { formatCurrencyValue } from "shared/utils/formatter";
+import Profitability from "shared/components/profitability/profitability";
+import TableCell from "shared/components/table/components/table-cell";
+import TableRow from "shared/components/table/components/table-row";
 import GVTIcon from "shared/media/currency/GVT.svg";
 
 export interface ITransactionRowProps {
@@ -40,24 +40,24 @@ class AllDepositsWithdrawalsRow extends React.Component<
           open={this.state.isOpen}
           onClose={this.closePopup}
         />
-        <TableRow className="wallet-deposits-withdrawals____row">
-          <TableCell className="wallet-deposits-withdrawals____cell wallet-deposits-withdrawals____cell--wallet">
+        <TableRow className="wallet-deposits-withdrawals__row" onClick={this.openPopup}>
+          <TableCell className="wallet-deposits-withdrawals__cell wallet-deposits-withdrawals__cell--wallet">
             <img
               src={GVTIcon}
-              className="wallet-deposits-withdrawals____icon"
+              className="wallet-deposits-withdrawals__icon"
               alt="Icon"
             />
             Genesis Vision
           </TableCell>
-          <TableCell className="wallet-deposits-withdrawals____cell wallet-deposits-withdrawals____cell--date">
+          <TableCell className="wallet-deposits-withdrawals__cell wallet-deposits-withdrawals__cell--date">
             {moment(transaction.date).format("DD-MM-YYYY, hh:mm a")}
           </TableCell>
-          <TableCell className="wallet-deposits-withdrawals____cell wallet-deposits-withdrawals____cell--status">
+          <TableCell className="wallet-deposits-withdrawals__cell wallet-deposits-withdrawals__cell--status">
             <a href={transaction.statusUrl} target="_blank">
               {transaction.status}
             </a>
           </TableCell>
-          <TableCell className="wallet-deposits-withdrawals____cell wallet-deposits-withdrawals____cell--amount">
+          <TableCell className="wallet-deposits-withdrawals__cell wallet-deposits-withdrawals__cell--amount">
             <Profitability
               value={formatCurrencyValue(
                 transaction.amount,
