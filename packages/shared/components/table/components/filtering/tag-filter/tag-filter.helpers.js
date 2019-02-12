44a28649639e2127affcffa49d49693eedf47b89
pavelzubov <pasha224@yandex.ru>
2019-02-12 14:41:20 +0300

    Change composeDefaultTagFilter to programsTagFilter

--- packages/shared/components/table/components/filtering/tag-filter/tag-filter.helpers.js
+++ packages/shared/components/table/components/filtering/tag-filter/tag-filter.helpers.js
@@ -4,9 +4,9 @@ import {
   TAG_FILTER_NAME
 } from "./tag-filter.constants";
 
-export const composeDefaultTagFilter = () => ({
+export const programsTagFilter = {
   name: TAG_FILTER_NAME,
   composeRequestValue: value => value,
   defaultValue: TAG_FILTER_DEFAULT_VALUE,
   type: FilterType.custom
-});
+};
