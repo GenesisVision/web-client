42ae25cf5035bc7a86fbf3ebafa5ea34bbd1399d
pavelzubov <pasha224@yandex.ru>
2019-02-12 10:54:37 +0300

    Change tag filter structure, remove Filter component

--- packages/shared/components/table/components/filtering/tag-filter/tag-filter.js
+++ packages/shared/components/table/components/filtering/tag-filter/tag-filter.js
@@ -1,30 +1,46 @@
 import "./tag-filter.scss";
 
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { translate } from "react-i18next";
 
-import Filter from "../filter";
 import TagFilterPopover from "./tag-filter-popover";
+import Popover from "shared/components/popover/popover";
 
 class TagFilter extends Component {
+  state = {
+    anchor: null
+  };
   renderValueText = value => value;
+  handleOpenPopover = event => this.setState({ anchor: event.currentTarget });
+  handleClosePopover = () => this.setState({ anchor: null });
+  handleChangeFilter = value => {
+    this.handleClosePopover();
+    this.props.onChange({ name: this.props.name, value });
+  };
 
   render() {
     const { t, values, value } = this.props;
+    const { anchor } = this.state;
     return (
-      <Filter
-        label={t("filters.level.label")}
-        name={this.props.name}
-        renderValueText={this.renderValueText}
-        value={Array.isArray(value) ? value : [value]}
-        updateFilter={this.props.onChange}
-      >
-        <TagFilterPopover
-          // value={Array.isArray(value) ? value : [value]}
-          values={values}
-        />
-      </Filter>
+      <Fragment>
+        <div className="filter" onClick={this.handleOpenPopover}>
+          <div className="filter__value">{value}</div>
+          <div className="filter__values">Values</div>
+        </div>
+        <Popover
+          anchorEl={anchor}
+          onClose={this.handleClosePopover}
+          horizontal={"right"}
+          noPadding
+        >
+          <TagFilterPopover
+            value={Array.isArray(value) ? value : [value]}
+            changeFilter={this.handleChangeFilter}
+            values={values}
+          />
+        </Popover>
+      </Fragment>
     );
   }
 }
