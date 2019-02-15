cbf3cfc8e946ac4deab08cb7c50c6f0bb95d4b46
Евгений Гребенщиков <evgreb@gmail.com>
2019-02-15 17:11:51 +0300

    GV-533 add i18n for open/close

--- packages/shared/modules/transaction-details/open-close-details.tsx
+++ packages/shared/modules/transaction-details/open-close-details.tsx
@@ -1,39 +1,43 @@
-import { TransactionDetails } from "gv-api-web";
 import * as React from "react";
 import NumberFormat from "react-number-format";
 
-import Profitability from "../../components/profitability/profitability";
 import StatisticItem from "../../components/statistic-item/statistic-item";
 import { formatCurrencyValue } from "../../utils/formatter";
 import TransactionAsset from "./details-asset";
+import { ITransactionDetailsProps } from "./transaction-details";
 
-const OpenCloseTransaction = (props: { data: TransactionDetails }) => {
-  const { data } = props;
+const OpenCloseTransaction = (props: ITransactionDetailsProps) => {
+  const { data, t } = props;
   return (
     <React.Fragment>
       <div className="dialog__top">
         <div className="dialog__header">
-          <h2>Transaction Details</h2>
+          <h2>{t(`transactions-details.title`)}</h2>
           <p>
-            {`${data.type}`}{" "}
-            {`${data.programDetails.programType.toLowerCase()}`}
+            {t(
+              `transactions-details.${data.type}.${
+                data.programDetails.programType
+              }`
+            )}
           </p>
         </div>
-      </div>
-      <div className="dialog__bottom">
-        <StatisticItem label={`${data.programDetails.programType}`}>
+        <StatisticItem
+          label={t(`transactions-details.${data.programDetails.programType}`)}
+        >
           <TransactionAsset data={data.programDetails} />
         </StatisticItem>
-        <StatisticItem label={"Status"}>{data.status}</StatisticItem>
-        <StatisticItem label={"Investment amount"} big>
-          <Profitability value={data.amount} prefix="sign">
-            <NumberFormat
-              value={formatCurrencyValue(data.amount, data.currency)}
-              suffix={` ${data.currency}`}
-              allowNegative={false}
-              displayType="text"
-            />
-          </Profitability>
+      </div>
+      <div className="dialog__bottom">
+        <StatisticItem label={t(`transactions-details.status.title`)}>
+          {data.status}
+        </StatisticItem>
+        <StatisticItem label={t(`transactions-details.investment.amount`)} big>
+          <NumberFormat
+            value={formatCurrencyValue(data.amount, data.currency)}
+            suffix={` ${data.currency}`}
+            allowNegative={true}
+            displayType="text"
+          />
         </StatisticItem>
       </div>
     </React.Fragment>
