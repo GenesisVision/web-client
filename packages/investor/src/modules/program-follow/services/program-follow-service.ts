fd3bb1bc74a7072842a411a66845a276258cc59d
pavelzubov <pasha224@yandex.ru>
2019-02-17 15:37:28 +0300

    Update types

--- packages/investor/src/modules/program-follow/services/program-follow-service.ts
+++ packages/investor/src/modules/program-follow/services/program-follow-service.ts
@@ -2,6 +2,7 @@ import walletApi from "shared/services/api-client/wallet-api";
 import authService from "shared/services/auth-service";
 import signalApi from "shared/services/api-client/signal-api";
 import { IRequestParams } from "../follow-popup/follow-popup-form";
+import { CopyTradingAccountsList, WalletsInfo } from "gv-api-web";
 
 export const getWalletsAddresses = (): Promise<WalletsInfo> =>
   walletApi.v10WalletAddressesGet(authService.getAuthArg());
