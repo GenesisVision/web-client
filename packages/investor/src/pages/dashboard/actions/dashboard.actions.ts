fcf3884122395b285f94df0d91b4e47f578fb66d
nkrivous <kolsln@list.ru>
2019-02-19 16:23:51 +0300

    Add types to dashboard actions

--- packages/investor/src/pages/dashboard/actions/dashboard.actions.ts
+++ packages/investor/src/pages/dashboard/actions/dashboard.actions.ts
@@ -1,5 +1,6 @@
 import investorApi from "shared/services/api-client/investor-api";
 import signalApi from "shared/services/api-client/signal-api";
+import { Nullable } from "shared/utils/types";
 
 export const DASHBOARD_PROGRAMS = "DASHBOARD_PROGRAMS";
 export const DASHBOARD_FUNDS = "DASHBOARD_FUNDS";
@@ -15,42 +16,49 @@ export const DASHBOARD_TRADES_HISTORY = "DASHBOARD_TRADES_HISTORY";
 export const CLEAR_DASHBOARD_ASSETS_TABLE = "CLEAR_DASHBOARD_ASSETS_TABLE";
 export const CLEAR_DASHBOARD_TRADES_TABLE = "CLEAR_DASHBOARD_TRADES_TABLE";
 
-export const fetchDashboardPrograms = (auth, filters) => {
+export const fetchDashboardPrograms = (
+  auth: Nullable<string>,
+  filters: any
+) => {
   return {
     type: DASHBOARD_PROGRAMS,
     payload: investorApi.v10InvestorProgramsGet(auth, filters)
   };
 };
 
-export const fetchDashboardFunds = (auth, filters) => {
+export const fetchDashboardFunds = (auth: Nullable<string>, filters: any) => {
   return {
     type: DASHBOARD_FUNDS,
     payload: investorApi.v10InvestorFundsGet(auth, filters)
   };
 };
 
-export const fetchPortfolioChart = (auth, filters) => {
+export const fetchPortfolioChart = (auth: Nullable<string>, filters?: any) => {
   return {
     type: DASHBOARD_PORTFOLIO_CHART,
     payload: investorApi.v10InvestorPortfolioChartGet(auth, filters)
   };
 };
 
-export const fetchPortfolioEvents = (auth, filters) => {
+export const fetchPortfolioEvents = (auth: Nullable<string>, filters: any) => {
   return {
     type: DASHBOARD_PORTFOLIO_EVENTS,
     payload: investorApi.v10InvestorPortfolioEventsGet(auth, filters)
   };
 };
 
-export const fetchInRequests = (auth, skip, take) => {
+export const fetchInRequests = (
+  auth: Nullable<string>,
+  skip: number,
+  take: number
+) => {
   return {
     type: DASHBOARD_IN_REQUESTS,
     payload: investorApi.v10InvestorRequestsBySkipByTakeGet(skip, take, auth)
   };
 };
 
-export const cancelProgramRequest = (auth, id) => {
+export const cancelProgramRequest = (auth: Nullable<string>, id: string) => {
   return {
     type: DASHBOARD_CANCEL_PROGRAM_REQUESTS,
     payload: investorApi.v10InvestorProgramsRequestsByIdCancelPost(id, auth)
@@ -61,14 +69,20 @@ export const clearDashboardAssetsTable = () => ({
   type: CLEAR_DASHBOARD_ASSETS_TABLE
 });
 
-export const fetchDashboardOpenTrades = (auth, filters) => {
+export const fetchDashboardOpenTrades = (
+  auth: Nullable<string>,
+  filters: any
+) => {
   return {
     type: DASHBOARD_OPEN_TRADES,
     payload: signalApi.v10SignalTradesOpenGet(auth, filters)
   };
 };
 
-export const fetchDashboardTradesHistory = (auth, filters) => {
+export const fetchDashboardTradesHistory = (
+  auth: Nullable<string>,
+  filters: any
+) => {
   return {
     type: DASHBOARD_OPEN_TRADES,
     payload: signalApi.v10SignalTradesOpenGet(auth, filters) //Use api for trades history
