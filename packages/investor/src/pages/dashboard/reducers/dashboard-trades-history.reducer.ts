fad78c5cfeefbc046c25d1b1844f7b095c260130
nkrivous <kolsln@list.ru>
2019-02-20 15:59:35 +0300

    Update api for trades history

--- packages/investor/src/pages/dashboard/reducers/dashboard-trades-history.reducer.ts
+++ packages/investor/src/pages/dashboard/reducers/dashboard-trades-history.reducer.ts
@@ -5,10 +5,16 @@ import {
   CLEAR_DASHBOARD_TRADES_TABLE,
   DASHBOARD_TRADES_HISTORY
 } from "../actions/dashboard.actions";
+import {
+  DASHBOARD_COPYTRADING_DEFAULT_FILTERING,
+  DASHBOARD_COPYTRADING_FILTERS
+} from "../components/dashboard-assets/dashboard-copytrading.constants";
 
 const dashboardTradesHistoryReducer = tableReducerFactory({
   type: DASHBOARD_TRADES_HISTORY,
   paging: DEFAULT_PAGING,
+  filtering: DASHBOARD_COPYTRADING_DEFAULT_FILTERING,
+  defaultFilters: DASHBOARD_COPYTRADING_FILTERS,
   clearable: true,
   clearableActionType: CLEAR_DASHBOARD_TRADES_TABLE
 });
