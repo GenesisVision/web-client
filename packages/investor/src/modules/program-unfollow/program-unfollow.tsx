887ed40e054ed34e0ae3927b2ad7f930ccd2c4d2
a.popova <bob.m@mail.ru>
2019-02-21 11:24:00 +0300

    fixed translation type in the unfollow popup

--- packages/investor/src/modules/program-unfollow/program-unfollow.tsx
+++ packages/investor/src/modules/program-unfollow/program-unfollow.tsx
@@ -1,23 +1,18 @@
-import { TranslationFunction } from "i18next";
 import React from "react";
-import { translate } from "react-i18next";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
 import ConfirmPopup from "shared/components/confirm-popup/confirm-popup";
 
 export interface IProgramUnfollowProps {
-  t: TranslationFunction;
   open: boolean;
   onClose(): void;
   onApply(): void;
   onCancel?(): void;
 }
 
-const ProgramUnfollow: React.FC<IProgramUnfollowProps> = ({
-  t,
-  open,
-  onClose,
-  onApply
-}) => {
+const ProgramUnfollow: React.FC<
+  InjectedTranslateProps & IProgramUnfollowProps
+> = ({ t, open, onClose, onApply }) => {
   return (
     <ConfirmPopup
       open={open}
