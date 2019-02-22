2a4704709f5feb6f1524c413b21bc84b5001aac9
Евгений Гребенщиков <evgreb@gmail.com>
2019-02-22 18:03:02 +0300

    fix facets

--- packages/shared/components/surface/surface.tsx
+++ packages/shared/components/surface/surface.tsx
@@ -1,17 +1,16 @@
 import "./surface.scss";
 
 import classnames from "classnames";
-import React from "react";
+import * as React from "react";
 
-interface ISurfaceProps {
-  className?: string;
-}
-
-const Surface: React.FunctionComponent<ISurfaceProps> = ({
-  className,
-  children
-}) => {
-  return <div className={classnames("surface", className)}>{children}</div>;
+const Surface: React.FunctionComponent<
+  React.HTMLAttributes<HTMLDivElement>
+> = ({ className, children, ...props }) => {
+  return (
+    <div className={classnames("surface", className)} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Surface;
