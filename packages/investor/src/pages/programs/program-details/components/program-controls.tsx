9e0331d0520beb02d6b82564e367739df6ed802f
nkrivous <kolsln@list.ru>
2019-02-26 12:56:43 +0300

    Fix blocks order

--- packages/investor/src/pages/programs/program-details/components/program-controls.tsx
+++ packages/investor/src/pages/programs/program-details/components/program-controls.tsx
@@ -27,16 +27,16 @@ class ProgramControls extends Component<
             isAuthenticated={isAuthenticated}
             redirectToLogin={redirectToLogin}
           />
-          {isAvailableFollowingTrades ? (
-            <div className="program-details-description__col program-details-description__col--small-size">
-              <SignalProviderControls
-                programDescription={programDescription}
-                isAuthenticated={isAuthenticated}
-                redirectToLogin={redirectToLogin}
-              />
-            </div>
-          ) : null}
         </div>
+        {isAvailableFollowingTrades ? (
+          <div className="program-details-description__col program-details-description__col--small-size">
+            <SignalProviderControls
+              programDescription={programDescription}
+              isAuthenticated={isAuthenticated}
+              redirectToLogin={redirectToLogin}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
