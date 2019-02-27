b4f0118b5cbfe2d5f97fd02d94e17f11070b8059
a.popova <bob.m@mail.ru>
2019-02-27 19:19:44 +0300

    GV-623 created service file for password change

---
+++ packages/shared/modules/password-change/service/password-change.service.ts
@@ -0,0 +1,26 @@
+import { ChangePasswordViewModel } from "gv-api-web";
+import { push } from "react-router-redux";
+import { Dispatch } from "redux";
+import authActions from "shared/actions/auth-actions";
+import { SETTINGS_ROUTE } from "shared/components/profile/profile.constants";
+import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
+import authApi from "shared/services/api-client/auth-api";
+import authService from "shared/services/auth-service";
+
+export const changePassword = (model: ChangePasswordViewModel) => (
+  dispatch: Dispatch
+) => {
+  return authApi
+    .v10AuthPasswordChangePost(authService.getAuthArg(), {
+      model
+    })
+    .then((response: string) => {
+      console.log(response);
+      authService.storeToken(response);
+      dispatch(authActions.updateToken());
+      dispatch(push(SETTINGS_ROUTE));
+      dispatch(
+        alertMessageActions.success("auth.password-change.success-alert", true)
+      );
+    });
+};
