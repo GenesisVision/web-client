e5222b57ef0fef832957ca1027cb71b49df1d1f7
a.popova <bob.m@mail.ru>
2019-02-27 13:00:16 +0300

    GV-626 updated the text and the styles for gvt-fees popup

--- packages/shared/components/gvt-fees/gvt-fees.js
+++ packages/shared/components/gvt-fees/gvt-fees.js
@@ -11,71 +11,98 @@ class GVTFees extends Component {
     return (
       <Dialog wider open={true} onClose={onClose} className="gvt-fees">
         <div className="gvt-fees__container">
-          <div className="gvt-fees__header">
-            <h1>{t("gvt-fees.titles.main")}</h1>
-          </div>
-          <div className="gvt-fees__row">
-            <div className="gvt-fees__text-block">
-              <p className="gvt-fees__paragraph">
-                {t("gvt-fees.section.text-1")}
-              </p>
-              {role === ROLE.INVESTOR ? (
-                <p className="gvt-fees__paragraph">
-                  {t("gvt-fees.section.text-2")}
-                </p>
-              ) : null}
-            </div>
+          <h1>{t("gvt-fees.titles.main")}</h1>
+          <p className="gvt-fees__paragraph">{t("gvt-fees.section.text-1")}</p>
+          {role === ROLE.INVESTOR ? (
+            <p className="gvt-fees__paragraph">
+              {t("gvt-fees.section.text-2")}
+            </p>
+          ) : null}
+          <div className="gvt-fees__table-wrapper">
             <table className="gvt-fees__table">
               <thead>
                 <tr>
                   <th className="gvt-fees__table-head">
-                    Amount held for 7 days*
+                    {t("gvt-fees.table-header.amount")}
                   </th>
-                  <th className="gvt-fees__table-head">Discount</th>
-                  <th className="gvt-fees__table-head">Fees</th>
+                  <th className="gvt-fees__table-head">
+                    {t("gvt-fees.table-header.discount")}
+                  </th>
+                  <th className="gvt-fees__table-head">
+                    {t("gvt-fees.table-header.fees")}
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td className="gvt-fees__table-value">Any GVT</td>
-                  <td className="gvt-fees__table-value">30%</td>
-                  <td className="gvt-fees__table-value">0.210%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-1.amount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-1.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-1.fees")}
+                  </td>
                 </tr>
                 <tr>
                   <td className="gvt-fees__table-value">
-                    From 10 GVT up to 25
+                    {t("gvt-fees.table-row-2.amount")}
                   </td>
-                  <td className="gvt-fees__table-value">35%</td>
-                  <td className="gvt-fees__table-value">0.195%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-2.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-2.fees")}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="gvt-fees__table-value">Up to 50 GVT</td>
-                  <td className="gvt-fees__table-value">40%</td>
-                  <td className="gvt-fees__table-value">0.180%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-3.amount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-3.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-3.fees")}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="gvt-fees__table-value">Up to 100 GVT</td>
-                  <td className="gvt-fees__table-value">45%</td>
-                  <td className="gvt-fees__table-value">0.165%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-4.amount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-4.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-4.fees")}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="gvt-fees__table-value">Up to 500 GVT</td>
-                  <td className="gvt-fees__table-value">50%</td>
-                  <td className="gvt-fees__table-value">0.150%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-5.amount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-5.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-5.fees")}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="gvt-fees__table-value">500 GVT+</td>
-                  <td className="gvt-fees__table-value">55%</td>
-                  <td className="gvt-fees__table-value">0.135%</td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-6.amount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-6.discount")}
+                  </td>
+                  <td className="gvt-fees__table-value">
+                    {t("gvt-fees.table-row-6.fees")}
+                  </td>
                 </tr>
               </tbody>
             </table>
-            <div className="gvt-fees__text-block">
-              <p className="gvt-fees__paragraph">
-                * Please note that to be eligible for a discount tier you should
-                hold GVT on your Genesis Markets wallet for at least 7 days
-              </p>
-            </div>
+            <p className="gvt-fees__paragraph">{t("gvt-fees.note")}</p>
           </div>
         </div>
       </Dialog>
