5cf2399564799260c0865ec48879a2c2f6267c1d
a.popova <bob.m@mail.ru>
2019-02-27 15:57:56 +0300

    GV-496 added the brokers info in the component(v2)

--- packages/manager/src/pages/create-program/components/create-program.container.js
+++ packages/manager/src/pages/create-program/components/create-program.container.js
@@ -97,7 +97,6 @@ class CreateProgramContainer extends Component {
     } = this;
     const { t, headerData, service, platformSettings, wallets } = this.props;
     if (!platformSettings || !headerData || !wallets) return null;
-
     return (
       <div className="create-program-page__container">
         <div className="create-program-page__tabs">
