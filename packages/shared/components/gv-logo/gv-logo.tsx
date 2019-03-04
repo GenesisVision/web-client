40ae1016ee9bedc12f2f9d363746ff8ae293843a
pavelzubov <pasha224@yandex.ru>
2019-03-04 22:32:40 +0300

    Convert GVLogo to tsx

--- packages/shared/components/gv-logo/gv-logo.tsx
+++ packages/shared/components/gv-logo/gv-logo.tsx
@@ -1,8 +1,8 @@
 import "./gv-logo.scss";
 
-import React from "react";
+import * as React from "react";
 
-const GVLogo = () => {
+const GVLogo: React.FC = () => {
   return <i className="gv-logo" />;
 };
 
