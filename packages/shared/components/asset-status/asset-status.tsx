3108c9884b84052c2ef4159cf62a9fd877bb533d
pavelzubov <pasha224@yandex.ru>
2019-03-06 10:45:51 +0300

    Convert AssetStatus to tsx

--- packages/shared/components/asset-status/asset-status.tsx
+++ packages/shared/components/asset-status/asset-status.tsx
@@ -1,18 +1,23 @@
 import "./asset-status.scss";
 
-import classnames from "classnames";
-import React, { Fragment, PureComponent } from "react";
-import { translate } from "react-i18next";
-import connect from "react-redux/es/connect/connect";
+import classNames from "classnames";
+import * as React from "react";
+import { connect } from "react-redux";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
 import GVScroll from "shared/components/scroll/gvscroll";
-import { STATUS } from "shared/constants/constants";
+import { ROLE, STATUS } from "shared/constants/constants";
 
-import Popover from "../popover/popover";
+import Popover, {
+  HORIZONTAL_POPOVER_POS,
+  VERTICAL_POPOVER_POS
+} from "shared/components/popover/popover";
 import AssetStatusRequests from "./asset-status-requests";
+import RootState from "shared/reducers/root-reducer";
+import { Nullable } from "shared/utils/types";
 
-const getStatusClassName = (status, className) => {
-  return classnames("asset-status", className, {
+const getStatusClassName = (status: STATUS, className?: string) => {
+  return classNames("asset-status", className, {
     "asset-status__active": status === STATUS.ACTIVE,
     "asset-status__investing": status === STATUS.INVESTING,
     "asset-status__withdrawing": status === STATUS.WITHDRAWING,
@@ -21,12 +26,30 @@ const getStatusClassName = (status, className) => {
   });
 };
 
-class AssetStatus extends PureComponent {
+interface IAssetStatusProps {
+  className?: string;
+  status: STATUS;
+  id: string;
+  role: ROLE;
+  asset: any;
+  onCancel: any;
+}
+
+interface IAssetStatusState {
+  anchor: Nullable<EventTarget>;
+}
+
+class AssetStatus extends React.Component<
+  IAssetStatusProps & InjectedTranslateProps,
+  IAssetStatusState
+> {
   state = {
     anchor: null
   };
 
-  handleOpenDropdown = event => {
+  handleOpenDropdown = (
+    event: React.MouseEvent<HTMLSpanElement, MouseEvent>
+  ) => {
     if (
       this.props.status === STATUS.INVESTING ||
       this.props.status === STATUS.WITHDRAWING
@@ -38,7 +61,7 @@ class AssetStatus extends PureComponent {
   render() {
     const { t, className, status, id, role, asset, onCancel } = this.props;
     return (
-      <Fragment>
+      <React.Fragment>
         <span
           className={getStatusClassName(status, className)}
           onClick={this.handleOpenDropdown}
@@ -46,8 +69,8 @@ class AssetStatus extends PureComponent {
           {status ? t(`program-statuses.${status}`) : ""}
         </span>
         <Popover
-          horizontal="right"
-          vertical="bottom"
+          horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+          vertical={VERTICAL_POPOVER_POS.BOTTOM}
           anchorEl={this.state.anchor}
           noPadding
           onClose={this.handleCloseDropdown}
@@ -64,12 +87,12 @@ class AssetStatus extends PureComponent {
             </div>
           </GVScroll>
         </Popover>
-      </Fragment>
+      </React.Fragment>
     );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: RootState) => ({
   role: state.profileHeader.info.data
     ? state.profileHeader.info.data.userType
     : null
