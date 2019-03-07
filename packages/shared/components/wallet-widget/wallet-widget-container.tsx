47dc0cd6b2f62af89d311342e2a4304644a19262
pavelzubov <pasha224@yandex.ru>
2019-03-08 00:08:28 +0300

    Convert WalletWidgetContainer to tsx

--- packages/shared/components/wallet-widget/wallet-widget-container.tsx
+++ packages/shared/components/wallet-widget/wallet-widget-container.tsx
@@ -1,9 +1,22 @@
-import React, { Component } from "react";
+import * as React from "react";
 import { connect } from "react-redux";
 import WalletWidget from "shared/components/wallet-widget/wallet-widget";
 import { fetchWallets } from "shared/components/wallet/services/wallet.services";
+import RootState from "shared/reducers/root-reducer";
+import { WalletsGrandTotal } from "gv-api-web";
 
-class WalletWidgetContainer extends Component {
+interface IWalletWidgetContainerProps {
+  className?: string;
+  fetchWallets(): void;
+}
+interface IWalletWidgetContainerStateProps {
+  currency: string;
+  info: WalletsGrandTotal;
+}
+
+class WalletWidgetContainer extends React.Component<
+  IWalletWidgetContainerProps & IWalletWidgetContainerStateProps
+> {
   getWallets = () => {
     this.props.fetchWallets();
   };
@@ -12,7 +25,9 @@ class WalletWidgetContainer extends Component {
     this.getWallets();
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(
+    prevProps: IWalletWidgetContainerProps & IWalletWidgetContainerStateProps
+  ) {
     if (prevProps.currency !== this.props.currency) {
       this.getWallets();
     }
@@ -40,10 +55,12 @@ class WalletWidgetContainer extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (
+  state: RootState
+): IWalletWidgetContainerStateProps => {
   return {
     currency: state.accountSettings.currency,
-    info: state.wallet.info.data ? state.wallet.info.data.grandTotal : {}
+    info: state.wallet.info.data ? state.wallet.info.data.grandTotal : null
   };
 };
 
