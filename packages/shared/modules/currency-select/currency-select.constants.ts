12dea85a609001ab1143343100c7da7da9272df0
pavelzubov <pasha224@yandex.ru>
2019-03-08 22:55:58 +0300

    Convert CurrencySelect to tsx

--- packages/shared/modules/currency-select/currency-select.constants.ts
+++ packages/shared/modules/currency-select/currency-select.constants.ts
@@ -15,3 +15,17 @@ export const CURRENCY_VALUES = {
   GVT: "Genesis Vision Token",
   ...HEADER_CURRENCY_VALUES
 };
+
+export enum CURRENCY_VALUES_ENUM {
+  USDT = "USD Tether",
+  BTC = "Bitcoin",
+  ETH = "Ethereum",
+  ADA = "Cardano",
+  USD = "US Dollar",
+  EUR = "Euro",
+  XRP = "Ripple",
+  BCH = "Bitcoin Cash",
+  LTC = "Litecoin",
+  DOGE = "Dogecoin",
+  GVT = "Genesis Vision Token"
+}
