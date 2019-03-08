0516ef689cd7ce4a3eb50615e3e5ef58efad5e30
pavelzubov <pasha224@yandex.ru>
2019-03-08 11:36:35 +0300

    Revert NOT_FOUND_PAGE_ROUTE to NotFoundPage

--- packages/shared/components/not-found/not-found.routes.tsx
+++ packages/shared/components/not-found/not-found.routes.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import Page from "shared/components/page/page";
 
+export const NOT_FOUND_PAGE_ROUTE = "/404";
+
 const NotFoundPage: React.FC<InjectedTranslateProps> = ({ t }) => (
   <Page title={t("not-found-page.title")}>
     <div className="app__main-wrapper">{t("not-found-page.body")}</div>
