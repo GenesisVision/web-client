0543256b25205139ae407c6e42274f260f9e1188
pavelzubov <pasha224@yandex.ru>
2019-03-10 12:12:43 +0300

    Convert table selector to tsx

---
+++ packages/shared/components/table/helpers/table.selector.ts
@@ -0,0 +1,30 @@
+import { createSelector, ParametricSelector } from "reselect";
+
+const defaultData = {
+  items: [],
+  total: 0
+};
+
+export type table = {
+  itemsData: { data: any };
+};
+
+export const tableSelectorCreator = <S, P, T>(
+  selector: ParametricSelector<S, P, table>[],
+  itemName: string
+) =>
+  createSelector(
+    selector,
+    table => {
+      const data = table.itemsData.data
+        ? {
+            total: table.itemsData.data.total,
+            items: table.itemsData.data[itemName]
+          }
+        : defaultData;
+      return {
+        ...table,
+        itemsData: { ...table.itemsData, data }
+      };
+    }
+  );
