8fa424ce2af4d51ff7fc852a0da1dd6ccf2ba110
a.popova <bob.m@mail.ru>
2019-03-11 13:34:53 +0300

    GV-678 updated service of changing password trading account and added type interface

--- packages/manager/src/modules/change-password-trading-account/services/change-password-trading-account.service.ts
+++ packages/manager/src/modules/change-password-trading-account/services/change-password-trading-account.service.ts
@@ -1,16 +1,20 @@
+import { ProgramPwdUpdate } from "gv-api-web";
 import { Dispatch } from "redux";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 import managerApi from "shared/services/api-client/manager-api";
 import authService from "shared/services/auth-service";
-import { ProgramPwdUpdate } from "gv-api-web";
-import { ResponseError } from "shared/utils/types";
 
-export const changePasswordTradingAccount = (
-  id: string,
+export interface IchangePasswordTradingAccount {
+  id: string;
   opts?: {
     model?: ProgramPwdUpdate;
-  }
-): any => (dispatch: Dispatch) => {
+  };
+}
+
+export const changePasswordTradingAccount = ({
+  id,
+  opts
+}: IchangePasswordTradingAccount): any => (dispatch: Dispatch) => {
   const authorization = authService.getAuthArg();
   return managerApi
     .v10ManagerProgramsByIdPasswordChangePost(id, authorization, opts)
@@ -22,8 +26,5 @@ export const changePasswordTradingAccount = (
         )
       );
       return;
-    })
-    .catch((error: ResponseError) => {
-      dispatch(alertMessageActions.error(error.errorMessage, true));
     });
 };
