a2df74ddd7c7496c150a1d07cf6aaa82ed5b0cff
pavelzubov <pasha224@yandex.ru>
2019-03-11 18:49:21 +0300

    Convert SortingFilter to tsx

---
+++ packages/shared/components/table/components/sorting/sorting-filter/sorting-filter.tsx
@@ -0,0 +1,76 @@
+import "./sorting-filter.scss";
+
+import { GVButton } from "gv-react-components";
+import * as React from "react";
+
+import {
+  SORTING_DIRECTION,
+  getSortingColumnName,
+  getSortingDirection
+} from "../../../helpers/sorting.helpers";
+import SelectFilter from "../../filtering/select-filter/select-filter";
+import {
+  FilterValue,
+  SortingColumn,
+  TFilter
+} from "../../filtering/filter.type";
+
+interface ISortingFilterProps {
+  sorting: string;
+  columns: SortingColumn[];
+  renderValueText(value: SortingColumn): string;
+  onChange?(): void;
+  updateSorting?(value: string): void;
+}
+
+class SortingFilter extends React.Component<ISortingFilterProps> {
+  composeSortingColumnValues = (): FilterValue[] =>
+    this.props.columns
+      .filter(x => x.sortingName)
+      .map(x => ({
+        value: x.sortingName,
+        label: this.props.renderValueText(x)
+      }));
+
+  composeSortingColumnName = (): string =>
+    getSortingColumnName(this.props.sorting);
+
+  isAsc = (): boolean =>
+    getSortingDirection(this.props.sorting) === SORTING_DIRECTION.ASC;
+
+  handleOnSelectChange = ({ value }: TFilter<any>): void =>
+    this.props.updateSorting(
+      value + (this.isAsc() ? SORTING_DIRECTION.ASC : SORTING_DIRECTION.DESC)
+    );
+
+  handleOnDirectionChange = (isAsc: boolean) => (): void =>
+    this.props.updateSorting(
+      this.composeSortingColumnName() +
+        (isAsc ? SORTING_DIRECTION.ASC : SORTING_DIRECTION.DESC)
+    );
+
+  render() {
+    const columnValues = this.composeSortingColumnValues();
+    return (
+      <span className="sorting-filter">
+        <SelectFilter
+          name="sorting"
+          label="Order By"
+          value={this.composeSortingColumnName()}
+          values={columnValues}
+          onChange={this.handleOnSelectChange}
+        />
+        <GVButton
+          variant="text"
+          color="secondary"
+          className="sorting-filter__btn"
+          onClick={this.handleOnDirectionChange(!this.isAsc())}
+        >
+          {this.isAsc() ? <span>&uarr;</span> : <span>&darr;</span>}
+        </GVButton>
+      </span>
+    );
+  }
+}
+
+export default SortingFilter;
