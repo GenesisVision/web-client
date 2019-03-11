65d76389c7b3610e2b51f55647d9ba1da7e80f5e
pavelzubov <pasha224@yandex.ru>
2019-03-11 15:46:46 +0300

    Add IInvestorEventFilterValue and IManagerEventFilterValue

--- packages/shared/components/table/components/filtering/filter.type.ts
+++ packages/shared/components/table/components/filtering/filter.type.ts
@@ -2,3 +2,14 @@ export type TFilter<T> = {
   name: string;
   value: T;
 };
+
+export interface IInvestorEventFilterValue<T> {
+  value: T;
+  label: T;
+}
+
+export interface IManagerEventFilterValue<T> {
+  value: T;
+  labelKey: string;
+  label?: string;
+}
