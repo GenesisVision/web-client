a6a37126e0c8665da0a862d25316ff363b5d7d19
a.popova <bob.m@mail.ru>
2019-03-11 19:31:01 +0300

    GV-678 updated the component form of changing trading account password

--- packages/manager/src/modules/change-password-trading-account/components/change-password-trading-account-form.tsx
+++ packages/manager/src/modules/change-password-trading-account/components/change-password-trading-account-form.tsx
@@ -1,6 +1,5 @@
 import { FormikProps, withFormik } from "formik";
-import { GVButton } from "gv-react-components";
-import SignalsFeeFormPartial from "pages/create-program/components/create-program-settings/signals-fee-form.partial";
+import { GVButton, GVFormikField, GVTextField } from "gv-react-components";
 import React, { ComponentType, FunctionComponent } from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
@@ -23,9 +22,9 @@ enum FORM_FIELDS {
 }
 
 export interface IChangePasswordTradingAccountFormValues {
-  [FORM_FIELDS.password]?: string;
-  [FORM_FIELDS.confirmPassword]?: string;
-  [FORM_FIELDS.twoFactorCode]?: string;
+  [FORM_FIELDS.password]: string;
+  [FORM_FIELDS.confirmPassword]: string;
+  [FORM_FIELDS.twoFactorCode]: string;
 }
 
 type ChangePasswordTradingAccountFormProps = InjectedTranslateProps &
@@ -34,22 +33,41 @@ type ChangePasswordTradingAccountFormProps = InjectedTranslateProps &
 
 const ChangePasswordTradingAccountForm: FunctionComponent<
   ChangePasswordTradingAccountFormProps
-> = ({ t, dirty, handleSubmit, errorMessage }) => {
+> = ({ t, dirty, isValid, handleSubmit, errorMessage, twoFactorEnabled }) => {
   return (
     <form id="change-password-trading-account" onSubmit={handleSubmit}>
       <div className="dialog__top">
         <div className="dialog__header">
-          <h2>{t("program-details-page.description.signal-provider.title")}</h2>
+          <h2>{t("password-change-trading-account.title")}</h2>
         </div>
       </div>
       <div className="dialog__bottom">
+        <GVFormikField
+          component={GVTextField}
+          label={t("password-change-trading-account.new-password")}
+          type="password"
+          name="password"
+          autoComplete="off"
+        />
+        <GVFormikField
+          component={GVTextField}
+          label={t("password-change-trading-account.confirm-password")}
+          type="password"
+          name="confirmPassword"
+          autoComplete="off"
+        />
+        {twoFactorEnabled && (
+          <GVFormikField
+            type="text"
+            name="twoFactorCode"
+            label={t("wallet-withdraw.two-factor-code-label")}
+            autoComplete="off"
+            component={GVTextField}
+          />
+        )}
         <div className="form-error">{errorMessage}</div>
         <div className="dialog__buttons">
-          <GVButton
-            type="submit"
-            id="programMakeSignalSubmit"
-            disabled={!dirty}
-          >
+          <GVButton type="submit" disabled={!isValid || !dirty}>
             {t("buttons.confirm")}
           </GVButton>
         </div>
@@ -66,11 +84,11 @@ export default compose<
     IChangePasswordTradingAccountFormOwnProps,
     IChangePasswordTradingAccountFormValues
   >({
-    displayName: "make-signal-form",
+    displayName: "change-password-trading-account",
     mapPropsToValues: () => ({
-      [FORM_FIELDS.password]: undefined,
-      [FORM_FIELDS.confirmPassword]: undefined,
-      [FORM_FIELDS.twoFactorCode]: undefined
+      [FORM_FIELDS.password]: "",
+      [FORM_FIELDS.confirmPassword]: "",
+      [FORM_FIELDS.twoFactorCode]: ""
     }),
     validationSchema: ChangePasswordTradingAccountValidationSchema,
     handleSubmit: (values, { props, setSubmitting }) => {
