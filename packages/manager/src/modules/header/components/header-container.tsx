d1e5d60e9035e5652e565408f50c48f219de5f81
Евгений Гребенщиков <evgreb@gmail.com>
2019-03-11 14:05:34 +0300

    add ts to notifications -M

--- packages/manager/src/modules/header/components/header-container.tsx
+++ packages/manager/src/modules/header/components/header-container.tsx
@@ -5,8 +5,8 @@ import { fetchTwoFactor } from "shared/actions/2fa-actions";
 import { GLOBAL_SEARCH_ROUTE } from "shared/components/global-search/global-search.routes";
 import { fetchProfileHeaderInfo } from "shared/components/header/actions/header-actions";
 import Header from "shared/components/header/header";
+import { notificationsToggle } from "shared/components/notifications/actions/notifications.actions";
 
-import { notificationsToggle } from "../../../pages/app/components/notifications/actions/notifications.actions";
 import { LOGIN_ROUTE } from "../../../pages/auth/login/login.routes";
 import { logout } from "../../../pages/auth/login/services/login.service";
 import { SIGNUP_ROUTE } from "../../../pages/auth/signup/signup.routes";
