c86ac8eb9abfa72f317127efcb1eb3cf22641003
a.popova <bob.m@mail.ru>
2019-03-13 16:26:12 +0300

    GV-694 added icons program in the notifications

--- packages/shared/components/notifications/components/notification/notification.tsx
+++ packages/shared/components/notifications/components/notification/notification.tsx
@@ -1,10 +1,44 @@
 import classnames from "classnames";
 import { NotificationViewModel } from "gv-api-web";
-import moment from "moment";
-import React from "react";
+import * as moment from "moment";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
+import NewsIcon from "shared/media/news.svg";
+import RedUserIcon from "shared/media/red-user.svg";
+import { composeProgramDetailsUrl } from "shared/utils/compose-url";
 
-const Notification: React.FunctionComponent<NotificationViewModel> = props => {
-  const { date, text, isUnread, type } = props;
+enum TYPE {
+  PROFILE = "profile",
+  PLATFORM = "platform"
+}
+
+const getStaticIconUrl = type => {
+  return type.indexOf(TYPE.PROFILE) !== -1
+    ? RedUserIcon
+    : type.indexOf(TYPE.PLATFORM) !== -1
+    ? NewsIcon
+    : null;
+};
+
+interface INotificationProps {
+  closeNotifications(): void;
+}
+
+const Notification: React.FunctionComponent<
+  NotificationViewModel & INotificationProps
+> = props => {
+  const {
+    date,
+    text,
+    isUnread,
+    type,
+    logo,
+    url,
+    color,
+    closeNotifications
+  } = props;
+  const staticIconUrl = getStaticIconUrl(type.toLowerCase());
   return (
     <div
       className={classnames(
@@ -15,6 +49,32 @@ const Notification: React.FunctionComponent<NotificationViewModel> = props => {
         }
       )}
     >
+      {staticIconUrl ? (
+        <div className="notification__icon">
+          <img
+            src={staticIconUrl}
+            alt={type}
+            className="notification__icon-logo"
+          />
+        </div>
+      ) : (
+        <Link
+          to={{
+            pathname: composeProgramDetailsUrl(url),
+            state: `/ ${type}`
+          }}
+          onClick={closeNotifications}
+          className="notification__icon"
+        >
+          <AssetAvatar
+            url={logo}
+            alt={type}
+            className="notification__icon-logo"
+            color={color}
+          />
+        </Link>
+      )}
+
       <div className="notification__content">
         <div className="notification__description">{text}</div>
         <div className="notification__date">{moment(date).format("LT")}</div>
