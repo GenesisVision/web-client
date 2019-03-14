4e6d92451dee2dcdd11b2bc01d5d3ea14f8ad5c8
a.popova <bob.m@mail.ru>
2019-03-14 21:30:55 +0300

    GV-681 added the dialog loader in the wallet add funds popup and fixed some typescripts errors

--- packages/shared/modules/wallet-add-funds/components/wallet-add-funds-container.tsx
+++ packages/shared/modules/wallet-add-funds/components/wallet-add-funds-container.tsx
@@ -5,30 +5,40 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 import RootState from "shared/reducers/root-reducer";
-import { IDispatchable } from "shared/utils/types";
 
 import WalletAddFundsForm from "./wallet-add-funds-form.js";
+import { DialogLoader } from "shared/components/dialog/dialog-loader/dialog-loader";
+import { Dispatch } from "redux";
+import { ActionType } from "shared/utils/types";
 
 export interface CurrentWallet {
   currency: string;
   available: number;
 }
 
-interface IWalletAddFundsContainerProps {
+interface IWalletAddFundsContainerOwnProps {
   currentWallet: CurrentWallet;
+}
+
+interface IWalletAddFundsContainerStateProps {
   wallets: WalletData[];
 }
 
 interface IWalletAddFundsContainerDispatchProps {
-  notifySuccess(x: string): IDispatchable<void>;
-  notifyError(x: string): IDispatchable<void>;
+  notifySuccess(x: string): void;
+  notifyError(x: string): void;
 }
 
+type IWalletAddFundsContainerProps = IWalletAddFundsContainerOwnProps &
+  IWalletAddFundsContainerStateProps &
+  IWalletAddFundsContainerDispatchProps;
+
 class WalletAddFundsContainer extends React.Component<
-  IWalletAddFundsContainerProps & IWalletAddFundsContainerDispatchProps
+  IWalletAddFundsContainerProps
 > {
   render() {
     const { currentWallet, notifySuccess, notifyError, wallets } = this.props;
+    if (!wallets.length) return <DialogLoader />;
     const enabledWallets = wallets.filter(wallet => wallet.isDepositEnabled);
     return (
       <WalletAddFundsForm
@@ -41,12 +51,16 @@ class WalletAddFundsContainer extends React.Component<
   }
 }
 
-const mapStateToProps = (state: RootState) => {
-  if (!state.wallet.info.data) return;
-  return { wallets: state.wallet.info.data.wallets };
+const mapStateToProps = (
+  state: RootState
+): IWalletAddFundsContainerStateProps => {
+  const wallets = state.wallet.info.data ? state.wallet.info.data.wallets : [];
+  return { wallets };
 };
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (
+  dispatch: Dispatch<ActionType>
+): IWalletAddFundsContainerDispatchProps => ({
   notifySuccess: text => dispatch(alertMessageActions.success(text)),
   notifyError: text => dispatch(alertMessageActions.error(text))
 });
