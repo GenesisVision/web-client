ba2922e225fb716cf68f8fcccd8e3f0aa453ab6d
pavelzubov <pasha224@yandex.ru>
2019-03-19 11:11:17 +0300

    Convert ManagerPrograms to tsx

--- packages/shared/components/manager/manager-history/manager-programs-table.tsx
+++ packages/shared/components/manager/manager-history/manager-programs-table.tsx
@@ -1,28 +1,37 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-history/trades.scss";
 
-import * as PropTypes from "prop-types";
-import React, { Component } from "react";
-import { translate } from "react-i18next";
+import * as React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
 import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
 import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
 import { toggleFavoriteProgram } from "shared/modules/favorite-asset/services/favorite-program.service";
 
-import ProgramTableModule from "../../../modules/programs-table/components/programs-table/programs-table-module";
+import ProgramTableModule from "shared/modules/programs-table/components/programs-table/programs-table-module";
 import {
   MANAGER_DEFAULT_FILTERS,
   MANAGER_FILTERING,
   MANAGER_SORTING
 } from "../manager.constants";
 import { fetchManagerPrograms } from "../services/manager.service";
+import { FilteringType } from "shared/components/table/components/filtering/filter.type";
+import { FundDetails } from "gv-api-web";
+import { IUpdateRowFunc } from "shared/components/table/components/table.types";
+import { IDataModel } from "shared/constants/constants";
 
-class ManagerPrograms extends Component {
-  fetchManagerPrograms = filters => {
+interface Props {
+  managerId: string;
+  title: string;
+  isAuthenticated: boolean;
+}
+
+class ManagerPrograms extends React.Component<Props & InjectedTranslateProps> {
+  fetchManagerPrograms = (filters: FilteringType): Promise<IDataModel> => {
     const { managerId } = this.props;
     return fetchManagerPrograms({ ...filters, managerId });
   };
 
-  toggleFavorite = (program, updateRow) => () => {
+  toggleFavorite = (program: FundDetails, updateRow: IUpdateRowFunc) => () => {
     const isFavorite = program.personalDetails.isFavorite;
     const newProgram = {
       ...program,
@@ -59,9 +68,5 @@ class ManagerPrograms extends Component {
     );
   }
 }
-
-ManagerPrograms.propTypes = {
-  managerId: PropTypes.string.isRequired
-};
 
 export default translate()(ManagerPrograms);
