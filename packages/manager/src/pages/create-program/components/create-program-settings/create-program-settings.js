19b57b3d69eed43e1c7627a43d9932ecb78c2d05
nkrivous <kolsln@list.ru>
2019-03-19 12:54:03 +0300

    Trim program description

--- packages/manager/src/pages/create-program/components/create-program-settings/create-program-settings.js
+++ packages/manager/src/pages/create-program/components/create-program-settings/create-program-settings.js
@@ -111,6 +111,7 @@ class CreateProgramSettings extends React.Component {
       title,
       currency
     } = values;
+    const descriptionTrimmedLength = description.trim().length;
     const imageInputError =
       errors &&
       errors.logo &&
@@ -202,13 +203,13 @@ class CreateProgramSettings extends React.Component {
                       "manager.create-program-page.settings.fields.description-requirements"
                     )}
                   </span>
-                  {description.length > 0 && (
+                  {descriptionTrimmedLength > 0 && (
                     <span className="create-program-settings__description-chars">
-                      {description.length}
+                      {descriptionTrimmedLength}
                       <GVProgramPeriod
                         start={0}
                         end={500}
-                        value={description.length}
+                        value={descriptionTrimmedLength}
                       />
                     </span>
                   )}
