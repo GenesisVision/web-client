48084703fd648ec0d7a33968aeb545a18fbed5c9
pavelzubov <pasha224@yandex.ru>
2019-03-20 18:13:38 +0300

    Convert program favorites reducer to tsx

---
+++ packages/shared/modules/programs-table/reducers/programs-favorites.reducer.ts
@@ -0,0 +1,61 @@
+import { SET_FAVORITE_PROGRAM } from "shared/modules/favorite-asset/actions/favorite-program.actions";
+import {
+  FAILURE_SUFFIX,
+  IApiState,
+  REQUEST_SUFFIX
+} from "shared/reducers/api-reducer/api-reducer";
+import { ProgramsList } from "gv-api-web";
+
+const updateFavoriteLocal = (
+  state: IApiState<ProgramsList>,
+  programId: string,
+  isFavorite: boolean
+): IApiState<ProgramsList> => {
+  return {
+    ...state,
+    data: {
+      ...state.data,
+      total: (state.data && state.data.programs.length) || 0,
+      programs:
+        (state.data &&
+          state.data.programs.map(program => {
+            if (program.id === programId) {
+              return {
+                ...program,
+                personalDetails: {
+                  ...program.personalDetails,
+                  isFavorite: isFavorite
+                }
+              };
+            }
+            return program;
+          })) ||
+        []
+    }
+  };
+};
+
+const favoritesReducer = (
+  state: IApiState<ProgramsList>,
+  action: any
+): IApiState<ProgramsList> => {
+  switch (action.type) {
+    case `${SET_FAVORITE_PROGRAM}_${REQUEST_SUFFIX}`:
+      return updateFavoriteLocal(
+        state,
+        action.meta.programId,
+        action.meta.isFavorite
+      );
+    case `${SET_FAVORITE_PROGRAM}_${FAILURE_SUFFIX}`: {
+      return updateFavoriteLocal(
+        state,
+        action.meta.programId,
+        !action.meta.isFavorite
+      );
+    }
+    default:
+      return state;
+  }
+};
+
+export default favoritesReducer;
