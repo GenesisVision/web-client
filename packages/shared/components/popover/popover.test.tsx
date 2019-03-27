0631243dd330c225eb05db51821f853639d99418
pavelzubov <pasha224@yandex.ru>
2019-03-27 13:14:57 +0300

    Add test to popover

---
+++ packages/shared/components/popover/popover.test.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { _Popover as Popover } from "./popover";
+import { shallow } from "enzyme";
+
+const modalRoot = document.createElement("div");
+modalRoot.setAttribute("id", "modal-root");
+document.body.append(modalRoot);
+
+describe("Popover tests", () => {
+  test("should render popover", () => {
+    const popover = shallow(<Popover scrollTop={0} anchorEl={() => {}} />);
+    expect(popover.find(".popover")).toHaveLength(1);
+  });
+});
