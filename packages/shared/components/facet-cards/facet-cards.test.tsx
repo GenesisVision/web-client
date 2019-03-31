42f408e9b93182676ccf9539f007af9c662c81f9
pavelzubov <pasha224@yandex.ru>
2019-03-31 20:31:09 +0300

    Add tests to facet cards

--- packages/shared/components/facet-cards/facet-cards.test.tsx
+++ packages/shared/components/facet-cards/facet-cards.test.tsx
@@ -2,25 +2,88 @@ import * as React from "react";
 import { shallow } from "enzyme";
 import FacetCard from "./facet-card";
 import { Facet } from "gv-api-web";
+import FacetCards from "./facet-cards";
+import {
+  _FacetCardsContainer as FacetCardsContainer,
+  ASSETS_FACETS
+} from "./faset-cards-container";
+import FacetCardsStub from "./facet-cards-stub";
 
 describe("Facet Cards tests", () => {
+  const facet: Facet = {
+    description: "",
+    id: "",
+    logo: "",
+    sortType: "New",
+    timeframe: "AllTime",
+    title: "",
+    url: ""
+  };
+  const title = "title";
+  const mockFunc = jest.fn();
   describe("FacetCard tests", () => {
-    const title = "title";
-    const facet: Facet = {
-      description: "",
-      id: "",
-      logo: "",
-      sortType: "New",
-      timeframe: "AllTime",
-      title: "",
-      url: ""
-    };
     test("should render FacetCard", () => {
-      const handleClick = jest.fn();
       const component = shallow(
-        <FacetCard title={title} facet={facet} composeFacetUrl={handleClick} />
+        <FacetCard title={title} facet={facet} composeFacetUrl={mockFunc} />
       );
       expect(component.find(".facet")).toHaveLength(1);
+    });
+    test("should set --hovered on mouseEnter", () => {
+      const component = shallow(
+        <FacetCard title={title} facet={facet} composeFacetUrl={mockFunc} />
+      );
+      component.simulate("mouseEnter");
+      expect(component.find(".facet--hovered")).toHaveLength(1);
+    });
+  });
+  describe("FacetCards tests", () => {
+    const facets: Facet[] = [facet];
+    test("should render FacetCard", () => {
+      const component = shallow(
+        <FacetCards title={title} facets={facets} composeFacetUrl={mockFunc} />
+      );
+      expect(component.find(".facets")).toHaveLength(1);
+    });
+  });
+  describe("FacetCardsContainer tests", () => {
+    test("should render FacetCards if facets is not empty", () => {
+      const facets: Facet[] = [facet];
+      const component = shallow(
+        <FacetCardsContainer
+          assetsFacets={ASSETS_FACETS.FUNDS}
+          title={title}
+          facets={facets}
+          composeFacetUrl={mockFunc}
+          isPending={false}
+        />
+      );
+      expect(component.find(FacetCards)).toHaveLength(1);
+    });
+    test("should render FacetCardsStub if facets is empty", () => {
+      const facets: Facet[] = [];
+      const component = shallow(
+        <FacetCardsContainer
+          assetsFacets={ASSETS_FACETS.FUNDS}
+          title={title}
+          facets={facets}
+          composeFacetUrl={mockFunc}
+          isPending={false}
+        />
+      );
+      expect(component.find(FacetCardsStub)).toHaveLength(1);
+    });
+    test("should render FacetCardsStub if isPending", () => {
+      const facets: Facet[] = [facet];
+      const component = shallow(
+        <FacetCardsContainer
+          assetsFacets={ASSETS_FACETS.FUNDS}
+          title={title}
+          facets={facets}
+          composeFacetUrl={mockFunc}
+          isPending={true}
+        />
+      );
+      expect(component.find(FacetCardsStub)).toHaveLength(1);
     });
   });
 });
