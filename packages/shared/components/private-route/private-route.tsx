3d1df7b483881f3edd144339a61a068fd283ac12
Евгений Гребенщиков <evgreb@gmail.com>
2019-04-02 18:48:45 +0300

    add tests for private-route

--- packages/shared/components/private-route/private-route.tsx
+++ packages/shared/components/private-route/private-route.tsx
@@ -1,12 +1,14 @@
-import { LOGIN_ROUTE } from "pages/auth/login/login.routes";
-import React, { Component } from "react";
+import * as React from "react";
 import { connect } from "react-redux";
+import { RouteComponentProps, RouteProps } from "react-router";
 import { Redirect, Route } from "react-router-dom";
+import { LOGIN_ROUTE } from "shared/pages/login.routes";
+import RootState from "shared/reducers/root-reducer";
 
-class Private extends Component {
-  renderComponent = props => {
+class _PrivateRoute extends React.Component<RouteProps & StateProps> {
+  renderComponent = (props: RouteComponentProps) => {
     const { component: Component, isAuthenticated } = this.props;
-    return isAuthenticated ? (
+    return isAuthenticated && Component ? (
       <Component {...props} />
     ) : (
       <Redirect
@@ -24,12 +26,16 @@ class Private extends Component {
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state: RootState): StateProps => {
   const { isAuthenticated } = state.authData;
   return {
     isAuthenticated
   };
 };
 
-const PrivateRoute = connect(mapStateToProps)(Private);
+const PrivateRoute = connect(mapStateToProps)(_PrivateRoute);
 export default PrivateRoute;
+
+interface StateProps {
+  isAuthenticated?: boolean;
+}
