0f61816d5ba31933d556e6ac27ab6891a428aadd
pavelzubov <pasha224@yandex.ru>
2019-04-04 12:55:04 +0300

    Remove unused actions, reducers and service from login -I

--- packages/investor/src/pages/auth/login/actions/login.actions.js
+++
@@ -1,9 +0,0 @@
-import authApi from "shared/services/api-client/auth-api";
-import { LOGIN } from "shared/components/auth/login/login.actions";
-
-export const loginUser = loginData => ({
-  type: LOGIN,
-  payload: authApi.v10AuthSigninInvestorPost({
-    model: loginData
-  })
-});
