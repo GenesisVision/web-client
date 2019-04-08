ee39a265bbd996045e27d554fa82473e0491bce7
pavelzubov <pasha224@yandex.ru>
2019-04-08 13:03:40 +0300

    Rename CaptchaContainer component, remove requestFunc and correct types

--- packages/investor/src/pages/auth/login/login/login.page.tsx
+++ packages/investor/src/pages/auth/login/login/login.page.tsx
@@ -1,19 +1,14 @@
 import "shared/components/auth/login/login/login.scss";
 
 import * as React from "react";
-import LoginFormContainer from "shared/components/auth/login/login/login-form-container";
-
 import AuthTabs from "shared/components/auth/components/auth-tabs/auth-tabs";
 import { FORGOT_PASSWORD_ROUTE } from "../../forgot-password/forgot-password.routes";
 import { ROLE } from "shared/constants/constants";
 import { HOME_ROUTE } from "shared/routes/app.routes";
 import { LOGIN_ROUTE } from "shared/components/auth/login/login.routes";
 import { LocationState } from "history";
-import LoginContainer from "shared/components/auth/login/login-container";
-import {
-  login,
-  SetSubmittingFuncType
-} from "shared/components/auth/login/login.service";
+import CaptchaContainer from "shared/components/auth/login/captcha-container";
+import { SetSubmittingFuncType } from "shared/components/auth/login/login.service";
 import LoginForm from "shared/components/auth/login/login/login-form";
 
 const LoginPage: React.FC<Props> = ({ location }) => {
@@ -21,31 +16,25 @@ const LoginPage: React.FC<Props> = ({ location }) => {
   return (
     <div className="login">
       <AuthTabs authPartUrl={LOGIN_ROUTE} role={ROLE.INVESTOR} />
-      <LoginContainer
+      <CaptchaContainer
         from={from}
         FORGOT_PASSWORD_ROUTE={FORGOT_PASSWORD_ROUTE}
         role={ROLE.INVESTOR}
-        requestFunc={login}
         renderForm={(
           handle: (
             loginFormData: Object,
             setSubmitting: SetSubmittingFuncType
           ) => void,
           errorMessage: string,
-          FORGOT_PASSWORD_ROUTE: string
+          FORGOT_PASSWORD_ROUTE?: string
         ) => (
           <LoginForm
             onSubmit={handle}
             error={errorMessage}
-            FORGOT_PASSWORD_ROUTE={FORGOT_PASSWORD_ROUTE}
+            FORGOT_PASSWORD_ROUTE={FORGOT_PASSWORD_ROUTE!}
           />
         )}
       />
-      {/*<LoginFormContainer
-        from={from}
-        FORGOT_PASSWORD_ROUTE={FORGOT_PASSWORD_ROUTE}
-        role={ROLE.INVESTOR}
-      />*/}
     </div>
   );
 };
