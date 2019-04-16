135546a5000566f7ecd0f763e6f4bf5e3984ba02
pavelzubov <pasha224@yandex.ru>
2019-04-16 11:20:49 +0300

    Convert ProgramBalanceChartSection to tsx

---
+++ packages/shared/components/programs/program-details/program-details-statistic-section/program-details-chart-section/program-balance-chart-section/program-balance-chart-section.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import NumberFormat from "react-number-format";
+import ChartPeriod from "shared/components/chart/chart-period/chart-period";
+import StatisticItem from "shared/components/statistic-item/statistic-item";
+import { formatValue } from "shared/utils/formatter";
+
+import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
+import { HandlePeriodChangeType } from "../../program-details-statistic-section";
+import { ProgramBalanceChart as ProgramBalanceChartType } from "gv-api-web";
+import ProgramBalanceChart from "./program-balance-chart";
+import { CURRENCIES } from "shared/modules/currency-select/currency-select.constants";
+
+const ProgramBalanceChartSection: React.FC<Props> = ({
+  balanceChart,
+  period,
+  onPeriodChange
+}) => (
+  <>
+    <div className="details-chart__value">
+      <StatisticItem
+        label={"Value"}
+        equivalent={balanceChart.programCurrencyBalance}
+        equivalentCurrency={balanceChart.programCurrency}
+        big
+        accent
+      >
+        <NumberFormat
+          value={formatValue(balanceChart.gvtBalance)}
+          thousandSeparator={" "}
+          displayType="text"
+          suffix={" GVT"}
+        />
+      </StatisticItem>
+    </div>
+    <ChartPeriod onChange={onPeriodChange} period={period} />
+    <div className="details-chart__profit">
+      <ProgramBalanceChart
+        balanceChart={balanceChart.balanceChart}
+        currency={balanceChart.programCurrency as CURRENCIES}
+      />
+    </div>
+  </>
+);
+
+interface Props {
+  balanceChart: ProgramBalanceChartType;
+  period: ChartDefaultPeriod;
+  onPeriodChange: HandlePeriodChangeType;
+}
+
+export default React.memo(ProgramBalanceChartSection);
