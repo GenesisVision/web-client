908147ce6c3b9ede34b074261a6fafed9cc2043d
pavelzubov <pasha224@yandex.ru>
2019-04-17 10:53:38 +0300

    Move HandlePeriodChangeType to shared types

--- packages/shared/utils/types.ts
+++ packages/shared/utils/types.ts
@@ -4,6 +4,7 @@ import { ManagerRootState } from "manager-web-portal/src/reducers";
 import { Action, AnyAction, Dispatch } from "redux";
 import { ThunkAction, ThunkDispatch } from "redux-thunk";
 
+import { ChartDefaultPeriod } from "../components/chart/chart-period/chart-period.helpers";
 import RootState from "../reducers/root-reducer";
 
 export interface IDispatchable<T> {
@@ -54,3 +55,4 @@ export type ResponseError = {
 };
 
 export type SetSubmittingType = (isSubmitting: boolean) => void;
+export type HandlePeriodChangeType = (period: ChartDefaultPeriod) => void;
