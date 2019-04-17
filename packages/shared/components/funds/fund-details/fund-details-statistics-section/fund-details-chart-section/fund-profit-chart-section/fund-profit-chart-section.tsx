079d0f5e601e88c1a03562c68ea51eae6c43a4db
pavelzubov <pasha224@yandex.ru>
2019-04-17 10:38:45 +0300

    Convert FundProfitChartSection to tsx

---
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-chart-section.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import NumberFormat from "react-number-format";
+import ChartPeriod from "shared/components/chart/chart-period/chart-period";
+import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
+import StatisticItem from "shared/components/statistic-item/statistic-item";
+import { formatCurrencyValue } from "shared/utils/formatter";
+import { HandlePeriodChangeType } from "shared/utils/types";
+
+import { FundDetailsProfitChart } from "../../../services/fund-details.types";
+import FundProfitChart from "./fund-profit-chart";
+
+const FundProfitChartSection: React.FC<Props> = ({
+  profitChart,
+  period,
+  onPeriodChange
+}) => (
+  <>
+    <div className="details-chart__value">
+      <StatisticItem
+        label={"Value"}
+        equivalent={formatCurrencyValue(profitChart.timeFrameUsdProfit, "USD")}
+        equivalentCurrency="USD"
+        big
+        accent
+      >
+        <NumberFormat
+          value={formatCurrencyValue(profitChart.timeFrameGvtProfit, "GVT")}
+          thousandSeparator={" "}
+          displayType="text"
+          suffix={" GVT"}
+        />
+      </StatisticItem>
+    </div>
+    <ChartPeriod onChange={onPeriodChange} period={period} />
+    <div className="details-chart__profit">
+      <FundProfitChart equityChart={profitChart.equityChart} />
+    </div>
+  </>
+);
+
+interface Props {
+  profitChart: FundDetailsProfitChart;
+  period: ChartDefaultPeriod;
+  onPeriodChange: HandlePeriodChangeType;
+}
+
+export default React.memo(FundProfitChartSection);
