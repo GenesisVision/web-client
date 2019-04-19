6b8edd8d1d57353f63c12590c151e2abfe4ce054
pavelzubov <pasha224@yandex.ru>
2019-04-19 14:42:24 +0300

    Convert wallet withdraw service to tsx

--- packages/shared/modules/wallet-withdraw/services/wallet-withdraw.services.tsx
+++ packages/shared/modules/wallet-withdraw/services/wallet-withdraw.services.tsx
@@ -1,12 +1,16 @@
 import { fetchWalletTransactions } from "shared/components/wallet/services/wallet.services";
 import walletApi from "shared/services/api-client/wallet-api";
 import authService from "shared/services/auth-service";
+import { MiddlewareDispatch } from "../../../utils/types";
+import { CancelablePromise, CreateWithdrawalRequestModel } from "gv-api-web";
 
 export const fetchPaymentInfo = () => {
   return walletApi.v10WalletWithdrawInfoGet(authService.getAuthArg());
 };
 
-export const newWithdrawRequest = data => (dispatch, getState) => {
+export const newWithdrawRequest = (data: CreateWithdrawalRequestModel) => (
+  dispatch: MiddlewareDispatch
+): CancelablePromise<any> => {
   return walletApi
     .v10WalletWithdrawRequestNewPost(authService.getAuthArg(), { model: data })
     .then(response => {
