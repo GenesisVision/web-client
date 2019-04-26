e8fb35df2fcd19a548aad2bce557bfa8b4fed8cc
pavelzubov <pasha224@yandex.ru>
2019-04-26 17:06:41 +0300

    Remove console.log

--- packages/shared/components/auth/signup/services/signup-email-pending.service.js
+++ packages/shared/components/auth/signup/services/signup-email-pending.service.js
@@ -1,7 +1,6 @@
 import { resendConfirmationLink } from "../actions/signup.actions";
 
 export const sendConfirmationLink = () => (dispatch, getState) => {
-  console.log(111);
   let { email } = getState().emailPending;
   dispatch(resendConfirmationLink({ email }));
 };
