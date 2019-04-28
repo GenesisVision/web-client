7c134e421fcabe2645cbd911e551448ff9da24dd
pavelzubov <pasha224@yandex.ru>
2019-04-28 07:41:30 +0300

    Add action status filter to dashboard constants

--- packages/shared/components/dashboard/dashboard.constants.ts
+++ packages/shared/components/dashboard/dashboard.constants.ts
@@ -1,13 +1,25 @@
 import {
+  DATE_RANGE_FILTER_NAME,
   DEFAULT_DATE_RANGE_FILTER_VALUE,
   DateRangeFilterTypes
 } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
 import { composeDefaultDateRangeFilter } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.helpers";
 import { SortingColumn } from "shared/components/table/components/filtering/filter.type";
 
+import { FILTER_TYPE } from "../table/helpers/filtering.helpers";
+import {
+  ACTION_STATUS_FILTER_DEFAULT_VALUE,
+  ACTION_STATUS_FILTER_NAME
+} from "./dashboard-assets/dashboard-programs/dashboard-programs.helpers";
+
 export const DASHBOARD_PROGRAMS_SORTING = "ByProfitDesc";
 
 export const DASHBOARD_PROGRAMS_FILTERS = [
+  {
+    type: FILTER_TYPE.GENERAL,
+    name: ACTION_STATUS_FILTER_NAME,
+    defaultValue: ACTION_STATUS_FILTER_DEFAULT_VALUE
+  },
   {
     ...composeDefaultDateRangeFilter({
       defaultValue: {
@@ -22,7 +34,8 @@ export const DASHBOARD_PROGRAMS_DEFAULT_FILTERING = {
   dateRange: {
     ...DEFAULT_DATE_RANGE_FILTER_VALUE,
     type: DateRangeFilterTypes.lastMonth
-  }
+  },
+  [ACTION_STATUS_FILTER_NAME]: ACTION_STATUS_FILTER_DEFAULT_VALUE
 };
 
 export const DASHBOARD_FUNDS_COLUMNS: SortingColumn[] = [
