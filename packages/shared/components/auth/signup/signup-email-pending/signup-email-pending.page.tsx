58c1d9b3c8d222426c5b57dfee6f4d735124b5d4
pavelzubov <pasha224@yandex.ru>
2019-04-29 13:00:05 +0300

    Convert EmailPending to tsx

--- packages/shared/components/auth/signup/signup-email-pending/signup-email-pending.page.tsx
+++ packages/shared/components/auth/signup/signup-email-pending/signup-email-pending.page.tsx
@@ -1,11 +1,11 @@
 import "shared/components/auth/signup/signup-email-pending.scss";
 
-import React from "react";
-import { translate } from "react-i18next";
+import * as React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import { sendConfirmationLink } from "shared/components/auth/signup/services/signup-email-pending.service";
 import SignupEmailPendingContainer from "shared/components/auth/signup/signup-email-pending/signup-email-pending-container";
 
-const EmailPending = ({ t }) => {
+const _EmailPending: React.FC<InjectedTranslateProps> = ({ t }) => {
   return (
     <div className="signup-email">
       <h1>{t("auth.signup.email-confirm-title")}</h1>
@@ -19,4 +19,5 @@ const EmailPending = ({ t }) => {
   );
 };
 
-export default translate()(EmailPending);
+const EmailPending = React.memo(translate()(_EmailPending));
+export default EmailPending;
