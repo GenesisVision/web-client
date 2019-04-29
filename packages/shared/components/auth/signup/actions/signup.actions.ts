6f6ecfc2912743d2c5f80f22851cb2b0a1c0ea0c
pavelzubov <pasha224@yandex.ru>
2019-04-29 12:56:42 +0300

    Convert signUp actions to tsx

--- packages/shared/components/auth/signup/actions/signup.actions.ts
+++ packages/shared/components/auth/signup/actions/signup.actions.ts
@@ -1,5 +1,10 @@
-import authApi from "shared/services/api-client/auth-api";
+import {
+  RegisterInvestorViewModel,
+  RegisterManagerViewModel,
+  ResendConfirmationViewModel
+} from "gv-api-web";
 import { ROLE, ROLE_ENV } from "shared/constants/constants";
+import authApi from "shared/services/api-client/auth-api";
 
 export const SIGN_UP = "SIGN_UP";
 export const RESEND_CONFIRMATION_LINK = "RESEND_CONFIRMATION_LINK";
@@ -9,12 +14,16 @@ const signUpMethod =
     ? authApi.v10AuthSignupInvestorPost
     : authApi.v10AuthSignupManagerPost;
 
-export const signUpUser = model => ({
+export type RegisterViewModel =
+  | RegisterInvestorViewModel
+  | RegisterManagerViewModel;
+
+export const signUpUser = (model: RegisterViewModel) => ({
   type: SIGN_UP,
   payload: signUpMethod({ model })
 });
 
-export const resendConfirmationLink = model => ({
+export const resendConfirmationLink = (model: ResendConfirmationViewModel) => ({
   type: RESEND_CONFIRMATION_LINK,
   payload: authApi.v10AuthResendconfirmationlinkPost({ model })
 });
