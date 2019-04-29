18316e5baf97f66b97a97fcaf96a1ee6c52972af
pavelzubov <pasha224@yandex.ru>
2019-04-29 13:01:25 +0300

    Convert SignUpPage to tsx

--- packages/shared/components/auth/signup/signup.page.tsx
+++ packages/shared/components/auth/signup/signup.page.tsx
@@ -1,19 +1,17 @@
 import "./signup.scss";
 
-import React from "react";
+import * as React from "react";
 import { translate } from "react-i18next";
 import AuthTabs from "shared/components/auth/components/auth-tabs/auth-tabs";
 import SignUpFormContainer from "shared/components/auth/signup/signup-form/signup-form-container";
 import { SIGNUP_ROUTE } from "shared/components/auth/signup/signup.routes";
 
-const SignUpPage = ({ t }) => {
-  return (
-    <div className="signup">
-      <h1>{t("auth.signup.title")}</h1>
-      <AuthTabs authPartUrl={SIGNUP_ROUTE} />
-      <SignUpFormContainer />
-    </div>
-  );
-};
+const _SignUpPage: React.FC = () => (
+  <div className="signup">
+    <AuthTabs authPartUrl={SIGNUP_ROUTE} />
+    <SignUpFormContainer />
+  </div>
+);
 
-export default translate()(SignUpPage);
+const SignUpPage = React.memo(translate()(_SignUpPage));
+export default SignUpPage;
