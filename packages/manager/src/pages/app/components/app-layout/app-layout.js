dcdcaab3e493081010b7b6ea1fe65b3328b38c04
nkrivous <kolsln@list.ru>
2019-04-30 10:47:10 +0300

    Use correct token in the store creation process

--- packages/manager/src/pages/app/components/app-layout/app-layout.js
+++ packages/manager/src/pages/app/components/app-layout/app-layout.js
@@ -3,7 +3,6 @@ import "./app-layout.scss";
 import HeaderContainer from "modules/header/components/header-container";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import authActions from "shared/actions/auth-actions";
 import platformActions from "shared/actions/platform-actions";
 import { initOnResizeEvent } from "shared/actions/ui-actions";
 import NotificationsContainer from "shared/components/notifications/components/notifications-container";
@@ -12,7 +11,6 @@ class AppLayout extends Component {
   componentDidMount() {
     this.props.fetchPlatformSettings();
     this.props.initOnResizeEvent();
-    this.props.updateToken();
   }
 
   render() {
@@ -36,8 +34,7 @@ export default connect(
   dispatch => ({
     fetchPlatformSettings: () =>
       dispatch(platformActions.fetchPlatformSettings),
-    initOnResizeEvent: () => dispatch(initOnResizeEvent()),
-    updateToken: () => dispatch(authActions.updateToken())
+    initOnResizeEvent: () => dispatch(initOnResizeEvent())
   }),
   null,
   { pure: false }
