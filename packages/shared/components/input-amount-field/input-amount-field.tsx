292662c5fdf309f263302c9c6448b2d91063052d
pavelzubov <pasha224@yandex.ru>
2019-05-02 11:36:26 +0300

    Refactoring InputAmountField

--- packages/shared/components/input-amount-field/input-amount-field.tsx
+++ packages/shared/components/input-amount-field/input-amount-field.tsx
@@ -1,11 +1,45 @@
 import { GVButton, GVFormikField } from "gv-react-components";
 import * as React from "react";
-import { TranslationFunction, translate } from "react-i18next";
+import { InjectedTranslateProps, translate } from "react-i18next";
 
 import GVNumberFiled from "../gv-number-field/gv-number-field";
 
-export interface IInputAmountFieldProps {
-  t: TranslationFunction;
+const _InputAmountField: React.FC<Props> = ({
+  onChange,
+  t,
+  name,
+  label,
+  currency,
+  isAllow,
+  setMax,
+  placeholder,
+  autoFocus = true
+}) => (
+  <GVFormikField
+    onChange={onChange}
+    name={name}
+    label={label}
+    placeholder={placeholder}
+    component={GVNumberFiled}
+    adornment={
+      <GVButton
+        onClick={setMax}
+        variant="text"
+        color="secondary"
+        className="gv-btn--no-padding"
+      >
+        {t("Max")}
+      </GVButton>
+    }
+    autoComplete="off"
+    autoFocus={autoFocus}
+    suffix={` ${currency}`}
+    allowNegative={false}
+    isAllowed={isAllow}
+  />
+);
+
+interface Props extends InjectedTranslateProps {
   name: string;
   label: string;
   currency: string;
@@ -16,44 +50,5 @@ export interface IInputAmountFieldProps {
   onChange?(event: string | number): void;
 }
 
-class InputAmountField extends React.Component<IInputAmountFieldProps> {
-  render() {
-    const {
-      onChange,
-      t,
-      name,
-      label,
-      currency,
-      isAllow,
-      setMax,
-      placeholder,
-      autoFocus = true
-    } = this.props;
-    return (
-      <GVFormikField
-        onChange={onChange}
-        name={name}
-        label={label}
-        placeholder={placeholder}
-        component={GVNumberFiled}
-        adornment={
-          <GVButton
-            onClick={setMax}
-            variant="text"
-            color="secondary"
-            className="gv-btn--no-padding"
-          >
-            {t("Max")}
-          </GVButton>
-        }
-        autoComplete="off"
-        autoFocus={autoFocus}
-        suffix={` ${currency}`}
-        allowNegative={false}
-        isAllowed={isAllow}
-      />
-    );
-  }
-}
-
-export default translate()(InputAmountField);
+const InputAmountField = React.memo(translate()(_InputAmountField));
+export default InputAmountField;
