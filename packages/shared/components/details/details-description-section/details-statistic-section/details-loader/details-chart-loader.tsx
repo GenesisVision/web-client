57f0e8a419fdb77c413ad3d0caa420ba40c9e95d
pavelzubov <pasha224@yandex.ru>
2019-05-03 10:37:01 +0300

    Convert DetailsChartLoader to tsx

--- packages/shared/components/details/details-description-section/details-statistic-section/details-loader/details-chart-loader.tsx
+++ packages/shared/components/details/details-description-section/details-statistic-section/details-loader/details-chart-loader.tsx
@@ -1,13 +1,13 @@
 import "./details-chart-loader.scss";
 
-import React from "react";
+import * as React from "react";
 import {
   ChartLoader,
   ChartPeriodLoader
 } from "shared/components/chart/chart-loader/chart-loader";
 import SvgLoader from "shared/components/svg-loader/svg-loader";
 
-const DetailsChartTabsLoader = () => (
+const DetailsChartTabsLoader: React.FC = () => (
   <div className="details-chart-loader__tabs">
     <SvgLoader height="20" width="160">
       <rect x="0" y="0" width="70" height="18" rx="8" ry="8" />
@@ -16,7 +16,7 @@ const DetailsChartTabsLoader = () => (
   </div>
 );
 
-const DetailsChartValueLoader = () => (
+const DetailsChartValueLoader: React.FC = () => (
   <div className="details-chart-loader__value">
     <SvgLoader height="51" width="70">
       <rect x="0" y="0" width="40" height="16" rx="8" ry="8" />
@@ -25,24 +25,22 @@ const DetailsChartValueLoader = () => (
   </div>
 );
 
-const DetailsChartLoader = () => {
-  return (
-    <div className="details-chart-loader">
-      <div className="details-chart-loader__row">
-        <DetailsChartTabsLoader />
-      </div>
-      <div className="details-chart-loader__row">
-        <DetailsChartValueLoader />
-      </div>
-      <div className="details-chart-loader__row">
-        <ChartPeriodLoader className="details-chart-loader__period" />
-        <ChartPeriodLoader className="details-chart-loader__period" />
-      </div>
-      <div className="details-chart-loader__row">
-        <ChartLoader className="details-chart-loader__chart" />
-      </div>
+const DetailsChartLoader: React.FC = () => (
+  <div className="details-chart-loader">
+    <div className="details-chart-loader__row">
+      <DetailsChartTabsLoader />
     </div>
-  );
-};
+    <div className="details-chart-loader__row">
+      <DetailsChartValueLoader />
+    </div>
+    <div className="details-chart-loader__row">
+      <ChartPeriodLoader className="details-chart-loader__period" />
+      <ChartPeriodLoader className="details-chart-loader__period" />
+    </div>
+    <div className="details-chart-loader__row">
+      <ChartLoader className="details-chart-loader__chart" />
+    </div>
+  </div>
+);
 
 export default DetailsChartLoader;
