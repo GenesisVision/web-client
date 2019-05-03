2afd0c726696330e0d9590f72349db762421382c
pavelzubov <pasha224@yandex.ru>
2019-05-03 15:40:59 +0300

    Convert ReallocateForm to tsx

--- packages/manager/src/modules/reallocate/components/reallocate-form.tsx
+++ packages/manager/src/modules/reallocate/components/reallocate-form.tsx
@@ -1,27 +1,33 @@
-import { withFormik } from "formik";
+import { FormikProps, withFormik } from "formik";
+import { FundAssetPartWithIcon } from "gv-api-web";
 import { GVButton } from "gv-react-components";
-import React, { Component } from "react";
-import { translate } from "react-i18next";
+import CreateFundSettingsAddAsset from "pages/create-fund/components/create-fund-settings/create-fund-settings-add-asset/create-fund-settings-add-asset";
+import CreateFundSettingsAssetsComponent from "pages/create-fund/components/create-fund-settings/create-fund-settings-assets-block/create-fund-settings-assets-block";
+import * as React from "react";
+import { ChangeEventHandler, MouseEventHandler } from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
 import ErrorMessage, {
-  OVER
+  MESSAGE_TYPES
 } from "shared/components/error-message/error-message";
 import FormError from "shared/components/form/form-error/form-error";
+import { TFundAssetRemoveHandle } from "shared/components/fund-asset/fund-asset-container";
+import { anchorElType } from "shared/components/popover/popover";
+import { SetSubmittingType } from "shared/utils/types";
 import { array, number, object } from "yup";
 
-import CreateFundSettingsAddAsset from "../../../pages/create-fund/components/create-fund-settings/create-fund-settings-add-asset/create-fund-settings-add-asset";
-import CreateFundSettingsAssetsComponent from "../../../pages/create-fund/components/create-fund-settings/create-fund-settings-assets-block/create-fund-settings-assets-block";
-
-class ReallocateForm extends Component {
+class _ReallocateForm extends React.PureComponent<Props, State> {
   state = {
-    anchor: null,
+    anchor: undefined,
     assets: this.props.assets,
     remainder: 0
   };
   componentDidMount() {
     this.setState({ remainder: this.getRemainder() });
   }
-  handlePercentChange = asset => e => {
+  handlePercentChange = (
+    asset: FundAssetPartWithIcon
+  ): ChangeEventHandler<any> => e => {
     let value = +e.target.value;
     if (isNaN(value)) return;
     if (value > this.getRemainderWithoutChoised(asset))
@@ -29,20 +35,19 @@ class ReallocateForm extends Component {
     asset.percent = value;
     this.updateAssets();
   };
-  handleDown = asset => () => {
+  handleDown = (asset: FundAssetPartWithIcon) => () => {
     if (asset.percent === 0) return;
     asset.percent--;
     this.updateAssets();
   };
-  handleUp = asset => () => {
+  handleUp = (asset: FundAssetPartWithIcon) => () => {
     if (this.state.remainder - 1 < 0) return;
     asset.percent++;
     this.updateAssets();
   };
-  getRemainder = () => {
-    return 100 - this.state.assets.reduce((sum, item) => sum + item.percent, 0);
-  };
-  getRemainderWithoutChoised = asset => {
+  getRemainder = () =>
+    100 - this.state.assets.reduce((sum, item) => sum + item.percent, 0);
+  getRemainderWithoutChoised = (asset: FundAssetPartWithIcon) => {
     return (
       100 -
       this.state.assets
@@ -62,14 +67,15 @@ class ReallocateForm extends Component {
       this.state.assets.filter(item => item.percent > 0)
     );
   };
-  removeHandle = currency => () => {
-    this.state.assets.find(item => item.asset === currency).percent = 0;
+  removeHandle: TFundAssetRemoveHandle = currency => () => {
+    const asset = this.state.assets.find(item => item.asset === currency);
+    asset!.percent = 0;
     this.updateAssets();
   };
-  handleOpenDropdown = event => {
+  handleOpenDropdown: MouseEventHandler = event => {
     this.setState({ anchor: event.currentTarget });
   };
-  handleCloseDropdown = () => this.setState({ anchor: null });
+  handleCloseDropdown = () => this.setState({ anchor: undefined });
   render() {
     const { anchor, assets, remainder } = this.state;
     const {
@@ -91,13 +97,13 @@ class ReallocateForm extends Component {
           <h2>{t("manager.reallocate.title")}</h2>
         </div>
         {(errors.assets && (
-          <ErrorMessage error={errors.assets} type={OVER} />
+          <ErrorMessage error={errors.assets} type={MESSAGE_TYPES.OVER} />
         )) ||
           (errors.remainder && (
-            <ErrorMessage error={errors.remainder} type={OVER} />
+            <ErrorMessage error={errors.remainder} type={MESSAGE_TYPES.OVER} />
           ))}
         <CreateFundSettingsAssetsComponent
-          assets={assets.filter(item => item.percent > 0)}
+          assets={assets.filter(item => item.percent > 0) || []}
           remainder={remainder}
           removeHandle={this.removeHandle}
           addHandle={this.handleOpenDropdown}
@@ -125,32 +131,61 @@ class ReallocateForm extends Component {
     );
   }
 }
-export default compose(
+
+export interface IReallocateFormValues {
+  assets: FundAssetPartWithIcon[];
+  remainder: number;
+}
+
+export interface IReallocateFormOwnProps {
+  assets: FundAssetPartWithIcon[];
+  onSubmit(
+    values: IReallocateFormValues,
+    setSubmitting: SetSubmittingType
+  ): void;
+  errorMessage?: string;
+}
+
+interface Props
+  extends FormikProps<IReallocateFormValues>,
+    IReallocateFormOwnProps,
+    InjectedTranslateProps {}
+
+interface State {
+  assets: FundAssetPartWithIcon[];
+  remainder: number;
+  anchor?: anchorElType;
+}
+
+const ReallocateForm = compose<
+  React.FunctionComponent<IReallocateFormOwnProps>
+>(
   translate(),
-  withFormik({
+  withFormik<IReallocateFormOwnProps, IReallocateFormValues>({
     displayName: "reallocate",
     enableReinitialize: true,
-    validationSchema: ({ t }) =>
+    validationSchema: (props: Props) =>
       object().shape({
         remainder: number()
           .required(
-            t("manager.create-fund-page.settings.validation.assets-share")
+            props.t("manager.create-fund-page.settings.validation.assets-share")
           )
           .max(
             0,
-            t("manager.create-fund-page.settings.validation.assets-share")
+            props.t("manager.create-fund-page.settings.validation.assets-share")
           ),
         assets: array()
           .required(
-            t("manager.create-fund-page.settings.validation.assets-count")
+            props.t("manager.create-fund-page.settings.validation.assets-count")
           )
           .min(
             2,
-            t("manager.create-fund-page.settings.validation.assets-count")
+            props.t("manager.create-fund-page.settings.validation.assets-count")
           )
       }),
     handleSubmit: (values, { props, setSubmitting }) => {
       props.onSubmit(values, setSubmitting);
     }
   })
-)(ReallocateForm);
+)(_ReallocateForm);
+export default ReallocateForm;
