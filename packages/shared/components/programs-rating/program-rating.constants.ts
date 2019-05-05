820ab107155213dbb028a872c6521d39b577e22d
pavelzubov <pasha224@yandex.ru>
2019-05-05 23:55:16 +0300

    Convert ProgramsRating constants to ts

--- packages/shared/components/programs-rating/program-rating.constants.ts
+++ packages/shared/components/programs-rating/program-rating.constants.ts
@@ -1,4 +1,4 @@
-import { PROGRAMS_COLUMNS } from "../../modules/programs-table/components/programs-table/programs.constants";
+import { PROGRAMS_COLUMNS } from "shared/modules/programs-table/components/programs-table/programs.constants";
 
 export const COLUMNS = [
   {
