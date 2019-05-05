6bbdb3a9a6d7f2dad55344e60ead31ceb8220e00
pavelzubov <pasha224@yandex.ru>
2019-05-05 23:55:40 +0300

    Convert ProgramsRatingStats to tsx

--- packages/shared/components/programs-rating/programs-rating-stats.tsx
+++ packages/shared/components/programs-rating/programs-rating-stats.tsx
@@ -1,14 +1,18 @@
 import "./programs-rating.scss";
 
-import React from "react";
-import { translate } from "react-i18next";
+import * as React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
 import NumberFormat from "react-number-format";
 import { compose } from "redux";
 import Profitability from "shared/components/profitability/profitability";
+import {
+  PROFITABILITY_PREFIX,
+  PROFITABILITY_VARIANT
+} from "shared/components/profitability/profitability.helper";
 import StatisticRatingItem from "shared/components/statistic-rating-item/statistic-rating-item";
 import { formatValue } from "shared/utils/formatter";
 
-const ProgramsRatingStats = ({ t, levelData }) => (
+const _ProgramsRatingStats: React.FC<Props> = ({ t, levelData }) => (
   <div className="programs-rating-stats">
     <StatisticRatingItem ellipse label={t("rating-page.programCounts")}>
       <NumberFormat value={levelData.total} displayType="text" />
@@ -19,8 +23,8 @@ const ProgramsRatingStats = ({ t, levelData }) => (
     <StatisticRatingItem label={t("rating-page.currentProfit")}>
       <Profitability
         value={levelData.targetProfit}
-        variant="chips"
-        prefix="arrow"
+        variant={PROFITABILITY_VARIANT.CHIPS}
+        prefix={PROFITABILITY_PREFIX.ARROW}
       >
         <NumberFormat
           value={formatValue(levelData.targetProfit, 2)}
@@ -33,4 +37,14 @@ const ProgramsRatingStats = ({ t, levelData }) => (
   </div>
 );
 
-export default compose(translate())(ProgramsRatingStats);
+interface Props extends InjectedTranslateProps, OwnProps {}
+
+interface OwnProps {
+  levelData: any;
+}
+
+const ProgramsRatingStats = compose<React.ComponentType<OwnProps>>(
+  React.memo,
+  translate()
+)(_ProgramsRatingStats);
+export default ProgramsRatingStats;
