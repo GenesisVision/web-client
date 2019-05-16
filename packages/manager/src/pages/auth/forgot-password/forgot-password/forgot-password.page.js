27608074d5bfae0d1597bf85ad13cec765e6be15
pavelzubov <pasha224@yandex.ru>
2019-05-16 21:17:30 +0300

    Remove unused props from ForgotPasswordContainer

--- packages/manager/src/pages/auth/forgot-password/forgot-password/forgot-password.page.js
+++ packages/manager/src/pages/auth/forgot-password/forgot-password/forgot-password.page.js
@@ -4,19 +4,13 @@ import React from "react";
 import { translate } from "react-i18next";
 import ForgotPasswordContainer from "shared/components/auth/forgot-password/forgot-password/forgot-password-container";
 
-import forgotPasswordService from "../services/forgot-password.service";
-import { LOGIN_ROUTE } from "shared/components/auth/login/login.routes";
-
 const ForgotPasswordPage = ({ t }) => {
   return (
     <div className="forgot-password">
       <p className="forgot-password__text">
         {t("auth.password-restore.forgot-password.text")}
       </p>
-      <ForgotPasswordContainer
-        forgotPasswordService={forgotPasswordService}
-        LOGIN_ROUTE={LOGIN_ROUTE}
-      />
+      <ForgotPasswordContainer />
     </div>
   );
 };
