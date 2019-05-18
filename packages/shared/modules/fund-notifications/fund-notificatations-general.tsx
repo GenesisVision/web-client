ac119dad43bb91a74e5d2b681160391d408252f1
pavelzubov <pasha224@yandex.ru>
2019-05-18 13:17:36 +0300

    Refactoring FundNotificationsGeneral

--- packages/shared/modules/fund-notifications/fund-notificatations-general.tsx
+++ packages/shared/modules/fund-notifications/fund-notificatations-general.tsx
@@ -1,3 +1,7 @@
+import {
+  NotificationSettingViewModel,
+  NotificationSettingViewModelTypeEnum
+} from "gv-api-web";
 import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { ResolveThunks, connect } from "react-redux";
@@ -10,56 +14,62 @@ import {
 import GeneralNotification from "shared/components/general-notification/general-notification";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 
-import { IAddNotificationSettingProps } from "../notification-settings/actions/notification-settings.actions";
+import {
+  IAddNotificationSettingProps,
+  IRemoveNotificationSettingProps
+} from "../notification-settings/actions/notification-settings.actions";
 import {
   addFundNotification,
   removeFundNotification
 } from "./services/fund-notifications.services";
 
-class FundNotificationsGeneral extends React.PureComponent<Props> {
-  success = (text: string) => this.props.service.success(text);
+class _FundNotificationsGeneral extends React.PureComponent<Props> {
+  getNotification = (
+    type: NotificationSettingViewModelTypeEnum
+  ): NotificationSettingViewModel =>
+    this.props.settings.find(setting => setting.type === type)!;
 
   handleAdd = (options: IAddNotificationSettingProps) => {
     const { service, t } = this.props;
     return service
       .addFundNotification(options)
       .then(() =>
-        this.success(
+        service.success(
           t(`notifications-page.fund.general.${options.type}.enabled-alert`)
         )
       );
   };
 
-  handleRemove = (options: { id: string; assetId: string; type: string }) => {
+  handleRemove = (options: IRemoveNotificationSettingProps) => {
     const { service, t } = this.props;
     return service
       .removeFundNotification(options)
       .then(() =>
-        this.success(
+        service.success(
           t(`notifications-page.fund.general.${options.type}.disabled-alert`)
         )
       );
   };
   render() {
-    const { t, settings, assetId } = this.props;
+    const { t, assetId } = this.props;
     return (
       <div className="notification-settings">
         <h3 className="notification-settings__subtitle">
           {t("notifications-page.fund.general.title")}
         </h3>
         <GeneralNotification
-          name="FundNewsAndUpdates"
+          name={NOTIFICATIONS.FundNewsAndUpdates}
           label={t("notifications-page.fund.general.news-updates")}
           assetId={assetId}
-          setting={settings.FundNewsAndUpdates}
+          setting={this.getNotification(NOTIFICATIONS.FundNewsAndUpdates)}
           addNotification={this.handleAdd}
           removeNotification={this.handleRemove}
         />
         <GeneralNotification
-          name="FundRebalancing"
+          name={NOTIFICATIONS.FundRebalancing}
           label={t("notifications-page.fund.general.fund-rebalancing")}
           assetId={assetId}
-          setting={settings.FundRebalancing}
+          setting={this.getNotification(NOTIFICATIONS.FundRebalancing)}
           addNotification={this.handleAdd}
           removeNotification={this.handleRemove}
         />
@@ -79,12 +89,18 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   )
 });
 
+enum NOTIFICATIONS {
+  FundNewsAndUpdates = "FundNewsAndUpdates",
+  FundRebalancing = "FundRebalancing"
+}
+
 interface Props extends OwnProps, DispatchProps, InjectedTranslateProps {}
 
 interface OwnProps {
-  settings: any;
+  settings: NotificationSettingViewModel[];
   assetId: string;
 }
+
 interface ServiceThunks extends ActionCreatorsMapObject {
   success: typeof alertMessageActions.success;
   addFundNotification: typeof addFundNotification;
@@ -94,10 +110,11 @@ interface DispatchProps {
   service: ResolveThunks<ServiceThunks>;
 }
 
-export default compose<React.ComponentType<OwnProps>>(
+const FundNotificationsGeneral = compose<React.ComponentType<OwnProps>>(
   translate(),
   connect(
     undefined,
     mapDispatchToProps
   )
-)(FundNotificationsGeneral);
+)(_FundNotificationsGeneral);
+export default FundNotificationsGeneral;
