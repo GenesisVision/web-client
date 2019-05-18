8e418b4c06698318a8f0c3cbe7a39a49aa550767
pavelzubov <pasha224@yandex.ru>
2019-05-18 08:06:28 +0300

    Convert FundNotificationsReducers to tsx

--- packages/shared/modules/fund-notifications/reducers/fund-notifications.reducers.ts
+++ packages/shared/modules/fund-notifications/reducers/fund-notifications.reducers.ts
@@ -2,11 +2,12 @@ import { FundNotificationSettingList } from "gv-api-web";
 
 import {
   ADD_ERROR_MESSAGE,
-  ADD_FUND_NOTIFICATIONS
+  ADD_FUND_NOTIFICATIONS,
+  FundNotificationsActionType
 } from "../actions/fund-notifications.actions";
 
 export type FundNotificationsState = Readonly<{
-  data: { [name: string]: FundNotificationSettingList };
+  data: { [name: string]: FundNotificationSettingList | undefined };
   errorMessage?: string;
 }>;
 
@@ -16,12 +17,12 @@ const initialState = {
 
 const fundNotificationsReducer = (
   state: FundNotificationsState = initialState,
-  action: any
+  action: FundNotificationsActionType
 ): FundNotificationsState => {
   switch (action.type) {
     case ADD_FUND_NOTIFICATIONS:
       return {
-        data: { ...state.data, [action.settings.url]: action.settings },
+        data: { ...state.data, [action.settings!.url]: action.settings },
         errorMessage: undefined
       };
     case ADD_ERROR_MESSAGE:
