da1a21d5ae56d069a6b9057b4d3bb2ea1aac2816
pavelzubov <pasha224@yandex.ru>
2019-05-21 16:39:12 +0300

    Add dot in program-withdraw-constants file name

--- packages/investor/src/modules/program-withdraw/reducer/program-withdraw.reducer.ts
+++ packages/investor/src/modules/program-withdraw/reducer/program-withdraw.reducer.ts
@@ -1,12 +1,12 @@
 import { ProgramWithdrawInfo } from "gv-api-web";
-import {
-  FETCH_WITHDRAW_PROGRAM_INFO,
-  WITHDRAW_SUBMIT_BY_ID
-} from "modules/program-withdraw/program-withdraw-constants";
 import { combineReducers } from "redux";
 import apiReducerFactory, {
   IApiState
 } from "shared/reducers/api-reducer/api-reducer";
+import {
+  FETCH_WITHDRAW_PROGRAM_INFO,
+  WITHDRAW_SUBMIT_BY_ID
+} from "src/modules/program-withdraw/program-withdraw.constants.ts";
 
 const programWithdrawReducer = apiReducerFactory<ProgramWithdrawInfo>({
   apiType: FETCH_WITHDRAW_PROGRAM_INFO
