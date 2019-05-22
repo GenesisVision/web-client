6f7487dd386bcc508b704814d15686fd55386363
pavelzubov <pasha224@yandex.ru>
2019-05-22 10:55:29 +0300

    Convert dashboard-chart.service to tsx

--- packages/investor/src/pages/dashboard/services/dashboard-chart.service.ts
+++ packages/investor/src/pages/dashboard/services/dashboard-chart.service.ts
@@ -1,8 +1,13 @@
+import { Dispatch } from "redux";
 import authService from "shared/services/auth-service";
+import { TGetState } from "shared/utils/types";
 
 import * as actions from "../actions/dashboard.actions";
 
-export const getPortfolioChart = (from, to) => (dispatch, getState) => {
+export const getPortfolioChart = (from: Date, to: Date) => (
+  dispatch: Dispatch,
+  getState: TGetState
+) => {
   const authorization = authService.getAuthArg();
   const { currency } = getState().accountSettings;
 
