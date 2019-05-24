a19d29968235fc4ed0405f6c50628e73a3229edb
pavelzubov <pasha224@yandex.ru>
2019-05-24 15:41:36 +0300

    Convert dashboard-asset-chart.reducer to tsx

--- packages/manager/src/pages/dashboard/reducers/dashboard-asset-chart.reducer.ts
+++ packages/manager/src/pages/dashboard/reducers/dashboard-asset-chart.reducer.ts
@@ -1,8 +1,14 @@
+import { ActionType, Nullable } from "shared/utils/types";
+
 import { DASHBOARD_ASSET_CHART } from "../actions/dashboard.actions";
+import { IDashboardAssetChart } from "./dashboard.reducers";
 
 const initialState = null;
 
-const dashboardAssetChartReducer = (state = initialState, action) => {
+const dashboardAssetChartReducer = (
+  state: Nullable<IDashboardAssetChart> = initialState,
+  action: ActionType
+): Nullable<IDashboardAssetChart> => {
   switch (action.type) {
     case DASHBOARD_ASSET_CHART:
       return {
