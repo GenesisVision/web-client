9c6d261c90e4ea4b59bac259c80deea329d2edd0
pavelzubov <pasha224@yandex.ru>
2019-05-25 14:10:32 +0300

    Remove unused manager page files

--- packages/manager/src/pages/manager/actions/manager.actions.js
+++
@@ -1,6 +0,0 @@
-import ManagerApi from "shared/services/api-client/manager-api";
-
-export const MANAGER_PROFILE = "MANAGER_PROFILE";
-
-export const fetchManagerProfile = managerId =>
-  ManagerApi.v10ManagerByIdGet(managerId);
