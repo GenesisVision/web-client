47d703871f545624665cba0baeda50f9c2a8d5f5
nkrivous <kolsln@list.ru>
2019-05-27 17:41:36 +0300

    Show successFeeCurrency for Profit transactions

--- packages/shared/modules/transaction-details/transactions/external-withdrawal-details.tsx
+++ packages/shared/modules/transaction-details/transactions/external-withdrawal-details.tsx
@@ -11,7 +11,7 @@ import { TransactionDetailsProps } from "shared/modules/transaction-details/tran
 import filesService from "shared/services/file-service";
 import { formatValue } from "shared/utils/formatter";
 
-import { TRANSACTIONS_DECIMAL_SCALE } from "./transactions.constans";
+import { TRANSACTIONS_DECIMAL_SCALE } from "./transactions.constants";
 
 const ExternalWithdrawal: React.FC<TransactionDetailsProps> = props => {
   const { data, t, handleCancel, handleResend } = props;
