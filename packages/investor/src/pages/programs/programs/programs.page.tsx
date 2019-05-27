1bdab0d84b37e16d9100fa107c8c4ecb9b2ace5b
pavelzubov <pasha224@yandex.ru>
2019-05-27 06:58:46 +0300

    Convert ProgramsPage to tsx

--- packages/investor/src/pages/programs/programs/programs.page.tsx
+++ packages/investor/src/pages/programs/programs/programs.page.tsx
@@ -1,28 +1,33 @@
-import React from "react";
-import { translate } from "react-i18next";
-import FacetCardsContainer from "shared/components/facet-cards/faset-cards-container";
+import * as React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
+import FacetCardsContainer, {
+  ASSETS_FACETS
+} from "shared/components/facet-cards/faset-cards-container";
 import NavigationTabsContainer from "shared/components/navigation-tabs/navigation-tabs-container";
 import Page from "shared/components/page/page";
 import Surface from "shared/components/surface/surface";
 import ProgramsTableContainer from "shared/modules/programs-table/components/programs-table/programs-table-container";
 import { composeProgramFacetUrl } from "shared/utils/compose-url";
 
-import * as routes from "../programs.routes";
+import {
+  PROGRAMS_EXPLORE_TAB_NAME,
+  PROGRAMS_FAVORITES_TAB_NAME,
+  PROGRAMS_TAB_ROUTE
+} from "../programs.routes";
 
-const ProgramsPage = ({ t }) => {
+const _ProgramsPage: React.FC<InjectedTranslateProps> = ({ t }) => {
   const title = t("programs-page.title");
   return (
     <Page title={title}>
       <NavigationTabsContainer
-        exploreTabName={routes.PROGRAMS_EXPLORE_TAB_NAME}
-        tabRoute={routes.PROGRAMS_TAB_ROUTE}
-        favoritesTabName={routes.PROGRAMS_FAVORITES_TAB_NAME}
+        exploreTabName={PROGRAMS_EXPLORE_TAB_NAME}
+        tabRoute={PROGRAMS_TAB_ROUTE}
+        favoritesTabName={PROGRAMS_FAVORITES_TAB_NAME}
       />
       <FacetCardsContainer
         title={title}
-        assetsFacets={"programsFacets"}
+        assetsFacets={ASSETS_FACETS.PROGRAMS}
         composeFacetUrl={composeProgramFacetUrl}
-        PROGRAMS_RATING_ROUTE={routes.PROGRAMS_RATING_ROUTE}
       />
       <Surface className="programs-table-container">
         <ProgramsTableContainer
@@ -34,4 +39,5 @@ const ProgramsPage = ({ t }) => {
   );
 };
 
-export default translate()(ProgramsPage);
+const ProgramsPage = React.memo(translate()(_ProgramsPage));
+export default ProgramsPage;
