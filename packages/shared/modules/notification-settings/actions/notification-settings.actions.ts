0b0fdd258ccef6ad2166289f5d91cc7771d4a1e8
pavelzubov <pasha224@yandex.ru>
2019-05-28 17:06:01 +0300

    Remove helpless addNotificationSettingsAction, change notificationSettingsReducer to apiReducerFactory, correct NotificationSettingsState to IApiState

--- packages/shared/modules/notification-settings/actions/notification-settings.actions.ts
+++ packages/shared/modules/notification-settings/actions/notification-settings.actions.ts
@@ -1,21 +1,16 @@
-import { NotificationSettingList } from "gv-api-web";
+import { CancelablePromise, NotificationSettingList } from "gv-api-web";
 import notificationsApi from "shared/services/api-client/notifications-api";
 import authService from "shared/services/auth-service";
-
-import { ActionType } from "../../../utils/types";
+import { ActionType } from "shared/utils/types";
 
 export const NOTIFICATION_SETTINGS = "NOTIFICATION_SETTINGS";
 export const ADD_NOTIFICATION_SETTINGS = "ADD_NOTIFICATION_SETTINGS";
 export const REMOVE_NOTIFICATION_SETTING = "REMOVE_NOTIFICATION_SETTING";
 export const ADD_NOTIFICATION_SETTING = "ADD_NOTIFICATION_SETTING";
 
-export const addNotificationSettingsAction = (
-  settings: NotificationSettingList
-) => {
-  return { type: ADD_NOTIFICATION_SETTINGS, settings };
-};
-
-export const fetchNotificationSettingsAction = () => ({
+export const fetchNotificationSettingsAction = (): ActionType<
+  CancelablePromise<NotificationSettingList>
+> => ({
   type: NOTIFICATION_SETTINGS,
   payload: notificationsApi.v10NotificationsSettingsGet(
     authService.getAuthArg()
@@ -38,7 +33,7 @@ export interface IAddNotificationSettingProps {
 
 export const addNotificationSettingAction = (
   opts: IAddNotificationSettingProps
-): ActionType => ({
+): ActionType<CancelablePromise<string>> => ({
   type: ADD_NOTIFICATION_SETTING,
   payload: notificationsApi.v10NotificationsSettingsAddPost(
     authService.getAuthArg(),
@@ -46,7 +41,9 @@ export const addNotificationSettingAction = (
   )
 });
 
-export const removeNotificationSettingAction = (id: string): ActionType => ({
+export const removeNotificationSettingAction = (
+  id: string
+): ActionType<CancelablePromise<void>> => ({
   type: REMOVE_NOTIFICATION_SETTING,
   payload: notificationsApi.v10NotificationsSettingsRemoveByIdPost(
     id,
