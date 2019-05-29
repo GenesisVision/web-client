e2e488abc83b96e290c65bd621be2cad0a7938ad
pavelzubov <pasha224@yandex.ru>
2019-05-29 10:33:40 +0300

    Change import routes to shared

--- packages/investor/src/pages/funds/funds/funds.page.tsx
+++ packages/investor/src/pages/funds/funds/funds.page.tsx
@@ -3,22 +3,25 @@ import { InjectedTranslateProps, translate } from "react-i18next";
 import FacetCardsContainer, {
   ASSETS_FACETS
 } from "shared/components/facet-cards/faset-cards-container";
+import {
+  FUNDS_EXPLORE_TAB_NAME,
+  FUNDS_FAVORITES_TAB_NAME,
+  FUNDS_TAB_ROUTE
+} from "shared/components/funds/funds.routes";
 import NavigationTabsContainer from "shared/components/navigation-tabs/navigation-tabs-container";
 import Page from "shared/components/page/page";
 import Surface from "shared/components/surface/surface";
 import FundsTableContainer from "shared/modules/funds-table/components/funds-table/funds-table-container";
 import { composeFundFacetUrl } from "shared/utils/compose-url";
 
-import * as routes from "../funds.routes";
-
 const _FundsPage: React.FC<InjectedTranslateProps> = ({ t }) => {
   const title = t("funds-page.title");
   return (
     <Page title={title}>
       <NavigationTabsContainer
-        exploreTabName={routes.FUNDS_EXPLORE_TAB_NAME}
-        tabRoute={routes.FUNDS_TAB_ROUTE}
-        favoritesTabName={routes.FUNDS_FAVORITES_TAB_NAME}
+        exploreTabName={FUNDS_EXPLORE_TAB_NAME}
+        tabRoute={FUNDS_TAB_ROUTE}
+        favoritesTabName={FUNDS_FAVORITES_TAB_NAME}
       />
       <FacetCardsContainer
         title={title}
