d2699994cde0b559ebbc404c9ddb56877d702f57
pavelzubov <pasha224@yandex.ru>
2019-05-30 11:19:24 +0300

    Remove usage NavigationBackButton

--- packages/manager/src/pages/profile/edit/edit.page.tsx
+++ packages/manager/src/pages/profile/edit/edit.page.tsx
@@ -1,11 +1,9 @@
-import NavigationBackButton from "modules/navigation-back-button/navigation-back-button";
 import * as React from "react";
 import ProfileLayout from "shared/components/profile/profile-layout";
 import ProfileContainer from "shared/modules/profile/profile-container";
 
 const _ProfileEditPage: React.FC = () => (
   <ProfileLayout route="details">
-    <NavigationBackButton />
     <ProfileContainer editable />
   </ProfileLayout>
 );
