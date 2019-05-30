aa66a239439741e0a32919ac080500326400f5d0
pavelzubov <pasha224@yandex.ru>
2019-05-30 11:20:48 +0300

    Change ways to HOME_ROUTE, PROGRAMS_ROUTE, FUNDS_ROUTE, DASHBOARD_ROUTE

--- packages/shared/components/navigation/navigation.tsx
+++ packages/shared/components/navigation/navigation.tsx
@@ -1,9 +1,6 @@
 import "./navigation.scss";
 
 import classNames from "classnames";
-import { HOME_ROUTE } from "pages/app/app.routes";
-import { DASHBOARD_ROUTE } from "pages/dashboard/dashboard.routes";
-import { PROGRAMS_ROUTE } from "pages/programs/programs.routes";
 import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { FUNDS_ROUTE } from "shared/components/funds/funds.routes";
@@ -12,6 +9,9 @@ import { DashboardIcon } from "shared/components/icon/dashboard-icon";
 import { FundsIcon } from "shared/components/icon/funds-icon";
 import { ProgramsIcon } from "shared/components/icon/programs-icon";
 import NavigationItem from "shared/components/navigation/navigation-item";
+import { HOME_ROUTE } from "shared/routes/app.routes";
+import { DASHBOARD_ROUTE } from "shared/routes/dashboard.routes";
+import { PROGRAMS_ROUTE } from "shared/routes/programs.routes";
 
 interface INavigationProps {
   className?: string;
