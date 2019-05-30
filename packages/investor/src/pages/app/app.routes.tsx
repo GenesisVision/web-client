0807180a3fce3929ca03fe0ace1e2f9197006dab
pavelzubov <pasha224@yandex.ru>
2019-05-30 11:18:43 +0300

    Convert AppRoutes to tsx

--- packages/investor/src/pages/app/app.routes.tsx
+++ packages/investor/src/pages/app/app.routes.tsx
@@ -22,39 +22,30 @@ import WalletWithdrawConfirmPage, {
   WALLET_WITHDRAW_CONFIRM_ROUTE
 } from "shared/components/wallet-withdraw-confirm/wallet-withdraw-confirm.page";
 import { WALLET_TOTAL_PAGE_ROUTE } from "shared/components/wallet/wallet.routes";
+import { HOME_ROUTE } from "shared/routes/app.routes";
 
 import FundsRoutes from "../funds/funds.routes";
 import AppLayout from "./components/app-layout/app-layout";
 
-export const HOME_ROUTE = "/";
-
-const AppRoutes = () => {
-  return (
-    <AppLayout>
-      <Switch>
-        <Redirect exact from={HOME_ROUTE} to={PROGRAMS_ROUTE} />
-        <Route path={PROGRAMS_ROUTE} component={ProgramsRoutes} />
-        <Route path={FUNDS_ROUTE} component={FundsRoutes} />
-        <Route path={GLOBAL_SEARCH_ROUTE} component={GlobalSearchPage} />
-        <Route
-          path={MANAGER_DETAILS_ROUTE_REGEXP}
-          component={ManagerContainer}
-        />
-        <Route
-          path={WALLET_WITHDRAW_CONFIRM_ROUTE}
-          component={WalletWithdrawConfirmPage}
-        />
-        <PrivateRoute path={PROFILE_ROUTE} component={ProfileRoutes} />
-        <PrivateRoute
-          path={NOTIFICATIONS_ROUTE}
-          component={NotificationRoutes}
-        />
-        <PrivateRoute path={DASHBOARD_ROUTE} component={DashboardRoutes} />
-        <PrivateRoute path={WALLET_TOTAL_PAGE_ROUTE} component={WalletRoutes} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </AppLayout>
-  );
-};
+const AppRoutes: React.FC = () => (
+  <AppLayout>
+    <Switch>
+      <Redirect exact from={HOME_ROUTE} to={PROGRAMS_ROUTE} />
+      <Route path={PROGRAMS_ROUTE} component={ProgramsRoutes} />
+      <Route path={FUNDS_ROUTE} component={FundsRoutes} />
+      <Route path={GLOBAL_SEARCH_ROUTE} component={GlobalSearchPage} />
+      <Route path={MANAGER_DETAILS_ROUTE_REGEXP} component={ManagerContainer} />
+      <Route
+        path={WALLET_WITHDRAW_CONFIRM_ROUTE}
+        component={WalletWithdrawConfirmPage}
+      />
+      <PrivateRoute path={PROFILE_ROUTE} component={ProfileRoutes} />
+      <PrivateRoute path={NOTIFICATIONS_ROUTE} component={NotificationRoutes} />
+      <PrivateRoute path={DASHBOARD_ROUTE} component={DashboardRoutes} />
+      <PrivateRoute path={WALLET_TOTAL_PAGE_ROUTE} component={WalletRoutes} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </AppLayout>
+);
 
 export default AppRoutes;
