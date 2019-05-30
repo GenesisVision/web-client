caba614da5232e25fb9446cc66979050fb0dffa0
a.popova <bob.m@mail.ru>
2019-05-30 13:19:35 +0300

    GV-1050 added the tooltip for transfer button in the wallet

--- packages/shared/components/wallet/components/wallet-balance/wallet-balance-buttons.tsx
+++ packages/shared/components/wallet/components/wallet-balance/wallet-balance-buttons.tsx
@@ -5,6 +5,24 @@ import { InjectedTranslateProps, translate } from "react-i18next";
 import GVButton from "shared/components/gv-button";
 import ArrowIcon from "shared/media/arrow-up.svg";
 import ConvertIcon from "shared/media/convert.svg";
+import Tooltip from "shared/components/tooltip/tooltip";
+
+const renderTooltipButton = (tooltipContent: string, buttonText: string) => (
+  <Tooltip
+    render={() => (
+      <div className="wallet-list__tooltip-button">{tooltipContent}</div>
+    )}
+  >
+    <span>
+      <img
+        className="wallet-balance__button-icon"
+        src={ConvertIcon}
+        alt="Convert icon"
+      />
+      {buttonText}
+    </span>
+  </Tooltip>
+);
 
 const _WalletBalanceButtons: React.FC<Props> = ({
   t,
@@ -13,43 +31,41 @@ const _WalletBalanceButtons: React.FC<Props> = ({
   handleTransfer,
   isDepositEnabled,
   isWithdrawalEnabled
-}) => (
-  <div className="wallet-balance__buttons">
-    <GVButton onClick={handleAddFunds} disabled={isDepositEnabled === false}>
-      <>
-        <span className="wallet-balance__button-icon wallet-balance__button-icon--sign">
-          +
-        </span>
-        {t("wallet-page.deposit")}
-      </>
-    </GVButton>
-    <GVButton
-      color="secondary"
-      variant="outlined"
-      onClick={handleWithdraw}
-      disabled={isWithdrawalEnabled === false}
-    >
-      <>
-        <img
-          className="wallet-balance__button-icon"
-          src={ArrowIcon}
-          alt="Arrow icon"
-        />
-        {t("wallet-page.withdraw")}
-      </>
-    </GVButton>
-    <GVButton color="secondary" variant="outlined" onClick={handleTransfer}>
-      <>
-        <img
-          className="wallet-balance__button-icon"
-          src={ConvertIcon}
-          alt="Convert icon"
-        />
-        {t("wallet-page.transfer")}
-      </>
-    </GVButton>
-  </div>
-);
+}) => {
+  return (
+    <div className="wallet-balance__buttons">
+      <GVButton onClick={handleAddFunds} disabled={isDepositEnabled === false}>
+        <>
+          <span className="wallet-balance__button-icon wallet-balance__button-icon--sign">
+            +
+          </span>
+          {t("wallet-page.deposit")}
+        </>
+      </GVButton>
+      <GVButton
+        color="secondary"
+        variant="outlined"
+        onClick={handleWithdraw}
+        disabled={isWithdrawalEnabled === false}
+      >
+        <>
+          <img
+            className="wallet-balance__button-icon"
+            src={ArrowIcon}
+            alt="Arrow icon"
+          />
+          {t("wallet-page.withdraw")}
+        </>
+      </GVButton>
+      <GVButton color="secondary" variant="outlined" onClick={handleTransfer}>
+        {renderTooltipButton(
+          t("wallet-page.tooltip.transfer"),
+          t("wallet-page.transfer")
+        )}
+      </GVButton>
+    </div>
+  );
+};
 
 interface Props extends InjectedTranslateProps {
   handleAddFunds: () => void;
