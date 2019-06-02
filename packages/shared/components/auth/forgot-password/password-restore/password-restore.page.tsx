19c8a6551bb80ae2920a020fac2ff5db37aaba83
pavelzubov <pasha224@yandex.ru>
2019-06-02 11:48:50 +0300

    Correct PasswordRestorePage component name and change way in routes

--- packages/shared/components/auth/forgot-password/password-restore/password-restore.page.tsx
+++ packages/shared/components/auth/forgot-password/password-restore/password-restore.page.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import PasswordRestoreContainer from "shared/components/auth/forgot-password/password-restore/password-restore-container";
 
-const _PasswordRestore: React.FC<
+const _PasswordRestorePage: React.FC<
   { location: Location } & InjectedTranslateProps
 > = ({ location, t }) => (
   <div className="password-restore">
@@ -18,5 +18,5 @@ const _PasswordRestore: React.FC<
   </div>
 );
 
-const PasswordRestore = React.memo(translate()(_PasswordRestore));
-export default PasswordRestore;
+const PasswordRestorePage = React.memo(translate()(_PasswordRestorePage));
+export default PasswordRestorePage;
