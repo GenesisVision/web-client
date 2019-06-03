a3ba02d7f1701c7d1b29a1a11c3648ac770e23b4
a.popova <bob.m@mail.ru>
2019-06-03 14:05:19 +0300

    GV-988 fixed labels of modes

--- packages/investor/src/modules/program-unfollow/components/program-unfollow-form.tsx
+++ packages/investor/src/modules/program-unfollow/components/program-unfollow-form.tsx
@@ -8,58 +8,54 @@ import GVFormikField from "shared/components/gv-formik-field";
 import GVTextField from "shared/components/gv-text-field";
 import Select from "shared/components/select/select";
 
-const _ProgramUnfollowForm: React.FC<Props> = ({
-  t,
-  onSubmit,
-  handleSubmit,
-  isSubmitting
-}) => {
-  return (
-    <form id="unfollow-form" onSubmit={handleSubmit} noValidate>
-      <div className="dialog__top">
-        <h2>{t("unfollow-program.title")}</h2>
-        <div className="dialog-field">
-          <GVFormikField
-            name="mode"
-            component={GVTextField}
-            label={t("unfollow-program.type")}
-            InputComponent={Select}
-          >
-            {Object.keys(modes).map((mode: string) => (
-              <option value={modes[mode].value} key={modes[mode].value}>
-                {modes[mode].label}
-              </option>
-            ))}
-          </GVFormikField>
+const _ProgramUnfollowForm: React.FC<Props> = React.memo(
+  ({ t, onSubmit, handleSubmit, isSubmitting }) => {
+    return (
+      <form id="unfollow-form" onSubmit={handleSubmit} noValidate>
+        <div className="dialog__top">
+          <h2>{t("unfollow-program.title")}</h2>
+          <div className="dialog-field">
+            <GVFormikField
+              name="mode"
+              component={GVTextField}
+              label={t("unfollow-program.type")}
+              InputComponent={Select}
+            >
+              {Object.keys(modes).map((mode: string) => (
+                <option value={modes[mode].value} key={modes[mode].value}>
+                  {t(modes[mode].label)}
+                </option>
+              ))}
+            </GVFormikField>
+          </div>
+          <div className="dialog__buttons">
+            <GVButton
+              type="submit"
+              className="invest-form__submit-button"
+              disabled={isSubmitting}
+            >
+              {t("unfollow-program.submit")}
+            </GVButton>
+          </div>
         </div>
-        <div className="dialog__buttons">
-          <GVButton
-            type="submit"
-            className="invest-form__submit-button"
-            disabled={isSubmitting}
-          >
-            {t("unfollow-program.submit")}
-          </GVButton>
-        </div>
-      </div>
-    </form>
-  );
-};
-
-const ProgramUnfollowForm = React.memo(
-  compose<React.ComponentType<OwnProps>>(
-    translate(),
-    withFormik<OwnProps, IProgramUnfollowFormValues>({
-      displayName: "confirm-form",
-      mapPropsToValues: () => ({
-        mode: modes.none.value as DetachFromSignalProviderModeEnum
-      }),
-      handleSubmit: (values, { props }) => {
-        props.onSubmit(values);
-      }
-    })
-  )(_ProgramUnfollowForm)
+      </form>
+    );
+  }
 );
+
+const ProgramUnfollowForm = compose<React.ComponentType<OwnProps>>(
+  translate(),
+  withFormik<OwnProps, IProgramUnfollowFormValues>({
+    displayName: "confirm-form",
+    mapPropsToValues: () => ({
+      mode: modes.none.value as DetachFromSignalProviderModeEnum
+    }),
+    handleSubmit: (values, { props }) => {
+      props.onSubmit(values);
+    }
+  })
+)(_ProgramUnfollowForm);
+
 export default ProgramUnfollowForm;
 
 type mode = {
@@ -68,9 +64,15 @@ type mode = {
 };
 
 const modes: { [key: string]: mode } = {
-  none: { label: "Manual closing", value: "None" },
-  closeOnly: { label: "Close only", value: "ProviderCloseOnly" },
-  closeAll: { label: "Close all immediately", value: "CloseAllImmediately" }
+  none: { label: "unfollow-program.modes.manual-closing", value: "None" },
+  closeOnly: {
+    label: "unfollow-program.modes.close-only",
+    value: "ProviderCloseOnly"
+  },
+  closeAll: {
+    label: "unfollow-program.modes.close-all-immediately",
+    value: "CloseAllImmediately"
+  }
 };
 
 interface OwnProps {
