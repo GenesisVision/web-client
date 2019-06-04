4f9e9c10cc01244cb8882a386ea735d5f1cd34cb
pavelzubov <pasha224@yandex.ru>
2019-06-04 14:40:56 +0300

    Change FundDetailsDescription to FC

--- packages/shared/components/funds/fund-details/fund-details-description/fund-details-description.tsx
+++ packages/shared/components/funds/fund-details/fund-details-description/fund-details-description.tsx
@@ -6,58 +6,52 @@ import DetailsNotification from "shared/components/details/details-description-s
 import GVButton from "shared/components/gv-button";
 import { composeManagerDetailsUrl } from "shared/utils/compose-url";
 
-class _FundDetailsDescription extends React.PureComponent<Props> { // TODO change to FC
-  render() {
-    const {
-      t,
-      assetDescription,
-      AssetDetailsAvatar,
-      AssetDetailsExtraBlock
-    } = this.props;
-
-    return (
-      <div className="program-details-description__main">
-        <div className="program-details-description__avatar">
-          <AssetDetailsAvatar />
-        </div>
-        <div className="program-details-description__info">
-          <h1 className="title-small-padding">{assetDescription.title}</h1>
-          <Link
-            to={{
-              pathname: composeManagerDetailsUrl(assetDescription.managerUrl),
-              state: `/ ${assetDescription.title}`
-            }}
-          >
-            <GVButton
-              variant="text"
-              className="program-details-description__author-btn"
-            >
-              {assetDescription.managerName}
-            </GVButton>
-          </Link>
-          <AssetDetailsExtraBlock />
-          <h4 className="program-details-description__subheading">
-            {t("program-details-page.description.strategy")}
-          </h4>
-          <div className="program-details-description__text">
-            {assetDescription.description}
-          </div>
-        </div>
-        <div className="program-details-description__settings">
-          <DetailsFavorite
-            id={assetDescription.id}
-            isFavorite={assetDescription.isFavorite}
-          />
-          <DetailsNotification
-            title={assetDescription.title}
-            url={assetDescription.notificationsUrl}
-            hasNotifications={assetDescription.hasNotifications}
-          />
-        </div>
+const _FundDetailsDescription: React.FC<Props> = ({
+  t,
+  assetDescription,
+  AssetDetailsAvatar,
+  AssetDetailsExtraBlock
+}) => (
+  <div className="program-details-description__main">
+    <div className="program-details-description__avatar">
+      <AssetDetailsAvatar />
+    </div>
+    <div className="program-details-description__info">
+      <h1 className="title-small-padding">{assetDescription.title}</h1>
+      <Link
+        to={{
+          pathname: composeManagerDetailsUrl(assetDescription.managerUrl),
+          state: `/ ${assetDescription.title}`
+        }}
+      >
+        <GVButton
+          variant="text"
+          className="program-details-description__author-btn"
+        >
+          {assetDescription.managerName}
+        </GVButton>
+      </Link>
+      <AssetDetailsExtraBlock />
+      <h4 className="program-details-description__subheading">
+        {t("program-details-page.description.strategy")}
+      </h4>
+      <div className="program-details-description__text">
+        {assetDescription.description}
       </div>
-    );
-  }
-}
+    </div>
+    <div className="program-details-description__settings">
+      <DetailsFavorite
+        id={assetDescription.id}
+        isFavorite={assetDescription.isFavorite}
+      />
+      <DetailsNotification
+        title={assetDescription.title}
+        url={assetDescription.notificationsUrl}
+        hasNotifications={assetDescription.hasNotifications}
+      />
+    </div>
+  </div>
+);
 
 const FundDetailsDescription = React.memo(translate()(_FundDetailsDescription));
 export default FundDetailsDescription;
