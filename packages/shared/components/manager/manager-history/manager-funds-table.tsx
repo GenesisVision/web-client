9f51d21a3088f57db093b002b8a6e45a4fa7e7bb
pavelzubov <pasha224@yandex.ru>
2019-06-05 13:36:32 +0300

    Convert ManagerFunds to FC

--- packages/shared/components/manager/manager-history/manager-funds-table.tsx
+++ packages/shared/components/manager/manager-history/manager-funds-table.tsx
@@ -1,6 +1,6 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-history/trades.scss";
 
-import * as React from "react";
+import React, { useCallback } from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
 import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
@@ -27,52 +27,57 @@ interface Props {
   isAuthenticated: boolean;
 }
 
-class _ManagerFunds extends React.PureComponent<
-  Props & InjectedTranslateProps
-> {
-  fetchManagerFunds: GetItemsFuncType = filters =>
-    fetchManagerFunds({ ...filters, managerId: this.props.managerId });
+const _ManagerFunds: React.FC<Props & InjectedTranslateProps> = ({
+  t,
+  title,
+  isAuthenticated,
+  managerId
+}) => {
+  const getManagerFunds: GetItemsFuncType = useCallback(
+    filters => fetchManagerFunds({ ...filters, managerId }),
+    [managerId]
+  );
 
-  toggleFavorite: TableToggleFavoriteType = (asset, updateRow) => () => {
-    const isFavorite = asset.personalDetails.isFavorite;
-    const newProgram = {
-      ...asset,
-      personalDetails: { ...asset.personalDetails, isFavorite: !isFavorite }
-    };
-    updateRow(newProgram);
-    toggleFavoriteFund(asset.id, isFavorite).catch(() => {
-      updateRow(asset);
-    });
-  };
+  const toggleFavorite: TableToggleFavoriteType = useCallback(
+    (asset, updateRow) => () => {
+      const isFavorite = asset.personalDetails.isFavorite;
+      const newProgram = {
+        ...asset,
+        personalDetails: { ...asset.personalDetails, isFavorite: !isFavorite }
+      };
+      updateRow(newProgram);
+      toggleFavoriteFund(asset.id, isFavorite).catch(() => {
+        updateRow(asset);
+      });
+    },
+    []
+  );
 
-  render() {
-    const { t, title, isAuthenticated } = this.props;
-    return (
-      <FundsTableModule
-        disableTitle
-        title={title}
-        getItems={this.fetchManagerFunds}
-        defaultFilters={MANAGER_DEFAULT_FILTERS}
-        filtering={MANAGER_FILTERING}
-        paging={DEFAULT_PAGING}
-        columns={FUNDS_TABLE_COLUMNS}
-        renderFilters={(
-          updateFilter: UpdateFilterFunc,
-          filtering: FilteringType
-        ) => (
-          <DateRangeFilter
-            name={DATE_RANGE_FILTER_NAME}
-            value={filtering[DATE_RANGE_FILTER_NAME]}
-            onChange={updateFilter}
-            startLabel={t("filters.date-range.program-start")}
-          />
-        )}
-        toggleFavorite={this.toggleFavorite}
-        isAuthenticated={isAuthenticated}
-      />
-    );
-  }
-}
+  return (
+    <FundsTableModule
+      disableTitle
+      title={title}
+      getItems={getManagerFunds}
+      defaultFilters={MANAGER_DEFAULT_FILTERS}
+      filtering={MANAGER_FILTERING}
+      paging={DEFAULT_PAGING}
+      columns={FUNDS_TABLE_COLUMNS}
+      renderFilters={(
+        updateFilter: UpdateFilterFunc,
+        filtering: FilteringType
+      ) => (
+        <DateRangeFilter
+          name={DATE_RANGE_FILTER_NAME}
+          value={filtering[DATE_RANGE_FILTER_NAME]}
+          onChange={updateFilter}
+          startLabel={t("filters.date-range.program-start")}
+        />
+      )}
+      toggleFavorite={toggleFavorite}
+      isAuthenticated={isAuthenticated}
+    />
+  );
+};
 
-const ManagerFunds = translate()(_ManagerFunds);
+const ManagerFunds = React.memo(translate()(_ManagerFunds));
 export default ManagerFunds;
