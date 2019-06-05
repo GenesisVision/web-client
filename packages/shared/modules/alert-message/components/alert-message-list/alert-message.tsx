c6f72c93f5e2327c59918e6e31206917d9805a77
pavelzubov <pasha224@yandex.ru>
2019-06-05 13:40:25 +0300

    Convert AlertMessage to FC

--- packages/shared/modules/alert-message/components/alert-message-list/alert-message.tsx
+++ packages/shared/modules/alert-message/components/alert-message-list/alert-message.tsx
@@ -1,49 +1,44 @@
 import classNames from "classnames";
-import * as React from "react";
+import React, { useCallback } from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 
 import { IMessage } from "../../reducers/alert-message-reducers";
 
-interface IAlertMessageProps {
+const _AlertMessage: React.FC<Props & InjectedTranslateProps> = ({
+  message,
+  t,
+  onClick
+}) => {
+  const handleClick = useCallback(() => onClick(message.id), [
+    onClick,
+    message
+  ]);
+
+  const getMessageText = useCallback(
+    ({
+      text,
+      isUseLocalization
+    }: {
+      text: string;
+      isUseLocalization: boolean;
+    }): string => (isUseLocalization && t(text)) || text,
+    [t]
+  );
+
+  return (
+    <div className={classNames("alert-message", message.className)}>
+      <div className="alert-message-list__text">{getMessageText(message)}</div>
+      <div className="alert-message-list__close" onClick={handleClick}>
+        <div className="alert-message-list__close-button">+</div>
+      </div>
+    </div>
+  );
+};
+
+interface Props {
   onClick(id: string): void;
   message: IMessage;
 }
 
-class AlertMessage extends React.PureComponent<
-  IAlertMessageProps & InjectedTranslateProps
-> {
-  handleClick = () => {
-    this.props.onClick(this.props.message.id);
-  };
-
-  getMessageText = ({
-    text,
-    isUseLocalization
-  }: {
-    text: string;
-    isUseLocalization: boolean;
-  }): string => {
-    let result = text;
-
-    if (isUseLocalization) {
-      result = this.props.t(text);
-    }
-    return result;
-  };
-
-  render() {
-    const { message } = this.props;
-    return (
-      <div className={classNames("alert-message", message.className)}>
-        <div className="alert-message-list__text">
-          {this.getMessageText(message)}
-        </div>
-        <div className="alert-message-list__close" onClick={this.handleClick}>
-          <div className="alert-message-list__close-button">+</div>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default translate()(AlertMessage);
+const AlertMessage = React.memo(translate()(_AlertMessage));
+export default AlertMessage;
