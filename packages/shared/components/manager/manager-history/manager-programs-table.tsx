3f8b54075e367bb276f5bfd33c1d2e3e2c172ced
pavelzubov <pasha224@yandex.ru>
2019-06-05 13:36:40 +0300

    Convert ManagerPrograms to FC

--- packages/shared/components/manager/manager-history/manager-programs-table.tsx
+++ packages/shared/components/manager/manager-history/manager-programs-table.tsx
@@ -1,6 +1,6 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-history/trades.scss";
 
-import * as React from "react";
+import React, { useCallback } from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
 import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
@@ -19,55 +19,60 @@ import {
 } from "../manager.constants";
 import { fetchManagerPrograms } from "../services/manager.service";
 
+const _ManagerPrograms: React.FC<Props & InjectedTranslateProps> = ({
+  t,
+  title,
+  isAuthenticated,
+  managerId
+}) => {
+  const getManagerPrograms: GetItemsFuncType = useCallback(
+    filters => fetchManagerPrograms({ ...filters, managerId }),
+    [managerId]
+  );
+
+  const toggleFavorite: TableToggleFavoriteType = useCallback(
+    (program, updateRow) => () => {
+      const isFavorite = program.personalDetails.isFavorite;
+      const newProgram = {
+        ...program,
+        personalDetails: { ...program.personalDetails, isFavorite: !isFavorite }
+      };
+      updateRow(newProgram);
+      toggleFavoriteProgram(program.id, isFavorite).catch(() => {
+        updateRow(program);
+      });
+    },
+    []
+  );
+
+  return (
+    <ProgramTableModule
+      disableTitle
+      title={title}
+      getItems={getManagerPrograms}
+      defaultFilters={MANAGER_DEFAULT_FILTERS}
+      filtering={MANAGER_FILTERING}
+      paging={DEFAULT_PAGING}
+      sorting={MANAGER_SORTING}
+      renderFilters={(updateFilter, filtering) => (
+        <DateRangeFilter
+          name={DATE_RANGE_FILTER_NAME}
+          value={filtering[DATE_RANGE_FILTER_NAME]}
+          onChange={updateFilter}
+          startLabel={t("filters.date-range.program-start")}
+        />
+      )}
+      toggleFavorite={toggleFavorite}
+      isAuthenticated={isAuthenticated}
+    />
+  );
+};
+
 interface Props {
   managerId: string;
   title: string;
   isAuthenticated: boolean;
 }
 
-class _ManagerPrograms extends React.PureComponent<
-  Props & InjectedTranslateProps
-> {
-  fetchManagerPrograms: GetItemsFuncType = filters =>
-    fetchManagerPrograms({ ...filters, managerId: this.props.managerId });
-
-  toggleFavorite: TableToggleFavoriteType = (program, updateRow) => () => {
-    const isFavorite = program.personalDetails.isFavorite;
-    const newProgram = {
-      ...program,
-      personalDetails: { ...program.personalDetails, isFavorite: !isFavorite }
-    };
-    updateRow(newProgram);
-    toggleFavoriteProgram(program.id, isFavorite).catch(() => {
-      updateRow(program);
-    });
-  };
-
-  render() {
-    const { t, title, isAuthenticated } = this.props;
-    return (
-      <ProgramTableModule
-        disableTitle
-        title={title}
-        getItems={this.fetchManagerPrograms}
-        defaultFilters={MANAGER_DEFAULT_FILTERS}
-        filtering={MANAGER_FILTERING}
-        paging={DEFAULT_PAGING}
-        sorting={MANAGER_SORTING}
-        renderFilters={(updateFilter, filtering) => (
-          <DateRangeFilter
-            name={DATE_RANGE_FILTER_NAME}
-            value={filtering[DATE_RANGE_FILTER_NAME]}
-            onChange={updateFilter}
-            startLabel={t("filters.date-range.program-start")}
-          />
-        )}
-        toggleFavorite={this.toggleFavorite}
-        isAuthenticated={isAuthenticated}
-      />
-    );
-  }
-}
-
-const ManagerPrograms = translate()(_ManagerPrograms);
+const ManagerPrograms = React.memo(translate()(_ManagerPrograms));
 export default ManagerPrograms;
