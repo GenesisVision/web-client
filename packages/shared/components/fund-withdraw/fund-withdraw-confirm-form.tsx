76eac348c4bd81078088baeecc6596f4b5e779af
pavelzubov <pasha224@yandex.ru>
2019-06-05 14:51:12 +0300

    Convert FundWithdrawConfirmForm to FC

--- packages/shared/components/fund-withdraw/fund-withdraw-confirm-form.tsx
+++ packages/shared/components/fund-withdraw/fund-withdraw-confirm-form.tsx
@@ -1,5 +1,4 @@
 import { InjectedFormikProps, withFormik } from "formik";
-import { ComponentType, PureComponent } from "react";
 import React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
@@ -24,62 +23,57 @@ interface IFundWithdrawConfirmFormProps
   extends InjectedTranslateProps,
     IFundWithdrawConfirmFormOwnProps {}
 
-class FundWithdrawConfirmForm extends PureComponent<
+const _FundWithdrawConfirmForm: React.FC<
   InjectedFormikProps<IFundWithdrawConfirmFormProps, {}>
-> {
-  render() {
-    const {
-      t,
-      availableToWithdraw,
-      percent,
-      currency,
-      exitFee,
-      isSubmitting,
-      errorMessage,
-      handleSubmit,
-      onBackClick
-    } = this.props;
+> = ({
+  t,
+  availableToWithdraw,
+  percent,
+  currency,
+  exitFee,
+  isSubmitting,
+  errorMessage,
+  handleSubmit,
+  onBackClick
+}) => (
+  <form id="withdraw-submit-form" onSubmit={handleSubmit}>
+    <div className="dialog-list__item">
+      {t("withdraw-fund.withdrawing")}
+      <span className="dialog-list__value">{formatValue(percent, 2)} %</span>
+    </div>
+    <FundWithdrawResult
+      availableToWithdraw={availableToWithdraw}
+      percent={percent}
+      currency={currency}
+      exitFee={exitFee}
+    />
+    <div className="form-error">
+      <FormError error={errorMessage} />
+    </div>
+    <div className="dialog__buttons">
+      <GVButton
+        onClick={onBackClick}
+        color="secondary"
+        variant="outlined"
+        title={t("buttons.back")}
+      >
+        {t("buttons.back")}
+      </GVButton>
+      <GVButton
+        type="submit"
+        id="fundWithdrawFormSubmit"
+        disabled={isSubmitting}
+      >
+        {t("buttons.confirm")}
+      </GVButton>
+    </div>
+  </form>
+);
 
-    return (
-      <form id="withdraw-submit-form" onSubmit={handleSubmit}>
-        <div className="dialog-list__item">
-          {t("withdraw-fund.withdrawing")}
-          <span className="dialog-list__value">
-            {formatValue(percent, 2)} %
-          </span>
-        </div>
-        <FundWithdrawResult
-          availableToWithdraw={availableToWithdraw}
-          percent={percent}
-          currency={currency}
-          exitFee={exitFee}
-        />
-        <div className="form-error">
-          <FormError error={errorMessage} />
-        </div>
-        <div className="dialog__buttons">
-          <GVButton
-            onClick={onBackClick}
-            color="secondary"
-            variant="outlined"
-            title={t("buttons.back")}
-          >
-            {t("buttons.back")}
-          </GVButton>
-          <GVButton
-            type="submit"
-            id="fundWithdrawFormSubmit"
-            disabled={isSubmitting}
-          >
-            {t("buttons.confirm")}
-          </GVButton>
-        </div>
-      </form>
-    );
-  }
-}
-
-export default compose<ComponentType<IFundWithdrawConfirmFormOwnProps>>(
+const FundWithdrawConfirmForm = compose<
+  React.ComponentType<IFundWithdrawConfirmFormOwnProps>
+>(
+  React.memo,
   translate(),
   withFormik<IFundWithdrawConfirmFormProps, {}>({
     displayName: "withdraw-form",
@@ -87,4 +81,5 @@ export default compose<ComponentType<IFundWithdrawConfirmFormOwnProps>>(
       props.onSubmit(setSubmitting);
     }
   })
-)(FundWithdrawConfirmForm);
+)(_FundWithdrawConfirmForm);
+export default FundWithdrawConfirmForm;
