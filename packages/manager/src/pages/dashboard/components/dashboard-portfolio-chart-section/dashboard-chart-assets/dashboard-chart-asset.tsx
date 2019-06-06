589cfc6176a0dba153230d0ad7316fd36350a706
pavelzubov <pasha224@yandex.ru>
2019-06-06 10:34:32 +0300

    Convert DashboardChartAsset to FC

--- packages/manager/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-chart-assets/dashboard-chart-asset.tsx
+++ packages/manager/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-chart-assets/dashboard-chart-asset.tsx
@@ -1,38 +1,39 @@
 import { ManagerSimpleFund, ManagerSimpleProgram } from "gv-api-web";
-import * as React from "react";
+import React, { useCallback } from "react";
 import { translate } from "react-i18next";
 import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
 import { ASSETS_TYPES } from "shared/components/table/components/filtering/asset-type-filter/asset-type-filter.constants";
 
-class _DashboardChartAsset extends React.PureComponent<Props> {
-  handleClick = () => {
-    const { chartAsset, type, selectAsset } = this.props;
-    selectAsset(chartAsset.id, chartAsset.title, type);
-  };
-  render() {
-    const { chartAsset } = this.props;
-    return (
-      <div
-        className="dashboard-chart-assets-popover__chart-asset"
-        onClick={this.handleClick}
-      >
-        <div className="dashboard-chart-assets-popover__logo">
-          <AssetAvatar
-            url={chartAsset.logo}
-            alt={chartAsset.title}
-            color={chartAsset.color}
-            level={"level" in chartAsset ? chartAsset.level : undefined}
-          />
-        </div>
-        <div className="dashboard-chart-assets-popover__info">
-          <div className="dashboard-chart-assets-popover__title">
-            {chartAsset.title}
-          </div>
+const _DashboardChartAsset: React.FC<Props> = ({
+  chartAsset,
+  type,
+  selectAsset
+}) => {
+  const handleClick = useCallback(
+    () => selectAsset(chartAsset.id, chartAsset.title, type),
+    [chartAsset, selectAsset, type]
+  );
+  return (
+    <div
+      className="dashboard-chart-assets-popover__chart-asset"
+      onClick={handleClick}
+    >
+      <div className="dashboard-chart-assets-popover__logo">
+        <AssetAvatar
+          url={chartAsset.logo}
+          alt={chartAsset.title}
+          color={chartAsset.color}
+          level={"level" in chartAsset ? chartAsset.level : undefined}
+        />
+      </div>
+      <div className="dashboard-chart-assets-popover__info">
+        <div className="dashboard-chart-assets-popover__title">
+          {chartAsset.title}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 interface Props {
   chartAsset: ManagerSimpleProgram | ManagerSimpleFund;
@@ -40,5 +41,5 @@ interface Props {
   selectAsset: (id: string, title: string, type: ASSETS_TYPES) => void;
 }
 
-const DashboardChartAsset = translate()(_DashboardChartAsset);
+const DashboardChartAsset = React.memo(translate()(_DashboardChartAsset));
 export default DashboardChartAsset;
