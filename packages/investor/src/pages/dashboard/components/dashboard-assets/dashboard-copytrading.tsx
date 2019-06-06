08d90f1caf6efdc43f6dc30b8d0318fd05f2ecf9
pavelzubov <pasha224@yandex.ru>
2019-06-06 09:57:18 +0300

    Convert DashboardCopytrading to FC

--- packages/investor/src/pages/dashboard/components/dashboard-assets/dashboard-copytrading.tsx
+++ packages/investor/src/pages/dashboard/components/dashboard-assets/dashboard-copytrading.tsx
@@ -5,6 +5,7 @@ import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import NumberFormat from "react-number-format";
 import { Link } from "react-router-dom";
+import { compose } from "redux";
 import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
 import {
   ACTION_STATUS_FILTER_NAME,
@@ -33,107 +34,104 @@ import { formatCurrencyValue } from "shared/utils/formatter";
 import { DASHBOARD_COPYTRADING_COLUMNS } from "./dashboard-copytrading.constants";
 import { dashboardCopytradingTableSelector } from "./dashboard-copytrading.selectors";
 
-class _DashboardCopytrading extends React.PureComponent<Props> {
-  render() {
-    const { t, title, role } = this.props;
-    return (
-      <TableContainer
-        getItems={getDashboardCopytrading}
-        dataSelector={dashboardCopytradingTableSelector}
-        isFetchOnMount={true}
-        columns={DASHBOARD_COPYTRADING_COLUMNS}
-        renderFilters={(
-          updateFilter: UpdateFilterFunc,
-          filtering: FilteringType
-        ) => (
-          <>
-            <SelectFilter
-              name={ACTION_STATUS_FILTER_NAME}
-              label={t(`${role}.dashboard-page.actions-status-filter.label`)}
-              value={filtering[ACTION_STATUS_FILTER_NAME] as SelectFilterType}
-              values={ACTION_STATUS_FILTER_VALUES}
-              onChange={updateFilter}
-            />
-            <DateRangeFilter
-              name={DATE_RANGE_FILTER_NAME}
-              value={filtering[DATE_RANGE_FILTER_NAME]}
-              onChange={updateFilter}
-              startLabel={t("filters.date-range.program-start")}
-            />
-          </>
-        )}
-        renderHeader={(column: Column) =>
-          t(`investor.dashboard-page.copytrading-header.${column.name}`)
-        }
-        renderBodyRow={(signal: SignalDetails) => (
-          <TableRow>
-            <TableCell className="programs-table__cell dashboard-programs__cell--title">
-              <div className="dashboard-programs__cell--avatar-title">
-                <Link
-                  to={{
-                    pathname: composeProgramDetailsUrl(signal.url),
-                    state: `/ ${title}`
-                  }}
-                >
-                  <AssetAvatar
-                    url={signal.logo}
-                    alt={signal.title}
-                    color={signal.color}
-                  />
-                </Link>
-                <Link
-                  to={{
-                    pathname: composeProgramDetailsUrl(signal.url),
-                    state: `/ ${title}`
-                  }}
-                >
-                  <GVButton variant="text" color="secondary">
-                    {signal.title}
-                  </GVButton>
-                </Link>
-              </div>
-            </TableCell>
-            <TableCell>{signal.currency}</TableCell>
-            <TableCell>{signal.personalDetails.tradesCount}</TableCell>
-            <TableCell>
-              {moment(signal.personalDetails.subscriptionDate).format()}
-            </TableCell>
-            <TableCell>
-              <Profitability
-                value={formatCurrencyValue(
-                  signal.statistic.profitValue,
-                  signal.currency
-                )}
-                prefix={PROFITABILITY_PREFIX.SIGN}
-              >
-                <NumberFormat
-                  value={formatCurrencyValue(
-                    signal.statistic.profitValue,
-                    signal.currency
-                  )}
-                  thousandSeparator=" "
-                  displayType="text"
-                  allowNegative={false}
-                  suffix={` ${signal.currency}`}
-                />
-              </Profitability>
-            </TableCell>
-            <TableCell className="programs-table__cell dashboard-programs__cell--chart">
-              {signal.chart.length && (
-                <ProgramSimpleChart data={signal.chart} programId={signal.id} />
+const _DashboardCopytrading: React.FC<Props> = ({ t, title, role }) => (
+  <TableContainer
+    getItems={getDashboardCopytrading}
+    dataSelector={dashboardCopytradingTableSelector}
+    isFetchOnMount={true}
+    columns={DASHBOARD_COPYTRADING_COLUMNS}
+    renderFilters={(
+      updateFilter: UpdateFilterFunc,
+      filtering: FilteringType
+    ) => (
+      <>
+        <SelectFilter
+          name={ACTION_STATUS_FILTER_NAME}
+          label={t(`${role}.dashboard-page.actions-status-filter.label`)}
+          value={filtering[ACTION_STATUS_FILTER_NAME] as SelectFilterType}
+          values={ACTION_STATUS_FILTER_VALUES}
+          onChange={updateFilter}
+        />
+        <DateRangeFilter
+          name={DATE_RANGE_FILTER_NAME}
+          value={filtering[DATE_RANGE_FILTER_NAME]}
+          onChange={updateFilter}
+          startLabel={t("filters.date-range.program-start")}
+        />
+      </>
+    )}
+    renderHeader={(column: Column) =>
+      t(`investor.dashboard-page.copytrading-header.${column.name}`)
+    }
+    renderBodyRow={(signal: SignalDetails) => (
+      <TableRow>
+        <TableCell className="programs-table__cell dashboard-programs__cell--title">
+          <div className="dashboard-programs__cell--avatar-title">
+            <Link
+              to={{
+                pathname: composeProgramDetailsUrl(signal.url),
+                state: `/ ${title}`
+              }}
+            >
+              <AssetAvatar
+                url={signal.logo}
+                alt={signal.title}
+                color={signal.color}
+              />
+            </Link>
+            <Link
+              to={{
+                pathname: composeProgramDetailsUrl(signal.url),
+                state: `/ ${title}`
+              }}
+            >
+              <GVButton variant="text" color="secondary">
+                {signal.title}
+              </GVButton>
+            </Link>
+          </div>
+        </TableCell>
+        <TableCell>{signal.currency}</TableCell>
+        <TableCell>{signal.personalDetails.tradesCount}</TableCell>
+        <TableCell>
+          {moment(signal.personalDetails.subscriptionDate).format()}
+        </TableCell>
+        <TableCell>
+          <Profitability
+            value={formatCurrencyValue(
+              signal.statistic.profitValue,
+              signal.currency
+            )}
+            prefix={PROFITABILITY_PREFIX.SIGN}
+          >
+            <NumberFormat
+              value={formatCurrencyValue(
+                signal.statistic.profitValue,
+                signal.currency
               )}
-            </TableCell>
-            <TableCell>{signal.status}</TableCell>
-          </TableRow>
-        )}
-      />
-    );
-  }
-}
-
-const DashboardCopytrading = withRole<OwnProps>(
-  translate()(_DashboardCopytrading)
+              thousandSeparator=" "
+              displayType="text"
+              allowNegative={false}
+              suffix={` ${signal.currency}`}
+            />
+          </Profitability>
+        </TableCell>
+        <TableCell className="programs-table__cell dashboard-programs__cell--chart">
+          {signal.chart.length && (
+            <ProgramSimpleChart data={signal.chart} programId={signal.id} />
+          )}
+        </TableCell>
+        <TableCell>{signal.status}</TableCell>
+      </TableRow>
+    )}
+  />
 );
+
+const DashboardCopytrading = compose<React.ComponentType<OwnProps>>(
+  React.memo,
+  withRole,
+  translate()
+)(_DashboardCopytrading);
 export default DashboardCopytrading;
 
 interface Props extends WithRoleProps, InjectedTranslateProps, OwnProps {}
