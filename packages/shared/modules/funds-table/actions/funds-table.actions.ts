404166fa04e5090a9ddbafb8f824f6c29f1ce322
pavelzubov <pasha224@yandex.ru>
2019-06-06 10:26:45 +0300

    Change fetchFunds to fetchFundsAction

--- packages/shared/modules/funds-table/actions/funds-table.actions.ts
+++ packages/shared/modules/funds-table/actions/funds-table.actions.ts
@@ -1,9 +1,9 @@
-import fundsApi from "shared/services/api-client/funds-api";
 import { ComposeFiltersAllType } from "shared/components/table/components/filtering/filter.type";
+import fundsApi from "shared/services/api-client/funds-api";
 
 export const FUNDS_TABLE = "FUNDS_TABLE";
 
-export const fetchFunds = (filters: ComposeFiltersAllType) => {
+export const fetchFundsAction = (filters: ComposeFiltersAllType) => {
   return {
     type: FUNDS_TABLE,
     payload: fundsApi.v10FundsGet(filters)
