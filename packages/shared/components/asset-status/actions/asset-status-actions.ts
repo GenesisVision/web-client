e0260281ffabb8e69b37991d521c7a70cde2082f
pavelzubov <pasha224@yandex.ru>
2019-06-07 14:59:10 +0300

    Reformat

--- packages/shared/components/asset-status/actions/asset-status-actions.ts
+++ packages/shared/components/asset-status/actions/asset-status-actions.ts
@@ -17,38 +17,30 @@ export interface IFetchInRequests<T = any> {
   (auth: string, skip: number, take: number): ApiAction<T>;
 }
 
-export const cancelInvestorProgramRequest: ICancelRequest = (id, auth) => {
-  return {
-    type: CANCEL_INVESTOR_PROGRAM_REQUESTS,
-    payload: investorApi.v10InvestorProgramsRequestsByIdCancelPost(id, auth)
-  };
-};
+export const cancelInvestorProgramRequest: ICancelRequest = (id, auth) => ({
+  type: CANCEL_INVESTOR_PROGRAM_REQUESTS,
+  payload: investorApi.v10InvestorProgramsRequestsByIdCancelPost(id, auth)
+});
 
-export const cancelManagerProgramRequest: ICancelRequest = (id, auth) => {
-  return {
-    type: CANCEL_MANAGER_PROGRAM_REQUESTS,
-    payload: managerApi.v10ManagerProgramsRequestsByIdCancelPost(id, auth)
-  };
-};
+export const cancelManagerProgramRequest: ICancelRequest = (id, auth) => ({
+  type: CANCEL_MANAGER_PROGRAM_REQUESTS,
+  payload: managerApi.v10ManagerProgramsRequestsByIdCancelPost(id, auth)
+});
 
 export const fetchInRequestsInvestor: IFetchInRequests<ProgramRequests> = (
   auth,
   skip,
   take
-) => {
-  return {
-    type: DASHBOARD_IN_REQUESTS,
-    payload: investorApi.v10InvestorRequestsBySkipByTakeGet(skip, take, auth)
-  };
-};
+) => ({
+  type: DASHBOARD_IN_REQUESTS,
+  payload: investorApi.v10InvestorRequestsBySkipByTakeGet(skip, take, auth)
+});
 
 export const fetchInRequestsManager: IFetchInRequests<ProgramRequests> = (
   auth,
   skip,
   take
-) => {
-  return {
-    type: DASHBOARD_IN_REQUESTS,
-    payload: managerApi.v10ManagerRequestsBySkipByTakeGet(skip, take, auth)
-  };
-};
+) => ({
+  type: DASHBOARD_IN_REQUESTS,
+  payload: managerApi.v10ManagerRequestsBySkipByTakeGet(skip, take, auth)
+});
