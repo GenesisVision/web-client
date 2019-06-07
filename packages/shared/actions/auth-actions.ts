98eb3e8b7c4f6c11e57e6c44de232f36e63c2c6a
pavelzubov <pasha224@yandex.ru>
2019-06-07 10:29:57 +0300

    Remove unused userName Declare type Change field to payload

--- packages/shared/actions/auth-actions.ts
+++ packages/shared/actions/auth-actions.ts
@@ -1,21 +1,22 @@
 import { HOME_ROUTE } from "shared/routes/app.routes";
 import authService from "shared/services/auth-service";
 import history from "shared/utils/history";
+import { ActionType } from "shared/utils/types";
 
 export const UPDATE_TOKEN = "UPDATE_TOKEN";
 export const LOGOUT = "LOGOUT";
+export type UpdateTokenActionType = ActionType<boolean>;
 
 const alreadyAuthenticated = () => {
   history.push(HOME_ROUTE);
 };
 
-const updateToken = () => ({
+const updateToken = (): UpdateTokenActionType => ({
   type: UPDATE_TOKEN,
-  isAuthenticated: authService.isAuthenticated(),
-  userName: authService.getUserName()
+  payload: authService.isAuthenticated()
 });
 
-const logout = () => ({
+const logout = (): ActionType => ({
   type: LOGOUT
 });
 
