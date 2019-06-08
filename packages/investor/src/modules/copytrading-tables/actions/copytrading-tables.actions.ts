4119d45d8beaff87ac2875ef5de2c4aaabde87f7
pavelzubov <pasha224@yandex.ru>
2019-06-08 12:36:05 +0300

    Add return type to copytrading actions

--- packages/investor/src/modules/copytrading-tables/actions/copytrading-tables.actions.ts
+++ packages/investor/src/modules/copytrading-tables/actions/copytrading-tables.actions.ts
@@ -1,5 +1,7 @@
+import { TradesSignalViewModel } from "gv-api-web";
 import { ComposeFiltersAllType } from "shared/components/table/components/filtering/filter.type";
 import signalApi from "shared/services/api-client/signal-api";
+import { ApiAction } from "shared/utils/types";
 
 export const CLEAR_COPYTRADING_TABLE = "CLEAR_COPYTRADING_TABLE";
 export const COPYTRADING_OPEN_TRADES = "DASHBOARD_OPEN_TRADES";
@@ -9,19 +11,18 @@ export const clearCopytradingTable = () => ({
   type: CLEAR_COPYTRADING_TABLE
 });
 
-export const fetchCopytradingOpenTrades = (auth: string, filters: any) => {
-  return {
-    type: COPYTRADING_OPEN_TRADES,
-    payload: signalApi.v10SignalTradesOpenGet(auth, filters)
-  };
-};
+export const fetchCopytradingOpenTrades = (
+  auth: string,
+  filters: any
+): ApiAction<TradesSignalViewModel> => ({
+  type: COPYTRADING_OPEN_TRADES,
+  payload: signalApi.v10SignalTradesOpenGet(auth, filters)
+});
 
 export const fetchCopytradingTradesHistory = (
   auth: string,
   filters: ComposeFiltersAllType
-) => {
-  return {
-    type: COPYTRADING_TRADES_HISTORY,
-    payload: signalApi.v10SignalTradesGet(auth, filters)
-  };
-};
+): ApiAction<TradesSignalViewModel> => ({
+  type: COPYTRADING_TRADES_HISTORY,
+  payload: signalApi.v10SignalTradesGet(auth, filters)
+});
