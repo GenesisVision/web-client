aae95547409cb9335abddd7dbeaf45e7ffd75be1
pavelzubov <pasha224@yandex.ru>
2019-06-08 12:55:41 +0300

    Set defaultReducer to twoFactorReducer

--- packages/shared/components/auth/login/reducers/two-factor.reducer.ts
+++ packages/shared/components/auth/login/reducers/two-factor.reducer.ts
@@ -1,5 +1,8 @@
-import { composeClearDataActionType } from "shared/actions/clear-data.factory";
-import { LOGIN_TWO_FACTOR, TStoreTwoFactorAction } from "shared/components/auth/login/login.actions";
+import {
+  LOGIN_TWO_FACTOR,
+  TStoreTwoFactorAction
+} from "shared/components/auth/login/login.actions";
+import defaultReducer from "shared/reducers/reducer-creators/default-reducer";
 import { HOME_ROUTE } from "shared/routes/app.routes";
 
 export const initialState = {
@@ -7,8 +10,6 @@ export const initialState = {
   password: "",
   from: { HOME_ROUTE }
 };
-
-const clearDataActionType = composeClearDataActionType(LOGIN_TWO_FACTOR);
 
 export type ITwoFactorState = Readonly<{
   email: string;
@@ -19,16 +20,12 @@ export type ITwoFactorState = Readonly<{
 const twoFactorReducer = (
   state: ITwoFactorState = initialState,
   action: TStoreTwoFactorAction
-): ITwoFactorState => {
-  switch (action.type) {
-    case LOGIN_TWO_FACTOR: {
-      return action.payload;
-    }
-    case clearDataActionType:
-      return initialState;
-    default:
-      return state;
-  }
-};
+): ITwoFactorState =>
+  defaultReducer<TStoreTwoFactorAction, ITwoFactorState>(
+    action,
+    state,
+    initialState,
+    LOGIN_TWO_FACTOR
+  );
 
 export default twoFactorReducer;
