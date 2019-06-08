6f0f781043f37e4929071bd982b94c1bca654b08
pavelzubov <pasha224@yandex.ru>
2019-06-08 09:35:50 +0300

    Set TDashboardChartAction to reducer

--- packages/manager/src/pages/dashboard/reducers/dashboard-asset-chart.reducer.ts
+++ packages/manager/src/pages/dashboard/reducers/dashboard-asset-chart.reducer.ts
@@ -1,13 +1,16 @@
-import { ActionType, Nullable } from "shared/utils/types";
+import { Nullable } from "shared/utils/types";
 
-import { DASHBOARD_ASSET_CHART } from "../actions/dashboard.actions";
+import {
+  DASHBOARD_ASSET_CHART,
+  TDashboardChartAction
+} from "../actions/dashboard.actions";
 import { IDashboardAssetChart } from "./dashboard.reducers";
 
 const initialState = null;
 
 const dashboardAssetChartReducer = (
   state: Nullable<IDashboardAssetChart> = initialState,
-  action: ActionType
+  action: TDashboardChartAction
 ): Nullable<IDashboardAssetChart> => {
   switch (action.type) {
     case DASHBOARD_ASSET_CHART:
