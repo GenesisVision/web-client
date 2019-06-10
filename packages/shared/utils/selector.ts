759e1c00422375243bf418ea8ba9f554eec6ad0c
pavelzubov <pasha224@yandex.ru>
2019-06-10 22:01:18 +0300

    Remove useless operator

--- packages/shared/utils/selector.ts
+++ packages/shared/utils/selector.ts
@@ -1,4 +1,4 @@
 import { IApiState } from "shared/reducers/reducer-creators/api-reducer";
 
 export const apiSelector = <T>(state: IApiState<T>): T | undefined =>
-  state.data ? state.data : undefined;
+  state.data;
