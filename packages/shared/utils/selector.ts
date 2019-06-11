edc946343c74c3751fb506c763da5658fd366ce3
pavelzubov <pasha224@yandex.ru>
2019-06-11 12:00:27 +0300

    Add memoize to apiSelector

--- packages/shared/utils/selector.ts
+++ packages/shared/utils/selector.ts
@@ -1,4 +1,7 @@
+import memoize from "fast-memoize";
 import { IApiState } from "shared/reducers/reducer-creators/api-reducer";
 
-export const apiSelector = <T>(state: IApiState<T>): T | undefined =>
-  state.data;
+export const apiSelector = () => {
+  const func = memoize((data: any) => data);
+  return <T>(state: IApiState<T>): T | undefined => func(state.data);
+};
