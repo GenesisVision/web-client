251e5590312d5a89c21a8c5ada54c62999e305f6
pavelzubov <pasha224@yandex.ru>
2019-06-11 14:53:40 +0300

    Change creating of apiSelector (add state selector inside)

--- packages/shared/utils/selector.ts
+++ packages/shared/utils/selector.ts
@@ -1,7 +1,10 @@
 import memoize from "fast-memoize";
 import { IApiState } from "shared/reducers/reducer-creators/api-reducer";
+import { RootState } from "shared/reducers/root-reducer";
 
-export const apiSelector = () => {
+export const apiSelector = <T>(
+  selector: (state: RootState) => IApiState<T>
+) => {
   const func = memoize((data: any) => data);
-  return <T>(state: IApiState<T>): T | undefined => func(state.data);
+  return (state: RootState): T | undefined => func(selector(state).data);
 };
