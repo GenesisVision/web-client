a53c02a23a10d7f166d582c7671932314bfbcd84
pavelzubov <pasha224@yandex.ru>
2019-06-12 16:40:23 +0300

    Add eventsSelector

--- packages/shared/reducers/platform-reducer.ts
+++ packages/shared/reducers/platform-reducer.ts
@@ -1,13 +1,17 @@
 import { PlatformInfo } from "gv-api-web";
+import { createSelector } from "reselect";
 import { PLATFORM_SETTINGS } from "shared/actions/platform-actions";
+import { SelectFilterValue } from "shared/components/table/components/filtering/filter.type";
 import apiReducerFactory, {
   IApiState
 } from "shared/reducers/reducer-creators/api-reducer";
+import { getUnique } from "shared/utils/array";
 import {
   apiFieldSelector,
   apiSelector,
   fieldSelector
 } from "shared/utils/selectors";
+import { AuthRootState } from "shared/utils/types";
 
 export type PlatformState = IApiState<PlatformInfo>;
 
@@ -31,6 +35,22 @@ export const programsInfoSelector = apiFieldSelector(
   platformDataSelector,
   fieldSelector(state => state.programsInfo),
   []
+);
+
+export const eventsSelector = createSelector<
+  AuthRootState,
+  PlatformInfo | undefined,
+  SelectFilterValue<string>[]
+>(
+  state => platformDataSelector(state),
+  data => {
+    if (!data) return [];
+    const { funds, programs } = data.enums.program.managerNotificationType;
+    return getUnique([...funds, ...programs]).map(event => ({
+      value: event,
+      labelKey: `dashboard-page.portfolio-events.types.${event}`
+    }));
+  }
 );
 
 const platformReducer = apiReducerFactory<PlatformInfo>({
