bdc45d1a4b001e413bf59d57d639a66b83bf1412
pavelzubov <pasha224@yandex.ru>
2019-06-12 16:42:08 +0300

    Set eventsSelector

--- packages/investor/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
+++ packages/investor/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
@@ -1,3 +1,4 @@
+import { ManagerRootState } from "manager-web-portal/src/reducers";
 import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { connect } from "react-redux";
@@ -7,6 +8,7 @@ import Page from "shared/components/page/page";
 import PortfolioEventsTableContainer from "shared/components/portfolio-events-table/portfolio-events-table-container";
 import { fetchPortfolioEvents } from "shared/components/programs/program-details/services/program-details.service";
 import withRole, { WithRoleProps } from "shared/decorators/with-role";
+import { eventsSelector } from "shared/reducers/platform-reducer";
 import { getUnique } from "shared/utils/array";
 
 export const PORTFOLIO_EVENTS_ALL_PAGE_ROUTE = "portfolio-events";
@@ -22,18 +24,9 @@ const _PortfolioEventsAllComponent: React.FC<Props> = ({ role, t, events }) => (
   </Page>
 );
 
-const mapStateToProps = (state: InvestorRootState): StateProps => {
-  if (!state.platformData.data) return { events: [] };
-  const {
-    funds,
-    programs
-  } = state.platformData.data.enums.program.investorNotificationType;
-  const events = getUnique([...funds, ...programs]).map(event => ({
-    value: event,
-    labelKey: `investor.dashboard-page.portfolio-events.types.${event}`
-  }));
-  return { events };
-};
+const mapStateToProps = (state: ManagerRootState): StateProps => ({
+  events: eventsSelector(state)
+});
 
 interface Props
   extends InjectedTranslateProps,
