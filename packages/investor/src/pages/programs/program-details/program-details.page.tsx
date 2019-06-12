67fd7ccf74399890f0c4514013f2f821a079591d
pavelzubov <pasha224@yandex.ru>
2019-06-12 16:42:45 +0300

    Reformat

--- packages/investor/src/pages/programs/program-details/program-details.page.tsx
+++ packages/investor/src/pages/programs/program-details/program-details.page.tsx
@@ -53,11 +53,9 @@ const eventsSelector = createSelector<
   }
 );
 
-const mapStateToProps = (state: InvestorRootState): StateProps => {
-  return {
-    events: eventsSelector(state)
-  };
-};
+const mapStateToProps = (state: InvestorRootState): StateProps => ({
+  events: eventsSelector(state)
+});
 
 interface StateProps {
   events: SelectFilterValue<string>[];
