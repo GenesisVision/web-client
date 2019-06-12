c64a54bdb5cdc37e8b5b8e6fc795050223a60aaf
pavelzubov <pasha224@yandex.ru>
2019-06-12 15:01:24 +0300

    Add headerSelector, isNewUserSelector, notificationsCountSelector, availableSelector, nameSelector, roleSelector

--- packages/shared/reducers/header-reducer.ts
+++ packages/shared/reducers/header-reducer.ts
@@ -3,8 +3,46 @@ import { PROFILE_HEADER } from "shared/components/header/header.constants";
 import apiReducerFactory, {
   IApiState
 } from "shared/reducers/reducer-creators/api-reducer";
+import {
+  apiFieldSelector,
+  apiSelector,
+  fieldSelector
+} from "shared/utils/selectors";
 
 export type HeaderState = IApiState<ProfileHeaderViewModel>;
+
+export const headerSelector = apiSelector<ProfileHeaderViewModel>(
+  state => state.profileHeader
+);
+
+export const isNewUserSelector = apiFieldSelector(
+  headerSelector,
+  fieldSelector(state => state.isNewUser)
+);
+
+export const notificationsCountSelector = apiFieldSelector(
+  headerSelector,
+  fieldSelector(state => state.notificationsCount),
+  0
+);
+
+export const availableSelector = apiFieldSelector(
+  headerSelector,
+  fieldSelector(state => state.available),
+  0
+);
+
+export const nameSelector = apiFieldSelector(
+  headerSelector,
+  fieldSelector(state => state.name),
+  ""
+);
+
+export const roleSelector = apiFieldSelector(
+  headerSelector,
+  fieldSelector(state => state.userType),
+  ""
+);
 
 const headerReducer = apiReducerFactory<ProfileHeaderViewModel>({
   apiType: PROFILE_HEADER
