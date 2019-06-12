04dbf682e45e31c4cb38b7f6b80944c7864dff6b
pavelzubov <pasha224@yandex.ru>
2019-06-12 14:58:51 +0300

    Add dashboardEventsSelector

--- packages/manager/src/pages/dashboard/reducers/dashboard-events.reducer.ts
+++ packages/manager/src/pages/dashboard/reducers/dashboard-events.reducer.ts
@@ -2,10 +2,18 @@ import { ManagerPortfolioEvents } from "gv-api-web";
 import apiReducerFactory, {
   IApiState
 } from "shared/reducers/reducer-creators/api-reducer";
+import { apiSelector } from "shared/utils/selectors";
+import { AuthRootState } from "shared/utils/types";
 
 import { DASHBOARD_PORTFOLIO_EVENTS } from "../actions/dashboard.actions";
 
 export type ManagerPortfolioEventsState = IApiState<ManagerPortfolioEvents>;
+
+export const dashboardEventsSelector = apiSelector<
+  ManagerPortfolioEvents,
+  AuthRootState
+>(state => state.dashboard.eventsData);
+
 const dashboardEventsReducer = apiReducerFactory<ManagerPortfolioEvents>({
   apiType: DASHBOARD_PORTFOLIO_EVENTS
 });
