3652f0104bb19761634f16cd2ba696916671143f
pavelzubov <pasha224@yandex.ru>
2019-06-12 15:01:48 +0300

    Add platformDataSelector, currenciesSelector, programTagsSelector, programsInfoSelector

--- packages/shared/reducers/platform-reducer.ts
+++ packages/shared/reducers/platform-reducer.ts
@@ -3,8 +3,35 @@ import { PLATFORM_SETTINGS } from "shared/actions/platform-actions";
 import apiReducerFactory, {
   IApiState
 } from "shared/reducers/reducer-creators/api-reducer";
+import {
+  apiFieldSelector,
+  apiSelector,
+  fieldSelector
+} from "shared/utils/selectors";
 
 export type PlatformState = IApiState<PlatformInfo>;
+
+export const platformDataSelector = apiSelector<PlatformInfo>(
+  state => state.platformData
+);
+
+export const currenciesSelector = apiFieldSelector(
+  platformDataSelector,
+  fieldSelector(state => state.currencies),
+  []
+);
+
+export const programTagsSelector = apiFieldSelector(
+  platformDataSelector,
+  fieldSelector(state => state.enums.program.programTags),
+  []
+);
+
+export const programsInfoSelector = apiFieldSelector(
+  platformDataSelector,
+  fieldSelector(state => state.programsInfo),
+  []
+);
 
 const platformReducer = apiReducerFactory<PlatformInfo>({
   apiType: PLATFORM_SETTINGS
