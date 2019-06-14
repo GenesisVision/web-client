b87efec2a7f870a5c920a2b20ff6179436391499
pavelzubov <pasha224@yandex.ru>
2019-06-14 19:32:03 +0300

    Remove useless handler and EventListener

--- packages/shared/components/dialog/dialog.tsx
+++ packages/shared/components/dialog/dialog.tsx
@@ -2,8 +2,6 @@ import "./dialog.scss";
 
 import classNames from "classnames";
 import * as React from "react";
-import { useCallback } from "react";
-import EventListener from "react-event-listener";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import GVButton from "shared/components/gv-button";
 import { CloseIcon } from "shared/components/icon/close-icon";
@@ -17,46 +15,39 @@ export const _Dialog: React.FC<IDialogProps & InjectedTranslateProps> = ({
   className,
   children,
   wider
-}) => {
-  const handleKeyPress = useCallback(
-    (event: KeyboardEvent) => event.keyCode === 27 && onClose(event),
-    [onClose]
-  );
-  return (
-    <Modal open={open} fixed disableBackdropClick>
-      <EventListener target={document} onKeyUp={handleKeyPress} />
-      <GVScroll autoHide>
-        <div className="dialog__content">
-          <div className="dialog__background" />
-          <div className="dialog__backdrop" onClick={onClose} />
+}) => (
+  <Modal open={open} fixed onClose={onClose}>
+    <GVScroll autoHide>
+      <div className="dialog__content">
+        <div className="dialog__background" />
+        <div className="dialog__backdrop" onClick={onClose} />
+        <GVButton
+          variant="text"
+          color="secondary"
+          className={classNames("dialog__close dialog__close--outside", {
+            "dialog__close--wider": wider
+          })}
+          onClick={onClose}
+        >
+          <>
+            <CloseIcon /> {t("buttons.close")}
+          </>
+        </GVButton>
+        <div className={classNames("dialog", className)}>
           <GVButton
             variant="text"
             color="secondary"
-            className={classNames("dialog__close dialog__close--outside", {
-              "dialog__close--wider": wider
-            })}
+            className="dialog__close dialog__close--inside"
             onClick={onClose}
           >
-            <>
-              <CloseIcon /> {t("buttons.close")}
-            </>
+            <CloseIcon />
           </GVButton>
-          <div className={classNames("dialog", className)}>
-            <GVButton
-              variant="text"
-              color="secondary"
-              className="dialog__close dialog__close--inside"
-              onClick={onClose}
-            >
-              <CloseIcon />
-            </GVButton>
-            {children}
-          </div>
+          {children}
         </div>
-      </GVScroll>
-    </Modal>
-  );
-};
+      </div>
+    </GVScroll>
+  </Modal>
+);
 
 const Dialog = translate()(React.memo(_Dialog));
 export default Dialog;
