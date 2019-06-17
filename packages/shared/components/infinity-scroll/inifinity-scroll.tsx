1249447c78427613b3fb68de8ad80bd7874209e6
Евгений Гребенщиков <evgreb@gmail.com>
2019-06-17 14:39:12 +0300

    fix infinity scroll and sidebars

--- packages/shared/components/infinity-scroll/inifinity-scroll.tsx
+++ packages/shared/components/infinity-scroll/inifinity-scroll.tsx
@@ -1,8 +1,36 @@
-import * as React from "react";
+import "./infinity-scroll.scss";
 
+import React, { createRef } from "react";
 class InfinityScroll extends React.PureComponent<Props> {
+  scroll: React.RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+  container: React.RefObject<HTMLDivElement> = createRef<HTMLDivElement>();
+
+  handleScroll = () => {
+    if (this.scroll.current && this.container.current) {
+      const scroll = this.scroll.current;
+      const scrollTop = scroll.scrollTop;
+      const clientHeight = window.innerHeight;
+      const scrollHeight = this.container.current.getBoundingClientRect()
+        .height;
+      const offsetBottom = scrollHeight - scrollTop - clientHeight;
+      if (offsetBottom < 100 && this.props.hasMore) {
+        this.props.loadMore();
+      }
+    }
+  };
+
   render() {
-    return this.props.children;
+    return (
+      <div
+        className="infinity-scroll"
+        ref={this.scroll}
+        onScroll={this.handleScroll}
+      >
+        <div className="infinity-scroll__container" ref={this.container}>
+          {this.props.children}
+        </div>
+      </div>
+    );
   }
 }
 
