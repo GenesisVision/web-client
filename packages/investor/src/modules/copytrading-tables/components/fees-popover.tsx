ba992f69014c0c2cbd774068e663a73b08746279
Евгений Гребенщиков <evgreb@gmail.com>
2019-06-20 13:04:16 +0300

    GV-928 merge

--- packages/investor/src/modules/copytrading-tables/components/fees-popover.tsx
+++ packages/investor/src/modules/copytrading-tables/components/fees-popover.tsx
@@ -3,7 +3,6 @@ import { ProviderFees } from "modules/copytrading-tables/components/provider-fee
 import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { compose } from "redux";
-import { GVScroll } from "shared/components/scroll/gvscroll";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
 import Tooltip from "shared/components/tooltip/tooltip";
 import { formatValue } from "shared/utils/formatter";
@@ -28,24 +27,18 @@ const _FeesPopover: React.FC<Props> = ({ trade, t }) => {
   return (
     <Tooltip
       render={() => (
-        <GVScroll autoHeight autoHeightMax="260px">
-          <div className="fees-popover">
-            <StatisticItem
-              label={t(`investor.copytrading-tables.fees.trading`)}
-            >
-              {`${trade.originalCommission} ${
-                trade.originalCommissionCurrency
-              }`}
-            </StatisticItem>
-            {providers.map(provider => (
-              <ProviderFees
-                isOnlyOne={isOnlyOne}
-                key={provider.programId}
-                provider={provider}
-              />
-            ))}
-          </div>
-        </GVScroll>
+        <div className="fees-popover">
+          <StatisticItem label={t(`investor.copytrading-tables.fees.trading`)}>
+            {`${trade.originalCommission} ${trade.originalCommissionCurrency}`}
+          </StatisticItem>
+          {providers.map(provider => (
+            <ProviderFees
+              isOnlyOne={isOnlyOne}
+              key={provider.programId}
+              provider={provider}
+            />
+          ))}
+        </div>
       )}
     >
       <div>{trade.totalCommission}</div>
