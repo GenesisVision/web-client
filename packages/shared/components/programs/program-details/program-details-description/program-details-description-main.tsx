087ff3fb98c435818e2834e23cce4835a36d01a3
pavelzubov <pasha224@yandex.ru>
2019-06-29 00:18:18 +0300

    Move markup to PerfomanceData

--- packages/shared/components/programs/program-details/program-details-description/program-details-description-main.tsx
+++ packages/shared/components/programs/program-details/program-details-description/program-details-description-main.tsx
@@ -9,18 +9,13 @@ import DetailsFavorite from "shared/components/details/details-description-secti
 import DetailsNotification from "shared/components/details/details-description-section/details-description/controls/details-notification";
 import DetailsSettingControl from "shared/components/details/details-description-section/details-description/controls/details-setting-control";
 import GVButton from "shared/components/gv-button";
-import PieContainerSmall from "shared/components/pie-container/pie-container-small";
 import Popover, {
+  anchorElType,
   HORIZONTAL_POPOVER_POS,
-  VERTICAL_POPOVER_POS,
-  anchorElType
+  VERTICAL_POPOVER_POS
 } from "shared/components/popover/popover";
-import ProgramPeriodPie from "shared/components/program-period/program-period-pie/program-period-pie";
 import SocialLinksBlock from "shared/components/social-links-block/social-links-block";
-import StatisticItem from "shared/components/statistic-item/statistic-item";
 import TagProgramItem from "shared/components/tag-program/tag-program-item";
-import { STATUS } from "shared/constants/constants";
-import filesService from "shared/services/file-service";
 import {
   composeManagerDetailsUrl,
   composeProgramNotificationsUrl,
@@ -29,6 +24,7 @@ import {
 
 import { IChangePasswordTradingAccountProps } from "../program-details.types";
 import InvestmentLimitsPopover from "./investment-limits-popover";
+import PerfomanceData from "./perfomance-data";
 
 interface IProgramDetailsDescriptionMainProps
   extends IIProgramDetailsDescriptionMainOwnProps,
@@ -60,7 +56,6 @@ class _ProgramDetailsDescriptionMain extends React.PureComponent<
       levelsParameters,
       t,
       programDescription,
-      ChangePasswordTradingAccount,
       isOwnProgram
     } = this.props;
     const personalDetails = programDescription.personalProgramDetails;
@@ -123,67 +118,10 @@ class _ProgramDetailsDescriptionMain extends React.PureComponent<
           <div className="program-details-description__text">
             {programDescription.description}
           </div>
-          <div className="program-details-description__perfomance-data">
-            <StatisticItem label={t("program-details-page.description.broker")}>
-              <img
-                className={"program-details-description__broker"}
-                src={filesService.getFileUrl(
-                  programDescription.brokerDetails.logo
-                )}
-              />
-            </StatisticItem>
-            <StatisticItem
-              label={t("program-details-page.description.leverage")}
-            >
-              {programDescription.leverageMin}:{programDescription.leverageMin}
-            </StatisticItem>
-            {programDescription.periodStarts && (
-              <StatisticItem
-                label={t("program-details-page.description.period")}
-              >
-                <ProgramPeriodPie
-                  condition={status !== STATUS.CLOSED}
-                  loader={t("program-period.program-closed")}
-                  start={programDescription.periodStarts}
-                  end={programDescription.periodEnds}
-                />
-              </StatisticItem>
-            )}
-            <StatisticItem label={t("program-details-page.description.age")}>
-              <PieContainerSmall
-                end={levelsParameters.programAgeMax}
-                value={programDescription.ageDays}
-                suffix={"days"}
-              />
-            </StatisticItem>
-            <StatisticItem
-              label={t("program-details-page.description.genesis-ratio")}
-            >
-              <PieContainerSmall
-                start={levelsParameters.genesisRatioMin}
-                end={levelsParameters.genesisRatioMax}
-                value={programDescription.genesisRatio}
-              />
-            </StatisticItem>
-            <StatisticItem
-              label={t("program-details-page.description.investment-scale")}
-            >
-              <PieContainerSmall
-                start={levelsParameters.investmentScaleMin}
-                end={levelsParameters.investmentScaleMax}
-                value={programDescription.investmentScale}
-              />
-            </StatisticItem>
-            <StatisticItem
-              label={t("program-details-page.description.volume-scale")}
-            >
-              <PieContainerSmall
-                start={levelsParameters.volumeScaleMin}
-                end={levelsParameters.volumeScaleMax}
-                value={programDescription.volumeScale}
-              />
-            </StatisticItem>
-          </div>
+          <PerfomanceData
+            levelsParameters={levelsParameters}
+            programDescription={programDescription}
+          />
         </div>
         <div className="program-details-description__settings">
           <DetailsFavorite
