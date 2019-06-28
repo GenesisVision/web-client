fa0f63a4bb14fa7aeafb0d53e64c35e54014a80d
nkrivous <kolsln@list.ru>
2019-06-28 11:17:11 +0300

    Update commissions in the signals tranasaction details

--- packages/shared/modules/transaction-details/transactions/signal-details.tsx
+++ packages/shared/modules/transaction-details/transactions/signal-details.tsx
@@ -1,15 +1,50 @@
+import { SignalFee } from "gv-api-web";
 import * as React from "react";
 import NumberFormat from "react-number-format";
+import { compose } from "redux";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
 import Status from "shared/components/status/status";
+import { ROLE } from "shared/constants/constants";
+import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
+import withRole, { WithRoleProps } from "shared/decorators/with-role";
 import { TransactionDetailsProps } from "shared/modules/transaction-details/transaction-details";
 import TransactionAsset from "shared/modules/transaction-details/transactions/transaction-asset";
 import { formatValue } from "shared/utils/formatter";
 
 import { TRANSACTIONS_DECIMAL_SCALE } from "./transactions.constants";
 
-const _SignalTransaction: React.FC<TransactionDetailsProps> = ({ data, t }) => {
+const SignalFees: React.ComponentType<
+  SignalFeesProps & WithLoaderProps
+> = withLoader(({ fees }) => {
+  return (
+    <>
+      {fees.map((x, idx) => (
+        <StatisticItem label={x.title} key={idx}>
+          <NumberFormat
+            value={formatValue(x.value, TRANSACTIONS_DECIMAL_SCALE)}
+            suffix={` ${x.currency}`}
+            displayType="text"
+          />
+        </StatisticItem>
+      ))}
+    </>
+  );
+});
+
+interface SignalFeesProps {
+  fees: SignalFee[];
+}
+
+const _SignalTransaction: React.FC<TransactionDetailsProps & WithRoleProps> = ({
+  t,
+  data,
+  role
+}) => {
   const details = data.programDetails;
+  const transactionDirectionLabel =
+    role === ROLE.INVESTOR
+      ? t("transactions-details.signal.to-signal-provider")
+      : t("transactions-details.signal.from-signal-provider");
   return (
     <>
       <div className="dialog__top">
@@ -17,24 +52,21 @@ const _SignalTransaction: React.FC<TransactionDetailsProps> = ({ data, t }) => {
           <h2>{t(`transactions-details.title`)}</h2>
           <p>{t(`transactions-details.signal.${data.type}`)}</p>
         </div>
-        <StatisticItem
-          condition={!!details}
-          label={t(
-            `transactions-details.investment.to-${
-              details ? details.programType : null
-            }`
-          )}
-        >
+        <StatisticItem condition={!!details} label={transactionDirectionLabel}>
           <TransactionAsset url={details ? details.logo : ""} data={details} />
         </StatisticItem>
       </div>
       <div className="dialog__bottom">
+        <SignalFees
+          condition={data.signalFees !== null}
+          fees={data.signalFees!}
+        />
         <StatisticItem label={t(`transactions-details.status.title`)}>
           <div className="external-transaction__status">
             {data.status} <Status status={data.status} />
           </div>
         </StatisticItem>
-        <StatisticItem label={t(`transactions-details.investment.amount`)} big>
+        <StatisticItem label={t(`transactions-details.signal.amount`)} big>
           <NumberFormat
             value={formatValue(data.amount, TRANSACTIONS_DECIMAL_SCALE)}
             suffix={data.currency}
@@ -45,7 +77,8 @@ const _SignalTransaction: React.FC<TransactionDetailsProps> = ({ data, t }) => {
     </>
   );
 };
-const SignalTransaction = React.memo<TransactionDetailsProps>(
-  _SignalTransaction
-);
+const SignalTransaction = compose<React.ComponentType<TransactionDetailsProps>>(
+  React.memo,
+  withRole
+)(_SignalTransaction);
 export default SignalTransaction;
