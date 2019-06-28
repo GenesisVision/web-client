43b9ae09abde489bbaad0249aae0e7902e33bd1d
pavelzubov <pasha224@yandex.ru>
2019-06-28 22:00:20 +0300

    Add redirectToProgram

--- packages/manager/src/pages/programs/programs-settings/services/program-settings.service.ts
+++ packages/manager/src/pages/programs/programs-settings/services/program-settings.service.ts
@@ -3,6 +3,15 @@ import { alertMessageActions } from "shared/modules/alert-message/actions/alert-
 import managerApi from "shared/services/api-client/manager-api";
 import authService from "shared/services/auth-service";
 import { ManagerThunk, ResponseError } from "shared/utils/types";
+import { push } from "connected-react-router";
+import { Dispatch } from "redux";
+import { RootState } from "shared/reducers/root-reducer";
+import getParams from "shared/utils/get-params";
+import {
+  PROGRAM_DETAILS_ROUTE,
+  PROGRAM_SLUG_URL_PARAM_NAME,
+  PROGRAMS_ROUTE
+} from "shared/routes/programs.routes";
 
 export const changeBrokerMethod = (
   programId: string,
@@ -24,3 +33,15 @@ export const changeBrokerMethod = (
     .catch((error: ResponseError) => {
       dispatch(alertMessageActions.error(error.errorMessage));
     });
+
+export const redirectToProgram = () => (
+  dispatch: Dispatch,
+  getState: () => RootState
+) => {
+  const { router } = getState();
+  const programSlugUrl = getParams(
+    router.location.pathname,
+    PROGRAM_DETAILS_ROUTE
+  )[PROGRAM_SLUG_URL_PARAM_NAME];
+  dispatch(push(`${PROGRAMS_ROUTE}/${programSlugUrl}`));
+};
