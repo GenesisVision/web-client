b372545f94d627ea62c8f9c5f3a55f702a7b46b9
pavelzubov <pasha224@yandex.ru>
2019-06-29 17:07:37 +0300

    Add formik

--- packages/manager/src/pages/programs/programs-settings/broker-edit.tsx
+++ packages/manager/src/pages/programs/programs-settings/broker-edit.tsx
@@ -7,45 +7,58 @@ import { compose } from "redux";
 import GVButton from "shared/components/gv-button";
 import Select from "shared/components/select/select";
 
-import { BROKER_CARD_EXTRA_STATE } from "../../create-program/components/create-program-broker/broker-card/broker-card.constants";
+import { BROKER_CARD_EXTRA_STATE } from "pages/create-program/components/create-program-broker/broker-card/broker-card.constants";
+import { FormikProps, withFormik } from "formik";
+import GVTextField from "shared/components/gv-text-field";
+import GVFormikField from "shared/components/gv-formik-field";
+import { SetSubmittingType } from "shared/utils/types";
 
 const _BrokerEdit: React.FC<Props> = ({
+  currentAccountTypeId,
+  handleSubmit,
+  values,
+  dirty,
+  isValid,
+  isSubmitting,
+  setFieldValue,
   t,
   id,
-  brokers,
-  selectedBroker,
-  changeBroker
+  brokers
 }) => {
-  const [selectedBrokerState, setSelectedBrokerState] = useState<Broker>(
-    selectedBroker
+  const [selectedBroker, setSelectedBroker] = useState<Broker>(
+    brokers.find(
+      broker =>
+        !!broker.accountTypes.find(
+          accountType => accountType.id === currentAccountTypeId
+        )
+    )!
   );
-  const [leverage, setLeverage] = useState<number>(1);
   const [account, setAccount] = useState<BrokerAccountType>(
-    selectedBroker.accountTypes[0]
+    selectedBroker.accountTypes.find(acc => acc.id === currentAccountTypeId)!
   );
   const selectBroker = useCallback(
     (brokerName: string) => () => {
-      const broker = brokers.find(x => x.name === brokerName);
-      setSelectedBrokerState(broker!);
-      setAccount(broker!.accountTypes[0]);
+      const broker = brokers.find(x => x.name === brokerName)!;
+      const firstAccount = broker.accountTypes[0];
+      setSelectedBroker(broker);
+      setAccount(firstAccount);
+      setFieldValue(FIELDS.brokerAccountTypeId, firstAccount.id);
+      setFieldValue(FIELDS.leverage, firstAccount.leverages[0]);
     },
     [brokers]
   );
-  const changeLeverage = useCallback(
-    ({ target }) => setLeverage(target.value),
-    []
-  );
   const changeAccount = useCallback(
-    ({ target }) =>
+    ({ target }) => {
       setAccount(
-        selectedBrokerState.accountTypes.find(
-          account => account.name === target.value
+        selectedBroker.accountTypes.find(
+          account => account.id === target.value
         )!
-      ),
-    [selectedBrokerState]
+      );
+    },
+    [selectedBroker]
   );
   return (
-    <>
+    <form id="change-broker-form" onSubmit={handleSubmit}>
       <h3>{t("manager.program-settings.broker.title")}</h3>
       <div className="program-edit__block-wrapper program-edit__block-wrapper--broker-list">
         {brokers.map(broker => (
@@ -53,79 +66,105 @@ const _BrokerEdit: React.FC<Props> = ({
             logo={broker.logo}
             key={broker.name}
             brokerName={broker.name}
-            isSelected={broker.name === selectedBrokerState.name}
+            isSelected={broker.name === selectedBroker.name}
             onSelect={selectBroker}
             cardState={BROKER_CARD_EXTRA_STATE.NONE}
           />
         ))}
       </div>
       <div className="gv-text-field__wrapper">
-        <label className="gv-text-field__label gv-text-field__label--shrink">
-          {t("manager.create-program-page.settings.fields.account-type")}
-        </label>
-        <div className="gv-text-field">
-          <Select
-            name={"accounts"}
-            className="gv-text-field__input"
-            value={account.name}
-            onChange={changeAccount}
-          >
-            {selectedBrokerState.accountTypes.map(account => (
-              <option value={account.name} key={account.name}>
-                {account.name}
-              </option>
-            ))}
-          </Select>
-        </div>
+        <GVFormikField
+          name={FIELDS.brokerAccountTypeId}
+          component={GVTextField}
+          label={t("manager.create-program-page.settings.fields.account-type")}
+          InputComponent={Select}
+          disableIfSingle
+          onChange={changeAccount}
+        >
+          {selectedBroker.accountTypes.map(account => (
+            <option value={account.id} key={account.id}>
+              {account.name}
+            </option>
+          ))}
+        </GVFormikField>
       </div>
-      <div className="gv-text-field__wrapper">
-        <label className="gv-text-field__label gv-text-field__label--shrink">
-          {t("manager.create-program-page.settings.fields.brokers-leverage")}
-        </label>
-        <div className="gv-text-field">
-          <Select
-            name={"leverage"}
-            className="gv-text-field__input"
-            value={String(leverage)}
-            onChange={changeLeverage}
+      {currentAccountTypeId !== values[FIELDS.brokerAccountTypeId] && (
+        <div className="gv-text-field__wrapper">
+          <GVFormikField
+            name={FIELDS.leverage}
+            component={GVTextField}
+            label={t(
+              "manager.create-program-page.settings.fields.brokers-leverage"
+            )}
+            InputComponent={Select}
+            disableIfSingle
+            className="create-program-settings__leverage"
           >
             {account.leverages.map(leverage => (
               <option value={leverage} key={leverage}>
                 {leverage}
               </option>
             ))}
-          </Select>
+          </GVFormikField>
         </div>
-      </div>
+      )}
       <p className="program-edit__text program-edit__text--color-accent program-edit__text--padding-top">
         {t("manager.program-settings.broker.text")}
       </p>
       <GVButton
+        type="submit"
         color="primary"
         className="invest-form__submit-button"
-        onClick={changeBroker(id, selectedBrokerState.accountTypes[0].id, 1)}
+        disabled={
+          !dirty || currentAccountTypeId === values[FIELDS.brokerAccountTypeId]
+        }
       >
         {t("manager.program-settings.buttons.change-broker")}
       </GVButton>
-    </>
+    </form>
   );
 };
 
-interface Props extends OwnProps, InjectedTranslateProps {}
+interface Props
+  extends OwnProps,
+    InjectedTranslateProps,
+    FormikProps<ChangeBrokerFormValues> {}
 
 interface OwnProps {
+  currentAccountTypeId: string;
+  onSubmit: (
+    values: ChangeBrokerFormValues,
+    setSubmitting: SetSubmittingType
+  ) => void;
   id: string;
-  selectedBroker: Broker;
   brokers: Broker[];
-  changeBroker: (
-    programId: string,
-    newBrokerAccountTypeId: string,
-    newLeverage: number
-  ) => () => void;
+}
+
+enum FIELDS {
+  brokerAccountTypeId = "brokerAccountTypeId",
+  leverage = "leverage"
+}
+
+export interface ChangeBrokerFormValues {
+  [FIELDS.brokerAccountTypeId]: string;
+  [FIELDS.leverage]: number;
 }
 
 const BrokerEdit = compose<React.ComponentType<OwnProps>>(
   translate(),
+  withFormik<OwnProps, ChangeBrokerFormValues>({
+    enableReinitialize: true,
+    displayName: "edit-form",
+    mapPropsToValues: props => {
+      return {
+        [FIELDS.brokerAccountTypeId]: props.currentAccountTypeId,
+        [FIELDS.leverage]: 1
+      };
+    },
+    handleSubmit: (values, { props, setSubmitting }) => {
+      props.onSubmit(values, setSubmitting);
+    }
+  }),
   React.memo
 )(_BrokerEdit);
 export default BrokerEdit;
