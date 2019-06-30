d39e20b7ad9fd1617daffad9cb653073d4340e4f
pavelzubov <pasha224@yandex.ru>
2019-06-30 13:13:42 +0300

    Change formatCurrencyValue to formatValue

--- packages/shared/components/programs/program-details/program-history/program-subscriptions-fees-tooltip.tsx
+++ packages/shared/components/programs/program-details/program-history/program-subscriptions-fees-tooltip.tsx
@@ -6,7 +6,7 @@ import { InjectedTranslateProps, translate } from "react-i18next";
 import FeeCommission from "shared/components/fee-commission/fee-commission";
 import { HORIZONTAL_POPOVER_POS } from "shared/components/popover/popover";
 import Tooltip from "shared/components/tooltip/tooltip";
-import { formatCurrencyValue } from "shared/utils/formatter";
+import { formatValue } from "shared/utils/formatter";
 
 const _FeesTooltip: React.FC<Props> = ({ t, subscription, children }) => {
   return (
@@ -18,30 +18,21 @@ const _FeesTooltip: React.FC<Props> = ({ t, subscription, children }) => {
           <div className="profile-menu__header">
             <FeeCommission
               title={t(`investor.copytrading-tables.fees.SuccessFee`)}
-              value={formatCurrencyValue(
-                subscription.totalSuccessFeeAmount,
-                subscription.totalSuccessFeeCurrency
-              )}
+              value={formatValue(subscription.totalSuccessFeeAmount, 8)}
               currency={subscription.totalSuccessFeeCurrency}
             />
             <FeeCommission
               title={t(
                 `investor.copytrading-tables.fees.ManagerSignalMasterVolumeFee`
               )}
-              value={formatCurrencyValue(
-                subscription.totalVolumeFeeAmount,
-                subscription.totalVolumeFeeCurrency
-              )}
+              value={formatValue(subscription.totalVolumeFeeAmount, 8)}
               currency={subscription.totalVolumeFeeCurrency}
             />
           </div>
           <div className={"fees-tooltip__footer "}>
             <FeeCommission
               title={t(`investor.copytrading-tables.fees.total`)}
-              value={formatCurrencyValue(
-                subscription.totalCommissionAmount,
-                subscription.totalCommissionCurrency
-              )}
+              value={formatValue(subscription.totalCommissionAmount, 8)}
               currency={subscription.totalCommissionCurrency}
             />
           </div>
