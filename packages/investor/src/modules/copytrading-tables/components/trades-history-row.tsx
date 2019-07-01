55a4de0d1f84f92d45a314a970e87487f79df6cb
pavelzubov <pasha224@yandex.ru>
2019-07-01 17:25:09 +0300

    Add ProvidersPopup

--- packages/investor/src/modules/copytrading-tables/components/trades-history-row.tsx
+++ packages/investor/src/modules/copytrading-tables/components/trades-history-row.tsx
@@ -1,23 +1,35 @@
-import { OrderProgramData, OrderSignalModel } from "gv-api-web";
-import { DECIMAL_SCALE } from "modules/copytrading-tables/components/copytrading-tables.constants";
+import {
+  OrderProgramData,
+  OrderSignalModel,
+  OrderSignalProgramInfo
+} from "gv-api-web";
+import {
+  DECIMAL_SCALE,
+  TRADES_HISTORY_PROVIDERS_COLUMNS
+} from "modules/copytrading-tables/components/copytrading-tables.constants";
 import moment from "moment";
 import * as React from "react";
+import { useState } from "react";
 import NumberFormat from "react-number-format";
 import { Link } from "react-router-dom";
 import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
-import Chip from "shared/components/chip/chip";
+import Count from "shared/components/avatar/count/count";
 import GVButton from "shared/components/gv-button";
 import BaseProfitability from "shared/components/profitability/base-profitability";
 import Profitability from "shared/components/profitability/profitability";
 import { PROFITABILITY_PREFIX } from "shared/components/profitability/profitability.helper";
 import TableCell from "shared/components/table/components/table-cell";
 import TableRow from "shared/components/table/components/table-row";
+import { UpdateRowFuncType } from "shared/components/table/components/table.types";
 import { composeProgramDetailsUrl } from "shared/utils/compose-url";
 import { formatValue } from "shared/utils/formatter";
 
+import ProvidersPopup from "./providers-popup";
+import TradeHistorySubRow from "./trade-history-sub-row";
 import TradesHistoryFeesTooltip from "./trades-history-fees-tooltip";
 
-const _TradesHistoryRow: React.FC<Props> = ({ trade, title }) => {
+const _TradesHistoryRow: React.FC<Props> = ({ trade, title, update }) => {
+  const [anchor, toggleSubrows] = useState<EventTarget | undefined>(undefined);
   const program =
     trade.providers.length > 0
       ? trade.providers[0].program
@@ -25,89 +37,108 @@ const _TradesHistoryRow: React.FC<Props> = ({ trade, title }) => {
   const otherPrograms = trade.providers;
   const hasOtherPrograms = otherPrograms.length > 1;
   return (
-    <TableRow className="details-trades__row">
-      <TableCell className="details-trades__cell program-details-trades__cell--entry traders-avatar">
-        {trade.providers.length > 0 ? (
-          <div className="dashboard-programs__cell--avatar-title">
-            <Link
-              to={{
-                pathname: composeProgramDetailsUrl(program.url),
-                state: `/ ${title}`
-              }}
-            >
-              <AssetAvatar
-                url={program.logo}
-                alt={program.title}
-                color={program.color}
-                level={program.level}
-                levelProgress={program.levelProgress}
-              />
-            </Link>
-            <Link
-              to={{
-                pathname: composeProgramDetailsUrl(program.url),
-                state: `/ ${title}`
-              }}
-            >
-              <GVButton variant={"text"} color={"secondary"}>
-                {program.title}
-              </GVButton>
-            </Link>
-            {hasOtherPrograms ? (
-              <Chip className={"traders-count"}>
-                +{otherPrograms.length - 1}
-              </Chip>
-            ) : null}
-          </div>
-        ) : null}
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--ticket">
-        {moment(trade.date).format()}
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--direction">
-        <BaseProfitability
-          isPositive={trade.direction === "Buy"}
-          isNegative={trade.direction === "Sell"}
-        >
-          {`${trade.direction}`}
-        </BaseProfitability>
-        {` / ${trade.entry}`}
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--symbol">
-        {trade.symbol}
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--volume">
-        <NumberFormat
-          value={formatValue(trade.volume, DECIMAL_SCALE / 2)}
-          displayType="text"
-          thousandSeparator=" "
-        />
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--price">
-        <NumberFormat
-          value={formatValue(trade.price, DECIMAL_SCALE)}
-          displayType="text"
-          thousandSeparator=" "
-        />
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--profit">
-        <Profitability
-          value={formatValue(trade.profit, DECIMAL_SCALE)}
-          prefix={PROFITABILITY_PREFIX.SIGN}
-        >
+    <>
+      <TableRow className="details-trades__row">
+        <TableCell className="details-trades__cell program-details-trades__cell--entry traders-avatar">
+          {trade.providers.length > 0 ? (
+            <div className="dashboard-programs__cell--avatar-title">
+              <Link
+                to={{
+                  pathname: composeProgramDetailsUrl(program.url),
+                  state: `/ ${title}`
+                }}
+              >
+                <AssetAvatar
+                  url={program.logo}
+                  alt={program.title}
+                  color={program.color}
+                  level={program.level}
+                  levelProgress={program.levelProgress}
+                />
+              </Link>
+              <Link
+                to={{
+                  pathname: composeProgramDetailsUrl(program.url),
+                  state: `/ ${title}`
+                }}
+              >
+                <GVButton variant={"text"} color={"secondary"}>
+                  {program.title}
+                </GVButton>
+              </Link>
+              {hasOtherPrograms && (
+                <Count
+                  count={otherPrograms.length - 1}
+                  onClick={event => {
+                    toggleSubrows(event.currentTarget);
+                  }}
+                />
+              )}
+            </div>
+          ) : null}
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--ticket">
+          {moment(trade.date).format()}
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--direction">
+          <BaseProfitability
+            isPositive={trade.direction === "Buy"}
+            isNegative={trade.direction === "Sell"}
+          >
+            {`${trade.direction}`}
+          </BaseProfitability>
+          {` / ${trade.entry}`}
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--symbol">
+          {trade.symbol}
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--volume">
           <NumberFormat
-            value={formatValue(trade.profit, DECIMAL_SCALE)}
-            thousandSeparator=" "
-            allowNegative={false}
+            value={formatValue(trade.volume, DECIMAL_SCALE / 2)}
             displayType="text"
-            suffix={trade.profit !== 0 ? ` ${trade.currency}` : ""}
+            thousandSeparator=" "
           />
-        </Profitability>
-      </TableCell>
-      <TableCell className="details-trades__cell program-details-trades__cell--date">
-        <TradesHistoryFeesTooltip trade={trade} />
-      </TableCell>
-    </TableRow>
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--price">
+          <NumberFormat
+            value={formatValue(trade.price, DECIMAL_SCALE)}
+            displayType="text"
+            thousandSeparator=" "
+          />
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--profit">
+          <Profitability
+            value={formatValue(trade.profit, DECIMAL_SCALE)}
+            prefix={PROFITABILITY_PREFIX.SIGN}
+          >
+            <NumberFormat
+              value={formatValue(trade.profit, DECIMAL_SCALE)}
+              thousandSeparator=" "
+              allowNegative={false}
+              displayType="text"
+              suffix={trade.profit !== 0 ? ` ${trade.currency}` : ""}
+            />
+          </Profitability>
+        </TableCell>
+        <TableCell className="details-trades__cell program-details-trades__cell--date">
+          <TradesHistoryFeesTooltip trade={trade} />
+        </TableCell>
+      </TableRow>
+      <ProvidersPopup
+        columns={TRADES_HISTORY_PROVIDERS_COLUMNS}
+        anchor={anchor}
+        onClose={() => toggleSubrows(undefined)}
+        otherPrograms={otherPrograms}
+        renderRow={(provider: OrderSignalProgramInfo) => (
+          <TradeHistorySubRow
+            title={title}
+            key={trade.id}
+            provider={provider}
+            symbol={trade.symbol}
+          />
+        )}
+      />
+    </>
   );
 };
 
@@ -117,4 +148,5 @@ export default TradesHistoryRow;
 interface Props {
   trade: OrderSignalModel;
   title: string;
+  update: UpdateRowFuncType;
 }
