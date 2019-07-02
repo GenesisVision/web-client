d45b5ee1c7d9a332de801e5636f4cf3a8046d7bd
pavelzubov <pasha224@yandex.ru>
2019-07-02 09:02:39 +0300

    Add mapStateToProps, mapDispatchToProps Add useEffect

--- packages/shared/components/auth/login/login/login.page.tsx
+++ packages/shared/components/auth/login/login/login.page.tsx
@@ -1,15 +1,33 @@
 import "shared/components/auth/login/login/login.scss";
 
+import { replace } from "connected-react-router";
 import { LocationState } from "history";
 import * as React from "react";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { Dispatch, bindActionCreators, compose } from "redux";
 import AuthTabs from "shared/components/auth/components/auth-tabs/auth-tabs";
 import CaptchaContainer from "shared/components/auth/login/captcha-container";
-import LoginForm from "shared/components/auth/login/login/login-form";
+import LoginForm, {
+  ILoginFormFormValues
+} from "shared/components/auth/login/login/login-form";
 import { HOME_ROUTE } from "shared/routes/app.routes";
 import { LOGIN_ROUTE } from "shared/routes/app.routes";
 
-const LoginPage: React.FC<Props> = ({ location }) => {
+import withRole, { WithRoleProps } from "../../../../decorators/with-role";
+import { isAuthenticatedSelector } from "../../../../reducers/auth-reducer";
+import { AuthRootState } from "../../../../utils/types";
+import { NOT_FOUND_PAGE_ROUTE } from "../../../not-found/not-found.routes";
+import {
+  LoginFuncType,
+  LoginService,
+  clearLoginDataFuncType
+} from "../login.service";
+import * as loginService from "../login.service";
+
+const _LoginPage: React.FC<Props> = ({ location, service }) => {
   const from = (location.state && location.state.pathname) || HOME_ROUTE;
+  useEffect(() => service.clearLoginData, []);
   return (
     <div className="login">
       <AuthTabs authPartUrl={LOGIN_ROUTE} />
@@ -23,8 +41,52 @@ const LoginPage: React.FC<Props> = ({ location }) => {
   );
 };
 
-interface Props {
+const mapStateToProps = (state: AuthRootState): StateProps => {
+  const { errorMessage } = state.loginData.login;
+  const { email, password } = state.loginData.twoFactor;
+  return {
+    isAuthenticated: isAuthenticatedSelector(state),
+    errorMessage,
+    email,
+    password
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  service: bindActionCreators(
+    {
+      ...(loginService as LoginService),
+      showNotFoundPage: () => dispatch(replace(NOT_FOUND_PAGE_ROUTE))
+    },
+    dispatch
+  )
+});
+
+interface DispatchProps {
+  service: {
+    clearLoginData: clearLoginDataFuncType;
+    login: LoginFuncType;
+    showNotFoundPage: () => void;
+  };
+}
+
+interface StateProps extends ILoginFormFormValues {
+  isAuthenticated: boolean;
+  errorMessage: string;
+}
+
+interface OwnProps {
   location: LocationState;
 }
 
-export default React.memo(LoginPage);
+interface Props extends OwnProps, StateProps, DispatchProps, WithRoleProps {}
+
+const LoginPage = compose<React.ComponentType<OwnProps>>(
+  withRole,
+  connect<StateProps, DispatchProps, OwnProps, AuthRootState>(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  React.memo
+)(_LoginPage);
+export default LoginPage;
