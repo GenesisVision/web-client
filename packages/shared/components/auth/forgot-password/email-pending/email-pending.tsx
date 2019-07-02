705c8342ff50a96b99dbaa9ff35d348ef5964aee
pavelzubov <pasha224@yandex.ru>
2019-07-02 09:01:18 +0300

    Add email prop Change submit handle

--- packages/shared/components/auth/forgot-password/email-pending/email-pending.tsx
+++ packages/shared/components/auth/forgot-password/email-pending/email-pending.tsx
@@ -2,18 +2,17 @@ import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import GVButton from "shared/components/gv-button";
 
-const _EmailPending: React.FC<Props> = ({ onSubmit, t }) => (
-  <>
-    <div className="password-pending__resend">
-      <GVButton variant="text" onClick={onSubmit}>
-        {t("auth.password-restore.email-pending.email-resend-button-text")}
-      </GVButton>
-    </div>
-  </>
+const _EmailPending: React.FC<Props> = ({ onSubmit, t, email }) => (
+  <div className="password-pending__resend">
+    <GVButton variant="text" onClick={() => onSubmit({ email })}>
+      {t("auth.password-restore.email-pending.email-resend-button-text")}
+    </GVButton>
+  </div>
 );
 
 interface Props extends InjectedTranslateProps {
-  onSubmit: () => void;
+  onSubmit: (values: { email: string }) => void;
+  email: string;
 }
 
 const EmailPending = translate()(React.memo(_EmailPending));
