0ce60370bac2b97310dc6e5295341440a14dab03
pavelzubov <pasha224@yandex.ru>
2019-07-02 08:59:57 +0300

    Add CaptchaContainer

---
+++ packages/shared/components/auth/captcha-container.tsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+import { compose } from "redux";
+import { SetSubmittingType } from "shared/utils/types";
+
+import * as authService from "./auth.service";
+import { CaptchasType } from "./auth.service";
+import Pow from "./captcha/pow";
+
+class _CaptchaContainer extends React.PureComponent<Props, State> {
+  state = {
+    pow: undefined,
+    geeTest: undefined,
+    prefix: undefined,
+    setSubmitting: undefined,
+    isSubmit: false,
+    captchaType: "None",
+    id: "",
+    email: "",
+    values: undefined
+  };
+
+  componentDidUpdate(): void {
+    const { request } = this.props;
+    const {
+      isSubmit,
+      prefix = "",
+      id,
+      setSubmitting,
+      captchaType,
+      values = {}
+    } = this.state;
+    const captchaCheckResult = {
+      id,
+      pow: {
+        prefix
+      },
+      geeTest: {}
+    };
+    const sendRequest = () =>
+      request(
+        {
+          ...values,
+          captchaCheckResult
+        },
+        setSubmitting!
+      );
+    if (isSubmit) {
+      switch (captchaType) {
+        case "Pow":
+          if (prefix) {
+            sendRequest();
+            this.setState({
+              pow: undefined,
+              prefix: undefined,
+              isSubmit: false
+            });
+          }
+          break;
+        default:
+          sendRequest();
+          this.setState({ isSubmit: false });
+          break;
+      }
+    }
+  }
+
+  handlePow = (prefix: number) => {
+    this.setState({ prefix });
+  };
+
+  handleSubmit = (values?: TValues, setSubmitting?: SetSubmittingType) => {
+    authService.getCaptcha(values.email).then(res => {
+      this.setState({
+        ...res,
+        values,
+        setSubmitting,
+        isSubmit: true,
+        email: values.email
+      });
+    });
+  };
+
+  render() {
+    const { renderForm } = this.props;
+    const { pow, email } = this.state;
+    return (
+      <>
+        {renderForm(this.handleSubmit)}
+        {pow && <Pow {...pow} login={email} handleSuccess={this.handlePow} />}
+      </>
+    );
+  }
+}
+
+type TValues = any; //{ [keys: string]: any };
+
+interface State extends CaptchasType {
+  isSubmit: boolean;
+  captchaType: string;
+  setSubmitting?: SetSubmittingType;
+  id?: string;
+  prefix?: number;
+  email?: string;
+  values?: TValues;
+}
+
+interface OwnProps {
+  request: (values: TValues, setSubmitting: SetSubmittingType) => void;
+  renderForm: (
+    handle: (values?: TValues, setSubmitting?: SetSubmittingType) => void
+  ) => JSX.Element;
+}
+
+interface Props extends OwnProps {}
+
+const CaptchaContainer = compose<React.ComponentType<OwnProps>>()(
+  _CaptchaContainer
+);
+export default CaptchaContainer;
