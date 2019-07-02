a555ff421ad69680225f0cd434512d93c1ce2aeb
a.popova <bob.m@mail.ru>
2019-07-02 17:30:21 +0200

    GV-1114 updated values in the FundReallocateHistory component

--- packages/shared/components/funds/fund-details/fund-details-history-section/fund-reallocate-history/fund-reallocate-history.tsx
+++ packages/shared/components/funds/fund-details/fund-details-history-section/fund-reallocate-history/fund-reallocate-history.tsx
@@ -1,13 +1,16 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-history/structure.scss";
 
-import { FundAssetInfo, FundAssetsListInfo } from "gv-api-web";
+import { ReallocationsViewModel } from "gv-api-web";
 import moment from "moment";
 import * as React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import { FUND_ASSET_TYPE } from "shared/components/fund-asset/fund-asset";
 import FundAssetContainer from "shared/components/fund-asset/fund-asset-container";
 import { FUND_REALLOCATE_HISTORY_COLUMNS } from "shared/components/funds/fund-details/fund-details.constants";
-import { SortingColumn } from "shared/components/table/components/filtering/filter.type";
+import {
+  FilteringType,
+  SortingColumn
+} from "shared/components/table/components/filtering/filter.type";
 import TableCell from "shared/components/table/components/table-cell";
 import TableModule from "shared/components/table/components/table-module";
 import TableRow from "shared/components/table/components/table-row";
@@ -15,16 +18,6 @@ import { GetItemsFuncType } from "shared/components/table/components/table.types
 import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
 
 import FundStructureHeaderCell from "../fund-structure/fund-structure-header-cell";
-
-interface Props {
-  id: string;
-  fetchStructure(id: string): Promise<FundAssetsListInfo>;
-}
-
-interface State {
-  isPending: boolean;
-  data?: FundAssetsListInfo;
-}
 
 class _FundReallocateHistory extends React.PureComponent<
   Props & InjectedTranslateProps,
@@ -35,69 +28,28 @@ class _FundReallocateHistory extends React.PureComponent<
     data: undefined
   };
 
-  fetchFundStructure: GetItemsFuncType = () => {
+  fetchFundReallocate: GetItemsFuncType = () => {
     this.setState({ isPending: true });
-    const { id, fetchStructure } = this.props;
-    return fetchStructure(id).then(data => {
+    const { id, fetchFundReallocateHistory } = this.props;
+    return fetchFundReallocateHistory(id).then(data => {
       this.setState({ data, isPending: false });
-      return { items: data.assets, total: data.assets.length };
+      return { items: data.reallocations, total: data.total };
     });
   };
 
   componentDidMount() {
-    this.fetchFundStructure();
+    this.fetchFundReallocate();
   }
 
   render() {
     const { t } = this.props;
 
-    // if (!this.state.data) return null;
-    // const data = {
-    //   items: this.state.data.assets,
-    //   total: this.state.data.assets.length
-    // };
-
-    const { assets } = {
-      assets: [
-        {
-          currentAssets: [
-            {
-              icon: "15c50486-a4fb-4c19-be6a-194eaefb262d",
-              color: "#21b2ce",
-              name: "Waltonchain",
-              asset: "WTC",
-              percent: 78.0
-            },
-            {
-              icon: "b2dcb7e5-4119-4098-963d-bd5ff329b536",
-              color: "#83074c",
-              name: "Chainlink",
-              asset: "LINK",
-              percent: 10.0
-            },
-            {
-              icon: "fdd088bb-20dc-4710-ac97-8f0390fd9839",
-              color: "#2b3775",
-              name: "Baelf",
-              asset: "ELF",
-              percent: 10.0
-            },
-            {
-              icon: "39f2aca5-d9fa-4f1b-9fa3-db2591d97708",
-              color: "#7b0087",
-              name: "Quantstamp",
-              asset: "QSP",
-              percent: 2.0
-            }
-          ]
-        }
-      ]
-    };
-
+    if (!this.state.data) return null;
     const data = {
-      items: assets,
-      total: assets.length
+      items: this.state.data.reallocations,
+      total: this.state.data.total
     };
+
     return (
       <TableModule
         data={data}
@@ -105,24 +57,24 @@ class _FundReallocateHistory extends React.PureComponent<
           ...DEFAULT_PAGING,
           itemsOnPage: data.total
         }}
-        getItems={this.fetchFundStructure}
+        getItems={this.fetchFundReallocate}
         columns={FUND_REALLOCATE_HISTORY_COLUMNS}
         renderHeader={(column: SortingColumn) => {
           return <FundStructureHeaderCell column={column} />;
         }}
-        renderBodyRow={(item: FundAssetInfo) => (
+        renderBodyRow={(item: any) => (
           <TableRow className="details-structure__row">
             <TableCell className="details-structure__cell fund-details-structure__cell">
-              {moment(new Date()).format()}
+              {moment(item.date).format()}
             </TableCell>
             <TableCell className="details-structure__cell">
               <FundAssetContainer
                 //@ts-ignore
-                assets={item.currentAssets}
+                assets={item.parts}
                 type={FUND_ASSET_TYPE.SHORT}
                 size={3}
                 //@ts-ignore
-                length={item.currentAssets.length}
+                length={item.parts.length}
               />
             </TableCell>
           </TableRow>
@@ -135,3 +87,16 @@ class _FundReallocateHistory extends React.PureComponent<
 const FundReallocateHistory = translate()(_FundReallocateHistory);
 
 export default FundReallocateHistory;
+
+interface Props {
+  id: string;
+  fetchFundReallocateHistory(
+    fundId: string,
+    filters?: FilteringType
+  ): Promise<ReallocationsViewModel>;
+}
+
+interface State {
+  isPending: boolean;
+  data?: ReallocationsViewModel;
+}
