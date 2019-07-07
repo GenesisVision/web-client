1d9bfb4ebb4c1cc424376e662a3bc4361816e663
pavelzubov <pasha224@yandex.ru>
2019-07-07 20:54:47 +0300

    Convert to FC Add useIsOpen

--- packages/shared/components/general-notification/general-notification.tsx
+++ packages/shared/components/general-notification/general-notification.tsx
@@ -1,9 +1,52 @@
 import "./general-notification.scss";
 
 import { CancelablePromise } from "gv-api-web";
-import * as React from "react";
+import React, { useCallback } from "react";
 import GVSwitch from "shared/components/gv-selection/gv-switch";
+import useIsOpen from "shared/hooks/is-open.hook";
 import { IRemoveNotificationSettingProps } from "shared/modules/notification-settings/actions/notification-settings.actions";
+
+const _GeneralNotification: React.FC<Props> = ({
+  setting,
+  name,
+  label,
+  addNotification,
+  assetId,
+  removeNotification
+}) => {
+  const [isPending, setIsPending, setNotPending] = useIsOpen();
+  const handleSwitch = useCallback(
+    () => {
+      setIsPending();
+      const method = !!setting
+        ? removeNotification({
+            id: setting.id!, // TODO ask backend remove optional from id
+            assetId: assetId,
+            type: name
+          })
+        : addNotification({
+            type: name,
+            assetId: assetId
+          });
+      method.then(setNotPending).catch(setNotPending);
+    },
+    [name, assetId, setting]
+  );
+  return (
+    <span className="notification-setting">
+      <GVSwitch
+        touched={false}
+        className="notification-setting__switch"
+        name={name}
+        value={!!setting}
+        disabled={isPending}
+        color="primary"
+        onChange={handleSwitch}
+      />
+      <span className="notification-setting__label">{label}</span>
+    </span>
+  );
+};
 
 export type Setting = {
   id?: string;
@@ -14,7 +57,7 @@ export type Setting = {
   conditionAmount?: number;
 };
 
-interface IGeneralNotificationProps {
+interface Props {
   setting: Setting;
   name: string;
   label: string;
@@ -25,67 +68,5 @@ interface IGeneralNotificationProps {
   ): CancelablePromise<void>;
 }
 
-interface IGeneralNotificationState {
-  isPending: boolean;
-}
-
-class GeneralNotification extends React.PureComponent<
-  IGeneralNotificationProps,
-  IGeneralNotificationState
-> {
-  state = {
-    isPending: false
-  };
-
-  handleSwitch = () => {
-    const { setting } = this.props;
-    if (!Boolean(setting)) {
-      this.addNotification();
-    } else {
-      this.removeNotification();
-    }
-  };
-
-  addNotification = () => {
-    this.setState({ isPending: true });
-    this.props
-      .addNotification({
-        type: this.props.name,
-        assetId: this.props.assetId
-      })
-      .then(() => this.setState({ isPending: false }))
-      .catch(() => this.setState({ isPending: false }));
-  };
-
-  removeNotification = () => {
-    this.setState({ isPending: true });
-    this.props
-      .removeNotification({
-        id: this.props.setting.id!, // TODO ask backend remove optional from id
-        assetId: this.props.assetId,
-        type: this.props.name
-      })
-      .then(() => this.setState({ isPending: false }))
-      .catch(() => this.setState({ isPending: false }));
-  };
-
-  render() {
-    const { setting, name, label } = this.props;
-    return (
-      <span className="notification-setting">
-        <GVSwitch
-          touched={false}
-          className="notification-setting__switch"
-          name={name}
-          value={Boolean(setting)}
-          disabled={this.state.isPending}
-          color="primary"
-          onChange={this.handleSwitch}
-        />
-        <span className="notification-setting__label">{label}</span>
-      </span>
-    );
-  }
-}
-
+const GeneralNotification = React.memo(_GeneralNotification);
 export default GeneralNotification;
