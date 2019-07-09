0c50e2f7e2ad49a78adc773b81852d38946676b9
pavelzubov <pasha224@yandex.ru>
2019-07-09 11:17:27 +0300

    Convert to FC Use useErrorMessage

--- packages/manager/src/modules/confirm/confirm-container.tsx
+++ packages/manager/src/modules/confirm/confirm-container.tsx
@@ -1,83 +1,75 @@
 import { TwoFactorAuthenticator } from "gv-api-web";
-import * as React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Dialog from "shared/components/dialog/dialog";
+import useErrorMessage from "shared/hooks/error-message.hook";
 import GoogleAuthStepsContainer from "shared/modules/2fa/google-auth/google-auth-steps/google-auth-steps";
-import { ResponseError, SetSubmittingType } from "shared/utils/types";
+import { SetSubmittingType } from "shared/utils/types";
 
 import { IConfirmFormValues } from "./components/confirm-form";
 import * as service from "./services/confirm.services";
 
-class _ConfirmContainer extends React.PureComponent<Props, State> {
-  state = { serverError: "", data: undefined, twoFactorCode: undefined };
-  componentDidMount() {
-    const { programId } = this.props;
-    service.get2faInfo({ programId }).then((data: TwoFactorAuthenticator) => {
-      this.setState({ data });
-    });
-  }
+const _ConfirmContainer: React.FC<Props> = ({
+  onClose,
+  programId,
+  open,
+  onApply
+}) => {
+  const {
+    errorMessage,
+    setErrorMessage,
+    cleanErrorMessage
+  } = useErrorMessage();
+  const [data, setData] = useState<TwoFactorAuthenticator | undefined>(
+    undefined
+  );
+  useEffect(() => {
+    service.get2faInfo({ programId }).then(setData);
+  }, []);
+  const handleClose = useCallback(
+    () => {
+      cleanErrorMessage();
+      onClose();
+    },
+    [onClose]
+  );
+  const handleConfirm = useCallback(
+    (values: IConfirmFormValues, setSubmitting: SetSubmittingType) => {
+      service
+        .confirm({ ...values, programId })
+        .then(() => {
+          handleClose();
+          onApply && onApply();
+        })
+        .catch(setErrorMessage)
+        .then(() => setSubmitting(false));
+    },
+    [programId]
+  );
+  if (!data) return null;
+  return (
+    <Dialog className={"dialog--width-auto"} open={open} onClose={handleClose}>
+      <GoogleAuthStepsContainer
+        authenticatorUri={data.authenticatorUri}
+        sharedKey={data.sharedKey}
+        onSubmit={handleConfirm}
+        errorMessage={errorMessage}
+        enablePassword={false}
+      />
+    </Dialog>
+  );
+};
 
-  handleClose = () => {
-    const { onClose } = this.props;
-    this.setState({ serverError: "" });
-    onClose();
-  };
-
-  handleConfirm = (
-    values: IConfirmFormValues,
-    setSubmitting: SetSubmittingType
-  ) => {
-    const { programId, onApply } = this.props;
-    service
-      .confirm({ ...values, programId })
-      .then(() => {
-        this.handleClose();
-        if (onApply) {
-          onApply();
-        }
-      })
-      .catch((error: ResponseError) => {
-        this.setState({ serverError: error.errorMessage });
-        setSubmitting(false);
-      });
-  };
-  render() {
-    const { data, serverError } = this.state;
-    if (!data) return null;
-    const { authenticatorUri, sharedKey } = data;
-    return (
-      data && (
-        <Dialog
-          className={"dialog--width-auto"}
-          open={this.props.open}
-          onClose={this.handleClose}
-        >
-          <GoogleAuthStepsContainer
-            authenticatorUri={authenticatorUri}
-            sharedKey={sharedKey}
-            onSubmit={this.handleConfirm}
-            errorMessage={serverError}
-            enablePassword={false}
-          />
-        </Dialog>
-      )
-    );
-  }
+export interface IConfirmProgramProps {
+  programId: string;
 }
 
-interface State {
-  serverError: string;
-  data?: TwoFactorAuthenticator;
-  twoFactorCode?: string;
-}
-interface Props extends IConfirmProgramProps, OwnProps {}
 interface OwnProps {
   onApply: () => void;
   open: boolean;
   onClose: () => void;
 }
-export interface IConfirmProgramProps {
-  programId: string;
-}
+
+interface Props extends IConfirmProgramProps, OwnProps {}
 
 const ConfirmContainer = React.memo(_ConfirmContainer);
 export default ConfirmContainer;
