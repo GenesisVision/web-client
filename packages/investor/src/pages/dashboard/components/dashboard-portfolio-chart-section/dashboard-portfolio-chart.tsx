b1b69d1b636df3a16309ca5fa5d14af1f9e08975
pavelzubov <pasha224@yandex.ru>
2019-07-09 10:14:33 +0300

    Convert to FC Add withLoader

--- packages/investor/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-portfolio-chart.tsx
+++ packages/investor/src/pages/dashboard/components/dashboard-portfolio-chart-section/dashboard-portfolio-chart.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
+import { useCallback, useState } from "react";
 import {
   Area,
-  AreaChart,
   Bar,
   Cell,
   ComposedChart,
@@ -16,108 +16,94 @@ import {
   gradientOffset
 } from "shared/components/chart/chart-gradient/chart-gradient";
 import GVColors from "shared/components/gv-styles/gv-colors";
+import withLoader from "shared/decorators/with-loader";
 
 import { BAR_COLORS } from "./dashboard-chart.constants";
 import DashboardPortfolioTooltip from "./dashboard-portfoio-tooltip";
 
-class DashboardPortfolioChart extends React.PureComponent<Props, State> {
-  state = {
-    activeIndex: undefined
-  };
-
-  grOffset = () =>
-    gradientOffset(
-      this.props.balance.filter(x => x.value !== undefined).map(x => x.value)
-    );
-
-  handleBarMouseOver: RechartsFunction = (data, index) => {
-    this.setState({
-      activeIndex: index
-    });
-  };
-
-  render() {
-    const { balance, assets } = this.props;
-
-    if (balance.length === 0) return null;
-    const { activeIndex } = this.state;
-    const assetsCount = 10;
-
-    return (
-      <ResponsiveContainer>
-        <ComposedChart stackOffset="sign" data={assets} margin={{ top: 20 }}>
-          <defs>
-            <ChartGradient
-              offset={this.grOffset()}
-              name="dashboardPortfolioChartFill"
-              color={GVColors.$primaryColor}
-              startOpacity={0.05}
-              stopOpacity={0.2}
-            />
-          </defs>
-          {chartXAxis(
-            new Date(balance[0].date),
-            new Date(balance[balance.length - 1].date)
-          )}
-          {/*
+const _DashboardPortfolioChart: React.FC<Props> = ({ balance, assets }) => {
+  const [activeIndex, setActiveIndex] = useState<number | undefined>(undefined);
+  const assetsCount = 10;
+  const grOffset = gradientOffset(
+    balance.filter(x => x.value !== undefined).map(x => x.value)
+  );
+  const handleBarMouseOver: RechartsFunction = useCallback(
+    (data, index) => setActiveIndex(index),
+    []
+  );
+  return (
+    <ResponsiveContainer>
+      <ComposedChart stackOffset="sign" data={assets} margin={{ top: 20 }}>
+        <defs>
+          <ChartGradient
+            offset={grOffset}
+            name="dashboardPortfolioChartFill"
+            color={GVColors.$primaryColor}
+            startOpacity={0.05}
+            stopOpacity={0.2}
+          />
+        </defs>
+        {chartXAxis(
+          new Date(balance[0].date),
+          new Date(balance[balance.length - 1].date)
+        )}
+        {/*
         //@ts-ignore*/}
-          <YAxis
-            dataKey="balance"
+        <YAxis
+          dataKey="balance"
+          data={assets}
+          axisLine={false}
+          tick={{ fill: GVColors.$labelColor, fontSize: "12" }}
+          tickFormatter={x => +x.toFixed(2)}
+          unit="GVT"
+          width={50}
+        />
+        {/*
+        //@ts-ignore*/}
+        <Area
+          type="monotone"
+          dataKey="balance"
+          data={balance}
+          stroke={GVColors.$primaryColor}
+          fill={`url(#dashboardPortfolioChartFill)`}
+          connectNulls={true}
+          strokeWidth={2}
+          isAnimationActive={false}
+        />
+        <Tooltip cursor={false} content={DashboardPortfolioTooltip} />
+        {[...Array(assetsCount).keys()].map(idx => (
+          <Bar
+            dataKey={`asset${assetsCount - idx - 1}.value`}
             data={assets}
-            axisLine={false}
-            tick={{ fill: GVColors.$labelColor, fontSize: "12" }}
-            tickFormatter={x => +x.toFixed(2)}
-            unit="GVT"
-            width={50}
-          />
-          {/*
-        //@ts-ignore*/}
-          <Area
-            type="monotone"
-            dataKey="balance"
-            data={balance}
-            stroke={GVColors.$primaryColor}
-            fill={`url(#dashboardPortfolioChartFill)`}
-            connectNulls={true}
-            strokeWidth={2}
+            stackId="bars"
+            barSize={15}
+            onMouseOver={handleBarMouseOver}
+            key={idx}
             isAnimationActive={false}
-          />
-          <Tooltip cursor={false} content={DashboardPortfolioTooltip} />
-          {[...Array(assetsCount).keys()].map(idx => (
-            <Bar
-              dataKey={`asset${assetsCount - idx - 1}.value`}
-              data={assets}
-              stackId="bars"
-              barSize={15}
-              onMouseOver={this.handleBarMouseOver}
-              key={idx}
-              isAnimationActive={false}
-            >
-              {assets.map((entry: any, index: number) => (
-                <Cell
-                  fill={
-                    activeIndex === index
-                      ? BAR_COLORS[assetsCount - idx - 1]
-                      : GVColors.$labelColor
-                  }
-                  key={index}
-                />
-              ))}
-            </Bar>
-          ))}
-        </ComposedChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+          >
+            {assets.map((entry: any, index: number) => (
+              <Cell
+                fill={
+                  activeIndex === index
+                    ? BAR_COLORS[assetsCount - idx - 1]
+                    : GVColors.$labelColor
+                }
+                key={index}
+              />
+            ))}
+          </Bar>
+        ))}
+      </ComposedChart>
+    </ResponsiveContainer>
+  );
+};
 
 interface Props {
   balance: any[];
   assets: any;
 }
 
-interface State {
-  activeIndex?: number;
-}
-
+const DashboardPortfolioChart = withLoader(
+  React.memo(_DashboardPortfolioChart)
+);
 export default DashboardPortfolioChart;
