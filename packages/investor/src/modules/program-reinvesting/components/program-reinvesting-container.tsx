6a8c36c7a0aacd3e915b0e72526ad08c59a5186b
pavelzubov <pasha224@yandex.ru>
2019-07-09 10:58:51 +0300

    Convert to FC Use useIsOpen

--- packages/investor/src/modules/program-reinvesting/components/program-reinvesting-container.tsx
+++ packages/investor/src/modules/program-reinvesting/components/program-reinvesting-container.tsx
@@ -1,5 +1,5 @@
-import classnames from "classnames";
-import * as React from "react";
+import classNames from "classnames";
+import React from "react";
 import { InjectedTranslateProps, translate } from "react-i18next";
 import {
   IProgramDetailContext,
@@ -8,66 +8,56 @@ import {
 import GVSwitch from "shared/components/gv-selection/gv-switch";
 import { IProgramReinvestingContainerOwnProps } from "shared/components/programs/program-details/program-details.types";
 import { TooltipLabel } from "shared/components/tooltip-label/tooltip-label";
+import useIsOpen from "shared/hooks/is-open.hook";
 
 import { toggleReinvesting } from "../services/program-reinvesting.service";
 
-class _ProgramReinvestingContainer extends React.PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      isReinvesting: props.isReinvesting,
-      isPending: false
-    };
-  }
-  toggleReinvesting = (updateDetails: () => void) => (value: boolean) => {
-    const { programId } = this.props;
-    this.setState({ isPending: true, isReinvesting: value });
-    toggleReinvesting(programId, value)
-      .then(() => {
-        this.setState({ isPending: false });
-        updateDetails();
-      })
-      .catch(() => {
-        this.setState({ isPending: false, isReinvesting: !value });
-      });
+const _ProgramReinvestingContainer: React.FC<Props> = ({
+  t,
+  isReinvesting: propIsReinvesting,
+  programId
+}) => {
+  const [isPending, setIsPending, setNotIsPending] = useIsOpen();
+  const [isReinvesting, setIs, setNotIs, setIsReinvestingValue] = useIsOpen(
+    propIsReinvesting
+  );
+  const onReinvestingLabelClick = (updateDetails: () => void) => () => {
+    setIsPending();
+    setIsReinvestingValue(!isReinvesting);
+    toggleReinvesting(programId, !isReinvesting)
+      .then(updateDetails)
+      .catch(() => setIsReinvestingValue(isReinvesting))
+      .then(setNotIsPending); // TODO change to finally
   };
-
-  onReinvestingLabelClick = (updateDetails: () => void) => () =>
-    this.toggleReinvesting(updateDetails)(!this.state.isReinvesting);
-
-  render() {
-    const { t } = this.props;
-    const { isReinvesting, isPending } = this.state;
-    return (
-      <ProgramDetailContext.Consumer>
-        {({ updateDetails }: IProgramDetailContext) => (
-          <span
-            className={classnames("reinvesting-widget", {
-              "reinvesting-widget--active": isReinvesting
-            })}
-            onClick={this.onReinvestingLabelClick(updateDetails)}
-          >
-            <GVSwitch
-              name="reinvesting"
-              touched={false}
-              value={isReinvesting}
-              color="primary"
-              onChange={this.onReinvestingLabelClick(updateDetails)}
-              label={
-                <TooltipLabel
-                  tooltipContent={t("program-details-page.tooltip.reinvest")}
-                  labelText={t("program-details-page.description.reinvest")}
-                  className="tooltip__label--cursor-pointer"
-                />
-              }
-              disabled={isPending}
-            />
-          </span>
-        )}
-      </ProgramDetailContext.Consumer>
-    );
-  }
-}
+  return (
+    <ProgramDetailContext.Consumer>
+      {({ updateDetails }: IProgramDetailContext) => (
+        <span
+          className={classNames("reinvesting-widget", {
+            "reinvesting-widget--active": isReinvesting
+          })}
+          onClick={onReinvestingLabelClick(updateDetails)}
+        >
+          <GVSwitch
+            name="reinvesting"
+            touched={false}
+            value={isReinvesting}
+            color="primary"
+            onChange={onReinvestingLabelClick(updateDetails)}
+            label={
+              <TooltipLabel
+                tooltipContent={t("program-details-page.tooltip.reinvest")}
+                labelText={t("program-details-page.description.reinvest")}
+                className="tooltip__label--cursor-pointer"
+              />
+            }
+            disabled={isPending}
+          />
+        </span>
+      )}
+    </ProgramDetailContext.Consumer>
+  );
+};
 
 const ProgramReinvestingContainer = translate()(
   React.memo(_ProgramReinvestingContainer)
@@ -77,8 +67,3 @@ export default ProgramReinvestingContainer;
 interface Props
   extends IProgramReinvestingContainerOwnProps,
     InjectedTranslateProps {}
-
-interface State {
-  isReinvesting: boolean;
-  isPending: boolean;
-}
