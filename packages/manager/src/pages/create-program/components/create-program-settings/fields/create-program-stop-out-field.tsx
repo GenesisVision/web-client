7f8a1753d8ca6ccff6c07dd7cfc262d9551da3af
pavelzubov <pasha224@yandex.ru>
2019-07-10 11:53:16 +0300

    Add CreateProgramStopOutField

---
+++ packages/manager/src/pages/create-program/components/create-program-settings/fields/create-program-stop-out-field.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { InjectedTranslateProps, translate } from "react-i18next";
+import GVFormikField from "shared/components/gv-formik-field";
+import GVTextField from "shared/components/gv-text-field";
+import Hint from "shared/components/hint/hint";
+import { VERTICAL_POPOVER_POS } from "shared/components/popover/popover";
+
+const _CreateProgramStopOutField: React.FC<Props> = ({ name, t }) => (
+  <div className="create-program-settings__item">
+    <GVFormikField
+      name={name}
+      label={t("manager.create-program-page.settings.fields.stop-out-level")}
+      adornment="%"
+      component={GVTextField}
+      type="number"
+      autoComplete="off"
+      decimalScale={4}
+    />
+    <Hint
+      content={t("manager.create-program-page.settings.hints.stop-out-level")}
+      className="create-program-settings__item-caption"
+      vertical={VERTICAL_POPOVER_POS.BOTTOM}
+      tooltipContent={t(
+        "manager.create-program-page.settings.hints.stop-out-level-description"
+      )}
+    />
+  </div>
+);
+
+interface Props extends InjectedTranslateProps {
+  name: string;
+}
+
+const CreateProgramStopOutField = translate()(
+  React.memo(_CreateProgramStopOutField)
+);
+export default CreateProgramStopOutField;
