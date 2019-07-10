5e5456a75185942475b03fd1d4e382722ea722e4
pavelzubov <pasha224@yandex.ru>
2019-07-10 13:49:02 +0300

    Add current prop Add React.memo

--- packages/shared/components/programs/program-details/program-details-description/about-levels/level-icon.tsx
+++ packages/shared/components/programs/program-details/program-details-description/about-levels/level-icon.tsx
@@ -1,18 +1,24 @@
+import classNames from "classnames";
 import { LevelInfo } from "gv-api-web";
 import * as React from "react";
 
-const LevelIcon: React.FC<Props> = ({ levelInfo }) => {
-  return (
-    <div
-      className={`about-levels__icon about-levels__icon--${levelInfo.level}`}
-    >
-      {levelInfo.level}
-    </div>
-  );
-};
+const LevelIcon: React.FC<Props> = React.memo(({ levelInfo, current }) => (
+  <div
+    className={classNames(
+      "about-levels__icon",
+      `about-levels__icon--${levelInfo.level}`,
+      {
+        "about-levels__icon--current": current
+      }
+    )}
+  >
+    {levelInfo.level}
+  </div>
+));
 
 export default LevelIcon;
 
 interface Props {
   levelInfo: LevelInfo;
+  current?: boolean;
 }
