a53457d139dba6cb5a33f8ab2ea376c368418e95
pavelzubov <pasha224@yandex.ru>
2019-07-12 20:28:01 +0300

    Convert to FC

--- packages/shared/components/table/components/table.tsx
+++ packages/shared/components/table/components/table.tsx
@@ -2,7 +2,7 @@ import "./table.scss";
 import "./table-cards.scss";
 
 import classNames from "classnames";
-import * as React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TableBody, {
   ITableBodyExternalProps
 } from "shared/components/table/components/table-body";
@@ -21,8 +21,113 @@ import {
 } from "shared/components/table/table.constants";
 import { loadData, saveData } from "shared/utils/localstorage";
 
-import { RenderBodyItemFuncType } from "./table.types";
 import { FilteringType } from "./filtering/filter.type";
+import { RenderBodyItemFuncType } from "./table.types";
+
+const _Table: React.FC<ITableProps> = ({
+  exportButtonToolbarRender,
+  updateItems,
+  className,
+  disableTitle,
+  items,
+  title,
+  renderFilters,
+  updateFilter,
+  filtering,
+  columns,
+  sorting,
+  updateSorting,
+  renderHeader,
+  renderSorting,
+  createButtonToolbar,
+  renderBodyCard,
+  renderBodyRow,
+  paging,
+  updatePaging,
+  isPending,
+  emptyMessage,
+  updateRow,
+  showSwitchView
+}) => {
+  const [view, setView] = useState<LIST_VIEW>(LIST_VIEW.TABLE);
+  const isViewSwitchEnabled =
+    renderBodyRow !== undefined &&
+    renderBodyCard !== undefined &&
+    !!showSwitchView;
+  useEffect(() => {
+    if (isViewSwitchEnabled)
+      setView(loadData(PROGRAMS_VIEW) || LIST_VIEW.TABLE);
+  }, []);
+  const changeView = useCallback((view: LIST_VIEW) => {
+    saveData(PROGRAMS_VIEW, view);
+    setView(view);
+  }, []);
+  if (!items && emptyMessage) return emptyMessage;
+  const renderBodyItem =
+    view === LIST_VIEW.CARDS ? renderBodyCard : renderBodyRow;
+  return (
+    <div className="table-wrapper">
+      <TableToolbar
+        disableTitle={disableTitle}
+        title={title}
+        renderFilters={renderFilters}
+        updateFilter={updateFilter}
+        filtering={filtering}
+        onChange={changeView}
+        view={view}
+        columns={columns}
+        sorting={sorting}
+        updateSorting={updateSorting}
+        renderSorting={renderSorting}
+        isViewSwitchEnabled={isViewSwitchEnabled}
+        createButtonToolbar={createButtonToolbar}
+        exportButtonToolbar={
+          exportButtonToolbarRender && exportButtonToolbarRender(filtering)
+        }
+      />
+      <div className={"table__scroll"}>
+        {view === LIST_VIEW.CARDS && (
+          <div className={classNames("table", className)}>
+            <TableBody
+              isPending={isPending}
+              items={items}
+              className="table-cards"
+              tag="div"
+              view={LIST_VIEW.CARDS}
+              renderBodyItem={renderBodyItem!}
+            />
+          </div>
+        )}
+        {view === LIST_VIEW.TABLE && (
+          <table className={classNames("table", className)}>
+            <TableHeader
+              columns={columns}
+              sorting={sorting}
+              updateSorting={updateSorting}
+              renderHeader={renderHeader}
+            />
+            <TableBody
+              isPending={isPending}
+              items={items}
+              className="table__body"
+              tag="tbody"
+              view={LIST_VIEW.TABLE}
+              updateRow={updateRow}
+              updateItems={updateItems}
+              renderBodyItem={renderBodyItem!}
+            />
+          </table>
+        )}
+      </div>
+      <TableFooter
+        condition={paging && !!(paging.totalPages && paging.totalPages >= 2)}
+        paging={paging}
+        updatePaging={updatePaging}
+        isPending={isPending}
+      />
+    </div>
+  );
+};
 
 export interface ITableProps
   extends ITableFooterProps,
@@ -31,133 +136,10 @@ export interface ITableProps
     ITableHeaderProps {
   renderBodyCard?: RenderBodyItemFuncType;
   renderBodyRow?: RenderBodyItemFuncType;
-  emptyMessage?: JSX.Element | string;
+  emptyMessage?: JSX.Element;
   showSwitchView?: boolean;
   exportButtonToolbarRender?: (filtering?: FilteringType) => JSX.Element;
 }
 
-interface ITableState {
-  view: LIST_VIEW;
-}
-
-class Table extends React.PureComponent<ITableProps, ITableState> {
-  state = {
-    view: LIST_VIEW.TABLE
-  };
-
-  componentDidMount() {
-    if (this.isViewSwitchEnabled)
-      this.setState({
-        view: (loadData(PROGRAMS_VIEW) as LIST_VIEW) || LIST_VIEW.TABLE
-      });
-  }
-
-  changeView = (view: LIST_VIEW) => {
-    saveData(PROGRAMS_VIEW, view);
-    this.setState({ view });
-  };
-
-  isViewSwitchEnabled =
-    this.props.renderBodyRow !== undefined &&
-    this.props.renderBodyCard !== undefined &&
-    !!this.props.showSwitchView;
-
-  renderTrackVertical = (): JSX.Element => (
-    <span className="table__vertical-track" />
-  );
-
-  render() {
-    const { view } = this.state;
-    const {
-      exportButtonToolbarRender,
-      updateItems,
-      className,
-      disableTitle,
-      items,
-      title,
-      renderFilters,
-      updateFilter,
-      filtering,
-      columns,
-      sorting,
-      updateSorting,
-      renderHeader,
-      renderSorting,
-      createButtonToolbar,
-      renderBodyCard,
-      renderBodyRow,
-      paging,
-      updatePaging,
-      isPending,
-      emptyMessage,
-      updateRow
-    } = this.props;
-    if (!items && emptyMessage) return emptyMessage;
-    const renderBodyItem =
-      view === LIST_VIEW.CARDS ? renderBodyCard : renderBodyRow;
-    return (
-      <div className="table-wrapper">
-        <TableToolbar
-          disableTitle={disableTitle}
-          title={title}
-          renderFilters={renderFilters}
-          updateFilter={updateFilter}
-          filtering={filtering}
-          onChange={this.changeView}
-          view={view}
-          columns={columns}
-          sorting={sorting}
-          updateSorting={updateSorting}
-          renderSorting={renderSorting}
-          isViewSwitchEnabled={this.isViewSwitchEnabled}
-          createButtonToolbar={createButtonToolbar}
-          exportButtonToolbar={
-            exportButtonToolbarRender && exportButtonToolbarRender(filtering)
-          }
-        />
-        <div className={"table__scroll"}>
-          {view === LIST_VIEW.CARDS && (
-            <div className={classNames("table", className)}>
-              <TableBody
-                isPending={isPending}
-                items={items}
-                className="table-cards"
-                tag="div"
-                view={LIST_VIEW.CARDS}
-                renderBodyItem={renderBodyItem!}
-              />
-            </div>
-          )}
-          {view === LIST_VIEW.TABLE && (
-            <table className={classNames("table", className)}>
-              <TableHeader
-                columns={columns}
-                sorting={sorting}
-                updateSorting={updateSorting}
-                renderHeader={renderHeader}
-              />
-              <TableBody
-                isPending={isPending}
-                items={items}
-                className="table__body"
-                tag="tbody"
-                view={LIST_VIEW.TABLE}
-                updateRow={updateRow}
-                updateItems={updateItems}
-                renderBodyItem={renderBodyItem!}
-              />
-            </table>
-          )}
-        </div>
-        <TableFooter
-          condition={paging && !!(paging.totalPages && paging.totalPages >= 2)}
-          paging={paging}
-          updatePaging={updatePaging}
-          isPending={isPending}
-        />
-      </div>
-    );
-  }
-}
-
+const Table = React.memo(_Table);
 export default Table;
