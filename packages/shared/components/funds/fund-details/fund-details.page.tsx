16913a21110887d4adb2dc798d1ef11716acee2b
pavelzubov <pasha224@yandex.ru>
2019-07-13 08:42:49 +0300

    Convert to FC

--- packages/shared/components/funds/fund-details/fund-details.page.tsx
+++ packages/shared/components/funds/fund-details/fund-details.page.tsx
@@ -1,7 +1,8 @@
 import "shared/components/details/details.scss";
 
-import { FundBalanceChart, FundDetailsFull } from "gv-api-web";
+import { FundDetailsFull } from "gv-api-web";
 import * as React from "react";
+import { useCallback, useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Dispatch, bindActionCreators, compose } from "redux";
 import { redirectToLogin } from "shared/components/auth/signin/signin.service";
@@ -12,100 +13,59 @@ import {
 } from "shared/components/funds/fund-details/services/fund-details.service";
 import NotFoundPage from "shared/components/not-found/not-found";
 import { IHistorySection } from "shared/components/programs/program-details/program-details.types";
+import useErrorMessage from "shared/hooks/error-message.hook";
 import { currencySelector } from "shared/reducers/account-settings-reducer";
 import { isAuthenticatedSelector } from "shared/reducers/auth-reducer";
 import { RootState } from "shared/reducers/root-reducer";
-import { CurrencyEnum, ResponseError } from "shared/utils/types";
+import { CurrencyEnum } from "shared/utils/types";
 
 import FundDetailsContainer from "./fund-details.container";
 import { IDescriptionSection } from "./fund-details.types";
-import {
-  FundDetailsProfitChart,
-  FundDetailsStatistic
-} from "./services/fund-details.types";
+import { FundStatisticResult } from "./services/fund-details.types";
 
-class _FundDetailsPage extends React.PureComponent<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      hasError: false,
-      isPending: false,
-      description: undefined,
-      profitChart: undefined,
-      balanceChart: undefined,
-      statistic: undefined
-    };
-  }
-
-  componentDidMount() {
-    this.getDetails();
-  }
-
-  updateDetails = (): Promise<void> => {
-    const { service } = this.props;
-    this.setState({ isPending: true });
-    return service
-      .getFundDescription()
-      .then(data => {
-        this.setState({ isPending: false, description: data });
-      })
-      .catch((e: ResponseError) => {
-        this.setState({ hasError: true });
-        throw e;
-      });
+const _FundDetailsPage: React.FC<Props> = ({
+  historySection,
+  currency,
+  service,
+  isAuthenticated,
+  descriptionSection
+}) => {
+  const [statistic, setStatistic] = useState<FundStatisticResult | undefined>(
+    undefined
+  );
+  const { errorMessage, setErrorMessage } = useErrorMessage();
+  const [description, setDescription] = useState<FundDetailsFull | undefined>(
+    undefined
+  );
+  useEffect(() => {
+    getDetails();
+  }, []);
+  const updateDetails = useCallback(() => service.getFundDescription(), []);
+  const getDetails = (): void => {
+    const update = updateDetails();
+    update.then(setDescription).catch(setErrorMessage);
+    update
+      .then(data => data.id)
+      .then(getFundStatistic)
+      .then(setStatistic)
+      .catch(setErrorMessage);
   };
-
-  getDetails = (): void => {
-    this.updateDetails()
-      .then(() => {
-        this.setState({ isPending: true });
-        return getFundStatistic(
-          this.state.description!.id,
-          this.props.currency
-        );
-      })
-      .then(data => {
-        this.setState({ isPending: false, ...data });
-      })
-      .catch(() => {
-        this.setState({ isPending: false });
-      });
-  };
-
-  render() {
-    const {
-      historySection,
-      currency,
-      service,
-      isAuthenticated,
-      descriptionSection
-    } = this.props;
-    const {
-      hasError,
-      description,
-      statistic,
-      profitChart,
-      balanceChart
-    } = this.state;
-    if (hasError) return <NotFoundPage />;
-    return (
-      <FundDetailsContainer
-        condition={!!description}
-        loader={<DetailsContainerLoader assets />}
-        updateDetails={this.updateDetails}
-        redirectToLogin={service.redirectToLogin}
-        historySection={historySection}
-        descriptionSection={descriptionSection}
-        description={description!}
-        profitChart={profitChart}
-        balanceChart={balanceChart}
-        statistic={statistic}
-        currency={currency}
-        isAuthenticated={isAuthenticated}
-      />
-    );
-  }
-}
+  if (errorMessage) return <NotFoundPage />;
+  return (
+    <FundDetailsContainer
+      condition={!!description}
+      loader={<DetailsContainerLoader assets />}
+      updateDetails={updateDetails}
+      redirectToLogin={service.redirectToLogin}
+      historySection={historySection}
+      descriptionSection={descriptionSection}
+      description={description!}
+      statistic={statistic}
+      currency={currency}
+      isAuthenticated={isAuthenticated}
+    />
+  );
+};
 
 const mapStateToProps = (state: RootState): StateProps => ({
   currency: currencySelector(state),
@@ -135,19 +95,11 @@ interface DispatchProps {
 
 interface Props extends OwnProps, StateProps, DispatchProps {}
 
-interface State {
-  isPending: boolean;
-  hasError: boolean;
-  description?: FundDetailsFull;
-  profitChart?: FundDetailsProfitChart;
-  balanceChart?: FundBalanceChart;
-  statistic?: FundDetailsStatistic;
-}
-
 const FundDetailsPage = compose<React.ComponentType<OwnProps>>(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )
+  ),
+  React.memo
 )(_FundDetailsPage);
 export default FundDetailsPage;
