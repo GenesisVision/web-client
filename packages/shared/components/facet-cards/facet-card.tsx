2250362d9a8ad3c6fd417bb65758b5764b834d13
Евгений Гребенщиков <evgreb@gmail.com>
2019-07-15 18:05:31 +0300

    fix links in facets

--- packages/shared/components/facet-cards/facet-card.tsx
+++ packages/shared/components/facet-cards/facet-card.tsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import { FundFacet, ProgramFacet } from "gv-api-web";
+import Link from "next/link";
 import * as React from "react";
-import { Link } from "react-router-dom";
 import ImageBase from "shared/components/avatar/image-base";
 import Surface from "shared/components/surface/surface";
 import useIsOpen from "shared/hooks/is-open.hook";
@@ -19,25 +19,28 @@ const _FacetCard: React.FC<Props> = ({ facet, composeFacetUrl, title }) => {
       onMouseLeave={setNotHovered}
     >
       <Link
-        to={{
-          pathname: composeFacetUrl(facet.url),
-          state: `/ ${title}`
-        }}
+        href={composeFacetUrl(facet.url)}
+        //   {
+        //   pathname: composeFacetUrl(facet.url),
+        //   state: `/ ${title}`
+        // }}
       >
-        <div className="facet__facet-container">
-          <div className="facet__logo-wrapper">
-            <ImageBase
-              url={facet.logo}
-              alt={facet.title}
-              defaultImage={facetImg}
-              imageClassName="facet__logo"
-            />
+        <a>
+          <div className="facet__facet-container">
+            <div className="facet__logo-wrapper">
+              <ImageBase
+                url={facet.logo}
+                alt={facet.title}
+                defaultImage={facetImg}
+                imageClassName="facet__logo"
+              />
+            </div>
+            <div className="facet__info">
+              <h2 className="facet__title">{facet.title}</h2>
+              <div className="facet__description">{facet.description}</div>
+            </div>
           </div>
-          <div className="facet__info">
-            <h2 className="facet__title">{facet.title}</h2>
-            <div className="facet__description">{facet.description}</div>
-          </div>
-        </div>
+        </a>
       </Link>
     </Surface>
   );
