e5e456fc11bb54734e9c4732afd0aecf5dd76813
pavelzubov <pasha224@yandex.ru>
2019-07-16 13:48:44 +0300

    Change ProgramDepositContainer to DepositContainer

--- packages/investor/src/modules/program-deposit/program-deposit.tsx
+++ packages/investor/src/modules/program-deposit/program-deposit.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
-import ProgramDepositContainer from "shared/components/deposit/components/program-deposit-container";
+import DepositContainer from "shared/components/deposit/components/deposit-container";
+import {
+  getProgramInfoCreator,
+  programInvestCreator
+} from "shared/components/deposit/services/program-deposit.service";
 import { IDialogProps } from "shared/components/dialog/dialog";
+import { ASSET } from "shared/constants/constants";
 import investorApi from "shared/services/api-client/investor-api";
+import { CurrencyEnum } from "shared/utils/types";
 
 const _ProgramDeposit: React.FC<OwnProps & IDialogProps> = ({
   id,
@@ -10,15 +16,20 @@ const _ProgramDeposit: React.FC<OwnProps & IDialogProps> = ({
   open,
   onClose
 }) => (
-  <ProgramDepositContainer
+  <DepositContainer
+    asset={ASSET.PROGRAM}
+    assetInvest={programInvestCreator(
+      investorApi.v10InvestorProgramsByIdInvestByAmountPost
+    )}
+    fetchInfo={getProgramInfoCreator(
+      investorApi.v10InvestorProgramsByIdInvestInfoByCurrencyGet
+    )}
     id={id}
     hasEntryFee
     currency={currency}
     onApply={onApply}
     open={open}
     onClose={onClose}
-    programInvest={investorApi.v10InvestorProgramsByIdInvestByAmountPost}
-    fetchInfo={investorApi.v10InvestorProgramsByIdInvestInfoByCurrencyGet}
   />
 );
 
@@ -27,6 +38,6 @@ export default ProgramDeposit;
 
 interface OwnProps {
   id: string;
-  currency: string;
+  currency: CurrencyEnum;
   onApply(): void;
 }
