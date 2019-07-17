74d5cb5b8e66eee625ea758b50bbeb0aa9a9f91a
pavelzubov <pasha224@yandex.ru>
2019-07-17 10:51:46 +0300

    Add withLoader

--- packages/manager/src/modules/fund-deposit/fund-deposit.tsx
+++ packages/manager/src/modules/fund-deposit/fund-deposit.tsx
@@ -6,6 +6,7 @@ import {
 } from "shared/components/deposit/services/fund-deposit.service";
 import { IDialogProps } from "shared/components/dialog/dialog";
 import { ASSET } from "shared/constants/constants";
+import withLoader from "shared/decorators/with-loader";
 import managerApi from "shared/services/api-client/manager-api";
 
 const _FundDeposit: React.FC<OwnProps & IDialogProps> = ({
@@ -30,7 +31,7 @@ const _FundDeposit: React.FC<OwnProps & IDialogProps> = ({
   />
 );
 
-const FundDeposit = React.memo(_FundDeposit);
+const FundDeposit = withLoader(React.memo(_FundDeposit));
 export default FundDeposit;
 
 interface OwnProps {
