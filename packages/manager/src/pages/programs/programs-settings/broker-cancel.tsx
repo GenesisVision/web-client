28603c635b9f8815b83035f9153f7e1a7670decf
pavelzubov <pasha224@yandex.ru>
2019-07-17 14:38:45 +0300

    Reformat

--- packages/manager/src/pages/programs/programs-settings/broker-cancel.tsx
+++ packages/manager/src/pages/programs/programs-settings/broker-cancel.tsx
@@ -14,80 +14,74 @@ const _BrokerCancel: React.FC<Props> = ({
   brokerTo,
   currentAccountTypeId,
   leverage
-}) => {
-  return (
-    <div>
-      <h3>{t("manager.program-settings.broker.title")}</h3>
-      <div className="program-edit__block-wrapper program-edit__block-wrapper--broker-list">
-        <div className="program-edit__broker-info">
-          <BrokerCard
-            logo={brokerFrom.logo}
-            key={brokerFrom.name}
-            brokerName={brokerFrom.name}
-            cardState={BROKER_CARD_EXTRA_STATE.NONE}
-            tags={brokerFrom.tags}
-          />
-          <StatisticItem
-            label={t(
-              "manager.create-program-page.settings.fields.account-type"
-            )}
-          >
-            {
-              brokerFrom.accountTypes.find(
-                account => account.id === currentAccountTypeId
-              )!.name
-            }
-          </StatisticItem>
-          <StatisticItem
-            label={t(
-              "manager.create-program-page.settings.fields.brokers-leverage"
-            )}
-          >
-            {leverage}
-          </StatisticItem>
-        </div>
-        <div className="broker-card__next-arrow">&rarr;</div>
-        <div className="program-edit__broker-info">
-          <BrokerCard
-            logo={brokerTo.brokerLogo}
-            key={brokerTo.brokerName}
-            brokerName={brokerTo.brokerName}
-            cardState={BROKER_CARD_EXTRA_STATE.NONE}
-            // tags={brokerTo.tags}
-          />
-          <StatisticItem
-            label={t(
-              "manager.create-program-page.settings.fields.account-type"
-            )}
-          >
-            {brokerTo.brokerTradingAccountName}
-          </StatisticItem>
-          <StatisticItem
-            label={t(
-              "manager.create-program-page.settings.fields.brokers-leverage"
-            )}
-          >
-            {brokerTo.newLeverage}
-          </StatisticItem>
-        </div>
+}) => (
+  <div>
+    <h3>{t("manager.program-settings.broker.title")}</h3>
+    <div className="program-edit__block-wrapper program-edit__block-wrapper--broker-list">
+      <div className="program-edit__broker-info">
+        <BrokerCard
+          logo={brokerFrom.logo}
+          key={brokerFrom.name}
+          brokerName={brokerFrom.name}
+          cardState={BROKER_CARD_EXTRA_STATE.NONE}
+          tags={brokerFrom.tags}
+        />
+        <StatisticItem
+          label={t("manager.create-program-page.settings.fields.account-type")}
+        >
+          {
+            brokerFrom.accountTypes.find(
+              account => account.id === currentAccountTypeId
+            )!.name
+          }
+        </StatisticItem>
+        <StatisticItem
+          label={t(
+            "manager.create-program-page.settings.fields.brokers-leverage"
+          )}
+        >
+          {leverage}
+        </StatisticItem>
       </div>
-      <p className="program-edit__text program-edit__text--color-accent program-edit__text--padding-top">
-        {t("manager.program-settings.broker.text-cancel", {
-          brokerFrom: brokerFrom.name,
-          brokerTo: brokerTo.brokerName
-        })}
-      </p>
-      <GVButton
-        type="submit"
-        color="primary"
-        className="invest-form__submit-button"
-        onClick={onSubmit}
-      >
-        {t("manager.program-settings.buttons.cancel-broker")}
-      </GVButton>
+      <div className="broker-card__next-arrow">&rarr;</div>
+      <div className="program-edit__broker-info">
+        <BrokerCard
+          logo={brokerTo.brokerLogo}
+          key={brokerTo.brokerName}
+          brokerName={brokerTo.brokerName}
+          cardState={BROKER_CARD_EXTRA_STATE.NONE}
+          // tags={brokerTo.tags}
+        />
+        <StatisticItem
+          label={t("manager.create-program-page.settings.fields.account-type")}
+        >
+          {brokerTo.brokerTradingAccountName}
+        </StatisticItem>
+        <StatisticItem
+          label={t(
+            "manager.create-program-page.settings.fields.brokers-leverage"
+          )}
+        >
+          {brokerTo.newLeverage}
+        </StatisticItem>
+      </div>
     </div>
-  );
-};
+    <p className="program-edit__text program-edit__text--color-accent program-edit__text--padding-top">
+      {t("manager.program-settings.broker.text-cancel", {
+        brokerFrom: brokerFrom.name,
+        brokerTo: brokerTo.brokerName
+      })}
+    </p>
+    <GVButton
+      type="submit"
+      color="primary"
+      className="invest-form__submit-button"
+      onClick={onSubmit}
+    >
+      {t("manager.program-settings.buttons.cancel-broker")}
+    </GVButton>
+  </div>
+);
 
 interface Props extends OwnProps, WithTranslation {}
 
