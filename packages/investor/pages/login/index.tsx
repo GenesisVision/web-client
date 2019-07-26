ff3bb7ef587fb8cfe179ff739a9bf32031e6388e
nkrivous <kolsln@list.ru>
2019-07-26 13:37:11 +0300

    Add portfolio events ssr

--- packages/investor/pages/login/index.tsx
+++ packages/investor/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
 import React from "react";
 import LoginFooter from "shared/components/auth/components/login-footer/login-footer";
 import LoginPage from "shared/components/auth/signin/login/login.page";
@@ -9,9 +9,20 @@ const Login: NextPage<Props> = ({ redirectFrom }) => {
   return <LoginPage redirectFrom={redirectFrom} />;
 };
 
-Login.getInitialProps = async () => {
+Login.getInitialProps = async (ctx: NextPageContext) => {
+  let redirectFrom = HOME_ROUTE;
+  if (ctx.req) {
+    let t = 1;
+  }
+  debugger;
+  if (!ctx.req && window.history.state.from) {
+    console.log(window.history.state);
+    redirectFrom = window.history.state.from;
+  }
+  console.log(ctx);
+
   return {
-    redirectFrom: HOME_ROUTE
+    redirectFrom
   };
 };
 
