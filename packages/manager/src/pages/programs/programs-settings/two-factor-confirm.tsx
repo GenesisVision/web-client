5082e22b9c6b27dae00b076d6ea3bb364ec24e35
pavelzubov <pasha224@yandex.ru>
2019-07-29 22:56:03 +0300

    Change markup to SettingsBlock

--- packages/manager/src/pages/programs/programs-settings/two-factor-confirm.tsx
+++ packages/manager/src/pages/programs/programs-settings/two-factor-confirm.tsx
@@ -3,28 +3,33 @@ import React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 import { compose } from "redux";
 import GVButton from "shared/components/gv-button";
+import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 import useIsOpen from "shared/hooks/is-open.hook";
+
+import SettingsBlock from "./settings-block";
 
 const _TwoFactorConfirm: React.FC<Props> = ({ id, t }) => {
   const [isOpen, setOpen, setClose] = useIsOpen();
   return (
-    <section className="program-settings__block">
-      <h3>{t("manager.program-settings.two-factor-confirm.title")}</h3>
-      <div className="program-settings__block-wrapper">
-        <p className="program-settings__text">
-          {t("manager.program-settings.two-factor-confirm.text")}
-        </p>
-        <GVButton color="primary" onClick={setOpen}>
-          {t("manager.program-settings.buttons.two-factor-confirm")}
-        </GVButton>
-      </div>
-      <ConfirmContainer
-        open={isOpen}
-        onClose={setClose}
-        onApply={setClose}
-        programId={id}
-      />
-    </section>
+    <SettingsBlock
+      label={t("manager.program-settings.two-factor-confirm.title")}
+      content={
+        <>
+          <p className="program-settings__text">
+            {t("manager.program-settings.two-factor-confirm.text")}
+          </p>
+          <GVButton color="primary" onClick={setOpen}>
+            {t("manager.program-settings.buttons.two-factor-confirm")}
+          </GVButton>
+          <ConfirmContainer
+            open={isOpen}
+            onClose={setClose}
+            onApply={setClose}
+            programId={id}
+          />
+        </>
+      }
+    />
   );
 };
 
@@ -34,7 +39,8 @@ interface OwnProps {
   id: string;
 }
 
-const TwoFactorConfirm = compose<React.ComponentType<OwnProps>>(
+const TwoFactorConfirm = compose<React.ComponentType<OwnProps & WithLoaderProps>>(
+  withLoader,
   translate(),
   React.memo
 )(_TwoFactorConfirm);
