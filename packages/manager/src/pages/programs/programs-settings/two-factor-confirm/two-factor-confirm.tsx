79b9d32a7c98c8cf49f04a38ca4ca030ef23e048
pavelzubov <pasha224@yandex.ru>
2019-07-29 21:56:59 +0300

    Add TwoFactorConfirm

---
+++ packages/manager/src/pages/programs/programs-settings/two-factor-confirm/two-factor-confirm.tsx
@@ -0,0 +1,41 @@
+import ConfirmContainer from "modules/confirm/confirm-container";
+import React from "react";
+import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { compose } from "redux";
+import GVButton from "shared/components/gv-button";
+import useIsOpen from "shared/hooks/is-open.hook";
+
+const _TwoFactorConfirm: React.FC<Props> = ({ id, t }) => {
+  const [isOpen, setOpen, setClose] = useIsOpen();
+  return (
+    <section className="program-edit__block">
+      <h3>{t("manager.program-settings.two-factor-confirm.title")}</h3>
+      <div className="program-edit__block-wrapper">
+        <p className="program-edit__text">
+          {t("manager.program-settings.two-factor-confirm.text")}
+        </p>
+        <GVButton color="primary" onClick={setOpen}>
+          {t("manager.program-settings.buttons.two-factor-confirm")}
+        </GVButton>
+      </div>
+      <ConfirmContainer
+        open={isOpen}
+        onClose={setClose}
+        onApply={setClose}
+        programId={id}
+      />
+    </section>
+  );
+};
+
+interface Props extends OwnProps, WithTranslation {}
+
+interface OwnProps {
+  id: string;
+}
+
+const TwoFactorConfirm = compose<React.ComponentType<OwnProps>>(
+  translate(),
+  React.memo
+)(_TwoFactorConfirm);
+export default TwoFactorConfirm;
