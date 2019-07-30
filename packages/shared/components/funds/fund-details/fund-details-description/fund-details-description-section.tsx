b89fffaa978be1c62d736c2ee7d127cb1a36348f
pavelzubov <pasha224@yandex.ru>
2019-07-30 10:49:08 +0300

    Remove assetDescription object

--- packages/shared/components/funds/fund-details/fund-details-description/fund-details-description-section.tsx
+++ packages/shared/components/funds/fund-details/fund-details-description/fund-details-description-section.tsx
@@ -19,7 +19,6 @@ import FundDetailsDescription from "shared/components/funds/fund-details/fund-de
 import { dispatchFundDescription } from "shared/components/funds/fund-details/services/fund-details.service";
 import { TooltipLabel } from "shared/components/tooltip-label/tooltip-label";
 import { FUND, STATUS } from "shared/constants/constants";
-import { composeFundNotificationsUrl } from "shared/utils/compose-url";
 
 import {
   IFundControlsProps,
@@ -35,64 +34,45 @@ const _FundFundDetailsDescription: React.FC<Props> = ({
   fundDescription,
   FundControls,
   FundWithdrawContainer
-}) => {
-  const { personalFundDetails } = fundDescription;
-  const assetDescription = {
-    id: fundDescription.id,
-    title: fundDescription.title,
-    description: fundDescription.description,
-    logo: fundDescription.logo,
-    notificationsUrl: composeFundNotificationsUrl(fundDescription.url),
-    isFavorite: fundDescription.personalFundDetails
-      ? fundDescription.personalFundDetails.isFavorite
-      : false,
-    hasNotifications: fundDescription.personalFundDetails
-      ? fundDescription.personalFundDetails.hasNotifications
-      : false,
-    managerUrl: fundDescription.manager.url,
-    managerName: fundDescription.manager.username,
-    managerSocialLinks: fundDescription.manager.socialLinks
-  };
-
-  return (
-    <div className="program-details-description">
-      <FundDetailsDescription
-        assetDescription={assetDescription}
-        AssetDetailsAvatar={() => (
-          <div className="details-description__avatar">
-            <AssetAvatar
-              url={fundDescription.logo}
-              alt={fundDescription.title}
-              size="big"
-              color={fundDescription.color}
+}) => (
+  <div className="program-details-description">
+    <FundDetailsDescription
+      description={fundDescription}
+      AssetDetailsAvatar={() => (
+        <div className="details-description__avatar">
+          <AssetAvatar
+            url={fundDescription.logo}
+            alt={fundDescription.title}
+            size="big"
+            color={fundDescription.color}
+          />
+        </div>
+      )}
+      AssetDetailsExtraBlock={() => (
+        <div className="details-description__info-block">
+          <h4 className="details-description__subheading tooltip__label">
+            <TooltipLabel
+              tooltipContent={t("fund-details-page.tooltip.assets")}
+              labelText={t("fund-details-page.description.assets")}
+            />
+          </h4>
+          <div>
+            <FundAssetContainer
+              type={FUND_ASSET_TYPE.LARGE}
+              assets={fundDescription.currentAssets}
+              size={7}
             />
           </div>
-        )}
-        AssetDetailsExtraBlock={() => (
-          <div className="details-description__info-block">
-            <h4 className="details-description__subheading tooltip__label">
-              <TooltipLabel
-                tooltipContent={t("fund-details-page.tooltip.assets")}
-                labelText={t("fund-details-page.description.assets")}
-              />
-            </h4>
-            <div>
-              <FundAssetContainer
-                type={FUND_ASSET_TYPE.LARGE}
-                assets={fundDescription.currentAssets}
-                size={7}
-              />
-            </div>
-          </div>
-        )}
-      />
-      <FundControls
-        fundDescription={fundDescription}
-        isAuthenticated={isAuthenticated}
-        redirectToLogin={redirectToLogin}
-      />
-
-      {personalFundDetails && personalFundDetails.status !== STATUS.ENDED && (
+        </div>
+      )}
+    />
+    <FundControls
+      fundDescription={fundDescription}
+      isAuthenticated={isAuthenticated}
+      redirectToLogin={redirectToLogin}
+    />
+    {fundDescription.personalFundDetails &&
+      fundDescription.personalFundDetails.status !== STATUS.ENDED && (
         <div className="program-details-description__additionally">
           <DetailsInvestment
             updateDescription={dispatchFundDescription}
@@ -100,14 +80,15 @@ const _FundFundDetailsDescription: React.FC<Props> = ({
             id={fundDescription.id}
             assetCurrency={"GVT"}
             accountCurrency={accountCurrency}
-            personalDetails={personalFundDetails as InvestmentDetails}
+            personalDetails={
+              fundDescription.personalFundDetails as InvestmentDetails
+            }
             WithdrawContainer={FundWithdrawContainer}
           />
         </div>
       )}
-    </div>
-  );
-};
+  </div>
+);
 
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
