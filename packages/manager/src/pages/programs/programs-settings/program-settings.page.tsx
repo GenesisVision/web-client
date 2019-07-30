79cc9b9e726e479edbf8c05f5fec639ce09e6237
pavelzubov <pasha224@yandex.ru>
2019-07-30 17:10:08 +0300

    Remove console.log Upgrade api

--- packages/manager/src/pages/programs/programs-settings/program-settings.page.tsx
+++ packages/manager/src/pages/programs/programs-settings/program-settings.page.tsx
@@ -104,7 +104,6 @@ const _ProgramsEditPage: React.FC<Props> = ({
     [description]
   );
   const applyCloseProgram = useCallback(() => redirectToProgram(), []);
-  console.log(description, brokersInfo);
   return (
     <Page title={t("manager.program-settings.title")}>
       <ProgramSettings
