8244dfce045c655730017c61cb833f87b6fbaea3
pavelzubov <pasha224@yandex.ru>
2019-07-30 10:50:35 +0300

    Remove unused action

--- packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
+++ packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
@@ -1,18 +1,11 @@
-import {
-  FundBalanceChart,
-  FundDetailsFull,
-  FundProfitChart,
-  LevelsParamsInfo
-} from "gv-api-web";
+import { FundBalanceChart, FundDetailsFull, FundProfitChart } from "gv-api-web";
 import { getDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
 import fundsApi from "shared/services/api-client/funds-api";
-import platformApi from "shared/services/api-client/platform-api";
-import { ApiAction, CurrencyEnum } from "shared/utils/types";
+import { ApiAction } from "shared/utils/types";
 
 export const FETCH_FUND_PROFIT_CHART = "FETCH_FUND_PROFIT_CHART";
 export const FETCH_FUND_BALANCE_CHART = "FETCH_FUND_BALANCE_CHART";
 export const FETCH_FUND_DESCRIPTION = "FETCH_FUND_DESCRIPTION";
-export const FETCH_LEVEL_PARAMETERS = "FETCH_LEVEL_PARAMETERS";
 
 export const fetchFundProfitChartAction = (
   id: string,
@@ -44,11 +37,4 @@ export const fetchFundDescriptionAction = (
 ): ApiAction<FundDetailsFull> => ({
   type: FETCH_FUND_DESCRIPTION,
   payload: fundsApi.v10FundsByIdGet(id, { authorization })
-});
-
-export const fetchLevelParametersAction = (
-  currency: CurrencyEnum
-): ApiAction<LevelsParamsInfo> => ({
-  type: FETCH_LEVEL_PARAMETERS,
-  payload: platformApi.v10PlatformLevelsParametersGet({ currency })
 });
