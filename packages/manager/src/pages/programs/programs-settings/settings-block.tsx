cce342ca23a6b635da3805303f1c0680eb9ad6ec
pavelzubov <pasha224@yandex.ru>
2019-07-30 12:03:45 +0300

    Set label as optional

--- packages/manager/src/pages/programs/programs-settings/settings-block.tsx
+++ packages/manager/src/pages/programs/programs-settings/settings-block.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const _SettingsBlock: React.FC<Props> = ({ label, content }) => (
   <section className="program-settings__block">
-    <h3>{label}</h3>
+    {label && <h3>{label}</h3>}
     <div className="program-settings__block-wrapper">{content}</div>
   </section>
 );
 
 interface OwnProps {
-  label: string;
   content: JSX.Element;
+  label?: string;
 }
 interface Props extends OwnProps {}
 
