693c2143ec957472ad4be5cdd9589f903f24fc18
a.popova <bob.m@mail.ru>
2019-07-30 16:34:59 +0300

    removed comments

--- packages/shared/components/manager/manager.page.tsx
+++ packages/shared/components/manager/manager.page.tsx
@@ -3,14 +3,11 @@ import "./manager.page.scss";
 import { ManagerProfile } from "gv-api-web";
 import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { connect } from "react-redux";
 import { compose } from "redux";
 import ManagerDescription from "shared/components/manager/manager-description/manager-description";
 import ManagerHistorySection from "shared/components/manager/manager-history/manager-history-section";
 import Page from "shared/components/page/page";
 import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
-import { isAuthenticatedSelector } from "shared/reducers/auth-reducer";
-import { AuthRootState } from "shared/utils/types";
 
 const _ManagerPage: React.FC<Props> = ({
   managerProfile,
@@ -40,24 +37,10 @@ interface OwnProps {
   isAuthenticated: boolean;
 }
 
-// interface StateProps {
-//   isAuthenticated: boolean;
-// }
-
-// const mapStateToProps = (state: AuthRootState): StateProps => ({
-//   isAuthenticated: isAuthenticatedSelector(state)
-// });
-
 const ManagerPage = compose<React.ComponentType<OwnProps & WithLoaderProps>>(
   withLoader,
   translate(),
   React.memo
 )(_ManagerPage);
 
-// const ManagerPage = compose<React.ComponentType<OwnProps & WithLoaderProps>>(
-//   connect(mapStateToProps),
-//   withLoader,
-//   translate(),
-//   React.memo
-// )(_ManagerPage);
 export default ManagerPage;
