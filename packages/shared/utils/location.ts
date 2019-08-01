54fa962778457a2a43ce1c8352c2ee5fdc254336
pavelzubov <pasha224@yandex.ru>
2019-08-01 14:43:09 +0300

    Clean hash if arg is empty

--- packages/shared/utils/location.ts
+++ packages/shared/utils/location.ts
@@ -4,7 +4,7 @@ export const getLocation = (): Location<any> => window.location;
 
 export const setHash = (hash: string): void => {
   if (history.pushState) {
-    history.pushState(null, "", hash);
+    history.pushState(null, "", hash || window.location.pathname);
   } else {
     location.hash = hash;
   }
