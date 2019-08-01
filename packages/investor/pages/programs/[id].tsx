0b3bb07e1cda2fa24ff05785292a691c7c5ce6a4
pavelzubov <pasha224@yandex.ru>
2019-08-01 13:46:01 +0300

    Remove dispatching charts

--- packages/investor/pages/programs/[id].tsx
+++ packages/investor/pages/programs/[id].tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { compose } from "redux";
+import {
+  ActionCreatorsMapObject,
+  bindActionCreators,
+  compose,
+  Dispatch
+} from "redux";
 import {
   dispatchProgramDescription,
   dispatchProgramId,
@@ -11,7 +16,7 @@ import withPrivateRoute from "shared/decorators/with-private-route";
 import { NextPageWithRedux } from "shared/utils/types";
 
 import ProgramDetailsPage from "../../src/pages/programs/program-details/program-details.page";
-import { DEFAULT_PERIOD } from "shared/components/chart/chart-period/chart-period.helpers";
+import { connect, ResolveThunks } from "react-redux";
 
 const ProgramDetails: NextPageWithRedux<Props, {}> = () => {
   return <ProgramDetailsPage />;
@@ -21,20 +26,40 @@ ProgramDetails.getInitialProps = async ctx => {
   const { id } = ctx.query;
   await Promise.all([
     ctx.reduxStore.dispatch(dispatchProgramId(id as string)),
-    ctx.reduxStore.dispatch(dispatchProgramDescription(id as string)),
-    ctx.reduxStore.dispatch(
-      getProfitChart({ id: id as string, period: DEFAULT_PERIOD })
-    ),
-    ctx.reduxStore.dispatch(
-      getBalanceChart({ id: id as string, period: DEFAULT_PERIOD })
-    )
+    ctx.reduxStore.dispatch(dispatchProgramDescription(id as string))
   ]);
   return {};
 };
 
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
+    {
+      dispatchProgramId,
+      dispatchProgramDescription,
+      getProfitChart,
+      getBalanceChart
+    },
+    dispatch
+  )
+});
+
 export default compose(
+  connect(
+    null,
+    mapDispatchToProps
+  ),
   withDefaultLayout,
   withPrivateRoute
 )(ProgramDetails);
 
-interface Props {}
+interface DispatchProps {
+  service: ResolveThunks<ServiceThunks>;
+}
+interface ServiceThunks extends ActionCreatorsMapObject {
+  dispatchProgramId: typeof dispatchProgramId;
+  dispatchProgramDescription: typeof dispatchProgramDescription;
+  getProfitChart: typeof getProfitChart;
+  getBalanceChart: typeof getBalanceChart;
+}
+
+interface Props extends DispatchProps {}
