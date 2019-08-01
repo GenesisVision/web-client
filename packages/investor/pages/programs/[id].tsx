36cd34320a2b485a08526e394505b4a965994a1a
pavelzubov <pasha224@yandex.ru>
2019-08-01 10:08:18 +0300

    Add dispatching data on server

--- packages/investor/pages/programs/[id].tsx
+++ packages/investor/pages/programs/[id].tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { compose } from "redux";
-import { dispatchProgramId } from "shared/components/programs/program-details/services/program-details.service";
+import {
+  dispatchProgramDescription,
+  dispatchProgramId,
+  getBalanceChart,
+  getProfitChart
+} from "shared/components/programs/program-details/services/program-details.service";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import withPrivateRoute from "shared/decorators/with-private-route";
 import { NextPageWithRedux } from "shared/utils/types";
 
 import ProgramDetailsPage from "../../src/pages/programs/program-details/program-details.page";
+import { DEFAULT_PERIOD } from "shared/components/chart/chart-period/chart-period.helpers";
 
 const ProgramDetails: NextPageWithRedux<Props, {}> = () => {
   return <ProgramDetailsPage />;
@@ -13,7 +19,16 @@ const ProgramDetails: NextPageWithRedux<Props, {}> = () => {
 
 ProgramDetails.getInitialProps = async ctx => {
   const { id } = ctx.query;
-  await Promise.all([ctx.reduxStore.dispatch(dispatchProgramId(id as string))]);
+  await Promise.all([
+    ctx.reduxStore.dispatch(dispatchProgramId(id as string)),
+    ctx.reduxStore.dispatch(dispatchProgramDescription(id as string)),
+    ctx.reduxStore.dispatch(
+      getProfitChart({ id: id as string, period: DEFAULT_PERIOD })
+    ),
+    ctx.reduxStore.dispatch(
+      getBalanceChart({ id: id as string, period: DEFAULT_PERIOD })
+    )
+  ]);
   return {};
 };
 
