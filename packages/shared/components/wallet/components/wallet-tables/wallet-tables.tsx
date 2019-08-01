e0886818b8bc8a32e2fc45c51be74095747b2735
pavelzubov <pasha224@yandex.ru>
2019-08-01 17:29:26 +0300

    Move logic to useHashTab hook

--- packages/shared/components/wallet/components/wallet-tables/wallet-tables.tsx
+++ packages/shared/components/wallet/components/wallet-tables/wallet-tables.tsx
@@ -1,17 +1,15 @@
 import "./wallet-tables.scss";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { Link, withRouter } from "react-router-dom";
 import { compose } from "redux";
 import GVTabs from "shared/components/gv-tabs";
 import GVTab from "shared/components/gv-tabs/gv-tab";
 import Surface from "shared/components/surface/surface";
 import { TooltipLabel } from "shared/components/tooltip-label/tooltip-label";
-import { WALLET_TOTAL_PAGE_ROUTE } from "shared/components/wallet/wallet.routes";
-import useTab from "shared/hooks/tab.hook";
 import { CURRENCIES } from "shared/modules/currency-select/currency-select.constants";
 
+import useHashTab from "../../services/hashTab.hook";
 import DepositsWithdrawalsRow from "./wallet-deposits-withdrawals/deposits-withdrawals-row";
 import WalletDepositsWithdrawals from "./wallet-deposits-withdrawals/wallet-deposits-withdrawals";
 import { WALLET_DEPOSITS_WITHDRAWALS_COLUMNS } from "./wallet-deposits-withdrawals/wallet-deposits-withdrawals.constants";
@@ -19,14 +17,8 @@ import TransactionsRow from "./wallet-transactions/transactions-row";
 import WalletTransactions from "./wallet-transactions/wallet-transactions";
 import { WALLET_TRANSACTIONS_COLUMNS } from "./wallet-transactions/wallet-transactions.constants";
 
-const _WalletTables: React.FC<Props> = ({ t, currency, location }) => {
-  const { tab, setTab } = useTab<TABS>(TABS.TRANSACTIONS_TAB);
-  useEffect(
-    () => {
-      setTab(null, location.hash);
-    },
-    [location, setTab]
-  );
+const _WalletTables: React.FC<Props> = ({ t, currency }) => {
+  const { tab, setTab } = useHashTab<TABS>(TABS.TRANSACTIONS_TAB);
   return (
     <Surface className="wallet-container">
       <div className="wallet-container__header">
@@ -36,31 +28,17 @@ const _WalletTables: React.FC<Props> = ({ t, currency, location }) => {
               className={"gv-tab"}
               value={TABS.TRANSACTIONS_TAB} //TODO add disable prop
               label={
-                <Link
-                  to={{
-                    prevPath: WALLET_TOTAL_PAGE_ROUTE,
-                    pathname: location.pathname,
-                    state: t("wallet-page.title")
-                  }}
-                >
-                  <TooltipLabel
-                    tooltipContent={t("wallet-page.tooltip.transactions")}
-                    labelText={t("wallet-page.tabs.transactions")}
-                    className="tooltip__label--cursor-pointer"
-                  />
-                </Link>
+                <TooltipLabel
+                  tooltipContent={t("wallet-page.tooltip.transactions")}
+                  labelText={t("wallet-page.tabs.transactions")}
+                  className="tooltip__label--cursor-pointer"
+                />
               }
             />
             <GVTab
               value={TABS.EXTERNAL_TAB}
               label={
-                <Link
-                  to={{
-                    prevPath: WALLET_TOTAL_PAGE_ROUTE,
-                    hash: TABS.EXTERNAL_TAB,
-                    state: t("wallet-page.title")
-                  }}
-                >
+                <>
                   <TooltipLabel
                     tooltipContent={t("wallet-page.tooltip.deposit")}
                     labelText={t("wallet-page.tabs.deposit")}
@@ -71,7 +49,7 @@ const _WalletTables: React.FC<Props> = ({ t, currency, location }) => {
                     labelText={t("wallet-page.tabs.withdrawals")}
                     className="tooltip__label--cursor-pointer"
                   />
-                </Link>
+                </>
               }
             />
           </GVTabs>
@@ -113,9 +91,7 @@ enum TABS {
   EXTERNAL_TAB = "#external"
 }
 
-interface Props extends WithTranslation, OwnProps {
-  location: Location;
-}
+interface Props extends WithTranslation, OwnProps {}
 
 interface OwnProps {
   currency: CURRENCIES;
@@ -123,7 +99,6 @@ interface OwnProps {
 
 const WalletContainer = compose<React.ComponentType<OwnProps>>(
   translate(),
-  withRouter,
   React.memo
 )(_WalletTables);
 export default WalletContainer;
