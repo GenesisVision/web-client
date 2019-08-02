f30ec774bfa159e49614d9bc759d28cd0abd88e7
pavelzubov <pasha224@yandex.ru>
2019-08-02 10:28:03 +0300

    Remove show logic from component

--- packages/shared/components/spiner/spiner.tsx
+++ packages/shared/components/spiner/spiner.tsx
@@ -2,29 +2,23 @@ import "./spinner.scss";
 
 import * as React from "react";
 
-interface ISpinnerProps {
-  isShown?: boolean;
-}
-
-const Spinner: React.FC<ISpinnerProps> = React.memo(({ isShown }) =>
-  isShown ? (
-    <div className="gv-spinner__wrapper">
-      <div className="gv-spinner">
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+const Spinner: React.FC = React.memo(() => (
+  <div className="gv-spinner__wrapper">
+    <div className="gv-spinner">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
-  ) : null
-);
+  </div>
+));
 
 export default Spinner;
