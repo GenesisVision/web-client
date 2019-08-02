bf079f0b080fd1e048f8518ed274a7297e8fb88a
pavelzubov <pasha224@yandex.ru>
2019-08-02 12:14:45 +0300

    Change id from route to id from props

--- packages/shared/components/notifications/program-settings.page.tsx
+++ packages/shared/components/notifications/program-settings.page.tsx
@@ -1,30 +1,26 @@
 import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { RouteComponentProps } from "react-router";
 import Page from "shared/components/page/page";
 import ProgramNotificationsContainer from "shared/modules/program-notifications/program-notifications-container";
 
-const _ProgramNotificationPage: React.FC<Props> = ({ t, match }) => {
-  const { id } = match.params;
-  return (
-    <Page title={t("notifications-page.program.title")}>
-      <div className="app__main-wrapper">
-        <h1 className="title-small-padding">
-          {t("notifications-page.program.title")}
-        </h1>
-        <ProgramNotificationsContainer id={id} />
-      </div>
-    </Page>
-  );
-};
+const _ProgramNotificationPage: React.FC<Props> = ({ t, id }) => (
+  <Page title={t("notifications-page.program.title")}>
+    <div className="app__main-wrapper">
+      <h1 className="title-small-padding">
+        {t("notifications-page.program.title")}
+      </h1>
+      <ProgramNotificationsContainer id={id} />
+    </div>
+  </Page>
+);
 
-interface Props extends RouteComponentProps<Params>, WithTranslation {}
+interface Props extends WithTranslation, OwnProps {}
 
-interface Params {
+interface OwnProps {
   id: string;
 }
 
-const ProgramNotificationPage = React.memo(
-  translate()(_ProgramNotificationPage)
+const ProgramNotificationPage = translate()(
+  React.memo(_ProgramNotificationPage)
 );
 export default ProgramNotificationPage;
