a2f3b3fc78aa201128749ee1075bff2d005299be
Евгений Гребенщиков <evgreb@gmail.com>
2019-08-05 20:16:32 +0300

    add sorting and paging to filter-hook

--- packages/shared/hooks/route-filters.hook.tsx
+++ packages/shared/hooks/route-filters.hook.tsx
@@ -1,22 +1,28 @@
 import { useRouter } from "next/router";
 import qs from "qs";
-import { useContext } from "react";
 
-import { calculateTotalPages } from "../components/table/helpers/paging.helpers";
-import { PROGRAMS_ROUTE } from "../routes/programs.routes";
+import { TFilter } from "../components/table/components/filtering/filter.type";
 
 const useRouteFilters = (url: string, defaultFilter: any): any => {
   const { asPath, pathname, push } = useRouter();
-  const filtering = qs.parse(asPath.slice(pathname.length + 1));
-  const updateFilter = (filter: any) => {
+  const queryParams = qs.parse(asPath.slice(pathname.length + 1));
+  const { sorting = "", page = 1, ...filtering } = queryParams;
+
+  const updateFilter = (filter: TFilter) => {
     const nf = {
-      ...filtering,
+      ...queryParams,
       [filter.name]: filter.value
     };
     const route = `${url}?${qs.stringify(nf)}`;
     push(route);
   };
-  return [{ ...filtering, ...defaultFilter }, updateFilter];
+
+  return [
+    { ...defaultFilter, ...filtering },
+    sorting,
+    parseInt(page),
+    updateFilter
+  ];
 };
 
 export default useRouteFilters;
