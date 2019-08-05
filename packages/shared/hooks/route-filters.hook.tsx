a08e5bc7fff2312a8c96ba366f2fbbd32673d3ac
Евгений Гребенщиков <evgreb@gmail.com>
2019-08-05 20:23:17 +0300

    fix hook types

--- packages/shared/hooks/route-filters.hook.tsx
+++ packages/shared/hooks/route-filters.hook.tsx
@@ -1,14 +1,17 @@
 import { useRouter } from "next/router";
 import qs from "qs";
 
-import { TFilter } from "../components/table/components/filtering/filter.type";
+import {
+  FilteringType,
+  TFilter
+} from "../components/table/components/filtering/filter.type";
 
-const useRouteFilters = (url: string, defaultFilter: any): any => {
+const useRouteFilters = (url: string, defaultFilter: any): UseRouteFilters => {
   const { asPath, pathname, push } = useRouter();
   const queryParams = qs.parse(asPath.slice(pathname.length + 1));
   const { sorting = "", page = 1, ...filtering } = queryParams;
 
-  const updateFilter = (filter: TFilter) => {
+  const updateFilter: UpdateFilter = filter => {
     const nf = {
       ...queryParams,
       [filter.name]: filter.value
@@ -26,3 +29,6 @@ const useRouteFilters = (url: string, defaultFilter: any): any => {
 };
 
 export default useRouteFilters;
+
+type UpdateFilter = (filter: TFilter) => void;
+type UseRouteFilters = [FilteringType, string, number, UpdateFilter];
