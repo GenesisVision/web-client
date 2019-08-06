ef1d7500199ee85c8edfc92a2e8cc25e74a218a9
pavelzubov <pasha224@yandex.ru>
2019-08-06 09:57:05 +0300

    Set redirecting on next router

--- packages/manager/src/pages/programs/programs-settings/services/program-settings.service.ts
+++ packages/manager/src/pages/programs/programs-settings/services/program-settings.service.ts
@@ -1,20 +1,13 @@
-import { push } from "connected-react-router";
-import { Dispatch } from "redux";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
-import { RootState } from "shared/reducers/root-reducer";
-import {
-  PROGRAMS_ROUTE,
-  PROGRAM_DETAILS_ROUTE,
-  PROGRAM_SLUG_URL_PARAM_NAME
-} from "shared/routes/programs.routes";
+import { PROGRAMS_ROUTE } from "shared/routes/programs.routes";
 import managerApi from "shared/services/api-client/manager-api";
 import authService from "shared/services/auth-service";
-import getParams from "shared/utils/get-params";
 import {
   ManagerThunk,
   ResponseError,
   SetSubmittingType
 } from "shared/utils/types";
+import Router from "next/router";
 
 export const cancelChangeBrokerMethod = (
   programId: string
@@ -57,14 +50,6 @@ export const changeBrokerMethod = (
       dispatch(alertMessageActions.error(error.errorMessage));
     });
 
-export const redirectToProgram = () => (
-  dispatch: Dispatch,
-  getState: () => RootState
-) => {
-  /*const { router } = getState();
-  const programSlugUrl = getParams(
-    router.location.pathname,
-    PROGRAM_DETAILS_ROUTE
-  )[PROGRAM_SLUG_URL_PARAM_NAME];
-  dispatch(push(`${PROGRAMS_ROUTE}/${programSlugUrl}`));*/
-};
+export const redirectToProgram = (id: string) =>
+  Router.replace(`${PROGRAMS_ROUTE}/${id}`);
+
