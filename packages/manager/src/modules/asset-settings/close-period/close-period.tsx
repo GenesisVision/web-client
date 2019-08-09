a3c432ef62a8c06bdf07724d11c4b6afa10f3d46
pavelzubov <pasha224@yandex.ru>
2019-08-09 07:21:42 +0300

    Remove disabled from button Correct way to translation Add withLoader

--- packages/manager/src/modules/asset-settings/close-period/close-period.tsx
+++ packages/manager/src/modules/asset-settings/close-period/close-period.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 import { compose } from "redux";
 import GVButton from "shared/components/gv-button";
+import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 import useIsOpen from "shared/hooks/is-open.hook";
 
 import ConfirmClosePeriodContainer from "./confirm-close-period-container";
 
-const _CloseAssetPeriod: React.FC<Props> = ({ id, t, onApply, canClose }) => {
+const _CloseAssetPeriod: React.FC<Props> = ({ id, t, onApply }) => {
   const [
     isClosePeriodOpen,
     setClosePeriodOpen,
@@ -16,14 +17,10 @@ const _CloseAssetPeriod: React.FC<Props> = ({ id, t, onApply, canClose }) => {
     <>
       <div className="asset-settings__block-wrapper">
         <p className="asset-settings__text">
-          {t("manager.asset-settings.period-and-closing.text-period")}
+          {t("manager.program-settings.period-and-closing.text-period")}
         </p>
-        <GVButton
-          color="primary"
-          disabled={!canClose}
-          onClick={setClosePeriodOpen}
-        >
-          {t("asset-details-page.close-period.title")}
+        <GVButton color="primary" onClick={setClosePeriodOpen}>
+          {t("program-details-page.close-period.title")}
         </GVButton>
       </div>
       <ConfirmClosePeriodContainer
@@ -38,13 +35,13 @@ const _CloseAssetPeriod: React.FC<Props> = ({ id, t, onApply, canClose }) => {
 
 interface Props extends OwnProps, WithTranslation {}
 
-interface OwnProps {
-  canClose: boolean;
+interface OwnProps extends WithLoaderProps {
   onApply: () => void;
   id: string;
 }
 
 const CloseAssetPeriod = compose<React.ComponentType<OwnProps>>(
+  withLoader,
   translate(),
   React.memo
 )(_CloseAssetPeriod);
