de738a5744a1f1442904a83eb726bea26a191b1b
pavelzubov <pasha224@yandex.ru>
2019-08-09 07:17:28 +0300

    Add types for asset settings

---
+++ packages/manager/src/modules/asset-settings/asset-settings.types.ts
@@ -0,0 +1,15 @@
+import { FundDetailsFull, ProgramDetailsFull } from "gv-api-web";
+import { IImageValue } from "shared/components/form/input-image/input-image";
+import { SetSubmittingType } from "shared/utils/types";
+
+export type AssetDescriptionType = ProgramDetailsFull & FundDetailsFull;
+
+export type TUpdateAssetFunc = (
+  values: {
+    description?: string;
+    logo?: IImageValue;
+    investmentLimit?: number;
+    stopOutLevel?: number;
+  },
+  setSubmitting: SetSubmittingType
+) => void;
