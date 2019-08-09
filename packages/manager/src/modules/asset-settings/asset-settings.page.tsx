15be2af08e0783040f7cae73a9539626ea9b589b
pavelzubov <pasha224@yandex.ru>
2019-08-09 07:23:42 +0300

    Refactoring to use with fund and program

--- packages/manager/src/modules/asset-settings/asset-settings.page.tsx
+++ packages/manager/src/modules/asset-settings/asset-settings.page.tsx
@@ -1,82 +1,37 @@
 import "shared/components/details/details.scss";
 
-import { BrokersProgramInfo, ProgramDetailsFull } from "gv-api-web";
-import { programEditSignal } from "modules/program-signal/program-edit-signal/services/program-edit-signal.service";
-import React, { useCallback, useEffect, useState } from "react";
+import "./asset-settings.scss";
+import { TUpdateProgramFunc } from "pages/programs/programs-settings/program-settings.page";
+import React, { useCallback, useEffect } from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { ResolveThunks, connect } from "react-redux";
+import { connect, ResolveThunks } from "react-redux";
 import {
   ActionCreatorsMapObject,
-  Dispatch,
   bindActionCreators,
-  compose
+  compose,
+  Dispatch
 } from "redux";
-import { IImageValue } from "shared/components/form/input-image/input-image";
 import Page from "shared/components/page/page";
-import { programDescriptionSelector } from "shared/components/programs/program-details/reducers/description.reducer";
-import {
-  dispatchProgramDescription,
-  getProgramBrokers
-} from "shared/components/programs/program-details/services/program-details.service";
 import { ASSET } from "shared/constants/constants";
-import { RootState } from "shared/reducers/root-reducer";
-import { SetSubmittingType } from "shared/utils/types";
+import { DispatchDescriptionType } from "shared/utils/types";
 
-import AssetSettings from "./asset-settings";
-import AssetSettingsLoader from "./asset-settings.loader";
-import { ChangeBrokerFormValues } from "./change-broker/change-broker-form";
-import {
-  cancelChangeBrokerMethod,
-  changeBrokerMethod,
-  editAsset,
-  redirectToProgram
-} from "./services/asset-settings.service";
-import { IAssetSignalFormValues } from "./signaling-edit";
+import { AssetDescriptionType, TUpdateAssetFunc } from "./asset-settings.types";
+import { editAsset } from "./services/asset-settings.service";
 
 const _AssetsEditPage: React.FC<Props> = ({
-  service: {
-    dispatchProgramDescription,
-    programEditSignal,
-    changeBrokerMethod,
-    cancelChangeBrokerMethod,
-    editAsset,
-    redirectToAsset
-  },
+  asset,
+  effect,
+  settingsBlocks,
+  service: { dispatchDescription, editAsset, redirectToAsset },
   t,
   description
 }) => {
-  const [brokersInfo, setBrokersInfo] = useState<
-    BrokersProgramInfo | undefined
-  >(undefined);
   useEffect(() => {
-    dispatchProgramDescription();
+    dispatchDescription();
   }, []);
   useEffect(
     () => {
-      description && getProgramBrokers(description.id).then(setBrokersInfo);
-    },
-    [description]
-  );
-  const changeSignaling = useCallback(
-    ({ volumeFee, successFee }: IAssetSignalFormValues) =>
-      programEditSignal(description!.id, successFee!, volumeFee!).then(
-        dispatchProgramDescription
-      ),
-    [description]
-  );
-  const changeBroker = useCallback(
-    ({ brokerAccountTypeId, leverage }: ChangeBrokerFormValues) => {
-      changeBrokerMethod(description!.id, brokerAccountTypeId, leverage).then(
-        dispatchProgramDescription
-      );
-    },
-    [description]
-  );
-  const cancelChangeBroker = useCallback(
-    () => {
-      cancelChangeBrokerMethod(description!.id).then(
-        dispatchProgramDescription
-      );
+      effect();
     },
     [description]
   );
@@ -89,85 +44,68 @@ const _AssetsEditPage: React.FC<Props> = ({
         logo: { src: description!.logo },
         investmentLimit: description!.availableInvestmentLimit
       };
-      editAsset(
-        description!.id,
-        { ...currentValues, ...values },
-        ASSET.PROGRAM
-      ).then(dispatchProgramDescription);
+      editAsset(description!.id, { ...currentValues, ...values }, asset).then(
+        dispatchDescription
+      );
     },
     [description]
   );
   const applyCloseAsset = useCallback(() => redirectToAsset(), []);
+  const title = t("manager.asset-settings.title", {
+    asset: String(asset).toLowerCase()
+  });
   return (
-    <Page title={t("manager.asset-settings.title")}>
-      <AssetSettings
-        condition={!!description && !!brokersInfo}
-        loader={<AssetSettingsLoader />}
-        changeSignaling={changeSignaling}
-        closePeriod={dispatchProgramDescription}
-        closeAsset={applyCloseAsset}
-        details={description!}
-        brokersInfo={brokersInfo!}
-        changeBroker={changeBroker}
-        editAsset={editAssetCallback}
-        cancelChangeBroker={cancelChangeBroker}
-      />
+    <Page title={title}>
+      <div className="asset-settings">
+        <h1 className="asset-settings__title">{title}</h1>
+        {settingsBlocks(editAssetCallback, applyCloseAsset, description)}
+      </div>
     </Page>
   );
 };
 
-const mapStateToProps = (state: RootState): StateProps => ({
-  description: programDescriptionSelector(state)
-});
-
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+const mapDispatchToProps = (
+  dispatch: Dispatch,
+  { dispatchDescription, redirectToAsset }: Props
+): DispatchProps => ({
   service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
     {
-      cancelChangeBrokerMethod,
-      dispatchProgramDescription,
+      dispatchDescription,
       editAsset,
-      programEditSignal,
-      changeBrokerMethod,
-      redirectToProgram
+      redirectToAsset
     },
     dispatch
   )
 });
 
-export type TUpdateAssetFunc = (
-  values: {
-    description?: string;
-    logo?: IImageValue;
-    investmentLimit?: number;
-    stopOutLevel?: number;
-  },
-  setSubmitting: SetSubmittingType
-) => void;
-
-interface OwnProps {}
-
-interface StateProps {
-  description?: ProgramDetailsFull;
+interface OwnProps {
+  redirectToAsset: () => void;
+  asset: ASSET;
+  description?: AssetDescriptionType;
+  dispatchDescription: DispatchDescriptionType;
+  effect: () => void;
+  settingsBlocks: (
+    editAsset: TUpdateProgramFunc,
+    closeAsset: () => void,
+    description?: AssetDescriptionType
+  ) => JSX.Element;
 }
 
 interface ServiceThunks extends ActionCreatorsMapObject {
-  cancelChangeBrokerMethod: typeof cancelChangeBrokerMethod;
-  dispatchProgramDescription: typeof dispatchProgramDescription;
+  dispatchDescription: DispatchDescriptionType;
   editAsset: typeof editAsset;
-  programEditSignal: typeof programEditSignal;
-  changeBrokerMethod: typeof changeBrokerMethod;
-  redirectToProgram: typeof redirectToProgram;
+  redirectToAsset: () => void;
 }
 interface DispatchProps {
   service: ResolveThunks<ServiceThunks>;
 }
 
-interface Props extends OwnProps, DispatchProps, WithTranslation, StateProps {}
+interface Props extends OwnProps, DispatchProps, WithTranslation {}
 
 const AssetSettingsPage = compose<React.ComponentType<OwnProps>>(
   translate(),
   connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
   ),
   React.memo
