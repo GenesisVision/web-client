5628f596d30286954254ad9d7076517573f80fe9
pavelzubov <pasha224@yandex.ru>
2019-08-09 07:24:30 +0300

    Change way to SettingsBlock Add condition to ClosePeriod Add asset prop

--- packages/manager/src/modules/asset-settings/period-and-closing.tsx
+++ packages/manager/src/modules/asset-settings/period-and-closing.tsx
@@ -1,11 +1,13 @@
+import SettingsBlock from "modules/asset-settings/settings-block";
 import React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { ASSET } from "shared/constants/constants";
 
 import CloseAsset from "./close-asset/close-asset";
 import ClosePeriod from "./close-period/close-period";
-import SettingsBlock from "./settings-block";
 
 const _PeriodAndClosing: React.FC<Props> = ({
+  asset,
   t,
   id,
   canClosePeriod,
@@ -17,14 +19,20 @@ const _PeriodAndClosing: React.FC<Props> = ({
     label={t("manager.asset-settings.period-and-closing.title")}
     content={
       <>
-        <ClosePeriod canClose={canClosePeriod} onApply={closePeriod} id={id} />
-        <CloseAsset canClose={canCloseAsset} onApply={closeAsset} id={id} />
+        <ClosePeriod condition={canClosePeriod} onApply={closePeriod} id={id} />
+        <CloseAsset
+          asset={asset}
+          canClose={canCloseAsset}
+          onApply={closeAsset}
+          id={id}
+        />
       </>
     }
   />
 );
 
 interface Props extends WithTranslation {
+  asset: ASSET;
   id: string;
   canClosePeriod: boolean;
   canCloseAsset: boolean;
