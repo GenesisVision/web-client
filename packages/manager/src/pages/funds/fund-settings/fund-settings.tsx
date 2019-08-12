f9cdf7cb6ecf1bf1a726cc3d48849f94177ec546
pavelzubov <pasha224@yandex.ru>
2019-08-12 15:13:54 +0300

    Add Reallocation

--- packages/manager/src/pages/funds/fund-settings/fund-settings.tsx
+++ packages/manager/src/pages/funds/fund-settings/fund-settings.tsx
@@ -1,4 +1,4 @@
-import { FundDetailsFull } from "gv-api-web";
+import { FundDetailsFull, PlatformAsset } from "gv-api-web";
 import AssetEdit from "modules/asset-settings/asset-edit";
 import PeriodAndClosing from "modules/asset-settings/period-and-closing";
 import React from "react";
@@ -8,8 +8,11 @@ import { ASSET } from "shared/constants/constants";
 import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 
 import { TUpdateProgramFunc } from "./fund-settings.page";
+import Reallocation from "./reallocation/reallocation";
 
 const _FundSettings: React.FC<Props> = ({
+  reallocate,
+  platformAssets,
   t,
   details,
   closeAsset,
@@ -29,6 +32,13 @@ const _FundSettings: React.FC<Props> = ({
         description={details.description}
         onSubmit={editAsset}
       />
+      <Reallocation
+        canReallocate={details.personalFundDetails.canReallocate}
+        onApply={reallocate}
+        id={details.id}
+        fundAssets={details.currentAssets}
+        platformAssets={platformAssets}
+      />
     </>
   );
 };
@@ -36,6 +46,8 @@ const _FundSettings: React.FC<Props> = ({
 interface Props extends OwnProps, WithTranslation {}
 
 interface OwnProps {
+  reallocate: () => void;
+  platformAssets: PlatformAsset[];
   details: FundDetailsFull;
   closeAsset: () => void;
   editAsset: TUpdateProgramFunc;
