fe081b3c2cd2a5b8f64963f16cd65009b4c1ea8a
pavelzubov <pasha224@yandex.ru>
2019-08-15 18:05:37 +0300

    Add VerificationStatus Set content as optional

--- packages/shared/modules/asset-settings/settings-block.tsx
+++ packages/shared/modules/asset-settings/settings-block.tsx
@@ -1,14 +1,29 @@
 import React from "react";
+import VerificationStatus, {
+  IStatusProps
+} from "shared/components/verification-status/verification-status";
 
-const _SettingsBlock: React.FC<Props> = ({ label, content }) => (
+const _SettingsBlock: React.FC<Props> = ({
+  label,
+  content,
+  checked,
+  verificationStatus
+}) => (
   <section className="asset-settings__block">
     {label && <h3>{label}</h3>}
+    {checked ||
+      (!!verificationStatus && (
+        <VerificationStatus
+          checked={checked}
+          verificationStatus={verificationStatus}
+        />
+      ))}
     <div className="asset-settings__block-wrapper">{content}</div>
   </section>
 );
 
-interface OwnProps {
-  content: JSX.Element;
+interface OwnProps extends IStatusProps {
+  content?: JSX.Element;
   label?: string;
 }
 interface Props extends OwnProps {}
