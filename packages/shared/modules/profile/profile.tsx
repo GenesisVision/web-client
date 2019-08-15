f1614bcc74ba2de15b9bf332862af3bdeb1f55b5
pavelzubov <pasha224@yandex.ru>
2019-08-15 11:02:24 +0300

    Use ProfileBlock

--- packages/shared/modules/profile/profile.tsx
+++ packages/shared/modules/profile/profile.tsx
@@ -5,12 +5,12 @@ import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 import { compose } from "redux";
 import ProfileImageContainer from "shared/components/profile/settings/profile-image/profile-image-container";
-import VerificationStatus from "shared/components/verification-status/verification-status";
 import { ROLE } from "shared/constants/constants";
 import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 import withRole, { WithRoleProps } from "shared/decorators/with-role";
 
 import About from "../about/about";
+import ProfileBlock from "./profile-block";
 import { ProfileField } from "./profile-personal";
 
 const _Profile: React.FC<Props> = ({ t, info, role }) => {
@@ -22,101 +22,46 @@ const _Profile: React.FC<Props> = ({ t, info, role }) => {
         <tbody>
           {isManager && (
             <>
-              <tr className="profile__title">
-                <td className="profile__left">
-                  <h4 className="profile__subtitle">01</h4>
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <h4 className="profile__subtitle">
-                    {t("profile-page.public-info")}
-                  </h4>
-                </td>
-              </tr>
-              <About about={info.about} userName={info.userName} />
-              <tr className="profile__title">
-                <td className="profile__left">
-                  <h4 className="profile__subtitle">02</h4>
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
-                  <h4 className="profile__subtitle">{t("profile-page.id")}</h4>
-                </td>
-              </tr>
-              <tr className="profile__content">
-                <td className="profile__left">
-                  <span className="profile__stick" />
-                </td>
-                <td className="profile__center" />
-                <td className="profile__right">
+              <ProfileBlock
+                number={"01"}
+                title={t("profile-page.public-info")}
+                content={<About about={info.about} userName={info.userName} />}
+              />
+              <ProfileBlock
+                number={"02"}
+                title={t("profile-page.public-info")}
+                content={
                   <ProfileField
                     label={t("profile-page.id")}
                     value={info.id}
-                    name="id"
+                    name="phone"
                   />
-                </td>
-              </tr>
+                }
+              />
             </>
           )}
-          <tr className="profile__title">
-            <td className="profile__left">
-              <h4 className="profile__subtitle">0{initCount + 1}</h4>
-            </td>
-            <td className="profile__center" />
-            <td className="profile__right">
-              <h4 className="profile__subtitle">
-                {t("profile-page.settings.profile-image")}
-              </h4>
-            </td>
-          </tr>
-          <tr className="profile__content">
-            <td className="profile__left">
-              <span className="profile__stick" />
-            </td>
-            <td className="profile__center" />
-            <td className="profile__right">
-              <ProfileImageContainer />
-            </td>
-          </tr>
-          <tr className="profile__title">
-            <td className="profile__left">
-              <h4 className="profile__subtitle">0{initCount + 2}</h4>
-            </td>
-            <td className="profile__center" />
-            <td className="profile__right">
-              <h4 className="profile__subtitle">
-                {t("profile-page.contacts")}
-              </h4>
-              <VerificationStatus checked={true} />
-            </td>
-          </tr>
-          <tr className="profile__content">
-            <td className="profile__left">
-              <span className="profile__stick" />
-            </td>
-            <td className="profile__center" />
-            <td className="profile__right">
+          <ProfileBlock
+            number={`0${initCount + 1}`}
+            title={t("profile-page.settings.profile-image")}
+            content={<ProfileImageContainer />}
+          />
+          <ProfileBlock
+            number={`0${initCount + 2}`}
+            title={t("profile-page.contacts")}
+            content={
               <ProfileField
                 label={t("profile-page.email")}
                 value={info.email}
                 name="phone"
               />
-            </td>
-          </tr>
-          <tr className="profile__title">
-            <td className="profile__left">
-              <h4 className="profile__subtitle">0{initCount + 3}</h4>
-            </td>
-            <td className="profile__center" />
-            <td className="profile__right">
-              <h4 className="profile__subtitle">
-                {t("profile-page.personal-info")}
-              </h4>
-              <VerificationStatus
-                verificationStatus={info.verificationStatus}
-              />
-            </td>
-          </tr>
+            }
+            checked={true}
+          />
+          <ProfileBlock
+            number={`0${initCount + 3}`}
+            title={t("profile-page.contacts")}
+            verificationStatus={info.verificationStatus}
+          />
         </tbody>
       </table>
     </div>
