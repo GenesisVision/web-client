0a30e764dc16f8ef478e89e16f4ff4d07ac76ad8
pavelzubov <pasha224@yandex.ru>
2019-08-17 08:10:50 +0300

    Add security route

--- packages/manager/src/pages/profile/profile.routes.tsx
+++ packages/manager/src/pages/profile/profile.routes.tsx
@@ -7,10 +7,12 @@ import {
   PASSWORD_ROUTE,
   PROFILE_EDIT_ROUTE,
   PROFILE_ROUTE,
+  SECURITY_ROUTE,
   SETTINGS_ROUTE,
   SOCIAL_LINKS_ROUTE
 } from "shared/components/profile/profile.constants";
 import ProfilePage from "shared/components/profile/profile/profile.page";
+import SecurityPage from "shared/components/profile/security/security.page";
 import SettingsPage from "shared/components/profile/settings/settings.page";
 
 import ProfileEditPage from "./edit/edit.page";
@@ -25,6 +27,7 @@ const ProfileRoutes: React.FC = () => (
     <Route path={SETTINGS_ROUTE} component={SettingsPage} />
     <Route path={SOCIAL_LINKS_ROUTE} component={SocialLinksPage} />
     <Route path={PROFILE_EDIT_ROUTE} component={ProfileEditPage} />
+    <Route path={SECURITY_ROUTE} component={SecurityPage} />
     <Route path={PROFILE_ROUTE} component={ProfilePageContainer} />
   </Switch>
 );
