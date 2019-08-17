905903182bea406fdf247cd012944d7374b9c214
pavelzubov <pasha224@yandex.ru>
2019-08-17 08:41:07 +0300

    Add security route

--- packages/investor/src/pages/profile/profile.routes.tsx
+++ packages/investor/src/pages/profile/profile.routes.tsx
@@ -6,9 +6,11 @@ import {
   KYC_ROUTE,
   PASSWORD_ROUTE,
   PROFILE_ROUTE,
+  SECURITY_ROUTE,
   SETTINGS_ROUTE
 } from "shared/components/profile/profile.constants";
 import ProfilePage from "shared/components/profile/profile/profile.page";
+import SecurityPage from "shared/components/profile/security/security.page";
 import SettingsPage from "shared/components/profile/settings/settings.page";
 
 const ProfileRoutes: React.FC = () => (
@@ -16,6 +18,7 @@ const ProfileRoutes: React.FC = () => (
     <Route path={KYC_ROUTE} component={KYCPage} />
     <Route path={PASSWORD_ROUTE} component={PasswordPage} />
     <Route path={SETTINGS_ROUTE} component={SettingsPage} />
+    <Route path={SECURITY_ROUTE} component={SecurityPage} />
     <Route path={PROFILE_ROUTE} component={ProfilePage} />
   </Switch>
 );
