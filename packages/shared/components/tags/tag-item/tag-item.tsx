3417f7cee0367b1f100bf1aecce7df332ecf5db3
pavelzubov <pasha224@yandex.ru>
2019-08-18 08:47:24 +0300

    Use TagBubble

--- packages/shared/components/tags/tag-item/tag-item.tsx
+++ packages/shared/components/tags/tag-item/tag-item.tsx
@@ -3,14 +3,11 @@ import "./tag-item.scss";
 import { ProgramTag } from "gv-api-web";
 import * as React from "react";
 
-const _TagItem: React.FC<ProgramTag> = ({ color, name }) => {
-  const styleTag = { color, backgroundColor: `${color}1a` };
-  return (
-    <div className="tag-item tag-button" style={styleTag}>
-      {name}
-    </div>
-  );
-};
+import TagBubble from "./tag-bubble";
+
+const _TagItem: React.FC<ProgramTag> = ({ color, name }) => (
+  <TagBubble color={color} content={name} />
+);
 
 const TagItem = React.memo(_TagItem);
 export default TagItem;
