6ec681a82b3de58dcb0e58230111048ed0d2c931
pavelzubov <pasha224@yandex.ru>
2019-08-19 13:39:12 +0300

    Set Promise.all instead one promise in fetchFundProfitChartAction

--- packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
+++ packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
@@ -1,22 +1,43 @@
-import { FundBalanceChart, FundDetailsFull, FundProfitChart } from "gv-api-web";
-import { getDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
+import {
+  CancelablePromise,
+  FundBalanceChart,
+  FundDetailsFull,
+  FundProfitChart
+} from "gv-api-web";
+import {
+  ChartDefaultPeriod,
+  getDefaultPeriod
+} from "shared/components/chart/chart-period/chart-period.helpers";
 import fundsApi from "shared/services/api-client/funds-api";
-import { ApiAction } from "shared/utils/types";
+import { ApiAction, CurrencyEnum } from "shared/utils/types";
+
+import { FundProfitChartDataType } from "../reducers/profit-chart.reducer";
 
 export const FETCH_FUND_PROFIT_CHART = "FETCH_FUND_PROFIT_CHART";
 export const FETCH_FUND_BALANCE_CHART = "FETCH_FUND_BALANCE_CHART";
 export const FETCH_FUND_DESCRIPTION = "FETCH_FUND_DESCRIPTION";
 
+const sendFundChartRequest = (
+  { start, end }: ChartDefaultPeriod,
+  id: string,
+  currency: CurrencyEnum
+): Promise<FundProfitChart> =>
+  fundsApi.v10FundsByIdChartsProfitGet(id, {
+    dateFrom: start,
+    dateTo: end,
+    maxPointCount: 100,
+    currency
+  });
+
 export const fetchFundProfitChartAction = (
   id: string,
-  period = getDefaultPeriod()
-): ApiAction<FundProfitChart> => ({
+  period = getDefaultPeriod(),
+  currencies: CurrencyEnum[]
+): ApiAction<FundProfitChartDataType> => ({
   type: FETCH_FUND_PROFIT_CHART,
-  payload: fundsApi.v10FundsByIdChartsProfitGet(id, {
-    dateFrom: period.start,
-    dateTo: period.end,
-    maxPointCount: 100
-  })
+  payload: Promise.all(
+    currencies.map(currency => sendFundChartRequest(period, id, currency))
+  ) as CancelablePromise<FundProfitChartDataType>
 });
 
 export const fetchFundBalanceChartAction = (
