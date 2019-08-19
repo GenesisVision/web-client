5b0c24989ffe9f80abc87b8ba003c5c4a827b98d
pavelzubov <pasha224@yandex.ru>
2019-08-19 13:55:12 +0300

    Add change currency logic

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
@@ -1,5 +1,6 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-statistic-section.scss";
 
+import { PlatformAsset } from "gv-api-web";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { ResolveThunks, connect } from "react-redux";
@@ -9,11 +10,19 @@ import {
   bindActionCreators,
   compose
 } from "redux";
+import { createSelector } from "reselect";
 import {
   ChartDefaultPeriod,
   DEFAULT_PERIOD
 } from "shared/components/chart/chart-period/chart-period.helpers";
+import { ISelectChangeEvent } from "shared/components/select/select";
+import { TChartCurrency } from "shared/modules/chart-currency-selector/chart-currency-selector";
+import {
+  currenciesSelector,
+  fundAssetsSelector
+} from "shared/reducers/platform-reducer";
 import { RootState } from "shared/reducers/root-reducer";
+import { CurrencyEnum } from "shared/utils/types";
 
 import {
   FundBalanceChartDataType,
@@ -31,26 +40,63 @@ import FundDetailsChart from "./fund-details-chart-section/fund-details-chart";
 import FundDetailsStatistics from "./fund-details-statistics/fund-details-statistics";
 
 const _FundDetailsStatisticSection: React.FC<Props> = ({
+  currencyValues,
+  chartCurrencies,
   balanceChart,
   profitChart,
   id,
   service: { getProfitChart, getBalanceChart }
 }) => {
   const [period, setPeriod] = useState<ChartDefaultPeriod>(DEFAULT_PERIOD);
+  const [currencies, setCurrencies] = useState<TChartCurrency[]>([
+    ...chartCurrencies.filter(chartCurrency => chartCurrency.mandatory)
+  ]);
+  const addCurrency = () => {
+    setCurrencies([
+      ...currencies,
+      chartCurrencies.find(({ asset }) => asset === currencyValues[0])!
+    ]);
+  };
+  const removeCurrency = (id: string) => {
+    setCurrencies([...currencies.filter(item => item.id !== id)]);
+  };
+  const changeCurrency = (i: number) => (event: ISelectChangeEvent) => {
+    currencies[i] = chartCurrencies.find(
+      ({ asset }) => asset === event.target.value
+    )!;
+    setCurrencies([...currencies]);
+  };
   useEffect(
     () => {
-      getProfitChart({ id, period });
       getBalanceChart({ id, period });
     },
     [period, id]
   );
+  useEffect(
+    () => {
+      getProfitChart({
+        id,
+        period,
+        currencies: currencies.map(({ asset }) => asset)
+      });
+    },
+    [period, id, currencies]
+  );
   return (
     <div className="details-statistic-section">
       <div className="details-statistic-section__statistic">
-        <FundDetailsStatistics statistic={profitChart} period={period} />
+        <FundDetailsStatistics
+          condition={!!profitChart}
+          statistic={profitChart && profitChart[0]}
+          period={period}
+        />
       </div>
       <div className="details-statistic-section__chart">
         <FundDetailsChart
+          chartCurrencies={currencies}
+          addChartCurrency={addCurrency}
+          removeChartCurrency={removeCurrency}
+          changeChartCurrency={changeCurrency}
           profitChart={profitChart}
           balanceChart={balanceChart}
           period={period}
@@ -68,7 +114,26 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   )
 });
 
+const convertToChartCurrency = ({
+  id,
+  asset,
+  color
+}: PlatformAsset): TChartCurrency => ({
+  id,
+  asset: asset as CurrencyEnum,
+  color,
+  mandatory: asset === "GVT"
+});
+
+const chartCurrenciesSelector = createSelector<
+  RootState,
+  PlatformAsset[],
+  TChartCurrency[]
+>(state => fundAssetsSelector(state), data => data.map(convertToChartCurrency));
+
 const mapStateToProps = (state: RootState): StateProps => ({
+  currencyValues: currenciesSelector(state) as CurrencyEnum[],
+  chartCurrencies: chartCurrenciesSelector(state),
   profitChart: fundProfitChartSelector(state),
   balanceChart: fundBalanceChartSelector(state)
 });
@@ -82,6 +147,8 @@ interface DispatchProps {
 }
 
 interface StateProps {
+  currencyValues: CurrencyEnum[];
+  chartCurrencies: TChartCurrency[];
   profitChart?: FundProfitChartDataType;
   balanceChart?: FundBalanceChartDataType;
 }
