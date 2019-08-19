266a68c11e369dc4df3f208e30d07734761d3123
pavelzubov <pasha224@yandex.ru>
2019-08-19 13:31:52 +0300

    Add chartCurrencies, addChartCurrency, removeChartCurrency, changeChartCurrency prop Correct profitChart type

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.tsx
@@ -1,19 +1,27 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-chart-section/details-chart-section.scss";
 
-import {
-  FundBalanceChart as FundBalanceChartType,
-  FundProfitChart
-} from "gv-api-web";
+import { FundBalanceChart as FundBalanceChartType } from "gv-api-web";
 import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
 import DetailsChartLoader from "shared/components/details/details-description-section/details-statistic-section/details-loader/details-chart-loader";
 import Surface from "shared/components/surface/surface";
+import {
+  TAddChartCurrency,
+  TChangeChartCurrency,
+  TChartCurrency,
+  TRemoveChartCurrency
+} from "shared/modules/chart-currency-selector/chart-currency-selector";
 import { HandlePeriodChangeType } from "shared/utils/types";
 
+import { FundProfitChartDataType } from "../../reducers/profit-chart.reducer";
 import FundDetailsChartElements from "./fund-details-chart-elements";
 
 const _FundDetailsChart: React.FC<Props> = ({
+  chartCurrencies,
+  addChartCurrency,
+  removeChartCurrency,
+  changeChartCurrency,
   t,
   period,
   onPeriodChange,
@@ -24,6 +32,10 @@ const _FundDetailsChart: React.FC<Props> = ({
     <h3>{t("fund-details-page.chart.heading")}</h3>
     <FundDetailsChartElements
       condition={!!profitChart && !!balanceChart}
+      chartCurrencies={chartCurrencies}
+      addChartCurrency={addChartCurrency}
+      removeChartCurrency={removeChartCurrency}
+      changeChartCurrency={changeChartCurrency}
       loader={<DetailsChartLoader />}
       profitChart={profitChart!}
       balanceChart={balanceChart!}
@@ -34,9 +46,13 @@ const _FundDetailsChart: React.FC<Props> = ({
 );
 
 interface Props extends WithTranslation {
+  chartCurrencies: TChartCurrency[];
+  addChartCurrency: TAddChartCurrency;
+  removeChartCurrency: TRemoveChartCurrency;
+  changeChartCurrency: TChangeChartCurrency;
   period: ChartDefaultPeriod;
   onPeriodChange: HandlePeriodChangeType;
-  profitChart?: FundProfitChart;
+  profitChart?: FundProfitChartDataType;
   balanceChart?: FundBalanceChartType;
 }
 
