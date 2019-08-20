2bb8d1c83b80376408a9568a2adfc7094ce58fc6
pavelzubov <pasha224@yandex.ru>
2019-08-20 17:30:02 +0300

    Add DetailsInvestment

--- packages/shared/components/programs/program-details/program-details.contaner.tsx
+++ packages/shared/components/programs/program-details/program-details.contaner.tsx
@@ -3,22 +3,35 @@ import "shared/components/details/details.scss";
 import { ProgramDetailsFull } from "gv-api-web";
 import * as React from "react";
 import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { ResolveThunks, connect } from "react-redux";
+import {
+  ActionCreatorsMapObject,
+  Dispatch,
+  bindActionCreators,
+  compose
+} from "redux";
+import DetailsInvestment from "shared/components/details/details-description-section/details-investment/details-investment";
+import { InvestmentDetails } from "shared/components/details/details-description-section/details-investment/details-investment.helpers";
 import Page from "shared/components/page/page";
 import ProgramDetailsDescriptionSection from "shared/components/programs/program-details/program-details-description/program-details-description-section";
 import ProgramDetailsStatisticSection from "shared/components/programs/program-details/program-details-statistic-section/program-details-statistic-section";
 import {
+  dispatchProgramDescription,
   fetchOpenPositions,
   fetchPeriodHistory,
   fetchProgramTrades
 } from "shared/components/programs/program-details/services/program-details.service";
-import { STATUS } from "shared/constants/constants";
-import withLoader from "shared/decorators/with-loader";
+import { ASSET, STATUS } from "shared/constants/constants";
+import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 import { CurrencyEnum } from "shared/utils/types";
 
+import SubscriptionDetailsContainer from "./program-details-description/subscription-details/subscription-details-container";
 import { IDescriptionSection, IHistorySection } from "./program-details.types";
 import ProgramDetailsHistorySection from "./program-history/program-details-history-section";
 
 const _ProgramDetailsContainer: React.FC<Props> = ({
+  service: { dispatchProgramDescription },
   isKycConfirmed,
   currency,
   isAuthenticated,
@@ -27,7 +40,8 @@ const _ProgramDetailsContainer: React.FC<Props> = ({
   historySection,
   description
 }) => {
-  const fetchHistoryPortfolioEvents = useCallback(
+  const [t] = useTranslation();
+  const fetchPortfolioEvents = useCallback(
     (filters: any) =>
       historySection.fetchPortfolioEvents({
         ...filters,
@@ -48,13 +62,45 @@ const _ProgramDetailsContainer: React.FC<Props> = ({
             isAuthenticated={isAuthenticated}
             redirectToLogin={redirectToLogin}
             ProgramControls={descriptionSection.ProgramControls}
-            ProgramWithdrawContainer={
-              descriptionSection.ProgramWithdrawContainer
-            }
-            ProgramReinvestingWidget={
-              descriptionSection.ProgramReinvestingWidget
-            }
           />
+        </div>
+        <div className="details__section">
+          {description.personalProgramDetails && isAuthenticated && (
+            <>
+              {description.personalProgramDetails.isInvested &&
+                description.personalProgramDetails.status !== STATUS.ENDED && (
+                  <DetailsInvestment
+                    eventTypeFilterValues={historySection.eventTypeFilterValues}
+                    fetchPortfolioEvents={fetchPortfolioEvents}
+                    updateDescription={dispatchProgramDescription}
+                    notice={t(
+                      "program-details-page.description.withdraw-notice-text"
+                    )}
+                    asset={ASSET.PROGRAM}
+                    id={description.id}
+                    assetCurrency={description.currency}
+                    accountCurrency={currency}
+                    personalDetails={
+                      description.personalProgramDetails as InvestmentDetails
+                    } // TODO fix type InvestmentDetails
+                    ProgramReinvestingWidget={
+                      descriptionSection.ProgramReinvestingWidget
+                    }
+                    WithdrawContainer={
+                      descriptionSection.ProgramWithdrawContainer
+                    }
+                  />
+                )}
+              {description.personalProgramDetails.signalSubscription
+                .hasActiveSubscription && (
+                <SubscriptionDetailsContainer
+                  id={description.id}
+                  currency={description.currency}
+                  personalDetails={description.personalProgramDetails}
+                />
+              )}
+            </>
+          )}
         </div>
         <div className="details__section">
           <ProgramDetailsStatisticSection
@@ -78,13 +124,11 @@ const _ProgramDetailsContainer: React.FC<Props> = ({
             fetchOpenPositions={fetchOpenPositions}
             fetchPeriodHistory={fetchPeriodHistory}
             fetchTrades={fetchProgramTrades}
-            fetchPortfolioEvents={fetchHistoryPortfolioEvents}
             fetchHistoryCounts={historySection.fetchHistoryCounts}
             programId={description.id}
             programCurrency={description.currency}
             currency={currency}
             isInvested={isInvested}
-            eventTypeFilterValues={historySection.eventTypeFilterValues}
             title={description.title}
           />
         </div>
@@ -92,6 +136,22 @@ const _ProgramDetailsContainer: React.FC<Props> = ({
     </Page>
   );
 };
+
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
+    {
+      dispatchProgramDescription
+    },
+    dispatch
+  )
+});
+
+interface ServiceThunks extends ActionCreatorsMapObject {
+  dispatchProgramDescription: typeof dispatchProgramDescription;
+}
+interface DispatchProps {
+  service: ResolveThunks<ServiceThunks>;
+}
 
 interface OwnProps {
   redirectToLogin: () => void;
@@ -103,9 +163,16 @@ interface OwnProps {
   currency: CurrencyEnum;
 }
 
-interface Props extends OwnProps {}
+interface Props extends OwnProps, DispatchProps {}
 
-const ProgramDetailsContainer = React.memo(
-  withLoader(_ProgramDetailsContainer)
-);
+const ProgramDetailsContainer = compose<
+  React.ComponentType<OwnProps & WithLoaderProps>
+>(
+  withLoader,
+  connect(
+    null,
+    mapDispatchToProps
+  ),
+  React.memo
+)(_ProgramDetailsContainer);
 export default ProgramDetailsContainer;
