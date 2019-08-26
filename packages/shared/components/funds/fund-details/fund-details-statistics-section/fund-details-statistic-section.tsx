12a0fcbe847726f2d6aebee6b043cb1ff9a79357
pavelzubov <pasha224@yandex.ru>
2019-08-26 11:02:25 +0300

    Remove unused items

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
@@ -1,28 +1,20 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-statistic-section.scss";
 
 import * as React from "react";
-import { compose } from "redux";
 
 import FundDetailsChart from "./fund-details-chart-section/fund-details-chart";
 import FundDetailsStatistics from "./fund-details-statistics/fund-details-statistics";
 
-const _FundDetailsStatisticSection: React.FC<Props> = () => {
-  return (
-    <div className="details-statistic-section">
-      <div className="details-statistic-section__statistic">
-        <FundDetailsStatistics />
-      </div>
-      <div className="details-statistic-section__chart">
-        <FundDetailsChart />
-      </div>
+const _FundDetailsStatisticSection: React.FC = () => (
+  <div className="details-statistic-section">
+    <div className="details-statistic-section__statistic">
+      <FundDetailsStatistics />
     </div>
-  );
-};
-interface OwnProps {}
+    <div className="details-statistic-section__chart">
+      <FundDetailsChart />
+    </div>
+  </div>
+);
 
-interface Props extends OwnProps {}
-
-const FundDetailsStatisticSection = compose<React.ComponentType<OwnProps>>(
-  React.memo
-)(_FundDetailsStatisticSection);
+const FundDetailsStatisticSection = React.memo(_FundDetailsStatisticSection);
 export default FundDetailsStatisticSection;
