d859125951dab2361b112b772ca0f3adc613c105
pavelzubov <pasha224@yandex.ru>
2019-08-27 10:39:11 +0300

    Change text to translation

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-tooltip.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-tooltip.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
+import { useTranslation } from "react-i18next";
 import ChartTooltip from "shared/components/chart/chart-tooltip/chart-tooltip";
 import FundAssetList from "shared/components/fund-asset-list/fund-asset-list";
 import FundAssetRatio from "shared/components/fund-asset-ratio/fund-asset-ratio";
 
 const FundProfitTooltip: React.FC<Props> = ({ active, label, payload }) => {
   if (!active || !payload.length) return null;
+  const [t] = useTranslation();
   return (
     <ChartTooltip
-      heading="Assets fund"
+      heading={t("fund-details-page.chart.assets")}
       date={new Date(label)}
       body={
         <div>
