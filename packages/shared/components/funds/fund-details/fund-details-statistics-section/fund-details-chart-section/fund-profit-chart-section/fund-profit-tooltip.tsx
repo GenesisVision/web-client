f0acff9daca033c19b135fd5b0d298a3b25bfb13
pavelzubov <pasha224@yandex.ru>
2019-08-27 10:40:58 +0300

    Revert translate instead text

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-tooltip.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-tooltip.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-import { useTranslation } from "react-i18next";
 import ChartTooltip from "shared/components/chart/chart-tooltip/chart-tooltip";
 import FundAssetList from "shared/components/fund-asset-list/fund-asset-list";
 import FundAssetRatio from "shared/components/fund-asset-ratio/fund-asset-ratio";
 
 const FundProfitTooltip: React.FC<Props> = ({ active, label, payload }) => {
   if (!active || !payload.length) return null;
-  const [t] = useTranslation();
   return (
     <ChartTooltip
-      heading={t("fund-details-page.chart.assets")}
+      heading={"Assets"}
       date={new Date(label)}
       body={
         <div>
