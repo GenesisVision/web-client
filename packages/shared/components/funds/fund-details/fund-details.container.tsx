b2580cb846fe11e45e7bf0c5a708c7a23078a64d
pavelzubov <pasha224@yandex.ru>
2019-08-29 13:24:35 +0300

    Fetch events for get events count Correct condition to show DetailsInvestment

--- packages/shared/components/funds/fund-details/fund-details.container.tsx
+++ packages/shared/components/funds/fund-details/fund-details.container.tsx
@@ -1,13 +1,13 @@
 import "shared/components/details/details.scss";
 
 import { FundDetailsFull } from "gv-api-web";
-import React, { useCallback } from "react";
-import { connect, ResolveThunks } from "react-redux";
+import React, { useCallback, useEffect, useState } from "react";
+import { ResolveThunks, connect } from "react-redux";
 import {
   ActionCreatorsMapObject,
+  Dispatch,
   bindActionCreators,
-  compose,
-  Dispatch
+  compose
 } from "redux";
 import DetailsInvestment from "shared/components/details/details-description-section/details-investment/details-investment";
 import { InvestmentDetails } from "shared/components/details/details-description-section/details-investment/details-investment.helpers";
@@ -17,7 +17,10 @@ import { ASSET } from "shared/constants/constants";
 import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
 import { CurrencyEnum } from "shared/utils/types";
 
-import { hasActiveInvestment } from "../../details/details-description-section/details-investment/investment-container";
+import {
+  haveActiveInvestment,
+  haveSubscription
+} from "../../details/details-description-section/details-investment/investment-container";
 import FundDetailsDescriptionSection from "./fund-details-description/fund-details-description-section";
 import FundDetailsHistorySection from "./fund-details-history-section/fund-details-history-section";
 import FundDetailsStatisticSection from "./fund-details-statistics-section/fund-details-statistic-section";
@@ -37,6 +40,12 @@ const _FundDetailsContainer: React.FC<Props> = ({
   historySection,
   description
 }) => {
+  const [haveEvents, setHaveEvents] = useState<boolean>(false);
+  useEffect(() => {
+    fetchHistoryPortfolioEvents({}).then(({ total }) =>
+      setHaveEvents(total > 0)
+    );
+  }, []);
   const fetchHistoryPortfolioEvents = useCallback(
     (filters: any) =>
       historySection.fetchPortfolioEvents({
@@ -45,9 +54,11 @@ const _FundDetailsContainer: React.FC<Props> = ({
       }),
     [historySection, description]
   );
-  const isInvested =
-    description.personalFundDetails &&
-    description.personalFundDetails.isInvested;
+  const haveInvestment = haveActiveInvestment(
+    description.personalFundDetails as InvestmentDetails
+  );
+  const showInvestment = haveEvents || haveInvestment;
+
   return (
     <Page title={description.title}>
       <div className="details">
@@ -60,12 +71,12 @@ const _FundDetailsContainer: React.FC<Props> = ({
             FundControls={descriptionSection.FundControls}
           />
         </div>
-        {hasActiveInvestment(
-          description.personalFundDetails as InvestmentDetails
-        ) && (
+        {showInvestment && (
           <div className="details__section">
             <div>
               <DetailsInvestment
+                haveEvents={haveEvents}
+                haveInvestment={haveInvestment}
                 eventTypeFilterValues={historySection.eventTypeFilterValues}
                 fetchPortfolioEvents={fetchHistoryPortfolioEvents}
                 updateDescription={dispatchFundDescription}
