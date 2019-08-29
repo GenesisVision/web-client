fca3c235048c950a764f6ebc827d5d156ce4eb92
pavelzubov <pasha224@yandex.ru>
2019-08-29 10:17:43 +0300

    Remove useless func

--- packages/investor/src/pages/dashboard/services/dashboard-events.services.ts
+++ packages/investor/src/pages/dashboard/services/dashboard-events.services.ts
@@ -4,7 +4,7 @@ import authService from "shared/services/auth-service";
 
 import * as actions from "../actions/dashboard.actions";
 
-export const getTopPortfolioEvents = () => (dispatch: Dispatch) => {
+export const getTopPortfolioEvents = (dispatch: Dispatch) => {
   const authorization = authService.getAuthArg();
 
   dispatch(
