90550ed5f206628eb414d50794c6195b7818a866
pavelzubov <pasha224@yandex.ru>
2019-08-30 13:49:41 +0300

    Add getEvents

--- packages/investor/src/pages/dashboard/services/dashboard-events.services.ts
+++ packages/investor/src/pages/dashboard/services/dashboard-events.services.ts
@@ -1,8 +1,12 @@
+import { CancelablePromise, InvestmentEventViewModels } from "gv-api-web";
 import { Dispatch } from "redux";
 import { EVENT_LOCATION } from "shared/components/programs/program-details/services/program-details.service";
+import { ComposeFiltersAllType } from "shared/components/table/components/filtering/filter.type";
 import authService from "shared/services/auth-service";
+import { ActionType } from "shared/utils/types";
 
 import * as actions from "../actions/dashboard.actions";
+import { fetchEventsAction } from "../actions/dashboard.actions";
 
 export const getTopPortfolioEvents = (dispatch: Dispatch) => {
   const authorization = authService.getAuthArg();
@@ -14,3 +18,8 @@ export const getTopPortfolioEvents = (dispatch: Dispatch) => {
     })
   );
 };
+
+export const getEvents = (eventLocation: EVENT_LOCATION) => (
+  filters: ComposeFiltersAllType
+): ActionType<CancelablePromise<InvestmentEventViewModels>> =>
+  fetchEventsAction(filters, eventLocation);
