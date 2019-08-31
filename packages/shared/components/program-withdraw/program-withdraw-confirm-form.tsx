1b95d5e3c14238252f0ceb6e76f3b5e031666d27
pavelzubov <pasha224@yandex.ru>
2019-08-31 09:19:44 +0300

    Convert to FC

--- packages/shared/components/program-withdraw/program-withdraw-confirm-form.tsx
+++ packages/shared/components/program-withdraw/program-withdraw-confirm-form.tsx
@@ -1,83 +1,78 @@
 import { InjectedFormikProps, withFormik } from "formik";
 import moment from "moment";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { compose } from "redux";
 import FormError from "shared/components/form/form-error/form-error";
 import GVButton from "shared/components/gv-button";
 import { formatCurrencyValue } from "shared/utils/formatter";
 import { SetSubmittingType } from "shared/utils/types";
 
-class _ProgramWithdrawConfirmForm extends React.PureComponent<
-  InjectedFormikProps<Props, {}>
-> {
-  render() {
-    const {
-      t,
-      programCurrency,
-      amount,
-      withdrawAll,
-      periodEnds,
-      isSubmitting,
-      errorMessage,
-      handleSubmit,
-      onBackClick
-    } = this.props;
-    return (
-      <form id="withdraw-submit-form" onSubmit={handleSubmit}>
-        <ul className="dialog-list">
-          <li className="dialog-list__item">
-            {t("withdraw-program.withdrawing")}
-            <span className="dialog-list__value">
-              {amount && !withdrawAll
-                ? `${formatCurrencyValue(
-                    amount,
-                    programCurrency
-                  )} ${programCurrency}`
-                : t("withdraw-program.all")}
-            </span>
-          </li>
-          <li className="dialog-list__item">
-            {t("withdraw-program.payout-date")}
-            <span className="dialog-list__value">
-              {moment(periodEnds).format()}
-            </span>
-          </li>
-        </ul>
-        <div className="form-error">
-          <FormError error={errorMessage} />
-        </div>
-        <div className="dialog__buttons">
-          <GVButton
-            onClick={onBackClick}
-            color="secondary"
-            variant="outlined"
-            title={"back"}
-          >
-            {t("withdraw-program.back")}
-          </GVButton>
-          <GVButton
-            title={"submit"}
-            type={"submit"}
-            id="programWithdrawFormSubmit"
-            disabled={isSubmitting}
-          >
-            {t("withdraw-program.submit")}
-          </GVButton>
-        </div>
-      </form>
-    );
-  }
-}
+const _ProgramWithdrawConfirmForm: React.FC<InjectedFormikProps<Props, {}>> = ({
+  programCurrency,
+  amount,
+  withdrawAll,
+  periodEnds,
+  isSubmitting,
+  errorMessage,
+  handleSubmit,
+  onBackClick
+}) => {
+  const [t] = useTranslation();
+  return (
+    <form id="withdraw-submit-form" onSubmit={handleSubmit}>
+      <ul className="dialog-list">
+        <li className="dialog-list__item">
+          {t("withdraw-program.withdrawing")}
+          <span className="dialog-list__value">
+            {amount && !withdrawAll
+              ? `${formatCurrencyValue(
+                  amount,
+                  programCurrency
+                )} ${programCurrency}`
+              : t("withdraw-program.all")}
+          </span>
+        </li>
+        <li className="dialog-list__item">
+          {t("withdraw-program.payout-date")}
+          <span className="dialog-list__value">
+            {moment(periodEnds).format()}
+          </span>
+        </li>
+      </ul>
+      <div className="form-error">
+        <FormError error={errorMessage} />
+      </div>
+      <div className="dialog__buttons">
+        <GVButton
+          onClick={onBackClick}
+          color="secondary"
+          variant="outlined"
+          title={"back"}
+        >
+          {t("withdraw-program.back")}
+        </GVButton>
+        <GVButton
+          title={"submit"}
+          type={"submit"}
+          id="programWithdrawFormSubmit"
+          disabled={isSubmitting}
+        >
+          {t("withdraw-program.submit")}
+        </GVButton>
+      </div>
+    </form>
+  );
+};
 
 const ProgramWithdrawConfirmForm = compose<React.ComponentType<OwnProps>>(
-  translate(),
   withFormik<Props, {}>({
     displayName: "withdraw-submit-form",
     handleSubmit: (_, { props, setSubmitting }) => {
       props.onSubmit(setSubmitting);
     }
-  })
+  }),
+  React.memo
 )(_ProgramWithdrawConfirmForm);
 
 export default ProgramWithdrawConfirmForm;
@@ -92,4 +87,4 @@ interface OwnProps {
   withdrawAll?: boolean;
 }
 
-interface Props extends WithTranslation, OwnProps {}
+interface Props extends OwnProps {}
