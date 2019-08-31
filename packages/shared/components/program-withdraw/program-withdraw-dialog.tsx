ac86cccdc08a260e4f42d1b713baac0659b48fbc
pavelzubov <pasha224@yandex.ru>
2019-08-31 09:02:04 +0300

    Move fetch logic from ProgramWithdrawPopup

--- packages/shared/components/program-withdraw/program-withdraw-dialog.tsx
+++ packages/shared/components/program-withdraw/program-withdraw-dialog.tsx
@@ -1,23 +1,61 @@
 import * as React from "react";
+import { useEffect, useState } from "react";
 import Dialog, { IDialogProps } from "shared/components/dialog/dialog";
+import { DialogLoader } from "shared/components/dialog/dialog-loader/dialog-loader";
+import FormError from "shared/components/form/form-error/form-error";
+import useErrorMessage from "shared/hooks/error-message.hook";
 
 import ProgramWithdrawPopup, {
   IProgramWithdrawPopupProps
 } from "./program-withdraw-popup";
 
-const ProgramWithdrawDialog: React.FC<
+const _ProgramWithdrawDialog: React.FC<
   IDialogProps & IProgramWithdrawPopupProps
-> = React.memo(
-  ({ open, onClose, accountCurrency, assetCurrency, fetchInfo, withdraw }) => (
+> = ({
+  open,
+  onClose,
+  accountCurrency,
+  assetCurrency,
+  fetchInfo,
+  withdraw
+}) => {
+  const [periodEnds, setPeriodEnds] = useState<Date | undefined>(undefined);
+  const [title, setTitle] = useState<string | undefined>(undefined);
+  const [availableToWithdraw, setAvailableToWithdraw] = useState<
+    number | undefined
+  >(undefined);
+  const [rate, setRate] = useState<number>(1);
+  const { errorMessage, setErrorMessage } = useErrorMessage();
+  useEffect(() => {
+    fetchInfo()
+      .then(({ periodEnds, title, availableToWithdraw, rate }) => {
+        setPeriodEnds(periodEnds);
+        setTitle(title);
+        setAvailableToWithdraw(availableToWithdraw);
+        setRate(rate);
+      })
+      .catch(setErrorMessage);
+  }, []);
+  return (
     <Dialog open={open} onClose={onClose}>
       <ProgramWithdrawPopup
+        condition={availableToWithdraw === undefined || !title || !periodEnds}
+        loader={<DialogLoader />}
+        periodEnds={periodEnds!}
+        title={title!}
+        availableToWithdraw={availableToWithdraw!}
+        rate={rate}
         withdraw={withdraw}
         accountCurrency={accountCurrency}
         assetCurrency={assetCurrency}
         fetchInfo={fetchInfo}
       />
+      <div className="form-error">
+        <FormError error={errorMessage} />
+      </div>
     </Dialog>
-  )
-);
+  );
+};
 
+const ProgramWithdrawDialog = React.memo(_ProgramWithdrawDialog);
 export default ProgramWithdrawDialog;
