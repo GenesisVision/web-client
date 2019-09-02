97b5ef8b4e8ca05f79900cc68e247a867e1f8e01
pavelzubov <pasha224@yandex.ru>
2019-09-02 11:47:29 +0300

    Change redux HOC to hook

--- packages/manager/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
+++ packages/manager/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { connect } from "react-redux";
-import { ManagerRootState } from "reducers";
-import { compose } from "redux";
+import { useSelector } from "react-redux";
 import Page from "shared/components/page/page";
 import PortfolioEventsTable from "shared/components/portfolio-events-table/portfolio-events-table";
 import { EVENT_LOCATION } from "shared/components/programs/program-details/services/program-details.service";
 import Surface from "shared/components/surface/surface";
-import { SelectFilterValue } from "shared/components/table/components/filtering/filter.type";
 import useRole from "shared/hooks/use-role.hook";
 import { allEventsSelector } from "shared/reducers/platform-reducer";
 
 import { dashboardEventsAllTableSelector } from "../../reducers/dashboard-events.reducer";
 import { getEvents } from "../../services/dashboard.service";
 
-const _PortfolioEventsAllComponent: React.FC<Props> = ({ events }) => {
+const _PortfolioEventsAllComponent: React.FC = () => {
   const [t] = useTranslation();
   const role = useRole();
+  const events = useSelector(allEventsSelector);
   return (
     <Page title={t(`${role}.dashboard-page.portfolio-events.title`)}>
       <Surface className="dashboard-portfolio-events-all">
@@ -34,20 +32,5 @@ const _PortfolioEventsAllComponent: React.FC<Props> = ({ events }) => {
   );
 };
 
-const mapStateToProps = (state: ManagerRootState): StateProps => ({
-  events: allEventsSelector(state)
-});
-
-interface Props extends StateProps, OwnProps {}
-
-interface OwnProps {}
-
-interface StateProps {
-  events: SelectFilterValue<string>[];
-}
-
-const PortfolioEventsAllComponent = compose<React.ComponentType<OwnProps>>(
-  connect(mapStateToProps),
-  React.memo
-)(_PortfolioEventsAllComponent);
+const PortfolioEventsAllComponent = React.memo(_PortfolioEventsAllComponent);
 export default PortfolioEventsAllComponent;
