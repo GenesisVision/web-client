a82e30ae306e30604a1b52095a3d945df41418ef
Евгений Гребенщиков <evgreb@gmail.com>
2019-09-04 16:50:13 +0300

    add prod server

--- packages/investor/server/index.js
+++ packages/investor/server/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
 const next = require("next");
 const dotenv = require("dotenv");
-const nextI18NextMiddleware = require("next-i18next/middleware");
-
-const nextI18next = require("shared/i18n");
+const devServer = require("./dev");
+const prodServer = require("./production");
 
 dotenv.config({ path: ".env.local" });
 dotenv.config({ path: ".env" });
@@ -11,23 +9,11 @@ dotenv.config({ path: ".env" });
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 
-if (!dev) {
-  dotenv.config({ path: ".env.production" });
-}
-
 const app = next({ dev });
-const handle = app.getRequestHandler();
 
-(async () => {
-  await app.prepare();
-  const server = express();
-
-  server.use(nextI18NextMiddleware(nextI18next));
-
-  server.get("*", (req, res) => handle(req, res));
-
-  server.listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-})();
+if (dev) {
+  devServer(app, port);
+} else {
+  dotenv.config({ path: ".env.production" });
+  prodServer(app, port);
+}
