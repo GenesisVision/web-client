b545497b5d95ea2914f26634610d1933c9f3f8c2
pavelzubov <pasha224@yandex.ru>
2019-09-04 10:32:12 +0300

    Move ProgramTradesRow to own component

--- packages/shared/components/programs/program-details/program-history-section/program-trades/program-trades.tsx
+++ packages/shared/components/programs/program-details/program-history-section/program-trades/program-trades.tsx
@@ -1,28 +1,19 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-history/trades.scss";
 
 import { OrderModel } from "gv-api-web";
-import moment from "moment";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import NumberFormat from "react-number-format";
-import BaseProfitability from "shared/components/profitability/base-profitability";
-import Profitability from "shared/components/profitability/profitability";
-import { PROFITABILITY_PREFIX } from "shared/components/profitability/profitability.helper";
 import { generateProgramTradesColumns } from "shared/components/programs/program-details/program-details.constants";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
 import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
-import TableCell from "shared/components/table/components/table-cell";
 import TableContainer from "shared/components/table/components/table-container";
-import TableRow from "shared/components/table/components/table-row";
 import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
-import Tooltip from "shared/components/tooltip/tooltip";
-import { DEFAULT_DECIMAL_SCALE } from "shared/constants/constants";
 import filesService from "shared/services/file-service";
-import { formatValue } from "shared/utils/formatter";
 
 import { tradesTableSelector } from "../../reducers/program-history.reducer";
 import { getTrades } from "../../services/program-details.service";
 import DownloadButtonToolbar from "../download-button-toolbar/download-button-toolbar";
+import ProgramTradesRow from "./program-trades-row";
 
 const _ProgramTrades: React.FC<Props> = ({
   showSwaps,
@@ -63,93 +54,13 @@ const _ProgramTrades: React.FC<Props> = ({
           {t(`program-details-page.history.trades.${column.name}`)}
         </span>
       )}
-      renderBodyRow={(trade: OrderModel) => {
-        const volume = +formatValue(trade.volume, DEFAULT_DECIMAL_SCALE / 2);
-        return (
-          <TableRow stripy>
-            <TableCell className="details-trades__cell program-details-trades__cell--direction/entry">
-              <BaseProfitability
-                isPositive={trade.direction === "Buy"}
-                isNegative={trade.direction === "Sell"}
-              >
-                {trade.direction}
-              </BaseProfitability>
-              {` / ${trade.entry}`}
-            </TableCell>
-            <TableCell className="details-trades__cell program-details-trades__cell--symbol">
-              {trade.symbol}
-            </TableCell>
-            <TableCell className="details-trades__cell program-details-trades__cell--volume">
-              <Tooltip
-                disable={trade.volume >= volume}
-                render={() => <div>{trade.volume}</div>}
-              >
-                <span>{trade.volume < volume ? `< ${volume}` : volume}</span>
-              </Tooltip>
-            </TableCell>
-            <TableCell className="details-trades__cell program-details-trades__cell--price">
-              <NumberFormat
-                value={formatValue(trade.price, DEFAULT_DECIMAL_SCALE)}
-                displayType="text"
-                thousandSeparator=" "
-              />
-            </TableCell>
-            <TableCell className="details-trades__cell program-details-trades__cell--profit">
-              <Profitability
-                value={formatValue(trade.profit, DEFAULT_DECIMAL_SCALE)}
-                prefix={PROFITABILITY_PREFIX.SIGN}
-              >
-                <NumberFormat
-                  value={formatValue(trade.profit, DEFAULT_DECIMAL_SCALE)}
-                  thousandSeparator=" "
-                  allowNegative={false}
-                  displayType="text"
-                />
-              </Profitability>
-            </TableCell>
-            <TableCell className="details-trades__cell program-details-trades__cell--commission">
-              <Tooltip
-                render={() =>
-                  trade.showOriginalCommission ? (
-                    <div>
-                      {`${formatValue(
-                        trade.originalCommission,
-                        DEFAULT_DECIMAL_SCALE
-                      )} ${trade.originalCommissionCurrency}`}
-                    </div>
-                  ) : (
-                    <div>
-                      {`${formatValue(
-                        trade.commission,
-                        DEFAULT_DECIMAL_SCALE
-                      )} ${trade.originalCommissionCurrency}`}
-                    </div>
-                  )
-                }
-              >
-                <NumberFormat
-                  value={formatValue(trade.commission, DEFAULT_DECIMAL_SCALE)}
-                  displayType="text"
-                  thousandSeparator=" "
-                />
-              </Tooltip>
-            </TableCell>
-            {showSwaps && (
-              <TableCell className="details-trades__cell program-details-trades__cell--swap">
-                {trade.swap}
-              </TableCell>
-            )}
-            <TableCell className="details-trades__cell program-details-trades__cell--date">
-              {moment(trade.date).format()}
-            </TableCell>
-            {showTickets && (
-              <TableCell className="details-trades__cell program-details-trades__cell--ticket">
-                {trade.ticket}
-              </TableCell>
-            )}
-          </TableRow>
-        );
-      }}
+      renderBodyRow={(trade: OrderModel) => (
+        <ProgramTradesRow
+          trade={trade}
+          showSwaps={showSwaps}
+          showTickets={showTickets}
+        />
+      )}
     />
   );
 };
