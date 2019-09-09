1f3d800fd24047f58d55e0c63696f7f996f3e3e7
pavelzubov <pasha224@yandex.ru>
2019-09-09 13:36:48 +0300

    Change HOCs to hooks

--- packages/shared/modules/profile/profile-container.tsx
+++ packages/shared/modules/profile/profile-container.tsx
@@ -1,22 +1,17 @@
 import { ProfileFullViewModel } from "gv-api-web";
 import React, { useEffect, useState } from "react";
-import {
-  WithTranslation,
-  withTranslation as translate,
-  useTranslation
-} from "react-i18next";
-import { connect } from "react-redux";
-import { compose } from "redux";
+import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 import profileApi from "shared/services/api-client/profile-api";
 import authService from "shared/services/auth-service";
-import { MiddlewareDispatch } from "shared/utils/types";
 
 import Profile from "./profile";
 
-const _ProfileContainer: React.FC<Props> = ({
-  service: { alertMessageActionsSuccess }
-}) => {
+const _ProfileContainer: React.FC = () => {
+  const dispatch = useDispatch();
+  const alertMessageActionsSuccess = (text: string) =>
+    dispatch(alertMessageActions.success(text));
   const [t] = useTranslation();
   const [data, setData] = useState<ProfileFullViewModel | undefined>(undefined);
   const fetch = () =>
@@ -38,29 +33,5 @@ const _ProfileContainer: React.FC<Props> = ({
   );
 };
 
-const mapDispatchToProps = (dispatch: MiddlewareDispatch): DispatchProps => ({
-  service: {
-    alertMessageActionsSuccess: (text: string) =>
-      dispatch(alertMessageActions.success(text))
-  }
-});
-
-interface OwnProps {}
-
-interface DispatchProps {
-  service: {
-    alertMessageActionsSuccess: (text: string) => void;
-  };
-}
-
-interface Props extends WithTranslation, DispatchProps, OwnProps {}
-
-const ProfileContainer = compose<React.ComponentType<OwnProps>>(
-  translate(),
-  connect(
-    null,
-    mapDispatchToProps
-  ),
-  React.memo
-)(_ProfileContainer);
+const ProfileContainer = React.memo(_ProfileContainer);
 export default ProfileContainer;
