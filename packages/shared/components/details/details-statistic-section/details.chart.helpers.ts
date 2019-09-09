d286d46a679d8b93d370c6cedaf7b47393eee8cc
pavelzubov <pasha224@yandex.ru>
2019-09-09 18:16:17 +0300

    Add TStatisticCurrencySelector, TBalanceChartSelector, TProfitChartSelector, TUseChartStateValues

--- packages/shared/components/details/details-statistic-section/details.chart.helpers.ts
+++ packages/shared/components/details/details-statistic-section/details.chart.helpers.ts
@@ -1,14 +1,37 @@
 import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
+import { FundBalanceChartDataType } from "shared/components/funds/fund-details/reducers/balance-chart.reducer";
+import { FundProfitChartDataType } from "shared/components/funds/fund-details/reducers/profit-chart.reducer";
+import { ProgramBalanceChartDataType } from "shared/components/programs/program-details/reducers/balance-chart.reducer";
+import { ProgramProfitChartDataType } from "shared/components/programs/program-details/reducers/profit-chart.reducer";
+import { ISelectChangeEvent } from "shared/components/select/select";
 import { TChartCurrency } from "shared/modules/chart-currency-selector/chart-currency-selector";
 import { RootState } from "shared/reducers/root-reducer";
-import { HandlePeriodChangeType } from "shared/utils/types";
+import { CurrencyEnum, HandlePeriodChangeType } from "shared/utils/types";
 
 import {
   StatisticPeriodState,
   TStatisticPeriodAction
 } from "../reducers/statistic-period.reducer";
+
+export type TStatisticCurrencySelector = (state: RootState) => CurrencyEnum;
+
+export type TBalanceChartSelector = (
+  state: RootState
+) => FundBalanceChartDataType | ProgramBalanceChartDataType | undefined;
+
+export type TProfitChartSelector = (
+  state: RootState
+) => FundProfitChartDataType | ProgramProfitChartDataType | undefined;
+
+export type TUseChartStateValues = () => {
+  selectedCurrencies: TChartCurrency[];
+  selectCurrencies: TChartCurrency[];
+  addCurrency: () => void;
+  removeCurrency: (name: string) => void;
+  changeCurrency: (i: number) => (event: ISelectChangeEvent) => void;
+};
 
 export type TUseChartPeriod = () => {
   period: ChartDefaultPeriod;
