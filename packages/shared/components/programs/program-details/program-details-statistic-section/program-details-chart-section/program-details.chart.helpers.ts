dc118587cd39b851d6365dd7f621d16cd5b0e2ed
pavelzubov <pasha224@yandex.ru>
2019-09-09 09:38:23 +0300

    useChartPeriodCreator

--- packages/shared/components/programs/program-details/program-details-statistic-section/program-details-chart-section/program-details.chart.helpers.ts
+++ packages/shared/components/programs/program-details/program-details-statistic-section/program-details-chart-section/program-details.chart.helpers.ts
@@ -1,26 +1,7 @@
-import { useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
-import { HandlePeriodChangeType } from "shared/utils/types";
+import { useChartPeriodCreator } from "shared/components/details/details-statistic-section/details.chart.helpers";
 
 import { statisticPeriodAction } from "../../actions/program-details.actions";
 import { statisticPeriodSelector } from "../../reducers/statistic-period.reducer";
 
-type TUseChartPeriod = () => {
-  period: ChartDefaultPeriod;
-  setPeriod: HandlePeriodChangeType;
-};
-export const useChartPeriod: TUseChartPeriod = () => {
-  const period = useSelector(statisticPeriodSelector);
-  const dispatch = useDispatch();
-  const setPeriod = useCallback(
-    period => {
-      dispatch(statisticPeriodAction(period));
-    },
-    [dispatch]
-  );
-  return {
-    period,
-    setPeriod
-  };
-};
+export const useChartPeriod = () =>
+  useChartPeriodCreator(statisticPeriodSelector, statisticPeriodAction);
