6c1e8c2c50d392814c62648a4f21934fc3c33180
pavelzubov <pasha224@yandex.ru>
2019-09-10 16:04:09 +0300

    Use DetailsStatisticSection

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-statistic-section.tsx
@@ -1,19 +1,60 @@
 import "shared/components/details/details-description-section/details-statistic-section/details-statistic-section.scss";
 
 import * as React from "react";
+import NumberFormat from "react-number-format";
+import DetailsStatisticsLoader from "shared/components/details/details-description-section/details-statistic-section/details-loader/details-statistic-loader";
+import DetailsStatisticSection from "shared/components/details/details-statistic-section/details-statistic-section";
 
-import FundDetailsChart from "./fund-details-chart-section/fund-details-chart";
-import FundDetailsStatistics from "./fund-details-statistics/fund-details-statistics";
+import { fundBalanceChartSelector } from "../reducers/balance-chart.reducer";
+import { fundProfitChartSelector } from "../reducers/profit-chart.reducer";
+import { statisticCurrencySelector } from "../reducers/statistic-currency.reducer";
+import FundBalanceChart from "./fund-details-chart-section/fund-balance-chart-section/fund-balance-chart";
+import {
+  useChartPeriod,
+  useFundChartStateValues
+} from "./fund-details-chart-section/fund-details-chart.helpers";
+import FundProfitChart from "./fund-details-chart-section/fund-profit-chart-section/fund-profit-chart";
+import FundDetailsStatisticsElements, {
+  IFundStatisticData
+} from "./fund-details-statistics/fund-details-statistics-elements";
 
 const _FundDetailsStatisticSection: React.FC = () => (
-  <div className="details-statistic-section">
-    <div className="details-statistic-section__statistic">
-      <FundDetailsStatistics />
-    </div>
-    <div className="details-statistic-section__chart">
-      <FundDetailsChart />
-    </div>
-  </div>
+  <DetailsStatisticSection
+    balanceChartSelector={fundBalanceChartSelector}
+    profitChartSelector={fundProfitChartSelector}
+    statisticCurrencySelector={statisticCurrencySelector}
+    useChartStateValues={useFundChartStateValues}
+    useChartPeriod={useChartPeriod}
+    renderProfitValue={({ chart }) => (
+      <NumberFormat
+        value={"profitPercent" in chart ? chart.profitPercent : 0}
+        thousandSeparator={" "}
+        displayType="text"
+        suffix={" %"}
+      />
+    )}
+    renderBalanceChart={({ currency, color, balanceChart }) => (
+      <FundBalanceChart
+        balanceChart={balanceChart}
+        currency={currency}
+        color={color}
+      />
+    )}
+    renderProfitChart={({ profitChart, chartCurrencies }) => (
+      <FundProfitChart
+        profitChart={profitChart}
+        chartCurrencies={chartCurrencies}
+      />
+    )}
+    renderDetailsStatisticsElements={({ period, statisticData }) => (
+      <FundDetailsStatisticsElements
+        condition={!!statisticData}
+        loader={<DetailsStatisticsLoader />}
+        period={period}
+        statisticData={statisticData! as IFundStatisticData}
+      />
+    )}
+  />
 );
 
 const FundDetailsStatisticSection = React.memo(_FundDetailsStatisticSection);
