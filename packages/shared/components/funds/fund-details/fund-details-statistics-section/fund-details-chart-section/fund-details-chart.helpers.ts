dfd69f2cf8231686797786adcc783c8c9b4d2cf8
pavelzubov <pasha224@yandex.ru>
2019-09-10 16:26:29 +0300

    Add statisticCurrencyAction to useChartStateDataCreator Remove unused items

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.helpers.ts
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.helpers.ts
@@ -1,26 +1,16 @@
-import { PlatformCurrency } from "gv-api-web";
-import { useEffect, useState } from "react";
-import { createSelector } from "reselect";
 import {
   useChartPeriodCreator,
   useChartStateDataCreator,
   useFundChartStateValuesCreator
 } from "shared/components/details/details-statistic-section/details.chart.helpers";
-import { TChartCurrency } from "shared/modules/chart-currency-selector/chart-currency-selector";
-import { platformCurrenciesSelector } from "shared/reducers/platform-reducer";
-import { RootState } from "shared/reducers/root-reducer";
-import { CurrencyEnum } from "shared/utils/types";
 
-import { statisticPeriodAction } from "../../actions/fund-details.actions";
 import {
-  FundBalanceChartDataType,
-  fundBalanceChartSelector
-} from "../../reducers/balance-chart.reducer";
+  statisticCurrencyAction,
+  statisticPeriodAction
+} from "../../actions/fund-details.actions";
+import { fundBalanceChartSelector } from "../../reducers/balance-chart.reducer";
 import { fundIdSelector } from "../../reducers/description.reducer";
-import {
-  FundProfitChartDataType,
-  fundProfitChartSelector
-} from "../../reducers/profit-chart.reducer";
+import { fundProfitChartSelector } from "../../reducers/profit-chart.reducer";
 import { statisticCurrencySelector } from "../../reducers/statistic-currency.reducer";
 import { statisticPeriodSelector } from "../../reducers/statistic-period.reducer";
 import {
@@ -28,33 +18,9 @@ import {
   getProfitChart
 } from "../../services/fund-details.service";
 
-export const convertToChartCurrency = ({
-  name,
-  color
-}: PlatformCurrency): TChartCurrency => ({
-  name: name as CurrencyEnum,
-  color
-});
-
-export const platformChartCurrenciesSelector = createSelector<
-  RootState,
-  PlatformCurrency[],
-  TChartCurrency[]
->(
-  state => platformCurrenciesSelector(state),
-  currencies => currencies.map(convertToChartCurrency)
-);
-
-export type TUseFundChartStateData = () => {
-  platformCurrencies: TChartCurrency[];
-  profitChart?: FundProfitChartDataType;
-  balanceChart?: FundBalanceChartDataType;
-  selectedCurrencies: TChartCurrency[];
-  setSelectedCurrencies: (currencies: TChartCurrency[]) => void;
-};
-
 export const useFundChartStateData = () =>
   useChartStateDataCreator({
+    statisticCurrencyAction,
     idSelector: fundIdSelector,
     statisticPeriodSelector,
     statisticCurrencySelector,
@@ -69,28 +35,3 @@ export const useFundChartStateValues = () =>
 
 export const useChartPeriod = () =>
   useChartPeriodCreator(statisticPeriodSelector, statisticPeriodAction);
-
-type TChartData<T> = {
-  chart: T;
-  selectedCurrencies: TChartCurrency[];
-};
-
-export const useChartData = <T>(
-  chart: T,
-  selectedCurrencies: TChartCurrency[]
-): TChartData<T> => {
-  const [chartData, setChartData] = useState<TChartData<T>>({
-    chart,
-    selectedCurrencies
-  });
-  useEffect(
-    () => {
-      setChartData({
-        chart,
-        selectedCurrencies: [...selectedCurrencies]
-      });
-    },
-    [chart, selectedCurrencies]
-  );
-  return chartData;
-};
