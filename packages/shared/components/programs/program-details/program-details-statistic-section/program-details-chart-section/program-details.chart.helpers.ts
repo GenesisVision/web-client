aa8076ccc8b2f484716155cd99783b4cb4893826
pavelzubov <pasha224@yandex.ru>
2019-09-10 16:06:02 +0300

    Add useProgramChartStateData, useProgramChartStateValues

--- packages/shared/components/programs/program-details/program-details-statistic-section/program-details-chart-section/program-details.chart.helpers.ts
+++ packages/shared/components/programs/program-details/program-details-statistic-section/program-details-chart-section/program-details.chart.helpers.ts
@@ -1,7 +1,33 @@
-import { useChartPeriodCreator } from "shared/components/details/details-statistic-section/details.chart.helpers";
+import {
+  useChartPeriodCreator,
+  useChartStateDataCreator,
+  useFundChartStateValuesCreator
+} from "shared/components/details/details-statistic-section/details.chart.helpers";
 
 import { statisticPeriodAction } from "../../actions/program-details.actions";
+import { programBalanceChartSelector } from "../../reducers/balance-chart.reducer";
+import { programIdSelector } from "../../reducers/description.reducer";
+import { programProfitChartSelector } from "../../reducers/profit-chart.reducer";
+import { statisticCurrencySelector } from "../../reducers/statistic-currency.reducer";
 import { statisticPeriodSelector } from "../../reducers/statistic-period.reducer";
+import {
+  getBalanceChart,
+  getProfitChart
+} from "../../services/program-details.service";
 
 export const useChartPeriod = () =>
   useChartPeriodCreator(statisticPeriodSelector, statisticPeriodAction);
+
+export const useProgramChartStateData = () =>
+  useChartStateDataCreator({
+    idSelector: programIdSelector,
+    statisticPeriodSelector,
+    statisticCurrencySelector,
+    profitChartSelector: programProfitChartSelector,
+    balanceChartSelector: programBalanceChartSelector,
+    getBalanceChart,
+    getProfitChart
+  });
+
+export const useProgramChartStateValues = () =>
+  useFundChartStateValuesCreator(useProgramChartStateData);
