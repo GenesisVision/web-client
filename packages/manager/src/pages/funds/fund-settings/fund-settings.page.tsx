a179295b3d958e488c1e6b88cf26e356f0df527c
pavelzubov <pasha224@yandex.ru>
2019-09-11 21:23:41 +0300

    Change redux HOCs to hooks Add programsInfo

--- packages/manager/src/pages/funds/fund-settings/fund-settings.page.tsx
+++ packages/manager/src/pages/funds/fund-settings/fund-settings.page.tsx
@@ -1,11 +1,10 @@
-import { FundDetailsFull, PlatformAsset } from "gv-api-web";
 import AssetSettingsLoader from "modules/asset-settings/asset-settings.loader";
 import AssetSettingsPage from "modules/asset-settings/asset-settings.page";
 import { AssetDescriptionType } from "modules/asset-settings/asset-settings.types";
 import { programEditSignal } from "modules/program-signal/program-edit-signal/services/program-edit-signal.service";
 import React, { useCallback } from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { ResolveThunks, connect } from "react-redux";
+import { withTranslation as translate } from "react-i18next";
+import { ResolveThunks, connect, useSelector } from "react-redux";
 import {
   ActionCreatorsMapObject,
   Dispatch,
@@ -16,24 +15,26 @@ import { IImageValue } from "shared/components/form/input-image/input-image";
 import { fundDescriptionSelector } from "shared/components/funds/fund-details/reducers/description.reducer";
 import { dispatchFundDescription } from "shared/components/funds/fund-details/services/fund-details.service";
 import { ASSET } from "shared/constants/constants";
-import { fundAssetsSelector } from "shared/reducers/platform-reducer";
-import { RootState } from "shared/reducers/root-reducer";
+import {
+  fundAssetsSelector,
+  programsInfoSelector
+} from "shared/reducers/platform-reducer";
 import { SetSubmittingType } from "shared/utils/types";
 
 import FundSettings from "./fund-settings";
 import { redirectToFund } from "./services/fund-settings.service";
 
 const _FundSettingsPage: React.FC<Props> = ({
-  platformAssets,
   service: {
     programEditSignal,
     changeBrokerMethod,
     cancelChangeBrokerMethod,
     dispatchDescription
-  },
-  t,
-  description
+  }
 }) => {
+  const programsInfo = useSelector(programsInfoSelector);
+  const description = useSelector(fundDescriptionSelector);
+  const platformAssets = useSelector(fundAssetsSelector);
   const reallocate = useCallback(() => {
     dispatchDescription();
   }, []);
@@ -45,6 +46,7 @@ const _FundSettingsPage: React.FC<Props> = ({
       dispatchDescription={dispatchFundDescription}
       settingsBlocks={(editProgram, applyCloseAsset) => (
         <FundSettings
+          programsInfo={programsInfo}
           reallocate={reallocate}
           condition={!!description && !!platformAssets}
           platformAssets={platformAssets}
@@ -58,11 +60,6 @@ const _FundSettingsPage: React.FC<Props> = ({
   );
 };
 
-const mapStateToProps = (state: RootState): StateProps => ({
-  description: fundDescriptionSelector(state),
-  platformAssets: fundAssetsSelector(state)
-});
-
 const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
     {
@@ -73,22 +70,19 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
   )
 });
 
-export type TUpdateProgramFunc = (
+export type TUpdateFundFunc = (
   values: {
     description?: string;
     logo?: IImageValue;
     investmentLimit?: number;
     stopOutLevel?: number;
+    entryFee?: number;
+    exitFee?: number;
   },
   setSubmitting: SetSubmittingType
 ) => void;
 
 interface OwnProps {}
-
-interface StateProps {
-  platformAssets: PlatformAsset[];
-  description?: FundDetailsFull;
-}
 
 interface ServiceThunks extends ActionCreatorsMapObject {
   dispatchDescription: typeof dispatchFundDescription;
@@ -98,12 +92,12 @@ interface DispatchProps {
   service: ResolveThunks<ServiceThunks>;
 }
 
-interface Props extends OwnProps, DispatchProps, WithTranslation, StateProps {}
+interface Props extends OwnProps, DispatchProps {}
 
 const FundSettingsPage = compose<React.ComponentType<OwnProps>>(
   translate(),
   connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
   ),
   React.memo
