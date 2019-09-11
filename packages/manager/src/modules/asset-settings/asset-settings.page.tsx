bdb305a664a68ffda2f9be514c4ffdc54a845996
pavelzubov <pasha224@yandex.ru>
2019-09-11 21:22:15 +0300

    Add exitFee to currentValues

--- packages/manager/src/modules/asset-settings/asset-settings.page.tsx
+++ packages/manager/src/modules/asset-settings/asset-settings.page.tsx
@@ -32,6 +32,7 @@ const _AssetsEditPage: React.FC<Props> = ({
   const editAssetCallback: TUpdateAssetFunc = useCallback(
     values => {
       const currentValues = {
+        exitFee: description!.exitFee,
         entryFee: description!.entryFee,
         successFee: description!.successFee,
         title: description!.title,
