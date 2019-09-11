9ce88d97bc32614165c486cd6d6f9da70362831c
Евгений Гребенщиков <evgreb@gmail.com>
2019-09-11 12:44:34 +0300

    fix config

--- packages/investor/server/index.js
+++ packages/investor/server/index.js
@@ -3,17 +3,18 @@ const dotenv = require("dotenv");
 const devServer = require("./dev");
 const prodServer = require("./production");
 
-dotenv.config({ path: ".env.local" });
-dotenv.config({ path: ".env" });
-
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
 
 const app = next({ dev });
 
 if (dev) {
+  dotenv.config({ path: ".env.local" });
+  dotenv.config({ path: ".env" });
   devServer(app, port);
 } else {
   dotenv.config({ path: ".env.production" });
+  dotenv.config({ path: ".env.local" });
+  dotenv.config({ path: ".env" });
   prodServer(app, port);
 }
