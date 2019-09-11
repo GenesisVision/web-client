7aab38c9cdc1497bdd896a4a63d5418a96fd458a
pavelzubov <pasha224@yandex.ru>
2019-09-11 21:22:53 +0300

    Add exitFee Add asset prop Add FeesSettings for funds

--- packages/manager/src/modules/asset-settings/investment-fees.tsx
+++ packages/manager/src/modules/asset-settings/investment-fees.tsx
@@ -6,14 +6,18 @@ import { compose } from "redux";
 import FeesSettings from "shared/components/fields/fees-settings";
 import GVButton from "shared/components/gv-button";
 import SettingsBlock from "shared/components/settings-block/settings-block";
+import { ASSET } from "shared/constants/constants";
 import { SetSubmittingType } from "shared/utils/types";
 import {
   entryFeeShape,
+  exitFeeShape,
   successFeeShape
 } from "shared/utils/validators/validators";
 import { object } from "yup";
 
 const _InvestmentFees: React.FC<Props> = ({
+  asset,
+  programsInfo: { managerMaxEntryFee, managerMaxExitFee },
   t,
   values,
   handleSubmit,
@@ -26,22 +30,46 @@ const _InvestmentFees: React.FC<Props> = ({
       label={t("manager.create-program-page.settings.investment-program-fees")}
       content={
         <form id="edit-form" onSubmit={handleSubmit}>
-          <FeesSettings
-            entryFeeName={FIELDS.entryFee}
-            entryFeeDescription={t(
-              "manager.create-program-page.settings.hints.entry-fee-description"
-            )}
-            secondFeeName={FIELDS.successFee}
-            secondFeeLabel={t(
-              "manager.create-program-page.settings.fields.success-fee"
-            )}
-            secondFeeUnderText={t(
-              "manager.create-program-page.settings.hints.success-fee"
-            )}
-            secondFeeDescription={t(
-              "manager.create-program-page.settings.hints.success-fee-description"
-            )}
-          />
+          {asset === ASSET.PROGRAM && (
+            <FeesSettings
+              entryFeeName={FIELDS.entryFee}
+              entryFeeDescription={t(
+                "manager.create-program-page.settings.hints.entry-fee-description"
+              )}
+              secondFeeName={FIELDS.successFee}
+              secondFeeLabel={t(
+                "manager.create-program-page.settings.fields.success-fee"
+              )}
+              secondFeeUnderText={t(
+                "manager.create-program-page.settings.hints.success-fee"
+              )}
+              secondFeeDescription={t(
+                "manager.create-program-page.settings.hints.success-fee-description"
+              )}
+            />
+          )}
+          {asset === ASSET.FUND && (
+            <FeesSettings
+              entryFeeName={FIELDS.entryFee}
+              entryFeeDescription={t(
+                "manager.create-fund-page.settings.hints.entry-fee-description",
+                { maxFee: managerMaxEntryFee }
+              )}
+              secondFeeName={FIELDS.exitFee}
+              secondFeeLabel={t(
+                "manager.create-fund-page.settings.fields.exit-fee"
+              )}
+              secondFeeUnderText={t(
+                "manager.create-fund-page.settings.hints.exit-fee"
+              )}
+              secondFeeDescription={t(
+                "manager.create-fund-page.settings.hints.exit-fee-description",
+                {
+                  maxFee: managerMaxExitFee
+                }
+              )}
+            />
+          )}
           <GVButton
             color="primary"
             type={"submit"}
@@ -57,13 +85,15 @@ const _InvestmentFees: React.FC<Props> = ({
 };
 
 enum FIELDS {
+  exitFee = "exitFee",
   entryFee = "entryFee",
   successFee = "successFee"
 }
 
 export interface InvesmentLimitFormValues {
+  [FIELDS.exitFee]?: number;
   [FIELDS.entryFee]: number;
-  [FIELDS.successFee]: number;
+  [FIELDS.successFee]?: number;
 }
 
 interface Props
@@ -72,9 +102,11 @@ interface Props
     FormikProps<InvesmentLimitFormValues> {}
 
 interface OwnProps {
+  asset: ASSET;
   programsInfo: ProgramsInfo;
+  exitFee?: number;
   entryFee: number;
-  successFee: number;
+  successFee?: number;
   onSubmit: (
     values: InvesmentLimitFormValues,
     setSubmitting: SetSubmittingType
@@ -86,12 +118,14 @@ const InvestmentFees = compose<React.ComponentType<OwnProps>>(
   withFormik<OwnProps, InvesmentLimitFormValues>({
     enableReinitialize: true,
     displayName: "edit-form",
-    mapPropsToValues: ({ entryFee, successFee }) => ({
+    mapPropsToValues: ({ entryFee, successFee, exitFee }) => ({
+      [FIELDS.exitFee]: exitFee,
       [FIELDS.entryFee]: entryFee,
       [FIELDS.successFee]: successFee
     }),
     validationSchema: ({ programsInfo, t }: Props) =>
       object().shape({
+        [FIELDS.exitFee]: exitFeeShape(t, programsInfo.managerMaxExitFee),
         [FIELDS.entryFee]: entryFeeShape(t, programsInfo.managerMaxEntryFee),
         [FIELDS.successFee]: successFeeShape(
           t,
