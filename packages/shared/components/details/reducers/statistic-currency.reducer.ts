a6d40306989c964739947be6e078050066a3afac
pavelzubov <pasha224@yandex.ru>
2019-09-12 19:14:42 +0300

    Change loadData to getCookie

--- packages/shared/components/details/reducers/statistic-currency.reducer.ts
+++ packages/shared/components/details/reducers/statistic-currency.reducer.ts
@@ -1,5 +1,6 @@
 import { ACCOUNT_CURRENCY_KEY } from "shared/middlewares/update-account-settings-middleware/update-account-settings-middleware";
 import defaultReducer from "shared/reducers/reducer-creators/default-reducer";
+import { getCookie } from "shared/utils/cookie";
 import { ActionType, CurrencyEnum } from "shared/utils/types";
 
 export type StatisticCurrencyDataType = CurrencyEnum;
@@ -7,7 +8,7 @@ export type TStatisticCurrencyAction = ActionType<StatisticCurrencyDataType>;
 export type StatisticCurrencyState = StatisticCurrencyDataType;
 
 const initialState: StatisticCurrencyState =
-  (loadData(ACCOUNT_CURRENCY_KEY) as StatisticCurrencyDataType) || "BTC";
+  (getCookie(ACCOUNT_CURRENCY_KEY) as StatisticCurrencyDataType) || "BTC";
 
 const statisticCurrencyReducerCreator = (type: string) => (
   state: StatisticCurrencyState = initialState,
