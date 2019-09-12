8bdafcaabd55fb1df29014816003dfa8aedf0029
pavelzubov <pasha224@yandex.ru>
2019-09-12 14:14:15 +0300

    Add div.details__section

--- packages/shared/components/funds/fund-details/fund-details-description/fund-details-description-section.tsx
+++ packages/shared/components/funds/fund-details/fund-details-description/fund-details-description-section.tsx
@@ -18,41 +18,43 @@ const _FundFundDetailsDescription: React.FC<Props> = ({
   fundDescription,
   FundControls
 }) => (
-  <div className="program-details-description">
-    <FundDetailsDescription
-      description={fundDescription}
-      AssetDetailsAvatar={() => (
-        <div className="details-description__avatar">
-          <AssetAvatar
-            url={fundDescription.logo}
-            alt={fundDescription.title}
-            size="big"
-            color={fundDescription.color}
-          />
-        </div>
-      )}
-      AssetDetailsExtraBlock={() => (
-        <div className="details-description__info-block">
-          <h4 className="details-description__subheading tooltip__label">
-            <TooltipLabel
-              tooltipContent={t("fund-details-page.tooltip.assets")}
-              labelText={t("fund-details-page.description.assets")}
-            />
-          </h4>
-          <div>
-            <FundAssetContainer
-              type={FUND_ASSET_TYPE.LARGE}
-              assets={fundDescription.currentAssets}
-              size={7}
+  <div className="details__section">
+    <div className="program-details-description">
+      <FundDetailsDescription
+        description={fundDescription}
+        AssetDetailsAvatar={() => (
+          <div className="details-description__avatar">
+            <AssetAvatar
+              url={fundDescription.logo}
+              alt={fundDescription.title}
+              size="big"
+              color={fundDescription.color}
             />
           </div>
-        </div>
-      )}
-    />
-    <FundControls
-      fundDescription={fundDescription}
-      isAuthenticated={isAuthenticated}
-    />
+        )}
+        AssetDetailsExtraBlock={() => (
+          <div className="details-description__info-block">
+            <h4 className="details-description__subheading tooltip__label">
+              <TooltipLabel
+                tooltipContent={t("fund-details-page.tooltip.assets")}
+                labelText={t("fund-details-page.description.assets")}
+              />
+            </h4>
+            <div>
+              <FundAssetContainer
+                type={FUND_ASSET_TYPE.LARGE}
+                assets={fundDescription.currentAssets}
+                size={7}
+              />
+            </div>
+          </div>
+        )}
+      />
+      <FundControls
+        fundDescription={fundDescription}
+        isAuthenticated={isAuthenticated}
+      />
+    </div>
   </div>
 );
 
