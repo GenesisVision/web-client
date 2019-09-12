5b6c0ecdd8bd5104b2b687228b7f0c2c3f1e08ed
pavelzubov <pasha224@yandex.ru>
2019-09-12 18:35:04 +0300

    Add .details__block

--- packages/manager/src/pages/programs/program-details/components/program-controls/program-controls.tsx
+++ packages/manager/src/pages/programs/program-details/components/program-controls/program-controls.tsx
@@ -19,7 +19,7 @@ const _ProgramControls: React.FC<Props> = ({
 
   return (
     <div className="program-details-description__controls">
-      <div className="program-details-description__col">
+      <div className="program-details-description__col details__block">
         <InvestmentProgramControls
           programDescription={programDescription}
           canCloseProgram={canCloseProgram}
@@ -29,7 +29,7 @@ const _ProgramControls: React.FC<Props> = ({
         />
       </div>
       {isOwnProgram && programDescription.isSignalProgram && (
-        <div className="program-details-description__col program-details-description__col--small-size">
+        <div className="program-details-description__col program-details-description__col--small-size  details__block">
           <SignalProgramInfo programDescription={programDescription} />
         </div>
       )}
