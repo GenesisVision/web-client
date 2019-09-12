20800e0866dfd76e72e4e01cde36a20d07138ae7
pavelzubov <pasha224@yandex.ru>
2019-09-12 10:42:08 +0300

    Create AddCurrencyButton component

---
+++ packages/shared/modules/chart-currency-selector/add-currency-button.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ISelectChangeEvent } from "shared/components/select/select";
+import TileFilterButton from "shared/components/table/components/filtering/tile-filter-button";
+import CurrencySelect from "shared/currency-select/components/currency-select";
+import { CurrencyEnum } from "shared/utils/types";
+
+import { TAddChartCurrency } from "./chart-currency-selector";
+
+const _AddCurrencyButton: React.FC<Props> = ({ onAdd, currencies }) => {
+  const onChange = ({ target: { value } }: ISelectChangeEvent) => onAdd();
+  return (
+    <TileFilterButton
+      title={
+        <CurrencySelect
+          value={"ADD"}
+          onChange={onChange}
+          currencyValues={currencies}
+        />
+      }
+    />
+  );
+};
+
+interface Props {
+  onAdd: TAddChartCurrency;
+  currencies: CurrencyEnum[];
+}
+
+const AddCurrencyButton = React.memo(_AddCurrencyButton);
+export default AddCurrencyButton;
