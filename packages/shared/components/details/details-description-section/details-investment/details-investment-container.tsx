2a80212e683f13f3e85f17e1cbaeb575c9be9660
pavelzubov <pasha224@yandex.ru>
2019-09-14 08:48:08 +0300

    Move markup from DetailsInvestment Rename to DetailsInvestment

--- packages/shared/components/details/details-description-section/details-investment/details-investment-container.tsx
+++ packages/shared/components/details/details-description-section/details-investment/details-investment-container.tsx
@@ -11,23 +11,31 @@ import {
   compose
 } from "redux";
 import { IFundWithdrawalContainerProps } from "shared/components/funds/fund-details/fund-details.types";
+import GVTabs from "shared/components/gv-tabs";
+import GVTab from "shared/components/gv-tabs/gv-tab";
+import PortfolioEventsTable from "shared/components/portfolio-events-table/portfolio-events-table";
 import { IProgramReinvestingContainerOwnProps } from "shared/components/programs/program-details/program-details.types";
 import {
   EVENT_LOCATION,
   getEvents
 } from "shared/components/programs/program-details/services/program-details.service";
+import Surface from "shared/components/surface/surface";
 import { SelectFilterValue } from "shared/components/table/components/filtering/filter.type";
 import { TableSelectorType } from "shared/components/table/components/table.types";
 import { ASSET } from "shared/constants/constants";
+import useTab from "shared/hooks/tab.hook";
 import { isAuthenticatedSelector } from "shared/reducers/auth-reducer";
 import { RootState } from "shared/reducers/root-reducer";
 import { CurrencyEnum, DispatchDescriptionType } from "shared/utils/types";
 
-import DetailsInvestment from "./details-investment";
 import { InvestmentDetails } from "./details-investment.helpers";
-import { haveActiveInvestment, haveSubscription } from "./investment-container";
+import InvestmentContainer, {
+  haveActiveInvestment,
+  haveSubscription
+} from "./investment-container";
 
-const _DetailsInvestmentContainer: React.FC<Props> = ({
+const _DetailsInvestment: React.FC<Props> = ({
+  notice,
   asset,
   eventTypesSelector,
   selector,
@@ -59,26 +67,58 @@ const _DetailsInvestmentContainer: React.FC<Props> = ({
   const haveInvestment =
     haveActiveInvestment(personalDetails) || haveSubscription(personalDetails);
   const showInvestment = haveEvents || haveInvestment;
+  const { tab, setTab } = useTab<TABS>(
+    haveInvestment ? TABS.INVESTMENT : TABS.EVENTS
+  );
+  if (!showInvestment) return null;
   return (
-    <DetailsInvestment
-      condition={showInvestment}
-      selector={selector}
-      haveEvents={haveEvents}
-      haveInvestment={haveInvestment}
-      eventTypeFilterValues={eventTypeFilterValues}
-      updateDescription={dispatchDescription}
-      notice={t("program-details-page.description.withdraw-notice-text")}
-      asset={asset}
-      id={id}
-      assetCurrency={currency}
-      personalDetails={personalDetails}
-      ProgramReinvestingWidget={ProgramReinvestingWidget}
-      WithdrawContainer={WithdrawContainer}
-    />
+    <Surface className="details__section details-investment">
+      <div className="details-investment__investment-tabs">
+        <GVTabs value={tab} onChange={setTab}>
+          <GVTab
+            visible={haveInvestment}
+            value={TABS.INVESTMENT}
+            label={t(`fund-details-page.description.yourInvestment.${asset}`)}
+          />
+          <GVTab
+            visible={haveEvents}
+            value={TABS.EVENTS}
+            label={t("program-details-page.history.tabs.events")}
+          />
+        </GVTabs>
+      </div>
+      {tab === TABS.INVESTMENT && (
+        <InvestmentContainer
+          updateDescription={dispatchDescription}
+          asset={asset}
+          notice={notice}
+          id={id}
+          assetCurrency={currency}
+          personalDetails={personalDetails}
+          WithdrawContainer={WithdrawContainer}
+          ProgramReinvestingWidget={ProgramReinvestingWidget}
+        />
+      )}
+      {tab === TABS.EVENTS && (
+        <PortfolioEventsTable
+          getItems={getEvents(id!, EVENT_LOCATION.Asset)}
+          selector={selector}
+          asset={asset}
+          eventLocation={EVENT_LOCATION.Asset}
+          dateRangeStartLabel={t("filters.date-range.program-start")}
+          eventTypeFilterValues={eventTypeFilterValues!}
+        />
+      )}
+    </Surface>
   );
 };
 
+enum TABS {
+  INVESTMENT = "INVESTMENT",
+  EVENTS = "EVENTS"
+}
 interface OwnProps {
+  notice?: string;
   asset: ASSET;
   eventTypesSelector: (state: RootState) => SelectFilterValue[];
   dispatchDescription: DispatchDescriptionType;
@@ -113,11 +153,11 @@ interface DispatchProps {
 
 interface Props extends DispatchProps, OwnProps {}
 
-const DetailsInvestmentContainer = compose<React.ComponentType<OwnProps>>(
+const DetailsInvestment = compose<React.ComponentType<OwnProps>>(
   connect(
     null,
     mapDispatchToProps
   ),
   React.memo
-)(_DetailsInvestmentContainer);
-export default DetailsInvestmentContainer;
+)(_DetailsInvestment);
+export default DetailsInvestment;
