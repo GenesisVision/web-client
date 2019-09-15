24ebb9ec9d2df74621c50d0d9c6d4c10175427a4
pavelzubov <pasha224@yandex.ru>
2019-09-15 20:00:14 +0300

    Remove useless div Change styles to table flag

--- packages/shared/components/details/details-description-section/details-investment/details-investment.tsx
+++ packages/shared/components/details/details-description-section/details-investment/details-investment.tsx
@@ -72,45 +72,43 @@ const _DetailsInvestment: React.FC<Props> = ({
   );
   if (!showInvestment) return null;
   return (
-    <DetailsBlock wide>
-      <div className="details-investment">
-        <div className="details-investment__investment-tabs">
-          <GVTabs value={tab} onChange={setTab}>
-            <GVTab
-              visible={haveInvestment}
-              value={TABS.INVESTMENT}
-              label={t(`fund-details-page.description.yourInvestment.${asset}`)}
-            />
-            <GVTab
-              visible={haveEvents}
-              value={TABS.EVENTS}
-              label={t("program-details-page.history.tabs.events")}
-            />
-          </GVTabs>
-        </div>
-        {tab === TABS.INVESTMENT && (
-          <InvestmentContainer
-            updateDescription={dispatchDescription}
-            asset={asset}
-            notice={notice}
-            id={id}
-            assetCurrency={currency}
-            personalDetails={personalDetails}
-            WithdrawContainer={WithdrawContainer}
-            ProgramReinvestingWidget={ProgramReinvestingWidget}
+    <DetailsBlock table wide className="details-investment">
+      <div className="details-investment__investment-tabs">
+        <GVTabs value={tab} onChange={setTab}>
+          <GVTab
+            visible={haveInvestment}
+            value={TABS.INVESTMENT}
+            label={t(`fund-details-page.description.yourInvestment.${asset}`)}
           />
-        )}
-        {tab === TABS.EVENTS && (
-          <PortfolioEventsTable
-            getItems={getEvents(id!, EVENT_LOCATION.Asset)}
-            selector={selector}
-            asset={asset}
-            eventLocation={EVENT_LOCATION.Asset}
-            dateRangeStartLabel={t("filters.date-range.program-start")}
-            eventTypeFilterValues={eventTypeFilterValues!}
+          <GVTab
+            visible={haveEvents}
+            value={TABS.EVENTS}
+            label={t("program-details-page.history.tabs.events")}
           />
-        )}
+        </GVTabs>
       </div>
+      {tab === TABS.INVESTMENT && (
+        <InvestmentContainer
+          updateDescription={dispatchDescription}
+          asset={asset}
+          notice={notice}
+          id={id}
+          assetCurrency={currency}
+          personalDetails={personalDetails}
+          WithdrawContainer={WithdrawContainer}
+          ProgramReinvestingWidget={ProgramReinvestingWidget}
+        />
+      )}
+      {tab === TABS.EVENTS && (
+        <PortfolioEventsTable
+          getItems={getEvents(id!, EVENT_LOCATION.Asset)}
+          selector={selector}
+          asset={asset}
+          eventLocation={EVENT_LOCATION.Asset}
+          dateRangeStartLabel={t("filters.date-range.program-start")}
+          eventTypeFilterValues={eventTypeFilterValues!}
+        />
+      )}
     </DetailsBlock>
   );
 };
