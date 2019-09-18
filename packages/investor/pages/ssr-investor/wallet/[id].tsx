839babd8a10e994f678126745b7733de05ce037b
pavelzubov <pasha224@yandex.ru>
2019-09-18 13:39:03 +0300

    Change fetchWallets to fetchWalletsWithCtx

--- packages/investor/pages/ssr-investor/wallet/[id].tsx
+++ packages/investor/pages/ssr-investor/wallet/[id].tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { compose } from "redux";
 import platformActions from "shared/actions/platform-actions";
 import WalletCurrencyContainer from "shared/components/wallet/components/wallet-currency.container";
-import { fetchWallets } from "shared/components/wallet/services/wallet.services";
+import { fetchWalletsWithCtx } from "shared/components/wallet/services/wallet.services";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import withPrivateRoute from "shared/decorators/with-private-route";
 import { CurrencyEnum, NextPageWithRedux } from "shared/utils/types";
@@ -17,7 +17,7 @@ WalletDetails.getInitialProps = async ctx => {
     ctx.reduxStore.dispatch(
       async dispatch => await dispatch(platformActions.fetchPlatformSettings())
     ),
-    ctx.reduxStore.dispatch(fetchWallets(ctx))
+    ctx.reduxStore.dispatch(fetchWalletsWithCtx(ctx))
   ]);
   return { id };
 };
