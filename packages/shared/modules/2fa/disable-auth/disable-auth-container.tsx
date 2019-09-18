2b5fe819a99812416bb8559544b3b215d76610cc
pavelzubov <pasha224@yandex.ru>
2019-09-18 10:28:35 +0300

    Use useApiRequest

--- packages/shared/modules/2fa/disable-auth/disable-auth-container.tsx
+++ packages/shared/modules/2fa/disable-auth/disable-auth-container.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
 import { useCallback } from "react";
-import useErrorMessage from "shared/hooks/error-message.hook";
+import useApiRequest from "shared/hooks/api-request.hook";
 import useIsOpen from "shared/hooks/is-open.hook";
-import authApi from "shared/services/api-client/auth-api";
-import authService from "shared/services/auth-service";
 import { SetSubmittingType } from "shared/utils/types";
 
+import { disableTFA } from "../services/2fa.service";
 import DisableAuthForm, {
   IDisableAuthFormFormValues
 } from "./disable-auth-form";
@@ -13,22 +12,15 @@ import DisableAuthSuccess from "./disable-auth-success";
 
 const _DisableAuthContainer: React.FC<Props> = ({ onSubmit }) => {
   const [isSuccess, setSuccess, setFail] = useIsOpen();
-  const { errorMessage, setErrorMessage } = useErrorMessage();
+  const { errorMessage, sendRequest } = useApiRequest({
+    request: disableTFA,
+    catchCallback: () => setFail()
+  });
   const handleSubmit = useCallback(
     (model: IDisableAuthFormFormValues, setSubmitting: SetSubmittingType) =>
-      authApi
-        .v10Auth2faDisablePost(authService.getAuthArg(), {
-          model
-        })
-        .then(() => {
-          setSuccess();
-          onSubmit();
-        })
-        .catch(error => {
-          setErrorMessage(error);
-          setFail();
-          setSubmitting(false);
-        }),
+      sendRequest(model, setSubmitting)
+        .then(setSuccess)
+        .then(onSubmit),
     []
   );
   return isSuccess ? (
