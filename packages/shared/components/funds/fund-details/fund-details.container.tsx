44b8a1fb6334a258f4ad45731334123867fcea20
pavelzubov <pasha224@yandex.ru>
2019-09-19 10:10:37 +0300

    Set fees

--- packages/shared/components/funds/fund-details/fund-details.container.tsx
+++ packages/shared/components/funds/fund-details/fund-details.container.tsx
@@ -29,7 +29,7 @@ const _FundDetailsContainer: React.FC<Props> = ({
     />
     <div className="details__divider" />
     <DetailsInvestment
-      successFeeCurrent={0}
+      fees={{ exitFee: description.exitFee, entryFee: description.entryFee }}
       dispatchDescription={dispatchFundDescription}
       eventTypesSelector={fundEventsSelector}
       asset={ASSET.FUND}
