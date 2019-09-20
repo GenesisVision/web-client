ab2f3dc39e19cdfc6dd6771e35ea25c91aa67f62
pavelzubov <pasha224@yandex.ru>
2019-09-20 14:13:32 +0300

    Use BackButtonBody

--- packages/shared/components/back-button/back-button.tsx
+++ packages/shared/components/back-button/back-button.tsx
@@ -2,31 +2,13 @@ import "./back-button.scss";
 
 import Router from "next/router";
 import React from "react";
-import { useTranslation } from "react-i18next";
-import GVButton from "shared/components/gv-button";
 import useHistoryContext from "shared/decorators/history-provider/use-history-context";
+import BackButtonBody from "./back-button-body";
 
 export const _BackButton: React.FC = () => {
-  const [t] = useTranslation();
   const { from } = useHistoryContext();
   if (!from) return null;
-
-  return (
-    <div className="back-button">
-      <GVButton
-        variant="text"
-        onClick={() => Router.back()}
-        color="secondary"
-        className="back-button__container"
-      >
-        <>
-          <div className="back-button__back-arrow">&larr;</div>
-          <div className="back-button__back">{t("buttons.back")}</div>
-        </>
-      </GVButton>
-      <div className="back-button__path">{from}</div>
-    </div>
-  );
+  return <BackButtonBody onClick={() => Router.back()} backPath={from} />;
 };
 
 const BackButton = React.memo(_BackButton);
