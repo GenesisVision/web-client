ef254ada735f5a9091d3fdd96d876183862df29d
pavelzubov <pasha224@yandex.ru>
2019-09-22 21:48:07 +0300

    Add label, value props

--- packages/shared/components/statistic-item/statistic-item.txt-loader.tsx
+++ packages/shared/components/statistic-item/statistic-item.txt-loader.tsx
@@ -4,21 +4,31 @@ import classNames from "classnames";
 import * as React from "react";
 import { getRandomInteger, getRandomText } from "shared/utils/helpers";
 
-const _StatisticItemTxtLoader: React.FC<{ className?: string }> = ({
-  className
+const _StatisticItemTextLoader: React.FC<Props> = ({
+  className,
+  label,
+  value
 }) => (
   <div className={classNames("statistics-item", className)}>
-    <div
-      className="statistics-item__top statistics-item__label statistics-item__top statistics-item__label--loader"
-      style={{ width: 70 }}
-    >
-      {getRandomText({ length: getRandomInteger(5, 9), charset: "alphabetic" })}
+    <div className="statistics-item__top statistics-item__label statistics-item__top statistics-item__label--loader">
+      {label ||
+        getRandomText({
+          length: getRandomInteger(5, 9),
+          charset: "alphabetic"
+        })}
     </div>
-    <div className="statistics-item__value" style={{ width: 50 }}>
-      {getRandomText({ length: getRandomInteger(3, 5), charset: "numeric" })}
+    <div className="statistics-item__value">
+      {value ||
+        getRandomText({ length: getRandomInteger(3, 5), charset: "numeric" })}
     </div>
   </div>
 );
 
-const StatisticItemTxtLoader = React.memo(_StatisticItemTxtLoader);
-export default StatisticItemTxtLoader;
+interface Props {
+  label?: string;
+  value?: string | JSX.Element;
+  className?: string;
+}
+
+const StatisticItemTextLoader = React.memo(_StatisticItemTextLoader);
+export default StatisticItemTextLoader;
