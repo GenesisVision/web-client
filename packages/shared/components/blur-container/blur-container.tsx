c5785ee8b10de491c49ba8ae6b9373ae45a11625
pavelzubov <pasha224@yandex.ru>
2019-09-23 22:13:54 +0300

    Add _BlurContainer_ for use without show

--- packages/shared/components/blur-container/blur-container.tsx
+++ packages/shared/components/blur-container/blur-container.tsx
@@ -1,10 +1,16 @@
 import "./blur-container.scss";
 
+import classNames from "classnames";
 import React from "react";
 
-const _BlurContainer: React.FC<Props> = ({ children, blur, show }) => (
+const _BlurContainer: React.FC<Props> = ({
+  children,
+  blur,
+  show,
+  className
+}) => (
   <div
-    className="blur-container"
+    className={classNames("blur-container", className)}
     style={{
       position: show ? "relative" : "absolute",
       opacity: show ? 1 : 0,
@@ -16,6 +22,7 @@ const _BlurContainer: React.FC<Props> = ({ children, blur, show }) => (
 );
 
 interface Props {
+  className?: string;
   blur: boolean;
   show: boolean;
   loader?: boolean;
@@ -23,3 +30,25 @@ interface Props {
 
 const BlurContainer = React.memo(_BlurContainer);
 export default BlurContainer;
+
+const _BlurContainer_: React.FC<Props_> = ({
+                                           children,
+                                           blur,
+                                           className
+                                         }) => (
+  <div
+    className={classNames("blur-container", className)}
+    style={{
+      filter: `blur(${blur ? 5 : 0}px)`
+    }}
+  >
+    {children}
+  </div>
+);
+
+interface Props_ {
+  className?: string;
+  blur: boolean;
+}
+
+export const BlurContainer_ = React.memo(_BlurContainer_);
