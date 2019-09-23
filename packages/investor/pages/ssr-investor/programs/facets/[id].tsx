8af03f0310d42b5dc24275e1e8820f86d4c57b39
Евгений Гребенщиков <evgreb@gmail.com>
2019-09-23 13:28:07 +0300

    fix most_reliable facet

--- packages/investor/pages/ssr-investor/programs/facets/[id].tsx
+++ packages/investor/pages/ssr-investor/programs/facets/[id].tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { compose } from "redux";
 import platformActions from "shared/actions/platform-actions";
+import ProgramsRatingContainer from "shared/components/programs-rating/programs-rating-container";
 import ProgramsFacetPage from "shared/components/programs/programs-facet/programs-facet.page";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import { NextPageWithRedux } from "shared/utils/types";
 
 const ProgramFacet: NextPageWithRedux<Props, {}> = ({ id }) => {
+  if (id === "most_reliable") {
+    return <ProgramsRatingContainer />;
+  }
   return <ProgramsFacetPage id={id} />;
 };
 
