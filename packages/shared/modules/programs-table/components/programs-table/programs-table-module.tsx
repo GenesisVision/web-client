9a559d06b836cc3ed9e6f708ea30df41f78118dc
pavelzubov <pasha224@yandex.ru>
2019-09-24 14:46:22 +0300

    Set loaderData

--- packages/shared/modules/programs-table/components/programs-table/programs-table-module.tsx
+++ packages/shared/modules/programs-table/components/programs-table/programs-table-module.tsx
@@ -16,6 +16,7 @@ import ProgramTableHeaderCell from "./program-table-header-cell";
 import ProgramTableRow from "./program-table-row";
 import { FAVORITE_COLUMN_NAME } from "./programs-table";
 import { PROGRAMS_COLUMNS } from "./programs.constants";
+import { programListLoaderData } from "./program-table.loader-data";
 
 const ProgramTableModule: React.FC<Props> = React.memo(
   ({
@@ -33,6 +34,7 @@ const ProgramTableModule: React.FC<Props> = React.memo(
     columns
   }) => (
     <TableModule
+      loaderData={programListLoaderData}
       disableTitle={disableTitle}
       getItems={getItems}
       defaultFilters={defaultFilters}
