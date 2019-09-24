9241dd6cee2ef5377db35077841ca8e4e697d497
pavelzubov <pasha224@yandex.ru>
2019-09-24 14:43:55 +0300

    Add Tag

--- packages/shared/components/blur-container/blur-container.tsx
+++ packages/shared/components/blur-container/blur-container.tsx
@@ -2,19 +2,26 @@ import "./blur-container.scss";
 
 import classNames from "classnames";
 import React from "react";
+import { TagType } from "shared/utils/types";
 
-const _BlurContainer: React.FC<Props> = ({ children, blur, className }) => (
-  <div
+const _BlurContainer: React.FC<Props> = ({
+  children,
+  blur,
+  className,
+  tag: Tag = "div"
+}) => (
+  <Tag
     className={classNames("blur-container", className)}
     style={{
       filter: `blur(${blur ? 7 : 0}px)`
     }}
   >
     {children}
-  </div>
+  </Tag>
 );
 
 interface Props {
+  tag?: TagType;
   className?: string;
   blur: boolean;
 }
