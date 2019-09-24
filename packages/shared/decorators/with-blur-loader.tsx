b98def1093fdd508d97f8e637f65eae723f2a910
pavelzubov <pasha224@yandex.ru>
2019-09-24 14:45:39 +0300

    Add tag prop

--- packages/shared/decorators/with-blur-loader.tsx
+++ packages/shared/decorators/with-blur-loader.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { BlurContainer } from "shared/components/blur-container/blur-container";
+import { TagType } from "shared/utils/types";
 
 export interface WithBlurLoaderProps<T> {
+  tag?: TagType;
   className?: string;
   loaderData: T;
 }
@@ -9,12 +11,13 @@ export interface WithBlurLoaderProps<T> {
 export const withBlurLoader = <T, U extends { data?: T }>(
   Component: React.ComponentType<U>
 ): React.ComponentType<WithBlurLoaderProps<T> & U> => ({
+  tag,
   data,
   loaderData,
   className,
   ...other
 }) => (
-  <BlurContainer blur={!data} className={className}>
+  <BlurContainer blur={!data} className={className} tag={tag}>
     <Component {...other as U} data={data ? data : loaderData} />
   </BlurContainer>
 );
