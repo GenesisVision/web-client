c9b24289d187fb0d77157ffbe3eaf67634c2ccbe
pavelzubov <pasha224@yandex.ru>
2019-09-25 14:26:00 +0300

    Create TradesUpdating component

---
+++ packages/manager/src/pages/programs/programs-settings/trades-updating.tsx
@@ -0,0 +1,115 @@
+import { FormikProps, withFormik } from "formik";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { compose } from "redux";
+import GVButton from "shared/components/gv-button";
+import SettingsBlock from "shared/components/settings-block/settings-block";
+import { SetSubmittingType } from "shared/utils/types";
+import GVTextField from "shared/components/gv-text-field";
+import Select from "shared/components/select/select";
+import GVFormikField from "shared/components/gv-formik-field";
+import { ProgramDetailsFullTradesDelayEnum } from "gv-api-web";
+
+const delays = [
+  {
+    label: "Without",
+    value: "None"
+  },
+  {
+    label: "5 minutes",
+    value: "FiveMinutes"
+  },
+  {
+    label: "15 minutes",
+    value: "FifteenMinutes"
+  },
+  {
+    label: "30 minutes",
+    value: "ThirtyMinutes"
+  },
+  {
+    label: "1 hour",
+    value: "OneHour"
+  },
+  {
+    label: "6 hours",
+    value: "SixHours"
+  }
+];
+
+const _TradesUpdating: React.FC<Props> = ({
+  values,
+  handleSubmit,
+  dirty,
+  isValid,
+  isSubmitting
+}) => {
+  const [t] = useTranslation();
+  return (
+    <SettingsBlock
+      label={t("manager.program-settings.trades-update.title")}
+      content={
+        <form id="edit-form" onSubmit={handleSubmit}>
+          <div className="program-settings__block-wrapper create-program-settings__row">
+            <GVFormikField
+              name={FIELDS.tradesDelay}
+              component={GVTextField}
+              label={t("manager.program-settings.trades-update.select")}
+              InputComponent={Select}
+            >
+              {delays.map(({ label, value }) => (
+                <option value={value} key={value}>
+                  {label}
+                </option>
+              ))}
+            </GVFormikField>
+          </div>
+          <p className="program-settings__text">
+            {t("manager.program-settings.trades-update.text")}
+          </p>
+          <GVButton
+            color="primary"
+            type={"submit"}
+            className="invest-form__submit-button"
+            disabled={!dirty || !isValid || isSubmitting}
+          >
+            {t("manager.program-settings.buttons.save")}
+          </GVButton>
+        </form>
+      }
+    />
+  );
+};
+
+enum FIELDS {
+  tradesDelay = "tradesDelay"
+}
+
+export interface TradesUpdatingFormValues {
+  [FIELDS.tradesDelay]: ProgramDetailsFullTradesDelayEnum;
+}
+
+interface Props extends OwnProps, FormikProps<TradesUpdatingFormValues> {}
+
+interface OwnProps {
+  tradesDelay: ProgramDetailsFullTradesDelayEnum;
+  onSubmit: (
+    values: TradesUpdatingFormValues,
+    setSubmitting: SetSubmittingType
+  ) => void;
+}
+
+const TradesUpdating = compose<React.ComponentType<OwnProps>>(
+  withFormik<OwnProps, TradesUpdatingFormValues>({
+    enableReinitialize: true,
+    displayName: "edit-form",
+    mapPropsToValues: ({ tradesDelay }) => ({
+      [FIELDS.tradesDelay]: tradesDelay
+    }),
+    handleSubmit: (values, { props, setSubmitting }) => {
+      props.onSubmit(values, setSubmitting);
+    }
+  }),
+  React.memo
+)(_TradesUpdating);
+export default TradesUpdating;
