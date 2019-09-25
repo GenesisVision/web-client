9c72856cea85c9d543eaf418b0361358179ecfd0
pavelzubov <pasha224@yandex.ru>
2019-09-25 15:09:34 +0300

    Set TradesDelayHint

--- packages/shared/components/programs/program-details/program-history-section/program-open-positions/program-open-positions.tsx
+++ packages/shared/components/programs/program-details/program-history-section/program-open-positions/program-open-positions.tsx
@@ -6,15 +6,61 @@ import { PROGRAM_OPEN_POSITIONS_COLUMNS } from "shared/components/programs/progr
 import TableContainer from "shared/components/table/components/table-container";
 import { CurrencyEnum } from "shared/utils/types";
 
-import { openPositionsTableSelector } from "../../reducers/program-history.reducer";
+import {
+  openPositionsSelector,
+  openPositionsTableSelector
+} from "../../reducers/program-history.reducer";
 import { getOpenPositions } from "../../services/program-details.service";
 import ProgramOpenPositionsRow from "./program-open-positions-row";
+import { useSelector } from "react-redux";
+import { TradesDelayHint } from "../trades-delay-hint";
+
+export const DELAYS_LABELS = {
+  None: "Without",
+  FiveMinutes: "5 minutes",
+  FifteenMinutes: "15 minutes",
+  ThirtyMinutes: "30 minutes",
+  OneHour: "1 hour",
+  SixHours: "6 hours"
+};
+
+export const DELAYS = [
+  {
+    label: DELAYS_LABELS["None"],
+    value: "None"
+  },
+  {
+    label: DELAYS_LABELS["FiveMinutes"],
+    value: "FiveMinutes"
+  },
+  {
+    label: DELAYS_LABELS["FifteenMinutes"],
+    value: "FifteenMinutes"
+  },
+  {
+    label: DELAYS_LABELS["ThirtyMinutes"],
+    value: "ThirtyMinutes"
+  },
+  {
+    label: DELAYS_LABELS["OneHour"],
+    value: "OneHour"
+  },
+  {
+    label: DELAYS_LABELS["SixHours"],
+    value: "SixHours"
+  }
+];
 
 const _ProgramOpenPositions: React.FC<Props> = ({ currency, programId }) => {
   const [t] = useTranslation();
+  const {
+    itemsData: { data }
+  } = useSelector(openPositionsSelector);
+  const delay = data ? data.tradesDelay : "None";
   if (!programId) return null;
   return (
     <TableContainer
+      exportButtonToolbarRender={() => <TradesDelayHint delay={delay} />}
       getItems={getOpenPositions(programId)}
       dataSelector={openPositionsTableSelector}
       isFetchOnMount={true}
