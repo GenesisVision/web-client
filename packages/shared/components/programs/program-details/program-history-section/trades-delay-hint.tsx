eb5c7fb978a73c1c8134f00fc0e33db245b5e1aa
pavelzubov <pasha224@yandex.ru>
2019-09-26 07:54:48 +0300

    Correct hint

--- packages/shared/components/programs/program-details/program-history-section/trades-delay-hint.tsx
+++ packages/shared/components/programs/program-details/program-history-section/trades-delay-hint.tsx
@@ -1,32 +1,34 @@
-import Tooltip from "shared/components/tooltip/tooltip";
-import React from "react";
-import { DELAYS_LABELS } from "./program-open-positions/program-open-positions";
-import { useTranslation } from "react-i18next";
 import { TradesViewModelTradesDelayEnum } from "gv-api-web";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { HORIZONTAL_POPOVER_POS } from "shared/components/popover/popover";
+import Tooltip from "shared/components/tooltip/tooltip";
+
+import { DELAYS_LABELS } from "./program-open-positions/program-open-positions";
 
 const _TradesDelayHint: React.FC<{ delay: TradesViewModelTradesDelayEnum }> = ({
   delay
 }) => {
   const [t] = useTranslation();
-  return (
+  return delay !== "None" ? (
     <>
-      {delay !== "None" && (
-        <Tooltip
-          render={() => (
-            <div>
-              {t("program-details-page.history.open-positions.delay-tooltip", {
-                delay
-              })}
-            </div>
-          )}
-        >
-          <div className="details-trades__delay-hint">
-            {DELAYS_LABELS[delay]}{" "}
-            {t("program-details-page.history.open-positions.delay")}
+      <div className="details-trades__delay-hint">
+        {DELAYS_LABELS[delay]}{" "}
+        {t("program-details-page.history.open-positions.delay")}
+      </div>
+      <Tooltip
+        horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+        render={() => (
+          <div className="details-trades__delay-tooltip">
+            {t("program-details-page.history.open-positions.delay-tooltip", {
+              delay
+            })}
           </div>
-        </Tooltip>
-      )}
+        )}
+      >
+        <div className="details-trades__delay-question">?</div>
+      </Tooltip>
     </>
-  );
+  ) : null;
 };
 export const TradesDelayHint = React.memo(_TradesDelayHint);
