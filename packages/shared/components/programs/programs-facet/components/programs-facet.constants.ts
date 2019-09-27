d93a0bd964d5d674207039fb43867b61fe5d7f54
pavelzubov <pasha224@yandex.ru>
2019-09-27 14:43:28 +0300

    Rename programCurrencyFilter to programsCurrencyMap

--- packages/shared/components/programs/programs-facet/components/programs-facet.constants.ts
+++ packages/shared/components/programs/programs-facet/components/programs-facet.constants.ts
@@ -1,11 +1,11 @@
 import { DEFAULT_PAGING } from "shared/components/table/reducers/table-paging.reducer";
 import {
-  programCurrencyFilter,
+  programsCurrencyMap,
   programsDateRangeFilter
 } from "shared/modules/programs-table/components/programs-table/programs.constants";
 
 export const PROGRAMS_FACET_TABLE_FILTERS = [
-  programCurrencyFilter,
+  programsCurrencyMap,
   programsDateRangeFilter
 ];
 
