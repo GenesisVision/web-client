9703b10d98c7128ff1b2746c96b8b9c7b91a3df3
pavelzubov <pasha224@yandex.ru>
2019-09-27 14:43:37 +0300

    Add composeCurrencyMap

--- packages/shared/modules/programs-table/components/programs-table/program-table.helpers.ts
+++ packages/shared/modules/programs-table/components/programs-table/program-table.helpers.ts
@@ -1,8 +1,15 @@
 import { SelectFilterValue } from "shared/components/table/components/filtering/filter.type";
 
-export const composeCurrencyFilter = (
-  currencies?: string[]
+export const composeCurrencyMap = (
+  currencies: string[] | undefined = []
 ): SelectFilterValue<string | undefined>[] => [
   { value: undefined, label: "Base" },
-  ...(currencies || []).map(x => ({ value: x, label: x }))
+  ...currencies.map(x => ({ value: x, label: x }))
+];
+
+export const composeCurrencyFilter = (
+  currencies: string[] | undefined = []
+): SelectFilterValue<string | undefined>[] => [
+  { value: undefined, label: "All" },
+  ...currencies.map(x => ({ value: x, label: x }))
 ];
