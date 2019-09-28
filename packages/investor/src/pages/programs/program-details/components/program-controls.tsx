cd76cfc108f85ae2d8ddd41702ad094f3b1e5867
pavelzubov <pasha224@yandex.ru>
2019-09-28 10:27:45 +0300

    Use SignalProviderControls

--- packages/investor/src/pages/programs/program-details/components/program-controls.tsx
+++ packages/investor/src/pages/programs/program-details/components/program-controls.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import DetailsBlock, {
-  DETAILS_BLOCK_TYPE
-} from "shared/components/details/details-block";
+import SignalProviderControls from "shared/components/details/details-description-section/details-description/controls/signal-provider-controls/signal-provider-controls";
 import { IProgramControlsProps } from "shared/components/programs/program-details/program-details.types";
+import { ROLE } from "shared/constants/constants";
+import useRole from "shared/hooks/use-role.hook";
 
 import InvestmentProgramControls from "./investment-program-controls";
-import SignalProviderControls from "./signal-provider-controls";
+import SignalProviderButtons from "./signal-provider-buttons";
 
 const _ProgramControls: React.FC<IProgramControlsProps> = ({
-  programDescription,
-  isAuthenticated
+  programDescription
 }) => {
-  const isAvailableFollowingTrades = programDescription.isSignalProgram;
+  const role = useRole();
   return (
     <div className="asset-details-description__controls">
-      <InvestmentProgramControls
-        programDescription={programDescription}
-        isAuthenticated={isAuthenticated}
-      />
-      {isAvailableFollowingTrades ? (
-        <DetailsBlock type={DETAILS_BLOCK_TYPE.BORDERED}>
-          <SignalProviderControls
-            programDescription={programDescription}
-            isAuthenticated={isAuthenticated}
+      <InvestmentProgramControls programDescription={programDescription} />
+      {programDescription.isSignalProgram && (
+        <SignalProviderControls programDescription={programDescription}>
+          <SignalProviderButtons
+            condition={
+              role === ROLE.INVESTOR &&
+              !!programDescription.personalProgramDetails
+            }
+            personalDetails={programDescription.personalProgramDetails}
+            id={programDescription.id}
+            title={programDescription.title}
+            currency={programDescription.currency}
           />
-        </DetailsBlock>
-      ) : null}
+        </SignalProviderControls>
+      )}
     </div>
   );
 };
