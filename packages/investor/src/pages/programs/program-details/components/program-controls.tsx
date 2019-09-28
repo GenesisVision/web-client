8deecbe39f723678a087971965d257b015b4f42d
pavelzubov <pasha224@yandex.ru>
2019-09-28 10:58:45 +0300

    Remove role from condition

--- packages/investor/src/pages/programs/program-details/components/program-controls.tsx
+++ packages/investor/src/pages/programs/program-details/components/program-controls.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import SignalProviderControls from "shared/components/details/details-description-section/details-description/controls/signal-provider-controls/signal-provider-controls";
 import { IProgramControlsProps } from "shared/components/programs/program-details/program-details.types";
-import { ROLE } from "shared/constants/constants";
-import useRole from "shared/hooks/use-role.hook";
 
 import InvestmentProgramControls from "./investment-program-controls";
 import SignalProviderButtons from "./signal-provider-buttons";
@@ -10,17 +8,13 @@ import SignalProviderButtons from "./signal-provider-buttons";
 const _ProgramControls: React.FC<IProgramControlsProps> = ({
   programDescription
 }) => {
-  const role = useRole();
   return (
     <div className="asset-details-description__controls">
       <InvestmentProgramControls programDescription={programDescription} />
       {programDescription.isSignalProgram && (
         <SignalProviderControls programDescription={programDescription}>
           <SignalProviderButtons
-            condition={
-              role === ROLE.INVESTOR &&
-              !!programDescription.personalProgramDetails
-            }
+            condition={!!programDescription.personalProgramDetails}
             personalDetails={programDescription.personalProgramDetails}
             id={programDescription.id}
             title={programDescription.title}
