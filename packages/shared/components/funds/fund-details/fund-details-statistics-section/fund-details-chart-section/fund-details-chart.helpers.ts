50d1ef9d88d8f1d6928bd1e92bd18ffede3f99da
pavelzubov <pasha224@yandex.ru>
2019-09-29 15:23:13 +0300

    Set view

--- packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.helpers.ts
+++ packages/shared/components/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-details-chart.helpers.ts
@@ -1,25 +1,21 @@
+import { DETAILS_CHART_TABS } from "shared/components/details/details-statistic-section/details-chart-section/details-chart";
 import {
   useChartPeriodCreator,
   useChartStateDataCreator,
   useFundChartStateValuesCreator
 } from "shared/components/details/details-statistic-section/details.chart.helpers";
 
-import {
-  statisticCurrencyAction,
-  statisticPeriodAction
-} from "../../actions/fund-details.actions";
+import { statisticCurrencyAction, statisticPeriodAction } from "../../actions/fund-details.actions";
 import { fundBalanceChartSelector } from "../../reducers/balance-chart.reducer";
 import { fundIdSelector } from "../../reducers/description.reducer";
 import { fundProfitChartSelector } from "../../reducers/profit-chart.reducer";
 import { statisticCurrencySelector } from "../../reducers/statistic-currency.reducer";
 import { statisticPeriodSelector } from "../../reducers/statistic-period.reducer";
-import {
-  getBalanceChart,
-  getProfitChart
-} from "../../services/fund-details.service";
+import { getBalanceChart, getProfitChart } from "../../services/fund-details.service";
 
-export const useFundChartStateData = () =>
+export const useFundChartStateData = (view: DETAILS_CHART_TABS) =>
   useChartStateDataCreator({
+    view,
     statisticCurrencyAction,
     idSelector: fundIdSelector,
     statisticPeriodSelector,
@@ -30,8 +26,7 @@ export const useFundChartStateData = () =>
     getProfitChart
   });
 
-export const useFundChartStateValues = () =>
-  useFundChartStateValuesCreator(useFundChartStateData);
+export const useFundChartStateValues = (view: DETAILS_CHART_TABS) => useFundChartStateValuesCreator(useFundChartStateData(view));
 
 export const useChartPeriod = () =>
   useChartPeriodCreator(statisticPeriodSelector, statisticPeriodAction);
