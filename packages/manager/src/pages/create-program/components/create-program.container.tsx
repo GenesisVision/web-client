56667965d6ee05a1348641cfb86245640f349a82
pavelzubov <pasha224@yandex.ru>
2019-09-30 13:39:33 +0300

    Convert to FC

--- packages/manager/src/pages/create-program/components/create-program.container.tsx
+++ packages/manager/src/pages/create-program/components/create-program.container.tsx
@@ -1,290 +1,178 @@
 import { push } from "connected-react-router";
-import {
-  Broker,
-  CancelablePromise,
-  ManagerProgramCreateResult,
-  ProfileHeaderViewModel,
-  ProgramsInfo,
-  WalletData
-} from "gv-api-web";
-import ConfirmContainer from "modules/confirm/confirm-container";
-import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { connect } from "react-redux";
-import { ManagerRootState } from "reducers";
-import { compose } from "redux";
+import { Broker } from "gv-api-web";
+import React, { useCallback, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
 import ConfirmPopup from "shared/components/confirm-popup/confirm-popup";
 import GVTabs from "shared/components/gv-tabs";
 import GVTab from "shared/components/gv-tabs/gv-tab";
 import { walletsSelector } from "shared/components/wallet/reducers/wallet.reducers";
 import { fetchWallets } from "shared/components/wallet/services/wallet.services";
+import useIsOpen from "shared/hooks/is-open.hook";
+import useTab from "shared/hooks/tab.hook";
 import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
+import { currencySelector } from "shared/reducers/account-settings-reducer";
 import { headerSelector } from "shared/reducers/header-reducer";
 import { programsInfoSelector } from "shared/reducers/platform-reducer";
 import { DASHBOARD_ROUTE } from "shared/routes/dashboard.routes";
-import { rateApi } from "shared/services/api-client/rate-api";
-import {
-  CurrencyEnum,
-  MiddlewareDispatch,
-  ResponseError,
-  SetSubmittingType
-} from "shared/utils/types";
+import { ResponseError, SetSubmittingType } from "shared/utils/types";
 
 import {
   createProgram,
   fetchBrokers
 } from "../services/create-program.service";
+import { TFAConfirmBlock } from "./confirm-block";
 import CreateProgramBroker from "./create-program-broker/create-program-broker";
 import CreateProgramSettingsSection from "./create-program-settings/create-program-settings-section";
-import { currencySelector } from "shared/reducers/account-settings-reducer";
+
+const _CreateProgramContainer: React.FC = () => {
+  const [t] = useTranslation();
+  const dispatch = useDispatch();
+
+  const currency = useSelector(currencySelector);
+  const wallets = useSelector(walletsSelector);
+  const headerData = useSelector(headerSelector);
+  const programsInfo = useSelector(programsInfoSelector);
+
+  const [minimumDepositsAmount, setMinimumDepositsAmount] = useState<
+    { [key: string]: number } | undefined
+  >(undefined);
+  const [programId, setProgramId] = useState<string | undefined>(undefined);
+  const [selectedBroker, setSelectedBroker] = useState<Broker | undefined>(
+    undefined
+  );
+  const [brokers, setBrokers] = useState<Broker[] | undefined>(undefined);
+  const [isPending, setIsPending, setIsNotPending] = useIsOpen(true);
+  const [twoFactorRequired, setTwoFactorRequired] = useIsOpen();
+  const [isConfirmDialogVisible] = useIsOpen();
+  const [
+    isNavigationDialogVisible,
+    setIsNavigationDialogVisible,
+    setIsNavigationDialogNotVisible
+  ] = useIsOpen();
+  const { tab, setTab } = useTab<TAB>(TAB.BROKER);
+
+  useEffect(() => {
+    dispatch(fetchWallets(currency));
+    fetchBrokers().then(brokers => {
+      setBrokers(brokers);
+      setSelectedBroker(brokers[0]);
+      setMinimumDepositsAmount(
+        brokers[0].accountTypes[0].minimumDepositsAmount
+      );
+      setIsNotPending();
+    });
+  }, []);
+
+  const confirmNavigateToBroker = useCallback(
+    (setSubmitting: (isSubmitting: boolean) => void) => {
+      setTab(null, TAB.BROKER);
+      setSubmitting(false);
+    },
+    []
+  );
+
+  const navigateToSettings = useCallback(() => {
+    setTab(null, TAB.SETTINGS);
+  }, []);
+
+  const createProgramHandle = (data: any, setSubmitting: SetSubmittingType) => {
+    createProgram(data)
+      .then(({ programId }) => {
+        if (twoFactorRequired) {
+          setProgramId(programId);
+          setTwoFactorRequired();
+        } else {
+          dispatch(push(DASHBOARD_ROUTE));
+          dispatch(
+            alertMessageActions.success(
+              "manager.create-program-page.notifications.create-success"
+            )
+          );
+          dispatch(fetchWallets(currency));
+          setSubmitting(false);
+        }
+      })
+      .catch((error: ResponseError) => {
+        dispatch(alertMessageActions.error(error.errorMessage));
+      })
+      .finally(() => {
+        setSubmitting(false);
+      });
+  };
+
+  if (
+    !brokers ||
+    !selectedBroker ||
+    !programsInfo ||
+    !headerData ||
+    !wallets.length ||
+    !minimumDepositsAmount
+  )
+    return null;
+
+  return (
+    <div className="create-program-page__container">
+      <div className="create-program-page__tabs">
+        <GVTabs value={tab}>
+          <GVTab
+            value={TAB.BROKER}
+            label={t("manager.create-program-page.tabs.select-broker")}
+          />
+          <GVTab
+            value={TAB.SETTINGS}
+            label={t("manager.create-program-page.tabs.settings")}
+          />
+        </GVTabs>
+      </div>
+      {!isPending && (
+        <div>
+          {tab === TAB.BROKER && (
+            <CreateProgramBroker
+              navigateToSettings={navigateToSettings}
+              brokers={brokers}
+              selectedBroker={selectedBroker}
+              setMinimumDepositsAmount={setMinimumDepositsAmount}
+              isForexAllowed={headerData.allowForex}
+              isKycConfirmed={headerData.kycConfirmed}
+            />
+          )}
+          {tab === TAB.SETTINGS && (
+            <CreateProgramSettingsSection
+              currency={currency}
+              minimumDepositsAmount={minimumDepositsAmount}
+              fetchWallets={currency => dispatch(fetchWallets(currency))}
+              wallets={wallets}
+              navigateBack={setIsNavigationDialogVisible}
+              broker={selectedBroker}
+              onSubmit={createProgramHandle}
+              notifyError={message =>
+                dispatch(alertMessageActions.error(message))
+              }
+              author={headerData.name}
+              programsInfo={programsInfo}
+            />
+          )}
+          {twoFactorRequired && (
+            <TFAConfirmBlock isOpen={isConfirmDialogVisible} id={programId!} />
+          )}
+          <ConfirmPopup
+            open={isNavigationDialogVisible}
+            onClose={setIsNavigationDialogNotVisible}
+            onApply={confirmNavigateToBroker}
+            body={t("manager.create-program-page.navigation-back-text")}
+            applyButtonText={t("buttons.continue")}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 enum TAB {
   BROKER = "BROKER",
   SETTINGS = "SETTINGS"
 }
 
-class _CreateProgramContainer extends React.PureComponent<Props, State> {
-  state: State = {
-    minimumDepositsAmount: undefined,
-    tab: TAB.BROKER,
-    selectedBroker: undefined,
-    brokers: undefined,
-    isPending: true,
-    isConfirmDialogVisible: false,
-    isNavigationDialogVisible: false
-  };
-
-  componentDidMount() {
-    const { service, currency } = this.props;
-    service.fetchWallets(currency);
-    fetchBrokers().then(brokers => {
-      this.setState({
-        brokers: brokers,
-        selectedBroker: brokers[0],
-        minimumDepositsAmount: brokers[0].accountTypes[0].minimumDepositsAmount,
-        isPending: false
-      });
-    });
-  }
-
-  selectBroker = (brokerName: string) => () => {
-    const selectedBroker = this.state.brokers!.find(x => x.name === brokerName);
-    const minimumDepositsAmount = selectedBroker
-      ? selectedBroker.accountTypes[0].minimumDepositsAmount
-      : undefined;
-    this.setState({ minimumDepositsAmount, selectedBroker });
-  };
-
-  confirmNavigateToBroker = (
-    setSubmitting: (isSubmitting: boolean) => void
-  ) => {
-    this.setState({ tab: TAB.BROKER, isNavigationDialogVisible: false }, () => {
-      setSubmitting(false);
-    });
-  };
-
-  navigateToSettings = () => {
-    this.setState({ tab: TAB.SETTINGS });
-  };
-
-  navigateToBroker = () => {
-    this.setState({ isNavigationDialogVisible: true });
-  };
-
-  fetchRate = (
-    fromCurrency: string,
-    toCurrency: string
-  ): CancelablePromise<number> => {
-    return rateApi.v10RateByFromByToGet(fromCurrency, toCurrency);
-  };
-
-  createProgram = (data: any, setSubmitting: SetSubmittingType) => {
-    const {
-      createProgram,
-      redirectToDashboard,
-      notifyError,
-      notifySuccess,
-      fetchWallets
-    } = this.props.service;
-    createProgram(data)
-      .then(res => {
-        const { programId, twoFactorRequired } = res;
-        if (twoFactorRequired)
-          this.setState({
-            programId,
-            twoFactorRequired,
-            isConfirmDialogVisible: true
-          });
-        else {
-          redirectToDashboard();
-          notifySuccess(
-            "manager.create-program-page.notifications.create-success"
-          );
-          fetchWallets(this.props.currency);
-          setSubmitting(false);
-        }
-      })
-      .catch((error: ResponseError) => {
-        setSubmitting(false);
-        notifyError(error.errorMessage);
-      });
-  };
-
-  closeConfirm = () => {
-    this.setState({ isConfirmDialogVisible: false });
-    this.props.service.redirectToDashboard();
-  };
-
-  render() {
-    const {
-      minimumDepositsAmount,
-      tab,
-      selectedBroker,
-      isPending,
-      brokers,
-      twoFactorRequired,
-      programId,
-      isConfirmDialogVisible,
-      isNavigationDialogVisible
-    } = this.state;
-
-    const {
-      t,
-      headerData,
-      service,
-      programsInfo,
-      wallets,
-      currency
-    } = this.props;
-    if (
-      !brokers ||
-      !selectedBroker ||
-      !programsInfo ||
-      !headerData ||
-      !wallets.length ||
-      !minimumDepositsAmount
-    )
-      return null;
-    return (
-      <div className="create-program-page__container">
-        <div className="create-program-page__tabs">
-          <GVTabs value={tab}>
-            <GVTab
-              value={TAB.BROKER}
-              label={t("manager.create-program-page.tabs.select-broker")}
-            />
-            <GVTab
-              value={TAB.SETTINGS}
-              label={t("manager.create-program-page.tabs.settings")}
-            />
-          </GVTabs>
-        </div>
-        {!isPending && (
-          <div>
-            {tab === TAB.BROKER && (
-              <CreateProgramBroker
-                navigateToSettings={this.navigateToSettings}
-                brokers={brokers}
-                selectedBroker={selectedBroker}
-                selectBroker={this.selectBroker}
-                isForexAllowed={headerData.allowForex}
-                isKycConfirmed={headerData.kycConfirmed}
-              />
-            )}
-            {tab === TAB.SETTINGS && (
-              <CreateProgramSettingsSection
-                currency={currency}
-                minimumDepositsAmount={minimumDepositsAmount}
-                fetchWallets={service.fetchWallets}
-                fetchRate={this.fetchRate}
-                wallets={wallets}
-                navigateBack={this.navigateToBroker}
-                broker={selectedBroker}
-                onSubmit={this.createProgram}
-                notifyError={service.notifyError}
-                author={headerData.name}
-                programsInfo={programsInfo}
-              />
-            )}
-            {twoFactorRequired && (
-              <ConfirmContainer
-                open={isConfirmDialogVisible}
-                onClose={this.closeConfirm}
-                onApply={this.closeConfirm}
-                programId={programId!}
-              />
-            )}
-            <ConfirmPopup
-              open={isNavigationDialogVisible}
-              onClose={() =>
-                this.setState({ isNavigationDialogVisible: false })
-              }
-              onApply={this.confirmNavigateToBroker}
-              body={t("manager.create-program-page.navigation-back-text")}
-              applyButtonText={t("buttons.continue")}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: ManagerRootState): StateProps => ({
-  currency: currencySelector(state),
-  wallets: walletsSelector(state),
-  headerData: headerSelector(state),
-  programsInfo: programsInfoSelector(state)
-});
-
-const mapDispatchToProps = (dispatch: MiddlewareDispatch): DispatchProps => ({
-  service: {
-    redirectToDashboard: () => dispatch(push(DASHBOARD_ROUTE)),
-    createProgram: (data: any) => dispatch(createProgram(data)),
-    fetchWallets: currency => dispatch(fetchWallets(currency)),
-    notifyError: (message: string) =>
-      dispatch(alertMessageActions.error(message)),
-    notifySuccess: (message: string) =>
-      dispatch(alertMessageActions.success(message, true))
-  }
-});
-
-const CreateProgramContainer = compose<React.ComponentType>(
-  translate(),
-  connect<StateProps, DispatchProps, {}, ManagerRootState>(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(_CreateProgramContainer);
+const CreateProgramContainer = React.memo(_CreateProgramContainer);
 export default CreateProgramContainer;
-
-interface State {
-  minimumDepositsAmount?: { [key: string]: number };
-  selectedBroker?: Broker;
-  brokers?: Broker[];
-  isPending: boolean;
-  tab: TAB.BROKER | TAB.SETTINGS;
-  isNavigationDialogVisible: boolean;
-  isConfirmDialogVisible: boolean;
-  twoFactorRequired?: boolean;
-  programId?: string;
-}
-
-interface StateProps {
-  currency: CurrencyEnum;
-  programsInfo?: ProgramsInfo;
-  wallets: WalletData[];
-  headerData?: ProfileHeaderViewModel;
-}
-
-interface DispatchProps {
-  service: {
-    fetchWallets: (currency: CurrencyEnum) => void;
-    createProgram: (data: any) => CancelablePromise<ManagerProgramCreateResult>;
-    notifyError: (message: string) => void;
-    notifySuccess: (message: string) => void;
-    redirectToDashboard: () => void;
-  };
-}
-
-interface Props extends StateProps, DispatchProps, WithTranslation {}
