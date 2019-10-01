701d00aebbc5d629adac407cc23e1d4ae1ad96c3
pavelzubov <pasha224@yandex.ru>
2019-10-01 14:54:00 +0300

    Create PeriodLength component

---
+++ packages/manager/src/pages/create-program/components/create-program-settings/fields/period-length.tsx
@@ -0,0 +1,37 @@
+import { ProgramsInfo } from "gv-api-web";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import GVFormikField from "shared/components/gv-formik-field";
+import GVTextField from "shared/components/gv-text-field";
+import Select from "shared/components/select/select";
+
+const _PeriodLength: React.FC<Props> = ({ name, programsInfo }) => {
+  const [t] = useTranslation();
+  return (
+    <div className="create-program-settings__field">
+      <GVFormikField
+        name={name}
+        component={GVTextField}
+        label={t("manager.create-program-page.settings.fields.period")}
+        InputComponent={Select}
+      >
+        {programsInfo.periods.map(period => (
+          <option value={period} key={period}>
+            {`${period} ${t(
+              "manager.create-program-page.settings.fields.period-option-notation.day",
+              { count: period }
+            )}`}
+          </option>
+        ))}
+      </GVFormikField>
+    </div>
+  );
+};
+
+interface Props {
+  name: string;
+  programsInfo: ProgramsInfo;
+}
+
+const PeriodLength = React.memo(_PeriodLength);
+export default PeriodLength;
