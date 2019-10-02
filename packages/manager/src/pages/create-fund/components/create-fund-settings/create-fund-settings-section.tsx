79edb9175cc511abdf21cefac2aaa2d3abd94bb6
pavelzubov <pasha224@yandex.ru>
2019-10-02 13:54:02 +0300

    Convert to FC

--- packages/manager/src/pages/create-fund/components/create-fund-settings/create-fund-settings-section.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund-settings/create-fund-settings-section.tsx
@@ -1,107 +1,59 @@
-import { PlatformAsset, WalletData } from "gv-api-web";
-import * as React from "react";
+import useCreateAssetSection from "components/create-asset/create-asset-section.hook";
+import React from "react";
+import withLoader from "shared/decorators/with-loader";
+import { fetchRate } from "shared/services/rate-service";
 import { CurrencyEnum, SetSubmittingType } from "shared/utils/types";
 
 import CreateFundSettings, {
   ICreateFundSettingsFormValues
 } from "./create-fund-settings";
-import withLoader from "shared/decorators/with-loader";
 
 const FUND_CURRENCY = "GVT";
 
-class _CreateFundSettingsSection extends React.PureComponent<
-  OwnProps,
-  StateProps
-> {
-  constructor(props: OwnProps) {
-    super(props);
-    const wallet = this.props.wallets.find(x => x.currency === FUND_CURRENCY)!;
+const _CreateFundSettingsSection: React.FC<OwnProps> = ({
+  navigateBack,
+  onSubmit,
+  author,
+  minimumDepositAmount,
+  managerMaxExitFee,
+  managerMaxEntryFee
+}) => {
+  const { rate, handleWalletChange, wallet, wallets } = useCreateAssetSection({
+    assetCurrency: FUND_CURRENCY
+  });
 
-    this.state = {
-      wallet,
-      rate: undefined
-    };
+  return (
+    <CreateFundSettings
+      condition={!!wallet}
+      wallets={wallets}
+      fundCurrency={FUND_CURRENCY}
+      navigateBack={navigateBack}
+      onSubmit={onSubmit}
+      author={author}
+      minimumDepositAmount={minimumDepositAmount}
+      managerMaxExitFee={managerMaxExitFee}
+      managerMaxEntryFee={managerMaxEntryFee}
+      rate={rate}
+      wallet={wallet}
+      onWalletChange={handleWalletChange}
+    />
+  );
+};
 
-    this.updateRate(wallet.currency, FUND_CURRENCY);
-  }
-
-  componentDidUpdate(prevProps: OwnProps) {
-    const wallet = this.props.wallets.find(x => x.id === this.state.wallet.id)!;
-    this.setState({ wallet });
-    this.updateRate(wallet.currency, FUND_CURRENCY);
-  }
-
-  handleWalletChange = (walletId: string): void => {
-    if (this.state.wallet && this.state.wallet.id === walletId) return;
-    this.props.fetchWallets(this.props.currency);
-    const wallet = this.props.wallets.find(x => x.id === walletId)!;
-    this.setState({ wallet });
-  };
-
-  updateRate = (from?: string, to?: string): void => {
-    if (!from || !to) {
-      this.setState({ rate: undefined });
-    } else {
-      this.props.fetchRate(from, to).then(rate => {
-        this.setState({ rate });
-      });
-    }
-  };
-  render() {
-    const {
-      wallets,
-      navigateBack,
-      onSubmit,
-      author,
-      assets,
-      minimumDepositAmount,
-      managerMaxExitFee,
-      managerMaxEntryFee,
-      notifyError
-    } = this.props;
-    const { wallet, rate } = this.state;
-    if (!rate) return null;
-    return (
-      <CreateFundSettings
-        wallets={wallets}
-        fundCurrency={FUND_CURRENCY}
-        navigateBack={navigateBack}
-        onSubmit={onSubmit}
-        author={author}
-        assets={assets}
-        minimumDepositAmount={minimumDepositAmount}
-        managerMaxExitFee={managerMaxExitFee}
-        managerMaxEntryFee={managerMaxEntryFee}
-        rate={rate}
-        wallet={wallet}
-        notifyError={notifyError}
-        onWalletChange={this.handleWalletChange}
-      />
-    );
-  }
-}
-
-const CreateFundSettingsSection = withLoader(_CreateFundSettingsSection);
+const CreateFundSettingsSection = withLoader(
+  React.memo(_CreateFundSettingsSection)
+);
 export default CreateFundSettingsSection;
 
 interface OwnProps {
   currency: CurrencyEnum;
   managerMaxExitFee: number;
   managerMaxEntryFee: number;
-  wallets: WalletData[];
-  fetchWallets: (currency: CurrencyEnum) => void;
-  fetchRate(from: string, to: string): Promise<number>;
-  onSubmit(
+  onSubmit: (
     data: ICreateFundSettingsFormValues,
     setSubmitting: SetSubmittingType
-  ): void;
-  navigateBack(): void;
+  ) => void;
+  navigateBack: () => void;
   author: string;
-  assets: PlatformAsset[];
   minimumDepositAmount: number;
-  notifyError(message: string): void;
-}
-interface StateProps {
-  rate?: number;
-  wallet: WalletData;
 }
