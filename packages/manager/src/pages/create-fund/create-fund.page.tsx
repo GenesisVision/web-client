e7b1c31e51e3eb0bb61a9be24e5ceead13847798
pavelzubov <pasha224@yandex.ru>
2019-10-02 13:53:25 +0300

    Change translation HOC to hook

--- packages/manager/src/pages/create-fund/create-fund.page.tsx
+++ packages/manager/src/pages/create-fund/create-fund.page.tsx
@@ -1,20 +1,23 @@
 import "./create-fund.page.scss";
 
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import Page from "shared/components/page/page";
 
 import CreateFundContainer from "./components/create-fund.container";
 
-const _CreateFundPage: React.FC<WithTranslation> = ({ t }) => (
-  <Page title={t("manager.create-fund-page.title")}>
-    <div className="create-fund-page">
-      <h1>{t("manager.create-fund-page.title")}</h1>
-      <CreateFundContainer />
-    </div>
-  </Page>
-);
+const _CreateFundPage: React.FC = () => {
+  const [t] = useTranslation();
+  return (
+    <Page title={t("manager.create-fund-page.title")}>
+      <div className="create-fund-page">
+        <h1>{t("manager.create-fund-page.title")}</h1>
+        <CreateFundContainer />
+      </div>
+    </Page>
+  );
+};
 
-const CreateFundPage = translate()(_CreateFundPage);
+const CreateFundPage = React.memo(_CreateFundPage);
 
 export default CreateFundPage;
