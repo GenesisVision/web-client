bfb13a02807595660d7e0355cf357413047c939a
pavelzubov <pasha224@yandex.ru>
2019-10-02 14:38:48 +0300

    Use CreateAssetField

--- packages/manager/src/pages/create-fund/components/create-fund-settings/assets-field.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund-settings/assets-field.tsx
@@ -1,3 +1,4 @@
+import CreateAssetField from "components/create-asset/create-asset-field/create-asset-field";
 import ReallocateField from "pages/funds/fund-settings/reallocation/components/reallocate-field";
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -9,12 +10,12 @@ const _AssetsField: React.FC<{ name: string }> = ({ name }) => {
   const [t] = useTranslation();
   const assets = useSelector(fundAssetsSelector);
   return (
-    <>
+    <CreateAssetField wide>
       <div className="create-asset-settings__text">
         {t("manager.create-fund-page.settings.fields.mandatory-assets")}
       </div>
       <GVFormikField name={name} component={ReallocateField} assets={assets} />
-    </>
+    </CreateAssetField>
   );
 };
 export const AssetsField = React.memo(_AssetsField);
