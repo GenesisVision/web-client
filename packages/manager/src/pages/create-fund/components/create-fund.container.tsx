49b1787c9798cb43c8f1525a1d9bbd39ca103962
pavelzubov <pasha224@yandex.ru>
2019-10-02 13:51:30 +0300

    Create rate service

--- packages/manager/src/pages/create-fund/components/create-fund.container.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund.container.tsx
@@ -3,19 +3,12 @@ import React, { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import ConfirmPopup from "shared/components/confirm-popup/confirm-popup";
-import { walletsSelector } from "shared/components/wallet/reducers/wallet.reducers";
-import { fetchWallets } from "shared/components/wallet/services/wallet.services";
 import useApiRequest from "shared/hooks/api-request.hook";
 import useIsOpen from "shared/hooks/is-open.hook";
-import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 import { currencySelector } from "shared/reducers/account-settings-reducer";
 import { nameSelector } from "shared/reducers/header-reducer";
-import {
-  fundAssetsSelector,
-  platformDataSelector
-} from "shared/reducers/platform-reducer";
-import { rateApi } from "shared/services/api-client/rate-api";
-import { CurrencyEnum, SetSubmittingType } from "shared/utils/types";
+import { platformDataSelector } from "shared/reducers/platform-reducer";
+import { SetSubmittingType } from "shared/utils/types";
 
 import {
   createFund,
@@ -27,10 +20,8 @@ import CreateFundSettingsSection from "./create-fund-settings/create-fund-settin
 const _CreateFundContainer: React.FC = () => {
   const dispatch = useDispatch();
   const [t] = useTranslation();
-  const wallets = useSelector(walletsSelector);
   const author = useSelector(nameSelector);
   const platformSettings = useSelector(platformDataSelector);
-  const fundAssets = useSelector(fundAssetsSelector);
   const currency = useSelector(currencySelector);
   const { data: minimumDepositAmount, sendRequest } = useApiRequest({
     request: fetchMinimumDepositAmount
@@ -43,37 +34,26 @@ const _CreateFundContainer: React.FC = () => {
   useEffect(() => {
     sendRequest();
   }, []);
+
   const handleSubmit = useCallback(
     (
       values: ICreateFundSettingsFormValues,
       setSubmitting: SetSubmittingType
     ) => {
       dispatch(createFund(values, setSubmitting));
-      // dispatch(fetchWallets(currency));
     },
     []
   );
-  const fetchRate = useCallback(
-    (fromCurrency: string, toCurrency: string) =>
-      rateApi.v10RateByFromByToGet(fromCurrency, toCurrency),
-    []
-  );
+
   return (
     <div className="create-fund-container">
       <div>
         <CreateFundSettingsSection
           currency={currency}
-          condition={
-            !!platformSettings && !!wallets.length && !!minimumDepositAmount
-          }
-          fetchWallets={(currency: CurrencyEnum) =>
-            dispatch(fetchWallets(currency))
-          }
-          wallets={wallets}
+          condition={!!platformSettings && !!minimumDepositAmount}
           navigateBack={setIsNavigationDialogVisible}
           onSubmit={handleSubmit}
           author={author}
-          assets={fundAssets}
           minimumDepositAmount={minimumDepositAmount}
           managerMaxExitFee={
             (platformSettings &&
@@ -85,10 +65,6 @@ const _CreateFundContainer: React.FC = () => {
               platformSettings!.programsInfo.managerMaxEntryFee) ||
             0
           }
-          notifyError={(message: string) =>
-            dispatch(alertMessageActions.error(message))
-          }
-          fetchRate={fetchRate}
         />
         <ConfirmPopup
           open={isNavigationDialogVisible}
