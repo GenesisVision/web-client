bcc9c0a4967eecbb8bd7a2fe1fa9cb7d3b571b14
pavelzubov <pasha224@yandex.ru>
2019-10-03 22:32:23 +0300

    Add getBrokerLoaderData, CreateProgramBrokerLoaderData

--- packages/manager/src/pages/create-program/services/create-program.service.ts
+++ packages/manager/src/pages/create-program/services/create-program.service.ts
@@ -1,3 +1,4 @@
+import faker from "faker";
 import { Broker, CancelablePromise } from "gv-api-web";
 import brokersApi from "shared/services/api-client/brokers-api";
 
@@ -11,3 +12,33 @@ export const fetchBrokers = (): CancelablePromise<Broker[]> =>
         (a, b) => +(b.name === GM_BROKER_NAME) - +(a.name === GM_BROKER_NAME)
       )
     );
+
+export const getBrokerLoaderData: () => Broker = () => ({
+  name: faker.lorem.word(),
+  description: faker.lorem.words(11),
+  logo: "",
+  terms: faker.lorem.word(),
+  assets: faker.lorem.word(),
+  fee: faker.random.number(),
+  leverageMin: faker.random.number(),
+  leverageMax: faker.random.number(),
+  accountTypes: [
+    {
+      id: faker.lorem.word(),
+      name: faker.lorem.word(),
+      description: faker.lorem.words(11),
+      type: "MetaTrader4",
+      leverages: [10],
+      currencies: ["GVT"],
+      minimumDepositsAmount: {},
+      isForex: false,
+      isSignalsAvailable: false
+    }
+  ],
+  isForex: false,
+  isSignalsAvailable: false,
+  tags: []
+});
+export const CreateProgramBrokerLoaderData: Broker[] = new Array(7)
+  .fill("")
+  .map(getBrokerLoaderData);
