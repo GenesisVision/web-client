024c7c6cc5a86e8e409268fe37adb0cae4d494a4
pavelzubov <pasha224@yandex.ru>
2019-10-03 19:30:57 +0300

    Change managerMaxExitFee, managerMaxEntryFee to platformSettings

--- packages/manager/src/pages/create-fund/components/create-fund-settings/create-fund-settings-section.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund-settings/create-fund-settings-section.tsx
@@ -3,40 +3,27 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { ASSET } from "shared/constants/constants";
 import useApiRequest from "shared/hooks/api-request.hook";
-import { nameSelector } from "shared/reducers/header-reducer";
 import { platformDataSelector } from "shared/reducers/platform-reducer";
 
 import { fetchMinimumDepositAmount } from "../../services/create-fund.service";
 import CreateFundSettings from "./create-fund-settings";
 
 const _CreateFundSettingsSection: React.FC = () => {
-  const author = useSelector(nameSelector);
-
   const { data: minimumDepositAmount, sendRequest } = useApiRequest({
     request: fetchMinimumDepositAmount
   });
   useEffect(() => {
     sendRequest();
   }, []);
-
   const platformSettings = useSelector(platformDataSelector);
-  const managerMaxExitFee =
-    (platformSettings && platformSettings!.programsInfo.managerMaxExitFee) || 0;
-
-  const managerMaxEntryFee =
-    (platformSettings && platformSettings!.programsInfo.managerMaxEntryFee) ||
-    0;
-
   const handleCreate = useCreateAssetSubmit({ asset: ASSET.FUND });
 
   return (
     <CreateFundSettings
-      condition={!!minimumDepositAmount}
+      condition={!!minimumDepositAmount && !!platformSettings}
       onSubmit={handleCreate}
-      author={author}
       minimumDepositAmount={minimumDepositAmount}
-      managerMaxExitFee={managerMaxExitFee}
-      managerMaxEntryFee={managerMaxEntryFee}
+      platformSettings={platformSettings!}
     />
   );
 };
