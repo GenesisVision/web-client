14094243a4c67c63d15dd76fefe2e45014a4c83e
pavelzubov <pasha224@yandex.ru>
2019-10-03 12:31:27 +0300

    Remove unused createProgram

--- packages/manager/src/pages/create-program/services/create-program.service.ts
+++ packages/manager/src/pages/create-program/services/create-program.service.ts
@@ -1,15 +1,5 @@
-import {
-  Broker,
-  CancelablePromise,
-  ManagerProgramCreateResult,
-  NewProgramRequest
-} from "gv-api-web";
+import { Broker, CancelablePromise } from "gv-api-web";
 import brokersApi from "shared/services/api-client/brokers-api";
-import managerApi from "shared/services/api-client/manager-api";
-import authService from "shared/services/auth-service";
-import filesService from "shared/services/file-service";
-
-import { ICreateProgramSettingsFormValues } from "../components/create-program-settings/create-program-settings";
 
 const GM_BROKER_NAME = "Genesis Markets";
 
@@ -21,27 +11,3 @@ export const fetchBrokers = (): CancelablePromise<Broker[]> =>
         (a, b) => +(b.name === GM_BROKER_NAME) - +(a.name === GM_BROKER_NAME)
       )
     );
-
-export const createProgram = (
-  createProgramData: ICreateProgramSettingsFormValues
-): CancelablePromise<ManagerProgramCreateResult> => {
-  const authorization = authService.getAuthArg();
-
-  let promise = Promise.resolve("") as CancelablePromise<any>;
-  if (createProgramData.logo.image) {
-    promise = filesService.uploadFile(
-      createProgramData.logo.image.cropped,
-      authorization
-    ) as CancelablePromise<any>;
-  }
-  return promise.then(response => {
-    const requestData = <NewProgramRequest>{
-      ...createProgramData,
-      logo: response
-    };
-
-    return managerApi.v10ManagerProgramsCreatePost(authorization, {
-      request: requestData
-    });
-  });
-};
