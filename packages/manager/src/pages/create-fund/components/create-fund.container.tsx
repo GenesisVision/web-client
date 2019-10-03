1f8547b2c1733e135280c5f180f6f412bda7f723
pavelzubov <pasha224@yandex.ru>
2019-10-03 12:30:17 +0300

    Move data getting, run create method and hooks into sub components

--- packages/manager/src/pages/create-fund/components/create-fund.container.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund.container.tsx
@@ -1,67 +1,21 @@
+import { CreateAssetBack } from "components/create-asset/create-asset-back";
 import { goBack } from "connected-react-router";
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch, useSelector } from "react-redux";
-import ConfirmPopup from "shared/components/confirm-popup/confirm-popup";
-import useApiRequest from "shared/hooks/api-request.hook";
-import useIsOpen from "shared/hooks/is-open.hook";
-import { currencySelector } from "shared/reducers/account-settings-reducer";
-import { nameSelector } from "shared/reducers/header-reducer";
-import { SetSubmittingType } from "shared/utils/types";
+import { useDispatch } from "react-redux";
+import { ASSET } from "shared/constants/constants";
 
-import {
-  createFund,
-  fetchMinimumDepositAmount
-} from "../services/create-fund.service";
-import { ICreateFundSettingsFormValues } from "./create-fund-settings/create-fund-settings";
 import CreateFundSettingsSection from "./create-fund-settings/create-fund-settings-section";
 
 const _CreateFundContainer: React.FC = () => {
   const dispatch = useDispatch();
   const [t] = useTranslation();
 
-  const author = useSelector(nameSelector);
-  const currency = useSelector(currencySelector);
-
-  const { data: minimumDepositAmount, sendRequest } = useApiRequest({
-    request: fetchMinimumDepositAmount
-  });
-  const [
-    isNavigationDialogVisible,
-    setIsNavigationDialogVisible,
-    setNotIsNavigationDialogVisible
-  ] = useIsOpen();
-  useEffect(() => {
-    sendRequest();
-  }, []);
-
-  const handleSubmit = useCallback(
-    (
-      values: ICreateFundSettingsFormValues,
-      setSubmitting: SetSubmittingType
-    ) => {
-      dispatch(createFund(values, setSubmitting));
-    },
-    []
-  );
-
   return (
     <>
-      <CreateFundSettingsSection
-        currency={currency}
-        condition={!!minimumDepositAmount}
-        navigateBack={setIsNavigationDialogVisible}
-        onSubmit={handleSubmit}
-        author={author}
-        minimumDepositAmount={minimumDepositAmount}
-      />
-      <ConfirmPopup
-        open={isNavigationDialogVisible}
-        onClose={setNotIsNavigationDialogVisible}
-        onApply={() => dispatch(goBack)}
-        body={t("manager.create-fund-page.navigation-back-text")}
-        applyButtonText={t("buttons.continue")}
-      />
+      <CreateAssetBack asset={ASSET.PROGRAM} onApply={() => dispatch(goBack)} />
+      <h1>{t("manager.create-fund-page.title")}</h1>
+      <CreateFundSettingsSection />
     </>
   );
 };
