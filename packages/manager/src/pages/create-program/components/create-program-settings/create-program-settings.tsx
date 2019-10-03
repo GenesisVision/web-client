206dc6f2d1ac0e98e46c7ca938dcb946ee52e566
pavelzubov <pasha224@yandex.ru>
2019-10-03 19:33:25 +0300

    Remove effect with wallet change Remove div

--- packages/manager/src/pages/create-program/components/create-program-settings/create-program-settings.tsx
+++ packages/manager/src/pages/create-program/components/create-program-settings/create-program-settings.tsx
@@ -3,25 +3,19 @@ import "shared/components/deposit-details/deposit-details.scss";
 import "./create-program-settings.scss";
 
 import CreateAssetSection from "components/create-asset/create-asset-section/create-asset-section";
+import useCreateAssetValidate from "components/create-asset/create-asset-validate.hook";
 import DescriptionBlock from "components/create-asset/fields/description-block";
 import FeesSettings from "components/create-asset/fields/fees-settings";
 import InvestmentLimitField from "components/create-asset/fields/investment-limit-field";
 import StopOutField from "components/create-asset/fields/stop-out-field";
 import { InjectedFormikProps, withFormik } from "formik";
-import {
-  Broker,
-  NewProgramRequestTradesDelayEnum,
-  ProgramsInfo
-} from "gv-api-web";
+import { Broker, NewProgramRequestTradesDelayEnum, ProgramsInfo } from "gv-api-web";
 import * as React from "react";
-import { useEffect } from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { useDispatch } from "react-redux";
 import { compose } from "redux";
 import { IImageValue } from "shared/components/form/input-image/input-image";
 import { ASSET } from "shared/constants/constants";
 import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
-import { alertMessageActions } from "shared/modules/alert-message/actions/alert-message-actions";
 import { CurrencyEnum, SetSubmittingType } from "shared/utils/types";
 
 import createProgramSettingsValidationSchema, {
@@ -58,131 +52,108 @@ const _CreateProgramSettings: React.FC<Props> = ({
     description
   }
 }) => {
-  const dispatch = useDispatch();
   const accountType = broker.accountTypes.find(
     ({ id }) => brokerAccountTypeId === id
   )!;
-  useEffect(
-    () => {
-      setFieldValue(CREATE_PROGRAM_FIELDS.depositAmount, "");
-    },
-    [depositWalletId]
-  );
-
-  const validateAndSubmit = (
-    e?: React.FormEvent<HTMLFormElement> | undefined
-  ): void => {
-    handleSubmit(e);
-    if (isValid) handleSubmit(e);
-    else
-      dispatch(
-        alertMessageActions.error(
-          t("manager.create-program-page.notifications.validate-error")
-        )
-      );
-    if (e) e.preventDefault();
-  };
-
+  const validateAndSubmit = useCreateAssetValidate({ handleSubmit, isValid });
   return (
-    <div className="create-program-settings">
-      <form onSubmit={validateAndSubmit}>
-        <CreateAssetSection
-          title={t("manager.create-program-page.settings.main-settings")}
-          blockNumber={"01"}
-        >
-          <DescriptionBlock
-            asset={ASSET.PROGRAM}
-            titleName={CREATE_PROGRAM_FIELDS.title}
-            descriptionName={CREATE_PROGRAM_FIELDS.description}
-            logoName={CREATE_PROGRAM_FIELDS.logo}
-            description={description}
-          />
-          <BrokerAccount
-            setAccountType={(value: string) =>
-              setFieldValue(CREATE_PROGRAM_FIELDS.brokerAccountTypeId, value)
-            }
-            setLeverage={(value: number) =>
-              setFieldValue(CREATE_PROGRAM_FIELDS.leverage, value)
-            }
-            setCurrency={(value: string) =>
-              setFieldValue(CREATE_PROGRAM_FIELDS.currency, value)
-            }
-            name={CREATE_PROGRAM_FIELDS.brokerAccountTypeId}
-            accountTypes={broker.accountTypes}
-          />
-          <Currency
-            name={CREATE_PROGRAM_FIELDS.currency}
-            disabled={accountType === undefined}
-            accountCurrencies={accountType.currencies as CurrencyEnum[]}
-          />
-          <Leverage
-            name={CREATE_PROGRAM_FIELDS.leverage}
-            disabled={!accountType}
-            accountLeverages={accountType.leverages}
-          />
-          <PeriodLength
-            programsInfo={programsInfo}
-            name={CREATE_PROGRAM_FIELDS.periodLength}
-          />
-          <StopOutField name={CREATE_PROGRAM_FIELDS.stopOutLevel} />
-          <TradesDelay name={CREATE_PROGRAM_FIELDS.tradesDelay} />
-          <InvestmentLimitField
-            checkboxName={CREATE_PROGRAM_FIELDS.hasInvestmentLimit}
-            inputName={CREATE_PROGRAM_FIELDS.investmentLimit}
-            hasInvestmentLimit={hasInvestmentLimit}
-            currency={currency as CurrencyEnum}
-          />
-          <SignalProgram
-            condition={broker.isSignalsAvailable}
-            name={CREATE_PROGRAM_FIELDS.isSignalProgram}
-          />
-        </CreateAssetSection>
-        <CreateAssetSection
-          title={t("manager.create-program-page.settings.fees-settings")}
-          blockNumber={"02"}
-        >
-          <FeesSettings
-            title={t(
-              "manager.create-program-page.settings.investment-program-fees"
-            )}
-            entryFeeName={CREATE_PROGRAM_FIELDS.entryFee}
-            entryFeeDescription={t(
-              "manager.create-program-page.settings.hints.entry-fee-description"
-            )}
-            secondFeeName={CREATE_PROGRAM_FIELDS.successFee}
-            secondFeeLabel={t(
-              "manager.create-program-page.settings.fields.success-fee"
-            )}
-            secondFeeUnderText={t(
-              "manager.create-program-page.settings.hints.success-fee"
-            )}
-            secondFeeDescription={t(
-              "manager.create-program-page.settings.hints.success-fee-description"
-            )}
-          />
-          {isSignalProgram && (
-            <SignalsFeeFormPartial
-              volumeFeeFieldName={CREATE_PROGRAM_FIELDS.signalVolumeFee}
-              successFeeFieldName={CREATE_PROGRAM_FIELDS.signalSuccessFee}
-            />
-          )}
-        </CreateAssetSection>
-        <DepositDetailsBlock
-          availableName={CREATE_PROGRAM_FIELDS.available}
-          rateName={CREATE_PROGRAM_FIELDS.rate}
-          walletFieldName={CREATE_PROGRAM_FIELDS.depositWalletId}
-          inputName={CREATE_PROGRAM_FIELDS.depositAmount}
-          depositAmount={depositAmount}
-          minimumDepositAmount={minimumDepositsAmount[currency]}
-          setFieldValue={setFieldValue}
-          assetCurrency={currency as CurrencyEnum}
-        />
-        <CreateAssetNavigation
+    <form onSubmit={validateAndSubmit}>
+      <CreateAssetSection
+        title={t("manager.create-program-page.settings.main-settings")}
+        blockNumber={"01"}
+      >
+        <DescriptionBlock
           asset={ASSET.PROGRAM}
-          isSubmitting={isSubmitting}
+          titleName={CREATE_PROGRAM_FIELDS.title}
+          descriptionName={CREATE_PROGRAM_FIELDS.description}
+          logoName={CREATE_PROGRAM_FIELDS.logo}
+          description={description}
         />
-      </form>
-    </div>
+        <BrokerAccount
+          setAccountType={(value: string) =>
+            setFieldValue(CREATE_PROGRAM_FIELDS.brokerAccountTypeId, value)
+          }
+          setLeverage={(value: number) =>
+            setFieldValue(CREATE_PROGRAM_FIELDS.leverage, value)
+          }
+          setCurrency={(value: string) =>
+            setFieldValue(CREATE_PROGRAM_FIELDS.currency, value)
+          }
+          name={CREATE_PROGRAM_FIELDS.brokerAccountTypeId}
+          accountTypes={broker.accountTypes}
+        />
+        <Currency
+          name={CREATE_PROGRAM_FIELDS.currency}
+          disabled={accountType === undefined}
+          accountCurrencies={accountType.currencies as CurrencyEnum[]}
+        />
+        <Leverage
+          name={CREATE_PROGRAM_FIELDS.leverage}
+          disabled={!accountType}
+          accountLeverages={accountType.leverages}
+        />
+        <PeriodLength
+          programsInfo={programsInfo}
+          name={CREATE_PROGRAM_FIELDS.periodLength}
+        />
+        <StopOutField name={CREATE_PROGRAM_FIELDS.stopOutLevel} />
+        <TradesDelay name={CREATE_PROGRAM_FIELDS.tradesDelay} />
+        <InvestmentLimitField
+          checkboxName={CREATE_PROGRAM_FIELDS.hasInvestmentLimit}
+          inputName={CREATE_PROGRAM_FIELDS.investmentLimit}
+          hasInvestmentLimit={hasInvestmentLimit}
+          currency={currency as CurrencyEnum}
+        />
+        <SignalProgram
+          condition={broker.isSignalsAvailable}
+          name={CREATE_PROGRAM_FIELDS.isSignalProgram}
+        />
+      </CreateAssetSection>
+      <CreateAssetSection
+        title={t("manager.create-program-page.settings.fees-settings")}
+        blockNumber={"02"}
+      >
+        <FeesSettings
+          title={t(
+            "manager.create-program-page.settings.investment-program-fees"
+          )}
+          entryFeeName={CREATE_PROGRAM_FIELDS.entryFee}
+          entryFeeDescription={t(
+            "manager.create-program-page.settings.hints.entry-fee-description"
+          )}
+          secondFeeName={CREATE_PROGRAM_FIELDS.successFee}
+          secondFeeLabel={t(
+            "manager.create-program-page.settings.fields.success-fee"
+          )}
+          secondFeeUnderText={t(
+            "manager.create-program-page.settings.hints.success-fee"
+          )}
+          secondFeeDescription={t(
+            "manager.create-program-page.settings.hints.success-fee-description"
+          )}
+        />
+        {isSignalProgram && (
+          <SignalsFeeFormPartial
+            volumeFeeFieldName={CREATE_PROGRAM_FIELDS.signalVolumeFee}
+            successFeeFieldName={CREATE_PROGRAM_FIELDS.signalSuccessFee}
+          />
+        )}
+      </CreateAssetSection>
+      <DepositDetailsBlock
+        availableName={CREATE_PROGRAM_FIELDS.available}
+        rateName={CREATE_PROGRAM_FIELDS.rate}
+        walletFieldName={CREATE_PROGRAM_FIELDS.depositWalletId}
+        inputName={CREATE_PROGRAM_FIELDS.depositAmount}
+        depositAmount={depositAmount}
+        minimumDepositAmount={minimumDepositsAmount[currency]}
+        setFieldValue={setFieldValue}
+        assetCurrency={currency as CurrencyEnum}
+      />
+      <CreateAssetNavigation
+        asset={ASSET.PROGRAM}
+        isSubmitting={isSubmitting}
+      />
+    </form>
   );
 };
 
@@ -194,7 +165,6 @@ interface OwnProps {
     setSubmitting: SetSubmittingType
   ) => void;
   minimumDepositsAmount: { [key: string]: number };
-  author: string;
 }
 
 export interface ICreateProgramSettingsProps
