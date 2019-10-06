f6380c644f6842b37d084d986ff32de201ed9ebc
pavelzubov <pasha224@yandex.ru>
2019-10-06 19:43:48 +0300

    Use DialogTop

--- packages/shared/modules/2fa/disable-auth/disable-auth-form.tsx
+++ packages/shared/modules/2fa/disable-auth/disable-auth-form.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
 import NumberFormat from "react-number-format";
 import { compose } from "redux";
+import { DialogTop } from "shared/components/dialog/dialog-top";
+import FormError from "shared/components/form/form-error/form-error";
 import GVButton from "shared/components/gv-button";
 import GVFormikField from "shared/components/gv-formik-field";
 import GVTextField from "shared/components/gv-text-field";
@@ -12,41 +14,38 @@ import { number, object, string } from "yup";
 const DisableAuth: React.FC<
   InjectedFormikProps<Props, IDisableAuthFormFormValues>
 > = ({ t, handleSubmit, errorMessage, isSubmitting }) => (
-  <form
-    id="disable-auth"
-    onSubmit={handleSubmit}
-    className="dialog__top"
-    autoComplete="off"
-  >
-    <div className="dialog__title">{t("2fa-page.disable.title")}</div>
-    <GVFormikField
-      name={FIELDS.twoFactorCode}
-      type="tel"
-      label={t("2fa-page.google-code")}
-      component={GVTextField}
-      autoComplete="off"
-      InputComponent={NumberFormat}
-      allowNegative={false}
-      format="######"
-    />
-    <GVFormikField
-      name={FIELDS.password}
-      type="password"
-      label={t("2fa-page.password")}
-      component={GVTextField}
-      autoComplete="new-password"
-    />
-    <div className="form-error">{errorMessage}</div>
-    <div className="dialog__buttons">
-      <GVButton
-        className="google-auth__button"
-        variant="contained"
-        color="primary"
-        type="submit"
-        disabled={isSubmitting}
-      >
-        {t("buttons.disable")}
-      </GVButton>
+  <form id="disable-auth" onSubmit={handleSubmit} autoComplete="off">
+    <DialogTop title={t("2fa-page.disable.title")} />
+    <div className="dialog__bottom">
+      <GVFormikField
+        name={FIELDS.twoFactorCode}
+        type="tel"
+        label={t("2fa-page.google-code")}
+        component={GVTextField}
+        autoComplete="off"
+        InputComponent={NumberFormat}
+        allowNegative={false}
+        format="######"
+      />
+      <GVFormikField
+        name={FIELDS.password}
+        type="password"
+        label={t("2fa-page.password")}
+        component={GVTextField}
+        autoComplete="new-password"
+      />
+      <FormError error={errorMessage} />
+      <div className="dialog__buttons">
+        <GVButton
+          className="google-auth__button"
+          variant="contained"
+          color="primary"
+          type="submit"
+          disabled={isSubmitting}
+        >
+          {t("buttons.disable")}
+        </GVButton>
+      </div>
     </div>
   </form>
 );
