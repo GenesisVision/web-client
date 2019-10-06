a993cdb0e651c46d2619f33dbd5ecd382de44106
pavelzubov <pasha224@yandex.ru>
2019-10-06 19:42:17 +0300

    Create DialogTop component

---
+++ packages/shared/components/dialog/dialog-top.tsx
@@ -0,0 +1,22 @@
+import "./dialog.scss";
+
+import * as React from "react";
+
+export const _DialogTop: React.FC<
+  React.HTMLAttributes<HTMLDivElement> & Props
+> = ({ title, subtitle, children }) => (
+  <div className="dialog__top">
+    <div className="dialog__header">
+      {title && <h2>{title}</h2>}
+      {subtitle && <p>{subtitle}</p>}
+    </div>
+    {children && <div className="dialog-field">{children}</div>}
+  </div>
+);
+
+interface Props {
+  title?: string;
+  subtitle?: string;
+}
+
+export const DialogTop = React.memo(_DialogTop);
