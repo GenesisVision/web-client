c66f7e549139fca87fe31bdb68fd4a8d5dfd14b9
pavelzubov <pasha224@yandex.ru>
2019-10-06 19:42:57 +0300

    Use DialogTop

--- packages/manager/src/modules/confirm/components/confirm-form.tsx
+++ packages/manager/src/modules/confirm/components/confirm-form.tsx
@@ -2,6 +2,7 @@ import { InjectedFormikProps, withFormik } from "formik";
 import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 import { compose } from "redux";
+import { DialogTop } from "shared/components/dialog/dialog-top";
 import GVButton from "shared/components/gv-button";
 import GVFormikField from "shared/components/gv-formik-field";
 import GVTextField from "shared/components/gv-text-field";
@@ -13,22 +14,17 @@ const _ConfirmForm: React.FC<
   InjectedFormikProps<Props, IConfirmFormValues>
 > = ({ t, handleSubmit, serverError, isSubmitting }) => (
   <form id="confirm-form" onSubmit={handleSubmit}>
-    <div className="dialog__top">
-      <div className="dialog__header">
-        <h2>Confirm</h2>
-        <p>Confirm</p>
-      </div>
-    </div>
-    <GVFormikField
-      disabled={isSubmitting}
-      type="text"
-      name={FIELDS.code}
-      label={t("auth.login.two-factor.input-label")}
-      autoComplete="off"
-      autoFocus
-      component={GVTextField}
-    />
+    <DialogTop title={"Confirm"} />
     <div className="dialog__bottom">
+      <GVFormikField
+        disabled={isSubmitting}
+        type="text"
+        name={FIELDS.code}
+        label={t("auth.login.two-factor.input-label")}
+        autoComplete="off"
+        autoFocus
+        component={GVTextField}
+      />
       <div className="form-error">{serverError}</div>
       <div className="dialog__buttons">
         <GVButton type="submit" id="signUpFormSubmit" disabled={isSubmitting}>
