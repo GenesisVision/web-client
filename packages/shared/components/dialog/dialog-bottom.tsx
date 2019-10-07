889266baff108793ca819adaa033a49cdc2b19b9
pavelzubov <pasha224@yandex.ru>
2019-10-07 11:53:28 +0300

    Add className prop

--- packages/shared/components/dialog/dialog-bottom.tsx
+++ packages/shared/components/dialog/dialog-bottom.tsx
@@ -1,9 +1,11 @@
 import "./dialog.scss";
 
+import classNames from "classnames";
 import * as React from "react";
 
 export const _DialogBottom: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+  className,
   children
-}) => <div className="dialog__bottom">{children}</div>;
+}) => <div className={classNames("dialog__bottom", className)}>{children}</div>;
 
 export const DialogBottom = React.memo(_DialogBottom);
