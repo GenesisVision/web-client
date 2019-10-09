d28fd5a8440c1df4c1bbc827910a33b24adb27b2
pavelzubov <pasha224@yandex.ru>
2019-10-09 17:32:50 +0300

    Remove role from redux

--- packages/shared/hooks/use-role.hook.tsx
+++ packages/shared/hooks/use-role.hook.tsx
@@ -1,10 +1,7 @@
-import { useSelector } from "react-redux";
 import { ROLE, ROLE_ENV } from "shared/constants/constants";
-import { roleSelector } from "shared/reducers/header-reducer";
 
 const useRole = () => {
-  const role = useSelector(roleSelector);
-  return (role || ROLE_ENV || ROLE.INVESTOR).toLowerCase() as ROLE;
+  return (ROLE_ENV || ROLE.INVESTOR).toLowerCase() as ROLE;
 };
 
 export default useRole;
