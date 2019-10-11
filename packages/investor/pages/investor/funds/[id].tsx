a592590ab5868f0392f65ab53622b53b5c2a0508
pavelzubov <pasha224@yandex.ru>
2019-10-11 23:18:57 +0300

    Set account currency from cookie to statistic currency

--- packages/investor/pages/investor/funds/[id].tsx
+++ packages/investor/pages/investor/funds/[id].tsx
@@ -1,19 +1,15 @@
 import FundDetailsPage from "pages/funds/fund-details/fund-details.page";
 import React from "react";
-import { connect, ResolveThunks } from "react-redux";
-import {
-  ActionCreatorsMapObject,
-  bindActionCreators,
-  compose,
-  Dispatch
-} from "redux";
+import { compose } from "redux";
 import {
   dispatchFundDescription,
   dispatchFundId
 } from "shared/components/funds/fund-details/services/fund-details.service";
 import withDefaultLayout from "shared/decorators/with-default-layout";
-import withPrivateRoute from "shared/decorators/with-private-route";
-import { NextPageWithRedux } from "shared/utils/types";
+import { CurrencyEnum, NextPageWithRedux } from "shared/utils/types";
+import { statisticCurrencyAction } from "shared/components/funds/fund-details/actions/fund-details.actions";
+import { getCookie } from "shared/utils/cookie";
+import { ACCOUNT_CURRENCY_KEY } from "shared/middlewares/update-account-settings-middleware/update-account-settings-middleware";
 
 const FundDetails: NextPageWithRedux<{}, {}> = () => {
   return <FundDetailsPage />;
@@ -22,6 +18,9 @@ const FundDetails: NextPageWithRedux<{}, {}> = () => {
 FundDetails.getInitialProps = async ctx => {
   const { id } = ctx.query;
   await Promise.all([
+    ctx.reduxStore.dispatch(dispatch =>
+      dispatch(statisticCurrencyAction(getCookie(ACCOUNT_CURRENCY_KEY, ctx)! as CurrencyEnum))
+    ),
     ctx.reduxStore.dispatch(dispatchFundId(id as string)),
     ctx.reduxStore.dispatch(dispatchFundDescription(ctx))
   ]);
