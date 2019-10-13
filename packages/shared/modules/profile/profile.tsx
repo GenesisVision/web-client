b5625a5796db4ba91de2fd40b29077ebeef45841
pavelzubov <pasha224@yandex.ru>
2019-10-13 10:48:21 +0300

    Remove bracket Remove compose

--- packages/shared/modules/profile/profile.tsx
+++ packages/shared/modules/profile/profile.tsx
@@ -7,13 +7,12 @@ import copy from "copy-to-clipboard";
 import { ProfileFullViewModel } from "gv-api-web";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { compose } from "redux";
 import GVButton from "shared/components/gv-button";
 import ProfileImageContainer from "shared/components/profile/settings/profile-image/profile-image-container";
 import SettingsBlock from "shared/components/settings-block/settings-block";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
 import { ROLE } from "shared/constants/constants";
-import withLoader, { WithLoaderProps } from "shared/decorators/with-loader";
+import withLoader from "shared/decorators/with-loader";
 import useRole from "shared/hooks/use-role.hook";
 import PublicInfo from "shared/modules/public-info/public-info";
 
@@ -40,7 +39,6 @@ const _Profile: React.FC<Props> = ({ info, notifySuccess, onSuccessEdit }) => {
               <div>{info.id}</div>
               <GVButton onClick={onCopy}>{t("buttons.copy")}</GVButton>
             </div>
-            }
           </SettingsBlock>
         </>
       )}
@@ -68,10 +66,5 @@ export interface IProfileOwnProps {
   onSuccessEdit: () => void;
 }
 
-const Profile = compose<
-  React.ComponentType<IProfileOwnProps & WithLoaderProps>
->(
-  withLoader,
-  React.memo
-)(_Profile);
+const Profile = withLoader(React.memo(_Profile));
 export default Profile;
