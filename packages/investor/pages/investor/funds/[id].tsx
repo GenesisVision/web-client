7383808409bfe55478a430fb017fbe04150f04c7
pavelzubov <pasha224@yandex.ru>
2019-10-16 12:44:23 +0300

    Move getting account currency from cookie to initialState and get account currency from redux

--- packages/investor/pages/investor/funds/[id].tsx
+++ packages/investor/pages/investor/funds/[id].tsx
@@ -6,10 +6,8 @@ import {
   dispatchFundId
 } from "shared/components/funds/fund-details/services/fund-details.service";
 import withDefaultLayout from "shared/decorators/with-default-layout";
-import { CurrencyEnum, NextPageWithRedux } from "shared/utils/types";
+import { NextPageWithRedux } from "shared/utils/types";
 import { statisticCurrencyAction } from "shared/components/funds/fund-details/actions/fund-details.actions";
-import { getCookie } from "shared/utils/cookie";
-import { ACCOUNT_CURRENCY_KEY } from "shared/middlewares/update-account-settings-middleware/update-account-settings-middleware";
 
 const FundDetails: NextPageWithRedux<{}, {}> = () => {
   return <FundDetailsPage />;
@@ -17,9 +15,12 @@ const FundDetails: NextPageWithRedux<{}, {}> = () => {
 
 FundDetails.getInitialProps = async ctx => {
   const { id } = ctx.query;
+  const {
+    accountSettings: { currency }
+  } = ctx.reduxStore.getState();
   await Promise.all([
     ctx.reduxStore.dispatch(dispatch =>
-      dispatch(statisticCurrencyAction(getCookie(ACCOUNT_CURRENCY_KEY, ctx)! as CurrencyEnum))
+      dispatch(statisticCurrencyAction(currency))
     ),
     ctx.reduxStore.dispatch(dispatchFundId(id as string)),
     ctx.reduxStore.dispatch(dispatchFundDescription(ctx))
