ac0244cf62cdd24178aaa09cb45dfa22e8fcb01b
pavelzubov <pasha224@yandex.ru>
2019-10-17 19:51:46 +0300

    Remove back button

--- packages/manager/src/pages/create-fund/components/create-fund.container.tsx
+++ packages/manager/src/pages/create-fund/components/create-fund.container.tsx
@@ -1,19 +1,12 @@
-import { CreateAssetBack } from "components/create-asset/create-asset-back";
-import { goBack } from "connected-react-router";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
-import { ASSET } from "shared/constants/constants";
 
 import CreateFundSettingsSection from "./create-fund-settings/create-fund-settings-section";
 
 const _CreateFundContainer: React.FC = () => {
-  const dispatch = useDispatch();
   const [t] = useTranslation();
-
   return (
     <>
-      <CreateAssetBack asset={ASSET.PROGRAM} onApply={() => dispatch(goBack)} />
       <div className="create-asset__header">
         <h1>{t("manager.create-fund-page.title")}</h1>
       </div>
