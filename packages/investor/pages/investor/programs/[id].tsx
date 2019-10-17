a6989c12617d1fa965efe3b4b854376b5e955d30
pavelzubov <pasha224@yandex.ru>
2019-10-17 18:58:20 +0300

    Use programDetailsCreator

--- packages/investor/pages/investor/programs/[id].tsx
+++ packages/investor/pages/investor/programs/[id].tsx
@@ -1,33 +1,5 @@
-import { ProgramDetailsFullOld } from "gv-api-web";
 import ProgramDetailsPage from "pages/programs/program-details/program-details.page";
 import React from "react";
-import { compose } from "redux";
-import { statisticCurrencyAction } from "shared/components/programs/program-details/actions/program-details.actions";
-import {
-  dispatchProgramDescription,
-  dispatchProgramId
-} from "shared/components/programs/program-details/services/program-details.service";
-import withDefaultLayout from "shared/decorators/with-default-layout";
-import { NextPageWithRedux } from "shared/utils/types";
+import { programDetailsCreator } from "shared/routes/ssr/programs/[id]";
 
-const ProgramDetails: NextPageWithRedux<{}> = () => {
-  return <ProgramDetailsPage />;
-};
-
-ProgramDetails.getInitialProps = async ctx => {
-  const { id } = ctx.query;
-  await Promise.all([
-    ctx.reduxStore.dispatch(dispatchProgramId(id as string)),
-    ctx.reduxStore.dispatch(dispatchProgramDescription(ctx))
-  ]).then(([_, descriptionResult]) => {
-    const description = ((descriptionResult as unknown) as {
-      value: ProgramDetailsFullOld;
-    }).value;
-    ctx.reduxStore.dispatch(dispatch =>
-      dispatch(statisticCurrencyAction(description.currency))
-    );
-  });
-  return {};
-};
-
-export default compose(withDefaultLayout)(ProgramDetails);
+export default programDetailsCreator(ProgramDetailsPage);
