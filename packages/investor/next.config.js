0c96e85e0885bc43f6f83935eebddba8f9018728
pavelzubov <pasha224@yandex.ru>
2019-10-18 14:11:41 +0300

    Move next config create logic to shared

--- packages/investor/next.config.js
+++ packages/investor/next.config.js
@@ -1,47 +1,4 @@
-//@ts-ignore
-const tm = require("next-transpile-modules");
-const sass = require("@zeit/next-sass");
-const css = require("@zeit/next-css");
-const withPlugins = require("next-compose-plugins");
-const images = require("next-images");
-const workers = require("@zeit/next-workers");
+const nextConfigCreator = require("shared/next.config");
 const path = require("path");
-const dotenv = require("dotenv");
 
-const isProd = process.env.NODE_ENV === "production";
-
-if (isProd) {
-  dotenv.config({ path: ".env.production" });
-}
-dotenv.config({ path: ".env.local" });
-dotenv.config({ path: ".env" });
-
-const nextConfig = {
-  webpack(config, { dev, webpack, buildId, isServer }) {
-    config.resolve.modules.push(path.resolve("./src"));
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-    return config;
-  },
-  assetPrefix: isProd ? "/static-investor" : ""
-};
-
-module.exports = withPlugins(
-  [
-    sass,
-    css,
-    images,
-    [
-      workers,
-      {
-        workerLoaderOptions: { inline: true }
-      }
-    ],
-    [
-      tm,
-      {
-        transpileModules: ["shared", "gv-api-web"]
-      }
-    ]
-  ],
-  nextConfig
-);
+module.exports = nextConfigCreator.create("investor", path.resolve("./src"));
---
+++ packages/shared/next.config.js
@@ -0,0 +1,55 @@
+const dotenv = require("dotenv");
+
+const tm = require("next-transpile-modules");
+const sass = require("@zeit/next-sass");
+const css = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
+const images = require("next-images");
+const workers = require("@zeit/next-workers");
+
+const isProd = process.env.NODE_ENV === "production";
+
+function create(role, path) {
+  if (isProd) {
+    dotenv.config({ path: ".env.production" });
+  }
+  dotenv.config({ path: ".env.local" });
+  dotenv.config({ path: ".env" });
+
+  const nextConfig = {
+    webpack(config, { dev, webpack }) {
+      config.devtool = false;
+      for (const r of config.module.rules) {
+        if (r.loader === "babel-loader") {
+          r.options.sourceMaps = false;
+        }
+      }
+      config.resolve.modules.push(path);
+      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+      return config;
+    },
+    assetPrefix: isProd ? `/static-${role.toLowerCase()}` : ""
+  };
+  return withPlugins(
+    [
+      sass,
+      css,
+      images,
+      [
+        workers,
+        {
+          workerLoaderOptions: { inline: true }
+        }
+      ],
+      [
+        tm,
+        {
+          transpileModules: ["shared", "gv-api-web"]
+        }
+      ]
+    ],
+    nextConfig
+  );
+}
+
+module.exports.create = create;
