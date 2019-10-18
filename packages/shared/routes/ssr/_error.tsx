23acc30c95697dd7aa3f63b05b19f6b78cfc9823
pavelzubov <pasha224@yandex.ru>
2019-10-18 16:07:05 +0300

    Move getting statusMessage, errorMessage to getInitialProps

--- packages/shared/routes/ssr/_error.tsx
+++ packages/shared/routes/ssr/_error.tsx
@@ -4,14 +4,13 @@ import Page from "shared/components/page/page";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import { useTranslation } from "shared/i18n";
 
-const Component: NextPage<ErrorPage> = ({ err, res }) => {
+const Component: NextPage<IErrorPage> = ({
+  code,
+  statusMessage,
+  errorMessage
+}) => {
   const { t } = useTranslation();
-  const errorCode = err ? err.statusCode : undefined;
-  const statusCode = res ? res.statusCode : undefined;
-  const code = errorCode || statusCode;
   const notFoundMessage = code === 404 && t("not-found-page.body");
-  const errorMessage = err && err.message;
-  const statusMessage = res && res.statusMessage;
   const message =
     notFoundMessage || errorMessage || statusMessage || "error...";
   return (
@@ -21,15 +20,25 @@ const Component: NextPage<ErrorPage> = ({ err, res }) => {
   );
 };
 
-Component.getInitialProps = async ({ res, err }) => ({
-  res,
-  err,
-  namespacesRequired: ["translation"]
-});
+Component.getInitialProps = async ({ res, err }) => {
+  const errorCode = err ? err.statusCode : undefined;
+  const statusCode = res ? res.statusCode : undefined;
+  const code = errorCode || statusCode || 200;
+  const statusMessage = res ? res.statusMessage : undefined;
+  const errorMessage = err ? err.message : undefined;
+
+  return {
+    code,
+    statusMessage,
+    errorMessage,
+    namespacesRequired: ["translation"]
+  };
+};
 
 export const ErrorPage = withDefaultLayout(Component);
 
-interface ErrorPage {
-  res: any;
-  err: any;
+interface IErrorPage {
+  code: number;
+  statusMessage?: string;
+  errorMessage?: string;
 }
