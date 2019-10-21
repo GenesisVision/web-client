6f36386c2a11d5b2c964f08e26566791141149dd
pavelzubov <pasha224@yandex.ru>
2019-10-21 18:50:33 +0300

    Use ROLE_HOME_ROUTE

--- packages/shared/routes/app.routes.ts
+++ packages/shared/routes/app.routes.ts
@@ -1,3 +1,6 @@
+import { ROLE_ENV } from "shared/constants/constants";
+
+export const ROLE_HOME_ROUTE = `/${ROLE_ENV}`;
 export const HOME_ROUTE = "/";
 export const SIGNUP_ROUTE = "/signup";
 export const LOGIN_ROUTE = "/login";
