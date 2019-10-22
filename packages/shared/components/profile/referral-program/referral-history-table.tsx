60187d88a3a8c47e05c98eca39005c3dc4e48a8d
pavelzubov <pasha224@yandex.ru>
2019-10-22 11:54:22 +0300

    Add exportButtonToolbarRender, title Correct row

--- packages/shared/components/profile/referral-program/referral-history-table.tsx
+++ packages/shared/components/profile/referral-program/referral-history-table.tsx
@@ -1,6 +1,7 @@
 import { RewardDetails } from "gv-api-web";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import DownloadButton from "shared/components/download-button/download-button-toolbar";
 import TableCell from "shared/components/table/components/table-cell";
 import TableContainer from "shared/components/table/components/table-container";
 import TableRow from "shared/components/table/components/table-row";
@@ -13,6 +14,10 @@ const _ReferralHistoryTable: React.FC = () => {
   const [t] = useTranslation();
   return (
     <TableContainer
+      exportButtonToolbarRender={(filtering: any) => (
+        <DownloadButton getExportFileUrl={() => ""} />
+      )}
+      title={t("profile-page.referral-program.referral-history.title")}
       getItems={getHistoryTable}
       dataSelector={referralHistoryTableSelector}
       isFetchOnMount={true}
@@ -25,9 +30,9 @@ const _ReferralHistoryTable: React.FC = () => {
       renderBodyRow={(reward: RewardDetails) => (
         <TableRow stripy>
           <TableCell>
-            {formatDate(reward.amount)} {reward.currency}
+            {reward.amount} {reward.currency}
           </TableCell>
-          <TableCell>{reward.date}</TableCell>
+          <TableCell>{formatDate(reward.date)}</TableCell>
         </TableRow>
       )}
     />
