92c015627373a36b7536b50fc6876f1a05337adf
pavelzubov <pasha224@yandex.ru>
2019-10-23 13:52:43 +0300

    Add ActivePopup

--- packages/shared/components/currency-item/currency-item.tsx
+++ packages/shared/components/currency-item/currency-item.tsx
@@ -1,30 +1,58 @@
 import "./currency-item.scss";
 
 import classNames from "classnames";
-import React from "react";
+import React, { useCallback } from "react";
+import ActivePopup from "shared/components/active/active.popup";
 import WalletImage from "shared/components/avatar/wallet-image/wallet-image";
+import useIsOpen from "shared/hooks/is-open.hook";
 
-const _CurrencyItem: React.FC<Props> = ({ logo, name, small, className }) => {
+const _CurrencyItem: React.FC<Props> = ({
+  logo,
+  name,
+  small,
+  className,
+  clickable = true
+}) => {
+  const [isOpenPopup, setOpenPopup, setClosePopup] = useIsOpen();
+  const openPopup = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      event.preventDefault();
+      setOpenPopup();
+    },
+    []
+  );
   return (
-    <div className="currency-item">
-      <div
-        className={classNames("currency-item__icon", {
-          "currency-item__icon--medium": !small,
-          "currency-item__icon--small": small
-        })}
-      >
-        <WalletImage url={logo} alt={name} />
-      </div>
-      {name && (
-        <div className={classNames("currency-item__name", className)}>
-          {name}
+    <>
+      <a href="http://example.com" onClick={openPopup}>
+        <div className="currency-item">
+          <div
+            className={classNames("currency-item__icon", {
+              "currency-item__icon--medium": !small,
+              "currency-item__icon--small": small
+            })}
+          >
+            <WalletImage url={logo} alt={name} />
+          </div>
+          {name && (
+            <div className={classNames("currency-item__name", className)}>
+              {name}
+            </div>
+          )}
         </div>
+      </a>
+      {clickable && (
+        <ActivePopup
+          open={isOpenPopup}
+          onClose={setClosePopup}
+          active={name!}
+        />
       )}
-    </div>
+    </>
   );
 };
 
 interface Props {
+  clickable?: boolean;
   className?: string;
   small?: boolean;
   logo: string;
