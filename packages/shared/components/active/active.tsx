e8fafd6dc5736453eb5762353d1a8e426a5d8565
pavelzubov <pasha224@yandex.ru>
2019-10-24 12:36:34 +0300

    Correct ways to translations

--- packages/shared/components/active/active.tsx
+++ packages/shared/components/active/active.tsx
@@ -47,14 +47,14 @@ const _Active: React.FC<Props> = ({ data: { name, rate, about, tags } }) => {
         <TagItemList tags={tagMocks} />
       </div>
       <div className="active__block">
-        <div className="active__title">{t("active.chart.title")}</div>
+        <div className="active__title">{t("active.chart")}</div>
         <div className="active__chart-container">
           <TradingViewWidget symbol={name} autosize theme={Themes.DARK} />
         </div>
       </div>
       <div className="active__block">
         <div className="active__title">
-          {t("active.about.title")} {name}
+          {t("active.about")} {name}
         </div>
         <div>{about}</div>
       </div>
