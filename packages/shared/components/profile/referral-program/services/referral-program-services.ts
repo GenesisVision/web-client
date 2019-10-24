6582367bb28e1e51f194f23465175e04098d9c47
pavelzubov <pasha224@yandex.ru>
2019-10-24 13:45:22 +0300

    Add getReferralDetails

--- packages/shared/components/profile/referral-program/services/referral-program-services.ts
+++ packages/shared/components/profile/referral-program/services/referral-program-services.ts
@@ -1,8 +1,14 @@
 import profileApi from "shared/services/api-client/profile-api";
 import authService from "shared/services/auth-service";
 
+import { referralDetailsAction } from "../actions/referral-details.actions";
 import { referralFriendsTableAction } from "../actions/referral-friends-table.actions";
 import { referralHistoryTableAction } from "../actions/referral-history-table.actions";
+
+export const getReferralDetails = () => {
+  const authorization = authService.getAuthArg();
+  return referralDetailsAction(authorization);
+};
 
 export const getFriendsTable = (filters: any) => {
   const authorization = authService.getAuthArg();
