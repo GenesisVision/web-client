0cd1864a1c93e448d3afe3fab16a73fcc924e9af
pavelzubov <pasha224@yandex.ru>
2019-10-24 11:49:39 +0300

    Add TradingViewWidget

--- packages/shared/components/active/active.tsx
+++ packages/shared/components/active/active.tsx
@@ -4,6 +4,7 @@ import * as faker from "faker";
 import { SocialLinkViewModelTypeEnum } from "gv-api-web";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import TradingViewWidget, { Themes } from "react-tradingview-widget";
 import { CurrencyItem } from "shared/components/currency-item/currency-item";
 import { withBlurLoader } from "shared/decorators/with-blur-loader";
 
@@ -34,17 +35,26 @@ const _Active: React.FC<Props> = ({ data: { name, rate, about, tags } }) => {
   return (
     <div>
       <div className="active__block">
-        <CurrencyItem logo={""} name={name} clickable={false} />
+        <CurrencyItem
+          logo={""}
+          name={name}
+          rate={10245}
+          clickable={false}
+          big
+        />
       </div>
-      <div className="active__block">
+      <div className="active__block active__tags">
         <TagItemList tags={tagMocks} />
       </div>
       <div className="active__block">
         <div className="active__title">{t("active.chart.title")}</div>
+        <div className="active__chart-container">
+          <TradingViewWidget symbol={name} autosize theme={Themes.DARK} />
+        </div>
       </div>
       <div className="active__block">
         <div className="active__title">
-          {t("active.chart.about")} {name}
+          {t("active.about.title")} {name}
         </div>
         <div>{about}</div>
       </div>
