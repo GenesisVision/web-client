d3383f3b92a15f9a207630ce86590c7c87afce1f
pavelzubov <pasha224@yandex.ru>
2019-10-27 19:27:14 +0300

    Convert to FC

--- packages/shared/components/header/header.container.tsx
+++ packages/shared/components/header/header.container.tsx
@@ -1,97 +1,49 @@
-import { ProfileHeaderViewModel } from "gv-api-web";
 import * as React from "react";
-import { connect, ResolveThunks } from "react-redux";
-import {
-  ActionCreatorsMapObject,
-  bindActionCreators,
-  compose,
-  Dispatch
-} from "redux";
-import { logout } from "shared/components/auth/signin/signin.service";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "shared/components/header/header";
 import {
   fetchProfileHeaderInfo,
-  fetchTwoFactor,
-  notificationsToggle
+  fetchTwoFactor
 } from "shared/components/header/header.service";
 import { isAuthenticatedSelector } from "shared/reducers/auth-reducer";
 import { headerSelector } from "shared/reducers/header-reducer";
 import { RootState } from "shared/reducers/root-reducer";
+import { TMenuItem } from "shared/routes/menu";
 
-class _HeaderContainer extends React.PureComponent<Props> {
-  componentDidMount() {
-    this.fetchHeaderInfo();
-  }
+const _HeaderContainer: React.FC<Props> = ({
+  topMenuItems,
+  mobileMenuItems
+}) => {
+  const dispatch = useDispatch();
+  const info = useSelector(headerSelector);
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
+  const backPath = useSelector((state: RootState) =>
+    state.router.location ? state.router.location.pathname : ""
+  );
 
-  componentDidUpdate(prevProps: Props) {
-    if (prevProps.isAuthenticated !== this.props.isAuthenticated)
-      this.fetchHeaderInfo();
-  }
-
-  fetchHeaderInfo = () => {
-    const { isAuthenticated, service } = this.props;
+  useEffect(() => {
     if (isAuthenticated) {
-      service.fetchProfileHeaderInfo();
-      service.fetchTwoFactor();
+      dispatch(fetchProfileHeaderInfo);
+      dispatch(fetchTwoFactor);
     }
-  };
+  }, [isAuthenticated]);
 
-  render() {
-    const { service, info, isAuthenticated, backPath } = this.props;
-    return (
-      <Header
-        profileHeader={info}
-        backPath={backPath}
-        isAuthenticated={isAuthenticated}
-        logout={service.logout}
-        openNotifications={service.notificationsToggle}
-      />
-    );
-  }
+  return (
+    <Header
+      topMenuItems={topMenuItems}
+      mobileMenuItems={mobileMenuItems}
+      profileHeader={info}
+      backPath={backPath}
+      isAuthenticated={isAuthenticated}
+    />
+  );
+};
+
+interface Props {
+  mobileMenuItems: TMenuItem[];
+  topMenuItems: TMenuItem[];
 }
 
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
-  service: bindActionCreators<ServiceThunks, ResolveThunks<ServiceThunks>>(
-    {
-      fetchProfileHeaderInfo,
-      logout,
-      notificationsToggle,
-      fetchTwoFactor
-    },
-    dispatch
-  )
-});
-
-const mapStateToProps = (state: RootState): StateProps => ({
-  info: headerSelector(state),
-  isAuthenticated: isAuthenticatedSelector(state),
-  backPath: state.router.location ? state.router.location.pathname : ""
-});
-
-interface Props extends StateProps, DispatchProps, OwnProps {}
-
-interface StateProps {
-  isAuthenticated: boolean;
-  backPath: string;
-  info?: ProfileHeaderViewModel;
-}
-
-interface ServiceThunks extends ActionCreatorsMapObject {
-  fetchProfileHeaderInfo: typeof fetchProfileHeaderInfo;
-  logout: typeof logout;
-  notificationsToggle: typeof notificationsToggle;
-  fetchTwoFactor: typeof fetchTwoFactor;
-}
-interface DispatchProps {
-  service: ResolveThunks<ServiceThunks>;
-}
-
-interface OwnProps {}
-
-const HeaderContainer = compose<React.ComponentType<OwnProps>>(
-  connect<StateProps, DispatchProps, OwnProps, RootState>(
-    mapStateToProps,
-    mapDispatchToProps
-  )
-)(_HeaderContainer);
+const HeaderContainer = React.memo(_HeaderContainer);
 export default HeaderContainer;
