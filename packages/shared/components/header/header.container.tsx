4c259cf535095926c0adfbf35ba29fe35bf3cbb9
pavelzubov <pasha224@yandex.ru>
2019-10-28 11:51:14 +0300

    Revert withRouter Remove unused backPath

--- packages/shared/components/header/header.container.tsx
+++ packages/shared/components/header/header.container.tsx
@@ -1,8 +1,8 @@
+import { WithRouterProps } from "next/dist/client/with-router";
+import { withRouter } from "next/router";
 import * as React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { WithRouterProps } from "next/dist/client/with-router";
-import { withRouter } from "next/router";
 import Header from "shared/components/header/header";
 import {
   fetchProfileHeaderInfo,
@@ -10,10 +10,9 @@ import {
 } from "shared/components/header/header.service";
 import { isAuthenticatedSelector } from "shared/reducers/auth-reducer";
 import { headerSelector } from "shared/reducers/header-reducer";
-import { RootState } from "shared/reducers/root-reducer";
 import { TMenuItem } from "shared/routes/menu";
 
-const _HeaderContainer: React.FC<Props> = ({
+const _HeaderContainer: React.FC<Props & WithRouterProps> = ({
   router,
   topMenuItems,
   mobileMenuItems
@@ -21,9 +20,6 @@ const _HeaderContainer: React.FC<Props> = ({
   const dispatch = useDispatch();
   const info = useSelector(headerSelector);
   const isAuthenticated = useSelector(isAuthenticatedSelector);
-  const backPath = useSelector((state: RootState) =>
-    state.router.location ? state.router.location.pathname : ""
-  );
 
   useEffect(() => {
     if (isAuthenticated) {
