42010c10bc77848c882b2a87591ce3a8b07535e9
pavelzubov <pasha224@yandex.ru>
2019-10-28 11:50:42 +0300

    Remove unused files

--- packages/manager/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import "shared/styles/index.scss";
-import "shared/i18n";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Root from "./root";
-
-ReactDOM.render(<Root />, document.getElementById("root"));
