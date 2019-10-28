e7f56fab219e3e9ce6248c999ac380ae5471f757
pavelzubov <pasha224@yandex.ru>
2019-10-28 11:51:28 +0300

    Revert withRouter

--- packages/shared/components/navigation/navigation.tsx
+++ packages/shared/components/navigation/navigation.tsx
@@ -2,8 +2,6 @@ import "./navigation.scss";
 
 import classNames from "classnames";
 import * as React from "react";
-import { withRouter } from "react-router";
-import { compose } from "redux";
 import { TMenuItem } from "shared/routes/menu";
 
 import { MenuNavigationItem } from "./menu-navigation-item";
@@ -27,8 +25,5 @@ const _Navigation: React.FC<INavigationProps> = ({ menuItems, className }) => {
   );
 };
 
-const Navigation = compose<React.ComponentType<INavigationProps>>(
-  withRouter,
-  React.memo
-)(_Navigation);
+const Navigation = React.memo(_Navigation);
 export default Navigation;
