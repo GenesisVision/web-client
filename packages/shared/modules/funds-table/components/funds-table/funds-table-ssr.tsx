a750df28906645b392ee3fc396f1286af9efd75a
pavelzubov <pasha224@yandex.ru>
2019-10-30 10:08:16 +0300

    Move DEFAULT_FILTERS and getFiltersFromContext to suitable files Add in getFiltersFromContext currency from filter

--- packages/shared/modules/funds-table/components/funds-table/funds-table-ssr.tsx
+++ packages/shared/modules/funds-table/components/funds-table/funds-table-ssr.tsx
@@ -1,28 +1,16 @@
 import { FundsList, PlatformAsset, PlatformCurrency } from "gv-api-web";
-import qs from "qs";
 import * as React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators, compose, Dispatch } from "redux";
 import DateRangeFilter from "shared/components/table/components/filtering/date-range-filter/date-range-filter";
-import {
-  DATE_RANGE_FILTER_NAME,
-  DEFAULT_DATE_RANGE_FILTER_VALUE
-} from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
+import { DATE_RANGE_FILTER_NAME } from "shared/components/table/components/filtering/date-range-filter/date-range-filter.constants";
 import { FilteringType } from "shared/components/table/components/filtering/filter.type";
 import FundAssetFilter from "shared/components/table/components/filtering/fund-asset-filter/fund-asset-filter";
-import {
-  FUND_ASSET_DEFAULT_VALUE,
-  FUND_ASSET_FILTER_NAME
-} from "shared/components/table/components/filtering/fund-asset-filter/fund-asset-filter.constants";
+import { FUND_ASSET_FILTER_NAME } from "shared/components/table/components/filtering/fund-asset-filter/fund-asset-filter.constants";
 import SelectFilter from "shared/components/table/components/filtering/select-filter/select-filter";
-import { composeFilters } from "shared/components/table/helpers/filtering.helpers";
-import {
-  calculateSkipAndTake,
-  calculateTotalPages
-} from "shared/components/table/helpers/paging.helpers";
+import { calculateTotalPages } from "shared/components/table/helpers/paging.helpers";
 import useRouteFilters from "shared/hooks/route-filters.hook";
 import { useTranslation } from "shared/i18n";
-import { ACCOUNT_CURRENCY_KEY } from "shared/middlewares/update-account-settings-middleware/update-account-settings-middleware";
 import {
   ToggleFavoriteDispatchableType,
   toggleFavoriteFundDispatchable
@@ -34,44 +22,16 @@ import {
   platformCurrenciesSelector
 } from "shared/reducers/platform-reducer";
 import { RootState } from "shared/reducers/root-reducer";
-import { getCookie } from "shared/utils/cookie";
-import { CurrencyEnum, NextPageWithReduxContext } from "shared/utils/types";
+import { CurrencyEnum } from "shared/utils/types";
 
 import { fundsDataSelector } from "../../reducers/funds-table.reducers";
 import FundsTable from "./funds-table";
 import {
   CURRENCY_MAP_NAME,
-  CURRENCY_MAP_VALUE,
+  DEFAULT_FUND_TABLE_FILTERS,
   DEFAULT_ITEMS_ON_PAGE,
-  FUNDS_TABLE_FILTERS,
   SORTING_FILTER_VALUE
 } from "./funds-table.constants";
-
-const DEFAULT_FILTERS = {
-  [CURRENCY_MAP_NAME]: CURRENCY_MAP_VALUE,
-  [DATE_RANGE_FILTER_NAME]: DEFAULT_DATE_RANGE_FILTER_VALUE,
-  [FUND_ASSET_FILTER_NAME]: FUND_ASSET_DEFAULT_VALUE
-};
-
-export const getFiltersFromContext = (ctx: NextPageWithReduxContext) => {
-  const { asPath = "", pathname, reduxStore } = ctx;
-  const { page, sorting = SORTING_FILTER_VALUE, ...other } = qs.parse(
-    asPath.slice(pathname.length + 1)
-  );
-  const currency =
-    (getCookie(ACCOUNT_CURRENCY_KEY, ctx) as CurrencyEnum) ||
-    reduxStore.getState().accountSettings.currency;
-  const skipAndTake = calculateSkipAndTake({
-    itemsOnPage: DEFAULT_ITEMS_ON_PAGE,
-    currentPage: page
-  });
-  return {
-    ...composeFilters(FUNDS_TABLE_FILTERS, { ...DEFAULT_FILTERS, ...other }),
-    ...skipAndTake,
-    currency,
-    sorting
-  };
-};
 
 const _FundsTableSSR: React.FC<Props> = ({
   currency,
@@ -84,7 +44,9 @@ const _FundsTableSSR: React.FC<Props> = ({
   service
 }) => {
   const { t } = useTranslation();
-  const [filtering, sorting, page, update] = useRouteFilters(DEFAULT_FILTERS);
+  const [filtering, sorting, page, update] = useRouteFilters(
+    DEFAULT_FUND_TABLE_FILTERS
+  );
   if (!filtering[CURRENCY_MAP_NAME]) {
     filtering[CURRENCY_MAP_NAME] = currency;
   }
@@ -119,14 +81,12 @@ const _FundsTableSSR: React.FC<Props> = ({
         </>
       )}
       renderFilters={(updateFilter: any, filtering: FilteringType) => (
-        <>
-          <FundAssetFilter
-            name={FUND_ASSET_FILTER_NAME}
-            value={filtering[FUND_ASSET_FILTER_NAME] as string[]}
-            values={fundAssets}
-            onChange={updateFilter}
-          />
-        </>
+        <FundAssetFilter
+          name={FUND_ASSET_FILTER_NAME}
+          value={filtering[FUND_ASSET_FILTER_NAME] as string[]}
+          values={fundAssets}
+          onChange={updateFilter}
+        />
       )}
       paging={{
         totalPages: totalPages,
