337f8bc5a1fd5524afb98a6e54078eeb48fac0cb
pavelzubov <pasha224@yandex.ru>
2019-10-30 10:14:41 +0300

    Change FundDetails to FundDetailsOld

--- packages/shared/modules/funds-table/components/funds-table/funds-table.tsx
+++ packages/shared/modules/funds-table/components/funds-table/funds-table.tsx
@@ -1,6 +1,6 @@
 import "./funds-table.scss";
 
-import { FundDetails } from "gv-api-web";
+import { FundDetailsOld } from "gv-api-web";
 import * as React from "react";
 import { Table } from "shared/components/table/components";
 import { ITableProps } from "shared/components/table/components/table";
@@ -14,7 +14,7 @@ import FundsTableHeaderCell from "./funds-table-header-cell";
 import { FUNDS_TABLE_COLUMNS } from "./funds-table.constants";
 
 interface Props extends ITableProps {
-  data?: FundDetails[];
+  data?: FundDetailsOld[];
   toggleFavorite: TableToggleFavoriteHandlerType;
   isAuthenticated: boolean;
 }
