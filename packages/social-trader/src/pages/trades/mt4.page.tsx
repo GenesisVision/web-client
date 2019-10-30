9b808034370f5cb203340090c1a1be72676e7dfe
pavelzubov <pasha224@yandex.ru>
2019-10-30 14:21:53 +0300

    Add page components of trades

---
+++ packages/social-trader/src/pages/trades/mt4.page.tsx
@@ -0,0 +1,29 @@
+import "./trades.scss";
+
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Page from "shared/components/page/page";
+
+const _Mt4: React.FC = () => {
+  const [t] = useTranslation();
+  const title = t("mt4-page.title");
+  return (
+    <Page title={title}>
+      <h1>{title}</h1>
+      <div className="mt-frame">
+        <iframe
+          title={title}
+          allowFullScreen="allowfullscreen"
+          name="webTerminalHost"
+          id="webTerminalHost"
+          src="https://trade.mql5.com/trade?version=4"
+        >
+          {title}
+        </iframe>
+      </div>
+    </Page>
+  );
+};
+
+const Mt4Page = React.memo(_Mt4);
+export default Mt4Page;
---
+++ packages/social-trader/src/pages/trades/mt5.page.tsx
@@ -0,0 +1,27 @@
+import "./trades.scss";
+
+import React from "react";
+import { useTranslation } from "react-i18next";
+import Page from "shared/components/page/page";
+
+const _Mt5: React.FC = () => {
+  const [t] = useTranslation();
+  const title = t("mt5-page.title");
+  return (
+    <Page title={title}>
+      <h1>{title}</h1>
+      <div className="mt-frame">
+        <iframe
+          title={title}
+          src="https://trade.mql5.com/trade?servers=GenesisMarketsLp-Demo,GenesisMarketsLp-Live&amp;trade_server=GenesisMarketsLp-Live&amp;demo_server=GenesisMarketsLp-Demo&amp;lang=en&amp;save_password=on"
+          allowFullScreen="allowfullscreen"
+        >
+          {title}
+        </iframe>
+      </div>
+    </Page>
+  );
+};
+
+const Mt5Page = React.memo(_Mt5);
+export default Mt5Page;
