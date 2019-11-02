23150bc1b0ef705bf94a38a78ff471a0dbdc9671
pavelzubov <pasha224@yandex.ru>
2019-11-02 12:26:19 +0300

    Remove dispatching description

--- packages/shared/components/follows/follow-details/follow-details.page.tsx
+++ packages/shared/components/follows/follow-details/follow-details.page.tsx
@@ -1,22 +1,14 @@
 import "shared/components/details/details.scss";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import FollowDetailsContainer from "./follow-details.contaner";
 import { IDescriptionSection } from "./follow-details.types";
-import {
-  followDescriptionSelector,
-  followIdSelector
-} from "./reducers/description.reducer";
-import { dispatchFollowDescription } from "./services/follow-details.service";
+import { followDescriptionSelector } from "./reducers/description.reducer";
 
 const _FollowDetailsPage: React.FC<Props> = ({ descriptionSection }) => {
-  const id = useSelector(followIdSelector);
   const description = useSelector(followDescriptionSelector);
-  useEffect(() => {
-    dispatchFollowDescription(id)();
-  }, []);
   return (
     <FollowDetailsContainer
       descriptionSection={descriptionSection}
