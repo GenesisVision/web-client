37d3c0b71afc22b273e583cfc8f5fa05f181cd6e
pavelzubov <pasha224@yandex.ru>
2019-11-02 14:05:44 +0300

    Change to follow components and methods

--- packages/social-trader/pages/invest/follows/[id]/settings.tsx
+++ packages/social-trader/pages/invest/follows/[id]/settings.tsx
@@ -1,23 +1,23 @@
-import ProgramSettingsPage from "pages/invest/programs/programs-settings/program-settings.page";
+import FollowSettingsPage from "pages/follows/follow-settings/follow-settings.page";
 import React from "react";
 import { compose } from "redux";
 import {
-  dispatchProgramDescription,
-  dispatchProgramId
-} from "shared/components/programs/program-details/services/program-details.service";
+  dispatchFollowDescription,
+  dispatchFollowId
+} from "shared/components/follows/follow-details/services/follow-details.service";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import withPrivateRoute from "shared/decorators/with-private-route";
 import { NextPageWithRedux } from "shared/utils/types";
 
 const ProgramSettings: NextPageWithRedux<void> = () => {
-  return <ProgramSettingsPage />;
+  return <FollowSettingsPage />;
 };
 
 ProgramSettings.getInitialProps = async ctx => {
   const { id } = ctx.query;
   await Promise.all([
-    ctx.reduxStore.dispatch(dispatchProgramId(id as string)),
-    ctx.reduxStore.dispatch(dispatchProgramDescription(ctx))
+    ctx.reduxStore.dispatch(dispatchFollowId(id as string)),
+    ctx.reduxStore.dispatch(dispatchFollowDescription(id as string)(ctx))
   ]);
 };
 
