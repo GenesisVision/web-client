5ccea97beda461a2406b3d383ecd6ea76d6b1890
pavelzubov <pasha224@yandex.ru>
2019-11-02 12:24:02 +0300

    Add ID

--- packages/shared/routes/app.routes.ts
+++ packages/shared/routes/app.routes.ts
@@ -4,3 +4,5 @@ export const ROLE_HOME_ROUTE = `/${ROLE_ENV}`;
 export const HOME_ROUTE = "/";
 export const SIGNUP_ROUTE = "/signup";
 export const LOGIN_ROUTE = "/login";
+
+export const ID = "id";
