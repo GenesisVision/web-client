90da937464973b5e65fabe5700e36aeccc49a341
pavelzubov <pasha224@yandex.ru>
2019-11-02 10:19:07 +0300

    Add follow details route

--- packages/social-trader/pages/invest/follow/[id]/index.tsx
+++ packages/social-trader/pages/invest/follow/[id]/index.tsx
@@ -1,5 +1,33 @@
-import ProgramDetailsPage from "pages/invest/programs/program-details/program-details.page";
+import { Broker, ProgramDetailsFullOld } from "gv-api-web";
+import FollowDetailsPage from "pages/follows/follow-details/follow-details.page";
 import React from "react";
-import { programDetailsCreator } from "shared/routes/ssr/programs/[id]";
+import { compose } from "redux";
+import { statisticCurrencyAction } from "shared/components/follows/follow-details/actions/follow-details.actions";
+import {
+  dispatchFollowDescription,
+  dispatchFollowId
+} from "shared/components/follows/follow-details/services/follow-details.service";
+import withDefaultLayout from "shared/decorators/with-default-layout";
+import { NextPageWithRedux } from "shared/utils/types";
 
-export default programDetailsCreator(ProgramDetailsPage);
+const Page: NextPageWithRedux<{}> = () => {
+  return <FollowDetailsPage />;
+};
+
+Page.getInitialProps = async ctx => {
+  const { id } = ctx.query;
+  await Promise.all([
+    ctx.reduxStore.dispatch(dispatchFollowId(id as string)),
+    ctx.reduxStore.dispatch(dispatchFollowDescription(id as string)(ctx))
+  ]).then(([_, descriptionResult]) => {
+    const description = ((descriptionResult as unknown) as {
+      value: ProgramDetailsFullOld;
+    }).value;
+    ctx.reduxStore.dispatch(dispatch =>
+      dispatch(statisticCurrencyAction(description.currency))
+    );
+  });
+  return {};
+};
+
+export default compose(withDefaultLayout)(Page);
