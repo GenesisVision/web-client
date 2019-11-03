c1b7267401beedcbf68e9ecaa2b0a5b14c0a1cde
pavelzubov <pasha224@yandex.ru>
2019-11-03 07:56:56 +0300

    Rename method

--- packages/shared/components/multi-chart/service/multi-chart.service.ts
+++ packages/shared/components/multi-chart/service/multi-chart.service.ts
@@ -6,7 +6,7 @@ import { ProfitChartDataType } from "../../details/details-statistic-section/det
 import { getProfitChartLoaderDataWithEquity } from "../../programs/program-details/program-details.loader-data";
 import { TChartAsset } from "../multi-chart.types";
 
-export const fetchMultiChart = ({
+export const fetchMultiChartData = ({
   assets,
   period
 }: {
@@ -24,22 +24,22 @@ export const fetchMultiChart = ({
 
 export const fetchAssets = (): CancelablePromise<TChartAsset> => {
   return (Promise.resolve([
-    { name: "Example1", logo: "", type: ASSET.FUND },
-    { name: "Example2", logo: "", type: ASSET.PROGRAM },
-    { name: "Example3", logo: "", type: ASSET.FUND },
-    { name: "Example4", logo: "", type: ASSET.FUND },
-    { name: "Example5", logo: "", type: ASSET.PROGRAM },
-    { name: "Example6", logo: "", type: ASSET.FUND },
-    { name: "Example8", logo: "", type: ASSET.PROGRAM },
-    { name: "Example9", logo: "", type: ASSET.FUND },
-    { name: "Example10", logo: "", type: ASSET.FUND },
-    { name: "Example11", logo: "", type: ASSET.FUND },
-    { name: "Example12", logo: "", type: ASSET.PROGRAM },
-    { name: "Example13", logo: "", type: ASSET.FUND },
-    { name: "Example14", logo: "", type: ASSET.FUND },
-    { name: "Example15", logo: "", type: ASSET.PROGRAM },
-    { name: "Example16", logo: "", type: ASSET.FUND },
-    { name: "Example18", logo: "", type: ASSET.PROGRAM }
+    { name: "Example1", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example2", logo: "", url: "", type: ASSET.PROGRAM },
+    { name: "Example3", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example4", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example5", logo: "", url: "", type: ASSET.PROGRAM },
+    { name: "Example6", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example8", logo: "", url: "", type: ASSET.PROGRAM },
+    { name: "Example9", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example10", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example11", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example12", logo: "", url: "", type: ASSET.PROGRAM },
+    { name: "Example13", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example14", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example15", logo: "", url: "", type: ASSET.PROGRAM },
+    { name: "Example16", logo: "", url: "", type: ASSET.FUND },
+    { name: "Example18", logo: "", url: "", type: ASSET.PROGRAM }
   ]) as unknown) as CancelablePromise<TChartAsset>;
 };
 
