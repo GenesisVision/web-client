9438f7d8dc1b094ba4597df07d46ef018fb5d953
pavelzubov <pasha224@yandex.ru>
2019-11-03 10:39:44 +0300

    Remove old blocks

--- packages/social-trader/src/pages/dashboard/dashboard.page.tsx
+++ packages/social-trader/src/pages/dashboard/dashboard.page.tsx
@@ -4,28 +4,15 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import DetailsBlock from "shared/components/details/details-block";
 import Page from "shared/components/page/page";
-import useRole from "shared/hooks/use-role.hook";
 
-import DashboardAssetsContainer from "./components/dashboard-assets/dashboard-assets-container";
 import DashboardPortfolioChartSection from "./components/dashboard-portfolio-chart-section/dashboard-portfolio-chart-section";
-import DashboardPortfolioEventsSection from "./components/dashboard-portfolio-events/dashboard-portfolio-events-section";
 
 const _DashboardPage: React.FC = () => {
   const [t] = useTranslation();
-  const role = useRole();
-  const title = t(`${role ? `${role}.` : ""}dashboard-page.title`);
   return (
-    <Page title={title}>
-      <div className="dashboard__row">
-        <DetailsBlock className="dashboard__chart-block">
-          <DashboardPortfolioChartSection />
-        </DetailsBlock>
-        <DetailsBlock horizontalPaddings className="dashboard__events-block">
-          <DashboardPortfolioEventsSection title={title} />
-        </DetailsBlock>
-      </div>
-      <DetailsBlock>
-        <DashboardAssetsContainer title={title} />
+    <Page title={t(`social-trader.dashboard-page.title`)}>
+      <DetailsBlock className="dashboard__chart-block">
+        <DashboardPortfolioChartSection />
       </DetailsBlock>
     </Page>
   );
