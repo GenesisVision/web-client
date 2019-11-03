1b6a58e9ba24de85124d289429c8752e0538de82
pavelzubov <pasha224@yandex.ru>
2019-11-03 22:13:16 +0300

    Add mock services

--- packages/social-trader/src/pages/dashboard/services/dashboard.service.ts
+++ packages/social-trader/src/pages/dashboard/services/dashboard.service.ts
@@ -1,3 +1,4 @@
+import * as faker from "faker";
 import {
   CancelablePromise,
   InvestmentEventViewModels,
@@ -5,6 +6,23 @@ import {
   ManagerSimpleProgram
 } from "gv-api-web";
 import { NextPageContext } from "next";
+import {
+  assetsLoaderData,
+  getInvestingStatisticLoaderData,
+  getProgramStatisticLoaderData,
+  getRecommendationLoaderData,
+  getTotalLoaderData,
+  portfolioAssetsData,
+  portfolioLoaderData
+} from "pages/dashboard/dashboard.loaders-data";
+import {
+  TAssets,
+  TDashboardInvestingStatistic,
+  TDashboardProgramsStatistic,
+  TDashboardTotal,
+  TPortfolio,
+  TRecommendation
+} from "pages/dashboard/dashboard.types";
 import { ManagerRootState } from "reducers";
 import { Dispatch } from "redux";
 import { ChartDefaultPeriod } from "shared/components/chart/chart-period/chart-period.helpers";
@@ -14,18 +32,48 @@ import { ASSETS_TYPES } from "shared/components/table/components/filtering/asset
 import { ComposeFiltersAllType } from "shared/components/table/components/filtering/filter.type";
 import { composeRequestFiltersByTableState } from "shared/components/table/services/table.service";
 import fundsApi from "shared/services/api-client/funds-api";
-import managerApi from "shared/services/api-client/manager-api";
 import programsApi from "shared/services/api-client/programs-api";
 import authService from "shared/services/auth-service";
-import {
-  ActionType,
-  MiddlewareDispatch,
-  TGetAuthState
-} from "shared/utils/types";
+import { getRandomInteger, tableLoaderCreator } from "shared/utils/helpers";
+import { ActionType } from "shared/utils/types";
 
 import * as actions from "../actions/dashboard.actions";
 import { fetchEventsAction } from "../actions/dashboard.actions";
 import { getDashboardFunds } from "./dashboard-funds.service";
+
+export const getPortfolio = (): CancelablePromise<TPortfolio> =>
+  (Promise.resolve(portfolioLoaderData()) as unknown) as CancelablePromise<
+    TPortfolio
+  >;
+
+export const getAssetsPercents = (): CancelablePromise<TAssets> =>
+  (Promise.resolve(assetsLoaderData()) as unknown) as CancelablePromise<
+    TAssets
+  >;
+
+export const getRecommendations = (): CancelablePromise<TRecommendation[]> =>
+  (Promise.resolve(
+    tableLoaderCreator(getRecommendationLoaderData, 15)
+  ) as unknown) as CancelablePromise<TRecommendation[]>;
+
+export const getTotal = (): CancelablePromise<TDashboardTotal> =>
+  (Promise.resolve(getTotalLoaderData()) as unknown) as CancelablePromise<
+    TDashboardTotal
+  >;
+
+export const getTotalProgramStatistic = (): CancelablePromise<
+  TDashboardProgramsStatistic
+> =>
+  (Promise.resolve(
+    getProgramStatisticLoaderData()
+  ) as unknown) as CancelablePromise<TDashboardProgramsStatistic>;
+
+export const getTotalInvestingStatistic = (): CancelablePromise<
+  TDashboardInvestingStatistic
+> =>
+  (Promise.resolve(
+    getInvestingStatisticLoaderData()
+  ) as unknown) as CancelablePromise<TDashboardInvestingStatistic>;
 
 export const getEvents = (eventLocation: EVENT_LOCATION) => (
   filters: ComposeFiltersAllType
