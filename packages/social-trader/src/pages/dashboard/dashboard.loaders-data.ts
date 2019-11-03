f43debf35bc4edffb15b4887f325bc7c11c864b0
pavelzubov <pasha224@yandex.ru>
2019-11-03 22:13:36 +0300

    Add mock loaders

--- packages/social-trader/src/pages/dashboard/dashboard.loaders-data.ts
+++ packages/social-trader/src/pages/dashboard/dashboard.loaders-data.ts
@@ -1,5 +1,9 @@
+import * as faker from "faker";
+import { TRecommendation } from "pages/dashboard/dashboard.types";
+import { getEquityChartLoaderData } from "shared/components/multi-chart/service/multi-chart.service";
 import { ASSETS_TYPES } from "shared/components/table/components/filtering/asset-type-filter/asset-type-filter.constants";
-import { IDashboardAssetChart } from "shared/constants/constants";
+import { ASSET, IDashboardAssetChart } from "shared/constants/constants";
+import { getRandomInteger, tableLoaderCreator } from "shared/utils/helpers";
 
 export const DashboardChartValueLoaderData: IDashboardAssetChart = {
   type: ASSETS_TYPES.Fund,
@@ -9,3 +13,115 @@ export const DashboardChartValueLoaderData: IDashboardAssetChart = {
   equityChart: [],
   pnLChart: []
 };
+
+export const assetsLoaderData = () =>
+  tableLoaderCreator(
+    () => ({
+      name: faker.finance.currencyCode(),
+      percent: getRandomInteger(0, 15)
+    }),
+    getRandomInteger(0, 15)
+  );
+
+export const portfolioLoaderData = () => [
+  {
+    name: "Funds",
+    percent: getRandomInteger(0, 25)
+  },
+  {
+    name: "Programs",
+    percent: getRandomInteger(0, 25)
+  },
+  {
+    name: "Trading",
+    percent: getRandomInteger(0, 25)
+  },
+  {
+    name: "Wallet",
+    percent: getRandomInteger(0, 25)
+  }
+];
+
+export const getProgramStatisticLoaderData = () => ({
+  equity: getRandomInteger(-10000, 10000),
+  AUM: getRandomInteger(-10000, 10000),
+  total: {
+    day: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    },
+    week: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    },
+    month: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    }
+  },
+  events: tableLoaderCreator(getEventLoaderData, getRandomInteger(1, 10))
+});
+export const getInvestingStatisticLoaderData = () => ({
+  balance: getRandomInteger(-10000, 10000),
+  programs: getRandomInteger(-10000, 10000),
+  funds: getRandomInteger(-10000, 10000),
+  total: {
+    day: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    },
+    week: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    },
+    month: {
+      value: getRandomInteger(-10000, 10000),
+      profit: getRandomInteger(-100, 100)
+    }
+  },
+  events: tableLoaderCreator(getEventLoaderData, getRandomInteger(1, 10))
+});
+
+const getEventLoaderData = () => ({
+  data: new Date(),
+  description: faker.lorem.words(3),
+  amount: getRandomInteger(-10000, 10000)
+});
+
+const getRandomAsset = () =>
+  [ASSET.FUND, ASSET.PROGRAM, ASSET.FOLLOW][getRandomInteger(0, 2)];
+
+export const getRecommendationLoaderData = (): TRecommendation => ({
+  type: getRandomAsset(),
+  color: faker.internet.color(),
+  level: 0,
+  levelProgress: 0,
+  logo: "",
+  id: "",
+  url: "",
+  title: faker.name.lastName(),
+  manager: {
+    username: faker.name.lastName(),
+    url: faker.name.lastName()
+  },
+  currency: "GVT",
+  profit: getRandomInteger(-1000, 1000),
+  profitPercent: getRandomInteger(-1000, 1000),
+  value: getRandomInteger(-1000, 1000),
+  chart: getEquityChartLoaderData()
+});
+
+export const getTotalLoaderData = () => ({
+  day: {
+    value: getRandomInteger(-10000, 10000),
+    profit: getRandomInteger(-100, 100)
+  },
+  week: {
+    value: getRandomInteger(-10000, 10000),
+    profit: getRandomInteger(-100, 100)
+  },
+  month: {
+    value: getRandomInteger(-10000, 10000),
+    profit: getRandomInteger(-100, 100)
+  }
+});
