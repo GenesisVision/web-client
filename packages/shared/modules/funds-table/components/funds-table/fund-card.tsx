233666a323ac340d65ca9e6aee51d12643017c26
pavelzubov <pasha224@yandex.ru>
2019-11-04 20:37:57 +0300

    Create TableCard components

--- packages/shared/modules/funds-table/components/funds-table/fund-card.tsx
+++ packages/shared/modules/funds-table/components/funds-table/fund-card.tsx
@@ -3,35 +3,27 @@ import * as React from "react";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import NumberFormat from "react-number-format";
-import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
 import { FUND_ASSET_TYPE } from "shared/components/fund-asset/fund-asset";
 import FundAssetContainer from "shared/components/fund-asset/fund-asset-container";
 import GVButton from "shared/components/gv-button";
-import { ActionsCircleIcon } from "shared/components/icon/actions-circle-icon";
 import Link from "shared/components/link/link";
 import Popover, {
   HORIZONTAL_POPOVER_POS,
   VERTICAL_POPOVER_POS
 } from "shared/components/popover/popover";
-import Profitability from "shared/components/profitability/profitability";
-import {
-  PROFITABILITY_PREFIX,
-  PROFITABILITY_VARIANT
-} from "shared/components/profitability/profitability.helper";
-import ProgramSimpleChart from "shared/components/program-simple-chart/program-simple-chart";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
-import useAnchor from "shared/hooks/anchor.hook";
+import TableCard, {
+  TableCardTable,
+  TableCardTableColumn,
+  TableCardTableRow
+} from "shared/components/table/components/table-card/table-card";
+import { TAnchor, TEvent } from "shared/hooks/anchor.hook";
 import { FUND_DETAILS_FOLDER_ROUTE } from "shared/routes/funds.routes";
-import { MANAGER_DETAILS_FOLDER_ROUTE } from "shared/routes/manager.routes";
-import {
-  composeFundsDetailsUrl,
-  composeManagerDetailsUrl
-} from "shared/utils/compose-url";
+import { composeFundsDetailsUrl } from "shared/utils/compose-url";
 import { formatCurrencyValue, formatValue } from "shared/utils/formatter";
 
 const _FundCard: React.FC<Props> = ({ fund, toggleFavorite, title }) => {
   const { t } = useTranslation();
-  const { anchor, setAnchor, clearAnchor } = useAnchor();
   const handleToggleFavorite = useCallback(
     () =>
       toggleFavorite(
@@ -40,120 +32,66 @@ const _FundCard: React.FC<Props> = ({ fund, toggleFavorite, title }) => {
       ),
     [fund.id, fund.personalDetails, toggleFavorite]
   );
-  return (
-    <div className="table-cards__card">
-      <div className="table-cards__row">
-        <div className="table-cards__avatar">
-          <Link
-            to={{
-              pathname: composeFundsDetailsUrl(fund.url),
-              state: `/ ${title}`
-            }}
+  const renderActions = ({
+    clearAnchor,
+    anchor
+  }: {
+    clearAnchor: (event: TEvent) => void;
+    anchor: TAnchor;
+  }) => (
+    <Popover
+      horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+      vertical={VERTICAL_POPOVER_POS.BOTTOM}
+      anchorEl={anchor}
+      noPadding
+      onClose={clearAnchor}
+    >
+      <div className="popover-list">
+        <Link
+          to={{
+            as: composeFundsDetailsUrl(fund.url),
+            pathname: FUND_DETAILS_FOLDER_ROUTE,
+            state: `/ ${title}`
+          }}
+        >
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("fund-actions.details")}
+          </GVButton>
+        </Link>
+        {fund.personalDetails && !fund.personalDetails.isFavorite && (
+          <GVButton
+            variant="text"
+            color="secondary"
+            onClick={handleToggleFavorite}
           >
-            <AssetAvatar
-              url={fund.logo}
-              alt={fund.title}
-              color={fund.color}
-              size="medium"
-            />
-          </Link>
-        </div>
-        <div className="table-cards__main-info">
-          <div className="table-cards__title-wrapper">
-            <Link
-              className="table-cards__title"
-              to={{
-                pathname: FUND_DETAILS_FOLDER_ROUTE,
-                as: composeFundsDetailsUrl(fund.url),
-                state: `/ ${title}`
-              }}
-            >
-              {fund.title}
-            </Link>
-            <Link
-              className="table-cards__name"
-              to={{
-                as: composeManagerDetailsUrl(fund.manager.url),
-                pathname: MANAGER_DETAILS_FOLDER_ROUTE,
-                state: `/ ${title}`
-              }}
-            >
-              {fund.manager.username}
-            </Link>
-          </div>
-          <div className="table-cards__actions">
-            <ActionsCircleIcon primary={!!anchor} onClick={setAnchor} />
-            <Popover
-              horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
-              vertical={VERTICAL_POPOVER_POS.BOTTOM}
-              anchorEl={anchor}
-              noPadding
-              onClose={clearAnchor}
-            >
-              <div className="popover-list">
-                <Link
-                  to={{
-                    as: composeFundsDetailsUrl(fund.url),
-                    pathname: FUND_DETAILS_FOLDER_ROUTE,
-                    state: `/ ${title}`
-                  }}
-                >
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={clearAnchor}
-                  >
-                    {t("fund-actions.details")}
-                  </GVButton>
-                </Link>
-                {fund.personalDetails && !fund.personalDetails.isFavorite && (
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={handleToggleFavorite}
-                  >
-                    {t("fund-actions.add-to-favorites")}
-                  </GVButton>
-                )}
-                {fund.personalDetails && fund.personalDetails.isFavorite && (
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={handleToggleFavorite}
-                  >
-                    {t("fund-actions.remove-from-favorites")}
-                  </GVButton>
-                )}
-              </div>
-            </Popover>
-          </div>
-        </div>
+            {t("fund-actions.add-to-favorites")}
+          </GVButton>
+        )}
+        {fund.personalDetails && fund.personalDetails.isFavorite && (
+          <GVButton
+            variant="text"
+            color="secondary"
+            onClick={handleToggleFavorite}
+          >
+            {t("fund-actions.remove-from-favorites")}
+          </GVButton>
+        )}
       </div>
-      <div className="table-cards__row">
-        <div className="table-cards__chart">
-          {fund.chart && (
-            <ProgramSimpleChart data={fund.chart} programId={fund.id} />
-          )}
-        </div>
-        <div className="table-cards__chart-info">
-          <div className="table-cards__profit">
-            <Profitability
-              value={formatValue(fund.statistic.profitPercent, 2)}
-              variant={PROFITABILITY_VARIANT.CHIPS}
-              prefix={PROFITABILITY_PREFIX.ARROW}
-            >
-              <NumberFormat
-                value={formatValue(fund.statistic.profitPercent, 2)}
-                suffix="%"
-                allowNegative={false}
-                displayType="text"
-              />
-            </Profitability>
-          </div>
-        </div>
-      </div>
-      <div className="table-cards__table table-cards__table--flex-wrap">
-        <div className="table-cards__table-column">
+    </Popover>
+  );
+  return (
+    <TableCard
+      asset={fund}
+      detailsUrl={{
+        pathname: composeFundsDetailsUrl(fund.url),
+        state: `/ ${title}`
+      }}
+      pathTitle={title}
+      profitPercent={fund.statistic.profitPercent}
+      renderActions={renderActions}
+    >
+      <TableCardTable wrap>
+        <TableCardTableColumn>
           <StatisticItem label={t("funds-page.funds-header.balance")}>
             <NumberFormat
               value={formatCurrencyValue(
@@ -164,8 +102,8 @@ const _FundCard: React.FC<Props> = ({ fund, toggleFavorite, title }) => {
               displayType="text"
             />
           </StatisticItem>
-        </div>
-        <div className="table-cards__table-column">
+        </TableCardTableColumn>
+        <TableCardTableColumn>
           <StatisticItem label={t("funds-page.funds-header.investors")}>
             <NumberFormat
               value={fund.statistic.investorsCount}
@@ -173,8 +111,8 @@ const _FundCard: React.FC<Props> = ({ fund, toggleFavorite, title }) => {
               decimalScale={0}
             />
           </StatisticItem>
-        </div>
-        <div className="table-cards__table-column">
+        </TableCardTableColumn>
+        <TableCardTableColumn>
           <StatisticItem label={t("funds-page.funds-header.drawdown")}>
             <NumberFormat
               value={formatValue(fund.statistic.drawdownPercent, 2)}
@@ -182,17 +120,19 @@ const _FundCard: React.FC<Props> = ({ fund, toggleFavorite, title }) => {
               suffix="%"
             />
           </StatisticItem>
-        </div>
-        <div className="table-cards__table-row">
-          <FundAssetContainer
-            assets={fund.topFundAssets}
-            type={FUND_ASSET_TYPE.SHORT}
-            size={3}
-            length={fund.totalAssetsCount}
-          />
-        </div>
-      </div>
-    </div>
+        </TableCardTableColumn>
+        <TableCardTableRow>
+          {fund.topFundAssets && (
+            <FundAssetContainer
+              assets={fund.topFundAssets}
+              type={FUND_ASSET_TYPE.SHORT}
+              size={3}
+              length={fund.totalAssetsCount}
+            />
+          )}
+        </TableCardTableRow>
+      </TableCardTable>
+    </TableCard>
   );
 };
 
