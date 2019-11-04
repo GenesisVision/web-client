73ddf0f0fb899d9310f3fd835383720ae68c85ae
pavelzubov <pasha224@yandex.ru>
2019-11-04 09:29:02 +0300

    Add pie chart

--- packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart.tsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import MultiPieContainer from "shared/components/pie-container/multi-pie.container";
 import { withBlurLoader } from "shared/decorators/with-blur-loader";
 
 const _DashboardPieChart: React.FC<Props> = ({ data }) => {
+  const [overItem, setOverItem] = useState();
   return (
-    <div>
-      {data.map(({ name }) => (
-        <div className="">{name}</div>
-      ))}
+    <div className="dashboard-pie-chart__container">
+      <div className="dashboard-pie-chart__pie-chart">
+        <MultiPieContainer
+          over={overItem}
+          data={data.map(item => ({ ...item, value: item.percent }))}
+        />
+      </div>
+      <div className="dashboard-pie-chart__names">
+        {data.map(({ name, color, percent }) => (
+          <div
+            className="dashboard-pie-chart__name-container"
+            onMouseEnter={() => setOverItem(name)}
+            onMouseLeave={() => setOverItem(undefined)}
+          >
+            <div
+              className="dashboard-pie-chart__bullet"
+              style={{
+                background: color
+              }}
+            />
+            <div className="dashboard-pie-chart__name">
+              {name} - {percent} %
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
