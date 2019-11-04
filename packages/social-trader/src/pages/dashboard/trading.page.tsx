7927ae0c44cdb96bff9099dd5425a315071c6118
pavelzubov <pasha224@yandex.ru>
2019-11-04 20:42:51 +0300

    Add DashboardTradingContainer

--- packages/social-trader/src/pages/dashboard/trading.page.tsx
+++ packages/social-trader/src/pages/dashboard/trading.page.tsx
@@ -1,10 +1,20 @@
 import "shared/components/dashboard/dashboard.scss";
 
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Page from "shared/components/page/page";
 
+import DashboardTradingContainer from "./components/dashboard-trading/dashboard-trading.container";
+
 const _TradingPage: React.FC = () => {
-  return <Page title={""}>Trading</Page>;
+  const [t] = useTranslation();
+  return (
+    <Page title={t(`social-trader.dashboard-page.title`)}>
+      <div>
+        <DashboardTradingContainer />
+      </div>
+    </Page>
+  );
 };
 
 const TradingPage = React.memo(_TradingPage);
