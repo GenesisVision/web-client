25374df04cabd80c47b7d0363d68e74060391007
pavelzubov <pasha224@yandex.ru>
2019-11-04 09:29:18 +0300

    Import styles

--- packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart-block.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart-block.tsx
@@ -1,3 +1,5 @@
+import "./dashboard-pie-chart.scss";
+
 import DashboardPieChart from "pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart";
 import React, { useEffect } from "react";
 import useApiRequest from "shared/hooks/api-request.hook";
