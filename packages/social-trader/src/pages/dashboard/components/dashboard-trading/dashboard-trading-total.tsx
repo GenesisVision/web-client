84c68043e9549e022eab651591a727e46961aae6
pavelzubov <pasha224@yandex.ru>
2019-11-04 23:00:38 +0300

    Move containers logic

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-trading-total.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-trading-total.tsx
@@ -1,42 +1,35 @@
-import DashboardBlock from "pages/dashboard/components/dashboard-block/dashboard-block";
 import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { StatisticItemList } from "shared/components/statistic-item-list/statistic-item-list";
 import { withBlurLoader } from "shared/decorators/with-blur-loader";
 
-import { TTrading, TTradingTotal } from "../../dashboard.types";
+import { TTradingTotal } from "../../dashboard.types";
 
 const _DashboardTradingTotal: React.FC<Props> = ({
   data: { equity, total, AUM }
 }) => {
   const [t] = useTranslation();
   return (
-    <DashboardBlock
-      label={t("dashboard-page.trading.total")}
-      all={""}
-      seeAll={false}
-    >
-      <div className="dashboard-trading__values">
-        <StatisticItemList>
-          <DashboardValueItem
-            label={t("dashboard-page.statistic.total")}
-            value={total}
-            currency={"GVT"}
-          />
-          <DashboardValueItem
-            label={t("dashboard-page.statistic.equity")}
-            value={equity}
-            currency={"GVT"}
-          />
-          <DashboardValueItem
-            label={t("dashboard-page.statistic.AUM")}
-            value={AUM}
-            currency={"GVT"}
-          />
-        </StatisticItemList>
-      </div>
-    </DashboardBlock>
+    <div className="dashboard-trading__values">
+      <StatisticItemList>
+        <DashboardValueItem
+          label={t("dashboard-page.statistic.total")}
+          value={total}
+          currency={"GVT"}
+        />
+        <DashboardValueItem
+          label={t("dashboard-page.statistic.equity")}
+          value={equity}
+          currency={"GVT"}
+        />
+        <DashboardValueItem
+          label={t("dashboard-page.statistic.AUM")}
+          value={AUM}
+          currency={"GVT"}
+        />
+      </StatisticItemList>
+    </div>
   );
 };
 
--- packages/social-trader/src/pages/dashboard/trading.page.tsx
+++ packages/social-trader/src/pages/dashboard/trading.page.tsx
@@ -1,17 +1,24 @@
 import "shared/components/dashboard/dashboard.scss";
 
+import DashboardPrivate from "pages/dashboard/components/dashboard-trading/dashboard-pirvate";
+import DashboardPublic from "pages/dashboard/components/dashboard-trading/dashboard-public";
+import DashboardTradingTotalContainer from "pages/dashboard/components/dashboard-trading/dashboard-trading-total.container";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Page from "shared/components/page/page";
-
-import DashboardTradingContainer from "./components/dashboard-trading/dashboard-trading.container";
 
 const _TradingPage: React.FC = () => {
   const [t] = useTranslation();
   return (
     <Page title={t(`social-trader.dashboard-page.title`)}>
       <div>
-        <DashboardTradingContainer />
+        <DashboardTradingTotalContainer />
+      </div>
+      <div>
+        <DashboardPublic />
+      </div>
+      <div>
+        <DashboardPrivate />
       </div>
     </Page>
   );
