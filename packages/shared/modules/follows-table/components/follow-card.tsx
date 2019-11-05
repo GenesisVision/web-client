aa0de9278853dd9dbd539a536a624621b5aafaf4
pavelzubov <pasha224@yandex.ru>
2019-11-05 14:05:40 +0300

    Use TableCard components

--- packages/shared/modules/follows-table/components/follow-card.tsx
+++ packages/shared/modules/follows-table/components/follow-card.tsx
@@ -3,30 +3,22 @@ import * as React from "react";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import NumberFormat from "react-number-format";
-import AssetAvatar from "shared/components/avatar/asset-avatar/asset-avatar";
 import GVButton from "shared/components/gv-button";
-import { ActionsCircleIcon } from "shared/components/icon/actions-circle-icon";
-import LevelTooltip from "shared/components/level-tooltip/level-tooltip";
 import Link from "shared/components/link/link";
 import Popover, {
   HORIZONTAL_POPOVER_POS,
   VERTICAL_POPOVER_POS
 } from "shared/components/popover/popover";
-import Profitability from "shared/components/profitability/profitability";
-import {
-  PROFITABILITY_PREFIX,
-  PROFITABILITY_VARIANT
-} from "shared/components/profitability/profitability.helper";
 import ProgramPeriodPie from "shared/components/program-period/program-period-pie/program-period-pie";
-import ProgramSimpleChart from "shared/components/program-simple-chart/program-simple-chart";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
+import TableCard, {
+  TableCardTable,
+  TableCardTableColumn
+} from "shared/components/table/components/table-card/table-card";
 import { TableToggleFavoriteHandlerType } from "shared/components/table/components/table.types";
 import TagProgramContainer from "shared/components/tags/tag-program-container/tag-program-container";
-import useAnchor from "shared/hooks/anchor.hook";
-import {
-  composeFollowDetailsUrl,
-  composeManagerDetailsUrl
-} from "shared/utils/compose-url";
+import { TAnchor, TEvent } from "shared/hooks/anchor.hook";
+import { composeFollowDetailsUrl } from "shared/utils/compose-url";
 import { distanceDate } from "shared/utils/dates";
 import {
   formatValue,
@@ -43,7 +35,6 @@ const DECIMAL_SCALE_SMALL_VALUE = 4;
 const DECIMAL_SCALE_BIG_VALUE = 2;
 
 const _FollowCard: React.FC<Props> = ({ follow, toggleFavorite, title }) => {
-  const { anchor, setAnchor, clearAnchor } = useAnchor();
   const handleToggleFavorite = useCallback(
     () =>
       toggleFavorite(
@@ -58,111 +49,64 @@ const _FollowCard: React.FC<Props> = ({ follow, toggleFavorite, title }) => {
     state: `/ ${title}`
   };
   const requestCurrency = follow.statistic.balance.currency;
+  const renderActions = ({
+    clearAnchor,
+    anchor
+  }: {
+    clearAnchor: (event: TEvent) => void;
+    anchor: TAnchor;
+  }) => (
+    <Popover
+      horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+      vertical={VERTICAL_POPOVER_POS.BOTTOM}
+      anchorEl={anchor}
+      noPadding
+      onClose={clearAnchor}
+    >
+      <div className="popover-list">
+        <Link to={linkProps}>
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("program-actions.details")}
+          </GVButton>
+        </Link>
+        {follow.personalDetails && !follow.personalDetails.isFavorite && (
+          <GVButton
+            variant="text"
+            color="secondary"
+            onClick={handleToggleFavorite}
+          >
+            {t("follow-actions.add-to-favorites")}
+          </GVButton>
+        )}
+        {follow.personalDetails && follow.personalDetails.isFavorite && (
+          <GVButton
+            variant="text"
+            color="secondary"
+            onClick={handleToggleFavorite}
+          >
+            {t("follow-actions.remove-from-favorites")}
+          </GVButton>
+        )}
+      </div>
+    </Popover>
+  );
   return (
-    <div className="table-cards__card">
-      <div className="table-cards__row">
-        <div className="table-cards__avatar">
-          <Link to={linkProps}>
-            <AssetAvatar
-              url={follow.logo}
-              levelProgress={follow.levelProgress}
-              level={follow.level}
-              alt={follow.title}
-              color={follow.color}
-              size="medium"
-              tooltip={<LevelTooltip level={follow.level} canLevelUp={false} />}
-            />
-          </Link>
-        </div>
-        <div className="table-cards__main-info">
-          <div className="table-cards__title-wrapper">
-            <Link className="table-cards__title" to={linkProps}>
-              {follow.title}
-            </Link>
-            <Link
-              className="table-cards__name"
-              to={{
-                pathname: "/managers/[id]",
-                as: composeManagerDetailsUrl(follow.manager.url),
-                state: `/ ${title}`
-              }}
-            >
-              {follow.manager.username}
-            </Link>
-          </div>
-          <div className="table-cards__actions">
-            <ActionsCircleIcon primary={!!anchor} onClick={setAnchor} />
-            <Popover
-              horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
-              vertical={VERTICAL_POPOVER_POS.BOTTOM}
-              anchorEl={anchor}
-              noPadding
-              onClose={clearAnchor}
-            >
-              <div className="popover-list">
-                <Link to={linkProps}>
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={clearAnchor}
-                  >
-                    {t("program-actions.details")}
-                  </GVButton>
-                </Link>
-                {follow.personalDetails && !follow.personalDetails.isFavorite && (
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={handleToggleFavorite}
-                  >
-                    {t("program-actions.add-to-favorites")}
-                  </GVButton>
-                )}
-                {follow.personalDetails && follow.personalDetails.isFavorite && (
-                  <GVButton
-                    variant="text"
-                    color="secondary"
-                    onClick={handleToggleFavorite}
-                  >
-                    {t("program-actions.remove-from-favorites")}
-                  </GVButton>
-                )}
-              </div>
-            </Popover>
-          </div>
-          <TagProgramContainer tags={follow.tags} />
-        </div>
-      </div>
-      <div className="table-cards__row">
-        <div className="table-cards__chart">
-          {follow.chart && (
-            <ProgramSimpleChart data={follow.chart} programId={follow.id} />
-          )}
-        </div>
-        <div className="table-cards__chart-info">
-          <div className="table-cards__profit">
-            <Profitability
-              value={formatValue(follow.statistic.profitPercent, 2)}
-              variant={PROFITABILITY_VARIANT.CHIPS}
-              prefix={PROFITABILITY_PREFIX.ARROW}
-            >
-              <NumberFormat
-                value={formatValue(follow.statistic.profitPercent, 2)}
-                suffix="%"
-                allowNegative={false}
-                displayType="text"
-              />
-            </Profitability>
-          </div>
-        </div>
-      </div>
-      <div className="table-cards__table">
-        <div className="table-cards__table-column">
+    <TableCard
+      asset={follow}
+      detailsUrl={linkProps}
+      pathTitle={title}
+      profit={follow.statistic.profitValue}
+      profitPercent={follow.statistic.profitPercent}
+      renderActions={renderActions}
+      extraBlock={follow.tags && <TagProgramContainer tags={follow.tags} />}
+    >
+      <TableCardTable>
+        <TableCardTableColumn>
           <StatisticItem label={t("follows-page.header.subscribers")}>
             {t("follows-page.header.subscribers")}
           </StatisticItem>
-        </div>
-        <div className="table-cards__table-column">
+        </TableCardTableColumn>
+        <TableCardTableColumn>
           <StatisticItem label={t("follows-page.header.trades")}>
             <NumberFormat
               value={follow.statistic.tradesCount}
@@ -170,8 +114,8 @@ const _FollowCard: React.FC<Props> = ({ follow, toggleFavorite, title }) => {
               decimalScale={0}
             />
           </StatisticItem>
-        </div>
-        <div className="table-cards__table-column">
+        </TableCardTableColumn>
+        <TableCardTableColumn>
           <StatisticItem
             label={t("programs-page.programs-header.available-to-invest")}
           >
@@ -185,9 +129,9 @@ const _FollowCard: React.FC<Props> = ({ follow, toggleFavorite, title }) => {
               suffix={` ${requestCurrency}`}
             />
           </StatisticItem>
-        </div>
-      </div>
-    </div>
+        </TableCardTableColumn>
+      </TableCardTable>
+    </TableCard>
   );
 };
 
