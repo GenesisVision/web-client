41756b2324dfbeda53062c3016c98c6937e032cb
pavelzubov <pasha224@yandex.ru>
2019-11-05 14:11:12 +0300

    Add info

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-public-card.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-public-card.tsx
@@ -1,75 +1,190 @@
-import ProgramDeposit from "modules/program-deposit/program-deposit";
-import ProgramWithdrawContainer from "modules/program-withdraw/program-withdraw-container";
+import ClosePeriodButton from "modules/asset-settings/close-period/close-period-button";
+import DepositWithdrawButtons from "pages/dashboard/components/dashboard-trading/deposit-withdraw-buttons";
 import { TAsset } from "pages/dashboard/dashboard.types";
+import ChangeAccountPasswordButton from "pages/programs/programs-settings/change-password/change-password-trading-account.button";
 import React from "react";
+import NumberFormat from "react-number-format";
 import GVButton from "shared/components/gv-button";
+import Link, { ToType } from "shared/components/link/link";
+import Popover, {
+  HORIZONTAL_POPOVER_POS,
+  VERTICAL_POPOVER_POS
+} from "shared/components/popover/popover";
+import StatisticItem from "shared/components/statistic-item/statistic-item";
 import TableCard, {
-  TableCardRow
+  TableCardTable,
+  TableCardTableColumn
 } from "shared/components/table/components/table-card/table-card";
-import useIsOpen from "shared/hooks/is-open.hook";
+import {
+  ASSET,
+  DECIMAL_SCALE_BIG_VALUE,
+  DECIMAL_SCALE_SMALL_VALUE
+} from "shared/constants/constants";
+import { TAnchor, TEvent } from "shared/hooks/anchor.hook";
 import { useTranslation } from "shared/i18n";
-import { composeProgramDetailsUrl } from "shared/utils/compose-url";
+import {
+  composeProgramDetailsUrl,
+  composeProgramSettingsUrl
+} from "shared/utils/compose-url";
+import { formatValueDifferentDecimalScale } from "shared/utils/formatter";
 
-const _DashboardPublicCard: React.FC<{ asset: TAsset; title: string }> = ({
+const _DashboardPublicCard: React.FC<{ asset: TAsset; title?: string }> = ({
   asset,
   title
 }) => {
-  const [
-    isOpenInvestPopup,
-    setIsOpenInvestPopup,
-    setIsCloseInvestPopup
-  ] = useIsOpen();
-  const [
-    isOpenWithdrawPopup,
-    setIsOpenWithdrawPopup,
-    setIsCloseWithdrawPopup
-  ] = useIsOpen();
   const [t] = useTranslation();
-  const linkProps = {
+  const detailsLink = {
     pathname: composeProgramDetailsUrl(asset.url),
     state: `/ ${title}`
   };
+  const settingsLink = {
+    pathname: composeProgramSettingsUrl(asset.url),
+    state: `/ ${asset.title}`
+  };
+
+  const renderActions = ({
+    anchor,
+    clearAnchor
+  }: {
+    anchor: TAnchor;
+    clearAnchor: (event: TEvent) => void;
+  }) => (
+    <DashboardPublicCardActions
+      anchor={anchor}
+      clearAnchor={clearAnchor}
+      id={asset.id}
+      settingsLink={settingsLink}
+      showChangePassword={asset.type === ASSET.PROGRAM}
+      showClosePeriod={asset.type === ASSET.PROGRAM}
+      showTerminal={asset.type === ASSET.PROGRAM}
+      title={asset.title}
+    />
+  );
+
   return (
     <TableCard
       asset={asset}
-      detailsUrl={linkProps}
+      detailsUrl={detailsLink}
       pathTitle={title}
       profit={asset.statistic.profit}
       profitPercent={asset.statistic.profitPercent}
+      renderActions={renderActions}
     >
-      <TableCardRow>
-        <GVButton
-          className="table-cards__button"
-          onClick={setIsOpenInvestPopup}
-        >
-          {t(`dashboard-page.recommendations.program`)}
-        </GVButton>
-        <GVButton
-          color="secondary"
-          variant="outlined"
-          onClick={setIsOpenWithdrawPopup}
-        >
-          {t("fund-details-page.description.withdraw")}
-        </GVButton>
-      </TableCardRow>
-      <ProgramDeposit
+      <TableCardTable>
+        <TableCardTableColumn>
+          <StatisticItem label={t("programs-page.programs-header.equity")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.balance.amount,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              suffix={` ${asset.statistic.balance.currency}`}
+              displayType="text"
+            />
+          </StatisticItem>
+          {asset.login && (
+            <StatisticItem label={t("dashboard-page.trading.login")}>
+              {asset.login}
+            </StatisticItem>
+          )}
+        </TableCardTableColumn>
+        <TableCardTableColumn>
+          <StatisticItem label={t("dashboard-page.trading.ddown")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.ddown,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              displayType="text"
+            />
+          </StatisticItem>
+        </TableCardTableColumn>
+        <TableCardTableColumn>
+          <StatisticItem label={t("dashboard-page.trading.age")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.age,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              displayType="text"
+            />
+          </StatisticItem>
+          {asset.broker && (
+            <StatisticItem label={t("dashboard-page.trading.broker")}>
+              {asset.broker}
+            </StatisticItem>
+          )}
+        </TableCardTableColumn>
+      </TableCardTable>
+      <DepositWithdrawButtons
+        type={asset.type}
+        id={asset.id}
         currency={asset.currency}
-        open={isOpenInvestPopup}
-        id={asset.id}
-        onClose={setIsCloseInvestPopup}
-        onApply={() => {}}
-      />
-      <ProgramWithdrawContainer
-        open={isOpenWithdrawPopup}
-        id={asset.id}
-        accountCurrency={"GVT"}
-        assetCurrency={asset.currency}
-        onClose={setIsCloseWithdrawPopup}
-        onSubmit={() => {}}
       />
     </TableCard>
   );
 };
+
+const _DashboardPublicCardActions: React.FC<
+  IDashboardPublicCardActionsProps
+> = ({
+  anchor,
+  clearAnchor,
+  settingsLink,
+  id,
+  title,
+  showChangePassword,
+  showTerminal,
+  showClosePeriod
+}) => {
+  const [t] = useTranslation();
+  const terminalLink = {
+    pathname: ""
+  };
+  return (
+    <Popover
+      horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+      vertical={VERTICAL_POPOVER_POS.BOTTOM}
+      anchorEl={anchor}
+      noPadding
+      onClose={clearAnchor}
+    >
+      <div className="popover-list">
+        <Link to={settingsLink}>
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("dashboard-page.trading.actions.settings")}
+          </GVButton>
+        </Link>
+        {showTerminal && (
+          <Link to={terminalLink}>
+            <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+              {t("dashboard-page.trading.actions.terminal")}
+            </GVButton>
+          </Link>
+        )}
+        {showClosePeriod && <ClosePeriodButton id={id} />}
+        {showChangePassword && (
+          <ChangeAccountPasswordButton id={id} title={title} />
+        )}
+      </div>
+    </Popover>
+  );
+};
+
+interface IDashboardPublicCardActionsProps {
+  clearAnchor: (event: TEvent) => void;
+  anchor: TAnchor;
+  settingsLink: ToType;
+  id: string;
+  title: string;
+  showChangePassword: boolean;
+  showTerminal: boolean;
+  showClosePeriod: boolean;
+}
+const DashboardPublicCardActions = React.memo(_DashboardPublicCardActions);
 
 const DashboardPublicCard = React.memo(_DashboardPublicCard);
 export default DashboardPublicCard;
