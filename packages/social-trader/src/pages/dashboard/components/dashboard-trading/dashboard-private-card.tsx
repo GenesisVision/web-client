6ffd915abccee34c57887a9888e47690b3ba22f3
pavelzubov <pasha224@yandex.ru>
2019-11-05 14:10:42 +0300

    Add info

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card.tsx
@@ -1,39 +1,139 @@
+import CloseAssetButton from "modules/asset-settings/close-asset/close-asset-button";
+import DepositWithdrawButtons from "pages/dashboard/components/dashboard-trading/deposit-withdraw-buttons";
 import { TRecommendation } from "pages/dashboard/dashboard.types";
 import * as React from "react";
-import TableCard from "shared/components/table/components/table-card/table-card";
-import useIsOpen from "shared/hooks/is-open.hook";
+import NumberFormat from "react-number-format";
+import GVButton from "shared/components/gv-button";
+import Link from "shared/components/link/link";
+import Popover, {
+  HORIZONTAL_POPOVER_POS,
+  VERTICAL_POPOVER_POS
+} from "shared/components/popover/popover";
+import StatisticItem from "shared/components/statistic-item/statistic-item";
+import TableCard, {
+  TableCardTable,
+  TableCardTableColumn
+} from "shared/components/table/components/table-card/table-card";
+import {
+  DECIMAL_SCALE_BIG_VALUE,
+  DECIMAL_SCALE_SMALL_VALUE
+} from "shared/constants/constants";
+import { TAnchor, TEvent } from "shared/hooks/anchor.hook";
 import { useTranslation } from "shared/i18n";
 import { composeProgramDetailsUrl } from "shared/utils/compose-url";
+import { formatValueDifferentDecimalScale } from "shared/utils/formatter";
+
+const _DashboardPrivateCard: React.FC<Props> = ({ asset, title }) => {
+  const [t] = useTranslation();
+  const detailsLink = {
+    pathname: composeProgramDetailsUrl(asset.url),
+    state: `/ ${title}`
+  };
+  const terminalLink = {
+    pathname: ""
+  };
+  const makeProgramLink = {
+    pathname: ""
+  };
+  const makeSignalAccountLink = {
+    pathname: ""
+  };
+  const renderActions = ({
+    anchor,
+    clearAnchor
+  }: {
+    anchor: TAnchor;
+    clearAnchor: (event: TEvent) => void;
+  }) => (
+    <Popover
+      horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+      vertical={VERTICAL_POPOVER_POS.BOTTOM}
+      anchorEl={anchor}
+      noPadding
+      onClose={clearAnchor}
+    >
+      <div className="popover-list">
+        <Link to={terminalLink}>
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("dashboard-page.trading.actions.terminal")}
+          </GVButton>
+        </Link>
+        <Link to={makeProgramLink}>
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("dashboard-page.trading.actions.make-program")}
+          </GVButton>
+        </Link>
+        <Link to={makeSignalAccountLink}>
+          <GVButton variant="text" color="secondary" onClick={clearAnchor}>
+            {t("dashboard-page.trading.actions.make-signal-account")}
+          </GVButton>
+        </Link>
+        <CloseAssetButton type={asset.type} id={asset.id} variant={"text"} />
+      </div>
+    </Popover>
+  );
+
+  return (
+    <TableCard
+      asset={asset}
+      detailsUrl={detailsLink}
+      pathTitle={title}
+      profit={asset.statistic.profit}
+      profitPercent={asset.statistic.profitPercent}
+      renderActions={renderActions}
+    >
+      <TableCardTable>
+        <TableCardTableColumn>
+          <StatisticItem label={t("programs-page.programs-header.equity")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.balance.amount,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              suffix={` ${asset.statistic.balance.currency}`}
+              displayType="text"
+            />
+          </StatisticItem>
+        </TableCardTableColumn>
+        <TableCardTableColumn>
+          <StatisticItem label={t("dashboard-page.trading.leverage")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.leverage,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              displayType="text"
+            />
+          </StatisticItem>
+        </TableCardTableColumn>
+        <TableCardTableColumn>
+          <StatisticItem label={t("dashboard-page.trading.age")}>
+            <NumberFormat
+              value={formatValueDifferentDecimalScale(
+                asset.statistic.age,
+                DECIMAL_SCALE_SMALL_VALUE,
+                DECIMAL_SCALE_BIG_VALUE
+              )}
+              displayType="text"
+            />
+          </StatisticItem>
+        </TableCardTableColumn>
+      </TableCardTable>
+      <DepositWithdrawButtons
+        type={asset.type}
+        id={asset.id}
+        currency={asset.currency}
+      />
+    </TableCard>
+  );
+};
 
 interface Props {
   asset: TRecommendation;
   title: string;
 }
-
-const _DashboardPrivateCard: React.FC<Props> = ({ asset, title }) => {
-  const [t] = useTranslation();
-  const [
-    isOpenInvestPopup,
-    setIsOpenInvestPopup,
-    setIsCloseInvestPopup
-  ] = useIsOpen();
-  const linkProps = {
-    pathname: composeProgramDetailsUrl(asset.url),
-    state: `/ ${title}`
-  };
-
-  const renderDepositContainer = () => {};
-
-  return (
-    <TableCard
-      asset={asset}
-      detailsUrl={linkProps}
-      pathTitle={title}
-      profit={asset.statistic.profit}
-      profitPercent={asset.statistic.profitPercent}
-    ></TableCard>
-  );
-};
 
 const DashboardPrivateCard = React.memo(_DashboardPrivateCard);
 export default DashboardPrivateCard;
