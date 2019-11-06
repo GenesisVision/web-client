bbb46e5aa036726335b2797400c14215e42d74ea
pavelzubov <pasha224@yandex.ru>
2019-11-06 18:52:56 +0300

    Add account currency

--- packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.container.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.container.tsx
@@ -5,22 +5,29 @@ import DashboardTotal from "pages/dashboard/components/dashboard-total/dashboard
 import { getTotalLoaderData } from "pages/dashboard/dashboard.loaders-data";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
 import useApiRequest from "shared/hooks/api-request.hook";
+import { currencySelector } from "shared/reducers/account-settings-reducer";
 
 import { TDashboardTotal } from "../../dashboard.types";
 import { getTotal } from "../../services/dashboard.service";
 
 const _DashboardTotalContainer: React.FC<Props> = () => {
   const [t] = useTranslation();
+  const currency = useSelector(currencySelector);
   const { data, sendRequest } = useApiRequest<TDashboardTotal>({
     request: getTotal
   });
   useEffect(() => {
-    sendRequest();
+    sendRequest({ currency });
   }, []);
   return (
     <DashboardBlock label={t("dashboard-page.total.title")} all={""}>
-      <DashboardTotal loaderData={getTotalLoaderData()} data={data!} />
+      <DashboardTotal
+        currency={currency}
+        loaderData={getTotalLoaderData()}
+        data={data!}
+      />
     </DashboardBlock>
   );
 };
