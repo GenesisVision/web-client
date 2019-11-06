4490f92301622dad49ec3bfce346b8fad6f023dd
pavelzubov <pasha224@yandex.ru>
2019-11-06 18:53:29 +0300

    Add new data

--- packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.tsx
@@ -1,18 +1,63 @@
 import DashboardStatisticPeriods from "pages/dashboard/components/dashboard-statistic/dashboard-statistic-periods";
+import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { StatisticItemList } from "shared/components/statistic-item-list/statistic-item-list";
 import { withBlurLoader } from "shared/decorators/with-blur-loader";
+import { CurrencyEnum } from "shared/utils/types";
 
 import { TDashboardTotal } from "../../dashboard.types";
 
-const _DashboardTotal: React.FC<Props> = ({ data }) => {
+const _DashboardTotal: React.FC<Props> = ({
+  currency,
+  data: {
+    available,
+    invested,
+    pending,
+    profits: { dayProfit, monthProfit, weekProfit },
+    total
+  }
+}) => {
+  const [t] = useTranslation();
   return (
     <div className="dashboard-total__values">
-      <DashboardStatisticPeriods data={data} withProfitability />
+      <StatisticItemList>
+        <DashboardValueItem
+          label={t("dashboard-page.total.available")}
+          value={available}
+          currency={currency}
+        />
+        <DashboardValueItem
+          label={t("dashboard-page.total.invested")}
+          value={invested}
+          currency={currency}
+        />
+        <DashboardValueItem
+          label={t("dashboard-page.total.pending")}
+          value={pending}
+          currency={currency}
+        />
+        <DashboardValueItem
+          label={t("dashboard-page.total.total")}
+          value={total}
+          currency={currency}
+        />
+      </StatisticItemList>
+      <DashboardStatisticPeriods
+        data={{
+          day: dayProfit,
+          month: monthProfit,
+          week: weekProfit
+        }}
+        currency={currency}
+        withProfitability
+      />
     </div>
   );
 };
 
 interface Props {
+  currency: CurrencyEnum;
   data: TDashboardTotal;
 }
 
