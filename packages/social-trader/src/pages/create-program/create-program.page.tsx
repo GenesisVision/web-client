e39a7028095a322cd769a1c11a888568b5a4af30
pavelzubov <pasha224@yandex.ru>
2019-11-06 14:51:29 +0300

    Update components for GV-1781

--- packages/social-trader/src/pages/create-program/create-program.page.tsx
+++ packages/social-trader/src/pages/create-program/create-program.page.tsx
@@ -1,24 +1,21 @@
 import "components/create-asset/create-asset.scss";
 
-import { Broker } from "gv-api-web";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 import Page from "shared/components/page/page";
 
 import CreateProgramContainer from "./components/create-program.container";
 
-const _CreateProgramPage: React.FC<Props> = ({ brokers }) => {
+const _CreateProgramPage: React.FC<Props> = () => {
   const [t] = useTranslation();
   return (
     <Page title={t("create-program-page.title")}>
-      <CreateProgramContainer brokers={brokers} />
+      <CreateProgramContainer />
     </Page>
   );
 };
 
-interface Props {
-  brokers: Broker[];
-}
+interface Props {}
 
 const CreateProgramPage = React.memo(_CreateProgramPage);
 export default CreateProgramPage;
