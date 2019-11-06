5f1b8c6ad7069b1fa9ffa48fd095f64a41263497
pavelzubov <pasha224@yandex.ru>
2019-11-06 20:26:30 +0300

    Change field names

--- packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-investing-statistic.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-investing-statistic.tsx
@@ -1,40 +1,41 @@
 import DashboardStatisticContainer from "pages/dashboard/components/dashboard-statistic/dashboard-statistic.container";
 import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
-import {
-  TDashboardInvestingStatistic,
-  TDashboardProgramsStatistic
-} from "pages/dashboard/dashboard.types";
+import { TDashboardInvestingStatistic } from "pages/dashboard/dashboard.types";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { currencySelector } from "shared/reducers/account-settings-reducer";
 
 import { getTotalInvestingStatistic } from "../../services/dashboard.service";
 
 const _DashboardInvestingStatistic: React.FC<Props> = () => {
+  const currency = useSelector(currencySelector);
   const [t] = useTranslation();
   return (
     <DashboardStatisticContainer
+      currency={currency}
       label={t("dashboard-page.statistic.investing")}
       request={getTotalInvestingStatistic}
       renderValues={({
-        balance,
-        programs,
-        funds
+        fundsCount,
+        programsCount,
+        equity
       }: TDashboardInvestingStatistic) => (
         <>
           <DashboardValueItem
             label={t("dashboard-page.statistic.balance")}
-            value={balance}
-            currency={"GVT"}
+            value={equity}
+            currency={currency}
           />
           <DashboardValueItem
             label={t("dashboard-page.statistic.programs")}
-            value={programs}
-            currency={"GVT"}
+            value={programsCount}
+            currency={currency}
           />
           <DashboardValueItem
             label={t("dashboard-page.statistic.funds")}
-            value={funds}
-            currency={"GVT"}
+            value={fundsCount}
+            currency={currency}
           />
         </>
       )}
