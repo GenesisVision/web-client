644e6ce0926e62e1c9f7f08540689c7bccd3a359
pavelzubov <pasha224@yandex.ru>
2019-11-06 19:10:11 +0300

    Add PieContainers

--- packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-total/dashboard-total.tsx
@@ -2,8 +2,15 @@ import DashboardStatisticPeriods from "pages/dashboard/components/dashboard-stat
 import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import GVColors from "shared/components/gv-styles/gv-colors";
+import PieContainer from "shared/components/pie-container/pie-container";
 import { StatisticItemList } from "shared/components/statistic-item-list/statistic-item-list";
+import {
+  $pieAvailableColor,
+  $piePendingColor
+} from "shared/components/wallet/components/wallet-balance/wallet-balance-elements";
 import { withBlurLoader } from "shared/decorators/with-blur-loader";
+import { getPercentageValue } from "shared/utils/helpers";
 import { CurrencyEnum } from "shared/utils/types";
 
 import { TDashboardTotal } from "../../dashboard.types";
@@ -21,25 +28,40 @@ const _DashboardTotal: React.FC<Props> = ({
   const [t] = useTranslation();
   return (
     <div className="dashboard-total__values">
-      <StatisticItemList>
+      <StatisticItemList className="dashboard-total__profits">
         <DashboardValueItem
-          label={t("dashboard-page.total.available")}
-          value={available}
+          label={t("dashboard-page.total.total")}
+          value={total}
           currency={currency}
+        />
+        <PieContainer
+          value={getPercentageValue(invested, total)}
+          label={`${getPercentageValue(invested, total)} %`}
+          color={GVColors.$primaryColor}
         />
         <DashboardValueItem
           label={t("dashboard-page.total.invested")}
           value={invested}
           currency={currency}
         />
+        <PieContainer
+          value={getPercentageValue(pending, total)}
+          label={`${getPercentageValue(pending, total)} %`}
+          color={$piePendingColor}
+        />
         <DashboardValueItem
           label={t("dashboard-page.total.pending")}
           value={pending}
           currency={currency}
         />
+        <PieContainer
+          value={getPercentageValue(available, total)}
+          label={`${getPercentageValue(available, total)} %`}
+          color={$pieAvailableColor}
+        />
         <DashboardValueItem
-          label={t("dashboard-page.total.total")}
-          value={total}
+          label={t("dashboard-page.total.available")}
+          value={available}
           currency={currency}
         />
       </StatisticItemList>
