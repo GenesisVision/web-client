49ef6cb605fb5607c80b64aae17e9232a2ca1273
pavelzubov <pasha224@yandex.ru>
2019-11-06 14:31:52 +0300

    Change index to [id] Remove brokers, add programDetails dispatch

--- packages/social-trader/pages/create-program/[id].tsx
+++ packages/social-trader/pages/create-program/[id].tsx
@@ -1,35 +1,40 @@
-import { Broker } from "gv-api-web";
+import { ProgramDetailsFullOld } from "gv-api-web";
 import CreateProgramPage from "pages/create-program/create-program.page";
-import { fetchBrokers } from "pages/create-program/services/create-program.service";
 import React from "react";
 import { compose } from "redux";
 import platformActions from "shared/actions/platform-actions";
+import { dispatchProgramDescription } from "shared/components/programs/program-details/services/program-details.service";
 import { fetchWalletsWithCtx } from "shared/components/wallet/services/wallet.services";
 import withDefaultLayout from "shared/decorators/with-default-layout";
 import withPrivateRoute from "shared/decorators/with-private-route";
 import { NextPageWithRedux } from "shared/utils/types";
 
-const CreateProgram: NextPageWithRedux<Props, {}> = ({ brokers }) => {
-  return <CreateProgramPage brokers={brokers} />;
+const CreateProgram: NextPageWithRedux<{}, {}> = () => {
+  return <CreateProgramPage />;
 };
 
 CreateProgram.getInitialProps = async ctx => {
-  let brokers;
   await Promise.all([
+    ctx.reduxStore.dispatch(dispatchProgramDescription(ctx)),
     ctx.reduxStore.dispatch(
       async dispatch => await dispatch(platformActions.fetchPlatformSettings())
     ),
-    ctx.reduxStore.dispatch(fetchWalletsWithCtx(ctx)),
-    fetchBrokers().then(res => (brokers = res))
-  ]);
-  return { brokers };
+    ctx.reduxStore.dispatch(fetchWalletsWithCtx(ctx))
+  ]).then(([descriptionResult]) => {
+    const description = ((descriptionResult as unknown) as {
+      value: ProgramDetailsFullOld;
+    }).value;
+    console.log(description.personalProgramDetails);
+    if (
+      !description.personalProgramDetails ||
+      !description.personalProgramDetails.isOwnProgram
+    )
+      throw new Error();
+  });
+  return {};
 };
 
 export default compose(
   withDefaultLayout,
   withPrivateRoute
 )(CreateProgram);
-
-interface Props {
-  brokers: Broker[];
-}
