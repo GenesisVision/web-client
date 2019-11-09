572ff22f3b9a822ae837c731652893f00550d568
pavelzubov <pasha224@yandex.ru>
2019-11-09 14:03:06 +0300

    Set real methods

--- packages/social-trader/src/pages/attach-account/services/attach-account.service.ts
+++ packages/social-trader/src/pages/attach-account/services/attach-account.service.ts
@@ -1,13 +1,13 @@
+import assetsApi from "shared/services/api-client/assets-api";
+import brokersApi from "shared/services/api-client/brokers-api";
+import authService from "shared/services/auth-service";
+
 import { IAttachAccountSettingsFormValues } from "../components/attach-account-settings/attach-account-settings";
 
-export const attachAccount = ({
-  values
-}: {
-  values: IAttachAccountSettingsFormValues;
-}) => {
-  return Promise.resolve();
-};
+export const attachAccount = (request: IAttachAccountSettingsFormValues) =>
+  assetsApi.createExternalTradingAccount(authService.getAuthArg(), {
+    request
+  });
 
-export const fetchExchanges = () => {
-  return Promise.resolve(["Binance"]);
-};
+export const fetchExchanges = () =>
+  brokersApi.getBrokersExternal().then(({ brokers }) => brokers);
