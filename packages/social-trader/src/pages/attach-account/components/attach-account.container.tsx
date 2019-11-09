7a7f908a77ff14b78d13283a5ff352178f0e8a6d
pavelzubov <pasha224@yandex.ru>
2019-11-09 14:02:55 +0300

    Add types Add push TRADING_ROUTE on success

--- packages/social-trader/src/pages/attach-account/components/attach-account.container.tsx
+++ packages/social-trader/src/pages/attach-account/components/attach-account.container.tsx
@@ -1,22 +1,39 @@
+import { Broker } from "gv-api-web";
 import {
   attachAccount,
   fetchExchanges
 } from "pages/attach-account/services/attach-account.service";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { Push } from "shared/components/link/link";
 import useApiRequest from "shared/hooks/api-request.hook";
+import { TRADING_ROUTE } from "shared/routes/dashboard.routes";
+import { SetSubmittingType } from "shared/utils/types";
 
-import AttachAccountSettings from "./attach-account-settings/attach-account-settings";
+import AttachAccountSettings, {
+  IAttachAccountSettingsFormValues
+} from "./attach-account-settings/attach-account-settings";
 
-const _AttachAccountPage: React.FC<Props> = ({}) => {
+const _AttachAccountPage: React.FC<Props> = () => {
   const [t] = useTranslation();
-  const { sendRequest: attach } = useApiRequest({ request: attachAccount });
-  const { sendRequest: getExchanges, data: exchanges } = useApiRequest({
+  const { sendRequest: attach } = useApiRequest({
+    request: attachAccount
+  });
+  const { sendRequest: getExchanges, data: exchanges } = useApiRequest<
+    Broker[]
+  >({
     request: fetchExchanges
   });
   useEffect(() => {
     getExchanges();
   }, []);
+  const handleSubmit = useCallback(
+    (
+      values: IAttachAccountSettingsFormValues,
+      setSubmitting: SetSubmittingType
+    ) => attach(values, setSubmitting).then(() => Push(TRADING_ROUTE)),
+    []
+  );
   return (
     <>
       <div className="create-asset__header">
@@ -24,7 +41,7 @@ const _AttachAccountPage: React.FC<Props> = ({}) => {
       </div>
       <div className="create-asset__content">
         <AttachAccountSettings
-          onSubmit={attach}
+          onSubmit={handleSubmit}
           data={exchanges!}
           loaderData={[]}
         />
