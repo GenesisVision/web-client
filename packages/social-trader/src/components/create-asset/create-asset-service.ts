86164e0e0f10f4ea098f67cc191277d997b1b050
pavelzubov <pasha224@yandex.ru>
2019-11-09 14:00:48 +0300

    Use real api

--- packages/social-trader/src/components/create-asset/create-asset-service.ts
+++ packages/social-trader/src/components/create-asset/create-asset-service.ts
@@ -1,13 +1,12 @@
 import {
   CancelablePromise,
-  NewFundRequest
-  // NewProgramRequest,
-  // ProgramCreateResult
+  NewFundRequest,
+  NewTradingAccountRequest
 } from "gv-api-web";
 import { ICreateAccountSettingsFormValues } from "pages/create-account/components/create-account-settings/create-account-settings";
 import { ICreateFundSettingsFormValues } from "pages/create-fund/components/create-fund-settings/create-fund-settings";
 import { ICreateProgramSettingsFormValues } from "pages/create-program/components/create-program-settings/create-program-settings";
-import { ASSET } from "shared/constants/constants";
+import { CREATE_ASSET } from "shared/constants/constants";
 import assetsApi from "shared/services/api-client/assets-api";
 //import managerApi from "shared/services/api-client/manager-api";
 import authService from "shared/services/auth-service";
@@ -18,13 +17,12 @@ export type ICreateAssetSettingsFormValues =
   | ICreateFundSettingsFormValues
   | ICreateAccountSettingsFormValues;
 
-export type NewAssetRequest = any; //NewFundRequest | NewProgramRequest;
+export type NewAssetRequest = NewFundRequest | NewTradingAccountRequest;
 
 export const createAsset = (
   createAssetData: ICreateAssetSettingsFormValues,
-  asset: ASSET
+  asset: CREATE_ASSET
 ): CancelablePromise<any> => {
-  //TODO ProgramCreateResult | null
   const authorization = authService.getAuthArg();
   let promise = Promise.resolve("") as CancelablePromise<any>;
   if ("logo" in createAssetData && createAssetData.logo.image) {
@@ -43,27 +41,21 @@ export const createAsset = (
 };
 
 const getCreateMethod = (
-  asset: ASSET
-  // @ts-ignore
+  asset: CREATE_ASSET
 ): ((request: NewAssetRequest) => CancelablePromise<any>) => {
-  //TODO ProgramCreateResult | null
+  const authorization = authService.getAuthArg();
   switch (asset) {
-    case "ACCOUNT" as ASSET:
-      return (request: NewAssetRequest) => new CancelablePromise<any>(() => {});
-    // return (request: NewAssetRequest) =>
-    //   (Promise.resolve() as unknown) as CancelablePromise<
-    //     ManagerProgramCreateResult
-    //   >;
-    case ASSET.PROGRAM:
-      return (request: NewAssetRequest) => new CancelablePromise<any>(() => {});
-    // assetsApi.createProgram(authService.getAuthArg(), {
-    //   request: request //as NewProgramRequest
-    // });
+    case CREATE_ASSET.ACCOUNT:
+      return (request: NewAssetRequest) =>
+        assetsApi.createTradingAccount(authorization, {
+          request: request as NewTradingAccountRequest
+        });
+    case CREATE_ASSET.FUND:
     default:
-      return (request: NewAssetRequest) => new CancelablePromise<any>(() => {});
-    // managerApi.createFund(authService.getAuthArg(), {
-    //   request: request as NewFundRequest
-    // });
+      return (request: NewAssetRequest) =>
+        assetsApi.createFund(authorization, {
+          request: request as NewFundRequest
+        });
   }
 };
 
