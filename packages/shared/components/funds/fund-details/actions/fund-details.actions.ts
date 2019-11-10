414b4e1ec27e6139409ffeb1336894f8ed131490
pavelzubov <pasha224@yandex.ru>
2019-11-10 21:53:39 +0300

    Add new api method Hide old method

--- packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
+++ packages/shared/components/funds/fund-details/actions/fund-details.actions.ts
@@ -1,21 +1,13 @@
-import {
-  CancelablePromise,
-  FundBalanceChart,
-  FundDetailsFull,
-  FundProfitChart,
-  ReallocationsViewModel
-} from "gv-api-web";
+import { FundBalanceChart, FundDetailsFull } from "gv-api-web";
 import {
   ChartDefaultPeriod,
   getDefaultPeriod
 } from "shared/components/chart/chart-period/chart-period.helpers";
 import { TStatisticCurrencyAction } from "shared/components/details/reducers/statistic-currency.reducer";
 import { TStatisticPeriodAction } from "shared/components/details/reducers/statistic-period.reducer";
-import { FilteringType } from "shared/components/table/components/filtering/filter.type";
 import fundsApi from "shared/services/api-client/funds-api";
 import { ActionType, ApiAction, CurrencyEnum } from "shared/utils/types";
 
-import { FundAssetsViewModel } from "../reducers/fund-history.reducer";
 import { FundIdState } from "../reducers/id.reducer";
 import { FundProfitChartDataType } from "../reducers/profit-chart.reducer";
 
@@ -29,27 +21,17 @@ export const SET_FUND_ID = "SET_FUND_ID";
 export const FUND_REALLOCATE_HISTORY = "FUND_REALLOCATE_HISTORY";
 export const FUND_STRUCTURE = "FUND_STRUCTURE";
 
-const sendFundChartRequest = (
-  { start, end }: ChartDefaultPeriod,
-  id: string,
-  currency: CurrencyEnum
-): CancelablePromise<FundProfitChart> =>
-  fundsApi.getFundProfitChart(id, {
-    dateFrom: start,
-    dateTo: end,
-    maxPointCount: 100,
-    currency
-  });
-
 export const fetchFundProfitChartAction = (
   id: string,
   period = getDefaultPeriod(),
   currencies: CurrencyEnum[]
 ): ApiAction<FundProfitChartDataType> => ({
   type: FETCH_FUND_PROFIT_CHART,
-  payload: Promise.all(
-    currencies.map(currency => sendFundChartRequest(period, id, currency))
-  ) as CancelablePromise<FundProfitChartDataType>
+  payload: fundsApi.getFundProfitChart(id, {
+    dateFrom: period.start,
+    dateTo: period.end,
+    currencies
+  })
 });
 
 export const fetchFundBalanceChartAction = (
@@ -88,22 +70,13 @@ export const statisticPeriodAction = (
   payload: period
 });
 
-export const fundReallocateHistoryAction = (
+/*export const fundReallocateHistoryAction = (
   fundId: string,
   filters?: FilteringType
 ): ApiAction<ReallocationsViewModel> => ({
   type: FUND_REALLOCATE_HISTORY,
   payload: fundsApi.getReallocationsHistory(fundId, filters)
-});
-
-// export const fundStructureAction = (
-//   fundId: string
-// ): ApiAction<FundAssetsViewModel> => ({
-//   type: FUND_STRUCTURE,
-//   payload: fundsApi
-//     .getFundAssets(fundId)
-//     .then(data => ({ ...data, total: data.assets.length }))
-// });
+});*/
 
 export interface SetFundIdAction extends ActionType<FundIdState> {
   type: typeof SET_FUND_ID;
