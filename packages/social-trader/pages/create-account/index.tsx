b55fbcb50e68d022cbfe468a7870b65bc14974ec
pavelzubov <pasha224@yandex.ru>
2019-11-10 11:14:01 +0300

    Move fetchBrokers to asset service

--- packages/social-trader/pages/create-account/index.tsx
+++ packages/social-trader/pages/create-account/index.tsx
@@ -1,6 +1,6 @@
+import { fetchBrokers } from "components/assets/asset.service";
 import { Broker } from "gv-api-web";
 import CreateAccountPage from "pages/create-account/create-account.page";
-import { fetchBrokers } from "pages/create-program/services/create-program.service";
 import React from "react";
 import { compose } from "redux";
 import platformActions from "shared/actions/platform-actions";
@@ -9,11 +9,11 @@ import withDefaultLayout from "shared/decorators/with-default-layout";
 import withPrivateRoute from "shared/decorators/with-private-route";
 import { NextPageWithRedux } from "shared/utils/types";
 
-const CreateProgram: NextPageWithRedux<Props, {}> = ({ brokers }) => {
+const Page: NextPageWithRedux<Props, {}> = ({ brokers }) => {
   return <CreateAccountPage brokers={brokers} />;
 };
 
-CreateProgram.getInitialProps = async ctx => {
+Page.getInitialProps = async ctx => {
   let brokers;
   await Promise.all([
     ctx.reduxStore.dispatch(
@@ -25,11 +25,11 @@ CreateProgram.getInitialProps = async ctx => {
   return { brokers };
 };
 
-export default compose(
-  withDefaultLayout,
-  withPrivateRoute
-)(CreateProgram);
-
 interface Props {
   brokers: Broker[];
 }
+
+export default compose(
+  withDefaultLayout,
+  withPrivateRoute
+)(Page);
