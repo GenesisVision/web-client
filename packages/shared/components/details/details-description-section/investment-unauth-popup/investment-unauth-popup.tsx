d146836418dce10d3e08a55681f623dd3652cbf3
pavelzubov <pasha224@yandex.ru>
2019-11-11 15:27:58 +0300

    Remove old routes

--- packages/shared/components/details/details-description-section/investment-unauth-popup/investment-unauth-popup.tsx
+++ packages/shared/components/details/details-description-section/investment-unauth-popup/investment-unauth-popup.tsx
@@ -9,8 +9,6 @@ import Dialog, { IDialogProps } from "shared/components/dialog/dialog";
 import { DialogBottom } from "shared/components/dialog/dialog-bottom";
 import { DialogButtons } from "shared/components/dialog/dialog-buttons";
 import GVButton from "shared/components/gv-button";
-import { ROLE } from "shared/constants/constants";
-import useRole from "shared/hooks/use-role.hook";
 import { LOGIN_ROUTE, SIGNUP_ROUTE } from "shared/routes/app.routes";
 
 const _InvestmentUnauthPopup: React.FC<Props> = ({
@@ -24,16 +22,6 @@ const _InvestmentUnauthPopup: React.FC<Props> = ({
   message
 }) => {
   const [t] = useTranslation();
-  const role = useRole();
-  const baseUrl =
-    role === ROLE.MANAGER
-      ? process.env.REACT_APP_INVESTOR_PORTAL_URL
-      : process.env.NODE_ENV === "development"
-      ? ``
-      : `/${ROLE.INVESTOR}`;
-
-  const loginUrl = `${baseUrl}${LOGIN_ROUTE}`;
-  const signUpUrl = `${baseUrl}${SIGNUP_ROUTE}`;
 
   return (
     <Dialog open={open} onClose={onClose}>
@@ -47,10 +35,10 @@ const _InvestmentUnauthPopup: React.FC<Props> = ({
       <DialogBottom>
         <p className="unauth-popup__message">{message}</p>
         <DialogButtons>
-          <a href={loginUrl}>
+          <a href={LOGIN_ROUTE}>
             <GVButton>{t("auth.login.title")}</GVButton>
           </a>
-          <a href={signUpUrl}>
+          <a href={SIGNUP_ROUTE}>
             <GVButton>{t("auth.signup.title")}</GVButton>
           </a>
         </DialogButtons>
