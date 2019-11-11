f0dcfb6b6ef9b76ec5cc6dfc79c557c8eccd53ac
pavelzubov <pasha224@yandex.ru>
2019-11-11 15:31:41 +0300

    Use ProgramDetailsContainer instead ProgramDetailsPageCommon

--- packages/social-trader/src/pages/programs/program-details/program-details.page.tsx
+++ packages/social-trader/src/pages/programs/program-details/program-details.page.tsx
@@ -1,16 +1,25 @@
-import ProgramWithdrawContainer from "modules/program-withdraw/program-withdraw-container";
-import React from "react";
-import ProgramDetailsPageCommon from "shared/components/programs/program-details/program-details.page";
-
-import ProgramControls from "./components/program-controls/program-controls";
+import { statisticCurrencyAction } from "pages/programs/program-details/actions/program-details.actions";
+import ProgramDetailsContainer from "pages/programs/program-details/components/program-details.contaner";
+import { programDescriptionSelector } from "pages/programs/program-details/reducers/description.reducer";
+import {
+  dispatchPlatformLevelsParameters,
+  dispatchProgramDescription
+} from "pages/programs/program-details/service/program-details.service";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const _ProgramDetailsPage: React.FC = () => {
-  const descriptionSection = {
-    Controls: ProgramControls,
-    WithdrawContainer: ProgramWithdrawContainer
-  };
-
-  return <ProgramDetailsPageCommon descriptionSection={descriptionSection} />;
+  const dispatch = useDispatch();
+  const description = useSelector(programDescriptionSelector);
+  useEffect(() => {
+    dispatch(dispatchProgramDescription());
+  }, []);
+  useEffect(() => {
+    if (!description) return;
+    dispatch(dispatchPlatformLevelsParameters(description.currency));
+    dispatch(statisticCurrencyAction(description.currency));
+  }, [description]);
+  return <ProgramDetailsContainer data={description!} />;
 };
 
 const ProgramDetailsPage = React.memo(_ProgramDetailsPage);
