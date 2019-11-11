ee8686d81f7a1f14a4235f4773cb15cf209ca2cf
pavelzubov <pasha224@yandex.ru>
2019-11-11 21:19:46 +0300

    Use direct FundDetailsContainer

--- packages/social-trader/src/pages/funds/fund-details/fund-details.page.tsx
+++ packages/social-trader/src/pages/funds/fund-details/fund-details.page.tsx
@@ -1,18 +1,20 @@
 import "shared/components/details/details.scss";
 
-import FundWithdrawalContainer from "modules/fund-withdrawal/fund-withdrawal-container";
 import * as React from "react";
-import FundDetailsPageCommon from "shared/components/funds/fund-details/fund-details.page";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import InvestmentFundControls from "./components/investment-fund-controls";
+import FundDetailsContainer from "./fund-details.container";
+import { fundDescriptionSelector } from "./reducers/description.reducer";
+import { dispatchFundDescription } from "./services/fund-details.service";
 
 const _FundDetailsPage: React.FC = () => {
-  const descriptionSection = {
-    WithdrawContainer: FundWithdrawalContainer,
-    Controls: InvestmentFundControls
-  };
-
-  return <FundDetailsPageCommon descriptionSection={descriptionSection} />;
+  const dispatch = useDispatch();
+  const description = useSelector(fundDescriptionSelector);
+  useEffect(() => {
+    dispatch(dispatchFundDescription());
+  }, []);
+  return <FundDetailsContainer data={description!} />;
 };
 
 const FundDetailsPage = React.memo(_FundDetailsPage);
