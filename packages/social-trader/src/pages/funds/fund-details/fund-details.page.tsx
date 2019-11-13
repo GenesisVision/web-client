1aa1423653f7a37c63b4da508368bd2c7c4e6657
pavelzubov <pasha224@yandex.ru>
2019-11-13 13:10:40 +0300

    Remove dispatch details

--- packages/social-trader/src/pages/funds/fund-details/fund-details.page.tsx
+++ packages/social-trader/src/pages/funds/fund-details/fund-details.page.tsx
@@ -1,19 +1,13 @@
 import "shared/components/details/details.scss";
 
 import * as React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import FundDetailsContainer from "./fund-details.container";
 import { fundDescriptionSelector } from "./reducers/description.reducer";
-import { dispatchFundDescription } from "./services/fund-details.service";
 
 const _FundDetailsPage: React.FC = () => {
-  const dispatch = useDispatch();
   const description = useSelector(fundDescriptionSelector);
-  useEffect(() => {
-    dispatch(dispatchFundDescription());
-  }, []);
   return <FundDetailsContainer data={description!} />;
 };
 
