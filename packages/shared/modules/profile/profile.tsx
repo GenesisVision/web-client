c9a6fee9f9b40d4854800fcd2d927c10d5f52f11
pavelzubov <pasha224@yandex.ru>
2019-11-17 16:48:35 +0300

    Add useCallback Remove role

--- packages/shared/modules/profile/profile.tsx
+++ packages/shared/modules/profile/profile.tsx
@@ -5,42 +5,36 @@ import "./profile.scss";
 import copy from "copy-to-clipboard";
 import { ProfileFullViewModel } from "gv-api-web";
 import * as React from "react";
+import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import GVButton from "shared/components/gv-button";
 import ProfileImageContainer from "shared/components/profile/settings/profile-image/profile-image-container";
 import SettingsBlock from "shared/components/settings-block/settings-block";
 import StatisticItem from "shared/components/statistic-item/statistic-item";
-import { ROLE } from "shared/constants/constants";
 import withLoader from "shared/decorators/with-loader";
-import useRole from "shared/hooks/use-role.hook";
 import PublicInfo from "shared/modules/public-info/public-info";
 
 const _Profile: React.FC<Props> = ({ info, notifySuccess, onSuccessEdit }) => {
   const [t] = useTranslation();
-  const role = useRole();
-  const onCopy = () => {
+  const onCopy = useCallback(() => {
     copy(info.id);
     notifySuccess(t("profile-page.success-copy"));
-  };
+  }, [info.id]);
   return (
     <>
-      {role === ROLE.MANAGER && (
-        <>
-          <SettingsBlock label={t("profile-page.public-info")}>
-            <PublicInfo
-              about={info.about}
-              userName={info.userName}
-              onSuccessEdit={onSuccessEdit}
-            />
-          </SettingsBlock>
-          <SettingsBlock label={t("profile-page.id")}>
-            <div className="profile__content">
-              <div>{info.id}</div>
-              <GVButton onClick={onCopy}>{t("buttons.copy")}</GVButton>
-            </div>
-          </SettingsBlock>
-        </>
-      )}
+      <SettingsBlock label={t("profile-page.public-info")}>
+        <PublicInfo
+          about={info.about}
+          userName={info.userName}
+          onSuccessEdit={onSuccessEdit}
+        />
+      </SettingsBlock>
+      <SettingsBlock label={t("profile-page.id")}>
+        <div className="profile__content">
+          <div>{info.id}</div>
+          <GVButton onClick={onCopy}>{t("buttons.copy")}</GVButton>
+        </div>
+      </SettingsBlock>
       <SettingsBlock label={t("profile-page.settings.profile-image")}>
         <ProfileImageContainer />
       </SettingsBlock>
