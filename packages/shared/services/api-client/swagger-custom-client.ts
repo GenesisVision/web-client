7523b2c113325c3b75c2ea4ae7bf089dfeef0aaa
Евгений Гребенщиков <evgreb@gmail.com>
2019-11-22 13:07:38 +0300

    remove services from shared

--- packages/shared/services/api-client/swagger-custom-client.ts
+++
@@ -1,7 +0,0 @@
-import { ApiClient } from "gv-api-web";
-
-const apiClient = ApiClient.instance;
-//@ts-ignore TODO fix types
-apiClient.basePath = process.env.REACT_APP_API_URL;
-
-export default apiClient;
