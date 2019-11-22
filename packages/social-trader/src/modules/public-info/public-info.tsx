e67fc0d7e77efc17a84fb8bca163308cd465d59a
pavelzubov <pasha224@yandex.ru>
2019-11-22 13:36:37 +0300

    Add isPending prop

--- packages/social-trader/src/modules/public-info/public-info.tsx
+++ packages/social-trader/src/modules/public-info/public-info.tsx
@@ -5,7 +5,12 @@ import { SetSubmittingType } from "utils/types";
 import PublicInfoForm, { IAboutFormValues } from "./public-info-form";
 import { updateProfile } from "./public-info.service";
 
-const _PublicInfo: React.FC<Props> = ({ userName, about, onUpdate }) => {
+const _PublicInfo: React.FC<Props> = ({
+  userName,
+  about,
+  onUpdate,
+  isPending
+}) => {
   const { sendRequest, errorMessage } = useApiRequest({
     request: updateProfile,
     successMessage: "profile-page.success-edit"
@@ -22,6 +27,7 @@ const _PublicInfo: React.FC<Props> = ({ userName, about, onUpdate }) => {
   );
   return (
     <PublicInfoForm
+      isPending={isPending}
       userName={userName}
       about={about}
       onSubmit={handleSubmit}
@@ -31,6 +37,7 @@ const _PublicInfo: React.FC<Props> = ({ userName, about, onUpdate }) => {
 };
 
 interface Props {
+  isPending: boolean;
   onUpdate: () => void;
   userName: string;
   about: string;
