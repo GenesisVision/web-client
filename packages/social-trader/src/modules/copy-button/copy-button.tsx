c331c66f8f454fa67c5d12b206f30ba480f1f54e
pavelzubov <pasha224@yandex.ru>
2019-11-23 19:31:01 +0300

    Create CopyButton component

---
+++ packages/social-trader/src/modules/copy-button/copy-button.tsx
@@ -0,0 +1,35 @@
+import GVButton from "components/gv-button";
+import CopyIcon from "components/icon/copy-icon";
+import useCopy from "hooks/copy.hook";
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+
+const _CopyButton: React.FC<Props> = ({ value, successMessage, text }) => {
+  const [t] = useTranslation();
+  const copy = useCopy(successMessage);
+  const onCopy = useCallback(() => {
+    copy(value);
+  }, [value]);
+  return (
+    <GVButton
+      color="secondary"
+      onClick={onCopy}
+      variant={text ? "text" : undefined}
+    >
+      <>
+        <CopyIcon />
+        &nbsp;
+        {t("buttons.copy")}
+      </>
+    </GVButton>
+  );
+};
+
+interface Props {
+  text?: boolean;
+  successMessage?: string;
+  value: string;
+}
+
+const CopyButton = React.memo(_CopyButton);
+export default CopyButton;
