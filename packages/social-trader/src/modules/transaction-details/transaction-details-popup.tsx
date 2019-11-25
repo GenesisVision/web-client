539f4565cec416d499fa7be6eb2d88f851c567b1
pavelzubov <pasha224@yandex.ru>
2019-11-25 10:10:05 +0300

    Reformat

--- packages/social-trader/src/modules/transaction-details/transaction-details-popup.tsx
+++ packages/social-trader/src/modules/transaction-details/transaction-details-popup.tsx
@@ -1,25 +1,30 @@
 import Dialog from "components/dialog/dialog";
+import { MultiWalletTransaction } from "components/wallet/wallet.types";
 import * as React from "react";
 
 import TransactionDetailsDialog from "./transaction-details-dialog";
 
-const TransactionDetailsPopup: React.FC<ITransactionDetailsProps> = React.memo(
-  ({ open, onClose, transactionId, onAction }) => (
-    <Dialog open={open} onClose={onClose} className="transaction-details">
-      <TransactionDetailsDialog
-        transactionId={transactionId}
-        close={onClose}
-        onAction={onAction}
-      />
-    </Dialog>
-  )
+const _TransactionDetailsPopup: React.FC<ITransactionDetailsProps> = ({
+  open,
+  onClose,
+  transaction,
+  onAction
+}) => (
+  <Dialog open={open} onClose={onClose} className="transaction-details">
+    <TransactionDetailsDialog
+      transaction={transaction}
+      close={onClose}
+      onAction={onAction}
+    />
+  </Dialog>
 );
 
 interface ITransactionDetailsProps {
   transaction: MultiWalletTransaction;
   open: boolean;
-  onClose(): void;
-  onAction(): void;
+  onClose: () => void;
+  onAction: () => void;
 }
 
+const TransactionDetailsPopup = React.memo(_TransactionDetailsPopup);
 export default TransactionDetailsPopup;
