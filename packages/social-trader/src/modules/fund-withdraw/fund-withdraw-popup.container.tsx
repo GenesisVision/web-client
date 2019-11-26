0c81444e09d7ffccd535935f2be89ecd6a512070
pavelzubov <pasha224@yandex.ru>
2019-11-26 09:35:25 +0300

    Split FundWithdrawDialog to two files Use fetchOnMount

---
+++ packages/social-trader/src/modules/fund-withdraw/fund-withdraw-popup.container.tsx
@@ -0,0 +1,35 @@
+import FormError from "components/form/form-error/form-error";
+import useApiRequest from "hooks/api-request.hook";
+import * as React from "react";
+
+import {
+  FundWithdrawPopup,
+  IFundWithdrawPopupProps
+} from "./fund-withdraw-popup";
+import { FundWithdrawLoaderData } from "./fund-withdraw.loader";
+import { getFundWithdrawInfo } from "./services/fund-withdraw.services";
+
+const _FundWithdrawPopupContainer: React.FC<IFundWithdrawPopupProps> = ({
+  id,
+  onClose
+}) => {
+  const { data, errorMessage } = useApiRequest({
+    request: () => getFundWithdrawInfo({ id }),
+    fetchOnMount: true
+  });
+  return (
+    <>
+      <FundWithdrawPopup
+        onClose={onClose}
+        id={id}
+        loaderData={FundWithdrawLoaderData}
+        data={data!}
+      />
+      <FormError error={errorMessage} />
+    </>
+  );
+};
+
+export const FundWithdrawPopupContainer = React.memo(
+  _FundWithdrawPopupContainer
+);
