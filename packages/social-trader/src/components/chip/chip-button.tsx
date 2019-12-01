49d594e5e0a3c2e4d64e3fd6fe1714bd22733e55
pavelzubov <pasha224@yandex.ru>
2019-12-01 16:36:09 +0300

    Set CHIP_SIZE.SMALL as init size value

--- packages/social-trader/src/components/chip/chip-button.tsx
+++ packages/social-trader/src/components/chip/chip-button.tsx
@@ -8,7 +8,7 @@ import { ReactNode } from "react";
 const ChipButton: React.FC<Props> = React.memo(
   ({
     reverseOrder,
-    size,
+    size = CHIP_SIZE.SMALL,
     type,
     children,
     rounded,
