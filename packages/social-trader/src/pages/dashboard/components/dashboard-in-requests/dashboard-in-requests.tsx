61c11de2ec9ccd87ce3c997aba1cfa5507b8266d
pavelzubov <pasha224@yandex.ru>
2019-12-02 16:06:31 +0300

    Create DashboardInRequests component

---
+++ packages/social-trader/src/pages/dashboard/components/dashboard-in-requests/dashboard-in-requests.tsx
@@ -0,0 +1,28 @@
+import "./dashboard-in-requests.scss";
+
+import RequestLine from "components/request-line/request-line";
+import { withBlurLoader } from "decorators/with-blur-loader";
+import { TDashboardInRequests } from "pages/dashboard/dashboard.types";
+import React from "react";
+
+const _DashboardInRequests: React.FC<Props> = ({ data, updateData }) => {
+  return (
+    <div className="">
+      {data.map(request => (
+        <RequestLine
+          key={request.id}
+          request={request}
+          onApplyCancelRequest={updateData}
+        />
+      ))}
+    </div>
+  );
+};
+
+interface Props {
+  updateData: () => void;
+  data: TDashboardInRequests;
+}
+
+const DashboardInRequests = withBlurLoader(React.memo(_DashboardInRequests));
+export default DashboardInRequests;
