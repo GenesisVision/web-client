3231a7786aa6a089637587e6dc4111425dd4d38b
pavelzubov <pasha224@yandex.ru>
2019-12-03 11:59:19 +0300

    Move CancelRequestButton to separate file

--- packages/social-trader/src/components/request-line/request-line.tsx
+++ packages/social-trader/src/components/request-line/request-line.tsx
@@ -1,18 +1,13 @@
 import "./request-line.scss";
 
-import ConfirmPopup from "components/confirm-popup/confirm-popup";
 import PortfolioEventLogo from "components/dashboard/dashboard-portfolio-events/dashboard-portfolio-event-logo/dashboard-portfolio-event-logo";
-import GVButton from "components/gv-button";
+import { CancelRequestButton } from "components/request-line/cancel-request-button";
 import { StatisticItemList } from "components/statistic-item-list/statistic-item-list";
 import StatisticItem from "components/statistic-item/statistic-item";
 import { AssetInvestmentRequest } from "gv-api-web";
-import useApiRequest from "hooks/api-request.hook";
-import useIsOpen from "hooks/is-open.hook";
-import React, { useCallback } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import NumberFormat from "react-number-format";
-import investmentsApi from "services/api-client/investments-api";
-import authService from "services/auth-service";
 import { ASSET } from "shared/constants/constants";
 import { localizedDate } from "shared/utils/dates";
 import { formatCurrencyValue } from "utils/formatter";
@@ -24,16 +19,6 @@ const _RequestLine: React.FC<Props> = ({
   onApplyCancelRequest
 }) => {
   const [t] = useTranslation();
-  const [disabled, setDisabled, setNotDisabled] = useIsOpen();
-  const { sendRequest } = useApiRequest({
-    request: (id: string) =>
-      investmentsApi.cancelRequest(id, authService.getAuthArg()),
-    middleware: [onApplyCancelRequest, setNotDisabled]
-  });
-  const handleApplyCancelRequest = useCallback(() => {
-    setDisabled();
-    sendRequest(request.id);
-  }, [request.id]);
   return (
     <div className="request-line">
       <div className="request-line__logo">
@@ -104,40 +89,14 @@ const _RequestLine: React.FC<Props> = ({
         </StatisticItem>
       </StatisticItemList>
       {request.canCancelRequest && (
-        <CancelRequestBlock
-          handleApplyCancelRequest={handleApplyCancelRequest}
-          disabled={disabled}
+        <CancelRequestButton
+          onApplyCancelRequest={onApplyCancelRequest}
+          id={request.id}
         />
       )}
     </div>
   );
 };
-
-const CancelRequestBlock: React.FC<{
-  handleApplyCancelRequest: () => void;
-  disabled: boolean;
-}> = React.memo(({ handleApplyCancelRequest, disabled }) => {
-  const [t] = useTranslation();
-  const [isOpenPopup, setOpenPopup, setClosePopup] = useIsOpen();
-  return (
-    <>
-      <GVButton color="primary" variant="text" onClick={setOpenPopup}>
-        {t("buttons.cancel")}
-      </GVButton>
-      <ConfirmPopup
-        open={isOpenPopup}
-        onClose={setClosePopup}
-        onCancel={setClosePopup}
-        onApply={handleApplyCancelRequest}
-        header={t("request-line.cancel-header")}
-        body={t("request-line.cancel-body")}
-        applyButtonText={t("buttons.confirm")}
-        className="dialog--wider"
-        disabled={disabled}
-      />
-    </>
-  );
-});
 
 interface Props {
   successFee?: number;
