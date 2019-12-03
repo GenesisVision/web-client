720a54a29c07e54c5a49b7883d57db810fa7f01c
pavelzubov <pasha224@yandex.ru>
2019-12-03 14:36:34 +0300

    Use AmountItem

--- packages/social-trader/src/components/wallet/components/wallet-tables/wallet-transactions/amount-convert.tsx
+++ packages/social-trader/src/components/wallet/components/wallet-tables/wallet-transactions/amount-convert.tsx
@@ -1,36 +1,18 @@
 import "./convert-fields.scss";
 
 import { AmountRowCell } from "gv-api-web";
+import AmountItem from "modules/transaction-details/transactions/amount-item";
 import React from "react";
-import NumberFormat from "react-number-format";
-import { DEFAULT_DECIMAL_SCALE } from "shared/constants/constants";
-import { formatValue } from "utils/formatter";
 
 const _AmountConvert: React.FC<{
   amount: AmountRowCell;
 }> = ({ amount: { first, second } }) => {
   return (
     <div className="convert-field">
-      {first && (
-        <span className="wallet-transactions__col">
-          <NumberFormat
-            value={formatValue(first.amount, DEFAULT_DECIMAL_SCALE)}
-            thousandSeparator=" "
-            displayType="text"
-            suffix={` ${first.currency}`}
-          />
-        </span>
-      )}
+      {first && <AmountItem amount={first.amount} currency={first.currency} />}
       <span className="wallet-transactions__back-arrow">&rarr;</span>
       {second && (
-        <span className="wallet-transactions__col">
-          <NumberFormat
-            value={formatValue(second.amount, DEFAULT_DECIMAL_SCALE)}
-            thousandSeparator=" "
-            displayType="text"
-            suffix={` ${second.currency}`}
-          />
-        </span>
+        <AmountItem amount={second.amount} currency={second.currency} />
       )}
     </div>
   );
