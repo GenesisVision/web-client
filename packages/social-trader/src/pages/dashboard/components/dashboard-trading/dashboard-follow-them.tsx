98cc83da69ba4411686a8bb001c888674c55faf9
pavelzubov <pasha224@yandex.ru>
2019-12-04 16:54:03 +0300

    Correct types

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-follow-them.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-follow-them.tsx
@@ -1,6 +1,6 @@
+import { FollowDetailsList } from "gv-api-web";
 import FollowCard from "modules/follows-table/components/follow-card";
 import DashboardTradingTable from "pages/dashboard/components/dashboard-trading/dashboard-trading-table";
-import { TAsset } from "pages/dashboard/dashboard.types";
 import { getFollowThem } from "pages/dashboard/services/dashboard.service";
 import React from "react";
 import { useTranslation } from "react-i18next";
@@ -11,12 +11,8 @@ const _DashboardFollowThem: React.FC<Props> = () => {
     <DashboardTradingTable
       getItems={getFollowThem}
       title={t("dashboard-page.trading.follow-them")}
-      renderBodyCard={(asset: TAsset) => (
-        <FollowCard
-          follow={asset as unknown}
-          toggleFavorite={() => {}}
-          title={""}
-        />
+      renderBodyCard={(asset: FollowDetailsList) => (
+        <FollowCard follow={asset} toggleFavorite={() => {}} title={""} />
       )}
     />
   );
