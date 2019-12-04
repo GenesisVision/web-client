62b9dae489e015c67219b797e86b9cb18f48705b
pavelzubov <pasha224@yandex.ru>
2019-12-04 16:49:08 +0300

    Set tabs and two tables

--- packages/social-trader/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-portfolio-events-all/dashboard-portfolio-events-all.tsx
@@ -1,44 +1,71 @@
+import DetailsBlock from "components/details/details-block";
+import GVTabs from "components/gv-tabs";
+import GVTab from "components/gv-tabs/gv-tab";
 import Page from "components/page/page";
-import PortfolioEventsTable from "components/portfolio-events-table/portfolio-events-table";
-import Surface from "components/surface/surface";
-import useRole from "hooks/use-role.hook";
+import PortfolioEventsTableModule from "components/portfolio-events-table/portfolio-events-table-module";
+import { PORTFOLIO_EVENTS_COLUMNS } from "components/portfolio-events-table/portfolio-events-table.constants";
+import useTab from "hooks/tab.hook";
 import { EVENT_LOCATION } from "pages/programs/program-details/service/program-details.service";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { allEventsSelector } from "reducers/platform-reducer";
 
-import { dashboardEventsAllTableSelector } from "../../reducers/dashboard-events.reducer";
-import { getEvents } from "../../services/dashboard.service";
+import {
+  fetchInvestmentHistory,
+  fetchTradingHistory
+} from "../../services/dashboard.service";
 
 const _PortfolioEventsAllComponent: React.FC = () => {
+  const { tab, setTab } = useTab<TABS>(TABS.INVESTMENT_HISTORY);
   const [t] = useTranslation();
-  const role = useRole();
   const events = useSelector(allEventsSelector);
   return (
-    <Page
-      title={t(
-        `${role ? `${role}.` : ""}dashboard-page.portfolio-events.title`
-      )}
-    >
-      <Surface className="dashboard-portfolio-events-all">
-        <PortfolioEventsTable
-          selector={dashboardEventsAllTableSelector}
-          getItems={getEvents(EVENT_LOCATION.EventsAll)}
-          eventLocation={EVENT_LOCATION.EventsAll}
-          title={t(
-            `${
-              role ? `${role}.` : ""
-            }dashboard-page.portfolio-events.table-title`
-          )}
-          className="portfolio-events-all-table"
-          dateRangeStartLabel={t("filters.date-range.account-creation")}
-          eventTypeFilterValues={events}
-        />
-      </Surface>
+    <Page title={t(`dashboard-page.portfolio-events.title`)}>
+      <DetailsBlock table>
+        <div className="details-history__header">
+          <div className="details-history__tabs">
+            <GVTabs value={tab} onChange={setTab}>
+              <GVTab
+                value={TABS.INVESTMENT_HISTORY}
+                label={t(`dashboard-page.portfolio-events.investment-history`)}
+              />
+              <GVTab
+                value={TABS.TRADING_HISTORY}
+                label={t(`dashboard-page.portfolio-events.trading-history`)}
+              />
+            </GVTabs>
+          </div>
+        </div>
+        {tab === TABS.INVESTMENT_HISTORY && (
+          <PortfolioEventsTableModule
+            columns={PORTFOLIO_EVENTS_COLUMNS}
+            getItems={fetchInvestmentHistory}
+            eventLocation={EVENT_LOCATION.Dashboard}
+            className="portfolio-events-all-table"
+            dateRangeStartLabel={t("filters.date-range.account-creation")}
+            eventTypeFilterValues={events}
+          />
+        )}
+        {tab === TABS.TRADING_HISTORY && (
+          <PortfolioEventsTableModule
+            columns={PORTFOLIO_EVENTS_COLUMNS}
+            getItems={fetchTradingHistory}
+            eventLocation={EVENT_LOCATION.Dashboard}
+            className="portfolio-events-all-table"
+            dateRangeStartLabel={t("filters.date-range.account-creation")}
+            eventTypeFilterValues={events}
+          />
+        )}
+      </DetailsBlock>
     </Page>
   );
 };
+
+enum TABS {
+  INVESTMENT_HISTORY = "Investment history",
+  TRADING_HISTORY = "Trading history"
+}
 
 const PortfolioEventsAllComponent = React.memo(_PortfolioEventsAllComponent);
 export default PortfolioEventsAllComponent;
