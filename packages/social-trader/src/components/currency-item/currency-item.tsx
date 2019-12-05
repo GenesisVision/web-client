f316cbcae59009207f1318433b266b1a53e2211a
pavelzubov <pasha224@yandex.ru>
2019-12-05 12:19:05 +0300

    Uncomment ActivePopup

--- packages/social-trader/src/components/currency-item/currency-item.tsx
+++ packages/social-trader/src/components/currency-item/currency-item.tsx
@@ -53,17 +53,16 @@ const _CurrencyItem: React.FC<Props> = ({
     </div>
   );
   return (
-    //TODO fix it
     (clickable && (
       <>
         <a href="http://example.com" onClick={openPopup}>
           {renderItemContent()}
         </a>
-        {/*<ActivePopup*/}
-        {/*  open={isOpenPopup}*/}
-        {/*  onClose={setClosePopup}*/}
-        {/*  active={symbol || name}*/}
-        {/*/>*/}
+        <ActivePopup
+          open={isOpenPopup}
+          onClose={setClosePopup}
+          active={symbol || name || ""}
+        />
       </>
     )) ||
     renderItemContent()
