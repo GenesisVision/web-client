45cc9603963c9c089b2a55e71448821f3f46dba2
pavelzubov <pasha224@yandex.ru>
2019-12-07 10:46:16 +0300

    Create ProgramAbsoluteProfitTooltip component

---
+++ packages/social-trader/src/pages/programs/program-details/program-details-statistic-section/program-details-chart-section/program-absolute-profit-chart-section/program-absolute-profit-tooltip.tsx
@@ -0,0 +1,33 @@
+import ChartTooltip from "components/chart/chart-tooltip/chart-tooltip";
+import * as React from "react";
+
+const TooltipBody: React.FC<{ equity: string }> = React.memo(({ equity }) => (
+  <div className="details-tooltip__statistic">
+    <div className="details-tooltip__value">{equity}</div>
+  </div>
+));
+
+const ProgramAbsoluteProfitTooltip: React.FC<Props> = ({
+  active,
+  label,
+  payload
+}) => {
+  if (!active || !payload.length) return null;
+  const equity =
+    payload[0] && `${payload[0].payload.value.toFixed(2)} ${payload[0].unit}`;
+  return (
+    <ChartTooltip
+      body={<TooltipBody equity={equity} />}
+      date={new Date(label)}
+      className="details-tooltip"
+    />
+  );
+};
+
+interface Props {
+  active: boolean;
+  label: string;
+  payload: any[];
+}
+
+export default ProgramAbsoluteProfitTooltip;
