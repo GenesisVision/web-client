c5ab3c5cd0d758497e0c01e30306ec93e6f1f6a9
pavelzubov <pasha224@yandex.ru>
2019-12-07 19:23:22 +0300

    Set size Set onApply

--- packages/social-trader/src/pages/programs/program-details/program-controls/investment-program-controls.tsx
+++ packages/social-trader/src/pages/programs/program-details/program-controls/investment-program-controls.tsx
@@ -2,6 +2,7 @@ import DetailsBlock, {
   DETAILS_BLOCK_TYPE
 } from "components/details/details-block";
 import InvestmentProgramInfo from "components/details/details-description-section/investment-program-info";
+import { GV_BTN_SIZE } from "components/gv-button";
 import { LevelsParamsInfo, ProgramDetailsFull } from "gv-api-web";
 import DepositButton from "modules/deposit/deposit.button";
 import LevelCalculator from "modules/level-calculator/components/level-calculator";
@@ -9,6 +10,7 @@ import * as React from "react";
 import { ASSET } from "shared/constants/constants";
 
 const _InvestmentProgramControls: React.FC<Props> = ({
+  onApply,
   isOwnProgram,
   programDescription,
   levelsParameters
@@ -26,6 +28,8 @@ const _InvestmentProgramControls: React.FC<Props> = ({
       />
       <div className="asset-details-description__statistic-container asset-details-description__statistic-container--btn">
         <DepositButton
+          onApply={onApply}
+          size={GV_BTN_SIZE.BIG}
           ownAsset={isOwnProgram}
           entryFee={programDescription.entryFeeCurrent}
           availableToInvest={programDescription.availableInvestmentBase}
@@ -40,6 +44,7 @@ const _InvestmentProgramControls: React.FC<Props> = ({
 };
 
 interface Props {
+  onApply?: VoidFunction;
   canCloseAsset: boolean;
   isOwnProgram: boolean;
   programDescription: ProgramDetailsFull;
