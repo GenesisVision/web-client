df46b04de084be637c6947a7bf53124aa8e1e28c
pavelzubov <pasha224@yandex.ru>
2019-12-08 22:23:26 +0300

    Move useGetRate to hook folder

--- packages/social-trader/src/hooks/get-rate.hook.ts
+++ packages/social-trader/src/hooks/get-rate.hook.ts
@@ -1,25 +1,13 @@
 import useApiRequest from "hooks/api-request.hook";
 import { fetchRate } from "services/rate-service";
 
-import { getSignalInfo } from "./services/follow-module-service";
-
-export const useGetSignalInfo = () => {
-  const { data, isPending, sendRequest } = useApiRequest({
-    request: getSignalInfo
-  });
-  return {
-    minDeposit: data,
-    isMinDepositPending: isPending,
-    getMinDeposit: sendRequest
-  };
-};
-
 export const useGetRate = () => {
   const { data, isPending, sendRequest } = useApiRequest({
+    defaultData: 1,
     request: ({ from, to }) => fetchRate(from, to)
   });
   return {
-    rate: data,
+    rate: data as number,
     isRatePending: isPending,
     getRate: sendRequest
   };
