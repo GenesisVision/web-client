5b0ce7e16b84e05fcab03c177f40b03bf525c80e
pavelzubov <pasha224@yandex.ru>
2019-12-08 19:02:28 +0300

    Use ActivePopupContainer

--- packages/social-trader/src/components/active/active.popup.tsx
+++ packages/social-trader/src/components/active/active.popup.tsx
@@ -1,22 +1,13 @@
 import "./active.scss";
 
+import ActivePopupContainer from "components/active/active.popup.container";
 import Dialog, { IDialogProps } from "components/dialog/dialog";
-import useApiRequest from "hooks/api-request.hook";
-import React, { useEffect } from "react";
-
-import Active from "./active";
-import { fetchActive, getActiveLoaderData } from "./service/active.service";
+import React from "react";
 
 const _ActivePopup: React.FC<Props> = ({ open, onClose, active }) => {
-  const { data } = useApiRequest({
-    request: () => fetchActive({ active }),
-    fetchOnMount: true
-  });
   return (
     <Dialog open={open} onClose={onClose} top>
-      <div className="active__popup">
-        <Active loaderData={getActiveLoaderData} data={data} />
-      </div>
+      <ActivePopupContainer active={active} />
     </Dialog>
   );
 };
