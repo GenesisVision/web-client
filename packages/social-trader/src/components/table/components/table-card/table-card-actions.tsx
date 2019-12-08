4f60848fda68e3fe9dd4affbd17c4d210e873fde
pavelzubov <pasha224@yandex.ru>
2019-12-08 19:10:10 +0300

    Add TableCardActionsItemContainer, TableCardFavoriteActionItem

--- packages/social-trader/src/components/table/components/table-card/table-card-actions.tsx
+++ packages/social-trader/src/components/table/components/table-card/table-card-actions.tsx
@@ -6,8 +6,13 @@ import Popover, {
   HORIZONTAL_POPOVER_POS,
   VERTICAL_POPOVER_POS
 } from "components/popover/popover";
+import { UpdateRowFuncType } from "components/table/components/table.types";
 import { TAnchor, TEvent } from "hooks/anchor.hook";
+import { ToggleAssetFavoriteButton } from "modules/toggle-asset-favorite-button/toggle-asset-favorite-button";
+import { ToggleableAssetType } from "modules/toggle-asset-favorite-button/toggle-asset-favorite-button.types";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { ASSET } from "shared/constants/constants";
 
 const _TableCardActions: React.FC<Props> = ({
   clearAnchor,
@@ -31,11 +36,48 @@ export const TableCardActionsItem: React.FC<
   ITableCardActionsItemProps
 > = React.memo(({ to, onClick, children }) => {
   return (
-    <Link to={to}>
-      <GVButton variant="text" color="secondary" onClick={onClick}>
-        {children}
-      </GVButton>
-    </Link>
+    <TableCardActionsItemContainer>
+      <Link to={to}>
+        <GVButton noPadding variant="text" color="secondary" onClick={onClick}>
+          {children}
+        </GVButton>
+      </Link>
+    </TableCardActionsItemContainer>
+  );
+});
+
+export const TableCardActionsItemContainer: React.FC<
+  React.HTMLAttributes<HTMLDivElement>
+> = React.memo(({ children }) => {
+  return <div className="table-card__actions-item">{children}</div>;
+});
+
+export const TableCardFavoriteActionItem: React.FC<{
+  asset?: ToggleableAssetType;
+  updateRow?: UpdateRowFuncType;
+  assetType: ASSET;
+  id: string;
+  isFavorite: boolean;
+  withDispatch?: boolean;
+  onApply?: VoidFunction;
+}> = React.memo(({ id, isFavorite, onApply, assetType, asset, updateRow }) => {
+  const { t } = useTranslation();
+  return (
+    <TableCardActionsItemContainer>
+      <ToggleAssetFavoriteButton
+        asset={asset}
+        updateRow={updateRow}
+        onApply={onApply}
+        withDispatch
+        assetType={assetType}
+        id={id}
+        isFavorite={isFavorite}
+      >
+        {isFavorite
+          ? t("fund-actions.add-to-favorites")
+          : t("fund-actions.remove-from-favorites")}
+      </ToggleAssetFavoriteButton>
+    </TableCardActionsItemContainer>
   );
 });
 
