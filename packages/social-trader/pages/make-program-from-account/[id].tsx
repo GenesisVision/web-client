15766daf2f13e5599d972cbaa82a353d30d31708
pavelzubov <pasha224@yandex.ru>
2019-12-09 11:24:25 +0300

    Set real broker

--- packages/social-trader/pages/make-program-from-account/[id].tsx
+++ packages/social-trader/pages/make-program-from-account/[id].tsx
@@ -1,10 +1,8 @@
-import platformActions from "actions/platform-actions";
 import withDefaultLayout from "decorators/with-default-layout";
 import withPrivateRoute from "decorators/with-private-route";
-import { ProgramDetailsFull } from "gv-api-web";
+import { fetchAccountDescriptionCtx } from "pages/accounts/account-details/services/account-details.service";
 import { CONVERT_ASSET } from "pages/convert-asset/convert-asset.contants";
 import ConvertAssetPage from "pages/convert-asset/convert-asset.page";
-import { dispatchProgramDescription } from "pages/programs/program-details/service/program-details.service";
 import React from "react";
 import { compose } from "redux";
 import { NextPageWithRedux } from "utils/types";
@@ -24,20 +22,12 @@ const Page: NextPageWithRedux<Props, {}> = ({ id, broker }) => {
 
 Page.getInitialProps = async ctx => {
   const { id } = ctx.query;
-  let broker = "MetaTrader4";
-  /*await Promise.all([
-    ctx.reduxStore.dispatch(dispatchProgramDescription(ctx)),
-    ctx.reduxStore.dispatch(
-      async dispatch => await dispatch(platformActions.fetchPlatformSettings())
-    )
-  ]).then(([descriptionResult]) => {
-    const description = ((descriptionResult as unknown) as {
-      value: ProgramDetailsFull;
-    }).value;
-    /!*if (!description.personalDetails || !description.personalDetails.isOwnAsset)
-      throw new Error();*!/
-    broker = description.brokerDetails.type;
-  });*/
+  let broker;
+  await fetchAccountDescriptionCtx(id as string, ctx).then(
+    ({ brokerDetails }) => {
+      broker = brokerDetails.type;
+    }
+  );
   return { id, broker };
 };
 
