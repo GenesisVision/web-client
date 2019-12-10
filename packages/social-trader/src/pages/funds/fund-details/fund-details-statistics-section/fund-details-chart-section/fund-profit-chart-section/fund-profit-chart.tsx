7f01399dd698c075ab580ef91838de185c171395
pavelzubov <pasha224@yandex.ru>
2019-12-10 18:32:17 +0300

    Uncomment getVerificationToken

--- packages/social-trader/src/pages/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-chart.tsx
+++ packages/social-trader/src/pages/funds/fund-details/fund-details-statistics-section/fund-details-chart-section/fund-profit-chart-section/fund-profit-chart.tsx
@@ -1,21 +1,35 @@
-import ProfitChart, { EquityChartType } from "components/chart/profit-chart";
-import { ChartsDataType } from "components/details/details-statistic-section/details.chart.types";
+import ProfitChart, {
+  EquityChartElementType,
+  EquityChartType
+} from "components/chart/profit-chart";
+import {
+  ChartAssetsType,
+  ChartsDataType
+} from "components/details/details-statistic-section/details.chart.types";
 import { TChartCurrency } from "modules/chart-currency-selector/chart-currency-selector";
 import * as React from "react";
 
 import FundProfitTooltip from "./fund-profit-tooltip";
 
 const _FundProfitChart: React.FC<Props> = ({
+  assets,
   profitChart,
   chartCurrencies
 }) => {
   const equityCharts = profitChart.map(chart => chart.chart);
-  const equities = equityCharts.map(equityChart =>
+  const assetsChart = assets.map(
+    assets =>
+      (({
+        ...assets,
+        value: undefined
+      } as unknown) as EquityChartElementType)
+  );
+  /*const equities = equityCharts.map(equityChart =>
     (equityChart as EquityChartType).map((item: any) => ({
       ...item,
       assets:
         [
-          ...[], //item.assetsState.assets,
+          ...assets,
           {
             icon: "",
             color: "grey",
@@ -25,18 +39,19 @@ const _FundProfitChart: React.FC<Props> = ({
           }
         ].filter(({ percent }) => !!percent) || []
     }))
-  );
+  );*/
   return (
     <ProfitChart
       equityCharts={equityCharts}
       tooltip={FundProfitTooltip}
-      equities={equities}
+      equities={[assetsChart, ...equityCharts]}
       colors={chartCurrencies}
     />
   );
 };
 
 interface Props {
+  assets: ChartAssetsType;
   profitChart: ChartsDataType;
   chartCurrencies?: TChartCurrency[];
 }
