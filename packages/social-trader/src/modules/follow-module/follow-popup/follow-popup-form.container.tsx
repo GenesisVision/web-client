b8af3afc3e850d49d6d37b3a3bef2e6eb4f78dac
pavelzubov <pasha224@yandex.ru>
2019-12-11 17:21:02 +0300

    Set 0 to minDeposit if isExternal

--- packages/social-trader/src/modules/follow-module/follow-popup/follow-popup-form.container.tsx
+++ packages/social-trader/src/modules/follow-module/follow-popup/follow-popup-form.container.tsx
@@ -35,11 +35,13 @@ const _FollowModuleContainer: React.FC<Props> = ({
   const tradingAccountMinDepositAmounts = useSelector(
     tradingAccountMinDepositAmountsSelector
   );
-  const minDeposit = tradingAccountMinDepositAmounts
-    .find(({ serverType }) => serverType === broker)!
-    .minDepositCreateAsset.find(
-      (minDeposit: AmountWithCurrency) => minDeposit.currency === currency
-    )!.amount;
+  const minDeposit = isExternal
+    ? 0
+    : tradingAccountMinDepositAmounts
+        .find(({ serverType }) => serverType === broker)!
+        .minDepositCreateAsset.find(
+          (minDeposit: AmountWithCurrency) => minDeposit.currency === currency
+        )!.amount;
   const wallets = useSelector(walletsSelector);
 
   const getAccountsMethod = isExternal ? fetchExternalAccounts : fetchAccounts;
