75ffff3c5a67ec1b25d6f0230e85467bc3728c48
pavelzubov <pasha224@yandex.ru>
2019-12-12 15:28:16 +0300

    Use DashboardHorizontalList

--- packages/social-trader/src/pages/dashboard/components/dashboard-recommendations/dashboard-recommendations.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-recommendations/dashboard-recommendations.tsx
@@ -1,15 +1,16 @@
 import { withBlurLoader } from "decorators/with-blur-loader";
+import DashboardHorizontalList from "pages/dashboard/components/dashboard-block/dashboard-horizontal-list";
 import RecommendationCard from "pages/dashboard/components/dashboard-recommendations/recommendation-card";
 import { TDashboardRecommendations } from "pages/dashboard/dashboard.types";
 import React from "react";
 
 const _DashboardRecommendations: React.FC<Props> = ({ data: { assets } }) => {
   return (
-    <div className="dashboard-recommendations__cards">
+    <DashboardHorizontalList>
       {assets.map(asset => (
         <RecommendationCard asset={asset} title={""} />
       ))}
-    </div>
+    </DashboardHorizontalList>
   );
 };
 
