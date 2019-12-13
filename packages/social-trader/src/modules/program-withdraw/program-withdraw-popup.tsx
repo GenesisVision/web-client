be1ee0125fff312ee7ce2e9f473cb2ce7aec5eed
e.grebenshikov <evgreb@gmail.com>
2019-12-13 18:23:16 +0300

    GV-1889 fix withdraw program popup

--- packages/social-trader/src/modules/program-withdraw/program-withdraw-popup.tsx
+++ packages/social-trader/src/modules/program-withdraw/program-withdraw-popup.tsx
@@ -23,7 +23,7 @@ const _ProgramWithdrawPopup: React.FC<Props> = ({
   onApply,
   id,
   onClose,
-  data: { availableToWithdraw, periodEnds, title },
+  data: { availableToWithdraw, periodEnds, title, isOwner },
   assetCurrency,
   accountCurrency
 }) => {
@@ -66,6 +66,7 @@ const _ProgramWithdrawPopup: React.FC<Props> = ({
       <DialogBottom>
         {tab === PROGRAM_WITHDRAW_FORM.ENTER_AMOUNT && (
           <ProgramWithdrawAmountForm
+            isOwner={isOwner}
             formValues={formValues}
             rate={rate}
             programCurrency={assetCurrency}
