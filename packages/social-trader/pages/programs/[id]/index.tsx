738963f96d3abade87190e1bb3f4f259b2bfc3cf
pavelzubov <pasha224@yandex.ru>
2019-12-13 19:51:17 +0300

    Remove unused files

--- packages/social-trader/pages/programs/[id]/index.tsx
+++
@@ -1,5 +0,0 @@
-import ProgramDetailsPage from "pages/programs/program-details/program-details.page";
-import React from "react";
-import { programDetailsCreator } from "routes/ssr/programs/[id]";
-
-export default programDetailsCreator(ProgramDetailsPage);
