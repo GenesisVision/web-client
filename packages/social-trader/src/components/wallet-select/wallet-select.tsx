4e924740a4047db6cb33cae3dad6a454f1061172
pavelzubov <pasha224@yandex.ru>
2019-12-14 11:17:56 +0300

    Rename ItemType to WalletItemType, CurrencySourceSelectItemType

--- packages/social-trader/src/components/wallet-select/wallet-select.tsx
+++ packages/social-trader/src/components/wallet-select/wallet-select.tsx
@@ -1,6 +1,7 @@
 import CurrencySourceSelect from "components/currency-source-select/currency-source-select";
 import { ISelectChangeEvent } from "components/select/select";
 import { WalletBaseData, WalletData } from "gv-api-web";
+import { TransferItemType } from "modules/transfer/transfer.types";
 import React from "react";
 
 const _WalletSelect: React.FC<Props> = ({ items, onChange, label, name }) => (
@@ -12,8 +13,8 @@ const _WalletSelect: React.FC<Props> = ({ items, onChange, label, name }) => (
   />
 );
 
-export type ItemsType = Array<ItemType>;
-export type ItemType = WalletData | WalletBaseData;
+export type ItemsType = Array<WalletItemType>;
+export type WalletItemType = WalletData | WalletBaseData | TransferItemType;
 
 interface Props {
   items: ItemsType;
