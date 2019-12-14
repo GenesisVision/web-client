3105e8c8ad97a051f692dd9264f6f3ae1b5daa68
pavelzubov <pasha224@yandex.ru>
2019-12-14 22:26:54 +0300

    Use DEFAULT_EVENTS_PAGING for details events table

--- packages/social-trader/src/pages/funds/fund-details/reducers/fund-events.reducer.ts
+++ packages/social-trader/src/pages/funds/fund-details/reducers/fund-events.reducer.ts
@@ -4,7 +4,10 @@ import {
   EVENTS_ACTION_TYPE
 } from "components/portfolio-events-table/portfolio-events-table.constants";
 import { tableSelectorCreator } from "components/table/helpers/table.selector";
-import { DEFAULT_PAGING } from "components/table/reducers/table-paging.reducer";
+import {
+  DEFAULT_EVENTS_PAGING,
+  DEFAULT_PAGING
+} from "components/table/reducers/table-paging.reducer";
 import tableReducerFactory, {
   ITableState
 } from "components/table/reducers/table.reducer";
@@ -26,7 +29,7 @@ export const fundEventsTableSelector = tableSelectorCreator<
 export const fundEventsReducer = tableReducerFactory<TFundEventsData>({
   clearable: true,
   type: EVENTS_ACTION_TYPE,
-  paging: DEFAULT_PAGING,
+  paging: DEFAULT_EVENTS_PAGING,
   filtering: ASSET_PORTFOLIO_EVENTS_DEFAULT_FILTERING,
   defaultFilters: ASSET_PORTFOLIO_EVENTS_FILTERS
 });
