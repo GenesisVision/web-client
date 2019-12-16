ade9d36b2a0785342f896aeeb145a9012fc0a124
pavelzubov <pasha224@yandex.ru>
2019-12-16 08:52:43 +0300

    Create ConvertField component

---
+++ packages/social-trader/src/components/wallet/components/wallet-tables/wallet-transactions/convert-field.tsx
@@ -0,0 +1,23 @@
+import "./convert-fields.scss";
+
+import React from "react";
+
+const _ConvertField: React.FC<Props> = ({ first, second }) => {
+  return (
+    <div className="convert-field">
+      {first}
+      {second && (
+        <span className="wallet-transactions__back-arrow">&rarr;</span>
+      )}
+      {second}
+    </div>
+  );
+};
+
+interface Props {
+  first: JSX.Element | string;
+  second?: JSX.Element | string;
+}
+
+const ConvertField = React.memo(_ConvertField);
+export default ConvertField;
