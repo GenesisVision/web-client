8394a168223b8a87156229dba2d6590c31048290
pavelzubov <pasha224@yandex.ru>
2019-12-17 12:00:47 +0300

    Change "managers" route to "users"

--- packages/social-trader/src/routes/manager.routes.ts
+++ packages/social-trader/src/routes/manager.routes.ts
@@ -4,7 +4,7 @@ import { composeManagerDetailsUrl } from "utils/compose-url";
 
 export const MANAGER_SLUG_URL_PARAM_NAME = "managerSlugUrl";
 
-export const MANAGERS_ROUTE = "/managers";
+export const MANAGERS_ROUTE = "/users";
 export const MANAGER_DETAILS_ROUTE = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}`;
 export const MANAGER_DETAILS_FOLDER_ROUTE = `${MANAGERS_ROUTE}/[id]`;
 export const MANAGER_DETAILS_ROUTE_REGEXP = `${MANAGERS_ROUTE}/:${MANAGER_SLUG_URL_PARAM_NAME}(${SLUG_URL_REGEXP})`;
