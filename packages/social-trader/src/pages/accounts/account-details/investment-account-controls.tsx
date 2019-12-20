d5cb301d6394071e2414c8df3796289d9078efe9
pavelzubov <pasha224@yandex.ru>
2019-12-20 16:45:50 +0300

    Create InvestmentAccountControls component

---
+++ packages/social-trader/src/pages/accounts/account-details/investment-account-controls.tsx
@@ -0,0 +1,58 @@
+import DetailsBlock, {
+  DETAILS_BLOCK_TYPE
+} from "components/details/details-block";
+import { DetailsRowItem } from "components/details/details-row-item.block";
+import TransferButton from "modules/transfer/transfer-button";
+import { TRANSFER_CONTAINER } from "modules/transfer/transfer.types";
+import { AccountDetailsDataType } from "pages/accounts/account-details/account-details.types";
+import { mapAccountFullToTransferItemType } from "pages/dashboard/services/dashboard.service";
+import * as React from "react";
+import { useTranslation } from "shared/i18n";
+
+const _InvestmentAccountControls: React.FC<Props> = ({ account, onApply }) => {
+  const [t] = useTranslation();
+  return (
+    <DetailsBlock type={DETAILS_BLOCK_TYPE.BORDERED} row>
+      <DetailsRowItem>
+        <TransferButton
+          onApply={onApply}
+          label={t("buttons.deposit")}
+          title={t("transfer.deposit-to", {
+            title: t(
+              `dashboard-page.trading.asset-types.${account.tradingAccountInfo.type}`
+            )
+          })}
+          currentItem={mapAccountFullToTransferItemType(account)}
+          currentItemContainer={TRANSFER_CONTAINER.DESTINATION}
+          sourceType={"Wallet"}
+          destinationType={"PrivateTradingAccount"}
+        />
+      </DetailsRowItem>
+      <DetailsRowItem>
+        <TransferButton
+          onApply={onApply}
+          color={"secondary"}
+          variant={"outlined"}
+          label={t("buttons.withdraw")}
+          title={t("transfer.withdraw-from", {
+            title: t(
+              `dashboard-page.trading.asset-types.${account.tradingAccountInfo.type}`
+            )
+          })}
+          currentItem={mapAccountFullToTransferItemType(account)}
+          currentItemContainer={TRANSFER_CONTAINER.SOURCE}
+          sourceType={"PrivateTradingAccount"}
+          destinationType={"Wallet"}
+        />
+      </DetailsRowItem>
+    </DetailsBlock>
+  );
+};
+
+interface Props {
+  account: AccountDetailsDataType;
+  onApply?: VoidFunction;
+}
+
+const InvestmentAccountControls = React.memo(_InvestmentAccountControls);
+export default InvestmentAccountControls;
