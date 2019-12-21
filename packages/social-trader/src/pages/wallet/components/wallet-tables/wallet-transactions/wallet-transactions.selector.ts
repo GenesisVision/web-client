e1a556e79b12900ad405aacfb7cea37e2f8dff36
pavelzubov <pasha224@yandex.ru>
2019-12-21 16:02:23 +0300

    Remove unused walletTableTransactionsSelector

--- packages/social-trader/src/pages/wallet/components/wallet-tables/wallet-transactions/wallet-transactions.selector.ts
+++
@@ -1,7 +0,0 @@
-import { tableSelectorCreator } from "components/table/helpers/table.selector";
-
-const walletTransactionsSelector = (state: any) => state.wallet.transactions; // TODO change state type
-export const walletTableTransactionsSelector = tableSelectorCreator(
-  walletTransactionsSelector,
-  "transactions"
-);
