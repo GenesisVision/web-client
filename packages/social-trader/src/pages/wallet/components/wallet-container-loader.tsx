a3880cd976d6460c0473c4ea0b95fcb8f00e5805
pavelzubov <pasha224@yandex.ru>
2019-12-21 09:14:59 +0300

    Remove unused loaders

--- packages/social-trader/src/pages/wallet/components/wallet-container-loader.tsx
+++ packages/social-trader/src/pages/wallet/components/wallet-container-loader.tsx
@@ -1,34 +1,10 @@
 import "./wallet-balance/wallet-balance.scss";
 
-import SvgLoader from "components/svg-loader/svg-loader";
 import * as faker from "faker";
 import { WalletData, WalletSummary } from "gv-api-web";
 import * as React from "react";
 import { getRandomInteger } from "utils/helpers";
 import { CurrencyEnum } from "utils/types";
-
-import WalletBalanceLoader from "./wallet-balance/wallet-balance-loader";
-import WalletSettingsLoader from "./wallet-settings/wallet-settings-loader";
-import WalletTablesLoader from "./wallet-tables/wallet-tables-loader";
-
-const WalletContainerLoader = () => (
-  <div className="wallet-balance">
-    <WalletTitleLoader />
-    <WalletBalanceLoader />
-    <WalletTablesLoader />
-  </div>
-);
-
-const WalletTitleLoader: React.FC = () => (
-  <div className="wallet-balance__wrapper">
-    <h1 className="wallet-balance__title">
-      <SvgLoader height={15} width={100}>
-        <rect x="0" y="0" width="100" height="15" rx="8" ry="8" />
-      </SvgLoader>
-    </h1>
-    <WalletSettingsLoader />
-  </div>
-);
 
 export const walletDataCreator = (): WalletData => ({
   availableCcy: getRandomInteger(1, 1000),
@@ -64,5 +40,3 @@ export const walletMultiSummaryLoaderData: WalletSummary = {
     .map(walletDataCreator),
   payFeesWithGvt: false
 };
-
-export default WalletContainerLoader;
--- packages/social-trader/src/pages/wallet/components/wallet-loader.tsx
+++ packages/social-trader/src/pages/wallet/components/wallet-loader.tsx
@@ -1,40 +1,8 @@
 import "./wallet-balance/wallet-balance.scss";
 
-import WalletImage from "components/avatar/wallet-image/wallet-image";
-import Page from "components/page/page";
-import SvgLoader from "components/svg-loader/svg-loader";
 import { WalletBaseData } from "gv-api-web";
 import * as React from "react";
-
-import { getRandomInteger, getRandomWord } from "../../../utils/helpers";
-import WalletBalanceButtonsLoader from "./wallet-balance/wallet-balance-buttons-loader";
-import WalletBalanceLoader from "./wallet-balance/wallet-balance-loader";
-
-const WalletLoader = () => (
-  <Page title={""}>
-    <div className="wallet-balance">
-      <WalletTitleLoader />
-      <WalletBalanceLoader />
-    </div>
-  </Page>
-);
-
-const WalletTitleLoader: React.FC = () => (
-  <div className="wallet-balance__wrapper">
-    <h1 className="wallet-balance__title">
-      <SvgLoader width={210} height={32}>
-        <rect x="0" y="0" width="100" height="32" rx="8" ry="8" />
-        <rect x="110" y="0" width="100" height="32" rx="8" ry="8" />
-      </SvgLoader>
-      <WalletImage
-        imageClassName="wallet-balance__title-icon"
-        alt={""}
-        url={""}
-      />
-    </h1>
-    <WalletBalanceButtonsLoader />
-  </div>
-);
+import { getRandomInteger, getRandomWord } from "utils/helpers";
 
 export const getWalletBaseLoaderData = (): WalletBaseData => ({
   id: getRandomWord(),
@@ -53,5 +21,3 @@ export const getWalletBaseLoaderDataWithETHCurrency = (): WalletBaseData => ({
   available: getRandomInteger(0, 100),
   rate: getRandomInteger(0, 100)
 });
-
-export default WalletLoader;
