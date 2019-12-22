15ac4e411b77d0629be6e8a6b70655f2ca6b5e11
pavelzubov <pasha224@yandex.ru>
2019-12-22 22:50:12 +0300

    Use DepositTransferButton, WithdrawTransferButton

--- packages/social-trader/src/pages/accounts/account-details/investment-account-controls.tsx
+++ packages/social-trader/src/pages/accounts/account-details/investment-account-controls.tsx
@@ -2,47 +2,31 @@ import DetailsBlock, {
   DETAILS_BLOCK_TYPE
 } from "components/details/details-block";
 import { DetailsBlockRowItem } from "components/details/details-block-row-item.block";
-import TransferButton from "modules/transfer/transfer-button";
-import { TRANSFER_CONTAINER } from "modules/transfer/transfer.types";
-import { AccountDetailsDataType } from "pages/accounts/account-details/account-details.types";
-import { mapAccountFullToTransferItemType } from "pages/dashboard/services/dashboard.service";
+import { WalletItemType } from "components/wallet-select/wallet-select";
+import { AssetTypeExt, PrivateTradingAccountType } from "gv-api-web";
+import { DepositTransferButton } from "modules/transfer/deposit-transfer-button";
+import { WithdrawTransferButton } from "modules/transfer/withdraw-transfer-button";
 import * as React from "react";
-import { useTranslation } from "shared/i18n";
 
-const _InvestmentAccountControls: React.FC<Props> = ({ account, onApply }) => {
-  const [t] = useTranslation();
+const _InvestmentAccountControls: React.FC<Props> = ({
+  onApply,
+  accountType,
+  transferableItem
+}) => {
   return (
     <DetailsBlock type={DETAILS_BLOCK_TYPE.BORDERED} row>
       <DetailsBlockRowItem>
-        <TransferButton
-          color={"primary"}
-          variant={"contained"}
+        <DepositTransferButton
           onApply={onApply}
-          label={t("buttons.deposit")}
-          title={t("transfer.deposit-to", {
-            title: t(
-              `dashboard-page.trading.asset-types.${account.tradingAccountInfo.type}`
-            )
-          })}
-          currentItem={mapAccountFullToTransferItemType(account)}
-          currentItemContainer={TRANSFER_CONTAINER.DESTINATION}
-          sourceType={"Wallet"}
-          destinationType={"PrivateTradingAccount"}
+          currentItem={transferableItem}
+          accountType={accountType}
         />
       </DetailsBlockRowItem>
       <DetailsBlockRowItem>
-        <TransferButton
+        <WithdrawTransferButton
           onApply={onApply}
-          label={t("buttons.withdraw")}
-          title={t("transfer.withdraw-from", {
-            title: t(
-              `dashboard-page.trading.asset-types.${account.tradingAccountInfo.type}`
-            )
-          })}
-          currentItem={mapAccountFullToTransferItemType(account)}
-          currentItemContainer={TRANSFER_CONTAINER.SOURCE}
-          sourceType={"PrivateTradingAccount"}
-          destinationType={"Wallet"}
+          currentItem={transferableItem}
+          accountType={accountType}
         />
       </DetailsBlockRowItem>
     </DetailsBlock>
@@ -50,7 +34,8 @@ const _InvestmentAccountControls: React.FC<Props> = ({ account, onApply }) => {
 };
 
 interface Props {
-  account: AccountDetailsDataType;
+  transferableItem: WalletItemType;
+  accountType?: PrivateTradingAccountType | AssetTypeExt;
   onApply?: VoidFunction;
 }
 
