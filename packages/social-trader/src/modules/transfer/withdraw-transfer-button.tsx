79334d2ea324debdac4754a463281124c0e22101
pavelzubov <pasha224@yandex.ru>
2019-12-22 22:08:29 +0300

    Create WithdrawTransferButton component

---
+++ packages/social-trader/src/modules/transfer/withdraw-transfer-button.tsx
@@ -0,0 +1,33 @@
+import { GV_BTN_SIZE } from "components/gv-button";
+import { WalletItemType } from "components/wallet-select/wallet-select";
+import { PrivateTradingAccountType } from "gv-api-web";
+import TransferButton from "modules/transfer/transfer-button";
+import { TRANSFER_CONTAINER } from "modules/transfer/transfer.types";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const _WithdrawTransferButton: React.FC<Props> = props => {
+  const { accountType } = props;
+  const [t] = useTranslation();
+  return (
+    <TransferButton
+      {...props}
+      size={GV_BTN_SIZE.MIDDLE}
+      label={t("buttons.withdraw")}
+      title={t("transfer.withdraw-from", {
+        title: t(`dashboard-page.trading.asset-types.${accountType}`)
+      })}
+      currentItemContainer={TRANSFER_CONTAINER.SOURCE}
+      sourceType={"PrivateTradingAccount"}
+      destinationType={"Wallet"}
+    />
+  );
+};
+
+interface Props {
+  currentItem: WalletItemType;
+  onApply?: VoidFunction;
+  accountType: PrivateTradingAccountType;
+}
+
+export const WithdrawTransferButton = React.memo(_WithdrawTransferButton);
