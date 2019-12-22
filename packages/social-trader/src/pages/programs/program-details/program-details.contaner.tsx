7a0795cd004d02a83b4e222a31f584d0b73e7747
pavelzubov <pasha224@yandex.ru>
2019-12-22 22:50:38 +0300

    Add InvestmentAccountControls

--- packages/social-trader/src/pages/programs/program-details/program-details.contaner.tsx
+++ packages/social-trader/src/pages/programs/program-details/program-details.contaner.tsx
@@ -5,6 +5,8 @@ import { DetailsDivider } from "components/details/details-divider.block";
 import { DETAILS_TYPE } from "components/details/details.types";
 import Page from "components/page/page";
 import { withBlurLoader } from "decorators/with-blur-loader";
+import InvestmentAccountControls from "pages/accounts/account-details/investment-account-controls";
+import { mapProgramFollowToTransferItemType } from "pages/dashboard/services/dashboard.service";
 import FollowControls from "pages/follows/follow-details/follow-controls/follow-controls";
 import FollowDetailsStatisticSection from "pages/follows/follow-details/follow-details-statistic-section/follow-details-statistic-section";
 import ProgramDetailsStatisticSection from "pages/programs/program-details/program-details-statistic-section/program-details-statistic-section";
@@ -121,6 +123,15 @@ const _ProgramDetailsContainer: React.FC<Props> = ({ data: description }) => {
         )}
         Controls={() => (
           <>
+            {followDetails && (
+              <InvestmentAccountControls
+                transferableItem={mapProgramFollowToTransferItemType(
+                  description
+                )}
+                accountType={"SignalTradingAccount"}
+                onApply={handleDispatchDescription}
+              />
+            )}
             {programDetails && (
               <InvestmentProgramControls
                 onApply={handleDispatchDescription}
