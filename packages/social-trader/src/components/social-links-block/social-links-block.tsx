31fe323eee159011350ef5e7e65f1b62e53fa51c
pavelzubov <pasha224@yandex.ru>
2019-12-24 14:50:21 +0300

    Correct type socialLinks

--- packages/social-trader/src/components/social-links-block/social-links-block.tsx
+++ packages/social-trader/src/components/social-links-block/social-links-block.tsx
@@ -1,23 +1,26 @@
 import "./social-links-block.scss";
 
 import SocialLinkImage from "components/avatar/social-link/social-link";
-import { SocialLinkViewModel } from "gv-api-web";
+import { AssetInfoLink, SocialLinkViewModel } from "gv-api-web";
 import * as React from "react";
 
 const _SocialLinksBlock: React.FC<Props> = ({ socialLinks }) => {
   return (
     <div className="social-links-block">
-      {socialLinks.map(socialLink => (
-        <a
-          key={socialLink.type}
-          href={socialLink.url + socialLink.value}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="social-links-block__social-link"
-        >
-          <SocialLinkImage url={socialLink.logo} alt={socialLink.name} />
-        </a>
-      ))}
+      {socialLinks.map(socialLink => {
+        const value = "value" in socialLink ? socialLink.value : 0;
+        return (
+          <a
+            key={socialLink.type}
+            href={socialLink.url + value}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-links-block__social-link"
+          >
+            <SocialLinkImage url={socialLink.logo} alt={socialLink.name} />
+          </a>
+        );
+      })}
     </div>
   );
 };
@@ -26,5 +29,5 @@ const SocialLinksBlock = React.memo(_SocialLinksBlock);
 export default SocialLinksBlock;
 
 interface Props {
-  socialLinks: SocialLinkViewModel[];
+  socialLinks: Array<SocialLinkViewModel | AssetInfoLink>;
 }
