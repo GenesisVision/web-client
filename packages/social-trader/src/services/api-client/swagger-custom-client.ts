42f928655e3e9e280436fec81d72bbefa6e288a5
e.grebenshikov <evgreb@gmail.com>
2019-12-24 14:51:41 +0300

    remove parse date

--- packages/social-trader/src/services/api-client/swagger-custom-client.ts
+++ packages/social-trader/src/services/api-client/swagger-custom-client.ts
@@ -1,9 +1,5 @@
 import { ApiClient } from "gv-api-web";
 
-ApiClient.parseDate = str => {
-  return new Date(str);
-};
-
 const apiClient = ApiClient.instance;
 //@ts-ignore TODO fix types
 apiClient.basePath = process.env.REACT_APP_API_URL;
