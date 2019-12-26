ced39d5ae632ec0f97cb9704a737ccec9240bcef
pavelzubov <pasha224@yandex.ru>
2019-12-26 08:15:20 +0300

    Use DashboardNewUserBlock and new translations for empty block

--- packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-trading-statistic.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-trading-statistic.tsx
@@ -1,9 +1,7 @@
-import GVButton from "components/gv-button";
-import Link from "components/link/link";
-import { useToLink } from "components/link/link.helper";
 import { StatisticItemList } from "components/statistic-item-list/statistic-item-list";
 import { CREATE_ACCOUNT_PAGE_ROUTE } from "pages/create-account/create-account.constants";
 import { CREATE_FUND_PAGE_ROUTE } from "pages/create-fund/create-fund.constants";
+import { DashboardNewUserBlock } from "pages/dashboard/components/dashboard-statistic/dashboard-new-user.block";
 import DashboardStatisticContainer from "pages/dashboard/components/dashboard-statistic/dashboard-statistic.container";
 import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
 import { TDashboardTradingStatistic } from "pages/dashboard/dashboard.types";
@@ -49,32 +47,37 @@ const _DashboardTradingStatistic: React.FC<Props> = ({
 };
 
 const DashboardTradingEmpty: React.FC = React.memo(() => {
-  const { linkCreator } = useToLink();
   const [t] = useTranslation();
   return (
-    <div className=" dashboard-statistic__values">
-      <div className="dashboard-statistic__create-block">
-        <h1>{t("dashboard-page.statistic.get-started-title")}</h1>
-        <div className="dashboard-statistic__create-block-text">
-          {t("dashboard-page.statistic.get-started-account")}
-        </div>
-        <div className="dashboard-statistic__create-block-links">
-          <Link to={linkCreator(CREATE_ACCOUNT_PAGE_ROUTE)}>
-            <GVButton color="primary">{t("buttons.create-account")}</GVButton>
-          </Link>
-        </div>
-      </div>
-      <div className="dashboard-statistic__create-block">
-        <div className="dashboard-statistic__create-block-text">
-          {t("dashboard-page.statistic.get-started-fund")}
-        </div>
-        <div className="dashboard-statistic__create-block-links">
-          <Link to={linkCreator(CREATE_FUND_PAGE_ROUTE)}>
-            <GVButton color="primary">{t("buttons.create-fund")}</GVButton>
-          </Link>
-        </div>
-      </div>
-    </div>
+    <DashboardNewUserBlock
+      leftField={{
+        link: CREATE_ACCOUNT_PAGE_ROUTE,
+        linkLabel: t(
+          "dashboard-page.statistic.get-started.trading.left-field.button"
+        ),
+        text: (
+          <>
+            {t("dashboard-page.statistic.get-started.trading.left-field.text")}{" "}
+            <span className="dashboard-new-user-block__text--muted">
+              {t(
+                "dashboard-page.statistic.get-started.trading.left-field.text-2"
+              )}
+            </span>
+          </>
+        )
+      }}
+      rightField={{
+        link: CREATE_FUND_PAGE_ROUTE,
+        linkLabel: t(
+          "dashboard-page.statistic.get-started.trading.right-field.button"
+        ),
+        text: (
+          <>
+            {t("dashboard-page.statistic.get-started.trading.right-field.text")}
+          </>
+        )
+      }}
+    />
   );
 });
 
