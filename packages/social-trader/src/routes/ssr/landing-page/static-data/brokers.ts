78c719c69068e5fa6fb30582c436a68d5bd7f36d
Anchello <bob.m@mail.ru>
2019-12-26 21:51:49 +0100

    finished brokers section

--- packages/social-trader/src/routes/ssr/landing-page/static-data/brokers.ts
+++ packages/social-trader/src/routes/ssr/landing-page/static-data/brokers.ts
@@ -1,6 +1,7 @@
 import { TTabsItem } from "routes/ssr/landing-page/components/tab-controls/tab-controls";
 import BinanceLogo from "routes/ssr/landing-page/images/brokers/binance.png";
 import ExanteLogo from "routes/ssr/landing-page/images/brokers/exante.png";
+import GMBg from "routes/ssr/landing-page/images/brokers/gm-bg.svg";
 import GMLogo from "routes/ssr/landing-page/images/brokers/gm.svg";
 import HuobiLogo from "routes/ssr/landing-page/images/brokers/huobi.png";
 import Just2tradeLogo from "routes/ssr/landing-page/images/brokers/just2trade.png";
@@ -88,7 +89,7 @@ export const brokersInfo: TBrokerInfo[] = [
       },
       {
         text: "BTC, USD Accounts",
-        imageBg: ""
+        imageBg: GMBg
       },
       {
         text: "Crypto Margin Trading"
@@ -110,25 +111,17 @@ export const brokersInfo: TBrokerInfo[] = [
       "Binance is one of the largest global cryptocurrency exchanges that provides an ecosystem for trading a wide range of different crypto assets.\n",
     listItems: [
       {
-        text: "Forex, Metals, Commodities, Indices, Shares, Crypto"
+        number: "600+",
+        text: "Crypto Assets"
       },
       {
-        text: "MetaTrader 5"
+        text: "Binance Web Trading Terminal"
       },
       {
-        text: "BTC, USD Accounts",
-        imageBg: ""
+        text: "GV Follow functionality"
       },
       {
-        text: "Crypto Margin Trading"
-      },
-      {
-        number: "500+",
-        text: "Altcoins"
-      },
-      {
-        number: "74",
-        text: "Forex Instruments"
+        text: "Plug-in your existing accounts"
       }
     ]
   },
@@ -139,25 +132,21 @@ export const brokersInfo: TBrokerInfo[] = [
       "RoboForex Ltd is an international IFSC Belize regulated broker which is a member of the RoboForex group. RoboForex provides an access to a wide range of financial instruments and account types.",
     listItems: [
       {
-        text: "Forex, Metals, Commodities, Indices, Shares, Crypto"
+        number: "9400+",
+        text: "assets"
       },
       {
-        text: "MetaTrader 5"
+        text:
+          "Forex, Stocks, Indices, ETFs, Commodities, Metals, Energies, Crypto"
       },
       {
-        text: "BTC, USD Accounts",
-        imageBg: ""
+        text: "MetaTrader 4"
       },
       {
-        text: "Crypto Margin Trading"
+        text: "Pro-Standard, ECN Accounts"
       },
       {
-        number: "500+",
-        text: "Altcoins"
-      },
-      {
-        number: "74",
-        text: "Forex Instruments"
+        text: "Leverage 1:10-1:100"
       }
     ]
   },
@@ -168,25 +157,18 @@ export const brokersInfo: TBrokerInfo[] = [
       "EXANTE is a European investment services company established in 2011 that offers global multi-asset financial services, including direct access to a wide range of financial markets in the US, European Union and Asia-Pacific",
     listItems: [
       {
-        text: "Forex, Metals, Commodities, Indices, Shares, Crypto"
+        number: "10000+",
+        text: "assets"
       },
       {
-        text: "MetaTrader 5"
+        text:
+          "Forex, Stocks, ETFs, global futures markets, bonds, metals, options, funds, crypto"
       },
       {
-        text: "BTC, USD Accounts",
-        imageBg: ""
+        text: "Unique EXANTE Trading Platform"
       },
       {
-        text: "Crypto Margin Trading"
-      },
-      {
-        number: "500+",
-        text: "Altcoins"
-      },
-      {
-        number: "74",
-        text: "Forex Instruments"
+        text: "Leverage 1:50"
       }
     ]
   },
@@ -197,25 +179,14 @@ export const brokersInfo: TBrokerInfo[] = [
       "Huobi is one of the largest Singapore-based cryptocurrency exchanges,  judging by 24-hour period volume. Founded in China, the company now has offices in Hong Kong, Korea, Japan and the United States.",
     listItems: [
       {
-        text: "Forex, Metals, Commodities, Indices, Shares, Crypto"
+        number: "400+",
+        text: "crypto trading pairs"
       },
       {
-        text: "MetaTrader 5"
+        text: "BTC, ETH, USDT Accounts"
       },
       {
-        text: "BTC, USD Accounts",
-        imageBg: ""
-      },
-      {
-        text: "Crypto Margin Trading"
-      },
-      {
-        number: "500+",
-        text: "Altcoins"
-      },
-      {
-        number: "74",
-        text: "Forex Instruments"
+        text: "Huobi Web Trading Terminal"
       }
     ]
   },
@@ -226,25 +197,13 @@ export const brokersInfo: TBrokerInfo[] = [
       "Just2Trade Inc. is a US-based financial services company with offices in New York and Cyprus and a subsidiary of Russian broker Finam Holdings. Just2Trade is a member of FINRA & SIPC, NFA regulatory organizations",
     listItems: [
       {
-        text: "Forex, Metals, Commodities, Indices, Shares, Crypto"
+        text: "Forex, Metals, CFD, Indices"
       },
       {
-        text: "MetaTrader 5"
+        text: "MetaTrader 4"
       },
       {
-        text: "BTC, USD Accounts",
-        imageBg: ""
-      },
-      {
-        text: "Crypto Margin Trading"
-      },
-      {
-        number: "500+",
-        text: "Altcoins"
-      },
-      {
-        number: "74",
-        text: "Forex Instruments"
+        text: "Leverage 1:10-1:100"
       }
     ]
   }
