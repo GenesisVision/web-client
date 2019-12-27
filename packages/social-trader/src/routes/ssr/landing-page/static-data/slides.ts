3428031b44274630252a96f7b9d27ccc5ae7ae73
r-kamenskiy <r@kamenskiy.net>
2019-12-27 20:22:06 +0300

    Update slides.ts

--- packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
+++ packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
@@ -15,7 +15,7 @@ export const slides: TSlides[] = [
     id: 0,
     title: "Pay with card",
     text:
-      "Forget about the burden of having to use an exchange to buy crypto and use your credit card to purchase all sorts of coins necessary to get started with the platform.",
+      "Use your credit or debit card to buy crypto - get started right away as a trader, an investor or a manager on the Genesis Vision platform.",
     image: ImgCreditCard,
     link: WALLET_TOTAL_PAGE_ROUTE
   },
@@ -23,7 +23,7 @@ export const slides: TSlides[] = [
     id: 1,
     title: "Enticing invitations",
     text:
-      "Use the Genesis Vision two-level Affiliate Network to receive a percentage of the commission paid by the people you invite, and also the people who were invited by them!",
+      "Use the Genesis Vision two-level Affiliate Network to receive a percentage of the commission paid by your referrals… and your referrals’ referrals!",
     image: ImgPeople,
     link: "/referral-program"
   }
