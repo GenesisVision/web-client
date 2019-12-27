dc9781c4510c9fa633166afef9b600d0b6f62afa
pavelzubov <pasha224@yandex.ru>
2019-12-27 13:51:51 +0300

    Add left modificator Add hiding shadow on end of list

--- packages/social-trader/src/pages/dashboard/components/dashboard-block/dashboard-horizontal-list.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-block/dashboard-horizontal-list.tsx
@@ -1,18 +1,41 @@
 import "./dashboard-block.scss";
 
 import classNames from "classnames";
-import React from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 const _DashboardHorizontalList: React.FC<
-  { darkShadow?: boolean } & React.HTMLAttributes<HTMLDivElement>
-> = ({ children, darkShadow }) => {
+  { darkShadow?: boolean; left?: boolean } & React.HTMLAttributes<
+    HTMLDivElement
+  >
+> = ({ children, darkShadow, left }) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [endOfList, setEndOfList] = useState(0);
+  const [scroll, setScroll] = useState(0);
+  useEffect(() => {
+    if (!ref.current) return;
+    left && (ref.current.scrollLeft = 100000);
+    setEndOfList(left ? 0 : ref.current.scrollWidth - ref.current.offsetWidth);
+  }, [ref, left, setEndOfList]);
+  const handleScroll = useCallback(() => {
+    if (!ref.current) return;
+    setScroll(ref.current.scrollLeft);
+  }, []);
   return (
     <div
       className={classNames("dashboard-horizontal-list__shadow-wrapper", {
+        "dashboard-horizontal-list__shadow-wrapper--hided":
+          scroll === endOfList,
+        "dashboard-horizontal-list__shadow-wrapper--left": left,
         "dashboard-horizontal-list__shadow-wrapper--dark-shadow": darkShadow
       })}
     >
-      <div className="dashboard-horizontal-list">{children}</div>
+      <div
+        ref={ref}
+        onScroll={handleScroll}
+        className="dashboard-horizontal-list"
+      >
+        {children}
+      </div>
     </div>
   );
 };
