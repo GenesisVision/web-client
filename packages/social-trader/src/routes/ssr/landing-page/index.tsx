7d62196be8b043df5dc392e553d1e0e3286a02fc
pavelzubov <pasha224@yandex.ru>
2019-12-29 00:03:08 +0300

    Divide index landing page Add setCookie for referral

--- packages/social-trader/src/routes/ssr/landing-page/index.tsx
+++ packages/social-trader/src/routes/ssr/landing-page/index.tsx
@@ -10,86 +10,22 @@ import {
 import { fetchFollows } from "modules/follows-table/services/follows-table.service";
 import { fetchFunds } from "modules/funds-table/services/funds-table.service";
 import { fetchPrograms } from "modules/programs-table/services/programs-table.service";
-import { NextPage } from "next";
+import { NextPage, NextPageContext } from "next";
+import * as qs from "qs";
 import React from "react";
-import FirstScreen from "routes/ssr/landing-page/components/first-screen/first-screen";
-import AdvantagesContainer from "routes/ssr/landing-page/containers/advantages-container/advantages-container";
-import BrokersContainer from "routes/ssr/landing-page/containers/brokers-container/brokers-container";
-import DownloadContainer from "routes/ssr/landing-page/containers/download-container/download-container";
-import EventsContainer from "routes/ssr/landing-page/containers/events-container/events-container";
-import FollowsContainer from "routes/ssr/landing-page/containers/follows-container/follows-container";
-import FundsContainer from "routes/ssr/landing-page/containers/funds-container/funds-container";
-import InfoContainer from "routes/ssr/landing-page/containers/info-container/info-container";
-import ProgramsContainer from "routes/ssr/landing-page/containers/programs-container/programs-container";
-import SocialContainer from "routes/ssr/landing-page/containers/social-container/social-container";
-import Layout from "routes/ssr/landing-page/layouts/_layout";
-import {
-  brokersInfo,
-  brokersTabs
-} from "routes/ssr/landing-page/static-data/brokers";
+import { LandingPage } from "routes/ssr/landing-page/landing.page";
 import platformApi from "services/api-client/platform-api";
 import { subtractDate } from "shared/utils/dates";
 import { addRequestAnimationFrame } from "utils/helpers";
 
-const IndexPage: NextPage<{
-  programsData: ItemsViewModelProgramDetailsListItem;
-  fundsData: ItemsViewModelFundDetailsListItem;
-  followsData: ItemsViewModelFollowDetailsListItem;
-  eventsData: PlatformEvents;
-}> = ({ programsData, fundsData, followsData, eventsData }) => {
-  return (
-    <Layout title="Genesis Vision">
-      <main className="home">
-        <FirstScreen />
-        <EventsContainer events={eventsData.events} />
-        <section className="home__section home__section--bg-gray">
-          <div className="home__container">
-            <FollowsContainer follows={followsData.items} />
-          </div>
-        </section>
-        <section className="home__section">
-          <div className="home__container">
-            <ProgramsContainer programs={programsData.items} />
-          </div>
-        </section>
-        <section className="home__section home__section--bg-gray">
-          <div className="home__container">
-            <FundsContainer funds={fundsData.items} />
-          </div>
-        </section>
-        <section id="info" className="home__section home__section--bg-white">
-          <div className="home__container">
-            <InfoContainer />
-          </div>
-        </section>
-        <section className="home__section home__section--bg-gray">
-          <div className="home__container">
-            <DownloadContainer />
-          </div>
-        </section>
-        <AdvantagesContainer />
-        <section className="home__section home__section--bg-gray home__section--horizontal-padding">
-          <div className="home__container">
-            <BrokersContainer
-              brokersInfo={brokersInfo}
-              brokersTabs={brokersTabs}
-              title="Brokers and Exhanges"
-            />
-          </div>
-        </section>
-        <section className="home__section home__section--last-screen">
-          <div className="home__container">
-            <SocialContainer />
-          </div>
-        </section>
-      </main>
-    </Layout>
-  );
+const IndexPage: NextPage<Props> = props => {
+  return <LandingPage {...props} />;
 };
 
-export default IndexPage;
-
-IndexPage.getInitialProps = async () => {
+IndexPage.getInitialProps = async (ctx: NextPageContext) => {
+  const { asPath = "", pathname } = ctx;
+  const { ref } = qs.parse(asPath.slice(pathname.length + 1));
+  console.log("ref : ", ref);
   try {
     addRequestAnimationFrame();
     const dateTo = new Date();
@@ -126,7 +62,13 @@ IndexPage.getInitialProps = async () => {
         take: 5
       })
     ]);
-    return { programsData, fundsData, followsData, eventsData };
+    return {
+      programsData,
+      fundsData,
+      followsData,
+      eventsData,
+      refLink: ref
+    };
   } catch (e) {
     const programsData = {
       total: 0,
@@ -143,6 +85,22 @@ IndexPage.getInitialProps = async () => {
     const eventsData = {
       events: []
     };
-    return { programsData, fundsData, followsData, eventsData };
+    return {
+      programsData,
+      fundsData,
+      followsData,
+      eventsData,
+      refLink: ""
+    };
   }
 };
+
+interface Props {
+  refLink?: string;
+  programsData: ItemsViewModelProgramDetailsListItem;
+  fundsData: ItemsViewModelFundDetailsListItem;
+  followsData: ItemsViewModelFollowDetailsListItem;
+  eventsData: PlatformEvents;
+}
+
+export default IndexPage;
