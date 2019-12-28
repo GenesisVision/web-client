38b6ce59b0d195943545d33ca6f0b3d911797174
pavelzubov <pasha224@yandex.ru>
2019-12-28 11:07:56 +0300

    Add isAuthenticated and conditional button

--- packages/social-trader/src/routes/ssr/landing-page/components/lp-header/lp-header.tsx
+++ packages/social-trader/src/routes/ssr/landing-page/components/lp-header/lp-header.tsx
@@ -2,18 +2,21 @@ import "./lp-header.scss";
 
 import Link from "components/link/link";
 import React from "react";
-import { HOME_ROUTE } from "routes/app.routes";
+import { useSelector } from "react-redux";
+import { isAuthenticatedSelector } from "reducers/auth-reducer";
+import { HOME_ROUTE, SIGNUP_ROUTE } from "routes/app.routes";
+import { OVERVIEW_ROUTE } from "routes/dashboard.routes";
 import LPButton from "routes/ssr/landing-page/components/lp-button/lp-button";
 import { MainLogo } from "routes/ssr/landing-page/components/main-logo/main-logo";
 import MobileNav from "routes/ssr/landing-page/components/mobile-nav/mobile-nav";
 import NavList from "routes/ssr/landing-page/components/nav/nav-list";
 import {
   navFooter,
-  navHeader,
-  START_ROUTE
+  navHeader
 } from "routes/ssr/landing-page/static-data/nav-links";
 
 const LPHeader: React.FC = () => {
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
   return (
     <header className="lp-header">
       <div className="lp-header__container">
@@ -33,8 +36,11 @@ const LPHeader: React.FC = () => {
           </div>
           <NavList menuItems={navHeader} className="lp-header__nav" />
           <div className="lp-header__start-btn">
-            <LPButton color="secondary" href={START_ROUTE}>
-              Get started
+            <LPButton
+              color="secondary"
+              href={isAuthenticated ? OVERVIEW_ROUTE : SIGNUP_ROUTE}
+            >
+              {isAuthenticated ? "Dashboard" : "Get started"}
             </LPButton>
           </div>
         </div>
