0a7cbb1b74f9f03c77240772e5f455792de35f97
Anchello <bob.m@mail.ru>
2019-12-28 17:28:15 +0100

    fixed nav links

--- packages/social-trader/src/routes/ssr/landing-page/components/nav/nav-subitem.tsx
+++ packages/social-trader/src/routes/ssr/landing-page/components/nav/nav-subitem.tsx
@@ -1,7 +1,5 @@
 import classNames from "classnames";
 import Link from "components/link/link";
-import { normalizeUrlString } from "components/link/link.helper";
-import { useRouter } from "next/router";
 import React from "react";
 import { TNavHeader } from "routes/ssr/landing-page/static-data/nav-links";
 
@@ -17,25 +15,27 @@ const _NavSubItem: React.FC<INavSubItemProps> = ({
   hideMobile,
   onClick
 }) => {
-  const { route } = useRouter();
   return (
     <li
       className={classNames("nav-list__item", {
         "nav-list__item--hide-mobile": hideMobile
       })}
     >
-      <Link
-        onClick={onClick}
-        to={{ pathname: href as string, state }}
-        className={classNames("nav-list__link", {
-          "nav-list__link--active": route.startsWith(
-            normalizeUrlString(String(href))
-          )
-        })}
-      >
-        {icon && <span className="nav-list__link-icon">{icon}</span>}
-        {name}
-      </Link>
+      {href && href.includes("http") ? (
+        <a href={href} className="nav-list__link">
+          {icon && <span className="nav-list__link-icon">{icon}</span>}
+          {name}
+        </a>
+      ) : (
+        <Link
+          onClick={onClick}
+          to={{ pathname: href as string, state }}
+          className="nav-list__link"
+        >
+          {icon && <span className="nav-list__link-icon">{icon}</span>}
+          {name}
+        </Link>
+      )}
     </li>
   );
 };
