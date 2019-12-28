6104d64822f7ca9228db134d0e3d487c8e4962c7
pavelzubov <pasha224@yandex.ru>
2019-12-28 09:50:32 +0300

    Use BROKER_PARAM_NAME in requestParams

--- packages/social-trader/src/pages/create-account/create-account.helpers.ts
+++ packages/social-trader/src/pages/create-account/create-account.helpers.ts
@@ -1,3 +1,4 @@
+import { BROKER_PARAM_NAME } from "pages/create-account/create-account.constants";
 import * as qs from "qs";
 import { NextPageWithReduxContext } from "utils/types";
 
@@ -5,6 +6,6 @@ export const getBrokerFromContext = ({
   asPath = "",
   pathname
 }: NextPageWithReduxContext): string => {
-  const { broker } = qs.parse(asPath.slice(pathname.length + 1));
-  return broker;
+  const requestParams = qs.parse(asPath.slice(pathname.length + 1));
+  return requestParams[BROKER_PARAM_NAME];
 };
