38835b2e1deb958cb0cd01d423f91465165eca81
Anchello <bob.m@mail.ru>
2019-12-28 15:58:13 +0100

    added fees page

---
+++ packages/social-trader/pages/fees/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Fees } from "routes/ssr/fees";
+
+export default Fees;
---
+++ packages/social-trader/src/routes/ssr/landing-page/components/fees-section/fees-section.tsx
@@ -0,0 +1,46 @@
+import "./fees-section.scss";
+
+import React, { useCallback, useState } from "react";
+import { animated, config, useTransition } from "react-spring";
+import FeesGeneral from "routes/ssr/landing-page/components/fees-info/fees-general";
+import FeesTrading from "routes/ssr/landing-page/components/fees-info/fees-trading";
+import InfoList from "routes/ssr/landing-page/components/info-list/info-list";
+import TabControls from "routes/ssr/landing-page/components/tab-controls/tab-controls";
+import { feesTabs } from "routes/ssr/landing-page/static-data/fees";
+import { infoList, infoTabs } from "routes/ssr/landing-page/static-data/info";
+
+const FeesSection: React.FC = () => {
+  const [currentTabId, setCurrentTab] = useState(0);
+
+  const handleChange = useCallback(
+    (id: number) => {
+      console.log(id);
+      if (id !== undefined && id !== currentTabId) setCurrentTab(id);
+    },
+    [currentTabId]
+  );
+  return (
+    <section className="fees-section">
+      <div className="internal__container">
+        <div className="fees-section__wrapper-controls">
+          <h1 className="internal__title">Fees</h1>
+          <p className="fees-section__text">
+            The list of fees charged by Genesis Vision platform
+          </p>
+          <TabControls
+            currentTabId={currentTabId}
+            tabsItems={feesTabs}
+            onChange={handleChange}
+            className="fees-section__controls"
+          />
+        </div>
+      </div>
+      <div className="fees-section__tab-info">
+        {currentTabId === 0 && <FeesGeneral />}
+        {currentTabId === 1 && <FeesTrading />}
+      </div>
+    </section>
+  );
+};
+
+export default FeesSection;
