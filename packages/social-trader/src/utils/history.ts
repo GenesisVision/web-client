7d09eca3537d0f502c8bd322fcf7b5e7a28b513b
pavelzubov <pasha224@yandex.ru>
2019-12-31 12:40:40 +0300

    Correct history import

--- packages/social-trader/src/utils/history.ts
+++ packages/social-trader/src/utils/history.ts
@@ -1,4 +1,4 @@
-import { createBrowserHistory as createHistory } from "utils/history";
+import { createBrowserHistory as createHistory } from "history";
 
 const history = createHistory({ basename: process.env.REACT_ROOT_ROUTE });
 export default history;
