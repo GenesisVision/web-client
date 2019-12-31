df0e265b4dfdca084c9534bcf18ed56b8640dd56
pavelzubov <pasha224@yandex.ru>
2019-12-31 17:08:24 +0300

    Move currentWallet find from WalletDeposit to WalletAddFundsContainer

--- packages/social-trader/src/modules/wallet-add-funds/components/wallet-add-funds-container.tsx
+++ packages/social-trader/src/modules/wallet-add-funds/components/wallet-add-funds-container.tsx
@@ -1,15 +1,20 @@
 import "./wallet-add-funds-form.scss";
 
 import { DialogLoader } from "components/dialog/dialog-loader/dialog-loader";
-import { WalletData } from "gv-api-web";
 import { walletsSelector } from "pages/wallet/reducers/wallet.reducers";
 import * as React from "react";
 import { useSelector } from "react-redux";
+import { safeGetElemFromArray } from "utils/helpers";
+import { CurrencyEnum } from "utils/types";
 
 import WalletAddFundsForm from "./wallet-add-funds-form";
 
-const _WalletAddFundsContainer: React.FC<Props> = ({ currentWallet }) => {
+const _WalletAddFundsContainer: React.FC<Props> = ({ currentCurrency }) => {
   const wallets = useSelector(walletsSelector);
+  const currentWallet = safeGetElemFromArray(
+    wallets,
+    ({ currency }) => currency === currentCurrency
+  );
   return (
     <WalletAddFundsForm
       condition={!!wallets.length && !!currentWallet}
@@ -21,7 +26,7 @@ const _WalletAddFundsContainer: React.FC<Props> = ({ currentWallet }) => {
 };
 
 interface Props {
-  currentWallet: WalletData;
+  currentCurrency: CurrencyEnum;
 }
 
 const WalletAddFundsContainer = React.memo(_WalletAddFundsContainer);
