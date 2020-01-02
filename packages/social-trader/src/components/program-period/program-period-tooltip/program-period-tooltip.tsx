4e2b9b8d50f9f5c328452779e062eeb0b7d583b7
pavelzubov <pasha224@yandex.ru>
2020-01-02 20:39:41 +0300

    Use TooltipContent, StatisticItem Remove unused styles in program-period-tooltip

--- packages/social-trader/src/components/program-period/program-period-tooltip/program-period-tooltip.tsx
+++ packages/social-trader/src/components/program-period/program-period-tooltip/program-period-tooltip.tsx
@@ -1,24 +1,30 @@
 import "./program-period-tooltip.scss";
 
+import StatisticItem from "components/statistic-item/statistic-item";
+import { TooltipContent } from "components/tooltip/tooltip-content";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import PeriodTimeLeft from "../period-time-left";
 
-const _ProgramPeriodTooltip: React.FC<Props> = ({ t, end }) => (
-  <div className="program-period-tooltip">
-    <div className="program-period-tooltip__header">
-      {t("program-period.time-left")}
-    </div>
-    <div className="program-period-tooltip__content">
-      <PeriodTimeLeft periodEnds={end} />
-    </div>
-  </div>
-);
+const _ProgramPeriodTooltip: React.FC<Props> = ({ end }) => {
+  const [t] = useTranslation();
+  return (
+    <TooltipContent>
+      <StatisticItem
+        withPadding={false}
+        accent
+        label={t("program-period.time-left")}
+      >
+        <PeriodTimeLeft periodEnds={end} />
+      </StatisticItem>
+    </TooltipContent>
+  );
+};
 
-interface Props extends WithTranslation {
+interface Props {
   end: Date;
 }
 
-const ProgramPeriodTooltip = translate()(React.memo(_ProgramPeriodTooltip));
+const ProgramPeriodTooltip = React.memo(_ProgramPeriodTooltip);
 export default ProgramPeriodTooltip;
