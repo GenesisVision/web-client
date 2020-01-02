19de0868fb19f8aa81d33011f2dc4922d7e00131
pavelzubov <pasha224@yandex.ru>
2020-01-02 20:34:01 +0300

    Use PopoverContent, PopoverContentCardBlock Remove unused styles in level-tooltip

--- packages/social-trader/src/components/level-tooltip/level-tooltip.tsx
+++ packages/social-trader/src/components/level-tooltip/level-tooltip.tsx
@@ -1,22 +1,26 @@
 import "./level-tooltip.scss";
 
+import { PopoverContentCardBlock } from "components/popover/popover-card.block";
+import { PopoverContent } from "components/popover/popover-content";
 import StatisticItem from "components/statistic-item/statistic-item";
 import * as React from "react";
 import { WithTranslation, withTranslation as translate } from "react-i18next";
 
 const _LevelTooltip: React.FC<ILevelTooltip> = ({ t, level, canLevelUp }) => (
-  <div className="level-tooltip">
-    <div className="level-tooltip__level">
-      {t("level-tooltip.genesis-level")} {level}
-    </div>
-    <StatisticItem
-      condition={!!canLevelUp}
-      accent
-      label={t("level-tooltip.level-up")}
-    >
-      {t("level-tooltip.top10")}
-    </StatisticItem>
-  </div>
+  <PopoverContent>
+    <PopoverContentCardBlock>
+      <div className="level-tooltip__level">
+        {t("level-tooltip.genesis-level")} {level}
+      </div>
+      <StatisticItem
+        condition={!!canLevelUp}
+        accent
+        label={t("level-tooltip.level-up")}
+      >
+        {t("level-tooltip.top10")}
+      </StatisticItem>
+    </PopoverContentCardBlock>
+  </PopoverContent>
 );
 
 const LevelTooltip = translate()(React.memo(_LevelTooltip));
