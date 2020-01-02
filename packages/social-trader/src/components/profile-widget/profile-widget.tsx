0ffbc6a40a21686c64837fb15841db72eb4dcad9
pavelzubov <pasha224@yandex.ru>
2020-01-02 20:37:12 +0300

    Use PopoverContent, PopoverContentCardBlock, PopoverContentCardBlockItem Remove unused styles in profile-widget

--- packages/social-trader/src/components/profile-widget/profile-widget.tsx
+++ packages/social-trader/src/components/profile-widget/profile-widget.tsx
@@ -8,9 +8,14 @@ import { LogoutIcon } from "components/icon/logout-icon";
 import { ReferrerIcon } from "components/icon/referrer-icon";
 import { SecurityIcon } from "components/icon/security-icon";
 import { SettingsIcon } from "components/icon/settings-icon";
-import Link from "components/link/link";
+import Link, { ToType } from "components/link/link";
 import { useToLink } from "components/link/link.helper";
 import Popover, { HORIZONTAL_POPOVER_POS } from "components/popover/popover";
+import {
+  PopoverContentCardBlock,
+  PopoverContentCardBlockItem
+} from "components/popover/popover-card.block";
+import { PopoverContent } from "components/popover/popover-content";
 import {
   PROFILE_ROUTE,
   REFERRAL_PROGRAM_ROUTE,
@@ -23,6 +28,33 @@ import { ProfileHeaderViewModel } from "gv-api-web";
 import useAnchor from "hooks/anchor.hook";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+
+const ProfileMenuItem: React.FC<{
+  to?: ToType | string;
+  onClick: VoidFunction;
+  label: any;
+  Icon: React.ComponentType;
+}> = React.memo(({ Icon, to, onClick, label }) => {
+  const renderLabel = () => (
+    <div className={classNames("profile-menu__item")}>
+      <Icon />
+      <div className="profile-menu__item-label">{label}</div>
+    </div>
+  );
+  const renderButton = () =>
+    to ? (
+      <Link to={to} onClick={onClick}>
+        {renderLabel()}
+      </Link>
+    ) : (
+      <GVButton variant="text" color={"danger"} noPadding onClick={onClick}>
+        {renderLabel()}
+      </GVButton>
+    );
+  return (
+    <PopoverContentCardBlockItem>{renderButton()}</PopoverContentCardBlockItem>
+  );
+});
 
 const _ProfileWidget: React.FC<Props> = ({
   profileHeader,
@@ -47,46 +79,43 @@ const _ProfileWidget: React.FC<Props> = ({
         onClose={clearAnchor}
         horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
       >
-        <div className="profile-menu">
-          <div className="profile-menu__header">{profileHeader.email}</div>
-          <div className="profile-menu__container">
-            <div className="profile-menu__item profile-menu__item--details">
-              <Link to={linkCreator(PROFILE_ROUTE)} onClick={clearAnchor}>
-                <DetailsIcon />
-                {t("profile-widget.personal-details")}
-              </Link>
-            </div>
-            <div className="profile-menu__item profile-menu__item--settings">
-              <Link to={linkCreator(SETTINGS_ROUTE)} onClick={clearAnchor}>
-                <SettingsIcon />
-                {t("profile-widget.settings")}
-              </Link>
-            </div>
-            <div className="profile-menu__item profile-menu__item--security">
-              <Link to={linkCreator(SECURITY_ROUTE)} onClick={clearAnchor}>
-                <SecurityIcon />
-                {t("profile-widget.security")}
-              </Link>
-            </div>
-            <div className="profile-menu__item profile-menu__item--referrer">
-              <Link
-                to={linkCreator(REFERRAL_PROGRAM_ROUTE)}
-                onClick={clearAnchor}
-              >
-                <ReferrerIcon />
-                {t("profile-page.tabs.referral-program")}
-              </Link>
-            </div>
-            <div className="profile-menu__item profile-menu__item--logout">
-              <GVButton variant="text" onClick={logout}>
-                <>
-                  <LogoutIcon />
-                  {t("profile-widget.logout")}
-                </>
-              </GVButton>
-            </div>
-          </div>
-        </div>
+        <PopoverContent>
+          <PopoverContentCardBlock dark>
+            <div className="profile-menu__header">{profileHeader.email}</div>
+          </PopoverContentCardBlock>
+          <PopoverContentCardBlock>
+            <ProfileMenuItem
+              Icon={DetailsIcon}
+              to={linkCreator(PROFILE_ROUTE)}
+              onClick={clearAnchor}
+              label={t("profile-widget.personal-details")}
+            />
+            <ProfileMenuItem
+              Icon={SettingsIcon}
+              to={linkCreator(SETTINGS_ROUTE)}
+              onClick={clearAnchor}
+              label={t("profile-widget.settings")}
+            />
+            <ProfileMenuItem
+              Icon={SecurityIcon}
+              to={linkCreator(SECURITY_ROUTE)}
+              onClick={clearAnchor}
+              label={t("profile-widget.security")}
+            />
+            <ProfileMenuItem
+              Icon={ReferrerIcon}
+              to={linkCreator(REFERRAL_PROGRAM_ROUTE)}
+              onClick={clearAnchor}
+              label={t("profile-page.tabs.referral-program")}
+            />
+            <div className="profile-menu__separator" />
+            <ProfileMenuItem
+              Icon={LogoutIcon}
+              onClick={logout}
+              label={t("profile-widget.logout")}
+            />
+          </PopoverContentCardBlock>
+        </PopoverContent>
       </Popover>
     </div>
   );
