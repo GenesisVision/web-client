5e58240da484bb2caa564a11479c40a9789a8af8
pavelzubov <pasha224@yandex.ru>
2020-01-11 15:14:55 +0300

    Set setOverItem to MultiPieContainer Add --selected to dashboard-pie-chart__name-container

--- packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-pie-chart/dashboard-pie-chart.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import MultiPieContainer from "components/pie-container/multi-pie.container";
 import { withBlurLoader } from "decorators/with-blur-loader";
 import React, { useState } from "react";
@@ -8,6 +9,7 @@ const _DashboardPieChart: React.FC<Props> = ({ data }) => {
     <div className="dashboard-pie-chart__container">
       <div className="dashboard-pie-chart__pie-chart">
         <MultiPieContainer
+          setOverItem={setOverItem}
           over={overItem}
           data={data.map(item => ({ ...item, value: item.percent }))}
         />
@@ -15,7 +17,9 @@ const _DashboardPieChart: React.FC<Props> = ({ data }) => {
       <div className="dashboard-pie-chart__names">
         {data.map(({ name, color, percent }) => (
           <div
-            className="dashboard-pie-chart__name-container"
+            className={classNames("dashboard-pie-chart__name-container", {
+              "dashboard-pie-chart__name-container--selected": name === overItem
+            })}
             onMouseEnter={() => setOverItem(name)}
             onMouseLeave={() => setOverItem(undefined)}
           >
