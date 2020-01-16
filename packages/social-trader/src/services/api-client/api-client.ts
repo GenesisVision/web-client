aad56df3a39ec02d063db289a58bc47b22d8aa9e
e.grebenshikov <evgreb@gmail.com>
2020-01-16 14:23:18 +0300

    update api

--- packages/social-trader/src/services/api-client/api-client.ts
+++
@@ -1,5 +0,0 @@
-import { ApiClient, SignalDetachMode } from "gv-api-web";
-
-const newClient = new ApiClient(process.env.REACT_APP_API_URL);
-
-export default newClient;
--- packages/social-trader/src/services/api-client/swagger-custom-client.ts
+++ packages/social-trader/src/services/api-client/swagger-custom-client.ts
@@ -1,5 +1,6 @@
 import { ApiClient } from "gv-api-web";
+import fetch from "isomorphic-unfetch";
 
-const newClient = new ApiClient(process.env.REACT_APP_API_URL);
+const newClient = new ApiClient(process.env.REACT_APP_API_URL, fetch);
 
 export default newClient;
