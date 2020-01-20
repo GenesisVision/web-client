c81c24580fac4981b651b5d377c964e71b2e0438
pavelzubov <pasha224@yandex.ru>
2020-01-20 10:54:27 +0300

    Use useTestHelpers

--- packages/social-trader/src/pages/auth/login/login.page.spec.ts
+++ packages/social-trader/src/pages/auth/login/login.page.spec.ts
@@ -1,19 +1,27 @@
-import { Page } from "puppeteer";
+import { Browser, Page } from "puppeteer";
 import { getTokenName } from "utils/get-token-name";
-import { ASYNC_TEST_TIMEOUT, authorize, getPage } from "utils/test-helpers";
+import {
+  ASYNC_TEST_TIMEOUT,
+  getBrowser,
+  useTestHelpers
+} from "utils/test-helpers";
 
 describe("Login", () => {
   let page: Page;
+  let browser: Browser;
   beforeAll(async () => {
-    page = await getPage();
-    await authorize({
-      page
-    });
+    browser = await getBrowser();
+    page = await browser.newPage();
+    const { authorize } = useTestHelpers(page);
+    await authorize();
   }, ASYNC_TEST_TIMEOUT);
   it("should be log in on login form", async () => {
     const cookies = await page.cookies();
     const tokenName = getTokenName();
     const tokenCookie = cookies.find(cookie => cookie.name === tokenName);
     expect(!!tokenCookie).toBe(true);
+  });
+  afterAll(() => {
+    browser.close();
   });
 });
--- packages/social-trader/src/pages/create-fund/create-fund.page.spec.ts
+++ packages/social-trader/src/pages/create-fund/create-fund.page.spec.ts
@@ -1,24 +1,27 @@
-import { Page } from "puppeteer";
+import { Browser, Page } from "puppeteer";
 import {
   ASYNC_TEST_TIMEOUT,
-  getPage,
-  openAuthPage,
-  testT
+  getBrowser,
+  testT,
+  useTestHelpers
 } from "utils/test-helpers";
 
 import { CREATE_FUND_PAGE_ROUTE } from "./create-fund.constants";
 
 describe("Create fund", () => {
   let page: Page;
+  let browser: Browser;
   beforeAll(async () => {
-    page = await getPage();
-    await openAuthPage({
-      page,
-      url: CREATE_FUND_PAGE_ROUTE
-    });
+    browser = await getBrowser();
+    page = await browser.newPage();
+    const { openAuthPage } = useTestHelpers(page);
+    await openAuthPage(CREATE_FUND_PAGE_ROUTE);
   }, ASYNC_TEST_TIMEOUT);
   it("should be titled Create fund", async () => {
     const title = await page.title();
     expect(title).toContain(testT("create-fund-page.title"));
+  });
+  afterAll(() => {
+    browser.close();
   });
 });
