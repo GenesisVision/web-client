ca0de78e93bf8f96e06e6b73d451d82b32047b5a
pavelzubov <pasha224@yandex.ru>
2020-01-20 10:58:27 +0300

    Use useTestHelpers

--- packages/social-trader/src/pages/invest/invest.page.spec.ts
+++ packages/social-trader/src/pages/invest/invest.page.spec.ts
@@ -1,23 +1,26 @@
-import { Page } from "puppeteer";
+import { Browser, Page } from "puppeteer";
 import { INVEST_ROUTE } from "routes/invest.routes";
 import {
   ASYNC_TEST_TIMEOUT,
-  getPage,
-  openPage,
-  testT
+  getBrowser,
+  testT,
+  useTestHelpers
 } from "utils/test-helpers";
 
 describe("Invest", () => {
   let page: Page;
+  let browser: Browser;
   beforeAll(async () => {
-    page = await getPage();
-    await openPage({
-      page,
-      url: INVEST_ROUTE
-    });
+    browser = await getBrowser();
+    page = await browser.newPage();
+    const { openPage } = useTestHelpers(page);
+    await openPage(INVEST_ROUTE);
   }, ASYNC_TEST_TIMEOUT);
   it("should be titled Invest", async () => {
     const title = await page.title();
     expect(title).toContain(testT("invest.title"));
+  });
+  afterAll(() => {
+    browser.close();
   });
 });
