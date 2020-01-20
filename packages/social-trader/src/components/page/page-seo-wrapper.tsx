dc874c614ed6930e19636d840172f0a48340a408
pavelzubov <pasha224@yandex.ru>
2020-01-20 20:58:58 +0300

    Create PageSeoWrapper component

---
+++ packages/social-trader/src/components/page/page-seo-wrapper.tsx
@@ -0,0 +1,48 @@
+import Head from "next/dist/next-server/lib/head";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import {
+  commonMeta,
+  descriptionMeta,
+  imageMeta,
+  schema,
+  SchemaType,
+  titleMeta,
+  urlMeta
+} from "utils/seo";
+
+export const PageSeoWrapper: React.FC<IPageSeoWrapperProps> = ({
+  children,
+  url,
+  schemas,
+  title,
+  description,
+  previewImage
+}) => {
+  const [t] = useTranslation();
+  const pageTitle = t("app.title") + title;
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        {commonMeta()}
+        {urlMeta(url)}
+        {schema(schemas)}
+        {titleMeta(title)}
+        {descriptionMeta(description)}
+        {imageMeta(previewImage)}
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export interface IPageSeoWrapperProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  showTitle?: boolean;
+  title: string;
+  schemas?: Array<SchemaType>;
+  description?: string;
+  previewImage?: string;
+  url?: string;
+}
