4bd5d2ace150e04e9a1bfbb336fcc7626e3de76d
Anchello <bob.m@mail.ru>
2020-01-21 12:46:27 +0100

    added new banner in first slider

--- packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
+++ packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
@@ -1,7 +1,11 @@
 import { WALLET_TOTAL_PAGE_ROUTE } from "pages/wallet/wallet.routes";
+import ImgBoxing from "routes/ssr/landing-page/images/slider/boxing.svg";
 import ImgCreditCard from "routes/ssr/landing-page/images/slider/credit-card.svg";
 import ImgPeople from "routes/ssr/landing-page/images/slider/people.svg";
 import { REFERRAL_PROGRAM_ROUTE } from "routes/ssr/landing-page/static-data/nav-links";
+
+const CREATE_ACCOUNT_BROKER_ROUTE =
+  "create-account?broker=Genesis%20Markets%20Demo";
 
 export type TSlides = {
   id: number;
@@ -27,5 +31,13 @@ export const slides: TSlides[] = [
       "Use the Genesis Vision two-level Affiliate Network to receive a percentage of the commission paid by your referrals… and your referrals’ referrals!",
     image: ImgPeople,
     link: REFERRAL_PROGRAM_ROUTE
+  },
+  {
+    id: 2,
+    title: "Trade on Demo",
+    text:
+      "Learn to trade or test your strategies without any financial risks and limitations",
+    image: ImgBoxing,
+    link: CREATE_ACCOUNT_BROKER_ROUTE
   }
 ];
