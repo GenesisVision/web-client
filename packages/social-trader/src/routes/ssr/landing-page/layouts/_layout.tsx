004e980af0ff28c71a7be65ca12f8a30bbfb7ef1
pavelzubov <pasha224@yandex.ru>
2020-01-21 12:36:24 +0300

    Set ORGANIZATION_SCHEMA to schemas

--- packages/social-trader/src/routes/ssr/landing-page/layouts/_layout.tsx
+++ packages/social-trader/src/routes/ssr/landing-page/layouts/_layout.tsx
@@ -4,7 +4,14 @@ import React from "react";
 import CookieMessage from "routes/ssr/landing-page/components/cookie-message/cookie-message";
 import LPFooter from "routes/ssr/landing-page/components/lp-footer/lp-footer";
 import LPHeader from "routes/ssr/landing-page/components/lp-header/lp-header";
-import { commonMeta, descriptionMeta, imageMeta, titleMeta } from "utils/seo";
+import {
+  commonMeta,
+  descriptionMeta,
+  imageMeta,
+  ORGANIZATION_SCHEMA,
+  schema,
+  titleMeta
+} from "utils/seo";
 
 const _Layout: React.FC<Props> = ({ title, children }) => {
   useRefLink();
@@ -12,6 +19,7 @@ const _Layout: React.FC<Props> = ({ title, children }) => {
     <div className="landing-page">
       <Head>
         <title>{title}</title>
+        {schema([ORGANIZATION_SCHEMA])}
         {commonMeta()}
         <meta property="og:url" content="https://genesis.vision" />
         {imageMeta("https://genesis.vision/platform.png")}
