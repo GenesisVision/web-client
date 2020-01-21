97dfab74e2d2736a2e9f7e582cd41807ff21da66
Anchello <bob.m@mail.ru>
2020-01-21 15:43:55 +0100

    updated data-slides

--- packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
+++ packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
@@ -18,6 +18,14 @@ export type TSlides = {
 export const slides: TSlides[] = [
   {
     id: 0,
+    title: "Trade on Demo",
+    text:
+      "Learn to trade or test your strategies without any financial risks and limitations",
+    image: ImgBoxing,
+    link: CREATE_ACCOUNT_BROKER_ROUTE
+  },
+  {
+    id: 1,
     title: "Pay with card",
     text:
       "Use your credit or debit card to buy crypto - get started right away as a trader, an investor or a manager on the Genesis Vision platform.",
@@ -25,19 +33,11 @@ export const slides: TSlides[] = [
     link: WALLET_TOTAL_PAGE_ROUTE
   },
   {
-    id: 1,
+    id: 2,
     title: "Enticing invitations",
     text:
       "Use the Genesis Vision two-level Affiliate Network to receive a percentage of the commission paid by your referrals… and your referrals’ referrals!",
     image: ImgPeople,
     link: REFERRAL_PROGRAM_ROUTE
-  },
-  {
-    id: 2,
-    title: "Trade on Demo",
-    text:
-      "Learn to trade or test your strategies without any financial risks and limitations",
-    image: ImgBoxing,
-    link: CREATE_ACCOUNT_BROKER_ROUTE
   }
 ];
