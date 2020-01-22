0f631591cb5a7f916f87e5158145055470e0b615
pavelzubov <pasha224@yandex.ru>
2020-01-22 10:08:12 +0300

    Add identifier, hoursAvailable, provider fields

--- packages/social-trader/src/pages/invest/programs/program-details/program-schema.tsx
+++ packages/social-trader/src/pages/invest/programs/program-details/program-schema.tsx
@@ -7,12 +7,17 @@ const isFollow = (description: ProgramDescriptionDataType) => {
   return !!description.followDetails;
 };
 
-export const getSchema = (description: ProgramDescriptionDataType) => ({
+export const getSchema = (details: ProgramDescriptionDataType) => ({
   "@context": "https://schema.org",
   "@type": isFollow ? "FinancialProduct" : "DepositAccount",
-  name: description.publicInfo.title,
-  description: description.publicInfo.description,
-  broker: description.brokerDetails.name,
+  identifier: details.id,
+  provider: details.owner.username,
+  hoursAvailable:
+    (details.programDetails && details.programDetails.ageDays * 24) ||
+    undefined,
+  name: details.publicInfo.title,
+  description: details.publicInfo.description,
+  broker: details.brokerDetails.name,
   feesAndCommissionsSpecification: "", //TODO
-  logo: filesService.getFileUrl(description.publicInfo.logo)
+  logo: filesService.getFileUrl(details.publicInfo.logo)
 });
