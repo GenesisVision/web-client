9808101660fdf9766ab6641910c689946a5d2c0d
Anchello <bob.m@mail.ru>
2020-01-23 18:12:15 +0100

    added new images for first-slider

--- packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
+++ packages/social-trader/src/routes/ssr/landing-page/static-data/slides.ts
@@ -1,7 +1,14 @@
 import { WALLET_TOTAL_PAGE_ROUTE } from "pages/wallet/wallet.routes";
-import ImgBoxing from "routes/ssr/landing-page/images/slider/boxing.svg";
-import ImgCreditCard from "routes/ssr/landing-page/images/slider/credit-card.svg";
-import ImgPeople from "routes/ssr/landing-page/images/slider/people.svg";
+import ImgBoxing1 from "routes/ssr/landing-page/images/slider/boxing-1.svg";
+import ImgBoxing2 from "routes/ssr/landing-page/images/slider/boxing-2.svg";
+import ImgBoxing3 from "routes/ssr/landing-page/images/slider/boxing-3.svg";
+import ImgBoxingBg from "routes/ssr/landing-page/images/slider/boxing-bg.svg";
+import ImgCreditCard from "routes/ssr/landing-page/images/slider/credit-card-1.svg";
+import ImgCreditCardBg from "routes/ssr/landing-page/images/slider/credit-card-bg.svg";
+import ImgPeople1 from "routes/ssr/landing-page/images/slider/people-1.svg";
+import ImgPeople2 from "routes/ssr/landing-page/images/slider/people-2.svg";
+import ImgPeople3 from "routes/ssr/landing-page/images/slider/people-3.svg";
+import ImgPeopleBg from "routes/ssr/landing-page/images/slider/people-bg.svg";
 import { REFERRAL_PROGRAM_ROUTE } from "routes/ssr/landing-page/static-data/nav-links";
 
 const CREATE_ACCOUNT_BROKER_ROUTE =
@@ -11,7 +18,8 @@ export type TSlides = {
   id: number;
   title: string;
   text: string;
-  image: string;
+  imageBg?: string;
+  images: Array<string>;
   link: string;
 };
 
@@ -21,7 +29,8 @@ export const slides: TSlides[] = [
     title: "Trade on Demo",
     text:
       "Learn to trade or test your strategies without any financial risks and limitations",
-    image: ImgBoxing,
+    imageBg: ImgBoxingBg,
+    images: [ImgBoxing1, ImgBoxing2, ImgBoxing3],
     link: CREATE_ACCOUNT_BROKER_ROUTE
   },
   {
@@ -29,7 +38,8 @@ export const slides: TSlides[] = [
     title: "Pay with card",
     text:
       "Use your credit or debit card to buy crypto - get started right away as a trader, an investor or a manager on the Genesis Vision platform.",
-    image: ImgCreditCard,
+    imageBg: ImgCreditCardBg,
+    images: [ImgCreditCard],
     link: WALLET_TOTAL_PAGE_ROUTE
   },
   {
@@ -37,7 +47,8 @@ export const slides: TSlides[] = [
     title: "Enticing invitations",
     text:
       "Use the Genesis Vision two-level Affiliate Network to receive a percentage of the commission paid by your referrals… and your referrals’ referrals!",
-    image: ImgPeople,
+    imageBg: ImgPeopleBg,
+    images: [ImgPeople1, ImgPeople2, ImgPeople3],
     link: REFERRAL_PROGRAM_ROUTE
   }
 ];
