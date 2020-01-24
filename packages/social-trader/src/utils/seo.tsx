1783f1e0f3cd318ad3fcbedb641e46957640fce9
pavelzubov <pasha224@yandex.ru>
2020-01-24 16:28:35 +0300

    Correct twitter card

--- packages/social-trader/src/utils/seo.tsx
+++ packages/social-trader/src/utils/seo.tsx
@@ -52,6 +52,7 @@ export const descriptionMeta = (description?: string) => {
 export const commonMeta = () => {
   return (
     <>
+      <meta name="twitter:card" content="summary" />
       <meta key={"og-type"} property="og:type" content="website" />
       <meta key={"og-sn"} property="og:site_name" content="Genesis Vision" />
       <meta key={"tw-site"} property="twitter:site" content="@genesis_vision" />
@@ -63,7 +64,6 @@ export const imageMeta = (image?: string) => {
   return image ? (
     <>
       <meta property="og:image" key="og-image" content={image} />
-      <meta name="twitter:image:src" key="twitter:image:src" content={image} />
       <meta name="twitter:image" key="twitter:image" content={image} />
     </>
   ) : null;
