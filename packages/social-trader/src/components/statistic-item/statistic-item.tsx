647b221f9ac8b94b3e6b3db9bee74cc98dc717c4
pavelzubov <pasha224@yandex.ru>
2020-01-24 10:56:54 +0300

    Refactoring statistic item and list

--- packages/social-trader/src/components/statistic-item/statistic-item.tsx
+++ packages/social-trader/src/components/statistic-item/statistic-item.tsx
@@ -1,12 +1,14 @@
 import "./statistic-item.scss";
 
-import classNames from "classnames";
-import { BlurContainer } from "components/blur-container/blur-container";
-import { StatisticItemContainerBlock } from "components/statistic-item/statistic-item-container.block";
+import {
+  IStatisticItemContainerBlockProps,
+  StatisticItemContainerBlock
+} from "components/statistic-item/statistic-item-container.block";
+import StatisticItemInner, {
+  IStatisticItemInnerProps
+} from "components/statistic-item/statistic-item-inner";
 import withLoader from "decorators/with-loader";
 import * as React from "react";
-import NumberFormat from "react-number-format";
-import { formatCurrencyValue } from "utils/formatter";
 
 const _StatisticItem: React.FC<Props> = ({
   noWrap = true,
@@ -26,25 +28,6 @@ const _StatisticItem: React.FC<Props> = ({
   equivalent,
   equivalentCurrency
 }) => {
-  const generateClasses = (item: ITEM) => {
-    switch (
-      (item === ITEM.VALUE && !invert) || (item === ITEM.LABEL && invert)
-    ) {
-      case true:
-        return classNames("statistics-item__value", {
-          "statistics-item__value--accent": accent,
-          "statistics-item__value--small": small,
-          "statistics-item__value--big": big,
-          "statistics-item__value--large": large
-        });
-      case false:
-      default:
-        return classNames("statistics-item__label", {
-          "statistics-item__label--no-wrap": noWrap
-        });
-    }
-  };
-
   return (
     <StatisticItemContainerBlock
       withPadding={withPadding}
@@ -52,55 +35,29 @@ const _StatisticItem: React.FC<Props> = ({
       small={small}
       className={className}
     >
-      <div
-        className={classNames(
-          { "statistics-item__top--hidden": hideLabel },
-          "statistics-item__top",
-          labelClassName,
-          generateClasses(ITEM.LABEL)
-        )}
+      <StatisticItemInner
+        noWrap={noWrap}
+        hideLabel={hideLabel}
+        isPending={isPending}
+        label={label}
+        equivalent={equivalent}
+        equivalentCurrency={equivalentCurrency}
+        small={small}
+        big={big}
+        large={large}
+        accent={accent}
+        invert={invert}
+        labelClassName={labelClassName}
       >
-        {label}
-      </div>
-      <div className={generateClasses(ITEM.VALUE)}>
-        <BlurContainer blur={!!isPending}>{children}</BlurContainer>
-      </div>
-      {equivalent !== undefined && equivalentCurrency !== undefined ? (
-        <div className="statistics-item__equivalent">
-          <NumberFormat
-            value={formatCurrencyValue(equivalent, equivalentCurrency)}
-            thousandSeparator={" "}
-            displayType="text"
-            suffix={` ${equivalentCurrency}`}
-          />
-        </div>
-      ) : null}
+        {children}
+      </StatisticItemInner>
     </StatisticItemContainerBlock>
   );
 };
 
-enum ITEM {
-  LABEL = "LABEL",
-  VALUE = "VALUE"
-}
-
-interface Props {
-  noWrap?: boolean;
-  withPadding?: boolean;
-  hideLabel?: boolean;
-  isPending?: boolean;
-  label?: string | React.ReactNode;
-  equivalent?: number;
-  equivalentCurrency?: string;
-  small?: boolean;
-  big?: boolean;
-  large?: boolean;
-  accent?: boolean;
-  half?: boolean;
-  invert?: boolean;
-  className?: string;
-  labelClassName?: string;
-}
+interface Props
+  extends IStatisticItemInnerProps,
+    IStatisticItemContainerBlockProps {}
 
 const StatisticItem = withLoader(_StatisticItem);
 export default StatisticItem;
