a4e2473ab02f27dd5653f787286f271ccc30e7bd
pavelzubov <pasha224@yandex.ru>
2020-01-27 22:14:48 +0300

    Add init .env.local

--- packages/social-trader/src/setupTests.js
+++ packages/social-trader/src/setupTests.js
@@ -1,4 +1,6 @@
+import dotenv from "dotenv";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
+dotenv.config({ path: ".env.local" });
 configure({ adapter: new Adapter() });
