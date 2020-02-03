315cf1d4893d3ed0ca45b02ef9c15b50f5dc447e
pavelzubov <pasha224@yandex.ru>
2020-02-03 09:50:22 +0300

    Add DemoDepositValidationSchema Move DEMO_DEPOSIT_FORM_FIELDS, IDemoDepositFormValues to services

--- packages/social-trader/src/modules/demo-deposit/demo-deposit.service.ts
+++ packages/social-trader/src/modules/demo-deposit/demo-deposit.service.ts
@@ -1,5 +1,7 @@
+import { TFunction } from "i18next";
 import assetsApi from "services/api-client/assets-api";
 import authService from "services/auth-service";
+import { object, string } from "yup";
 
 export type DemoDepositResponse = Promise<void>;
 
@@ -12,3 +14,18 @@ export const depositToDemo = ({ id, amount }: any): DemoDepositResponse => {
     }
   ) as unknown) as DemoDepositResponse;
 };
+
+export enum DEMO_DEPOSIT_FORM_FIELDS {
+  amount = "amount"
+}
+
+export interface IDemoDepositFormValues {
+  [DEMO_DEPOSIT_FORM_FIELDS.amount]: string;
+}
+
+export const DemoDepositValidationSchema = (t: TFunction) =>
+  object().shape({
+    [DEMO_DEPOSIT_FORM_FIELDS.amount]: string().required(
+      t("demo-deposit.validations.amount")
+    )
+  });
