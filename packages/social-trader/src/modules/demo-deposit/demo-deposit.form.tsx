8039c38ef26b31535cf2de9775c084c2b2b0cab3
pavelzubov <pasha224@yandex.ru>
2020-02-03 09:52:43 +0300

    Upgrade DemoDepositForm

--- packages/social-trader/src/modules/demo-deposit/demo-deposit.form.tsx
+++ packages/social-trader/src/modules/demo-deposit/demo-deposit.form.tsx
@@ -1,41 +1,56 @@
 import { DialogButtons } from "components/dialog/dialog-buttons";
 import GVButton from "components/gv-button";
 import { SimpleNumberField } from "components/simple-fields/simple-number-field";
-import { DemoDepositResponse } from "modules/demo-deposit/demo-deposit.service";
+import {
+  DEMO_DEPOSIT_FORM_FIELDS,
+  DemoDepositResponse,
+  DemoDepositValidationSchema,
+  IDemoDepositFormValues
+} from "modules/demo-deposit/demo-deposit.service";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { CurrencyEnum, SetSubmittingType } from "utils/types";
+import { CurrencyEnum } from "utils/types";
 
-const _DemoDepositForm: React.FC<Props> = ({ currency }) => {
+const _DemoDepositForm: React.FC<Props> = ({ currency, onSubmit }) => {
+  const [t] = useTranslation();
   const {
+    triggerValidation,
+    watch,
     errors,
     setValue,
     register,
     handleSubmit,
-    formState: { isSubmitting, touched }
+    formState: { isSubmitting, touched, isValid }
   } = useForm<IDemoDepositFormValues>({
+    validationSchema: DemoDepositValidationSchema(t),
     validateCriteriaMode: "firstError",
     mode: "onChange"
   });
-  const [t] = useTranslation();
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <SimpleNumberField
-        touched={!!touched.amount}
-        error={errors.amount && errors.amount.message}
+        autoFocus
+        triggerValidation={triggerValidation}
+        value={watch()[DEMO_DEPOSIT_FORM_FIELDS.amount]}
+        touched={!!touched[DEMO_DEPOSIT_FORM_FIELDS.amount]}
+        error={
+          errors[DEMO_DEPOSIT_FORM_FIELDS.amount] &&
+          errors[DEMO_DEPOSIT_FORM_FIELDS.amount]!.message
+        }
         setFieldValue={setValue}
         suffix={` ${currency}`}
         wide
         label={t("transfer.amount")}
-        refProp={register(
-          { name: FORM_FIELDS.amount, type: "custom" },
-          { required: "Amount is required" }
-        )}
-        name={FORM_FIELDS.amount}
+        refProp={register({
+          name: DEMO_DEPOSIT_FORM_FIELDS.amount,
+          type: "custom"
+        })}
+        name={DEMO_DEPOSIT_FORM_FIELDS.amount}
       />
       <DialogButtons>
-        <GVButton wide type="submit" disabled={isSubmitting}>
+        <GVButton wide type="submit" disabled={isSubmitting || !isValid}>
           {t("deposit-asset.confirm")}
         </GVButton>
       </DialogButtons>
@@ -43,28 +58,9 @@ const _DemoDepositForm: React.FC<Props> = ({ currency }) => {
   );
 };
 
-export enum FORM_FIELDS {
-  amount = "amount"
-}
-
 interface Props {
   currency: CurrencyEnum;
-  onSubmit: (
-    values: IDemoDepositFormValues,
-    setSubmitting: SetSubmittingType
-  ) => DemoDepositResponse;
-}
-
-export interface IDemoDepositFormValues {
-  amount: string;
-}
-
-export interface IDemoDepositFormProps {
-  currency: CurrencyEnum;
-  onSubmit: (
-    values: IDemoDepositFormValues,
-    setSubmitting: SetSubmittingType
-  ) => DemoDepositResponse;
+  onSubmit: (values: IDemoDepositFormValues) => DemoDepositResponse;
 }
 
 const DemoDepositForm = React.memo(_DemoDepositForm);
