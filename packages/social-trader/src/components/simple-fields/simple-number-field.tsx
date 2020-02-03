fb8f99d39d1361c26d9a1f4004d9a567c1a0e0e4
pavelzubov <pasha224@yandex.ru>
2020-02-03 09:49:55 +0300

    Upgrade SimpleNumberField

--- packages/social-trader/src/components/simple-fields/simple-number-field.tsx
+++ packages/social-trader/src/components/simple-fields/simple-number-field.tsx
@@ -1,30 +1,43 @@
 import "./simple-text-field.scss";
 
-import {
-  ISimpleTextFieldProps,
-  SimpleTextField
-} from "components/simple-fields/simple-text-field";
+import GVTextField, { GVTextFieldProps } from "components/gv-text-field";
 import React, { useCallback } from "react";
 import NumberFormat, { NumberFormatValues } from "react-number-format";
 
 const _SimpleNumberField: React.FC<ISimpleNumberFieldProps> = props => {
-  const { setFieldValue, onChange, name } = props;
-  const handleOnChange = useCallback((value: NumberFormatValues) => {
-    if (onChange) onChange(value.value);
-    setFieldValue(name, value.value);
-  }, []);
+  const {
+    setFieldValue,
+    name,
+    triggerValidation,
+    emptyInit,
+    init,
+    value
+  } = props;
+  const handleOnChange = useCallback(
+    ({ value }: NumberFormatValues) => {
+      setFieldValue(name, value);
+      if (triggerValidation) triggerValidation(value);
+    },
+    [name]
+  );
 
+  const setEmpty = emptyInit && init;
   return (
-    <SimpleTextField
+    <GVTextField
       {...props}
-      onChange={handleOnChange}
+      value={setEmpty ? "" : value}
+      //@ts-ignore
+      onValueChange={handleOnChange}
       InputComponent={NumberFormat}
     />
   );
 };
 
-interface ISimpleNumberFieldProps extends ISimpleTextFieldProps {
+interface ISimpleNumberFieldProps extends GVTextFieldProps {
+  refProp?: any;
+  triggerValidation: (name: string) => any;
   setFieldValue: (name: string, value?: any) => void;
+  [key: string]: any;
 }
 
 export const SimpleNumberField = React.memo(_SimpleNumberField);
