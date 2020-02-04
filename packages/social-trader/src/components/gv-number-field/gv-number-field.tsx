eb1d3f3e27602df57c5ef5b70cf1efd04a516430
pavelzubov <pasha224@yandex.ru>
2020-02-04 11:42:45 +0300

    Use SimpleNumberField

--- packages/social-trader/src/components/gv-number-field/gv-number-field.tsx
+++ packages/social-trader/src/components/gv-number-field/gv-number-field.tsx
@@ -1,42 +1,11 @@
-import GVTextField, { GVTextFieldProps } from "components/gv-text-field";
+import { GVTextFieldProps } from "components/gv-text-field";
+import { SimpleNumberField } from "components/simple-fields/simple-number-field";
 import { FormikActions } from "formik";
 import * as React from "react";
-import { useCallback, useEffect, useState } from "react";
-import NumberFormat, { NumberFormatValues } from "react-number-format";
 
 const _GVNumberField: React.FC<Props> = props => {
-  const { value, emptyInit, autoFocus, form, name } = props;
-  const [init, setInit] = useState(true);
-
-  const handleOnChange = useCallback(
-    ({ value }: NumberFormatValues) => {
-      setInit(false);
-      form.setFieldValue(name, value);
-    },
-    [name]
-  );
-
-  const [input, setInput] = useState<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (input !== null && autoFocus) {
-      setImmediate(() => {
-        input.focus && input.focus();
-      });
-    }
-  }, [input, autoFocus]);
-
-  const setEmpty = emptyInit && init;
   return (
-    <GVTextField
-      {...props}
-      value={setEmpty ? "" : value}
-      name={name}
-      //@ts-ignore
-      onValueChange={handleOnChange}
-      InputComponent={NumberFormat}
-      getInputRef={(ref: HTMLDivElement) => setInput(ref)}
-    />
+    <SimpleNumberField {...props} setFieldValue={props.form.setFieldValue} />
   );
 };
 
