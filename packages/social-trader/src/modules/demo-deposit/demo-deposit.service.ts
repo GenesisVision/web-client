0e173505621cbd4f0578eeefc3f77ebcc783889b
pavelzubov <pasha224@yandex.ru>
2020-02-05 12:11:13 +0300

    Add max to DemoDepositValidationSchema

--- packages/social-trader/src/modules/demo-deposit/demo-deposit.service.ts
+++ packages/social-trader/src/modules/demo-deposit/demo-deposit.service.ts
@@ -1,7 +1,7 @@
 import { TFunction } from "i18next";
 import assetsApi from "services/api-client/assets-api";
 import authService from "services/auth-service";
-import { object, string } from "yup";
+import { number, object, string } from "yup";
 
 export type DemoDepositResponse = Promise<void>;
 
@@ -23,9 +23,9 @@ export interface IDemoDepositFormValues {
   [DEMO_DEPOSIT_FORM_FIELDS.amount]: string;
 }
 
-export const DemoDepositValidationSchema = (t: TFunction) =>
+export const DemoDepositValidationSchema = (t: TFunction, maxAmount: number) =>
   object().shape({
-    [DEMO_DEPOSIT_FORM_FIELDS.amount]: string().required(
-      t("demo-deposit.validations.amount")
-    )
+    [DEMO_DEPOSIT_FORM_FIELDS.amount]: number()
+      .required(t("demo-deposit.validations.required"))
+      .max(maxAmount, t("demo-deposit.validations.max-amount", { maxAmount }))
   });
