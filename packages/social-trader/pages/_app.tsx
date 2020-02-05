3a657df6f28ddf90eac3ecfe087a7515dbd86f3c
pavelzubov <pasha224@yandex.ru>
2020-02-05 20:58:53 +0300

    Fix initializeStore

--- packages/social-trader/pages/_app.tsx
+++ packages/social-trader/pages/_app.tsx
@@ -7,26 +7,24 @@ import App from "next/app";
 import React from "react";
 import { Provider } from "react-redux";
 import { compose, Store } from "redux";
-import { InitializeStoreType } from "utils/types";
+import { initializeStore } from "store";
 
-export default (initializeStore: InitializeStoreType) => {
-  class CustomApp extends App<Props> {
-    render() {
-      const { Component, pageProps, reduxStore } = this.props;
-      return (
-        <Provider store={reduxStore}>
-          <Component {...pageProps} />
-        </Provider>
-      );
-    }
+class CustomApp extends App<Props> {
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
+    return (
+      <Provider store={reduxStore}>
+        <Component {...pageProps} />
+      </Provider>
+    );
   }
+}
 
-  return compose(
-    withHistoryProvider,
-    appWithTranslation,
-    withReduxStore(initializeStore)
-  )(CustomApp);
-};
+export default compose(
+  withHistoryProvider,
+  appWithTranslation,
+  withReduxStore(initializeStore)
+)(CustomApp);
 
 interface Props {
   reduxStore: Store;
