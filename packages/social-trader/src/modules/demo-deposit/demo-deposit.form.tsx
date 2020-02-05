71ac718d0e1b590bad30bfa54bc737bc825a3c8a
pavelzubov <pasha224@yandex.ru>
2020-02-05 12:24:10 +0300

    Add setMax

--- packages/social-trader/src/modules/demo-deposit/demo-deposit.form.tsx
+++ packages/social-trader/src/modules/demo-deposit/demo-deposit.form.tsx
@@ -7,25 +7,36 @@ import {
   DemoDepositValidationSchema,
   IDemoDepositFormValues
 } from "modules/demo-deposit/demo-deposit.service";
-import React from "react";
+import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { useSelector } from "react-redux";
+import { minDemoDepositAmountSelector } from "reducers/platform-reducer";
 import { CurrencyEnum } from "utils/types";
 
 const _DemoDepositForm: React.FC<Props> = ({ currency, onSubmit }) => {
+  const maxAmount = useSelector(minDemoDepositAmountSelector);
   const [t] = useTranslation();
   const form = useForm<IDemoDepositFormValues>({
-    validationSchema: DemoDepositValidationSchema(t),
+    validationSchema: DemoDepositValidationSchema(t, maxAmount),
     mode: "onChange"
   });
   const {
+    triggerValidation,
+    setValue,
     handleSubmit,
     formState: { isSubmitting, isValid }
   } = form;
 
+  const setMax = useCallback(() => {
+    setValue(DEMO_DEPOSIT_FORM_FIELDS.amount, String(maxAmount));
+    triggerValidation(DEMO_DEPOSIT_FORM_FIELDS.amount);
+  }, [maxAmount]);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <HookFormAmountField
+        setMax={setMax}
         form={form}
         currency={currency}
         name={DEMO_DEPOSIT_FORM_FIELDS.amount}
