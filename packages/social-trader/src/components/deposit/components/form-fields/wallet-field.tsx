f8c239ed3848a6b2fe9808955e1cd89e60ecc951
pavelzubov <pasha224@yandex.ru>
2020-02-12 11:38:46 +0300

    Add label prop Remove DialogField

--- packages/social-trader/src/components/deposit/components/form-fields/wallet-field.tsx
+++ packages/social-trader/src/components/deposit/components/form-fields/wallet-field.tsx
@@ -1,9 +1,10 @@
 import { DialogField } from "components/dialog/dialog-field";
 import { ISelectChangeEvent } from "components/select/select";
 import WalletSelect, {
-  HookFormWalletSelect
+  HookFormWalletSelect,
+  ItemsType,
+  WalletItemType
 } from "components/wallet-select/wallet-select";
-import { WalletBaseData } from "gv-api-web";
 import * as React from "react";
 import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
@@ -34,14 +35,20 @@ const _WalletField: React.FC<Props> = ({ name, wallets, onChange }) => {
 };
 
 interface Props {
-  wallets: WalletBaseData[];
+  label?: string;
+  wallets: ItemsType;
   name: string;
-  onChange: (wallet: WalletBaseData) => void;
+  onChange: (wallet: WalletItemType) => void;
 }
 
 export const WalletField = React.memo(_WalletField);
 
-const _HookFormWalletField: React.FC<Props> = ({ name, wallets, onChange }) => {
+const _HookFormWalletField: React.FC<Props> = ({
+  label,
+  name,
+  wallets,
+  onChange
+}) => {
   const [t] = useTranslation();
   const onChangeCurrencyFrom = useCallback(
     (event: ISelectChangeEvent): void => {
@@ -54,14 +61,12 @@ const _HookFormWalletField: React.FC<Props> = ({ name, wallets, onChange }) => {
     [wallets]
   );
   return (
-    <DialogField>
-      <HookFormWalletSelect
-        name={name}
-        label={t("follow-program.create-account.from")}
-        items={wallets}
-        onChange={onChangeCurrencyFrom}
-      />
-    </DialogField>
+    <HookFormWalletSelect
+      name={name}
+      label={label || t("follow-program.create-account.from")}
+      items={wallets}
+      onChange={onChangeCurrencyFrom}
+    />
   );
 };
 export const HookFormWalletField = React.memo(_HookFormWalletField);
