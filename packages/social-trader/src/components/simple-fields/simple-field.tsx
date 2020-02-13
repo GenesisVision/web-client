abe50c411ee0d5d872aa00013b5e05012f8beb2e
pavelzubov <pasha224@yandex.ru>
2020-02-13 16:52:04 +0300

    Add dirty state for validate on change after first blur

--- packages/social-trader/src/components/simple-fields/simple-field.tsx
+++ packages/social-trader/src/components/simple-fields/simple-field.tsx
@@ -1,8 +1,10 @@
 import GVTextField, { GVTextFieldProps } from "components/gv-text-field";
+import useIsOpen from "hooks/is-open.hook";
 import React, { useCallback, useState } from "react";
 
 const _SimpleField: React.FC<ISimpleFieldProps> = props => {
   const {
+    triggerValidation,
     setFieldValue,
     name,
     emptyInit,
@@ -10,19 +12,28 @@ const _SimpleField: React.FC<ISimpleFieldProps> = props => {
     InputComponent,
     valueCallback
   } = props;
+  const [dirty, setDirty] = useIsOpen();
   const [init, setInit] = useState(true);
   const handleOnChange = useCallback(
     (value: any) => {
       setInit(false);
-      if (setFieldValue) setFieldValue(name, valueCallback(value), true);
+      if (setFieldValue) setFieldValue(name, valueCallback(value), dirty);
     },
-    [name, setFieldValue, valueCallback]
+    [name, setFieldValue, valueCallback, dirty]
   );
+  const handleOnBlur = useCallback(() => {
+    if (!dirty) {
+      setDirty();
+      triggerValidation && triggerValidation(name);
+    }
+  }, [name, triggerValidation, dirty]);
+
   const setEmpty = emptyInit && init;
   return (
     <GVTextField
       {...props}
       value={setEmpty ? "" : value}
+      onBlur={handleOnBlur}
       onChange={!InputComponent ? handleOnChange : undefined}
       onValueChange={handleOnChange}
       InputComponent={InputComponent}
@@ -31,6 +42,7 @@ const _SimpleField: React.FC<ISimpleFieldProps> = props => {
 };
 
 export interface ISimpleFieldProps extends GVTextFieldProps {
+  triggerValidation?: (name: string) => void;
   valueCallback: (value: any) => any;
   validateOnInput?: boolean;
   refProp?: any;
