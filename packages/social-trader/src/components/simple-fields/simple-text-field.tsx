e266d1fd3ef39cc56a3837256bb22adf66693e2d
pavelzubov <pasha224@yandex.ru>
2020-02-13 11:42:07 +0300

    Upgrade SimpleTextField

--- packages/social-trader/src/components/simple-fields/simple-text-field.tsx
+++ packages/social-trader/src/components/simple-fields/simple-text-field.tsx
@@ -1,127 +1,31 @@
-import "./simple-text-field.scss";
-
-import classNames from "classnames";
-import useIsOpen from "hooks/is-open.hook";
-import React, { useCallback, useRef } from "react";
+import GVTextField, { GVTextFieldProps } from "components/gv-text-field";
+import React, { useCallback, useState } from "react";
 
 const _SimpleTextField: React.FC<ISimpleTextFieldProps> = props => {
-  const {
-    onChange,
-    name,
-    errorClassName,
-    adornment,
-    adornmentPosition,
-    adornmentClassName,
-    InputComponent,
-    refProp,
-    type,
-    inputClassName,
-    label,
-    labelClassName,
-    wrapperClassName,
-    noMargin,
-    wide,
-    className,
-    disabled,
-    touched,
-    error,
-    onBlur,
-    ...otherProps
-  } = props;
-  const input = useRef(null);
-
-  const [focused, setFocused, setBlur] = useIsOpen();
-  const handleBlur = useCallback((e: any) => {
-    setBlur();
-    onBlur && onBlur(e);
-  }, []);
+  const { setFieldValue, name, emptyInit, value } = props;
+  const [init, setInit] = useState(true);
+  const handleOnChange = useCallback(
+    (event: React.ChangeEvent<any>) => {
+      setInit(false);
+      if (setFieldValue) setFieldValue(name, event.target.value, true);
+    },
+    [name, setFieldValue]
+  );
+  const setEmpty = emptyInit && init;
   return (
-    <div
-      className={classNames("simple-text-field__wrapper", wrapperClassName, {
-        "simple-text-field__wrapper--no-margin": noMargin,
-        "simple-text-field__wrapper--wide": wide
-      })}
-    >
-      <label
-        className={classNames("simple-text-field__label", labelClassName, {
-          "simple-text-field__label--shrink":
-            focused ||
-            adornment ||
-            (input.current &&
-              // @ts-ignore
-              input.current.state.value !== undefined &&
-              // @ts-ignore
-              input.current.state.value !== "")
-        })}
-      >
-        {label}
-      </label>
-      <div
-        className={classNames("simple-text-field", className, {
-          "simple-text-field--disabled": disabled,
-          "simple-text-field--invalid": touched && error,
-          "simple-text-field--focused": focused
-        })}
-      >
-        {InputComponent && (
-          <InputComponent
-            onValueChange={onChange}
-            name={name}
-            ref={input}
-            type={type}
-            className={classNames("simple-text-field__input", inputClassName)}
-            onFocus={setFocused}
-            onBlur={handleBlur}
-            {...otherProps}
-          />
-        )}
-        {adornment && (
-          <div
-            className={classNames(
-              "simple-text-field__adornment",
-              adornmentClassName,
-              {
-                "simple-text-field__adornment--start":
-                  adornmentPosition === "start",
-                "simple-text-field__adornment--end": adornmentPosition === "end"
-              }
-            )}
-          >
-            {adornment}
-          </div>
-        )}
-      </div>
-      {touched && error && (
-        <div className={classNames("simple-text-field__error", errorClassName)}>
-          {error}
-        </div>
-      )}
-    </div>
+    <GVTextField
+      {...props}
+      value={setEmpty ? "" : value}
+      onChange={handleOnChange}
+    />
   );
 };
 
-export interface ISimpleTextFieldProps {
-  onChange?: (e: any) => void;
-  name: string;
-  adornment?: React.ReactNode;
-  adornmentPosition?: "start" | "end";
-  InputComponent?: React.ComponentType<any> | string;
-  refProp: any;
-  type?: string;
-  label?: string;
-  value?: string | number;
-  noMargin?: boolean;
-  wide?: boolean;
-  disabled?: boolean;
-  touched?: boolean;
-  error?: string;
-  onBlur?: (e: any) => void;
-  className?: string;
-  wrapperClassName?: string;
-  errorClassName?: string;
-  adornmentClassName?: string;
-  labelClassName?: string;
-  inputClassName?: string;
+export interface ISimpleTextFieldProps extends GVTextFieldProps {
+  triggerValidation: (name: string) => void;
+  validateOnInput?: boolean;
+  refProp?: any;
+  setFieldValue?: (name: string, value?: any, validate?: boolean) => void;
   [key: string]: any;
 }
 
