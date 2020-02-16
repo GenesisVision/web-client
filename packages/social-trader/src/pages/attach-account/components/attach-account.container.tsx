0ab0c3d10b6f62d4538f60c7bdaa613c1a680ad5
pavelzubov <pasha224@yandex.ru>
2020-02-16 18:48:57 +0300

    Change formik to react-hook-form in attach account form

--- packages/social-trader/src/pages/attach-account/components/attach-account.container.tsx
+++ packages/social-trader/src/pages/attach-account/components/attach-account.container.tsx
@@ -2,22 +2,28 @@ import { AssetContentBlock } from "components/assets/asset-fields/asset-content.
 import { Push } from "components/link/link";
 import { Broker } from "gv-api-web";
 import useApiRequest from "hooks/api-request.hook";
+import useIsOpen from "hooks/is-open.hook";
 import {
   attachAccount,
   fetchExchanges
 } from "pages/attach-account/services/attach-account.service";
 import React, { useCallback } from "react";
 import { TRADING_ROUTE } from "routes/dashboard.routes";
-import { SetSubmittingType } from "utils/types";
 
 import AttachAccountSettings, {
   IAttachAccountSettingsFormValues
 } from "./attach-account-settings/attach-account-settings";
 
 const _AttachAccountPage: React.FC<Props> = ({ requestBrokerName }) => {
-  const pushMiddleware = () => Push(TRADING_ROUTE);
-  const { sendRequest: attach } = useApiRequest({
-    middleware: [pushMiddleware],
+  const [success, setSuccess] = useIsOpen();
+  const pushMiddleware = () => {
+    Push(TRADING_ROUTE);
+  };
+  const successMiddleware = () => {
+    setSuccess();
+  };
+  const { sendRequest: attach, errorMessage, isPending } = useApiRequest({
+    middleware: [successMiddleware, pushMiddleware],
     request: attachAccount
   });
   const { data: exchanges } = useApiRequest<Broker[]>({
@@ -26,19 +32,19 @@ const _AttachAccountPage: React.FC<Props> = ({ requestBrokerName }) => {
   });
 
   const handleSubmit = useCallback(
-    (
-      values: IAttachAccountSettingsFormValues,
-      setSubmitting: SetSubmittingType
-    ) => attach(values, setSubmitting),
+    (values: IAttachAccountSettingsFormValues) => attach(values),
     []
   );
   return (
     <AssetContentBlock>
       <AttachAccountSettings
+        isPending={isPending}
+        success={success}
+        errorMessage={errorMessage}
         requestBrokerName={requestBrokerName}
         onSubmit={handleSubmit}
         data={exchanges!}
-        loaderData={[]}
+        condition={!!exchanges}
       />
     </AssetContentBlock>
   );
