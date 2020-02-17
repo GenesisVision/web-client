a7549890cbc20be5fc4a35d9a45f831fd75dc651
pavelzubov <pasha224@yandex.ru>
2020-02-17 09:29:59 +0300

    Change formik to react-hook-form in CloseAssetForm

--- packages/social-trader/src/modules/asset-settings/close-asset/close-asset-form.tsx
+++ packages/social-trader/src/modules/asset-settings/close-asset/close-asset-form.tsx
@@ -5,12 +5,11 @@ import { DialogTop } from "components/dialog/dialog-top";
 import GVButton from "components/gv-button";
 import GVFormikField from "components/gv-formik-field";
 import GVTextField from "components/gv-text-field";
-import { InjectedFormikProps, withFormik } from "formik";
 import { CloseableAssetType } from "modules/asset-settings/close-asset/close-asset";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { compose } from "redux";
-import { SetSubmittingType } from "utils/types";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
 import { twoFactorValidator } from "utils/validators/validators";
 import { object } from "yup";
 
@@ -18,89 +17,89 @@ enum FIELDS {
   twoFactorCode = "twoFactorCode"
 }
 
-const _CloseAssetForm: React.FC<InjectedFormikProps<
-  FormProps,
-  ICloseAssetFormValues
->> = ({
+const _CloseAssetForm: React.FC<Props> = ({
+  errorMessage,
   asset,
-  t,
   onCancel,
   twoFactorEnabled,
-  handleSubmit,
-  isSubmitting,
+  onSubmit,
   assetName
-}) => (
-  <form id="closeAssetForm" onSubmit={handleSubmit} noValidate>
-    <DialogTop
-      title={t(
-        `asset-settings.period-and-closing.close-confirm-title-${asset.toLowerCase()}`
-      )}
-      subtitle={assetName}
-    />
-    <DialogBottom>
-      <DialogField>
-        {t(
-          `asset-settings.period-and-closing.close-confirm-notification-${asset.toLowerCase()}`
+}) => {
+  const [t] = useTranslation();
+  const form = useForm<ICloseAssetFormValues>({
+    defaultValues: {
+      [FIELDS.twoFactorCode]: ""
+    },
+    validationSchema: object().shape({
+      [FIELDS.twoFactorCode]: twoFactorValidator(t, twoFactorEnabled)
+    })
+  });
+  const {
+    formState: { isSubmitting, isValid, dirty, isSubmitted }
+  } = form;
+  const isSuccessful = isSubmitted && !errorMessage;
+  const disabled = !isValid || !dirty || isSubmitting || isSuccessful;
+  return (
+    <HookForm form={form} onSubmit={onSubmit}>
+      <DialogTop
+        title={t(
+          `asset-settings.period-and-closing.close-confirm-title-${asset.toLowerCase()}`
         )}
-      </DialogField>
-      {twoFactorEnabled && (
+        subtitle={assetName}
+      />
+      <DialogBottom>
         <DialogField>
-          <GVFormikField
-            wide
-            type="tel"
-            name={FIELDS.twoFactorCode}
-            label={t("wallet-withdraw.two-factor-code-label")}
-            autoComplete="off"
-            component={GVTextField}
-          />
+          {t(
+            `asset-settings.period-and-closing.close-confirm-notification-${asset.toLowerCase()}`
+          )}
         </DialogField>
-      )}
-      <DialogButtons>
-        <GVButton type="submit" disabled={isSubmitting}>
-          {t("buttons.confirm")}
-        </GVButton>
-        <GVButton
-          color="secondary"
-          variant="outlined"
-          disabled={isSubmitting}
-          onClick={onCancel}
-        >
-          {t("buttons.cancel")}
-        </GVButton>
-      </DialogButtons>
-    </DialogBottom>
-  </form>
-);
+        {twoFactorEnabled && (
+          <DialogField>
+            <GVFormikField
+              wide
+              type="tel"
+              name={FIELDS.twoFactorCode}
+              label={t("wallet-withdraw.two-factor-code-label")}
+              autoComplete="off"
+              component={GVTextField}
+            />
+          </DialogField>
+        )}
+        <DialogButtons>
+          <GVButton
+            type="submit"
+            disabled={disabled}
+            isPending={isSubmitting}
+            isSuccessful={isSuccessful}
+          >
+            {t("buttons.confirm")}
+          </GVButton>
+          <GVButton
+            color="secondary"
+            variant="outlined"
+            disabled={isSubmitting}
+            onClick={onCancel}
+          >
+            {t("buttons.cancel")}
+          </GVButton>
+        </DialogButtons>
+      </DialogBottom>
+    </HookForm>
+  );
+};
 
-const CloseAssetForm = compose<React.ComponentType<OwnProps>>(
-  translate(),
-  withFormik<OwnProps, ICloseAssetFormValues>({
-    displayName: "close-asset",
-    mapPropsToValues: () => ({ [FIELDS.twoFactorCode]: "" }),
-    validationSchema: ({ t, twoFactorEnabled }: FormProps) =>
-      object().shape({
-        [FIELDS.twoFactorCode]: twoFactorValidator(t, twoFactorEnabled)
-      }),
-    handleSubmit: (values, { props, setSubmitting }) => {
-      props.onSubmit(values, setSubmitting);
-    }
-  })
-)(_CloseAssetForm);
-export default CloseAssetForm;
-
-interface OwnProps {
+interface Props {
+  errorMessage?: string;
   assetName?: string;
   asset: CloseableAssetType;
   onCancel: () => void;
   twoFactorEnabled: boolean;
-  onSubmit: (
-    values: ICloseAssetFormValues,
-    setSubmitting: SetSubmittingType
-  ) => void;
+  onSubmit: (values: ICloseAssetFormValues) => void;
 }
 
 export interface ICloseAssetFormValues {
   [FIELDS.twoFactorCode]: string;
 }
 
-type FormProps = WithTranslation & OwnProps;
+const CloseAssetForm = React.memo(_CloseAssetForm);
+export default CloseAssetForm;
