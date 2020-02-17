d4ce7271e9f8ad21a72b72d4123ec5e537ccc967
pavelzubov <pasha224@yandex.ru>
2020-02-17 09:33:58 +0300

    Change formik to react-hook-form in AssetEdit

--- packages/social-trader/src/modules/asset-settings/asset-edit.tsx
+++ packages/social-trader/src/modules/asset-settings/asset-edit.tsx
@@ -5,11 +5,10 @@ import inputImageShape from "components/form/input-image/input-image.validation"
 import GVButton from "components/gv-button";
 import LogoField from "components/logo-field/logo-field";
 import SettingsBlock from "components/settings-block/settings-block";
-import { FormikProps, withFormik } from "formik";
 import React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { compose } from "redux";
-import { SetSubmittingType } from "utils/types";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
 import {
   assetDescriptionShape,
   assetTitleShape
@@ -23,45 +22,73 @@ enum FIELDS {
 }
 
 const _AssetEdit: React.FC<Props> = ({
-  t,
-  values,
-  handleSubmit,
-  dirty,
-  isValid,
-  isSubmitting
-}) => (
-  <SettingsBlock>
-    <form
-      id="edit-form"
-      className={"asset-settings-block__form"}
-      onSubmit={handleSubmit}
-    >
-      <div className="asset-settings__block-wrapper">
-        <h3>{t("asset-settings.avatar.title")}</h3>
-        <LogoField name={FIELDS.logo} />
-      </div>
-      <h3>{t("asset-settings.name.title")}</h3>
-      <div className="asset-settings__block-wrapper">
-        <TitleField name={FIELDS.title} />
-      </div>
-      <h3>{t("asset-settings.strategy.title")}</h3>
-      <div className="asset-settings__block-wrapper asset-settings__block-wrapper--wide">
-        <DescriptionField
-          name={FIELDS.description}
-          description={values.description}
-        />
-      </div>
-      <GVButton
-        color="primary"
-        type={"submit"}
-        className="invest-form__submit-button"
-        disabled={!dirty || !isValid || isSubmitting}
+  editError,
+  onSubmit,
+  title,
+  description: descriptionProp,
+  logo
+}) => {
+  const [t] = useTranslation();
+
+  const form = useForm<AssetEditFormValues>({
+    defaultValues: {
+      [FIELDS.title]: title,
+      [FIELDS.description]: descriptionProp,
+      [FIELDS.logo]: {
+        src: logo.src
+      }
+    },
+    validationSchema: object().shape({
+      [FIELDS.title]: assetTitleShape(t),
+      [FIELDS.description]: assetDescriptionShape(t),
+      [FIELDS.logo]: inputImageShape(t)
+    }),
+    mode: "onBlur"
+  });
+  const {
+    watch,
+    formState: { isValid, dirty, isSubmitting, isSubmitted }
+  } = form;
+  const { description } = watch();
+
+  const isSuccessful = isSubmitted && !editError;
+  const disabled = !isValid || !dirty || isSubmitting || isSuccessful;
+  return (
+    <SettingsBlock>
+      <HookForm
+        className={"asset-settings-block__form"}
+        form={form}
+        onSubmit={onSubmit}
       >
-        {t("asset-settings.buttons.save")}
-      </GVButton>
-    </form>
-  </SettingsBlock>
-);
+        <div className="asset-settings__block-wrapper">
+          <h3>{t("asset-settings.avatar.title")}</h3>
+          <LogoField name={FIELDS.logo} />
+        </div>
+        <h3>{t("asset-settings.name.title")}</h3>
+        <div className="asset-settings__block-wrapper">
+          <TitleField name={FIELDS.title} />
+        </div>
+        <h3>{t("asset-settings.strategy.title")}</h3>
+        <div className="asset-settings__block-wrapper asset-settings__block-wrapper--wide">
+          <DescriptionField
+            name={FIELDS.description}
+            description={description}
+          />
+        </div>
+        <GVButton
+          color="primary"
+          type={"submit"}
+          className="invest-form__submit-button"
+          isPending={isSubmitting}
+          isSuccessful={isSuccessful}
+          disabled={disabled}
+        >
+          {t("asset-settings.buttons.save")}
+        </GVButton>
+      </HookForm>
+    </SettingsBlock>
+  );
+};
 
 export interface AssetEditFormValues {
   [FIELDS.title]: string;
@@ -69,42 +96,13 @@ export interface AssetEditFormValues {
   [FIELDS.logo]: IImageValue;
 }
 
-interface Props
-  extends OwnProps,
-    WithTranslation,
-    FormikProps<AssetEditFormValues> {}
-
-interface OwnProps {
+interface Props {
+  editError?: boolean;
   logo: IImageValue;
   title: string;
   description: string;
   onSubmit: (values: AssetEditFormValues) => void;
 }
 
-const AssetEdit = compose<React.ComponentType<OwnProps>>(
-  translate(),
-  withFormik<OwnProps, AssetEditFormValues>({
-    enableReinitialize: true,
-    displayName: "edit-form",
-    mapPropsToValues: props => {
-      return {
-        [FIELDS.title]: props.title,
-        [FIELDS.description]: props.description,
-        [FIELDS.logo]: {
-          src: props.logo.src
-        }
-      };
-    },
-    validationSchema: (props: Props) =>
-      object().shape({
-        [FIELDS.title]: assetTitleShape(props.t),
-        [FIELDS.description]: assetDescriptionShape(props.t),
-        [FIELDS.logo]: inputImageShape(props.t)
-      }),
-    handleSubmit: (values, { props, setSubmitting, resetForm }) => {
-      props.onSubmit(values, setSubmitting, resetForm);
-    }
-  }),
-  React.memo
-)(_AssetEdit);
+const AssetEdit = React.memo(_AssetEdit);
 export default AssetEdit;
