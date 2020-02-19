02454eb3256a30bbb9306e6d4df7fe00575db455
pavelzubov <pasha224@yandex.ru>
2020-02-19 20:11:40 +0300

    Remove unused components

--- packages/social-trader/src/components/deposit/components/form-fields/wallet-field.tsx
+++ packages/social-trader/src/components/deposit/components/form-fields/wallet-field.tsx
@@ -1,6 +1,5 @@
-import { DialogField } from "components/dialog/dialog-field";
 import { ISelectChangeEvent } from "components/select/select";
-import WalletSelect, {
+import {
   HookFormWalletSelect,
   ItemsType,
   WalletItemType
@@ -10,38 +9,12 @@ import { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 import { safeGetElemFromArray } from "utils/helpers";
 
-const _WalletField: React.FC<Props> = ({ name, wallets, onChange }) => {
-  const [t] = useTranslation();
-  const onChangeCurrencyFrom = useCallback(
-    (event: ISelectChangeEvent, target: JSX.Element): void => {
-      const wallet = safeGetElemFromArray(
-        wallets,
-        wallet => wallet.id === target.props.value
-      );
-      onChange(wallet);
-    },
-    [wallets]
-  );
-  return (
-    <DialogField>
-      <WalletSelect
-        name={name}
-        label={t("follow-program.create-account.from")}
-        items={wallets}
-        onChange={onChangeCurrencyFrom}
-      />
-    </DialogField>
-  );
-};
-
 interface Props {
   label?: string;
   wallets: ItemsType;
   name: string;
   onChange: (wallet: WalletItemType) => void;
 }
-
-export const WalletField = React.memo(_WalletField);
 
 const _HookFormWalletField: React.FC<Props> = ({
   label,
--- packages/social-trader/src/components/wallet-select/wallet-select.tsx
+++ packages/social-trader/src/components/wallet-select/wallet-select.tsx
@@ -1,25 +1,8 @@
-import CurrencySourceSelect from "components/currency-source-select/currency-source-select";
 import { CurrencySourceSelect as HookFormCurrencySourceSelect } from "components/currency-source-select/hook-form-currency-source-select";
 import { ISelectChangeEvent } from "components/select/select";
 import { WalletBaseData, WalletData } from "gv-api-web";
 import { TransferItemType } from "modules/transfer/transfer.types";
 import React from "react";
-
-const _WalletSelect: React.FC<Props> = ({
-  items,
-  onChange,
-  label,
-  name,
-  disabled
-}) => (
-  <CurrencySourceSelect
-    disabled={disabled}
-    label={label}
-    items={items}
-    name={name}
-    onChange={onChange}
-  />
-);
 
 export type ItemsType = Array<WalletItemType>;
 export type WalletItemType = WalletData | WalletBaseData | TransferItemType;
@@ -31,9 +14,6 @@ interface Props {
   onChange?: (event: ISelectChangeEvent, child: JSX.Element) => void;
   disabled?: boolean;
 }
-
-const WalletSelect = React.memo(_WalletSelect);
-export default WalletSelect;
 
 const _HookFormWalletSelect: React.FC<Props> = ({
   items,
