5cf436c784ceabd83b9cccbadeef3886d14f3020
pavelzubov <pasha224@yandex.ru>
2020-02-19 13:03:10 +0300

    Change formik to react-hook-form in RestorePassword

--- packages/social-trader/src/pages/auth/forgot-password/password-restore/password-restore.tsx
+++ packages/social-trader/src/pages/auth/forgot-password/password-restore/password-restore.tsx
@@ -1,12 +1,11 @@
 import FormError from "components/form/form-error/form-error";
 import GVButton from "components/gv-button";
-import GVFormikField from "components/gv-formik-field";
-import GVTextField from "components/gv-text-field";
-import { InjectedFormikProps, withFormik } from "formik";
+import { GVHookFormField } from "components/gv-hook-form-field";
+import { SimpleTextField } from "components/simple-fields/simple-text-field";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { compose } from "redux";
-import { SetSubmittingType } from "utils/types";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
 import { object, ref, string } from "yup";
 
 enum FIELDS {
@@ -14,46 +13,70 @@ enum FIELDS {
   confirmPassword = "confirmPassword"
 }
 
-const _RestorePassword: React.FC<InjectedFormikProps<
-  Props,
-  IRestorePasswordFormValues
->> = ({ t, isSubmitting, handleSubmit, error }) => (
-  <form id="passwordRestoreForm" onSubmit={handleSubmit} noValidate>
-    <GVFormikField
-      type="password"
-      name={FIELDS.password}
-      label={t("auth.password-restore.new-password.password-field-text")}
-      component={GVTextField}
-    />
-    <GVFormikField
-      type="password"
-      name={FIELDS.confirmPassword}
-      label={t(
-        "auth.password-restore.new-password.password-confirm-field-text"
-      )}
-      component={GVTextField}
-    />
-    <FormError error={error} />
-    <div className="password-restore__navigation">
-      <GVButton
-        type="submit"
-        id="passwordRestoreSubmit"
-        disabled={isSubmitting}
-      >
-        {t("auth.password-restore.new-password.confirm-button-text")}
-      </GVButton>
-    </div>
-  </form>
-);
+const _RestorePassword: React.FC<Props> = ({ errorMessage, onSubmit }) => {
+  const [t] = useTranslation();
+  const form = useForm<IRestorePasswordFormValues>({
+    defaultValues: {
+      [FIELDS.password]: "",
+      [FIELDS.confirmPassword]: ""
+    },
+    validationSchema: object().shape({
+      [FIELDS.password]: string()
+        .min(6, t("auth.password-restore.validators.password-weak"))
+        .required(t("auth.password-restore.validators.password-required")),
+      [FIELDS.confirmPassword]: string()
+        .oneOf(
+          [ref(FIELDS.password)],
+          t("auth.password-restore.validators.password-dont-match")
+        )
+        .required(
+          t("auth.password-restore.validators.confirm-password-required")
+        )
+    }),
+    mode: "onChange"
+  });
 
-interface Props extends OwnProps, WithTranslation {}
+  const {
+    formState: { isValid, dirty, isSubmitting, isSubmitted }
+  } = form;
 
-interface OwnProps {
-  onSubmit(
-    data: IRestorePasswordFormValues,
-    setSubmitting: SetSubmittingType
-  ): void;
-  error: string;
+  const isSuccessful = isSubmitted && !errorMessage;
+  const disabled = !isValid || !dirty || isSubmitting || isSuccessful;
+  return (
+    <HookForm form={form} onSubmit={onSubmit}>
+      <GVHookFormField
+        type="password"
+        name={FIELDS.password}
+        label={t("auth.password-restore.new-password.password-field-text")}
+        component={SimpleTextField}
+      />
+      <GVHookFormField
+        type="password"
+        name={FIELDS.confirmPassword}
+        label={t(
+          "auth.password-restore.new-password.password-confirm-field-text"
+        )}
+        component={SimpleTextField}
+      />
+      <FormError error={errorMessage} />
+      <div className="password-restore__navigation">
+        <GVButton
+          type="submit"
+          id="passwordRestoreSubmit"
+          isPending={isSubmitting}
+          isSuccessful={isSuccessful}
+          disabled={disabled}
+        >
+          {t("auth.password-restore.new-password.confirm-button-text")}
+        </GVButton>
+      </div>
+    </HookForm>
+  );
+};
+
+interface Props {
+  errorMessage?: string;
+  onSubmit: (data: IRestorePasswordFormValues) => void;
 }
 
 export interface IRestorePasswordFormValues {
@@ -61,32 +84,5 @@ export interface IRestorePasswordFormValues {
   confirmPassword: string;
 }
 
-const RestorePassword = compose<React.FC<OwnProps>>(
-  translate(),
-  withFormik<Props, IRestorePasswordFormValues>({
-    displayName: "passwordRestoreForm",
-    mapPropsToValues: () => ({
-      [FIELDS.password]: "",
-      [FIELDS.confirmPassword]: ""
-    }),
-    validationSchema: ({ t }: Props) =>
-      object().shape({
-        [FIELDS.password]: string()
-          .min(6, t("auth.password-restore.validators.password-weak"))
-          .required(t("auth.password-restore.validators.password-required")),
-        [FIELDS.confirmPassword]: string()
-          .oneOf(
-            [ref(FIELDS.password)],
-            t("auth.password-restore.validators.password-dont-match")
-          )
-          .required(
-            t("auth.password-restore.validators.confirm-password-required")
-          )
-      }),
-    handleSubmit: (values, { props, setSubmitting }) => {
-      props.onSubmit(values, setSubmitting);
-    }
-  }),
-  React.memo
-)(_RestorePassword);
+const RestorePassword = React.memo(_RestorePassword);
 export default RestorePassword;
