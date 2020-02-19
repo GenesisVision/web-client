94e040315f7691359e41e62857122ad92af9beab
pavelzubov <pasha224@yandex.ru>
2020-02-19 19:56:16 +0300

    Change formik to react-hook-form in FollowCreateExternalAccount

--- packages/social-trader/src/modules/follow-module/follow-popup/follow-popup-create-external-account.tsx
+++ packages/social-trader/src/modules/follow-module/follow-popup/follow-popup-create-external-account.tsx
@@ -2,52 +2,63 @@ import { DialogBottom } from "components/dialog/dialog-bottom";
 import { DialogButtons } from "components/dialog/dialog-buttons";
 import { DialogField } from "components/dialog/dialog-field";
 import GVButton from "components/gv-button";
-import GVFormikField from "components/gv-formik-field";
-import GVTextField from "components/gv-text-field";
-import { FormikProps, withFormik } from "formik";
+import { GVHookFormField } from "components/gv-hook-form-field";
+import { SimpleTextField } from "components/simple-fields/simple-text-field";
 import * as React from "react";
-import { useCallback } from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
-import { compose } from "redux";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
 
 import { CreateExternalAccountFormValidationSchema } from "./follow-popup-create-account.validators";
 
+export enum CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS {
+  secret = "secret",
+  key = "key"
+}
+
 const _FollowCreateExternalAccount: React.FC<CreateAccountFormProps> = ({
-  onClick,
-  isValid,
-  dirty,
-  t,
-  values
+  onClick
 }) => {
+  const [t] = useTranslation();
+
+  const form = useForm<CreateAccountFormValues>({
+    validationSchema: CreateExternalAccountFormValidationSchema({
+      t
+    }),
+    mode: "onChange"
+  });
+  const {
+    formState: { isValid, dirty }
+  } = form;
+
   const disableButton = !dirty || !isValid;
-  const handleNext = useCallback(() => onClick(values), [onClick, values]);
   return (
-    <form id="follow-create-account">
+    <HookForm form={form} onSubmit={onClick}>
       <DialogBottom>
         <DialogField>
-          <GVFormikField
+          <GVHookFormField
             wide
             type="text"
             name={CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS.key}
             label={t("attach-account-page.settings.fields.api-key")}
             autoComplete="off"
-            component={GVTextField}
+            component={SimpleTextField}
           />
         </DialogField>
         <DialogField>
-          <GVFormikField
+          <GVHookFormField
             wide
             type="text"
             name={CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS.secret}
             label={t("attach-account-page.settings.fields.api-secret")}
             autoComplete="off"
-            component={GVTextField}
+            component={SimpleTextField}
           />
         </DialogField>
         <DialogButtons>
           <GVButton
             wide
-            onClick={handleNext}
+            type="submit"
             className="invest-form__submit-button"
             disabled={disableButton}
           >
@@ -55,16 +66,11 @@ const _FollowCreateExternalAccount: React.FC<CreateAccountFormProps> = ({
           </GVButton>
         </DialogButtons>
       </DialogBottom>
-    </form>
+    </HookForm>
   );
 };
 
-export enum CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS {
-  secret = "secret",
-  key = "key"
-}
-
-interface OwnProps {
+interface CreateAccountFormProps {
   onClick: (values: CreateAccountFormValues) => void;
 }
 
@@ -73,22 +79,5 @@ export interface CreateAccountFormValues {
   [CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS.key]: string;
 }
 
-export interface CreateAccountFormProps
-  extends OwnProps,
-    WithTranslation,
-    FormikProps<CreateAccountFormValues> {}
-
-const FollowCreateExternalAccount = compose<React.ComponentType<OwnProps>>(
-  translate(),
-  withFormik({
-    displayName: "follow-create-account",
-    mapPropsToValues: () => ({
-      [CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS.secret]: "",
-      [CREATE_EXTERNAL_ACCOUNT_FORM_FIELDS.key]: ""
-    }),
-    validationSchema: CreateExternalAccountFormValidationSchema,
-    handleSubmit: () => {}
-  }),
-  React.memo
-)(_FollowCreateExternalAccount);
+const FollowCreateExternalAccount = React.memo(_FollowCreateExternalAccount);
 export default FollowCreateExternalAccount;
