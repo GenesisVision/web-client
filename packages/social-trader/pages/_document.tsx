4ec5376b8b98561e8ade3f380129a108ddedee39
pavelzubov <pasha224@yandex.ru>
2020-02-25 07:27:59 +0300

    Disable GA for non-prod deploys

--- packages/social-trader/pages/_document.tsx
+++ packages/social-trader/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#131e26" />
           <script
             dangerouslySetInnerHTML={{
-              __html: `var env = '${process.env.NODE_ENV}';if (env === 'production'){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              __html: `var env = '${process.env.NODE_ENV}';var hostname = '${process.env.HOSTNAME}';if (env === 'production' && hostname){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
