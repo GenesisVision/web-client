8318ec1303d2d8d17e42f58ac03e9239aea87441
pavelzubov <pasha224@yandex.ru>
2020-02-26 16:38:45 +0300

    Add setCookie to updateCurrency

--- packages/social-trader/src/modules/currency-select/services/currency-select.service.ts
+++ packages/social-trader/src/modules/currency-select/services/currency-select.service.ts
@@ -1,9 +1,12 @@
 import { updateAccountSettingsCurrencyAction } from "actions/account-settings-actions";
+import { ACCOUNT_CURRENCY_KEY } from "middlewares/update-account-settings-middleware/update-account-settings-middleware";
 import { Dispatch } from "redux";
+import { setCookie } from "utils/cookie";
 import { ActionType, CurrencyEnum } from "utils/types";
 
 export const updateCurrency = (currency: CurrencyEnum) => (
   dispatch: Dispatch<ActionType>
 ) => {
+  setCookie(ACCOUNT_CURRENCY_KEY, currency);
   dispatch(updateAccountSettingsCurrencyAction(currency));
 };
