d322c64a3fc583152a674c2b64322c33694dc44f
Anchello <bob.m@mail.ru>
2020-02-28 18:18:37 +0100

    GV-2283 moved the static text from the FundsContainer and the ProgramsContainer in the file of transactions

--- packages/social-trader/src/pages/landing-page/containers/programs-container/programs-container.tsx
+++ packages/social-trader/src/pages/landing-page/containers/programs-container/programs-container.tsx
@@ -2,6 +2,7 @@ import "./programs-container.scss";
 
 import ImageBaseElement from "components/avatar/image-base.element";
 import { ProgramDetailsListItem } from "gv-api-web";
+import { useTranslation } from "i18n";
 import { JoinButton } from "pages/landing-page/components/join-button";
 import ProgramsList from "pages/landing-page/components/programs/programs-list";
 import ProgramIcon from "pages/landing-page/images/common/program-icon.svg";
@@ -13,24 +14,27 @@ interface Props {
 }
 
 const ProgramsContainer: React.FC<Props> = ({ programs }) => {
+  const { t } = useTranslation();
   if (!programs.length) return null;
   return (
     <div className="programs-container">
       <div className="programs-container__info">
         <ImageBaseElement
           src={ProgramIcon}
-          alt="Programs"
+          alt={t("landing-page.programs-container.title")}
           className="programs-container__img"
         />
-        <h2 className="programs-container__title">Programs</h2>
+        <h2 className="programs-container__title">
+          {t("landing-page.programs-container.title")}
+        </h2>
         <p className="programs-container__text">
-          Select investment programs that suit your personal profile and let the
-          manager do the work for you. You will be able to withdraw your
-          investment or profits only at predefined intervals set by the
-          program’s manager.
+          {t("landing-page.programs-container.text")}
         </p>
-        <JoinButton eventLabel={"Discover"} href={PROGRAMS_ROUTE}>
-          Discover
+        <JoinButton
+          eventLabel={t("landing-page.buttons.discover")}
+          href={PROGRAMS_ROUTE}
+        >
+          {t("landing-page.buttons.discover")}
         </JoinButton>
       </div>
       <ProgramsList className="programs-container__list" programs={programs} />
