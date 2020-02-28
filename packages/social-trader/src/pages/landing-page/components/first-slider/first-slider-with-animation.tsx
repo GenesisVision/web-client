b742430cdcbb7a054c2f807cb0b87ffd99025fd3
pavelzubov <pasha224@yandex.ru>
2020-02-28 14:53:04 +0300

    Use JoinButton

--- packages/social-trader/src/pages/landing-page/components/first-slider/first-slider-with-animation.tsx
+++ packages/social-trader/src/pages/landing-page/components/first-slider/first-slider-with-animation.tsx
@@ -11,7 +11,6 @@ import {
   SliderText,
   SliderTitle
 } from "pages/landing-page/components/first-slider/slider.blocks";
-import LPButton from "pages/landing-page/components/lp-button/lp-button";
 import { TSlide } from "pages/landing-page/static-data/slides";
 import React, { useCallback, useState } from "react";
 import { animated, useTransition } from "react-spring";
@@ -90,7 +89,7 @@ const _FirstSliderWithAnimation: React.FC<Props> = ({
         <SliderControlsWrapper>
           {transitions.map(({ item, props: { transform, ...rest }, key }) => (
             <animated.div key={key} style={rest as any}>
-              <LPButton href={item.link}>Join</LPButton>
+              <JoinButton href={item.link}>Join</JoinButton>
             </animated.div>
           ))}
           <SliderControls
