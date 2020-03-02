f309339265d490144dedb91bf838fd9ebd383175
pavelzubov <pasha224@yandex.ru>
2020-03-02 18:55:37 +0300

    Remove useless fetchPlatformSettings

--- packages/social-trader/pages/create-fund/index.tsx
+++ packages/social-trader/pages/create-fund/index.tsx
@@ -1,4 +1,3 @@
-import platformActions from "actions/platform-actions";
 import withDefaultLayout from "decorators/with-default-layout";
 import withPrivateRoute from "decorators/with-private-route";
 import CreateFundPage from "pages/create-fund/create-fund.page";
@@ -7,17 +6,12 @@ import React from "react";
 import { compose } from "redux";
 import { NextPageWithRedux } from "utils/types";
 
-const CreateFund: NextPageWithRedux<Props, {}> = ({}) => {
+const CreateFund: NextPageWithRedux<Props, {}> = () => {
   return <CreateFundPage />;
 };
 
 CreateFund.getInitialProps = async ctx => {
-  await Promise.all([
-    ctx.reduxStore.dispatch(
-      async dispatch => await dispatch(platformActions.fetchPlatformSettings())
-    ),
-    ctx.reduxStore.dispatch(fetchWalletsWithCtx(ctx))
-  ]);
+  await ctx.reduxStore.dispatch(fetchWalletsWithCtx(ctx));
   return {};
 };
 
