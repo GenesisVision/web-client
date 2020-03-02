b4f71243fc9de87bb54d88f0bb9e19cd8f2d963e
pavelzubov <pasha224@yandex.ru>
2020-03-02 15:24:56 +0300

    Add --wrap Add className prop

--- packages/social-trader/src/components/row/row.tsx
+++ packages/social-trader/src/components/row/row.tsx
@@ -3,10 +3,17 @@ import "./row.scss";
 import classNames from "classnames";
 import React from "react";
 
-export const Row: React.FC<Props> = ({ small, large, children }) => {
+export const Row: React.FC<Props> = ({
+  className,
+  wrap,
+  small,
+  large,
+  children
+}) => {
   return (
     <div
-      className={classNames("row", {
+      className={classNames("row", className, {
+        "row--wrap": wrap,
         "row--small": small,
         "row--large": large
       })}
@@ -17,6 +24,8 @@ export const Row: React.FC<Props> = ({ small, large, children }) => {
 };
 
 interface Props {
+  className?: string;
+  wrap?: boolean;
   small?: boolean;
   large?: boolean;
 }
