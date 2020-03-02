5e66806b92f1cd83b45f66681f32eb4afd6f91a5
pavelzubov <pasha224@yandex.ru>
2020-03-02 15:25:08 +0300

    Add className prop

--- packages/social-trader/src/components/row-item/row-item.tsx
+++ packages/social-trader/src/components/row-item/row-item.tsx
@@ -3,10 +3,15 @@ import "./row-item.scss";
 import classNames from "classnames";
 import React from "react";
 
-export const RowItem: React.FC<Props> = ({ small, large, children }) => {
+export const RowItem: React.FC<Props> = ({
+  className,
+  small,
+  large,
+  children
+}) => {
   return (
     <div
-      className={classNames("row-item", {
+      className={classNames("row-item", className, {
         "row-item--small": small,
         "row-item--large": large
       })}
@@ -17,6 +22,7 @@ export const RowItem: React.FC<Props> = ({ small, large, children }) => {
 };
 
 interface Props {
+  className?: string;
   small?: boolean;
   large?: boolean;
 }
