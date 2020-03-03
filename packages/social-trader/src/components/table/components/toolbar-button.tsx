acdbf763789e31293ff957fffbee46a2429442f1
pavelzubov <pasha224@yandex.ru>
2020-03-03 12:06:40 +0300

    Use RowItem instead TableToolbarItemBlock

--- packages/social-trader/src/components/table/components/toolbar-button.tsx
+++ packages/social-trader/src/components/table/components/toolbar-button.tsx
@@ -1,6 +1,6 @@
 import Link from "components/link/link";
 import { useToLink } from "components/link/link.helper";
-import { TableToolbarItemBlock } from "components/table/components/table-toolbar-item.block";
+import { RowItem } from "components/row-item/row-item";
 import React from "react";
 
 export const ToolbarButton: React.FC<{
@@ -9,10 +9,10 @@ export const ToolbarButton: React.FC<{
 }> = React.memo(({ text, route }) => {
   const { linkCreator } = useToLink();
   return (
-    <TableToolbarItemBlock>
+    <RowItem>
       <Link to={linkCreator(route)} className="toolbar-button">
         {text}
       </Link>
-    </TableToolbarItemBlock>
+    </RowItem>
   );
 });
