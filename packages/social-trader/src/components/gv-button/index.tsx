2dca69c8ea92547f80d133f0126332c05769b138
pavelzubov <pasha224@yandex.ru>
2020-03-04 21:21:04 +0300

    Correct success label in GvButton

--- packages/social-trader/src/components/gv-button/index.tsx
+++ packages/social-trader/src/components/gv-button/index.tsx
@@ -77,7 +77,20 @@ const GVButton: React.FC<GVButtonProps> = ({
       type={type}
       name={name}
     >
-      {isSuccessful ? "✔" : children}
+      <span
+        className={classNames("gv-btn__label", {
+          "gv-btn__label--success": isSuccessful
+        })}
+      >
+        {children}
+      </span>
+      <span
+        className={classNames("gv-btn__success-symbol", {
+          "gv-btn__success-symbol--success": isSuccessful
+        })}
+      >
+        ✔
+      </span>
     </button>
   );
 };
