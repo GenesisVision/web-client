70b5dd5ceacb3ae7bf53653304beebe89fae2ca4
pavelzubov <pasha224@yandex.ru>
2020-03-04 14:39:23 +0300

    Correct a init color

--- packages/social-trader/pages/_document.tsx
+++ packages/social-trader/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <style>{"body, a, a:visited {color: white}"}</style>
+          <style>{"body {color: white}; a, a:visited {color: #03bdaf}"}</style>
           <link rel="icon" sizes="192x192" href="/icon.png" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <link rel="shortcut icon" href="/favicon.ico" />
