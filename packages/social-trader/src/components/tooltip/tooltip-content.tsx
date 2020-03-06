c5efeda836cfdd2561932b91e2f40779fb03ac93
pavelzubov <pasha224@yandex.ru>
2020-03-06 12:48:33 +0300

    Add --small and style file to TooltipContent

--- packages/social-trader/src/components/tooltip/tooltip-content.tsx
+++ packages/social-trader/src/components/tooltip/tooltip-content.tsx
@@ -1,14 +1,30 @@
+import "./tooltip-content.scss";
+
+import classNames from "classnames";
 import { PopoverContentCardBlock } from "components/popover/popover-card.block";
 import React from "react";
 
-const _TooltipContent: React.FC<{ fixed?: boolean } & React.HTMLAttributes<
-  HTMLDivElement
->> = ({ children, fixed = true }) => {
+const _TooltipContent: React.FC<Props> = ({
+  small,
+  children,
+  fixed = true
+}) => {
   return (
     <PopoverContentCardBlock size={"small"} fixed={fixed}>
-      {children}
+      <div
+        className={classNames({
+          "tooltip-content--small": small
+        })}
+      >
+        {children}
+      </div>
     </PopoverContentCardBlock>
   );
 };
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  small?: boolean;
+  fixed?: boolean;
+}
 
 export const TooltipContent = React.memo(_TooltipContent);
