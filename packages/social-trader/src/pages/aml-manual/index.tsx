a39c9fe113e131ca58cc12e2c8028acbd1835b39
Anchello <bob.m@mail.ru>
2020-03-06 14:06:45 +0100

    GV-2283 moved titles of pages in the translations file

--- packages/social-trader/src/pages/aml-manual/index.tsx
+++ packages/social-trader/src/pages/aml-manual/index.tsx
@@ -1,14 +1,16 @@
 import "pages/landing-page/styles/index.scss";
 import "pages/landing-page/styles/internal.scss";
 
+import { useTranslation } from "i18n";
 import { NextPage } from "next";
 import AmlManualSection from "pages/landing-page/components/aml-manual-section/aml-manual-section";
 import Layout from "pages/landing-page/layouts/_layout";
 import React from "react";
 
 export const AmlManual: NextPage = () => {
+  const { t } = useTranslation();
   return (
-    <Layout title="Genesis Vision Aml Manual">
+    <Layout title={t("landing-page.page-titles.aml-manual")}>
       <main className="internal">
         <div className="internal__container">
           <AmlManualSection />
--- packages/social-trader/src/pages/glossary/index.tsx
+++ packages/social-trader/src/pages/glossary/index.tsx
@@ -1,14 +1,16 @@
 import "pages/landing-page/styles/index.scss";
 import "pages/landing-page/styles/internal.scss";
 
+import { useTranslation } from "i18n";
 import { NextPage } from "next";
 import GlossarySection from "pages/landing-page/components/glossary-section/glossary-section";
 import Layout from "pages/landing-page/layouts/_layout";
 import React from "react";
 
 export const Glossary: NextPage = () => {
+  const { t } = useTranslation();
   return (
-    <Layout title="Genesis Vision Glossary">
+    <Layout title={t("landing-page.page-titles.glossary")}>
       <main className="internal">
         <div className="internal__container">
           <GlossarySection />
