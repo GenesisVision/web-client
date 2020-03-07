19241844f04dc2afb0ec3212917e79b08192e619
pavelzubov <pasha224@yandex.ru>
2020-03-07 07:41:38 +0300

    Add hide --modificator in Row Correct --middle

--- packages/social-trader/src/components/row/row.tsx
+++ packages/social-trader/src/components/row/row.tsx
@@ -4,14 +4,25 @@ import classNames from "classnames";
 import React from "react";
 
 export const Row: React.FC<Props> = props => {
-  const { center = true, className, wrap, small, large, children } = props;
+  const {
+    middle = true,
+    hide,
+    center = true,
+    className,
+    wrap,
+    small,
+    large,
+    children
+  } = props;
   return (
     <div
       {...props}
       className={classNames("row", className, {
+        "row--hidden": hide,
         "row--center": center,
         "row--wrap": wrap,
         "row--small": small,
+        "row--middle": middle && !(small || large),
         "row--large": large
       })}
     >
@@ -21,9 +32,11 @@ export const Row: React.FC<Props> = props => {
 };
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  hide?: boolean;
   center?: boolean;
   className?: string;
   wrap?: boolean;
   small?: boolean;
+  middle?: boolean;
   large?: boolean;
 }
