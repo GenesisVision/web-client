b8e8c4f78d93a43a745beb5468ecf6a4f2279c81
pavelzubov <pasha224@yandex.ru>
2020-03-12 10:10:00 +0300

    Move getGroups, parseDate, sortGroups, NotificationGroups to notifications.helpers

---
+++ packages/social-trader/src/components/notifications/components/notifications.helpers.ts
@@ -0,0 +1,35 @@
+import dayjs from "dayjs";
+import { NotificationViewModel } from "gv-api-web";
+
+export type NotificationGroups = { [name: number]: NotificationViewModel[] };
+
+export const parseDate = (
+  unix: number,
+  sameDay: string,
+  lastDay: string
+): string =>
+  dayjs(unix)
+    .calendar(undefined, {
+      sameDay: `[${sameDay}], DD MMMM`,
+      lastDay: `[${lastDay}], DD MMMM`,
+      lastWeek: "dddd, DD MMMM",
+      sameElse: "dddd, DD MMMM"
+    })
+    .toUpperCase();
+
+export const sortGroups = (a: string, b: string) => parseInt(b) - parseInt(a);
+
+export const getGroups = (
+  notifications: NotificationViewModel[]
+): NotificationGroups =>
+  notifications.reduce<NotificationGroups>((acc, notification) => {
+    const key = dayjs(notification.date)
+      .startOf("day")
+      .toDate()
+      .getTime();
+    if (!Array.isArray(acc[key])) {
+      acc[key] = [];
+    }
+    acc[key].push(notification);
+    return acc;
+  }, {});
--- packages/social-trader/src/components/notifications/components/notifications.tsx
+++ packages/social-trader/src/components/notifications/components/notifications.tsx
@@ -8,43 +8,21 @@ import Link from "components/link/link";
 import { useToLink } from "components/link/link.helper";
 import ClearButton from "components/notifications/components/ClearButton";
 import NotificationsGroup from "components/notifications/components/notification-group/notification-group";
+import {
+  getGroups,
+  NotificationGroups,
+  parseDate,
+  sortGroups
+} from "components/notifications/components/notifications.helpers";
 import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
 import Spinner from "components/spiner/spiner";
-import dayjs from "dayjs";
 import { NotificationViewModel } from "gv-api-web";
 import useApiRequest from "hooks/api-request.hook";
 import React, { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 import { NOTIFICATIONS_ROUTE } from "../notifications.routes";
-
-const parseDate = (unix: number, sameDay: string, lastDay: string): string =>
-  dayjs(unix)
-    .calendar(undefined, {
-      sameDay: `[${sameDay}], DD MMMM`,
-      lastDay: `[${lastDay}], DD MMMM`,
-      lastWeek: "dddd, DD MMMM",
-      sameElse: "dddd, DD MMMM"
-    })
-    .toUpperCase();
-
-const sortGroups = (a: string, b: string) => parseInt(b) - parseInt(a);
-
-const getGroups = (
-  notifications: NotificationViewModel[]
-): NotificationGroups =>
-  notifications.reduce<NotificationGroups>((acc, notification) => {
-    const key = dayjs(notification.date)
-      .startOf("day")
-      .toDate()
-      .getTime();
-    if (!Array.isArray(acc[key])) {
-      acc[key] = [];
-    }
-    acc[key].push(notification);
-    return acc;
-  }, {});
 
 const _Notifications: React.FC<Props> = ({
   notifications = [],
@@ -130,5 +108,3 @@ interface Props {
   total: number;
   notifications: NotificationViewModel[];
 }
-
-type NotificationGroups = { [name: number]: NotificationViewModel[] };
