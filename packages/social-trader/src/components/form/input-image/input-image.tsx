4779c7c78ab15e2551c07eba839973a901b76f48
pavelzubov <pasha224@yandex.ru>
2020-03-18 12:47:32 +0300

    Use DropZoneWrapper in InputImage

--- packages/social-trader/src/components/form/input-image/input-image.tsx
+++ packages/social-trader/src/components/form/input-image/input-image.tsx
@@ -1,15 +1,13 @@
 import classNames from "classnames";
+import { DropZoneWrapper } from "components/form/input-image/drop-zone-wrapper";
 import { InputImageCropper } from "components/form/input-image/input-image-cropper";
 import "cropperjs/dist/cropper.css";
 import * as React from "react";
 import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
 import { useTranslation } from "react-i18next";
 
 import InputImageDefault from "./input-image-default";
 import "./input-image.scss";
-
-type FileWithPreview = any;
 
 const _InputImage: React.FC<IInputImageProps> = ({
   value = {},
@@ -22,50 +20,6 @@ const _InputImage: React.FC<IInputImageProps> = ({
   const { src, image } = value;
 
   const [t] = useTranslation();
-
-  const onDrop = useCallback(
-    (files: FileWithPreview[]) => {
-      if (files.length === 0) return;
-
-      const file = files[0];
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        let src = reader.result as string;
-        let img = new Image();
-        img.src = src;
-        img.onload = () => {
-          const cropped: INewImage = {
-            cropped: file,
-            name: file.name,
-            type: file.type,
-            size: file.size,
-            height: img.height,
-            width: img.width,
-            src
-          };
-
-          onChange({
-            target: { value: { image: cropped }, name }
-          });
-        };
-      };
-      reader.readAsDataURL(file);
-    },
-    [onChange, name]
-  );
-
-  const {
-    open,
-    getRootProps,
-    getInputProps,
-    isDragAccept,
-    isDragReject
-  } = useDropzone({
-    noClick: true,
-    onDrop,
-    accept: "image/jpeg, image/png"
-  });
 
   const clear = useCallback(
     (event: React.SyntheticEvent) => {
@@ -85,52 +39,45 @@ const _InputImage: React.FC<IInputImageProps> = ({
         "input-image--error": error !== undefined
       })}
     >
-      <div {...getRootProps({ className: "input-image__dropzone" })}>
-        <input {...getInputProps()} />
-        {isDragAccept && (
-          <div className="input-image__dropzone-helper">
-            {t("input-image.drop-files")}
+      <DropZoneWrapper
+        className="input-image__dropzone"
+        name={name}
+        onChange={onChange}
+        content={open => (
+          <div className="input-image__dropzone-content">
+            <div className="input-image__image-container">
+              {image && !hasSizeError && (
+                <InputImageCropper
+                  error={error}
+                  image={image}
+                  onChange={onChange}
+                />
+              )}
+
+              {image && hasSizeError && (
+                <InputImageDefault defaultImage={image.src} />
+              )}
+
+              {!image && (
+                <InputImageDefault src={src} defaultImage={defaultImage} />
+              )}
+            </div>
+            <p className="input-image__text input-image__text--big">
+              {t("input-image.drag-or-click")}
+              <span className="input-image__text-upload" onClick={open}>
+                {t("input-image.upload")}
+              </span>
+              {t("input-image.to-browse")}
+            </p>
+            <p
+              className="input-image__text input-image__text--small"
+              onClick={open}
+            >
+              {t("input-image.tap-to-upload")}
+            </p>
           </div>
         )}
-        {isDragReject && (
-          <div className="input-image__dropzone-helper">
-            {t("Some files will be rejected")}
-          </div>
-        )}
-
-        <div className="input-image__dropzone-content">
-          <div className="input-image__image-container">
-            {image && !hasSizeError && (
-              <InputImageCropper
-                error={error}
-                image={image}
-                onChange={onChange}
-              />
-            )}
-
-            {image && hasSizeError && (
-              <InputImageDefault defaultImage={image.src} />
-            )}
-
-            {!image && (
-              <InputImageDefault src={src} defaultImage={defaultImage} />
-            )}
-          </div>
-          <p className="input-image__text input-image__text--big">
-            {t("input-image.drag-or-click")}
-            <span className="input-image__text-upload" onClick={open}>
-              {t("input-image.upload")}
-            </span>
-            {t("input-image.to-browse")}
-          </p>
-          <p
-            className="input-image__text input-image__text--small"
-            onClick={open}
-          >
-            {t("input-image.tap-to-upload")}
-          </p>
-        </div>
-      </div>
+      />
       {(image || src) && (
         <div className="input-image__clear-btn" onClick={clear}>
           &#10006;
