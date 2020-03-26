e2e5376a3dbb1f3f4de4e3da58a47105b4666572
pavelzubov <pasha224@yandex.ru>
2020-03-26 15:39:02 +0300

    Create header.service.ts, add getHeader, use in fetchProfileHeaderInfoAction

--- packages/social-trader/src/components/header/actions/header-actions.ts
+++ packages/social-trader/src/components/header/actions/header-actions.ts
@@ -1,10 +1,9 @@
 import { PROFILE_HEADER } from "components/header/header.constants";
+import { getHeader } from "components/header/services/header.service";
 import { ProfileHeaderViewModel } from "gv-api-web";
-import profileApi from "services/api-client/profile-api";
-import authService from "services/auth-service";
 import { ApiAction } from "utils/types";
 
 export const fetchProfileHeaderInfoAction = (): ApiAction<ProfileHeaderViewModel> => ({
   type: PROFILE_HEADER,
-  payload: profileApi.getProfileHeader(authService.getAuthArg())
+  payload: getHeader()
 });
