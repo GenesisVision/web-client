edfad8e229f7ee1be0ea12f3c3f825712b18e65c
pavelzubov <pasha224@yandex.ru>
2020-03-28 15:03:13 +0300

    Add assetType prop, use real api method in close-position-button.tsx

--- packages/social-trader/src/pages/invest/programs/program-details/program-history-section/program-open-positions/close-position-button.tsx
+++ packages/social-trader/src/pages/invest/programs/program-details/program-history-section/program-open-positions/close-position-button.tsx
@@ -1,11 +1,14 @@
 import { CloseCircleButton } from "components/close-circle-button/close-circle-button";
 import ConfirmPopup from "components/confirm-popup/confirm-popup";
+import { TRADE_ASSET_TYPE } from "constants/constants";
 import useApiRequest from "hooks/api-request.hook";
 import useIsOpen from "hooks/is-open.hook";
+import { closePosition } from "pages/invest/programs/program-details/service/program-details.service";
 import React, { useCallback } from "react";
 import { useTranslation } from "react-i18next";
 
 const _ClosePositionButton: React.FC<Props> = ({
+  assetType,
   id,
   symbol,
   volume,
@@ -15,7 +18,7 @@ const _ClosePositionButton: React.FC<Props> = ({
   const [isOpenPopup, setOpenPopup, setClosePopup] = useIsOpen();
   const { sendRequest, isPending: disabled } = useApiRequest({
     successMessage: "copytrading-tables.close-trade-confirm.success-message",
-    request: async (id: string) => {},
+    request: closePosition(assetType),
     middleware: [
       () => {
         onApply && onApply();
@@ -27,8 +30,8 @@ const _ClosePositionButton: React.FC<Props> = ({
     setOpenPopup();
   }, []);
   const handleApplyCancelRequest = useCallback(() => {
-    return sendRequest({ id });
-  }, [id]);
+    return sendRequest({ id, symbol });
+  }, [id, symbol]);
   return (
     <>
       <CloseCircleButton onClick={handleOpenClose} />
@@ -51,6 +54,7 @@ const _ClosePositionButton: React.FC<Props> = ({
 };
 
 interface Props {
+  assetType: TRADE_ASSET_TYPE;
   onApply?: () => void;
   symbol: string;
   volume: number;
