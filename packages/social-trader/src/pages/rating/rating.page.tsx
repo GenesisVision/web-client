7b4da18514b9c7a1c2cdfd2f3124fdb7a21c185a
pavelzubov <pasha224@yandex.ru>
2020-04-02 09:30:16 +0300

    Add RatingBlockContainer components to rating.page.tsx

--- packages/social-trader/src/pages/rating/rating.page.tsx
+++ packages/social-trader/src/pages/rating/rating.page.tsx
@@ -1,14 +1,58 @@
 import Page from "components/page/page";
-import { RatingContainer } from "pages/rating/rating.container";
+import { RowItem } from "components/row-item/row-item";
+import { Row } from "components/row/row";
+import { RatingBlockContainer } from "pages/rating/rating.block.container";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { formatCurrencyValue } from "utils/formatter";
 
 export const RatingPage: React.FC<Props> = () => {
   const [t] = useTranslation();
   const title = t(`Rating`);
   return (
     <Page title={title}>
-      <RatingContainer />
+      <Row>
+        <RowItem wide>
+          <RatingBlockContainer
+            title={"Follower rating"}
+            valueField={"followersCount"}
+            sorting={"ByFollowersDesc"}
+            renderValue={value => {
+              return <>{value}</>;
+            }}
+          />
+        </RowItem>
+        <RowItem wide>
+          <RatingBlockContainer
+            title={"Trading profit rating"}
+            valueField={"tradingProfit"}
+            sorting={"ByTradingProfitDesc"}
+            renderValue={value => {
+              return <>{value} %</>;
+            }}
+          />
+        </RowItem>
+        {/*<RowItem>
+          <RatingBlockContainer
+            title={"Invest profit rating"}
+            valueField={"investingProfit"}
+            sorting={"ByInvestingProfitDesc"}
+            renderValue={value => {
+              return <>{value} %</>;
+            }}
+          />
+        </RowItem>*/}
+        <RowItem wide>
+          <RatingBlockContainer
+            title={"AUM rating"}
+            valueField={"assetsUnderManagement"}
+            sorting={"ByAumDesc"}
+            renderValue={value => {
+              return <>{formatCurrencyValue(+value, "GVT")} GVT</>;
+            }}
+          />
+        </RowItem>
+      </Row>
     </Page>
   );
 };
