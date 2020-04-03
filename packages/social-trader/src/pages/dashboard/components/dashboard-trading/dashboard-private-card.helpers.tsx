7fd44cceb90ceefd398e2877bc56814b0293c123
pavelzubov <pasha224@yandex.ru>
2020-04-03 11:27:31 +0300

    Add makeProgramLink prop to MakeProgramButton

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card.helpers.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card.helpers.tsx
@@ -1,5 +1,6 @@
 import GVButton from "components/gv-button";
 import Hint from "components/hint/hint";
+import { ToType } from "components/link/link";
 import { useToLink } from "components/link/link.helper";
 import { MutedText } from "components/muted-text/muted-text";
 import { VERTICAL_POPOVER_POS } from "components/popover/popover";
@@ -15,38 +16,48 @@ import * as React from "react";
 import { CurrencyEnum } from "utils/types";
 
 export const MakeProgramButton: React.FC<{
+  makeProgramLink?: string | ToType;
   necessaryMoney: string;
   isEnoughMoney: boolean;
   id: string;
   clearAnchor: (event: TEvent) => void;
-}> = React.memo(({ isEnoughMoney, id, clearAnchor, necessaryMoney }) => {
-  const { linkCreator } = useToLink();
-  const [t] = useTranslation();
-  const makeProgramLinkMethod = makeProgramLinkCreator({
-    assetFrom: CONVERT_ASSET.ACCOUNT,
-    assetTo: CONVERT_ASSET.PROGRAM
-  });
-  const makeProgramLink = linkCreator(makeProgramLinkMethod(id));
-  const label = t("dashboard-page.trading.actions.make-program");
-  return isEnoughMoney ? (
-    <TableCardActionsItem to={makeProgramLink} onClick={clearAnchor}>
-      {label}
-    </TableCardActionsItem>
-  ) : (
-    <GVButton variant="text" color="secondary">
-      <MutedText>
-        <Hint
-          content={label}
-          vertical={VERTICAL_POPOVER_POS.BOTTOM}
-          tooltipContent={t(
-            "dashboard-page.trading.tooltips.is-not-enough-money",
-            { value: necessaryMoney }
-          )}
-        />
-      </MutedText>
-    </GVButton>
-  );
-});
+}> = React.memo(
+  ({
+    makeProgramLink: makeProgramLinkProp,
+    isEnoughMoney,
+    id,
+    clearAnchor,
+    necessaryMoney
+  }) => {
+    const { linkCreator } = useToLink();
+    const [t] = useTranslation();
+    const makeProgramLinkMethod = makeProgramLinkCreator({
+      assetFrom: CONVERT_ASSET.ACCOUNT,
+      assetTo: CONVERT_ASSET.PROGRAM
+    });
+    const makeProgramLink =
+      makeProgramLinkProp || linkCreator(makeProgramLinkMethod(id));
+    const label = t("dashboard-page.trading.actions.make-program");
+    return isEnoughMoney ? (
+      <TableCardActionsItem to={makeProgramLink} onClick={clearAnchor}>
+        {label}
+      </TableCardActionsItem>
+    ) : (
+      <GVButton variant="text" color="secondary">
+        <MutedText>
+          <Hint
+            content={label}
+            vertical={VERTICAL_POPOVER_POS.BOTTOM}
+            tooltipContent={t(
+              "dashboard-page.trading.tooltips.is-not-enough-money",
+              { value: necessaryMoney }
+            )}
+          />
+        </MutedText>
+      </GVButton>
+    );
+  }
+);
 
 export const ConfirmTFAButton: React.FC<{
   onApply: VoidFunction;
