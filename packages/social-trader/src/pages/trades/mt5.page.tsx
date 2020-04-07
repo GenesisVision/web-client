be17d7641ccb23fa8222a8880b81df10f4de5c16
e.grebenshikov <evgreb@gmail.com>
2020-04-07 17:03:24 +0300

    GV-2427 adblock message

--- packages/social-trader/src/pages/trades/mt5.page.tsx
+++ packages/social-trader/src/pages/trades/mt5.page.tsx
@@ -1,15 +1,17 @@
-import "./trades.scss";
-
 import Page from "components/page/page";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import "./trades.scss";
+
 const _Mt5: React.FC = () => {
   const [t] = useTranslation();
   const title = t("mt5-page.title");
+
   return (
     <Page showTitle title={title}>
       <div className="mt-frame">
+        <div className={"addblock-warning"}>{t("mt5-page.warning")}</div>
         <iframe
           title={title}
           src="https://trade.mql5.com/trade?servers=GenesisMarketsLTD-Demo,GenesisMarketsLTD-Live&amp;trade_server=GenesisMarketsLTD-Live&amp;demo_server=GenesisMarketsLTD-Demo&amp;lang=en&amp;save_password=on"
