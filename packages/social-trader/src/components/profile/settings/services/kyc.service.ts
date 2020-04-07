043ccc5fe82623574fbbc0a19ea58574ba122684
pavelzubov <pasha224@yandex.ru>
2020-04-07 12:12:40 +0300

    Update style version in kyc.service.ts

--- packages/social-trader/src/components/profile/settings/services/kyc.service.ts
+++ packages/social-trader/src/components/profile/settings/services/kyc.service.ts
@@ -44,7 +44,7 @@ export const loadKycIFrame = (externalUserId: string) => {
           },
           requiredDocuments: "IDENTITY:PASSPORT,ID_CARD,DRIVERS;SELFIE:SELFIE",
           uiConf: {
-            customCss: "https://genesis.vision/assets/kyc/style.css?v=1",
+            customCss: "https://genesis.vision/assets/kyc/style.css?v=2",
             steps: {}
           }
         },
