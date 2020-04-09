81217b6a2fd56a4a739ec9eef40fd6f522d9b1a7
pavelzubov <pasha224@yandex.ru>
2020-04-09 17:17:13 +0300

    Use PostContainer instead Post in PostPage

--- packages/social-trader/src/pages/posts/post.page.tsx
+++ packages/social-trader/src/pages/posts/post.page.tsx
@@ -1,24 +1,14 @@
-import { getPost } from "components/conversation/conversation.service";
-import { Post } from "components/conversation/post/post";
+import { PostContainer } from "components/conversation/post/post-container";
 import Page from "components/page/page";
-import useApiRequest from "hooks/api-request.hook";
-import React, { useEffect } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 
 export const PostPage: React.FC<Props> = ({ id }) => {
   const [t] = useTranslation();
   const title = t(`Post`);
-  const { data, sendRequest } = useApiRequest({
-    request: () => getPost({ id }),
-    fetchOnMount: true
-  });
-  useEffect(() => {
-    sendRequest();
-  }, [id]);
-  if (!data) return null;
   return (
     <Page title={title}>
-      <Post post={data} updateData={sendRequest} />
+      <PostContainer id={id} />
     </Page>
   );
 };
