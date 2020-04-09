227ac9d29d4f3e9381058569ef777d74e5b26e93
pavelzubov <pasha224@yandex.ru>
2020-04-09 20:50:22 +0300

    Create users page next.js file

---
+++ packages/social-trader/pages/social/users/index.tsx
@@ -0,0 +1,15 @@
+import withDefaultLayout from "decorators/with-default-layout";
+import { NextPage } from "next";
+import { UsersPage } from "pages/social/users/users.page";
+import React from "react";
+import { compose } from "redux";
+
+const Page: NextPage = () => {
+  return <UsersPage />;
+};
+
+Page.getInitialProps = async () => ({
+  namespacesRequired: ["users-page"]
+});
+
+export default compose(withDefaultLayout)(Page);
