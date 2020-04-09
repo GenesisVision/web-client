fa6ef99e3cd0421e1d7affa2e83c2f30fd9ae582
pavelzubov <pasha224@yandex.ru>
2020-04-09 17:20:07 +0300

    Use PostListWithInput in FeedContainer, TagsContainer

--- packages/social-trader/src/pages/feed/feed.container.tsx
+++ packages/social-trader/src/pages/feed/feed.container.tsx
@@ -1,12 +1,9 @@
-import { getEmptyPostLoaderData } from "components/conversation/conversation.loader";
 import {
   getGlobalFeed,
   getNewsFeed
 } from "components/conversation/conversation.service";
-import { PostList } from "components/conversation/post-list/post-list";
-import { PostInputContainer } from "components/conversation/post/post-input/post-input.container";
-import useApiRequest from "hooks/api-request.hook";
-import React, { useEffect } from "react";
+import { PostListWithInput } from "components/conversation/post-list/post-list-with-input";
+import React from "react";
 import { useSelector } from "react-redux";
 import { isAuthenticatedSelector } from "reducers/auth-reducer";
 
@@ -21,22 +18,9 @@ interface Props {
 
 const _FeedContainer: React.FC<Props> = ({ feedType }) => {
   const isAuthenticated = useSelector(isAuthenticatedSelector);
-  const { data, sendRequest } = useApiRequest({
-    request: () =>
-      feedType === FEED_TYPE.ALL ? getGlobalFeed() : getNewsFeed()
-  });
-  useEffect(() => {
-    sendRequest();
-  }, []);
+  const fetchMethod = feedType === FEED_TYPE.ALL ? getGlobalFeed : getNewsFeed;
   return (
-    <div>
-      {isAuthenticated && <PostInputContainer onSuccess={sendRequest} />}
-      <PostList
-        loaderData={[getEmptyPostLoaderData()]}
-        data={data!?.items}
-        updateData={sendRequest}
-      />
-    </div>
+    <PostListWithInput fetchMethod={fetchMethod} showInput={isAuthenticated} />
   );
 };
 
--- packages/social-trader/src/pages/tags/tags.container.tsx
+++ packages/social-trader/src/pages/tags/tags.container.tsx
@@ -1,29 +1,13 @@
-import { getEmptyPostLoaderData } from "components/conversation/conversation.loader";
 import { searchInFeed } from "components/conversation/conversation.service";
-import { PostList } from "components/conversation/post-list/post-list";
-import { PostInputContainer } from "components/conversation/post/post-input/post-input.container";
-import useApiRequest from "hooks/api-request.hook";
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { idSelector } from "reducers/header-reducer";
+import { PostListWithInput } from "components/conversation/post-list/post-list-with-input";
+import React from "react";
 
 const _TagsContainer: React.FC<Props> = ({ hashTags }) => {
-  const id = useSelector(idSelector);
-  const { data, sendRequest } = useApiRequest({
-    request: () => searchInFeed({ hashTags })
-  });
-  useEffect(() => {
-    sendRequest();
-  }, [id]);
   return (
-    <div>
-      {!hashTags && <PostInputContainer onSuccess={sendRequest} />}
-      <PostList
-        loaderData={[getEmptyPostLoaderData()]}
-        data={data!?.items}
-        updateData={sendRequest}
-      />
-    </div>
+    <PostListWithInput
+      fetchMethod={() => searchInFeed({ hashTags })}
+      showInput={!hashTags}
+    />
   );
 };
 
