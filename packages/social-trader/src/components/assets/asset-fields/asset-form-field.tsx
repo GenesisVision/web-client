4590c4ff3d07e29d93196d960a5fa08ad2264a4e
pavelzubov <pasha224@yandex.ru>
2020-04-11 20:04:06 +0300

    Use RowItem, Center in AssetFormField, remove unused styles

--- packages/social-trader/src/components/assets/asset-fields/asset-form-field.tsx
+++ packages/social-trader/src/components/assets/asset-fields/asset-form-field.tsx
@@ -1,9 +1,11 @@
 import classNames from "classnames";
+import { Center } from "components/center/center";
 import { GVHookFormField } from "components/gv-hook-form-field";
 import GVProgramPeriod from "components/gv-program-period";
 import Hint from "components/hint/hint";
 import { MutedText } from "components/muted-text/muted-text";
 import { VERTICAL_POPOVER_POS } from "components/popover/popover";
+import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
 import { TextInputValues } from "components/text-input-component/text-input-component";
 import * as React from "react";
@@ -43,23 +45,21 @@ export const _AssetFormField: React.FC<React.HTMLAttributes<HTMLDivElement> &
         disabled={disabled}
       />
       {caption && (
-        <Row>
-          <MutedText small>
-            <div className="asset-form-field__caption">
-              <span>{caption}</span>
-              {trimmedLength > 0 && (
-                <div className="asset-form-field__caption-count">
-                  {trimmedLength}
-                  <GVProgramPeriod
-                    start={0}
-                    end={max}
-                    value={trimmedLength}
-                    variant="pie"
-                  />
-                </div>
-              )}
-            </div>
-          </MutedText>
+        <Row className="asset-form-field__caption">
+          <MutedText small>{caption}</MutedText>
+          {trimmedLength > 0 && (
+            <Center>
+              <RowItem small>
+                <MutedText>{trimmedLength}</MutedText>
+              </RowItem>
+              <GVProgramPeriod
+                start={0}
+                end={max}
+                value={trimmedLength}
+                variant="pie"
+              />
+            </Center>
+          )}
         </Row>
       )}
       {hintContent && (
