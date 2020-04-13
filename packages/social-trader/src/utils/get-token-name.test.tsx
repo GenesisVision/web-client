751c991e95958271eec93bdd78e7e450c313b9c6
pavelzubov <pasha224@yandex.ru>
2020-04-13 10:20:49 +0300

    Change getTokenName to GV_TOKEN_KEY, use cookieServiceCreator in auth-service.ts

--- packages/social-trader/src/utils/get-token-name.test.tsx
+++
@@ -1,7 +0,0 @@
-import { getTokenName } from "./get-token-name";
-
-describe("test get-token", () => {
-  it("must be gvTestToken", () => {
-    expect(getTokenName()).toEqual(`gvToken`);
-  });
-});
