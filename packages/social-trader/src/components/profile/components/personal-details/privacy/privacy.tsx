65abea0a03e84bb7ec5de89b9cbfc4ab61059f27
pavelzubov <pasha224@yandex.ru>
2020-04-14 12:18:30 +0300

    Create Privacy component

---
+++ packages/social-trader/src/components/profile/components/personal-details/privacy/privacy.tsx
@@ -0,0 +1,34 @@
+import PrivacyForm, {
+  IPrivacyFormValues
+} from "components/profile/components/personal-details/privacy/privacy.form";
+import { updatePrivacy } from "components/profile/services/profile.service";
+import useApiRequest from "hooks/api-request.hook";
+import React, { useCallback } from "react";
+
+const _Privacy: React.FC<Props> = ({ data, onUpdate, isPending }) => {
+  const { sendRequest, errorMessage } = useApiRequest({
+    middleware: [onUpdate],
+    request: updatePrivacy,
+    successMessage: "profile-page.success-edit"
+  });
+  const handleSubmit = useCallback(
+    (values: IPrivacyFormValues) => sendRequest(values),
+    []
+  );
+  return (
+    <PrivacyForm
+      onSubmit={handleSubmit}
+      isPending={isPending}
+      data={data}
+      errorMessage={errorMessage}
+    />
+  );
+};
+
+interface Props {
+  isPending: boolean;
+  onUpdate: () => void;
+  data: any;
+}
+
+export const Privacy = React.memo(_Privacy);
