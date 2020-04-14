d3874e5e8751ae92f62a310ba42e56bd1cff149d
pavelzubov <pasha224@yandex.ru>
2020-04-14 11:38:31 +0300

    Split profile page items to profile details and personal details

--- packages/social-trader/src/components/profile/components/profile/profile.tsx
+++ packages/social-trader/src/components/profile/components/profile/profile.tsx
@@ -1,10 +1,6 @@
-import GVButton from "components/gv-button";
-import Link from "components/link/link";
 import { useToLink } from "components/link/link.helper";
-import { KYC_ROUTE } from "components/profile/profile.constants";
 import ProfileImageContainer from "components/profile/settings/profile-image/profile-image-container";
 import SettingsBlock from "components/settings-block/settings-block";
-import StatisticItem from "components/statistic-item/statistic-item";
 import withLoader from "decorators/with-loader";
 import { ProfileFullViewModel } from "gv-api-web";
 import PublicInfo from "modules/public-info/public-info";
@@ -30,26 +26,6 @@ const _Profile: React.FC<IProfileOwnProps> = ({
       </SettingsBlock>
       <SettingsBlock label={t("profile-page.settings.profile-image")}>
         <ProfileImageContainer />
-      </SettingsBlock>
-      <SettingsBlock label={t("profile-page.contacts")} checked={true}>
-        <StatisticItem label={t("profile-page.email")}>
-          {info.email}
-        </StatisticItem>
-      </SettingsBlock>
-      <SettingsBlock
-        label={t("profile-page.personal-info")}
-        verificationStatus={info.verificationStatus}
-      >
-        {info.verificationStatus === "NotVerified" && (
-          <Link
-            className="level-calculator-popup__btn-verify"
-            to={linkCreator(KYC_ROUTE)}
-          >
-            <GVButton color="primary" variant="outlined">
-              {t("buttons.verify")}
-            </GVButton>
-          </Link>
-        )}
       </SettingsBlock>
     </>
   );
