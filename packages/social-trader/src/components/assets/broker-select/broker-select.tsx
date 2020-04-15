badf3afbd888db94246794b04cf94006efe8fa04
pavelzubov <pasha224@yandex.ru>
2020-04-15 16:12:20 +0300

    Use Row, RowItem, remove unused styles in BrokerSelect

--- packages/social-trader/src/components/assets/broker-select/broker-select.tsx
+++ packages/social-trader/src/components/assets/broker-select/broker-select.tsx
@@ -26,68 +26,77 @@ const _BrokerSelectBroker: React.FC<Props> = ({
 }) => {
   const [t] = useTranslation();
   return (
-    <div className="broker-select">
-      <Row wrap className="broker-select__list">
-        {data.map((broker, i) => (
-          <RowItem bottomOffset key={i}>
-            <BrokerCard
-              logo={broker.logoUrl}
-              brokerName={broker.name}
-              isSelected={broker === selectedBroker}
-              onSelect={selectBrokerHandle}
-              cardState={getBrokerState(
-                broker.isKycRequired,
-                isForexAllowed,
-                isKycConfirmed
-              )}
-              tags={broker.tags}
-            />
-          </RowItem>
-        ))}
-        <div className="broker-select__navigation">
+    <Row className="broker-select">
+      <RowItem>
+        <Row wrap wide className="broker-select__list">
+          {data.map((broker, i) => (
+            <RowItem bottomOffset key={i}>
+              <BrokerCard
+                logo={broker.logoUrl}
+                brokerName={broker.name}
+                isSelected={broker === selectedBroker}
+                onSelect={selectBrokerHandle}
+                cardState={getBrokerState(
+                  broker.isKycRequired,
+                  isForexAllowed,
+                  isKycConfirmed
+                )}
+                tags={broker.tags}
+              />
+            </RowItem>
+          ))}
+        </Row>
+        <Row className="broker-select__navigation">
           <NavigateToSettings
             isForex={selectedBroker.isKycRequired}
             isKycConfirmed={isKycConfirmed}
             navigateToSettings={navigateToSettings}
           />
-        </div>
-      </Row>
+        </Row>
+      </RowItem>
       <Surface className="surface--horizontal-paddings broker-select__description">
-        <h3>{selectedBroker.name}</h3>
-        <StatisticItem half label={t("create-program-page.broker-info.about")}>
-          {selectedBroker.description}
-        </StatisticItem>
-        <StatisticItem
-          label={t("create-program-page.broker-info.account-type")}
-        >
-          {getAccountTypes(selectedBroker.accountTypes)}
-        </StatisticItem>
-        <StatisticItem
-          label={t("create-program-page.broker-info.trading-platform")}
-        >
-          {selectedBroker.accountTypes[0].type}
-        </StatisticItem>
-        <StatisticItem label={t("create-program-page.broker-info.terms")}>
-          <a
-            title={t("create-program-page.broker-info.read-terms")}
-            href={selectedBroker.terms}
-            target="_blank"
-            rel="noopener noreferrer"
+        <Row>
+          <h3>{selectedBroker.name}</h3>
+        </Row>
+        <Row onlyOffset>
+          <StatisticItem
+            half
+            label={t("create-program-page.broker-info.about")}
           >
-            {t("create-program-page.broker-info.read-terms")}
-          </a>
-        </StatisticItem>
-        <StatisticItem label={t("create-program-page.broker-info.leverage")}>
-          {getLeverageDescription(
-            selectedBroker.leverageMin,
-            selectedBroker.leverageMax
-          )}
-        </StatisticItem>
-        <StatisticItem label={t("create-program-page.broker-info.assets")}>
-          {selectedBroker.assets}
-        </StatisticItem>
+            {selectedBroker.description}
+          </StatisticItem>
+          <StatisticItem
+            label={t("create-program-page.broker-info.account-type")}
+          >
+            {getAccountTypes(selectedBroker.accountTypes)}
+          </StatisticItem>
+          <StatisticItem
+            label={t("create-program-page.broker-info.trading-platform")}
+          >
+            {selectedBroker.accountTypes[0].type}
+          </StatisticItem>
+          <StatisticItem label={t("create-program-page.broker-info.terms")}>
+            <a
+              title={t("create-program-page.broker-info.read-terms")}
+              href={selectedBroker.terms}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {t("create-program-page.broker-info.read-terms")}
+            </a>
+          </StatisticItem>
+          <StatisticItem label={t("create-program-page.broker-info.leverage")}>
+            {getLeverageDescription(
+              selectedBroker.leverageMin,
+              selectedBroker.leverageMax
+            )}
+          </StatisticItem>
+          <StatisticItem label={t("create-program-page.broker-info.assets")}>
+            {selectedBroker.assets}
+          </StatisticItem>
+        </Row>
       </Surface>
-    </div>
+    </Row>
   );
 };
 
