47c9e2dec370f2983b80ad61ae99efbf8b707d65
pavelzubov <pasha224@yandex.ru>
2020-04-15 10:18:46 +0300

    Use MutedText, Row, remove unused styles in email confirm page

--- packages/social-trader/src/pages/auth/email-confirm/email-confirm-failure.tsx
+++ packages/social-trader/src/pages/auth/email-confirm/email-confirm-failure.tsx
@@ -1,16 +1,21 @@
+import FormError from "components/form/form-error/form-error";
+import { MutedText } from "components/muted-text/muted-text";
+import { Row } from "components/row/row";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-
-import "./email-confirm-failure.scss";
 
 const _EmailConfirmFailure: React.FC<Props> = ({ errorMessage }) => {
   const [t] = useTranslation();
   return (
-    <div className="email-confirm-failure">
-      <div className="email-confirm-failure__main-text">
-        {t("auth.email-confirm.error-during-confirmation")}
-      </div>
-      <div className="email-confirm-failure__error-text">{errorMessage}</div>
+    <div>
+      <Row>
+        <MutedText noWrap={false}>
+          {t("auth.email-confirm.error-during-confirmation")}
+        </MutedText>
+      </Row>
+      <Row>
+        <FormError error={errorMessage} />
+      </Row>
     </div>
   );
 };
