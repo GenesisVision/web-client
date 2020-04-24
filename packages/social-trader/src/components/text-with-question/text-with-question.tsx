42cc4ae5c33fd9f671b3752079ce5da982d1d1b5
pavelzubov <pasha224@yandex.ru>
2020-04-24 16:59:44 +0300

    Create TextWithQuestion component

---
+++ packages/social-trader/src/components/text-with-question/text-with-question.tsx
@@ -0,0 +1,67 @@
+import classNames from "classnames";
+import { Center } from "components/center/center";
+import HelpButton from "components/help-button/help-button";
+import {
+  HORIZONTAL_POPOVER_POS,
+  VERTICAL_POPOVER_POS
+} from "components/popover/popover";
+import { RowItem } from "components/row-item/row-item";
+import Tooltip from "components/tooltip/tooltip";
+import * as React from "react";
+
+import "./text-with-question.scss";
+
+const _SwitchWithQuestion: React.FC<Props> = ({
+  tooltipContent,
+  onClickHelp,
+  label
+}) => {
+  return (
+    <Center>
+      <RowItem small>
+        <HelpContainer tooltipContent={tooltipContent}>
+          <HelpButton
+            className={classNames("text-with-question__question", {
+              "text-with-question__question-button": !!tooltipContent
+            })}
+            onClick={onClickHelp}
+          />
+        </HelpContainer>
+      </RowItem>
+      <RowItem small className="text-with-question__label">
+        {label}
+      </RowItem>
+    </Center>
+  );
+};
+
+const HelpContainer: React.FC<{
+  tooltipContent?: string | JSX.Element;
+} & React.HTMLAttributes<HTMLDivElement>> = ({ tooltipContent, children }) => {
+  switch (!!tooltipContent) {
+    case true:
+      return (
+        <Tooltip
+          horizontal={HORIZONTAL_POPOVER_POS.LEFT}
+          vertical={VERTICAL_POPOVER_POS.BOTTOM}
+          render={() => tooltipContent}
+        >
+          <div className="text-with-question__question-container">
+            {children}
+          </div>
+        </Tooltip>
+      );
+    case false:
+    default:
+      return <>{children}</>;
+  }
+};
+
+interface Props {
+  label: string;
+  onClickHelp?: VoidFunction;
+  tooltipContent?: string | JSX.Element;
+}
+
+const TextWithQuestion = React.memo(_SwitchWithQuestion);
+export default TextWithQuestion;
