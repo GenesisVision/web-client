7218bb77a31d0720d9a1660516a3b05f0ee1dd83
pavelzubov <pasha224@yandex.ru>
2020-04-24 19:48:29 +0300

    Add Trade, OpenOrdersBlock, use TradingInfoContextProvider in TradingContainer

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/trading.container.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/trading.container.tsx
@@ -1,5 +1,10 @@
+import { RowItem } from "components/row-item/row-item";
+import { Row } from "components/row/row";
 import { TradeAuthDataType } from "pages/trades/binance-trade-page/binance-trade.helpers";
 import { MarketWatchBlock } from "pages/trades/binance-trade-page/trading/market-watch/market-watch.block";
+import { OpenOrdersBlock } from "pages/trades/binance-trade-page/trading/open-orders/open-orders.block";
+import { Trade } from "pages/trades/binance-trade-page/trading/trade/trade";
+import { TradingInfoContextProvider } from "pages/trades/binance-trade-page/trading/trading-info.context";
 import React from "react";
 
 interface Props {
@@ -8,9 +13,19 @@ interface Props {
 
 const _TradingContainer: React.FC<Props> = ({ authData }) => {
   return (
-    <div>
-      <MarketWatchBlock />
-    </div>
+    <TradingInfoContextProvider>
+      <Row>
+        <RowItem>
+          <MarketWatchBlock />
+        </RowItem>
+        <RowItem>
+          <Trade />
+        </RowItem>
+      </Row>
+      <Row onlyOffset>
+        <OpenOrdersBlock />
+      </Row>
+    </TradingInfoContextProvider>
   );
 };
 
