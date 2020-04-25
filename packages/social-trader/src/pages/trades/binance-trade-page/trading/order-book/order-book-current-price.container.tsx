9b40fba2e8955a231a4e27432b5bf95873ee42c2
pavelzubov <pasha224@yandex.ru>
2020-04-25 15:48:29 +0300

    Add setting price on tradeSocketData change in OrderBookCurrentPriceContainer

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/order-book/order-book-current-price.container.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/order-book/order-book-current-price.container.tsx
@@ -2,6 +2,7 @@ import { useGetRate } from "hooks/get-rate.hook";
 import { OrderBookCurrentPrice } from "pages/trades/binance-trade-page/trading/order-book/order-book-current-price";
 import { tradeSocket } from "pages/trades/binance-trade-page/trading/services/binance-ws.service";
 import { TradingInfoContext } from "pages/trades/binance-trade-page/trading/trading-info.context";
+import { TradingPriceContext } from "pages/trades/binance-trade-page/trading/trading-price.context";
 import { getSymbol } from "pages/trades/binance-trade-page/trading/trading.helpers";
 import { Trade } from "pages/trades/binance-trade-page/trading/trading.types";
 import React, { useContext, useEffect, useState } from "react";
@@ -10,6 +11,7 @@ import { headerAccountCurrencySelector } from "reducers/header-reducer";
 import { useSockets } from "services/websocket.service";
 
 export const OrderBookCurrentPriceContainer: React.FC = () => {
+  const { setPrice, price } = useContext(TradingPriceContext);
   const {
     symbol: { baseAsset, quoteAsset }
   } = useContext(TradingInfoContext);
@@ -31,6 +33,11 @@ export const OrderBookCurrentPriceContainer: React.FC = () => {
       setTradeSocketData(data);
     });
   }, [baseAsset, quoteAsset]);
+  useEffect(() => {
+    if (+price === 0 && tradeSocketData) {
+      setPrice(tradeSocketData.price);
+    }
+  }, [tradeSocketData]);
   return (
     <OrderBookCurrentPrice
       tradeId={tradeSocketData?.tradeId}
