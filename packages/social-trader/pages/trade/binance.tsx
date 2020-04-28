a3268eaa6c18e8763ed773f125af4b967084326f
pavelzubov <pasha224@yandex.ru>
2020-04-28 20:16:45 +0300

    Use withTradeLayout in /trade/binance route

--- packages/social-trader/pages/trade/binance.tsx
+++ packages/social-trader/pages/trade/binance.tsx
@@ -1,4 +1,4 @@
-import withDefaultLayout from "decorators/with-default-layout";
+import withTradeLayout from "decorators/with-trade-layout";
 import { NextPage } from "next";
 import { BinanceTrade } from "pages/trades/binance-trade-page/binance-trade.page";
 import React from "react";
@@ -11,4 +11,4 @@ Page.getInitialProps = async () => ({
   namespacesRequired: ["mt5-page"]
 });
 
-export default withDefaultLayout(Page);
+export default withTradeLayout(Page);
