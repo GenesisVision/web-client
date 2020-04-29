474898d5017c36def16d5b9ba17857c41051aa06
pavelzubov <pasha224@yandex.ru>
2020-04-29 10:55:10 +0300

    Add size prop to search input

--- packages/social-trader/src/components/global-search/components/global-search-result/global-search-input.tsx
+++ packages/social-trader/src/components/global-search/components/global-search-result/global-search-input.tsx
@@ -5,25 +5,28 @@ import SearchIcon from "components/icon/search-icon/search-icon";
 import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
 import React, { useCallback } from "react";
+import { SizesType } from "utils/types";
 
 export const SearchInputField: React.FC<{
+  size?: SizesType;
   canClose?: boolean;
   onBlur?: VoidFunction;
   value: string;
   onCancel?: VoidFunction;
   onChange: (event: React.ChangeEvent<any>) => void;
-}> = ({ canClose = true, value, onChange, onCancel, onBlur }) => {
+}> = ({ size, canClose = true, value, onChange, onCancel, onBlur }) => {
   return (
     <Row wide>
       <RowItem wide>
         <GVTextField
+          size={size}
           onBlur={onBlur}
           wide
           noMargin
           name="queryValue"
           placeholder="Search"
           autoComplete="off"
-          adornment={<SearchIcon primary />}
+          adornment={<SearchIcon size={size} primary />}
           adornmentPosition="start"
           value={value}
           onChange={onChange}
@@ -46,7 +49,7 @@ export const SearchInputField: React.FC<{
 };
 
 const GlobalSearchInput: React.FC<Props> = React.memo(
-  ({ canClose, query, onChange }) => {
+  ({ size, canClose, query, onChange }) => {
     const handleOnChange = useCallback(
       (event: React.ChangeEvent<any>) => onChange(event.target.value),
       [onChange]
@@ -57,6 +60,7 @@ const GlobalSearchInput: React.FC<Props> = React.memo(
         {/*
       //@ts-ignore TODO сделать фикс GVTextField*/}
         <SearchInputField
+          size={size}
           canClose={canClose}
           value={query}
           onChange={handleOnChange}
@@ -67,8 +71,11 @@ const GlobalSearchInput: React.FC<Props> = React.memo(
 );
 
 interface Props {
+  size?: SizesType;
   canClose?: boolean;
+
   onChange(value: string): void;
+
   query: string;
 }
 
