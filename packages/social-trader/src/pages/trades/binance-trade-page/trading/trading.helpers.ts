f48faab655c9643376207c09afb64325a1e06c7d
pavelzubov <pasha224@yandex.ru>
2020-04-30 13:50:01 +0300

    Add formatValueWithTick to trading.helpers.ts

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/trading.helpers.ts
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/trading.helpers.ts
@@ -1,3 +1,4 @@
+import { formatValue } from "utils/formatter";
 import { ColoredTextColor } from "components/colored-text/colored-text";
 import { TradeCurrency } from "pages/trades/binance-trade-page/trading/trading.types";
 
@@ -9,3 +10,8 @@ export const getTextColor = (value: number): ColoredTextColor | undefined => {
 
 export const getSymbol = (base: TradeCurrency, quote: TradeCurrency): string =>
   base + quote;
+
+export const formatValueWithTick = (value: any, tick: string): string => {
+  const decimalScale = getDividerParts(formatValue(tick)).fracLength;
+  return formatValue(value, decimalScale);
+};
