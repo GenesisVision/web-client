234e07e4e815bc7a5f8c1649c402cb671bf9b487
pavelzubov <pasha224@yandex.ru>
2020-05-01 09:46:32 +0300

    Change large desktop breakpoint to 1440

--- packages/social-trader/src/utils/breakpoints.ts
+++ packages/social-trader/src/utils/breakpoints.ts
@@ -2,7 +2,7 @@ const PHONE_LANDSCAPE = 576;
 const TABLET = 768;
 const TABLET_LANDSCAPE = 992;
 const DESKTOP = 1200;
-const LARGE_DESKTOP = 1920;
+const LARGE_DESKTOP = 1440;
 
 export const isPhone = (width: number): boolean => width < PHONE_LANDSCAPE;
 export const isPhoneLandscape = (width: number): boolean => width < TABLET;
