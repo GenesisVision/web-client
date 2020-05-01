8c401b41a6458232333be4603debc37d7578a921
pavelzubov <pasha224@yandex.ru>
2020-05-01 21:12:09 +0300

    Create and use useHeaderInfo

--- packages/social-trader/src/components/header/header.service.ts
+++ packages/social-trader/src/components/header/header.service.ts
@@ -1,6 +1,23 @@
+import { useAccountCurrency } from "hooks/account-currency.hook";
+import { updateCurrency } from "modules/currency-select/services/currency-select.service";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { isAuthenticatedSelector } from "reducers/auth-reducer";
+import { headerSelector } from "reducers/header-reducer";
 import { Dispatch } from "redux";
 
 import { fetchProfileHeaderInfoAction } from "./actions/header-actions";
+
+export const useHeaderInfo = () => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(isAuthenticatedSelector);
+  const headerAccountCurrency = useAccountCurrency();
+  useEffect(() => {
+    if (isAuthenticated) dispatch(fetchProfileHeaderInfo);
+    if (headerAccountCurrency) updateCurrency(headerAccountCurrency);
+  }, [isAuthenticated, headerAccountCurrency]);
+  return useSelector(headerSelector);
+};
 
 export const fetchProfileHeaderInfo = (dispatch: Dispatch) => {
   dispatch(fetchProfileHeaderInfoAction());
