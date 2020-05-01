21ffa391d51d27b3d27c46decd299789cc945628
pavelzubov <pasha224@yandex.ru>
2020-05-01 21:57:26 +0300

    Remove header in TradeLayout

--- packages/social-trader/src/components/layouts/trade-layout/trade-layout.tsx
+++ packages/social-trader/src/components/layouts/trade-layout/trade-layout.tsx
@@ -1,5 +1,3 @@
-import HeaderContainer from "components/header/header.container";
-import { Row } from "components/row/row";
 import AlertMessageList from "modules/alert-message/components/alert-message-list/alert-message-list";
 import React, { ComponentType } from "react";
 
@@ -8,10 +6,7 @@ import styles from "./trade-layout.module.scss";
 export const TradeLayout: ComponentType = ({ children }) => {
   return (
     <div className={styles["trade-layout__wrapper"]}>
-      <Row className={styles["trade-layout__header"]}>
-        <div id="modal-root" />
-        <HeaderContainer />
-      </Row>
+      <div id="modal-root" />
       <div className={styles["trade-layout__main"]}>{children}</div>
       <AlertMessageList />
     </div>
