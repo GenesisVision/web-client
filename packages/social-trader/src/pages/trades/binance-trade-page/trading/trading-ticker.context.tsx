b5e341c10eb23676152bca9761eb07c1059215d3
pavelzubov <pasha224@yandex.ru>
2020-05-01 10:27:04 +0300

    Create TradingTickerContextProvider component

---
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/trading-ticker.context.tsx
@@ -0,0 +1,78 @@
+import {
+  normalizeMarketList,
+  normalizeSymbolsList
+} from "pages/trades/binance-trade-page/trading/market-watch/market-watch.helpers";
+import { getTickersStream } from "pages/trades/binance-trade-page/trading/services/binance-stream.service";
+import { TradingInfoContext } from "pages/trades/binance-trade-page/trading/trading-info.context";
+import {
+  MergedTickerSymbolType,
+  Symbol,
+  Ticker
+} from "pages/trades/binance-trade-page/trading/trading.types";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from "react";
+import { map } from "rxjs/operators";
+import { useSockets } from "services/websocket.service";
+
+type TradingTickerContextState = MergedTickerSymbolType[];
+
+export const TradingTickerInitialState: TradingTickerContextState = [];
+
+export const TradingTickerContext = createContext<TradingTickerContextState>(
+  TradingTickerInitialState
+);
+
+export const TradingTickerContextProvider: React.FC = ({ children }) => {
+  const [normalizedSymbols, setNormalizedSymbols] = useState<{
+    [key: string]: Symbol;
+  }>({});
+  const [list, setList] = useState<{
+    [key: string]: MergedTickerSymbolType;
+  }>({});
+  const [socketData, setSocketData] = useState<{
+    [key: string]: Ticker;
+  }>({});
+  const { connectSocket } = useSockets();
+
+  const { exchangeInfo } = useContext(TradingInfoContext);
+
+  useEffect(() => {
+    if (exchangeInfo)
+      setNormalizedSymbols(normalizeSymbolsList(exchangeInfo.symbols));
+  }, [exchangeInfo]);
+
+  useEffect(() => {
+    const marketTickets = getTickersStream(connectSocket).pipe(
+      map(items => normalizeMarketList(items))
+    );
+    marketTickets.subscribe(data => {
+      setSocketData(data);
+    });
+  }, []);
+  useEffect(() => {
+    const updatedList = { ...list };
+    Object.keys(socketData).forEach(name => {
+      updatedList[name] = { ...updatedList[name], ...socketData[name] };
+    });
+    setList(updatedList);
+  }, [socketData]);
+  useEffect(() => {
+    const updatedList = { ...list };
+    Object.keys(normalizedSymbols).forEach(name => {
+      updatedList[name] = { ...updatedList[name], ...normalizedSymbols[name] };
+    });
+    setList(updatedList);
+  }, [normalizedSymbols]);
+  const items = useMemo(() => Object.values(list), [list]);
+
+  return (
+    <TradingTickerContext.Provider value={items}>
+      {children}
+    </TradingTickerContext.Provider>
+  );
+};
