4d9d04dccca7bd85b7e2220a6bd4230aa4684863
pavelzubov <pasha224@yandex.ru>
2020-05-02 09:55:20 +0300

    Remove unused imports

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/market-watch/market-watch.block.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/market-watch/market-watch.block.tsx
@@ -1,18 +1,11 @@
 import { DefaultBlock } from "components/default.block/default.block";
-import { MutedText } from "components/muted-text/muted-text";
-import { RowItem } from "components/row-item/row-item";
-import { Row } from "components/row/row";
 import { SIZES } from "constants/constants";
 import { MarketWatchContainer } from "pages/trades/binance-trade-page/trading/market-watch/market-watch.container";
-import { TradingInfoContext } from "pages/trades/binance-trade-page/trading/trading-info.context";
-import { getSymbol } from "pages/trades/binance-trade-page/trading/trading.helpers";
-import React, { useContext } from "react";
+import React from "react";
 
 import styles from "./market-watch.module.scss";
 
-interface Props {}
-
-export const MarketWatchBlock: React.FC<Props> = () => {
+export const MarketWatchBlock: React.FC = () => {
   return (
     <DefaultBlock
       size={SIZES.SMALL}
