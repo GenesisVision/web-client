81789747f312df095b92a845a21efcf763d31202
pavelzubov <pasha224@yandex.ru>
2020-05-03 12:36:27 +0300

    Add height to TableCell

--- packages/social-trader/src/components/table/components/table-cell.tsx
+++ packages/social-trader/src/components/table/components/table-cell.tsx
@@ -1,16 +1,28 @@
 import classNames from "classnames";
 import * as React from "react";
 import { ReactNode } from "react";
+import { SizesType } from "utils/types";
 
 import styles from "./table.module.scss";
 
-const TableCell: React.FC<Props> = ({ className = "", children }) => {
+const TableCell: React.FC<Props> = ({
+  height = "medium",
+  className = "",
+  children
+}) => {
   return (
-    <td className={classNames(styles["table__cell"], className)}>{children}</td>
+    <td
+      className={classNames(styles["table__cell"], className, {
+        [styles["table__cell--low"]]: height === "small"
+      })}
+    >
+      {children}
+    </td>
   );
 };
 
 interface Props {
+  height?: SizesType;
   className?: string;
   children?: ReactNode; // TODO fix React.memo type
 }
