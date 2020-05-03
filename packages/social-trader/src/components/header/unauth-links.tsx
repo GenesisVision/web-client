35551d1b83291da35dffbaeb1df5f9a35bb2287c
pavelzubov <pasha224@yandex.ru>
2020-05-03 10:24:21 +0300

    Correct UnauthLinks markup

--- packages/social-trader/src/components/header/unauth-links.tsx
+++ packages/social-trader/src/components/header/unauth-links.tsx
@@ -1,6 +1,8 @@
+import { Center } from "components/center/center";
 import GVButton from "components/gv-button";
 import Link from "components/link/link";
 import { useToLink } from "components/link/link.helper";
+import { RowItem } from "components/row-item/row-item";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { LOGIN_ROUTE, SIGNUP_ROUTE } from "routes/app.routes";
@@ -9,23 +11,28 @@ const UnauthLinks: React.FC<Props> = ({ backPath }) => {
   const { linkCreator } = useToLink();
   const [t] = useTranslation();
   return (
-    <div className="header__buttons">
-      <Link
-        to={{
-          pathname: LOGIN_ROUTE,
-          state: backPath
-        }}
-      >
-        <GVButton variant="outlined" color="secondary">
-          {t("auth.login.title")}
-        </GVButton>
-      </Link>
-      <Link to={linkCreator(SIGNUP_ROUTE)}>
-        <GVButton variant="contained" color="primary">
-          {t("auth.signup.title")}
-        </GVButton>
-      </Link>
-    </div>
+    <Center className="header__buttons">
+      <RowItem>
+        <Link
+          to={{
+            pathname: LOGIN_ROUTE,
+            state: backPath
+          }}
+        >
+          <GVButton variant="outlined" color="secondary">
+            {t("auth.login.title")}
+          </GVButton>
+        </Link>
+      </RowItem>
+      <RowItem>
+        {" "}
+        <Link to={linkCreator(SIGNUP_ROUTE)}>
+          <GVButton variant="contained" color="primary">
+            {t("auth.signup.title")}
+          </GVButton>
+        </Link>
+      </RowItem>
+    </Center>
   );
 };
 
