1cad03c8ebc1cd9be1f8f42a8421e0d5a6a6355e
Anchello <bob.m@mail.ru>
2020-05-04 21:45:20 +0300

    fixed css-modules for faq page

--- packages/social-trader/src/pages/landing-page/components/faq-section/faq-section.tsx
+++ packages/social-trader/src/pages/landing-page/components/faq-section/faq-section.tsx
@@ -14,29 +14,29 @@ import styles from "./faq-section.module.scss";
 const FaqSection: React.FC = () => {
   return (
     <section className={styles["faq-section"]}>
-      <div className="internal__container">
+      <div className={styles["faq-section__container"]}>
         <h1 className={styles["faq-section__title"]}>FAQ</h1>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>General Questions</h2>
           <AccordionContainer accordions={faqGeneral} />
         </article>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>Genesis Vision Program</h2>
           <AccordionContainer accordions={faqPrograms} />
         </article>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>Genesis Vision Funds</h2>
           <AccordionContainer accordions={faqFunds} />
         </article>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>Genesis Vision Follow</h2>
           <AccordionContainer accordions={faqFollow} />
         </article>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>Genesis Vision Token</h2>
           <AccordionContainer accordions={faqGVT} />
         </article>
-        <article className="internal__article">
+        <article className={styles["faq-section__article"]}>
           <h2>ICO</h2>
           <AccordionContainer accordions={faqICO} />
         </article>
