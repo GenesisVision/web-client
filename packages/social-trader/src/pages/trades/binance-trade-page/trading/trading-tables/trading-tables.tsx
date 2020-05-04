f0dbb1f03a42be1f47f54ca963b5ab1de807f6ad
pavelzubov <pasha224@yandex.ru>
2020-05-04 15:43:40 +0300

    Add FundsContainer to TradingTables

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/trading-tables/trading-tables.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/trading-tables/trading-tables.tsx
@@ -3,6 +3,7 @@ import GVTabs from "components/gv-tabs";
 import GVTab from "components/gv-tabs/gv-tab";
 import { SIZES } from "constants/constants";
 import useTab from "hooks/tab.hook";
+import { FundsContainer } from "pages/trades/binance-trade-page/trading/trading-tables/funds/funds.container";
 import { OpenOrdersContainer } from "pages/trades/binance-trade-page/trading/trading-tables/open-orders/open-orders.container";
 import { OrderHistoryContainer } from "pages/trades/binance-trade-page/trading/trading-tables/order-history/order-history.container";
 import React from "react";
@@ -41,6 +42,7 @@ const _TradingTables: React.FC<Props> = () => {
       <div className={styles["trading-tables__tables-container"]}>
         {tab === TABS.OPEN_ORDERS && <OpenOrdersContainer />}
         {tab === TABS.ORDER_HISTORY && <OrderHistoryContainer />}
+        {tab === TABS.FUNDS && <FundsContainer />}
       </div>
     </DefaultBlock>
   );
