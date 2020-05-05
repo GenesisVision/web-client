b377d834c24d5b9876c30f120e0783194c106d24
pavelzubov <pasha224@yandex.ru>
2020-05-05 18:35:12 +0300

    Create market trade form components

---
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/market-trade-form.tsx
@@ -0,0 +1,154 @@
+import FormError from "components/form/form-error/form-error";
+import HookFormAmountField from "components/input-amount-field/hook-form-amount-field";
+import { Slider } from "components/range/range";
+import { Row } from "components/row/row";
+import StatisticItemInner from "components/statistic-item/statistic-item-inner";
+import { PlaceOrderSubmitButton } from "pages/trades/binance-trade-page/trading/place-order/place-order-submit-button";
+import {
+  Account,
+  ExchangeInfo,
+  OrderSide,
+  TradeCurrency
+} from "pages/trades/binance-trade-page/trading/trading.types";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
+
+import {
+  ITradeFormValues,
+  limitValidationSchema,
+  RANGE_MARKS,
+  TRADE_FORM_FIELDS,
+  usePlaceOrderAutoFill,
+  usePlaceOrderFormReset,
+  usePlaceOrderInfo
+} from "./place-order.helpers";
+
+export interface IMarketTradeFormProps {
+  outerPrice: number;
+  baseAsset: TradeCurrency;
+  quoteAsset: TradeCurrency;
+  direction: OrderSide;
+  onSubmit: (values: ITradeFormValues) => any;
+}
+
+const _MarketTradeForm: React.FC<IMarketTradeFormProps & {
+  accountInfo: Account;
+  exchangeInfo: ExchangeInfo;
+}> = ({
+  accountInfo,
+  exchangeInfo,
+  outerPrice,
+  onSubmit,
+  quoteAsset,
+  baseAsset,
+  direction
+}) => {
+  const [t] = useTranslation();
+
+  const {
+    minPrice,
+    maxPrice,
+    tickSize,
+    minQty,
+    stepSize,
+    minNotional,
+    maxQuantityWithWallet,
+    maxTotalWithWallet
+  } = usePlaceOrderInfo({
+    balances: accountInfo.balances,
+    side: direction,
+    quoteAsset,
+    baseAsset,
+    exchangeInfo
+  });
+
+  const form = useForm<ITradeFormValues>({
+    validationSchema: limitValidationSchema({
+      t,
+      quoteAsset,
+      baseAsset,
+      stepSize: +stepSize,
+      tickSize: +tickSize,
+      maxTotal: maxTotalWithWallet,
+      maxPrice: +maxPrice,
+      minPrice: +minPrice,
+      maxQuantity: maxQuantityWithWallet,
+      minQuantity: +minQty,
+      minNotional: +minNotional
+    }),
+    mode: "onChange"
+  });
+  const { watch, setValue, reset, errors } = form;
+  const { quantity, total, price } = watch();
+
+  const { sliderValue, setSliderValue } = usePlaceOrderFormReset({
+    stepSize,
+    outerPrice,
+    watch,
+    reset,
+    baseAsset,
+    quoteAsset,
+    side: direction,
+    setValue,
+    balances: accountInfo.balances,
+    quantityName: TRADE_FORM_FIELDS.quantity,
+    totalName: TRADE_FORM_FIELDS.total
+  });
+
+  usePlaceOrderAutoFill({
+    totalName: TRADE_FORM_FIELDS.total,
+    quantityName: TRADE_FORM_FIELDS.quantity,
+    setValue,
+    tickSize,
+    price,
+    quantity,
+    stepSize,
+    total
+  });
+
+  return (
+    <HookForm form={form} onSubmit={onSubmit}>
+      <Row hide>
+        <HookFormAmountField
+          autoFocus={false}
+          label={t("Price")}
+          currency={quoteAsset}
+          name={TRADE_FORM_FIELDS.price}
+        />
+      </Row>
+      <StatisticItemInner label={t("Price")}>
+        {t("Market watch")}
+      </StatisticItemInner>
+      <Row>
+        <HookFormAmountField
+          autoFocus={false}
+          label={t("Amount")}
+          currency={baseAsset}
+          name={TRADE_FORM_FIELDS.quantity}
+        />
+      </Row>
+      {errors.total?.message && (
+        <Row>
+          <FormError error={errors.total.message} />
+        </Row>
+      )}
+      {direction === "SELL" && (
+        <Row wide onlyOffset>
+          <Slider
+            dots
+            min={0}
+            max={RANGE_MARKS.length - 1}
+            marks={RANGE_MARKS}
+            value={sliderValue}
+            onChange={setSliderValue}
+          />
+        </Row>
+      )}
+      <PlaceOrderSubmitButton side={direction} asset={baseAsset} />
+    </HookForm>
+  );
+};
+
+export const MarketTradeForm = React.memo(_MarketTradeForm);
