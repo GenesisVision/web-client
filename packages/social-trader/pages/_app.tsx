e5b1ede516ad8b3ca66b04c39ed6aa50de197bd6
pavelzubov <pasha224@yandex.ru>
2020-05-05 18:52:56 +0300

    Add import "rc-slider/assets/index.css" to _app.tsx

--- packages/social-trader/pages/_app.tsx
+++ packages/social-trader/pages/_app.tsx
@@ -1,12 +1,12 @@
-import "shared/styles/index.scss";
-
 import withHistoryProvider from "decorators/history-provider/with-history-provider";
 import withReduxStore from "decorators/with-redux-store";
 import { appWithTranslation } from "i18n";
 import App from "next/app";
+import "rc-slider/assets/index.css";
 import React from "react";
 import { Provider } from "react-redux";
 import { compose, Store } from "redux";
+import "shared/styles/index.scss";
 import { initializeStore } from "store";
 
 class CustomApp extends App<Props> {
