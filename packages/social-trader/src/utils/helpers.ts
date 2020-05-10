862cc02c24d51a73e418552dc4e729dc40d113cd
pavelzubov <pasha224@yandex.ru>
2020-05-10 20:11:17 +0300

    Correct getRandomColor in helpers.ts

--- packages/social-trader/src/utils/helpers.ts
+++ packages/social-trader/src/utils/helpers.ts
@@ -144,10 +144,11 @@ export const getRandomWords = (length: number = getRandomInteger(3, 8)) =>
 export const getRandomEmail = () =>
   `${getRandomWord()}@${getRandomWord()}.${getRandomWord()}`;
 
-export const getRandomColorNumber = () => getRandomInteger(0, 255).toString(16);
+export const getRandomColorNumber = () =>
+  getRandomInteger(150, 255).toString(16);
 
 const getRandomColor = () =>
-  `#${getRandomColorNumber}${getRandomColorNumber}${getRandomColorNumber}`;
+  `#${getRandomColorNumber()}${getRandomColorNumber()}${getRandomColorNumber()}`;
 
 export const tableLoaderCreator = (
   itemCreator: (...args: any) => any,
