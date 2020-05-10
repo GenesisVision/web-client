c7e4e72a92044c87e98285312a9f7850f341d1e3
pavelzubov <pasha224@yandex.ru>
2020-05-10 20:11:34 +0300

    Create IconFavoriteButton component

---
+++ packages/social-trader/src/modules/toggle-asset-favorite-button/icon-favorite-button.tsx
@@ -0,0 +1,30 @@
+import FavoriteIcon from "components/favorite-asset/favorite-icon/favorite-icon";
+import { ASSET } from "constants/constants";
+import { ToggleAssetFavoriteButton } from "modules/toggle-asset-favorite-button/toggle-asset-favorite-button";
+import { ToggleableAssetType } from "modules/toggle-asset-favorite-button/toggle-asset-favorite-button.types";
+import React, { useCallback, useState } from "react";
+
+interface Props {
+  asset: ToggleableAssetType;
+  assetType: ASSET;
+}
+
+const _IconFavoriteButton: React.FC<Props> = ({ asset, assetType }) => {
+  const [programState, setProgramState] = useState(asset);
+  const handleUpdateRow = useCallback(program => {
+    setProgramState(program);
+  }, []);
+  return (
+    <ToggleAssetFavoriteButton
+      asset={programState}
+      updateRow={handleUpdateRow}
+      assetType={assetType}
+      id={asset.id}
+      isFavorite={programState.personalDetails.isFavorite}
+    >
+      <FavoriteIcon selected={programState.personalDetails.isFavorite} />
+    </ToggleAssetFavoriteButton>
+  );
+};
+
+export const IconFavoriteButton = React.memo(_IconFavoriteButton);
