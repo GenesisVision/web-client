89b808b3b7183e3a43c6e192b8757d09984f64b4
pavelzubov <pasha224@yandex.ru>
2020-05-13 10:24:27 +0300

    Create binance-futures.types.ts

---
+++ packages/social-trader/src/pages/trades/binance-trade-page/services/futures/binance-futures.types.ts
@@ -0,0 +1,133 @@
+import {
+  OrderSide,
+  OrderType,
+  TimeInForce,
+  Trade,
+  TradeCurrency
+} from "pages/trades/binance-trade-page/trading/trading.types";
+
+export type FuturesAccountEventType =
+  | "ACCOUNT_UPDATE"
+  | "MARGIN_CALL"
+  | "ORDER_TRADE_UPDATE";
+
+export type PositionSideType = "BOTH" | "LONG" | "SHORT";
+
+export type MarginType = "CROSSED";
+
+export interface FuturesAsset {
+  asset: TradeCurrency;
+  initialMargin: string;
+  maintMargin: string;
+  marginBalance: string;
+  maxWithdrawAmount: string;
+  openOrderInitialMargin: string;
+  positionInitialMargin: string;
+  unrealizedProfit: string;
+  walletBalance: string;
+}
+
+export interface FuturesPosition {
+  isolated: boolean;
+  leverage: string;
+  initialMargin: string;
+  maintMargin: string;
+  openOrderInitialMargin: string;
+  positionInitialMargin: string;
+  symbol: TradeCurrency;
+  unrealizedProfit: string;
+  positionSide: PositionSideType; // BOTH means that it is the position of One-way Mode
+}
+
+export interface FuturesAccount {
+  assets: FuturesAsset[];
+  canDeposit: boolean;
+  canTrade: boolean;
+  canWithdraw: boolean;
+  feeTier: number;
+  maxWithdrawAmount: "8.41264592";
+  positions: FuturesPosition[];
+  totalInitialMargin: string;
+  totalMaintMargin: string;
+  totalMarginBalance: string;
+  totalOpenOrderInitialMargin: string;
+  totalPositionInitialMargin: string;
+  totalUnrealizedProfit: number;
+  totalWalletBalance: string;
+  updateTime: 0;
+}
+
+export interface FuturesAccountEventPosition {
+  symbol: TradeCurrency; // Symbol
+  positionSide: PositionSideType; // Position Side
+  positionAmount: string; // Position Amount
+  marginType: MarginType; // Margin Type
+  isolatedWallet: string; // Isolated Wallet (if isolated position)
+  markPrice: string; // Mark Price
+  unrealizedPnL: string; // Unrealized PnL
+  maintenanceMarginRequired: string; // Maintenance Margin Required
+}
+
+export interface FuturesAccountEventBalance {
+  asset: TradeCurrency; // Asset
+  walletBalance: string; // Wallet Balance
+  crossWalletBalance: string; // Cross Wallet Balance
+}
+
+export interface FuturesTradeOrder {
+  symbol: TradeCurrency; // Symbol
+  clientOrderId: string; // Client Order Id
+  // special client order id:
+  // starts with "autoclose-": liquidation order
+  // "adl_autoclose": ADL auto close order
+  side: OrderSide; // Side
+  orderType: OrderType; // Order Type
+  timeInForce: TimeInForce; // Time in Force
+  originalQuantity: string; // Original Quantity
+  originalPrice: string; // Original Price
+  averagePrice: string; // Average Price
+  stopPrice: string; // Stop Price
+  executionType: string; // Execution Type
+  orderStatus: string; // Order Status
+  orderId: number; // Order Id
+  orderLastFilledQuantity: string; // Order Last Filled Quantity
+  orderFilledAccumulatedQuantity: string; // Order Filled Accumulated Quantity
+  lastFilledPrice: string; // Last Filled Price
+  commissionAsset: Trade; // Commission Asset, will not push if no commission
+  commission: string; // Commission, will not push if no commission
+  orderTradeTime: number; // Order Trade Time
+  tradeId: number; // Trade Id
+  bidsNotional: string; // Bids Notional
+  askNotional: string; // Ask Notional
+  isMakerSide: boolean; // Is this trade the maker side?
+  isReduceOnly: boolean; // Is this reduce only
+  stopPriceWorkingType: string; // stop price working type
+  ifCloseAll: boolean; // If Close-All, only pushed with conditional order
+  activationPrice: string; // Activation Price, only puhed with TRAILING_STOP_MARKET order
+  callbackRate: string; // Callback Rate, only puhed with TRAILING_STOP_MARKET order
+}
+
+export interface FuturesMarginCallEvent {
+  eventType: FuturesAccountEventType; // Event Type
+  eventTime: number; // Event Time
+  crossWalletBalance: string; // Cross Wallet Balance. Only pushed with crossed position margin call
+  positions: FuturesAccountEventPosition[];
+}
+
+export interface FuturesAccountUpdateEvent {
+  eventType: FuturesAccountEventType; // Event Type
+  eventTime: number; // Event Time               // Event Time
+  transactionTime: number; // Transaction
+  accountUpdate: // Update Data
+  {
+    balances: FuturesAccountEventBalance[];
+    positions: FuturesAccountEventPosition[];
+  };
+}
+
+export interface FuturesTradeOrderUpdateEvent {
+  eventType: FuturesAccountEventType; // Event Type
+  eventTime: number; // Event Time               // Event Time
+  transactionTime: number; // Tran        //  Transaction Time
+  order: FuturesTradeOrder;
+}
