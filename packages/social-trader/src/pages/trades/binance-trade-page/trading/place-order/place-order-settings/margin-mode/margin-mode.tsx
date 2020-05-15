82f082c6f6f92f235f412946275789a5c756602b
pavelzubov <pasha224@yandex.ru>
2020-05-15 19:24:01 +0300

    Set button size to small in margin-mode.tsx

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/margin-mode/margin-mode.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/margin-mode/margin-mode.tsx
@@ -1,4 +1,4 @@
-import GVButton from "components/gv-button";
+import GVButton, { GV_BTN_SIZE } from "components/gv-button";
 import useIsOpen from "hooks/is-open.hook";
 import { MarginModeDialog } from "pages/trades/binance-trade-page/trading/place-order/place-order-settings/margin-mode/margin-mode.dialog";
 import { MarginModeType } from "pages/trades/binance-trade-page/trading/trading.types";
@@ -19,7 +19,11 @@ const _MarginMode: React.FC<Props> = ({ mode, onChange }) => {
   );
   return (
     <>
-      <GVButton variant={"outlined"} onClick={setIsOpen}>
+      <GVButton
+        size={GV_BTN_SIZE.SMALL}
+        variant={"outlined"}
+        onClick={setIsOpen}
+      >
         {mode}
       </GVButton>
       <MarginModeDialog
