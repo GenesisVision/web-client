e260c10df886a92550520cc392016cb78aca97e6
pavelzubov <pasha224@yandex.ru>
2020-05-19 12:09:43 +0300

    Set balances to outerCurrentItemContainerItems for ExchangeAccount in DashboardPrivateCardTransfer

--- packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card/dashboard-private-card-transfer.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-trading/dashboard-private-card/dashboard-private-card-transfer.tsx
@@ -2,6 +2,7 @@ import { DashboardTradingAsset } from "gv-api-web";
 import { DemoDepositButton } from "modules/demo-deposit/demo-deposit.button";
 import { DepositTransferButton } from "modules/transfer/deposit-transfer-button";
 import { WithdrawTransferButton } from "modules/transfer/withdraw-transfer-button";
+import { transformAmountWithCurrencyToTransferItem } from "pages/dashboard/components/dashboard-trading/dashboard-private-card.helpers";
 import { mapAccountToTransferItemType } from "pages/dashboard/services/dashboard.service";
 import React from "react";
 
@@ -14,16 +15,24 @@ const _DashboardPrivateCardTransfer: React.FC<Props> = ({
   asset,
   updateItems
 }) => {
+  const currentItemContainerItems =
+    asset.accountInfo.type === "ExchangeAccount"
+      ? asset.accountInfo.balances.map(
+          transformAmountWithCurrencyToTransferItem
+        )
+      : undefined;
   return (
     <>
       {asset.actions.canTransferMoney && (
         <>
           <DepositTransferButton
+            outerCurrentItemContainerItems={currentItemContainerItems}
             onApply={updateItems}
             currentItem={mapAccountToTransferItemType(asset)}
             accountType={asset.accountInfo.type}
           />
           <WithdrawTransferButton
+            outerCurrentItemContainerItems={currentItemContainerItems}
             onApply={updateItems}
             currentItem={mapAccountToTransferItemType(asset)}
             accountType={asset.accountInfo.type}
