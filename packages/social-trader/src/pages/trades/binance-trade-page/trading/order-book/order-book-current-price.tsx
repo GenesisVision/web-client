c9139fbab7cd19317b5fde1e19fdbeef7b01d8f0
pavelzubov <pasha224@yandex.ru>
2020-05-21 10:18:10 +0300

    Use terminalMoneyFormat in OrderBookCurrentPrice

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/order-book/order-book-current-price.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/order-book/order-book-current-price.tsx
@@ -1,8 +1,10 @@
 import { Center } from "components/center/center";
 import { RowItem } from "components/row-item/row-item";
+import { terminalMoneyFormat } from "pages/trades/binance-trade-page/trading/components/terminal-money-format/terminal-money-format";
 import { TradeStatefulValue } from "pages/trades/binance-trade-page/trading/components/trade-stateful-value/trade-stateful-value";
+import { TradingInfoContext } from "pages/trades/binance-trade-page/trading/trading-info.context";
 import { TradeCurrency } from "pages/trades/binance-trade-page/trading/trading.types";
-import React from "react";
+import React, { useContext } from "react";
 
 import styles from "./order-book.module.scss";
 
@@ -19,10 +21,14 @@ const _OrderBookCurrentPrice: React.FC<Props> = ({
   equivalent,
   equivalentCurrency
 }) => {
+  const { tickSize } = useContext(TradingInfoContext);
   return (
     <Center className={styles["order-book__current-price"]}>
       <RowItem large>
-        <TradeStatefulValue value={price} trigger={tradeId} />
+        <TradeStatefulValue
+          value={terminalMoneyFormat({ amount: price, tickSize })}
+          trigger={tradeId}
+        />
       </RowItem>
       {equivalent && (
         <RowItem>
