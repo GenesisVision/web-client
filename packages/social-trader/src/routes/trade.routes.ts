619d0e4d14b51f97e88a7c10ccf0717f4c97fa84
pavelzubov <pasha224@yandex.ru>
2020-05-22 18:02:01 +0300

    Add TERMINAL_FOLDER_ROUTE to trade.routes.ts

--- packages/social-trader/src/routes/trade.routes.ts
+++ packages/social-trader/src/routes/trade.routes.ts
@@ -3,6 +3,7 @@ export const TRADE_ROUTE = `/${TRADE}`;
 
 export const TERMINAL = "terminal";
 export const TERMINAL_ROUTE = `${TRADE_ROUTE}/${TERMINAL}`;
+export const TERMINAL_FOLDER_ROUTE = `${TRADE_ROUTE}/${TERMINAL}/[id]`;
 export const BINANCE = "binance";
 export const BINANCE_ROUTE = `${TRADE_ROUTE}/${BINANCE}`;
 export const BINANCE_FOLDER_ROUTE = `${TRADE_ROUTE}/${BINANCE}/[id]`;
