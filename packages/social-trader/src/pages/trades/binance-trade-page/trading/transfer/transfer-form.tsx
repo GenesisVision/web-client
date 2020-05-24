446fd4306e606f8cfa172d11a7bbbe0191ef13f8
pavelzubov <pasha224@yandex.ru>
2020-05-24 21:22:57 +0300

    Create futures transfer component files

---
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/transfer/transfer-form.tsx
@@ -0,0 +1,135 @@
+import { CurrencyItem } from "components/currency-item/currency-item";
+import { GVHookFormField } from "components/gv-hook-form-field";
+import HookFormAmountField from "components/input-amount-field/hook-form-amount-field";
+import { RowItem } from "components/row-item/row-item";
+import { Row } from "components/row/row";
+import Select from "components/select/select";
+import { SimpleTextField } from "components/simple-fields/simple-text-field";
+import StatisticItemInner from "components/statistic-item/statistic-item-inner";
+import { SubmitButton } from "components/submit-button/submit-button";
+import {
+  BalancesForTransfer,
+  TradeCurrency
+} from "pages/trades/binance-trade-page/trading/trading.types";
+import {
+  ENABLE_TRANSFER_ACCOUNTS,
+  futuresTransferValidationSchema,
+  getBalanceNameFromNumber,
+  getMaxValueForFuturesTransfer,
+  TRANSFER_FORM_FIELDS,
+  TransferFormValues
+} from "pages/trades/binance-trade-page/trading/transfer/transfer.helpers";
+import React, { useCallback, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useTranslation } from "react-i18next";
+import { HookForm } from "utils/hook-form.helpers";
+
+import styles from "./transfer.module.scss";
+
+interface Props {
+  asset: TradeCurrency;
+  isSuccessful?: boolean;
+  data: BalancesForTransfer;
+  onSubmit: (values: TransferFormValues) => Promise<void>;
+}
+
+const _TransferForm: React.FC<Props> = ({
+  asset: assetProp,
+  isSuccessful,
+  data,
+  onSubmit
+}) => {
+  const [t] = useTranslation();
+  const form = useForm<TransferFormValues>({
+    defaultValues: {
+      [TRANSFER_FORM_FIELDS.type]: 1,
+      [TRANSFER_FORM_FIELDS.asset]: assetProp
+    },
+    validationSchema: futuresTransferValidationSchema({
+      t,
+      balances: data
+    }),
+    mode: "onChange"
+  });
+  const { watch, setValue, reset } = form;
+  const { asset, type } = watch();
+
+  useEffect(() => {
+    reset({ asset, type });
+  }, [asset, type]);
+
+  const maxAmount = getMaxValueForFuturesTransfer({
+    type,
+    asset,
+    balances: data
+  });
+
+  const setMax = useCallback(() => {
+    setValue(TRANSFER_FORM_FIELDS.amount, maxAmount, true);
+  }, [maxAmount, setValue]);
+
+  const handleTurn = useCallback(() => {
+    const newType = type === 1 ? 2 : 1;
+    setValue(TRANSFER_FORM_FIELDS.type, newType, true);
+  }, [type, setValue]);
+
+  const fromSource = getBalanceNameFromNumber(type);
+  const toSource = getBalanceNameFromNumber(type === 1 ? 2 : 1);
+
+  return (
+    <HookForm form={form} onSubmit={onSubmit}>
+      <Row className={styles["transfer__type-block"]}>
+        <RowItem>
+          <StatisticItemInner label={t("From")}>
+            {fromSource} wallet
+          </StatisticItemInner>
+        </RowItem>
+        <RowItem className={styles["transfer__type-turn"]} onClick={handleTurn}>
+          turn
+        </RowItem>
+        <RowItem hide>
+          <GVHookFormField
+            component={SimpleTextField}
+            name={TRANSFER_FORM_FIELDS.type}
+          />
+        </RowItem>
+        <RowItem>
+          <StatisticItemInner label={t("To")}>
+            {toSource} wallet
+          </StatisticItemInner>
+        </RowItem>
+      </Row>
+      <Row>
+        <GVHookFormField
+          wide
+          name={TRANSFER_FORM_FIELDS.asset}
+          component={SimpleTextField}
+          label={t("Coin")}
+          InputComponent={Select}
+        >
+          {ENABLE_TRANSFER_ACCOUNTS.map(({ asset, title }) => (
+            <option value={asset} key={asset}>
+              <CurrencyItem clickable={false} symbol={asset} name={asset} />
+            </option>
+          ))}
+        </GVHookFormField>
+      </Row>
+      <Row>
+        <HookFormAmountField
+          setMax={setMax}
+          autoFocus={false}
+          label={t("Amount")}
+          currency={asset}
+          name={TRANSFER_FORM_FIELDS.amount}
+        />
+      </Row>
+      <Row>
+        <SubmitButton isSuccessful={isSuccessful} wide>
+          {t("Confirm transfer")}
+        </SubmitButton>
+      </Row>
+    </HookForm>
+  );
+};
+
+export const TransferForm = React.memo(_TransferForm);
