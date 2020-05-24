d9a28f1145958c611230dfa411a2ad2bc9750fac
pavelzubov <pasha224@yandex.ru>
2020-05-24 15:54:47 +0300

    Add PositionModeContainer to PlaceOrderSettings

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/place-order-settings.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/place-order-settings.tsx
@@ -2,6 +2,7 @@ import { Center } from "components/center/center";
 import { RowItem } from "components/row-item/row-item";
 import { ChangeLeverageContainer } from "pages/trades/binance-trade-page/trading/place-order/place-order-settings/change-leverage/change-leverage.container";
 import { MarginModeContainer } from "pages/trades/binance-trade-page/trading/place-order/place-order-settings/margin-mode/margin-mode.container";
+import { PositionModeContainer } from "pages/trades/binance-trade-page/trading/place-order/place-order-settings/position-mode/position-mode.container";
 import React from "react";
 
 interface Props {}
@@ -14,6 +15,9 @@ const _PlaceOrderSettings: React.FC<Props> = () => {
       </RowItem>
       <RowItem>
         <ChangeLeverageContainer />
+      </RowItem>
+      <RowItem>
+        <PositionModeContainer />
       </RowItem>
     </Center>
   );
