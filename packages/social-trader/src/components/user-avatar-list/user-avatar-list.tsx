21f6f321011f07bbb4118ed76dece735cd386321
pavelzubov <pasha224@yandex.ru>
2020-05-26 14:41:24 +0300

    Use Link in UserAvatarList

--- packages/social-trader/src/components/user-avatar-list/user-avatar-list.tsx
+++ packages/social-trader/src/components/user-avatar-list/user-avatar-list.tsx
@@ -1,8 +1,11 @@
 import classNames from "classnames";
 import ProfileAvatar from "components/avatar/profile-avatar/profile-avatar";
 import { Center } from "components/center/center";
+import Link from "components/link/link";
+import { useToLink } from "components/link/link.helper";
 import { ProfilePublicShort } from "gv-api-web";
 import React from "react";
+import { managerToPathCreator } from "routes/manager.routes";
 
 import styles from "./user-avatar-list.module.scss";
 
@@ -12,6 +15,7 @@ interface Props {
 }
 
 const _UserAvatarList: React.FC<Props> = ({ length = 3, list }) => {
+  const { contextTitle } = useToLink();
   return (
     <Center
       className={styles["user-avatar-list__container"]}
@@ -19,16 +23,21 @@ const _UserAvatarList: React.FC<Props> = ({ length = 3, list }) => {
         width: (44 - 20) * (Math.min(length, list.length) + 1)
       }}
     >
-      {list.slice(0, length).map(({ logoUrl, username }, i) => (
-        <div
-          className={styles["user-avatar-list__item"]}
-          style={{
-            left: -(20 * i)
-          }}
-        >
-          <ProfileAvatar url={logoUrl} alt={username} />
-        </div>
-      ))}
+      {list.slice(0, length).map(({ url, logoUrl, username }, i) => {
+        const profileUrl = managerToPathCreator(url, contextTitle);
+        return (
+          <div
+            className={styles["user-avatar-list__item"]}
+            style={{
+              left: -(20 * i)
+            }}
+          >
+            <Link to={profileUrl}>
+              <ProfileAvatar url={logoUrl} alt={username} />
+            </Link>
+          </div>
+        );
+      })}
       {list.length > length && (
         <div
           className={classNames(
