b9cfd80daa636faadec7a0b591dcc3b513b98df5
pavelzubov <pasha224@yandex.ru>
2020-05-27 10:45:24 +0300

    Add getExchangeAccountCredentials request to trade/terminal/id route

--- packages/social-trader/pages/trade/terminal/[id].tsx
+++ packages/social-trader/pages/trade/terminal/[id].tsx
@@ -1,5 +1,4 @@
 import withTradeLayout from "decorators/with-trade-layout";
-import { NextPage } from "next";
 import { TYPE_PARAM_NAME } from "pages/trades/binance-trade-page/binance-trade.helpers";
 import { BinanceTrade } from "pages/trades/binance-trade-page/binance-trade.page";
 import { SymbolState } from "pages/trades/binance-trade-page/trading/trading-info.context";
@@ -12,7 +11,9 @@ import {
   TradeAuthDataType
 } from "pages/trades/binance-trade-page/trading/trading.types";
 import React from "react";
+import { api } from "services/api-client/swagger-custom-client";
 import { getParamsFromCtxWithSplit } from "utils/ssr-helpers";
+import { NextPageWithRedux } from "utils/types";
 
 interface Props {
   authData: TradeAuthDataType;
@@ -20,19 +21,40 @@ interface Props {
   symbol?: SymbolState;
 }
 
-const Page: NextPage<Props> = ({ authData, type, symbol }) => {
+const Page: NextPageWithRedux<Props> = ({ authData, type, symbol }) => {
   return <BinanceTrade authData={authData} type={type} symbol={symbol} />;
 };
 
 Page.getInitialProps = async ctx => {
   const { get } = authCookieService(ctx);
-  const authData = get();
+  let authData;
+  let brokerType;
   const { id } = ctx.query;
   const params = getParamsFromCtxWithSplit(ctx);
+  const exchangeAccountId = params["id"];
+  if (exchangeAccountId) {
+    const credentialsData = await api
+      .dashboard(ctx.token)
+      .getExchangeAccountCredentials({ exchangeAccountId });
+    const {
+      credentials: { apiKey, apiSecret },
+      broker: { type }
+    } = credentialsData;
+    authData = {
+      publicKey: apiKey,
+      privateKey: apiSecret
+    };
+    brokerType = type;
+  } else {
+    authData = get();
+    brokerType = "Binance";
+  }
+  const symbol = id ? parseSymbolFromUrlParam(String(id)) : undefined;
   return {
+    brokerType,
     authData,
-    type: (params[TYPE_PARAM_NAME] || "").toLowerCase(),
-    symbol: parseSymbolFromUrlParam(String(id))
+    symbol,
+    type: (params[TYPE_PARAM_NAME] || "").toLowerCase()
   };
 };
 
