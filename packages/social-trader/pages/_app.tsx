09610658ab7b367ea4d8341d79a445eccce55a45
e.grebenshikov <evgreb@gmail.com>
2020-05-27 17:51:00 +0300

    GV-2727 move move scroll styles into _app.tsx

--- packages/social-trader/pages/_app.tsx
+++ packages/social-trader/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { $textDarkColor } from "components/gv-styles/gv-colors/gv-colors";
 import "cropperjs/dist/cropper.css";
 import withHistoryProvider from "decorators/history-provider/with-history-provider";
 import withReduxStore from "decorators/with-redux-store";
@@ -18,6 +19,36 @@ class CustomApp extends App<Props> {
     return (
       <Provider store={reduxStore}>
         <Component {...pageProps} />
+        <style global jsx>{`
+          :root {
+            --scroll-width: 6px;
+            --scroll-radius: calc(var(--scroll-width) / 2);
+            --scroll-background: transparent;
+            --scroll-thumb-color: ${$textDarkColor};
+          }
+          ::-webkit-scrollbar {
+            width: var(--scroll-width);
+            height: var(--scroll-width);
+          }
+
+          ::-webkit-scrollbar-track {
+            background: var(--scroll-background);
+          }
+
+          ::-webkit-scrollbar-thumb {
+            background: var(--scroll-thumb-color);
+            border-radius: var(--scroll-radius);
+          }
+
+          ::-webkit-scrollbar-corner {
+            background: var(--scroll-background);
+          }
+
+          * {
+            scrollbar-width: thin;
+            scrollbar-color: var(--scroll-thumb-color) var(--scroll-background);
+          }
+        `}</style>
       </Provider>
     );
   }
