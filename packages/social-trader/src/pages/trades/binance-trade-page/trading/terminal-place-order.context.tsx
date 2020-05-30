06cbfb8972f6cb5e56e0328b2b82c42aada82c6c
pavelzubov <pasha224@yandex.ru>
2020-05-30 11:28:27 +0300

    Add leverage, setLeverage to TerminalPlaceOrderContextProvider, TerminalPlaceOrderContext

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/terminal-place-order.context.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/terminal-place-order.context.tsx
@@ -10,12 +10,18 @@ import React, {
   useState
 } from "react";
 
+const InitialTerminalLeverageState = 1;
+
 type TradingAccountInfoState = {
+  leverage: number;
+  setLeverage: (leverage: number) => void;
   currentPositionMode?: PositionModeType;
   updatePositionMode: VoidFunction;
 };
 
 export const TerminalPlaceOrderInitialState: TradingAccountInfoState = {
+  leverage: InitialTerminalLeverageState,
+  setLeverage: () => {},
   updatePositionMode: () => {}
 };
 
@@ -27,6 +33,7 @@ export const TerminalPlaceOrderContextProvider: React.FC = ({ children }) => {
   const { authData } = useContext(TradingInfoContext);
   const { getPositionMode } = useContext(TerminalMethodsContext);
 
+  const [leverage, setLeverage] = useState(InitialTerminalLeverageState);
   const [currentPositionMode, setCurrentPositionMode] = useState<
     PositionModeType | undefined
   >();
@@ -41,10 +48,10 @@ export const TerminalPlaceOrderContextProvider: React.FC = ({ children }) => {
       getPositionMode({ authData }).then(setCurrentPositionMode);
   }, [getPositionMode, authData]);
 
-  const value = useMemo(() => ({ updatePositionMode, currentPositionMode }), [
-    updatePositionMode,
-    currentPositionMode
-  ]);
+  const value = useMemo(
+    () => ({ leverage, setLeverage, updatePositionMode, currentPositionMode }),
+    [leverage, setLeverage, updatePositionMode, currentPositionMode]
+  );
 
   return (
     <TerminalPlaceOrderContext.Provider value={value}>
