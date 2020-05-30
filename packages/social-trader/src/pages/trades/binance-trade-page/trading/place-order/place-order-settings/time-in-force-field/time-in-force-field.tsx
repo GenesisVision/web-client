bc3fe468ab3e6f2f911c92b2a429b1a54b256496
pavelzubov <pasha224@yandex.ru>
2020-05-30 04:37:49 +0300

    Add terminalType === "spot" to condition to choice values in TimeInForceField

--- packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/time-in-force-field/time-in-force-field.tsx
+++ packages/social-trader/src/pages/trades/binance-trade-page/trading/place-order/place-order-settings/time-in-force-field/time-in-force-field.tsx
@@ -2,11 +2,12 @@ import { GVHookFormField } from "components/gv-hook-form-field";
 import Select from "components/select/select";
 import { SimpleTextField } from "components/simple-fields/simple-text-field";
 import { TRADE_FORM_FIELDS } from "pages/trades/binance-trade-page/trading/place-order/place-order.helpers";
+import { TradingInfoContext } from "pages/trades/binance-trade-page/trading/trading-info.context";
 import {
   OrderType,
   TimeInForce as TimeInForceType
 } from "pages/trades/binance-trade-page/trading/trading.types";
-import React from "react";
+import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 
 interface Props {
@@ -16,8 +17,9 @@ interface Props {
 export const TIME_IN_FORCE_VALUES: TimeInForceType[] = ["GTC", "IOC", "FOK"];
 
 const _TimeInForceField: React.FC<Props> = ({ orderType }) => {
+  const { terminalType } = useContext(TradingInfoContext);
   const values: TimeInForceType[] =
-    orderType === "STOP_LOSS_LIMIT"
+    terminalType === "spot" || orderType === "STOP_LOSS_LIMIT"
       ? TIME_IN_FORCE_VALUES
       : [...TIME_IN_FORCE_VALUES, "GTX"];
   const [t] = useTranslation();
