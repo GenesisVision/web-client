1a0b99614fe6441c25be9aaabd98c804cdd430b2
pavelzubov <pasha224@yandex.ru>
2020-06-05 10:16:48 +0300

    Add usage exchange accounts in InvestmentAccountControls

--- packages/social-trader/src/pages/accounts/account-details/investment-account-controls.tsx
+++ packages/social-trader/src/pages/accounts/account-details/investment-account-controls.tsx
@@ -4,31 +4,53 @@ import DetailsBlock, {
 import { DetailsBlockRowItem } from "components/details/details-block-row-item.block";
 import { GV_BTN_SIZE } from "components/gv-button";
 import { WalletItemType } from "components/wallet-select/wallet-select";
-import { AssetTypeExt, PrivateTradingAccountType } from "gv-api-web";
+import {
+  AmountWithCurrency,
+  AssetTypeExt,
+  PrivateTradingAccountType
+} from "gv-api-web";
 import { DepositTransferButton } from "modules/transfer/deposit-transfer-button";
 import { WithdrawTransferButton } from "modules/transfer/withdraw-transfer-button";
+import { transformAmountWithCurrencyToTransferItem } from "pages/dashboard/components/dashboard-trading/dashboard-private-card.helpers";
 import * as React from "react";
 
 const _InvestmentAccountControls: React.FC<Props> = ({
+  id,
+  balances,
   onApply,
   accountType,
   transferableItem
 }) => {
+  const isExchangeAccount = accountType === "ExchangeAccount";
+  const currentItemContainerItems =
+    isExchangeAccount && balances
+      ? balances.map(transformAmountWithCurrencyToTransferItem)
+      : undefined;
+  const currentItem =
+    isExchangeAccount && balances
+      ? transformAmountWithCurrencyToTransferItem(balances[0])
+      : transferableItem;
   return (
     <DetailsBlock type={DETAILS_BLOCK_TYPE.BORDERED} row>
       <DetailsBlockRowItem>
         <DepositTransferButton
+          fixedSelects={isExchangeAccount}
+          accountId={id}
+          outerCurrentItemContainerItems={currentItemContainerItems}
           size={GV_BTN_SIZE.BIG}
           onApply={onApply}
-          currentItem={transferableItem}
+          currentItem={currentItem}
           accountType={accountType}
         />
       </DetailsBlockRowItem>
       <DetailsBlockRowItem>
         <WithdrawTransferButton
+          fixedSelects={isExchangeAccount}
+          accountId={id}
+          outerCurrentItemContainerItems={currentItemContainerItems}
           size={GV_BTN_SIZE.BIG}
           onApply={onApply}
-          currentItem={transferableItem}
+          currentItem={currentItem}
           accountType={accountType}
         />
       </DetailsBlockRowItem>
@@ -37,6 +59,8 @@ const _InvestmentAccountControls: React.FC<Props> = ({
 };
 
 interface Props {
+  id?: string;
+  balances?: AmountWithCurrency[];
   transferableItem: WalletItemType;
   accountType?: PrivateTradingAccountType | AssetTypeExt;
   onApply: VoidFunction;
