00d49caa0b3afa157b65728e5a19936afb0fa8bb
pavelzubov <pasha224@yandex.ru>
2020-06-12 17:10:40 +0300

    Create UpperButtonContainer, UpperButton components

---
+++ packages/social-trader/src/components/upper-button/upper-button.tsx
@@ -0,0 +1,47 @@
+import classNames from "classnames";
+import React, { useCallback, useEffect, useState } from "react";
+import EventListener from "react-event-listener";
+
+import styles from "./upper-button.module.scss";
+
+export const UpperButtonContainer: React.FC = () => {
+  const [windowHeight, setWindowHeight] = useState<number>(0);
+  const [scrollTop, setScrollTop] = useState<number>(0);
+  const [visible, setVisible] = useState(false);
+  const handleScroll = useCallback(() => setScrollTop(window.scrollY), []);
+
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+    setScrollTop(window.scrollY);
+  }, []);
+
+  useEffect(() => {
+    if (scrollTop > windowHeight) setVisible(true);
+    else setVisible(false);
+  }, [scrollTop]);
+
+  return (
+    <>
+      <EventListener target={"window"} onScroll={handleScroll} />
+      <UpperButton visible={visible} />
+    </>
+  );
+};
+
+export const UpperButton: React.FC<{ visible: boolean }> = ({ visible }) => {
+  const handleClick = useCallback(() => {
+    window.scroll({ top: 0 });
+  }, []);
+  return (
+    <>
+      <div
+        onClick={handleClick}
+        className={classNames(styles["upper-button"], {
+          [styles["upper-button--visible"]]: visible
+        })}
+      >
+        <div className={styles["upper-button__arrow"]}>&uarr;</div>
+      </div>
+    </>
+  );
+};
