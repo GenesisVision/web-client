a11c5e8fbe194bf671d63071fc4b912802d061eb
pavelzubov <pasha224@yandex.ru>
2020-06-15 14:05:34 +0300

    Set article to type of Page in fund and program details page

--- packages/social-trader/src/pages/invest/programs/program-details/program-details.contaner.tsx
+++ packages/social-trader/src/pages/invest/programs/program-details/program-details.contaner.tsx
@@ -125,6 +125,7 @@ const _ProgramDetailsContainer: React.FC<Props> = ({
 
   return (
     <Page
+      type={"article"}
       title={`${
         assetType === ASSET.FOLLOW
           ? t("follows-page.title")
