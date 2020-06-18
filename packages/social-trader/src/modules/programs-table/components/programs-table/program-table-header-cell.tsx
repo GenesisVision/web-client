b91008f59c6e18ab21253f7026d10a6b608de4e3
Anchello <bob.m@mail.ru>
2020-06-18 14:29:28 +0200

    added tooltips for ProgramTableHeaderCell component

--- packages/social-trader/src/modules/programs-table/components/programs-table/program-table-header-cell.tsx
+++ packages/social-trader/src/modules/programs-table/components/programs-table/program-table-header-cell.tsx
@@ -1,5 +1,8 @@
 import classNames from "classnames";
+import { HORIZONTAL_POPOVER_POS } from "components/popover/popover";
 import { SortingColumn } from "components/table/components/filtering/filter.type";
+import Tooltip from "components/tooltip/tooltip";
+import { TooltipContent } from "components/tooltip/tooltip-content";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
@@ -13,7 +16,7 @@ const _ProgramTableHeaderCell: React.FC<{ column: SortingColumn }> = ({
   const isAuthenticated = useSelector(isAuthenticatedSelector);
   const { t } = useTranslation();
   if (!isAuthenticated && column.name === "favorite") return null;
-  return (
+  const renderCell = () => (
     <span
       className={classNames(
         styles["programs-table__cell"],
@@ -22,6 +25,20 @@ const _ProgramTableHeaderCell: React.FC<{ column: SortingColumn }> = ({
     >
       {t(`programs-page.programs-header.${column.name}`)}
     </span>
+  );
+  return column.tooltip ? (
+    <Tooltip
+      horizontal={HORIZONTAL_POPOVER_POS.LEFT}
+      render={() => (
+        <TooltipContent>
+          {t(`programs-page.tooltips.${column.name}`)}
+        </TooltipContent>
+      )}
+    >
+      {renderCell()}
+    </Tooltip>
+  ) : (
+    renderCell()
   );
 };
 
