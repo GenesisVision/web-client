958dad92fda87afa995a5037b5f193752b5b5403
pavelzubov <pasha224@yandex.ru>
2020-06-20 10:10:19 +0300

    Move social data request to server

--- packages/social-trader/pages/social/index.tsx
+++ packages/social-trader/pages/social/index.tsx
@@ -1,16 +1,26 @@
 import withBetaTesting from "decorators/with-beta-testing";
 import withDefaultLayout from "decorators/with-default-layout";
+import { SocialSummary } from "gv-api-web";
 import { NextPage } from "next";
+import { getSocialPageData } from "pages/social/social/services/social-page.service";
 import { SocialPage } from "pages/social/social/social.page";
 import React from "react";
 import { compose } from "redux";
 
-const Page: NextPage = () => {
-  return <SocialPage />;
+interface Props {
+  data: SocialSummary;
+}
+
+const Page: NextPage<Props> = ({ data }) => {
+  return <SocialPage data={data} />;
 };
 
-Page.getInitialProps = async () => ({
-  namespacesRequired: ["social-page"]
-});
+Page.getInitialProps = async () => {
+  const data = await getSocialPageData();
+  return {
+    data,
+    namespacesRequired: ["social-page"]
+  };
+};
 
 export default compose(withDefaultLayout, withBetaTesting("Social"))(Page);
