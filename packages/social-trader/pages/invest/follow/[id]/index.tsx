4e4346bf7ce0c29e9d83ba815701496a4187419c
pavelzubov <pasha224@yandex.ru>
2020-06-21 16:52:26 +0300

    Move translations for follow details page to separate file

--- packages/social-trader/pages/invest/follow/[id]/index.tsx
+++ packages/social-trader/pages/invest/follow/[id]/index.tsx
@@ -19,7 +19,7 @@ Page.getInitialProps = async ctx => {
     ctx.reduxStore.dispatch(dispatchProgramId(id as string)),
     ctx.reduxStore.dispatch(dispatchProgramDescription(ctx, ASSET.FOLLOW))
   ]);
-  return {};
+  return { namespacesRequired: ["follow-details-page"] };
 };
 
 export default compose(withDefaultLayout)(Page);
