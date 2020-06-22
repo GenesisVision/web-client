834d6d3e57c6695e6b3c40c639504797987a0554
pavelzubov <pasha224@yandex.ru>
2020-06-22 09:32:28 +0300

    Move translations for profile pages to separate file

--- packages/social-trader/pages/profile/social-links.tsx
+++ packages/social-trader/pages/profile/social-links.tsx
@@ -1,11 +1,16 @@
 import withDefaultLayout from "decorators/with-default-layout";
 import withPrivateRoute from "decorators/with-private-route";
+import { NextPage } from "next";
 import SocialLinksPage from "pages/profile/social-links/social-links.page";
 import React from "react";
 import { compose } from "redux";
 
-const Page: React.FC = () => {
+const Page: NextPage = () => {
   return <SocialLinksPage />;
 };
+
+Page.getInitialProps = async () => ({
+  namespacesRequired: ["profile-page"]
+});
 
 export default compose(withDefaultLayout, withPrivateRoute)(Page);
