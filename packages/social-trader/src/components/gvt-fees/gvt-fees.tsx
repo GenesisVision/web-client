5f74bc49be959e190210a3f8e68c4b1b575766f0
pavelzubov <pasha224@yandex.ru>
2020-06-23 17:58:09 +0300

    Move translations for gvt-fees

--- packages/social-trader/src/components/gvt-fees/gvt-fees.tsx
+++ packages/social-trader/src/components/gvt-fees/gvt-fees.tsx
@@ -15,7 +15,7 @@ const _GVTFees: React.FC<Props> = ({ open, onClose }) => {
   const [t] = useTranslation();
   return (
     <Dialog open={open} onClose={onClose} className={styles["gvt-fees"]}>
-      <DialogTop title={t("gvt-fees.titles.main")} />
+      <DialogTop title={t("labels.gvt-fees-title")} />
       <DialogBottom className={styles["gvt-fees__container"]}>
         <Row wide>
           <FeesTradingDiscountInfo withoutOffset dark />
