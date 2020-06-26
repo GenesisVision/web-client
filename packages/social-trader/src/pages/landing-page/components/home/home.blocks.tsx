e76b077edf366273e7fd5790ea1248237c200fa8
Anchello <bob.m@mail.ru>
2020-06-26 17:57:46 +0200

    added the home module

---
+++ packages/social-trader/src/pages/landing-page/components/home/home.blocks.tsx
@@ -0,0 +1,37 @@
+import classNames from "classnames";
+import React from "react";
+
+import styles from "./home.module.scss";
+
+export const HomeContainer: React.FC = ({ children }) => {
+  return <div className={styles["home__container"]}>{children}</div>;
+};
+
+export const HomeSection: React.FC<HomeSectionProps> = ({
+  children,
+  bgColor,
+  isFirst,
+  isLast,
+  hasPadding
+}) => {
+  return (
+    <section
+      className={classNames(styles["home__section"], {
+        [styles["home__section--bg-gray"]]: bgColor === "gray",
+        [styles["home__section--bg-white"]]: bgColor === "white",
+        [styles["home__section--first-screen"]]: isFirst,
+        [styles["home__section--last-screen"]]: isLast,
+        [styles["home__section--horizontal-padding"]]: hasPadding
+      })}
+    >
+      {children}
+    </section>
+  );
+};
+
+interface HomeSectionProps {
+  bgColor?: "gray" | "white";
+  isFirst?: boolean;
+  isLast?: boolean;
+  hasPadding?: boolean;
+}
