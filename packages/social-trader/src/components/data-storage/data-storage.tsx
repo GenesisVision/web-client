8e6735be76b933d0688c041b4e33e5b748d26e3a
pavelzubov <pasha224@yandex.ru>
2020-06-27 19:37:57 +0300

    Create DataStorageContextProvider component

---
+++ packages/social-trader/src/components/data-storage/data-storage.tsx
@@ -0,0 +1,51 @@
+import useApiRequest, { API_REQUEST_STATUS } from "hooks/api-request.hook";
+import React, { createContext, useCallback, useMemo } from "react";
+import { AnyObjectType } from "utils/types";
+
+interface Props {
+  fetchOnMountData?: any;
+  request: (...args: any[]) => Promise<any>;
+}
+
+interface DataStorageContextState {
+  status: API_REQUEST_STATUS;
+  data: any;
+  updateData: (...args: any[]) => void;
+}
+
+export const DataStorageInitialState = {
+  updateData: () => {}
+} as DataStorageContextState;
+
+export const DataStorageContext = createContext<DataStorageContextState>(
+  DataStorageInitialState
+);
+
+export const DataStorageContextProvider: React.FC<Props> = ({
+  fetchOnMountData,
+  children,
+  request
+}) => {
+  const { data, sendRequest, status } = useApiRequest({
+    request,
+    fetchOnMount: true,
+    fetchOnMountData
+  });
+
+  const updateData = useCallback(
+    (values?: AnyObjectType) => sendRequest({ ...fetchOnMountData, ...values }),
+    [fetchOnMountData, sendRequest]
+  );
+
+  const value = useMemo(() => ({ data, updateData, status }), [
+    data,
+    updateData,
+    status
+  ]);
+
+  return (
+    <DataStorageContext.Provider value={value}>
+      {children}
+    </DataStorageContext.Provider>
+  );
+};
