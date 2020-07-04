2f6daf7451f5fa97f949e2ca6d709795f1d1348f
pavelzubov <pasha224@yandex.ru>
2020-07-04 19:27:19 +0300

    Add show event flag in FeedContainer

--- packages/social-trader/src/pages/feed/feed.container.tsx
+++ packages/social-trader/src/pages/feed/feed.container.tsx
@@ -7,9 +7,11 @@ import {
 } from "components/conversation/conversation.service";
 import { PostListWithInput } from "components/conversation/post-list/post-list-with-input";
 import { IPostListContainerInitData } from "components/conversation/post-list/post-list.container";
-import React, { useCallback } from "react";
+import { SocialPageContext } from "pages/social/social/social-page.context";
+import React, { useCallback, useContext } from "react";
 import { useSelector } from "react-redux";
 import { isAuthenticatedSelector } from "reducers/auth-reducer";
+import Token from "services/api-client/token";
 
 export enum FEED_TYPE {
   ALL = "ALL",
@@ -23,15 +25,22 @@ interface Props extends IPostListContainerInitData {
   feedType?: FEED_TYPE;
 }
 
-const getFeedMethod = (
-  feedType?: FEED_TYPE,
-  searchValue?: SearchInFeedValues,
-  showTop?: boolean
-) => {
+const getFeedMethod = ({
+  feedType,
+  searchValue,
+  showTop,
+  showEvents
+}: {
+  feedType?: FEED_TYPE;
+  searchValue?: SearchInFeedValues;
+  showTop?: boolean;
+  showEvents?: boolean;
+}) => {
   if (searchValue) return searchInFeed(searchValue);
   if (showTop) return getTopPosts;
   if (feedType === FEED_TYPE.PERSONAL) return getNewsFeed;
-  return getGlobalFeed;
+  return (values?: Object, token?: Token) =>
+    getGlobalFeed({ ...values, showEvents }, token);
 };
 
 const _FeedContainer: React.FC<Props> = ({
@@ -41,10 +50,11 @@ const _FeedContainer: React.FC<Props> = ({
   showInput,
   feedType
 }) => {
+  const { showEvents } = useContext(SocialPageContext);
   const isAuthenticated = useSelector(isAuthenticatedSelector);
   const fetchMethod = useCallback(
-    () => getFeedMethod(feedType, searchValue, showTop),
-    [feedType, searchValue, showTop]
+    () => getFeedMethod({ feedType, searchValue, showTop, showEvents }),
+    [showEvents, feedType, searchValue, showTop]
   );
   return (
     <PostListWithInput
