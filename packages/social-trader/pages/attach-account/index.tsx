ac43af9afea1312e357726f331c88d0837ee1116
pavelzubov <pasha224@yandex.ru>
2020-07-07 18:27:22 +0300

    Add account-settings to namespacesRequired in attach-account/

--- packages/social-trader/pages/attach-account/index.tsx
+++ packages/social-trader/pages/attach-account/index.tsx
@@ -13,7 +13,11 @@ const Page: NextPageWithRedux<Props, {}> = ({ requestBrokerName }) => {
 Page.getInitialProps = async ctx => {
   const requestBrokerName = getBrokerFromContext(ctx);
   return {
-    namespacesRequired: ["asset-settings", "attach-account-page"],
+    namespacesRequired: [
+      "account-settings",
+      "asset-settings",
+      "attach-account-page"
+    ],
     requestBrokerName
   };
 };
