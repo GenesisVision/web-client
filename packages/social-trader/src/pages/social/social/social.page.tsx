d8ad480f060f24749e423ac351d36ba98acd305c
pavelzubov <pasha224@yandex.ru>
2020-07-08 13:20:17 +0300

    Correct way to SocialPageContextProvider

--- packages/social-trader/src/pages/social/social/social.page.tsx
+++ packages/social-trader/src/pages/social/social/social.page.tsx
@@ -1,6 +1,6 @@
 import Page from "components/page/page";
-import { SocialPageContextProvider } from "feed.context.tsx";
 import { PostItemsViewModel, SocialSummary } from "gv-api-web";
+import { SocialPageContextProvider } from "pages/social/social/feed.context";
 import { SocialPageContainer } from "pages/social/social/social-page.container";
 import React from "react";
 import { useTranslation } from "react-i18next";
