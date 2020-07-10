8246bc712d251de40c769c61fa911483b29c05f0
pavelzubov <pasha224@yandex.ru>
2020-07-10 15:13:33 +0300

    Use FirstSliderWithAnimation without effectiveConnectionType in FirstScreen

--- packages/social-trader/src/pages/landing-page/components/first-screen/first-screen.tsx
+++ packages/social-trader/src/pages/landing-page/components/first-screen/first-screen.tsx
@@ -1,38 +1,17 @@
 import { PlatformNews } from "gv-api-web";
-import { useNetworkStatusInWindow } from "hooks/network-status";
-import dynamic from "next/dynamic";
-import FirstSlider from "pages/landing-page/components/first-slider/first-slider";
-import styles from "pages/landing-page/components/first-slider/first-slider.module.scss";
+import FirstSliderWithAnimation from "pages/landing-page/components/first-slider/first-slider-with-animation";
 import NewsList from "pages/landing-page/components/news/news-list";
 import { slides } from "pages/landing-page/static-data/slides";
-import React, { useCallback } from "react";
-
-const FirstSliderWithAnimation = dynamic(() =>
-  import(
-    "pages/landing-page/components/first-slider/first-slider-with-animation"
-  )
-);
+import React from "react";
 
 interface Props {
   news: Array<PlatformNews>;
 }
 
 const _FirstScreen: React.FC<Props> = ({ news }) => {
-  const { effectiveConnectionType } = useNetworkStatusInWindow();
-  const renderSlider = useCallback(() => {
-    switch (effectiveConnectionType) {
-      case "4g":
-        return <FirstSliderWithAnimation slidesItems={slides} />;
-      case "3g":
-      case "2g":
-        return <FirstSlider slidesItems={slides} />;
-      default:
-        return null;
-    }
-  }, [effectiveConnectionType, slides]);
   return (
     <>
-      <div className={styles["slider__container"]}>{renderSlider()}</div>
+      <FirstSliderWithAnimation slidesItems={slides} />
       <NewsList newsItems={news} />
     </>
   );
