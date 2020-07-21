9d5686dbc19bfcbe419d33665eb46232e6b7027f
pavelzubov <pasha224@yandex.ru>
2020-07-21 17:05:51 +0300

    Change markup, add translations in InvestmentLimitPopup

--- packages/social-trader/src/pages/dashboard/components/kyc-limit/investment-limit-popup/investment-limit-popup.tsx
+++ packages/social-trader/src/pages/dashboard/components/kyc-limit/investment-limit-popup/investment-limit-popup.tsx
@@ -1,10 +1,18 @@
 import Dialog from "components/dialog/dialog";
 import { DialogBottom } from "components/dialog/dialog-bottom";
+import GVButton from "components/gv-button";
+import { Li } from "components/li/li";
+import Link from "components/link/link";
+import { useToLink } from "components/link/link.helper";
+import { KYC_ROUTE } from "components/profile/profile.constants";
+import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
+import { Text } from "components/text/text";
+import { Ul } from "components/ul/ul";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 
-import styles from "./gvt-fees.module.scss";
+import styles from "./investment-limit-popup.module.scss";
 
 interface Props {
   open: boolean;
@@ -12,48 +20,64 @@ interface Props {
 }
 
 const _InvestmentLimitPopup: React.FC<Props> = ({ open, onClose }) => {
+  const { linkCreator } = useToLink();
   const [t] = useTranslation();
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogBottom>
+      <DialogBottom className={styles["investment-limit-popup"]}>
+        <Row />
         <Row>
-          <h3>{t("Without KYC you can")}</h3>
+          <RowItem wide>
+            <h3>{t("dashboard-page:kyc-limit.text-1")}</h3>
+          </RowItem>
+          <RowItem>
+            <Link to={linkCreator(KYC_ROUTE, KYC_ROUTE)}>
+              <GVButton color="primary">
+                {t("dashboard-page:kyc-limit.text-2")}
+              </GVButton>
+            </Link>
+          </RowItem>
         </Row>
         <Row>
-          <ul>
-            <li>{t("Deposit and withdraw crypto without limits")}</li>
-            <li>{t("Exchange cryptocurrencies")}</li>
-            <li>{t("Create crypto trading accounts")}</li>
-            <li>
-              {t(
-                "Create Programs (with a maximum investment amount equivalent to $1,000) and Funds"
-              )}
-            </li>
-            <li>
-              {t(
-                "Bind your external trading account and use copy trading functionality (Follow)"
-              )}
-            </li>
-            <li>{t("Invest in Programs and Funds up to $1,000 in total")}</li>
-          </ul>
+          <Ul>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-3")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-4")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-5")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-6")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-7")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-8")}</Text>
+            </Li>
+          </Ul>
+        </Row>
+        <Row size={"xlarge"}>
+          <h3>{t("dashboard-page:kyc-limit.text-9")}</h3>
         </Row>
         <Row>
-          <h3>{t("After you pass KYC you also can")}</h3>
+          <Ul>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-10")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-11")}</Text>
+            </Li>
+            <Li>
+              <Text muted>{t("dashboard-page:kyc-limit.text-12")}</Text>
+            </Li>
+          </Ul>
         </Row>
-        <Row>
-          <ul>
-            <li>{t("Create Forex trading accounts")}</li>
-            <li>{t("Invest in Programs and Funds without limits")}</li>
-            <li>
-              {t("Attract investments in your Programs beyond the KYC limit")}
-            </li>
-          </ul>
-        </Row>
-        <Row>
-          {t(
-            "Note\n" +
-              'KYC is an authentication mechanism required in the financial industry to help ensure companies are compliant with anti money laundering regulations."'
-          )}
+        <Row size={"xlarge"}>
+          <Text muted>{t("dashboard-page:kyc-limit.text-13")}</Text>
         </Row>
       </DialogBottom>
     </Dialog>
