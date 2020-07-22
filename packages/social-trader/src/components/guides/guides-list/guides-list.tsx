c0136ba9974fe6906b8b057563c3c631914e7273
Anchello <bob.m@mail.ru>
2020-07-22 20:03:49 +0200

    created GuidesListItem and GuidesListHeader components

--- packages/social-trader/src/components/guides/guides-list/guides-list.tsx
+++ packages/social-trader/src/components/guides/guides-list/guides-list.tsx
@@ -1,10 +1,6 @@
-import clsx from "clsx";
-import ImageBaseElement from "components/avatar/image-base.element";
-import Link from "components/link/link";
-import { useToLink } from "components/link/link.helper";
+import GuidesListHeader from "components/guides/guides-list/guides-list-header";
+import GuidesListItem from "components/guides/guides-list/guides-list-item";
 import { Guide } from "gv-api-web";
-import Ok from "media/ok.svg";
-import { GUIDES_TOTAL_PAGE_ROUTE } from "pages/guides/guides.paths";
 import React, { useCallback, useEffect, useState } from "react";
 
 import styles from "./guides-list.module.scss";
@@ -16,9 +12,6 @@ interface Props {
 }
 
 const _GuidesList: React.FC<Props> = ({ name, guides, currentId }) => {
-  const counts = guides.length;
-  const countsText = counts === 1 ? `${counts} step` : `${counts} steps`;
-  const { linkCreator } = useToLink();
   const [isVisibleList, setIsVisibleList] = useState(false);
 
   const handleClickTitle = useCallback(() => {
@@ -30,39 +23,20 @@ const _GuidesList: React.FC<Props> = ({ name, guides, currentId }) => {
   }, [guides, currentId]);
   return (
     <>
-      <h3 className={styles["guides-list-subtitle"]} onClick={handleClickTitle}>
-        {name}
-        {!isVisibleList && (
-          <span className={styles["guides-list-subtitle__number"]}>
-            {countsText}
-          </span>
-        )}
-      </h3>
+      <GuidesListHeader
+        name={name}
+        guidesTotal={guides.length}
+        onClick={handleClickTitle}
+        isVisibleList={isVisibleList}
+      />
       {isVisibleList && (
         <ul className={styles["guides-list"]}>
           {guides.map(guide => (
-            <li key={guide.id} className={styles["guides-list__item"]}>
-              <Link
-                className={clsx(styles["guides-list__item-link"], {
-                  [styles["guides-list__item-link--active"]]:
-                    guide.id === currentId,
-                  [styles["guides-list__item-link--done"]]: guide.isPassed,
-                  [styles["guides-list__item-link--disabled"]]: guide.isDisabled
-                })}
-                to={linkCreator(
-                  `${GUIDES_TOTAL_PAGE_ROUTE}#${guide.canonicalName}`
-                )}
-              >
-                {guide.name}
-                {guide.isPassed && (
-                  <ImageBaseElement
-                    className={styles["guides-list__item-icon"]}
-                    src={Ok}
-                    alt="Done"
-                  />
-                )}
-              </Link>
-            </li>
+            <GuidesListItem
+              key={guide.id}
+              guide={guide}
+              currentGuideId={currentId}
+            />
           ))}
         </ul>
       )}
