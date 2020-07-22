19fd84f05a89a1ee11c42068dfe1df85d8b3ff4a
Anchello <bob.m@mail.ru>
2020-07-22 19:17:47 +0200

    created guides helpers and services

---
+++ packages/social-trader/src/components/guides/services/guides.services.tsx
@@ -0,0 +1,9 @@
+import { api } from "services/api-client/swagger-custom-client";
+
+export const passGuide = (id?: string) =>
+  api
+    .guides()
+    .passGuide({
+      id
+    })
+    .then(response => response);
