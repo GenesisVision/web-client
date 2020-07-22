3f6a81107b1eff75ca3dc7bb189db8028847cf7d
Anchello <bob.m@mail.ru>
2020-07-22 19:15:43 +0200

    updated GuidesNav component

--- packages/social-trader/src/components/guides/guides-nav/guides-nav.tsx
+++ packages/social-trader/src/components/guides/guides-nav/guides-nav.tsx
@@ -1,20 +1,20 @@
 import DetailsBlock from "components/details/details-block";
-import GuidesList from "components/guides/guides-list";
+import GuidesList from "components/guides/guides-list/guides-list";
 import { GuidesCategory } from "gv-api-web";
 import React from "react";
 
-import styles from "./guides.container.module.scss";
+import styles from "./guides-nav.module.scss";
 
 interface Props {
   navGuides: GuidesCategory[];
   currentId?: string;
 }
 
-const _GuidesNavList: React.FC<Props> = ({ navGuides, currentId }) => (
-  <nav className={styles["guides-container__nav"]}>
+const _GuidesNav: React.FC<Props> = ({ navGuides, currentId }) => (
+  <nav className={styles["guides-nav"]}>
     {navGuides.map(navGuide => (
       <DetailsBlock
-        className={styles["guides-container__nav-item"]}
+        className={styles["guides-nav__item"]}
         horizontalPaddings
         tablet
         key={navGuide.name}
@@ -29,5 +29,5 @@ const _GuidesNavList: React.FC<Props> = ({ navGuides, currentId }) => (
   </nav>
 );
 
-const GuidesNavList = React.memo(_GuidesNavList);
-export default GuidesNavList;
+const GuidesNav = React.memo(_GuidesNav);
+export default GuidesNav;
