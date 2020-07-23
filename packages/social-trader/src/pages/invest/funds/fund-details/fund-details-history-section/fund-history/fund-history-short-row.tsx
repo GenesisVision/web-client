a80e513aac7b8ed5f9c69f3f8a9bd827b3523249
pavelzubov <pasha224@yandex.ru>
2020-07-23 11:51:35 +0300

    Update fund history row markup

--- packages/social-trader/src/pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-short-row.tsx
+++ packages/social-trader/src/pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-short-row.tsx
@@ -1,3 +1,6 @@
+import { Center } from "components/center/center";
+import ShortArrow from "components/icon/short-arrow/short-arrow";
+import { RowItem } from "components/row-item/row-item";
 import TableCell from "components/table/components/table-cell";
 import TableRow from "components/table/components/table-row";
 import { FundHistoryDescription } from "pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-description";
@@ -8,22 +11,30 @@ import { formatDate } from "utils/dates";
 import styles from "./fund-history.module.scss";
 
 interface Props {
+  isOpen: boolean;
   setOpen: VoidFunction;
   item: IFundHistoryDataItem;
 }
 
-const _FundHistoryShortRow: React.FC<Props> = ({ setOpen, item }) => {
+const _FundHistoryShortRow: React.FC<Props> = ({ isOpen, setOpen, item }) => {
   return (
     <TableRow onClick={setOpen} className={styles["fund-history__short-row"]}>
       <TableCell>{formatDate(item.date)}</TableCell>
       <TableCell>{item.type}</TableCell>
       <TableCell>
-        <FundHistoryDescription
-          amount={item.amount}
-          name={item.asset.name}
-          type={item.type}
-          reallocateAssets={item.reallocateAssets}
-        />
+        <Center>
+          <RowItem wide>
+            <FundHistoryDescription
+              amount={item.amount}
+              name={item.asset.name}
+              type={item.type}
+              reallocateAssets={item.reallocateAssets}
+            />
+          </RowItem>
+          <RowItem className={styles["fund-history__expand-icon"]}>
+            <ShortArrow direction={isOpen ? "top" : "bottom"} />
+          </RowItem>
+        </Center>
       </TableCell>
     </TableRow>
   );
