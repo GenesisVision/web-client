b74c875e7feafb7a4a894aacff076c2955d1d8e5
pavelzubov <pasha224@yandex.ru>
2020-07-23 17:09:07 +0300

    Add icon to FundHistoryShortRow

--- packages/social-trader/src/pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-short-row.tsx
+++ packages/social-trader/src/pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-short-row.tsx
@@ -1,8 +1,10 @@
+import ImageBase from "components/avatar/image-base";
 import { Center } from "components/center/center";
 import ShortArrow from "components/icon/short-arrow/short-arrow";
 import { RowItem } from "components/row-item/row-item";
 import TableCell from "components/table/components/table-cell";
 import TableRow from "components/table/components/table-row";
+import SocialLink from "media/social-link.svg";
 import { FundHistoryDescription } from "pages/invest/funds/fund-details/fund-details-history-section/fund-history/fund-history-description";
 import { IFundHistoryDataItem } from "pages/invest/funds/fund-details/fund-details.types";
 import React from "react";
@@ -20,7 +22,21 @@ const _FundHistoryShortRow: React.FC<Props> = ({ isOpen, setOpen, item }) => {
   return (
     <TableRow onClick={setOpen} className={styles["fund-history__short-row"]}>
       <TableCell>{formatDate(item.date)}</TableCell>
-      <TableCell>{item.type}</TableCell>
+      <TableCell>
+        <Center>
+          <RowItem
+            size={"small"}
+            className={styles["fund-history__event-logo-icon"]}
+          >
+            <ImageBase
+              src={item.logoUrl}
+              alt={item.type}
+              defaultImage={SocialLink}
+            />
+          </RowItem>
+          <RowItem> {item.type}</RowItem>
+        </Center>
+      </TableCell>
       <TableCell>
         <Center>
           <RowItem wide>
