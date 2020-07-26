82061b8f99beb862b48d10bed6b1c1ca7a6de661
pavelzubov <pasha224@yandex.ru>
2020-07-26 15:40:53 +0300

    Change translate HOC to hook in level-tooltip.tsx

--- packages/social-trader/src/components/level-tooltip/level-tooltip.tsx
+++ packages/social-trader/src/components/level-tooltip/level-tooltip.tsx
@@ -3,31 +3,34 @@ import { PopoverContentCardBlock } from "components/popover/popover-card.block";
 import { PopoverContent } from "components/popover/popover-content";
 import { Text } from "components/text/text";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import styles from "./level-tooltip.module.scss";
 
-const _LevelTooltip: React.FC<ILevelTooltip> = ({ t, level, canLevelUp }) => (
-  <PopoverContent>
-    <PopoverContentCardBlock>
-      <div className={styles["level-tooltip__level"]}>
-        {t("level-tooltip.genesis-level")} {level}
-      </div>
-      {canLevelUp && (
-        <LabeledValue label={t("level-tooltip.level-up")}>
-          <Text weight={"bold"} size={"large"}>
-            {t("level-tooltip.top10")}
-          </Text>
-        </LabeledValue>
-      )}
-    </PopoverContentCardBlock>
-  </PopoverContent>
-);
+const _LevelTooltip: React.FC<ILevelTooltip> = ({ level, canLevelUp }) => {
+  const [t] = useTranslation();
+  return (
+    <PopoverContent>
+      <PopoverContentCardBlock>
+        <div className={styles["level-tooltip__level"]}>
+          {t("level-tooltip.genesis-level")} {level}
+        </div>
+        {canLevelUp && (
+          <LabeledValue label={t("level-tooltip.level-up")}>
+            <Text weight={"bold"} size={"large"}>
+              {t("level-tooltip.top10")}
+            </Text>
+          </LabeledValue>
+        )}
+      </PopoverContentCardBlock>
+    </PopoverContent>
+  );
+};
 
-const LevelTooltip = translate()(React.memo(_LevelTooltip));
+const LevelTooltip = React.memo(_LevelTooltip);
 export default LevelTooltip;
 
-export interface ILevelTooltip extends WithTranslation {
+export interface ILevelTooltip {
   level: number;
   canLevelUp: boolean;
 }
