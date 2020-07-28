d3473fa4608880fc936d32fb6a28d450832ca1ff
pavelzubov <pasha224@yandex.ru>
2020-07-28 20:49:07 +0300

    Change translation HOC to hook in FundNotificationPage

--- packages/social-trader/src/components/notifications/fund-settings.page.tsx
+++ packages/social-trader/src/components/notifications/fund-settings.page.tsx
@@ -1,19 +1,20 @@
 import Page from "components/page/page";
 import FundNotificationsContainer from "modules/fund-notifications/fund-notifications-container";
 import * as React from "react";
-import { WithTranslation, withTranslation as translate } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-const _FundNotificationPage: React.FC<Props> = ({ t, id }) => (
-  <Page showTitle title={t("notifications-page:fund.title")}>
-    <FundNotificationsContainer id={id} />
-  </Page>
-);
-
-interface Props extends WithTranslation, OwnProps {}
-
-interface OwnProps {
+interface Props {
   id: string;
 }
 
-const FundNotificationPage = translate()(React.memo(_FundNotificationPage));
+const _FundNotificationPage: React.FC<Props> = ({ id }) => {
+  const [t] = useTranslation();
+  return (
+    <Page showTitle title={t("notifications-page:fund.title")}>
+      <FundNotificationsContainer id={id} />
+    </Page>
+  );
+};
+
+const FundNotificationPage = React.memo(_FundNotificationPage);
 export default FundNotificationPage;
