f15e131293c32ac44c26a18b2595ef37ea2fb1a4
pavelzubov <pasha224@yandex.ru>
2020-08-09 13:49:35 +0300

    Use styled-components in HelpButton

--- packages/social-trader/src/components/help-button/help-button.tsx
+++ packages/social-trader/src/components/help-button/help-button.tsx
@@ -1,25 +1,62 @@
-import clsx from "clsx";
+import {
+  $iconColor,
+  $labelColor
+} from "components/gv-styles/gv-colors/gv-colors";
+import { withStyles } from "decorators/withStyles";
 import * as React from "react";
+import { css } from "styled-components";
 import { OptionalClickable } from "utils/types";
-
-import styles from "./help-button.module.scss";
-
-const HelpButton: React.FC<Props> = ({ muted, className, onClick }) => {
-  return (
-    <div
-      className={clsx(styles["help-button"], className, {
-        [styles["help-button--muted"]]: muted
-      })}
-      onClick={onClick}
-    >
-      ?
-    </div>
-  );
-};
-
-export default HelpButton;
 
 interface Props extends OptionalClickable {
   muted?: boolean;
   className?: string;
 }
+
+const styleTable = {
+  background: "none",
+  padding: "0",
+  "box-sizing": "border-box",
+  width: {
+    value: "15",
+    unit: "px"
+  },
+  height: {
+    value: "15",
+    unit: "px"
+  },
+  border: `1px solid ${$iconColor}`,
+  "border-radius": {
+    value: "50",
+    unit: "%"
+  },
+  "line-height": {
+    value: "15",
+    unit: "px"
+  },
+  "text-align": "center",
+  "font-size": {
+    value: "8",
+    unit: "px"
+  },
+  "font-weight": "700",
+  outline: "none",
+  cursor: "pointer"
+};
+
+const additionalStyles = css`
+  border-color: ${({ muted }: Props) => (muted ? $labelColor : $iconColor)};
+  color: ${({ muted }: Props) => (muted ? $labelColor : $iconColor)};
+`;
+
+const HelpButton: React.FC<Props> = ({ muted, className, onClick }) => {
+  return (
+    <div className={className} onClick={onClick}>
+      ?
+    </div>
+  );
+};
+
+export default withStyles({
+  styleTable,
+  additionalStyles
+})(HelpButton);
