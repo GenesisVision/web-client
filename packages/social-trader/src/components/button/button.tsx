14c789aa21857b93b84e510e6f39a0c2baafcc50
pavelzubov <pasha224@yandex.ru>
2020-08-09 18:01:33 +0300

    Add Label, SuccessMark, Button in button.tsx

---
+++ packages/social-trader/src/components/button/button.tsx
@@ -0,0 +1,58 @@
+import {
+  LabelAdditionalStyles,
+  SuccessMarkAdditionalStyles,
+  SuccessMarkStyles
+} from "components/button/button.styles";
+import {
+  IButtonProps,
+  ILabelProps,
+  ISuccessMarkProps
+} from "components/button/button.types";
+import { withStyles } from "decorators/withStyles";
+import React from "react";
+
+const _Label: React.FC<ILabelProps> = ({ className, children }) => {
+  return <span className={className}>{children}</span>;
+};
+
+const Label = withStyles<ILabelProps>({
+  additionalStyles: LabelAdditionalStyles
+})(_Label);
+
+const _SuccessMark: React.FC<ISuccessMarkProps> = ({ className }) => {
+  return <span className={className}>✔</span>;
+};
+const SuccessMark = withStyles<ISuccessMarkProps>({
+  styleTable: SuccessMarkStyles,
+  additionalStyles: SuccessMarkAdditionalStyles
+})(_SuccessMark);
+
+const _Button: React.FC<IButtonProps> = ({
+  children,
+  testId,
+  id,
+  disabled,
+  className,
+  onClick,
+  title,
+  type,
+  name
+}) => {
+  return (
+    <button
+      data-test-id={testId}
+      id={id}
+      disabled={disabled}
+      className={className}
+      onClick={onClick}
+      title={title}
+      type={type}
+      name={name}
+    >
+      <Label>{children}</Label>
+      <SuccessMark />
+    </button>
+  );
+};
+
+export const Button = React.memo(_Button);
