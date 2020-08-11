a19ff36de5279fd06bfeb9f411cfe3352f308877
pavelzubov <pasha224@yandex.ru>
2020-08-11 13:56:31 +0300

    Create row-item.style.ts, add styles

---
+++ packages/social-trader/src/components/row-item/row-item.style.ts
@@ -0,0 +1,40 @@
+import {
+  $paddingMedium,
+  $paddingSmall,
+  $paddingXsmall,
+  $paddingXxsmall,
+  $paddingXxxsmall
+} from "components/gv-styles/gv-sizes";
+import { IRowItemProps } from "components/row-item/row-item.types";
+import { css } from "styled-components";
+import { adaptiveMargin } from "utils/style/style-mixins";
+
+export const RowItemStaticStyles = {
+  "box-sizing": "border-box"
+};
+
+const getOffset = (size: number, bottom: boolean) => {
+  return `${adaptiveMargin("right", `${size}px`)}${
+    bottom ? adaptiveMargin("bottom", `${size}px`) : ""
+  }`;
+};
+
+export const RowItemDynamicStyles = css`
+  cursor: ${({ onClick }: IRowItemProps) => (onClick ? "pointer" : "default")};
+  display: ${({ hide }: IRowItemProps) => (hide ? "none" : "block")};
+  width: ${({ wide }: IRowItemProps) => (wide ? "100%" : "auto")};
+  ${({ size, bottomOffset }: IRowItemProps) => {
+    switch (size) {
+      case "xsmall":
+        return getOffset($paddingXxxsmall, !!bottomOffset);
+      case "small":
+        return getOffset($paddingXxsmall, !!bottomOffset);
+      case "middle":
+        return getOffset($paddingXsmall, !!bottomOffset);
+      case "large":
+        return getOffset($paddingSmall, !!bottomOffset);
+      case "xlarge":
+        return getOffset($paddingMedium, !!bottomOffset);
+    }
+  }}
+`;
