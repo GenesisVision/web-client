f47798c99697e578994ab8810b90fd72e6a34e38
pavelzubov <pasha224@yandex.ru>
2020-08-11 13:55:25 +0300

    Create gv-media.ts, add media breakpoints

---
+++ packages/social-trader/src/components/gv-styles/gv-media.ts
@@ -0,0 +1,39 @@
+import { css } from "styled-components";
+import {
+  DESKTOP,
+  LARGE_DESKTOP,
+  PHONE,
+  PHONE_LANDSCAPE,
+  TABLET,
+  TABLET_LANDSCAPE
+} from "utils/breakpoints";
+
+const createMediaQueryCSS = (content: string, breakpoint: number) => css`
+  @media (min-width: ${breakpoint}) {
+    ${content}
+  }
+`;
+
+export const mediaBreakpointPhone = (content: string) => {
+  return createMediaQueryCSS(content, PHONE);
+};
+
+export const mediaBreakpointLandscapePhone = (content: string) => {
+  return createMediaQueryCSS(content, PHONE_LANDSCAPE);
+};
+
+export const mediaBreakpointTablet = (content: string) => {
+  return createMediaQueryCSS(content, TABLET);
+};
+
+export const mediaBreakpointLandscapeTablet = (content: string) => {
+  return createMediaQueryCSS(content, TABLET_LANDSCAPE);
+};
+
+export const mediaBreakpointDesktop = (content: string) => {
+  return createMediaQueryCSS(content, DESKTOP);
+};
+
+export const mediaBreakpointLargeDesktop = (content: string) => {
+  return createMediaQueryCSS(content, LARGE_DESKTOP);
+};
