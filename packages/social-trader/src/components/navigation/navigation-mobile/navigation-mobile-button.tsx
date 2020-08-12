151868e72af82c19220e7558495eb16df1eee04f
pavelzubov <pasha224@yandex.ru>
2020-08-12 15:00:03 +0300

    Use styled-components in NavigationMobileButton

--- packages/social-trader/src/components/navigation/navigation-mobile/navigation-mobile-button.tsx
+++ packages/social-trader/src/components/navigation/navigation-mobile/navigation-mobile-button.tsx
@@ -1,5 +1,9 @@
+import { mediaBreakpointLandscapeTablet } from "components/gv-styles/gv-media";
+import { $paddingMedium, $walletItemSize } from "components/gv-styles/gv-sizes";
+import { $boxShadow4 } from "components/gv-styles/gv-style-constants";
 import { Icon } from "components/icon/icon";
 import NavigationMobileContainer from "components/navigation/navigation-mobile/navigation-mobile.container";
+import { withStyles } from "decorators/withStyles";
 import { ProfileHeaderViewModel } from "gv-api-web";
 import useIsOpen from "hooks/is-open.hook";
 import { logout } from "pages/auth/signin/signin.service";
@@ -7,10 +11,38 @@ import * as React from "react";
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 import { TMenuItem } from "routes/menu";
+import { css } from "styled-components";
+import { adaptiveMargin } from "utils/style/style-mixins";
 
-import styles from "../navigation.module.scss";
+interface Props {
+  className?: string;
+  mobileMenuItems: TMenuItem[];
+  backPath: string;
+  isAuthenticated: boolean;
+  profileHeader?: ProfileHeaderViewModel;
+}
+
+const dynamicStyles = css`
+  padding: 0;
+  min-height: ${$walletItemSize}px;
+  min-width: ${$walletItemSize}px;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.1);
+  cursor: pointer;
+  ${adaptiveMargin("right", $paddingMedium)}
+
+  &:hover {
+    box-shadow: ${$boxShadow4};
+  }
+
+  ${mediaBreakpointLandscapeTablet("display: none !important;")}
+`;
 
 const _NavigationMobileButton: React.FC<Props> = ({
+  className,
   mobileMenuItems,
   isAuthenticated,
   profileHeader,
@@ -21,7 +53,7 @@ const _NavigationMobileButton: React.FC<Props> = ({
   const handlerLogout = useCallback(() => dispatch(logout), []);
   return (
     <>
-      <div className={styles["navigation__menu"]} onClick={setOpen}>
+      <div className={className} onClick={setOpen}>
         <Icon type="menu" />
       </div>
       <NavigationMobileContainer
@@ -37,12 +69,7 @@ const _NavigationMobileButton: React.FC<Props> = ({
   );
 };
 
-interface Props {
-  mobileMenuItems: TMenuItem[];
-  backPath: string;
-  isAuthenticated: boolean;
-  profileHeader?: ProfileHeaderViewModel;
-}
-
-const NavigationMobileButton = React.memo(_NavigationMobileButton);
+const NavigationMobileButton = withStyles<Props>({ dynamicStyles })(
+  React.memo(_NavigationMobileButton)
+);
 export default NavigationMobileButton;
