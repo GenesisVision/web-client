f7858d25020686a997b4bf89d59701daad7795ca
pavelzubov <pasha224@yandex.ru>
2020-08-14 18:40:46 +0300

    Use styled components in GVProgramPeriod

--- packages/social-trader/src/components/gv-program-period/index.tsx
+++ packages/social-trader/src/components/gv-program-period/index.tsx
@@ -1,41 +1,62 @@
-import clsx from "clsx";
+import { $primaryColor } from "components/gv-styles/gv-colors/gv-colors";
 import { calcPercent } from "components/pie-container/pie.helpers";
+import { withStyles } from "decorators/withStyles";
 import React from "react";
-
-import styles from "./style.module.scss";
+import styled, { css } from "styled-components";
 
 interface GVProgramPeriodProps {
+  className?: string;
+  bgColor?: string;
   start: Date | number;
   end: Date | number;
   value: Date | number;
   variant?: "pie" | "line";
-  className?: string;
-  valueClassName?: string;
 }
 
 const calcDash = (percent: number) => `${percent} ${100 - percent}`;
 
+interface IValueProps {
+  valuePercent: number;
+}
+const Value = styled.div<IValueProps>`
+  width: ${({ valuePercent }: IValueProps) => valuePercent}%;
+  position: absolute;
+  top: -1px;
+  height: 4px;
+  background-color: ${$primaryColor};
+  border-radius: 0.25rem;
+`;
+
+interface ILineProps {
+  bgColor?: string;
+}
+const Line = styled.div<ILineProps>`
+  display: block;
+  height: 2px;
+  background-color: ${({ bgColor }: ILineProps) => bgColor || "#2a353f"};
+  border-radius: 0.25rem;
+  width: 100%;
+  position: relative;
+`;
+
+const GVProgramPeriodStyles = css`
+  display: block;
+  width: 20px;
+  height: 20px;
+`;
+
 const GVProgramPeriod: React.FC<GVProgramPeriodProps> = ({
+  className,
+  bgColor,
   start,
   end,
   value,
-  variant,
-  className,
-  valueClassName
+  variant = "pie"
 }) => {
   const valuePercent = calcPercent(value, start, end);
   if (variant === "pie")
     return (
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 42 42"
-        className={clsx(
-          styles["gv-program-period"],
-          styles["gv-program-period--pie"],
-          className
-        )}
-      >
+      <svg width="100%" height="100%" viewBox="0 0 42 42" className={className}>
         <circle
           cx="21"
           cy="21"
@@ -59,23 +80,12 @@ const GVProgramPeriod: React.FC<GVProgramPeriodProps> = ({
     );
   else
     return (
-      <div
-        className={clsx(
-          styles["gv-program-period"],
-          styles["gv-program-period--line"],
-          className
-        )}
-      >
-        <div
-          className={clsx(styles["gv-program-period__value"], valueClassName)}
-          style={{ width: `${valuePercent}%` }}
-        />
-      </div>
+      <Line bgColor={bgColor}>
+        <Value valuePercent={valuePercent} />
+      </Line>
     );
 };
 
-GVProgramPeriod.defaultProps = {
-  variant: "pie"
-};
-
-export default GVProgramPeriod;
+export default withStyles<GVProgramPeriodProps>({
+  dynamicStyles: GVProgramPeriodStyles
+})(GVProgramPeriod);
