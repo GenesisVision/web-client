7041ee229920eee9b5ada5d6607558fd593f1869
pavelzubov <pasha224@yandex.ru>
2020-08-14 18:38:47 +0300

    Use styled-components in Text

---
+++ packages/social-trader/src/components/text/text.styles.ts
@@ -0,0 +1,66 @@
+import {
+  $labelColor,
+  $negativeColor,
+  $positiveColor,
+  $textLightColor,
+  $yellow
+} from "components/gv-styles/gv-colors/gv-colors";
+import {
+  $fontSizeCommon,
+  $fontSizeH2,
+  $fontSizeH4,
+  $fontSizeSmall,
+  $fontSizeXsmall
+} from "components/gv-styles/gv-sizes";
+import { ITextProps } from "components/text/text.types";
+import { css } from "styled-components";
+import { fontSize } from "utils/style/style-mixins";
+
+export const dynamicTextStyles = css`
+  ${({ wrap = true, preWrap }: ITextProps) =>
+    wrap && !preWrap && "white-space: normal;"};
+  white-space: ${({ preWrap }: ITextProps) =>
+    preWrap ? "pre-wrap" : "nowrap"};
+  font-weight: ${({ weight = "normal" }: ITextProps) => {
+    switch (weight) {
+      case "thin":
+        return 200;
+      case "normal":
+        return 400;
+      case "bold":
+        return 600;
+      case "bolder":
+        return 800;
+    }
+  }};
+  font-size: ${({ size = "middle", sizeValue }: ITextProps) => {
+    if (sizeValue) return sizeValue;
+    switch (size) {
+      case "xsmall":
+        return fontSize($fontSizeXsmall);
+      case "small":
+        return fontSize($fontSizeSmall);
+      case "middle":
+        return fontSize($fontSizeCommon);
+      case "large":
+        return fontSize($fontSizeH4);
+      case "xlarge":
+        return fontSize($fontSizeH2);
+    }
+  }};
+  color: ${({ color, muted }: ITextProps) => {
+    if (muted) return $labelColor;
+    switch (color) {
+      case "white":
+        return $textLightColor;
+      case "#00ff00":
+      case "green":
+        return $positiveColor;
+      case "red":
+      case "#ff0000":
+        return $negativeColor;
+      case "yellow":
+        return $yellow;
+    }
+  }};
+`;
