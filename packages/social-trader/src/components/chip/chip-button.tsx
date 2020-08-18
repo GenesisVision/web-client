bf542e502b005020bd21b25891a2edca44c28cb5
pavelzubov <pasha224@yandex.ru>
2020-08-18 17:10:58 +0300

    Use styled-components in Chip, ChipButton

--- packages/social-trader/src/components/chip/chip-button.tsx
+++ packages/social-trader/src/components/chip/chip-button.tsx
@@ -1,10 +1,10 @@
-import clsx from "clsx";
 import Chip, { CHIP_TYPE } from "components/chip/chip";
+import { $primaryColor } from "components/gv-styles/gv-colors/gv-colors";
+import useFlag from "hooks/flag.hook";
 import * as React from "react";
 import { ReactNode } from "react";
+import styled from "styled-components";
 import { Clickable, Sizeable } from "utils/types";
-
-import styles from "./chip.module.scss";
 
 interface Props extends Sizeable, Clickable {
   stretch?: boolean;
@@ -16,6 +16,25 @@ interface Props extends Sizeable, Clickable {
   rounded?: boolean;
   type?: CHIP_TYPE;
 }
+
+const Label = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 24px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  &:hover {
+    svg [stroke] {
+      stroke: ${$primaryColor};
+    }
+  }
+  ${({ reverseOrder }: { reverseOrder?: boolean }) =>
+    reverseOrder && `flex-direction: row-reverse;`}
+`;
 
 const ChipButton: React.FC<Props> = React.memo(
   ({
@@ -30,25 +49,30 @@ const ChipButton: React.FC<Props> = React.memo(
     disabled,
     label,
     chipLabel
-  }) => (
-    <div
-      onClick={onClick}
-      className={clsx(styles["chip-button"], {
-        [styles["chip-button--reversed"]]: reverseOrder
-      })}
-    >
-      <Chip
-        type={type}
-        disabled={disabled}
-        rounded={rounded}
-        size={size}
-        stretch={stretch}
+  }) => {
+    const [over, setOver, setLeave] = useFlag();
+    return (
+      <Container
+        reverseOrder={reverseOrder}
+        onClick={onClick}
+        onMouseEnter={setOver}
+        onMouseLeave={setLeave}
       >
-        {chipLabel}
-      </Chip>
-      {label && <div className={styles["chip-button__label"]}>{label}</div>}
-    </div>
-  )
+        <Chip
+          pointer
+          hover={over}
+          type={type}
+          disabled={disabled}
+          rounded={rounded}
+          size={size}
+          stretch={stretch}
+        >
+          {chipLabel}
+        </Chip>
+        {label && <Label>{label}</Label>}
+      </Container>
+    );
+  }
 );
 
 export default ChipButton;
