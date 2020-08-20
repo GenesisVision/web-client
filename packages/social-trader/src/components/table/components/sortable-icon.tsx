d30fd2e4bea603fc0fc77043be32b59724f4c6a6
pavelzubov <pasha224@yandex.ru>
2020-08-20 16:15:03 +0300

    Create SortableIcon component

---
+++ packages/social-trader/src/components/table/components/sortable-icon.tsx
@@ -0,0 +1,66 @@
+import { SORTING_DIRECTION } from "components/table/helpers/sorting.helpers";
+import { withStyles } from "decorators/withStyles";
+import React from "react";
+import { css } from "styled-components";
+import { transition } from "utils/style/style-mixins";
+
+interface Props {
+  className?: string;
+  sortingDirection: SORTING_DIRECTION;
+}
+
+const sortableBorderIcon = css`
+  position: absolute;
+  right: -15px;
+  bottom: 3px;
+  content: "";
+  border-top: 1px solid white;
+`;
+
+const styles = css<Props>`
+  cursor: pointer;
+  padding-right: 10px;
+  span {
+    opacity: 1;
+  }
+  position: relative;
+  &::before {
+    ${transition("width")};
+    ${sortableBorderIcon};
+    height: 4px;
+    width: ${({ sortingDirection }) => {
+      switch (sortingDirection) {
+        case SORTING_DIRECTION.ASC:
+          return "5px";
+        case SORTING_DIRECTION.DESC:
+          return "10px";
+        case SORTING_DIRECTION.NONE:
+        default:
+          return "0";
+      }
+    }};
+  }
+  &::after {
+    ${transition("width")};
+    ${sortableBorderIcon};
+    width: ${({ sortingDirection }) => {
+      switch (sortingDirection) {
+        case SORTING_DIRECTION.ASC:
+          return "10px";
+        case SORTING_DIRECTION.DESC:
+          return "5px";
+        case SORTING_DIRECTION.NONE:
+        default:
+          return "0";
+      }
+    }};
+  }
+`;
+
+const _SortableIcon: React.FC<Props> = ({ children, className }) => {
+  return <span className={className}>{children}</span>;
+};
+
+export const SortableIcon = withStyles<Props>({ dynamicStyles: styles })(
+  _SortableIcon
+);
