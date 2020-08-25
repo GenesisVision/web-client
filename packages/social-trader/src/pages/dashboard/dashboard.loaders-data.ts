13b8a2af6a0ab4cc84801da5281a447e75be46d0
pavelzubov <pasha224@yandex.ru>
2020-08-25 18:34:24 +0300

    Update investmentEventViewModelCreator, getEventLoaderData

--- packages/social-trader/src/pages/dashboard/dashboard.loaders-data.ts
+++ packages/social-trader/src/pages/dashboard/dashboard.loaders-data.ts
@@ -112,7 +112,14 @@ const getEventLoaderData = (): TDashboardEvent => ({
     title: "",
     url: "",
     assetType: (getRandomAsset() as unknown) as AssetType,
-    programDetails: { level: 0, levelProgress: 0 }
+    programDetails: {
+      successFeeSelected: 0,
+      successFeeCurrent: 0,
+      managementFeeSelected: 0,
+      managementFeeCurrent: 0,
+      level: 0,
+      levelProgress: 0
+    }
   },
   currency: "GVT",
   changeState: "NotChanged",
