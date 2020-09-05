b398d6728068a57579cc12bb74137c385676d196
pavelzubov <pasha224@yandex.ru>
2020-09-05 22:57:30 +0300

    Use styled-components in download button

--- packages/social-trader/src/components/icon/export-icon.tsx
+++ packages/social-trader/src/components/icon/export-icon.tsx
@@ -1,15 +1,14 @@
-import styles from "components/download-button/download-button.module.scss";
 import React from "react";
+import styled from "styled-components";
+
+const StyledSvg = styled.svg`
+  margin-left: 5px;
+`;
 
 export const ExportIcon: React.FC = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="14"
-    className={styles["download-icon"]}
-  >
+  <StyledSvg xmlns="http://www.w3.org/2000/svg" width="12" height="14">
     <g fill="none" fillRule="evenodd" stroke="#00BDAF" strokeWidth="2">
       <path d="M.5 13h11M6 10V3M2 5l4-3 4 3" />
     </g>
-  </svg>
+  </StyledSvg>
 );
