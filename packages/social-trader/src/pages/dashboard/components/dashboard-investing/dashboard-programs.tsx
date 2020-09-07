7ecac642e1b6f5ee64a185f431fbd3cd094516ed
pavelzubov <pasha224@yandex.ru>
2020-09-07 11:38:20 +0300

    Add sorting to dashboard investing tables

--- packages/social-trader/src/pages/dashboard/components/dashboard-investing/dashboard-programs.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-investing/dashboard-programs.tsx
@@ -1,4 +1,5 @@
 import { DataStorageContext } from "components/data-storage/data-storage";
+import { SortingColumn } from "components/table/components/filtering/filter.type";
 import { ProgramInvestingDetailsList } from "gv-api-web";
 import DashboardInvestingTable from "pages/dashboard/components/dashboard-investing/dashboard-investing-table";
 import DashboardProgramCard from "pages/dashboard/components/dashboard-investing/dashboard-program-card";
@@ -9,6 +10,15 @@ import {
 import { getInvestingPrograms } from "pages/dashboard/services/dashboard.service";
 import React, { useCallback, useContext } from "react";
 import { useTranslation } from "react-i18next";
+
+const INIT_SORTING = "value";
+
+const COLUMNS: SortingColumn[] = [
+  { name: "value", sortingName: "value" },
+  { name: "name", sortingName: "name" },
+  { name: "profit", sortingName: "profit" },
+  { name: "period", sortingName: "period" }
+];
 
 const _DashboardInvestingPrograms: React.FC = () => {
   const { updateData } = useContext(DataStorageContext);
@@ -22,6 +32,8 @@ const _DashboardInvestingPrograms: React.FC = () => {
   );
   return (
     <DashboardInvestingTable
+      columns={COLUMNS}
+      sorting={INIT_SORTING}
       filtering={DASHBOARD_INVESTMENTS_FILTERING}
       defaultFilters={DASHBOARD_INVESTMENTS_DEFAULT_FILTERS}
       getItemsFunc={getInvestingPrograms}
