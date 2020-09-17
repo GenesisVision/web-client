afcb48d2009d70a771a0fd307aa8811efb91dc9c
pavelzubov <pasha224@yandex.ru>
2020-09-17 11:58:38 +0300

    Update next.config.js for use preact

--- packages/social-trader/next.config.js
+++ packages/social-trader/next.config.js
@@ -27,7 +27,41 @@ function create(path) {
     publicRuntimeConfig: {
       apiUrl: process.env.REACT_APP_API_URL
     },
-    webpack(config) {
+    webpack(config, { dev, isServer }) {
+      // Move Preact into the framework chunk instead of duplicating in routes:
+      const splitChunks =
+        config.optimization && config.optimization.splitChunks;
+      if (splitChunks) {
+        const cacheGroups = splitChunks.cacheGroups;
+        const test = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+        if (cacheGroups.framework) {
+          cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+            test
+          });
+          // if you want to merge the 2 small commons+framework chunks:
+          // cacheGroups.commons.name = 'framework';
+        }
+      }
+
+      if (isServer) {
+        // mark `preact` stuffs as external for server bundle to prevent duplicate copies of preact
+        config.externals.push(
+          /^(preact|preact-render-to-string|preact-context-provider)([\\/]|$)/
+        );
+      }
+
+      // Automatically inject Preact DevTools:
+      if (dev && !isServer) {
+        const entry = config.entry;
+        config.entry = () =>
+          entry().then(entries => {
+            entries["main.js"] = ["preact/debug"].concat(
+              entries["main.js"] || []
+            );
+            return entries;
+          });
+      }
+
       config.resolve.alias = {
         ...config.resolve.alias,
         react: "preact/compat",
