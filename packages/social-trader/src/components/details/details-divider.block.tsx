e7f21baeaae82c8c49dfe8e74009f05e215bd50b
pavelzubov <pasha224@yandex.ru>
2020-09-18 12:01:04 +0300

    Correct DetailsDivider style

--- packages/social-trader/src/components/details/details-divider.block.tsx
+++ packages/social-trader/src/components/details/details-divider.block.tsx
@@ -1,12 +1,9 @@
 import { mediaBreakpointLandscapePhone } from "components/gv-styles/gv-media";
-import { $paddingBig } from "components/gv-styles/gv-sizes";
+import { $paddingBig, $paddingXsmall } from "components/gv-styles/gv-sizes";
 import * as React from "react";
 import styled from "styled-components";
 
-const StyledDiv = styled.div`
-  ${mediaBreakpointLandscapePhone(`padding-bottom: ${$paddingBig}px`)}
+export const DetailsDivider = styled.div`
+  padding-bottom: ${$paddingXsmall}px;
+  ${mediaBreakpointLandscapePhone(`padding-bottom: ${$paddingBig}px`)};
 `;
-
-export const DetailsDivider: React.FC = () => {
-  return <StyledDiv />;
-};
