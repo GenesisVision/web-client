67d9c3e6ba049c272f3f665f2e1c416f9bec8c7f
pavelzubov <pasha224@yandex.ru>
2020-09-24 14:15:39 +0300

    Move LazyHydrate to HomeSection in landing page

--- packages/social-trader/src/pages/landing-page/components/home/home.blocks.tsx
+++ packages/social-trader/src/pages/landing-page/components/home/home.blocks.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import React from "react";
+import LazyHydrate from "react-lazy-hydration";
 
 import styles from "./home.module.scss";
 
@@ -16,18 +17,20 @@ export const HomeSection: React.FC<HomeSectionProps> = ({
   hasPadding
 }) => {
   return (
-    <section
-      id={id}
-      className={clsx(styles["home__section"], {
-        [styles["home__section--bg-gray"]]: bgColor === "gray",
-        [styles["home__section--bg-white"]]: bgColor === "white",
-        [styles["home__section--first-screen"]]: isFirst,
-        [styles["home__section--last-screen"]]: isLast,
-        [styles["home__section--horizontal-padding"]]: hasPadding
-      })}
-    >
-      {children}
-    </section>
+    <LazyHydrate whenVisible>
+      <section
+        id={id}
+        className={clsx(styles["home__section"], {
+          [styles["home__section--bg-gray"]]: bgColor === "gray",
+          [styles["home__section--bg-white"]]: bgColor === "white",
+          [styles["home__section--first-screen"]]: isFirst,
+          [styles["home__section--last-screen"]]: isLast,
+          [styles["home__section--horizontal-padding"]]: hasPadding
+        })}
+      >
+        {children}
+      </section>
+    </LazyHydrate>
   );
 };
 
--- packages/social-trader/src/pages/landing-page/landing.page.tsx
+++ packages/social-trader/src/pages/landing-page/landing.page.tsx
@@ -27,7 +27,6 @@ import {
 } from "pages/landing-page/static-data/brokers";
 import { useUtm } from "pages/landing-page/utils";
 import React from "react";
-import LazyHydrate from "react-lazy-hydration";
 
 const _LandingPage: React.FC<Props> = ({
   cookieAccept,
@@ -51,55 +50,53 @@ const _LandingPage: React.FC<Props> = ({
             <FirstScreen news={news} />
           </HomeContainer>
         </HomeSection>
-        <LazyHydrate whenVisible>
-          <HomeSection bgColor="white">
-            <HomeContainer>
-              <EventsContainer events={events} />
-            </HomeContainer>
-          </HomeSection>
-          <HomeSection bgColor="gray">
-            <HomeContainer>
-              <FundsContainer funds={funds.items} />
-            </HomeContainer>
-          </HomeSection>
-          <section className="home__section">
-            <HomeContainer>
-              <ProgramsContainer programs={programs.items} />
-            </HomeContainer>
-          </section>
-          <HomeSection bgColor="gray">
-            <HomeContainer>
-              <FollowsContainer follows={follows.items} />
-            </HomeContainer>
-          </HomeSection>
-          <HomeSection id="info" bgColor="white">
-            <HomeContainer>
-              <InfoContainer />
-            </HomeContainer>
-          </HomeSection>
-          <HomeSection bgColor="gray">
-            <HomeContainer>
-              <DownloadContainer />
-            </HomeContainer>
-          </HomeSection>
-          <HomeSection bgColor="white">
-            <AdvantagesContainer />
-          </HomeSection>
-          <HomeSection bgColor="gray" hasPadding>
-            <HomeContainer>
-              <BrokersContainer
-                brokersInfo={brokersInfo}
-                brokersTabs={brokersTabs}
-                title={t("landing-page:brokers.title")}
-              />
-            </HomeContainer>
-          </HomeSection>
-          <HomeSection isLast>
-            <HomeContainer>
-              <SocialContainer />
-            </HomeContainer>
-          </HomeSection>
-        </LazyHydrate>
+        <HomeSection bgColor="white">
+          <HomeContainer>
+            <EventsContainer events={events} />
+          </HomeContainer>
+        </HomeSection>
+        <HomeSection bgColor="gray">
+          <HomeContainer>
+            <FundsContainer funds={funds.items} />
+          </HomeContainer>
+        </HomeSection>
+        <section className="home__section">
+          <HomeContainer>
+            <ProgramsContainer programs={programs.items} />
+          </HomeContainer>
+        </section>
+        <HomeSection bgColor="gray">
+          <HomeContainer>
+            <FollowsContainer follows={follows.items} />
+          </HomeContainer>
+        </HomeSection>
+        <HomeSection id="info" bgColor="white">
+          <HomeContainer>
+            <InfoContainer />
+          </HomeContainer>
+        </HomeSection>
+        <HomeSection bgColor="gray">
+          <HomeContainer>
+            <DownloadContainer />
+          </HomeContainer>
+        </HomeSection>
+        <HomeSection bgColor="white">
+          <AdvantagesContainer />
+        </HomeSection>
+        <HomeSection bgColor="gray" hasPadding>
+          <HomeContainer>
+            <BrokersContainer
+              brokersInfo={brokersInfo}
+              brokersTabs={brokersTabs}
+              title={t("landing-page:brokers.title")}
+            />
+          </HomeContainer>
+        </HomeSection>
+        <HomeSection isLast>
+          <HomeContainer>
+            <SocialContainer />
+          </HomeContainer>
+        </HomeSection>
       </main>
     </Layout>
   );
