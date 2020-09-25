90af89f4f21ffd9661c4f348fb9b79f55ac92a4f
pavelzubov <pasha224@yandex.ru>
2020-09-25 18:03:14 +0300

    Use styled-components in SocialPageFeedBlock

--- packages/social-trader/src/pages/social/social/social-page.styles.tsx
+++ packages/social-trader/src/pages/social/social/social-page.styles.tsx
@@ -5,6 +5,7 @@ import {
 import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
 import styled, { css } from "styled-components";
+import { transition } from "utils/style/style-mixins";
 
 const $desktopCenterWidth = "60%";
 const $desktopSideBlockWidth = `calc((100% - ${$desktopCenterWidth}) / 2)`;
@@ -46,4 +47,20 @@ export const SocialPageFeedContainer = styled(RowItem)`
   width: 100%;
   ${mediaBreakpointLandscapeTablet(`width: ${$tabletCenterWidth};`)}
   ${mediaBreakpointDesktop(`width: ${$tabletCenterWidth};`)}
+`;
+
+export const SocialPageTabsContainer = styled.div<{ open?: boolean }>`
+  overflow: hidden;
+  ${transition("width")};
+  width: ${({ open }) => (open ? "100%" : 0)};
+`;
+
+export const SocialPageSearchContainer = styled.div<{ open?: boolean }>`
+  height: 34px;
+  ${transition("width")};
+  width: ${({ open }) => (open ? "100%" : "40px")};
+`;
+
+export const SocialPageSearchButton = styled.div`
+  cursor: pointer;
 `;
