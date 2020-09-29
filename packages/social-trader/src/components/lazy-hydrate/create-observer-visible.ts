e9e83fbc29c73b9203626aa68f472c68811320b9
pavelzubov <pasha224@yandex.ru>
2020-09-29 11:37:11 +0300

    Create LazyHydrate component

---
+++ packages/social-trader/src/components/lazy-hydrate/create-observer-visible.ts
@@ -0,0 +1,36 @@
+import useFlag from "hooks/flag.hook";
+import { RefObject, useEffect } from "react";
+
+const isBrowser = typeof window !== "undefined";
+
+export const createUseObserverVisible = (
+  observerOptions: IntersectionObserverInit = {}
+) => (ref: RefObject<HTMLElement>) => {
+  const [isVisible, setIsVisible] = useFlag();
+
+  useEffect(() => {
+    if (!ref.current || isVisible) return;
+
+    if (!ref.current.childElementCount) {
+      setIsVisible();
+      return;
+    }
+
+    const io = new IntersectionObserver((entries, observer) => {
+      entries.forEach(({ isIntersecting }) => {
+        if (isIntersecting) {
+          observer.disconnect();
+          setIsVisible();
+        }
+      });
+    }, observerOptions);
+
+    io.observe(ref.current.children[0]);
+
+    return () => {
+      io.disconnect();
+    };
+  }, [ref]);
+
+  return isBrowser ? isVisible : true;
+};
