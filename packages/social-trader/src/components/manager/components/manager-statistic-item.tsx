9ba85aa96525bcab83452e64498dc84bfd1aa040
pavelzubov <pasha224@yandex.ru>
2020-10-02 14:53:51 +0300

    Use styled components in ManagerStatisticItem

--- packages/social-trader/src/components/manager/components/manager-statistic-item.tsx
+++ packages/social-trader/src/components/manager/components/manager-statistic-item.tsx
@@ -1,23 +1,26 @@
 import { Row } from "components/row/row";
 import React from "react";
+import styled from "styled-components";
 import { OptionalClickable } from "utils/types";
-
-import styles from "./manager-statistic-item.module.scss";
-
-const _ManagerStatisticItem: React.FC<Props> = ({ onClick, label, value }) => {
-  return (
-    <div className={styles["manager-statistic-item"]} onClick={onClick}>
-      <Row>
-        <h3>{label}</h3>
-      </Row>
-      <Row>{value}</Row>
-    </div>
-  );
-};
 
 interface Props extends OptionalClickable {
   label: string;
   value: string | number;
 }
+
+const Container = styled.div`
+  cursor: pointer;
+`;
+
+const _ManagerStatisticItem: React.FC<Props> = ({ onClick, label, value }) => {
+  return (
+    <Container onClick={onClick}>
+      <Row>
+        <h3>{label}</h3>
+      </Row>
+      <Row>{value}</Row>
+    </Container>
+  );
+};
 
 export const ManagerStatisticItem = React.memo(_ManagerStatisticItem);
