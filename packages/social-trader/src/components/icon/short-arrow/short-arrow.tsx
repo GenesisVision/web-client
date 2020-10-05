134e874d39cff4b83504af7bb09766928a00dc32
pavelzubov <pasha224@yandex.ru>
2020-10-05 12:48:50 +0300

    Use styled components in ShortArrow

--- packages/social-trader/src/components/icon/short-arrow/short-arrow.tsx
+++ packages/social-trader/src/components/icon/short-arrow/short-arrow.tsx
@@ -1,9 +1,7 @@
-import clsx from "clsx";
 import { ButtonIcon } from "components/button-icon/button-icon";
 import { ShortArrowSource } from "components/icon/short-arrow/short-arrow.source";
 import React from "react";
-
-import styles from "./short-arrow.module.scss";
+import styled from "styled-components";
 
 export type ShortArrowDirectionType = "top" | "right" | "bottom" | "left";
 
@@ -11,18 +9,28 @@ interface Props {
   direction?: ShortArrowDirectionType;
 }
 
+const StyledButtonIcon = styled(ButtonIcon)<Props>`
+  transform: rotate(
+    ${({ direction }) => {
+      switch (direction) {
+        case "top":
+          return "180deg";
+        case "right":
+          return "-90deg";
+        case "bottom":
+          return "0";
+        case "left":
+          return "90deg";
+      }
+    }}
+  );
+`;
+
 const ShortArrow: React.FC<Props> = ({ direction = "right" }) => {
   return (
-    <ButtonIcon
-      className={clsx({
-        [styles["short-arrow__top"]]: direction === "top",
-        [styles["short-arrow__right"]]: direction === "right",
-        [styles["short-arrow__bottom"]]: direction === "bottom",
-        [styles["short-arrow__left"]]: direction === "left"
-      })}
-    >
+    <StyledButtonIcon direction={direction}>
       <ShortArrowSource />
-    </ButtonIcon>
+    </StyledButtonIcon>
   );
 };
 
