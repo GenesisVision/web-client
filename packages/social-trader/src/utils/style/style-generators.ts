03e85a846bed5ab591eac8129b3b1c1e7a755af4
pavelzubov <pasha224@yandex.ru>
2020-10-10 14:28:42 +0300

    Correct getHEXA in style-generators.ts

--- packages/social-trader/src/utils/style/style-generators.ts
+++ packages/social-trader/src/utils/style/style-generators.ts
@@ -32,7 +32,9 @@ export type GetPropsStylesFuncType = (
 ) => GetPropsStylesFuncOutputType;
 
 export const getHEXA = (color: string, alpha: number): string => {
-  const alphaInHEX = (alpha * 255 + 0x10000).toString(16).substr(-2);
+  const alphaInHEX = (Math.round(alpha * 255) + 0x10000)
+    .toString(16)
+    .substr(-2);
   return color + alphaInHEX;
 };
 
