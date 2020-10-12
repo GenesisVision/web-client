75bcb7dcc31aba1ae433cfc9316c2cef7e2d2dc5
pavelzubov <pasha224@yandex.ru>
2020-10-12 09:22:53 +0300

    Use styled-components in Li

--- packages/social-trader/src/components/li/li.tsx
+++ packages/social-trader/src/components/li/li.tsx
@@ -1,19 +1,35 @@
-import clsx from "clsx";
+import {
+  $backgroundColor,
+  $primaryColor
+} from "components/gv-styles/gv-colors/gv-colors";
+import { $paddingSmall, $paddingXsmall } from "components/gv-styles/gv-sizes";
 import React from "react";
+import styled from "styled-components";
 import { Sizeable } from "utils/types";
-
-import style from "./li.module.scss";
 
 interface Props extends Sizeable {}
 
-export const Li: React.FC<Props> = ({ size = "small", children }) => {
-  return (
-    <li
-      className={clsx(style["li"], {
-        [style["li--small"]]: size === "small"
-      })}
-    >
-      {children}
-    </li>
-  );
-};
+const size = $paddingXsmall;
+
+export const Li = styled.li<Props>`
+  position: relative;
+  padding-left: ${size * 2}px;
+  &::before {
+    border: 7px solid ${$backgroundColor};
+    content: "";
+    position: absolute;
+    width: ${size}px;
+    height: ${size}px;
+    display: block;
+    border-radius: 50%;
+    background: ${$primaryColor};
+    left: 0;
+    box-sizing: border-box;
+  }
+  ${({ size }) => {
+    switch (size) {
+      case "small":
+        return `margin-top: ${$paddingSmall};`;
+    }
+  }}
+`;
