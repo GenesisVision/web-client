f01bf1f213b600c804e178a4a1aac945b1cbd8dc
pavelzubov <pasha224@yandex.ru>
2020-10-15 10:09:30 +0300

    Move DashboardTradingEmpty to separate file

--- packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-trading-statistic.tsx
+++ packages/social-trader/src/pages/dashboard/components/dashboard-statistic/dashboard-trading-statistic.tsx
@@ -1,11 +1,7 @@
 import { StatisticItemList } from "components/statistic-item-list/statistic-item-list";
-import { Text } from "components/text/text";
 import { TooltipLabel } from "components/tooltip-label/tooltip-label";
 import { useAccountCurrency } from "hooks/account-currency.hook";
-import { CREATE_ACCOUNT_PAGE_ROUTE } from "pages/create-account/create-account.constants";
-import { CREATE_FUND_PAGE_ROUTE } from "pages/create-fund/create-fund.constants";
 import { DashboardBlockOrientation } from "pages/dashboard/components/dashboard-block/dashboard-block";
-import { DashboardNewUserBlock } from "pages/dashboard/components/dashboard-statistic/dashboard-new-user.block";
 import DashboardStatisticContainer from "pages/dashboard/components/dashboard-statistic/dashboard-statistic.container";
 import DashboardValueItem from "pages/dashboard/components/dashboard-statistic/dashboard-value-item";
 import { TDashboardTradingStatistic } from "pages/dashboard/dashboard.types";
@@ -14,6 +10,7 @@ import { useTranslation } from "react-i18next";
 import { TRADING_ROUTE } from "routes/dashboard.routes";
 
 import { fetchTradingTotalStatistic } from "../../services/dashboard.service";
+import { DashboardTradingEmpty } from "pages/dashboard/components/dashboard-statistic/dashboard-trading-empty";
 
 interface Props {
   orientation?: DashboardBlockOrientation;
@@ -59,41 +56,6 @@ const _DashboardTradingStatistic: React.FC<Props> = ({ orientation }) => {
     />
   );
 };
-
-const DashboardTradingEmpty: React.FC = React.memo(() => {
-  const [t] = useTranslation();
-  return (
-    <DashboardNewUserBlock
-      leftField={{
-        link: CREATE_ACCOUNT_PAGE_ROUTE,
-        linkLabel: t(
-          "dashboard-page:statistic.get-started.trading.left-field.button"
-        ),
-        text: (
-          <>
-            {t("dashboard-page:statistic.get-started.trading.left-field.text")}{" "}
-            <Text muted>
-              {t(
-                "dashboard-page:statistic.get-started.trading.left-field.text-2"
-              )}
-            </Text>
-          </>
-        )
-      }}
-      rightField={{
-        link: CREATE_FUND_PAGE_ROUTE,
-        linkLabel: t(
-          "dashboard-page:statistic.get-started.trading.right-field.button"
-        ),
-        text: (
-          <>
-            {t("dashboard-page:statistic.get-started.trading.right-field.text")}
-          </>
-        )
-      }}
-    />
-  );
-});
 
 const DashboardTradingStatistic = React.memo(_DashboardTradingStatistic);
 export default DashboardTradingStatistic;
