4ad6a57a25824550b1d184f0602d36beeba98467
pavelzubov <pasha224@yandex.ru>
2020-10-16 19:46:16 +0300

    Simplify DetailsBlockTitleBox

--- packages/social-trader/src/components/details/details-block-title-box.tsx
+++ packages/social-trader/src/components/details/details-block-title-box.tsx
@@ -2,14 +2,8 @@ import { detailsBlockHorizontalPaddings } from "components/details/details.const
 import React from "react";
 import styled from "styled-components";
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
-
-const StyledDiv = styled.div`
+const DetailsBlockTitleBox = styled.div`
   ${detailsBlockHorizontalPaddings}
 `;
-
-const DetailsBlockTitleBox: React.FC<Props> = ({ children }) => {
-  return <StyledDiv>{children}</StyledDiv>;
-};
 
 export default DetailsBlockTitleBox;
