21649459bb24e3a78b1293a5e480b7bf0df12cdb
pavelzubov <pasha224@yandex.ru>
2020-10-18 21:48:41 +0300

    Remove unused parseStyles

--- packages/social-trader/src/utils/style/generators.ts
+++ packages/social-trader/src/utils/style/generators.ts
@@ -1,37 +1,6 @@
-export interface IStyleValue<T = any> {
-  value: T;
-  unit?: string;
-}
-
-export type IStyleTable = {
-  [keys: string]: IStyleValue | string;
-};
-
 export const getHEXA = (color: string, alpha: number): string => {
   const alphaInHEX = (Math.round(alpha * 255) + 0x10000)
     .toString(16)
     .substr(-2);
   return color + alphaInHEX;
-};
-
-export const parseStyles = ({
-  styleTable
-}: {
-  styleTable: IStyleTable;
-}): string => {
-  return (
-    Object.entries(styleTable)
-      .map(([name, value]) => {
-        const normalValue = (typeof value === "string"
-          ? { value }
-          : value) as IStyleValue;
-        return [name, normalValue];
-      })
-      // @ts-ignore
-      .map(([name, { value, unit = "" }]) => {
-        return `${name}: ${value}${unit}; 
-      `;
-      })
-      .join("")
-  );
 };
