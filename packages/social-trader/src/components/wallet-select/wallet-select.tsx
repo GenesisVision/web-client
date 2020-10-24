9d9e73ce6c2d8afb132c736d36f2592deb2b401c
pavelzubov <pasha224@yandex.ru>
2020-10-24 19:07:21 +0300

    Add CommonWalletType, export IWalletSelectProps in wallet-select.tsx

--- packages/social-trader/src/components/wallet-select/wallet-select.tsx
+++ packages/social-trader/src/components/wallet-select/wallet-select.tsx
@@ -1,18 +1,31 @@
 import { CurrencySourceSelect as HookFormCurrencySourceSelect } from "components/currency-source-select/hook-form-currency-source-select";
 import { ISelectChangeEvent } from "components/select/select";
-import { WalletBaseData, WalletData } from "gv-api-web";
+import { AssetDetails, Currency, WalletBaseData, WalletData } from "gv-api-web";
 import { TransferItemType } from "modules/transfer/transfer.types";
 import React from "react";
 
 export type ItemsType = Array<WalletItemType>;
 export type WalletItemType = WalletData | WalletBaseData | TransferItemType;
+export interface CommonWalletType {
+  id: string;
+  title?: string;
+  logoUrl?: string;
+  currency: Currency;
+  available: number;
+  asset?: AssetDetails;
+  rate?: number;
+  isWithdrawalEnabled?: boolean;
+}
 
-interface Props {
-  items: ItemsType;
-  label: string;
+export interface IWalletSelectProps {
   name: string;
-  onChange?: (event: ISelectChangeEvent, child: JSX.Element) => void;
   disabled?: boolean;
+}
+
+interface Props extends IWalletSelectProps {
+  onChange?: (event: ISelectChangeEvent, child: JSX.Element) => void;
+  label: string;
+  items: CommonWalletType[];
 }
 
 const _HookFormWalletSelect: React.FC<Props> = ({
