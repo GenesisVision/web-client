0989aabc28832459f1b6f9f5c0f2e69d267f52cd
pavelzubov <pasha224@yandex.ru>
2020-10-25 18:21:02 +0300

    Change GVTextField to UpdatableGvTextField in CurrencySourceSelectElement

--- packages/social-trader/src/components/currency-source-select/currency-source-select.element.tsx
+++ packages/social-trader/src/components/currency-source-select/currency-source-select.element.tsx
@@ -2,19 +2,22 @@ import {
   CurrencySourceSelectItemsType,
   getCurrencySourceSelectItems
 } from "components/currency-source-select/currency-source-select-items";
-import GVTextField from "components/gv-text-field";
 import { GVTextFieldProps } from "components/gv-text-field/gv-text-field.style";
+import {
+  IUpdatableGvTextFieldProps,
+  UpdatableGvTextField
+} from "components/gv-text-field/updatable-gv-text-field";
 import Select from "components/select/select";
 import React from "react";
 
-interface Props extends GVTextFieldProps {
+interface Props extends GVTextFieldProps, IUpdatableGvTextFieldProps {
   items: CurrencySourceSelectItemsType;
 }
 
 const _CurrencySourceSelect: React.FC<Props> = props => (
-  <GVTextField {...props} fixedVertical InputComponent={Select}>
+  <UpdatableGvTextField {...props} fixedVertical InputComponent={Select}>
     {getCurrencySourceSelectItems(props.items)}
-  </GVTextField>
+  </UpdatableGvTextField>
 );
 
 export const CurrencySourceSelectElement = React.memo(_CurrencySourceSelect);
