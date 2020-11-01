3096d360c619264115b88ef3c9ea23d4074782e5
pavelzubov <pasha224@yandex.ru>
2020-11-01 08:09:37 -0800

    Use styled-components in AuthLayout

--- packages/social-trader/src/pages/auth/components/auth-layout/auth-layout.tsx
+++ packages/social-trader/src/pages/auth/components/auth-layout/auth-layout.tsx
@@ -9,9 +9,143 @@ import { NextPage } from "next";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { HOME_ROUTE } from "routes/app.routes";
+import styled from "styled-components";
+import {
+  $panelBackgroundColor,
+  $secondaryBackgroundColor
+} from "utils/style/colors";
+import {
+  mediaBreakpointDesktop,
+  mediaBreakpointLandscapePhone,
+  mediaBreakpointLandscapeTablet,
+  mediaBreakpointTablet
+} from "utils/style/media";
+import {
+  $borderRadius,
+  $fontSizeCommon,
+  $fontSizeH2,
+  $paddingBig,
+  $paddingLarge,
+  $paddingMedium,
+  $paddingSmall,
+  $paddingXlarge,
+  $paddingXsmall,
+  $paddingXxlarge
+} from "utils/style/sizes";
 
+import background from "../../../../media/bgnd.svg";
 import { ILoginFooterProps } from "../login-footer/login-footer";
-import styles from "./auth-layout.module.scss";
+
+interface Props extends OwnProps {}
+
+interface OwnProps {
+  Footer: React.ComponentType<ILoginFooterProps>;
+  footerAuthRoute: string;
+  titleKey: string;
+  children: React.ReactChild;
+  quoteNo: number;
+}
+
+const Container = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  min-height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url(${background}) no-repeat bottom;
+  background-size: 100%;
+
+  ${mediaBreakpointLandscapePhone(`
+     padding: ${$paddingXsmall}px;
+  `)}
+  ${mediaBreakpointLandscapeTablet(`
+    padding: ${$paddingLarge}px 0;
+    flex-direction: row;
+    align-items: inherit;
+  `)}
+  ${mediaBreakpointDesktop(`
+    padding: ${$paddingXlarge}px ${$paddingXxlarge}px;
+    justify-content: space-between;
+  `)}
+`;
+
+const Left = styled.div`
+  padding: ${$paddingSmall} 0;
+  ${mediaBreakpointLandscapeTablet(`
+      min-height: 600px;
+      padding-left: ${$paddingMedium}px;
+    `)}
+  ${mediaBreakpointDesktop(`
+      padding-left: 0;
+    `)}
+`;
+
+const Right = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 340px;
+  background-color: ${$panelBackgroundColor};
+  border-radius: ${$borderRadius}px;
+  ${mediaBreakpointTablet(`
+      min-width: 460px;
+  `)}
+  ${mediaBreakpointDesktop(`
+      box-sizing: border-box;
+      min-width: 476px;
+  `)}
+`;
+
+const StyledLink = styled(Link)`
+  display: none;
+  ${mediaBreakpointTablet(`
+     display: flex;
+  `)}
+`;
+const Quote = styled.blockquote`
+  display: none;
+  font-weight: 400;
+  font-size: 44px;
+  letter-spacing: 0.3px;
+  ${mediaBreakpointLandscapeTablet(`
+      display: block;
+      margin-top: 110px;
+      margin-left: 0;
+      max-width: 760px;
+  `)}
+`;
+const QuoteFooter = styled.footer`
+  font-size: ${$fontSizeH2}px;
+  margin-top: ${$paddingMedium}px;
+`;
+
+const Content = styled.div`
+  box-sizing: border-box;
+  padding: ${$paddingXsmall}px;
+  ${mediaBreakpointTablet(`
+      padding: ${$paddingMedium}px;
+  `)}
+  ${mediaBreakpointDesktop(`
+      padding: ${$paddingLarge}px ${$paddingLarge}px ${$paddingBig}px;
+  `)}
+`;
+
+const FooterContainer = styled(Center)`
+  justify-content: space-between;
+  font-size: ${$fontSizeCommon}px;
+  background-color: ${$secondaryBackgroundColor};
+  border-radius: 0 0 ${$borderRadius}px ${$borderRadius}px;
+  padding: ${$paddingXsmall}px;
+  ${mediaBreakpointTablet(`
+      padding: ${$paddingXsmall}px ${$paddingMedium}px;
+  `)}
+  ${mediaBreakpointDesktop(`
+      padding: ${$paddingMedium}px ${$paddingLarge}px;
+  `)}
+`;
 
 const _AuthLayout: NextPage<Props> = ({
   quoteNo,
@@ -23,9 +157,9 @@ const _AuthLayout: NextPage<Props> = ({
   const { linkCreator } = useToLink();
   const [t] = useTranslation();
   return (
-    <div className={styles["auth"]}>
-      <div className={styles["auth__left"]}>
-        <Link className={styles["auth__logo"]} to={linkCreator(HOME_ROUTE)}>
+    <Container>
+      <Left>
+        <StyledLink to={linkCreator(HOME_ROUTE)}>
           <Center>
             <RowItem size={"small"}>
               <GvLogo />
@@ -34,43 +168,30 @@ const _AuthLayout: NextPage<Props> = ({
               <GvBrand />
             </RowItem>
           </Center>
-        </Link>
-        <blockquote className={styles["auth__quote"]}>
+        </StyledLink>
+        <Quote>
           {t(`auth:auth-quotes.${quoteNo}.quote`)}
-          <footer className={styles["auth__quote-footer"]}>
-            —{" "}
-            <cite className={styles["auth__quote-author"]}>
-              {t(`auth:auth-quotes.${quoteNo}.author`)}
-            </cite>
-          </footer>
-        </blockquote>
-      </div>
-      <div className={styles["auth__right"]}>
-        <div className={styles["auth__content"]}>
+          <QuoteFooter>
+            — <cite>{t(`auth:auth-quotes.${quoteNo}.author`)}</cite>
+          </QuoteFooter>
+        </Quote>
+      </Left>
+      <Right>
+        <Content>
           {titleKey && <h1>{t(titleKey)}</h1>}
           <Row size={"large"} onlyOffset>
             {children}
           </Row>
-        </div>
+        </Content>
         {Footer && (
-          <Center className={styles["auth__footer"]}>
+          <FooterContainer>
             <Footer ROUTE={footerAuthRoute} />
-          </Center>
+          </FooterContainer>
         )}
-      </div>
-    </div>
+      </Right>
+    </Container>
   );
 };
-
-interface Props extends OwnProps {}
-
-interface OwnProps {
-  Footer: React.ComponentType<ILoginFooterProps>;
-  footerAuthRoute: string;
-  titleKey: string;
-  children: React.ReactChild;
-  quoteNo: number;
-}
 
 const AuthLayout = React.memo(_AuthLayout);
 export default AuthLayout;
