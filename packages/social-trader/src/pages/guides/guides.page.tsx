7e7f68da67e7f988c5db0257161dfdec3fd3a33c
A Popova <a-nap@inbox.ru>
2020-11-12 23:37:44 +0100

    Remove GuidesContainer

--- packages/social-trader/src/pages/guides/guides.page.tsx
+++ packages/social-trader/src/pages/guides/guides.page.tsx
@@ -1,5 +1,6 @@
-import GuidesContainer from "components/guides/guides.container";
+import GuidesSection from "components/guides/guides-section/guides-section";
 import Page from "components/page/page";
+import { navGuides } from "pages/guides/guides.static-data";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -9,7 +10,7 @@ const _GuidesPage: React.FC = () => {
 
   return (
     <Page title={title}>
-      <GuidesContainer />
+      <GuidesSection navGuides={navGuides} />
     </Page>
   );
 };
