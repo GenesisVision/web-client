990a1fa815fd43c3e170b1014e029eafc8d5962f
pavelzubov <pasha224@yandex.ru>
2020-11-25 13:31:55 +0300

    Upgrade account selector

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/components/account-selector/account-selector.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/components/account-selector/account-selector.tsx
@@ -2,7 +2,12 @@ import { CurrencySourceSelectItemType } from "components/currency-source-select/
 import { CurrencySourceSelectElement } from "components/currency-source-select/currency-source-select.element";
 import { ISelectChangeEvent } from "components/select/select";
 import { ExchangeAsset } from "gv-api-web";
-import React, { useCallback, useState } from "react";
+import { TerminalInfoContext } from "pages/trade/binance-trade-page/trading/terminal-info.context";
+import {
+  stringifySymbolFromToParam,
+  useUpdateTerminalUrlParams
+} from "pages/trade/binance-trade-page/trading/terminal.helpers";
+import React, { useCallback, useContext, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export interface IAccountSelectorProps {
@@ -14,12 +19,12 @@ const mapExchangeAssetToCurrencySourceSelectItem = ({
   title,
   balance,
   currency,
-  credentials,
+  id,
   asset
 }: ExchangeAsset): CurrencySourceSelectItemType => ({
   asset,
   available: balance,
-  id: credentials.apiKey,
+  id,
   currency,
   logoUrl: asset?.logoUrl,
   title
@@ -29,12 +34,20 @@ const _AccountSelector: React.FC<IAccountSelectorProps> = ({
   currentAccount,
   items
 }) => {
+  const { symbol } = useContext(TerminalInfoContext);
+  const { updateUrl } = useUpdateTerminalUrlParams();
   const [t] = useTranslation();
   const [account, setAccount] = useState(currentAccount);
 
   const onChange = useCallback(
     (event: ISelectChangeEvent) => {
-      setAccount(event.target.value);
+      const id = event.target.value;
+      setAccount(id);
+      updateUrl({
+        url: stringifySymbolFromToParam(symbol),
+        reloadPage: true,
+        updates: { id }
+      });
     },
     [setAccount]
   );
