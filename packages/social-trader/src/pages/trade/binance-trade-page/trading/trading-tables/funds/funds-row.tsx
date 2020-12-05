94532061f0f698fe2d9f37b733cec8db5dfb849e
pavelzubov <pasha224@yandex.ru>
2020-12-05 20:48:19 +0400

    Use useGetRate in FundsRow

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/funds/funds-row.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/funds/funds-row.tsx
@@ -1,7 +1,10 @@
+import { useAccountCurrency } from "hooks/account-currency.hook";
+import { useGetRate } from "hooks/get-rate.hook";
 import { TerminalInfoContext } from "pages/trade/binance-trade-page/trading/terminal-info.context";
 import { formatValueWithTick } from "pages/trade/binance-trade-page/trading/terminal.helpers";
 import { TransferButton } from "pages/trade/binance-trade-page/trading/transfer/transfer.button";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { formatCurrencyValue } from "utils/formatter";
 
 interface Props {
   asset: string;
@@ -10,21 +13,28 @@ interface Props {
   locked: number;
 }
 
-const _FundsFRow: React.FC<Props> = ({
+const _FundsRow: React.FC<Props> = ({
   amountInCurrency,
   asset,
   available,
   locked
 }) => {
+  const currency = useAccountCurrency();
+  const { rate, getRate } = useGetRate();
   const { terminalType } = useContext(TerminalInfoContext);
   const total = formatValueWithTick(+available + +locked, "0.00000001");
+
+  useEffect(() => {
+    getRate({ from: asset, to: currency });
+  }, [currency, asset]);
+
   return (
     <tr>
       <td>{asset}</td>
       <td>{total}</td>
       <td>{formatValueWithTick(available, "0.00000001")}</td>
       <td>{locked}</td>
-      <td>{amountInCurrency}</td>
+      <td>{formatCurrencyValue(+total * rate, currency)}</td>
       {terminalType === "futures" && (
         <td>
           <TransferButton asset={asset} />
@@ -34,4 +44,4 @@ const _FundsFRow: React.FC<Props> = ({
   );
 };
 
-export const FundsRow = React.memo(_FundsFRow);
+export const FundsRow = React.memo(_FundsRow);
