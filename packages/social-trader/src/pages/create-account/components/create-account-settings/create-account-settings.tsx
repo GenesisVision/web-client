3c60ff4714b131cc58df6d529002262f86b4421a
pavelzubov <pasha224@yandex.ru>
2020-12-19 21:05:57 +0400

    Correct broker.accountTypes type

--- packages/social-trader/src/pages/create-account/components/create-account-settings/create-account-settings.tsx
+++ packages/social-trader/src/pages/create-account/components/create-account-settings/create-account-settings.tsx
@@ -12,7 +12,7 @@ import DepositDetailsBlock from "components/assets/fields/deposit-details-block"
 import Leverage from "components/assets/fields/leverage";
 import { Row } from "components/row/row";
 import SettingsBlock from "components/settings-block/settings-block";
-import { Broker } from "gv-api-web";
+import { Broker, BrokerAccountType } from "gv-api-web";
 import { KycRequiredBlock } from "pages/create-account/components/create-account-settings/kyc-required-block";
 import * as React from "react";
 import { useState } from "react";
@@ -83,7 +83,7 @@ const _CreateAccountSettings: React.FC<Props> = ({
   const { brokerAccountTypeId, depositAmount, currency } = watch();
 
   const accountType = safeGetElemFromArray(
-    broker.accountTypes,
+    broker.accountTypes as BrokerAccountType[],
     ({ id }) => brokerAccountTypeId === id
   );
 
