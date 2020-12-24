36ff4be7beef9bd8f910d629291180e6fe2c765d
pavelzubov <pasha224@yandex.ru>
2020-12-24 14:22:06 +0400

    Move transformKlineBar, transformToUnitedOrder, getPriceWithCorrectFrac, transformDepthToString to api.helpers.ts

--- packages/social-trader/src/pages/trade/binance-trade-page/services/api.helpers.ts
+++ packages/social-trader/src/pages/trade/binance-trade-page/services/api.helpers.ts
@@ -1,6 +1,20 @@
-import { BrokerTradeServerType } from "gv-api-web";
+import { DEFAULT_DECIMAL_SCALE } from "constants/constants";
+import dayjs from "dayjs";
+import {
+  BinanceRawKline,
+  BinanceRawOrder,
+  BinanceRawOrderBookEntry,
+  BrokerTradeServerType
+} from "gv-api-web";
 import { getBinanceTerminalApiMethods } from "pages/trade/binance-trade-page/binance-trade.helpers";
-import { TerminalType } from "pages/trade/binance-trade-page/trading/terminal.types";
+import { Bar } from "pages/trade/binance-trade-page/trading/chart/charting_library/datafeed-api";
+import { DividerPartsType } from "pages/trade/binance-trade-page/trading/order-book/order-book.helpers";
+import {
+  StringBidDepth,
+  TerminalType,
+  UnitedOrder
+} from "pages/trade/binance-trade-page/trading/terminal.types";
+import { formatValue } from "utils/formatter";
 
 export const getTerminalApiMethods = (
   brokerType?: BrokerTradeServerType,
@@ -11,4 +25,72 @@ export const getTerminalApiMethods = (
     default:
       return getBinanceTerminalApiMethods(type);
   }
+};
+
+export const transformKlineBar = ({
+  close,
+  high,
+  low,
+  open,
+  openTime,
+  baseVolume
+}: BinanceRawKline): Bar => ({
+  close,
+  high,
+  low,
+  open,
+  time: dayjs(openTime).unix() * 1000,
+  volume: baseVolume
+});
+
+export const transformToUnitedOrder = ({
+  commissionAsset,
+  status,
+  commission,
+  quoteQuantityFilled,
+  orderId,
+  createTime,
+  symbol,
+  type,
+  side,
+  stopPrice,
+  price,
+  quantity,
+  quoteQuantity,
+  quantityFilled
+}: BinanceRawOrder): UnitedOrder => ({
+  commissionAsset,
+  orderStatus: status,
+  commission,
+  quoteQuantityFilled,
+  executedQuantity: quoteQuantity,
+  id: orderId,
+  time: createTime,
+  symbol,
+  type,
+  side,
+  stopPrice,
+  price,
+  quantityFilled,
+  quantity
+});
+
+export const getPriceWithCorrectFrac = (
+  price: string,
+  correctFracLength: number = 8
+) => {
+  const [int, frac = ""] = price.split(".");
+  const correctFrac = frac + "0".repeat(correctFracLength - frac.length);
+  return [int, correctFrac].join(".");
+};
+
+export const transformDepthToString = (dividerParts: DividerPartsType) => ({
+  price,
+  quantity
+}: BinanceRawOrderBookEntry): StringBidDepth => {
+  const newPrice = getPriceWithCorrectFrac(
+    formatValue(price, DEFAULT_DECIMAL_SCALE),
+    dividerParts.fracLength
+  );
+  return [newPrice, String(quantity)];
 };
