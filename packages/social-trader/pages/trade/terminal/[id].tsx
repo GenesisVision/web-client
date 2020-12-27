84e772ea501efb1b02723ac051d5236f24e614f6
pavelzubov <pasha224@yandex.ru>
2020-12-27 22:42:16 +0400

    Add and use getTerminalType in trade/terminal/[id] page

--- packages/social-trader/pages/trade/terminal/[id].tsx
+++ packages/social-trader/pages/trade/terminal/[id].tsx
@@ -6,17 +6,14 @@ import { TYPE_PARAM_NAME } from "pages/trade/binance-trade-page/binance-trade.he
 import { getTerminalApiMethods } from "pages/trade/binance-trade-page/services/api.helpers";
 import { TerminalMethodsContextProvider } from "pages/trade/binance-trade-page/trading/contexts/terminal-methods.context";
 import { parseSymbolFromUrlParam } from "pages/trade/binance-trade-page/trading/terminal.helpers";
-import {
-  SymbolState,
-  TerminalAuthDataType,
-  TerminalType
-} from "pages/trade/binance-trade-page/trading/terminal.types";
+import { SymbolState, TerminalAuthDataType, TerminalType } from "pages/trade/binance-trade-page/trading/terminal.types";
 import { TerminalPage } from "pages/trade/terminal.page";
 import React from "react";
 import { compose } from "redux";
 import { initializeStore } from "store";
 import { getParamsFromCtxWithSplit } from "utils/ssr-helpers";
-import { NextPageWithRedux } from "utils/types";
+import { AnyObjectType, NextPageWithRedux } from "utils/types";
+import { api } from "services/api-client/swagger-custom-client";
 
 interface Props {
   exchangeAccountId?: string;
@@ -25,6 +22,19 @@ interface Props {
   terminalType?: TerminalType;
   symbol?: SymbolState;
 }
+
+const getTerminalType = async (
+  params?: AnyObjectType
+): Promise<TerminalType> => {
+  if (params?.["id"]) {
+    const accountInfo = await api
+      .terminal()
+      .getAccountInfo({ accountId: params?.["id"], currency: "USDT" });
+    return accountInfo.accountType.toLowerCase as TerminalType;
+  }
+  if (params?.[TYPE_PARAM_NAME]) return params[TYPE_PARAM_NAME].toLowerCase();
+  return "spot";
+};
 
 const Page: NextPageWithRedux<Props> = ({
   exchangeAccountId,
@@ -50,9 +60,7 @@ Page.getInitialProps = async ctx => {
   const { id } = ctx.query;
   const params = getParamsFromCtxWithSplit(ctx);
   const exchangeAccountId = params["id"];
-  const terminalType = params[TYPE_PARAM_NAME]
-    ? params[TYPE_PARAM_NAME].toLowerCase()
-    : undefined;
+  const terminalType = await getTerminalType(params);
   const symbol = id ? parseSymbolFromUrlParam(String(id)) : undefined;
 
   let brokerType: BrokerTradeServerType | undefined;
