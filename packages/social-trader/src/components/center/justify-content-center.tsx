1cedb66a0ab7e1ce73a651c923053872609eba2b
A Popova <a-nap@inbox.ru>
2020-12-30 15:37:39 +0100

    Move JustifyContentSpaceAround and JustifyContentCenter to Guides file

--- packages/social-trader/src/components/center/justify-content-center.tsx
+++
@@ -1,7 +0,0 @@
-import { Center } from "components/center/center";
-import React from "react";
-import styled from "styled-components";
-
-export const JustifyContentCenter = styled(Center)`
-  justify-content: center;
-`;
