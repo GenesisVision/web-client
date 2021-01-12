80d9f4cd0a3cdf37debab198309f534d3f12a3b9
pavelzubov <pasha224@yandex.ru>
2021-01-12 14:28:12 +0400

    Correct markup in DeleteApiKeyButton

--- packages/social-trader/src/components/api-keys/api-key/delete-api-key/delete-api-key.button.tsx
+++ packages/social-trader/src/components/api-keys/api-key/delete-api-key/delete-api-key.button.tsx
@@ -7,12 +7,19 @@ import { useTranslation } from "react-i18next";
 
 import { ApiKeysContext } from "../../api-keys.context";
 import { deleteApiKey } from "../../services/api-keys.service";
+import styled from "styled-components";
+import { Text } from "components/text/text";
 
 interface Props {
   id: string;
   title: string;
   apiKey: string;
 }
+
+const Key = styled(Text)`
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+`;
 
 const _DeleteApiKeyButton: React.FC<Props> = ({ id, title, apiKey }) => {
   const [t] = useTranslation();
@@ -26,7 +33,16 @@ const _DeleteApiKeyButton: React.FC<Props> = ({ id, title, apiKey }) => {
 
   const handleApply = useCallback(sendRequest, [sendRequest]);
 
-  const body = `${t("api-keys:key-dialog.delete-text")} ${title} ? ${apiKey}`;
+  const body = (
+    <div>
+      {`${t("api-keys:key-dialog.delete-text")} ${title} ?`}
+      <br />
+      <br />
+      <Key muted size={"xsmall"}>
+        {apiKey}
+      </Key>
+    </div>
+  );
 
   return (
     <>
