9c2f98f2e3ca293579c2e64eec8f229b8915c655
pavelzubov <pasha224@yandex.ru>
2021-01-19 23:04:55 +0400

    Add TERMINAL_SPOT_ROUTE, TERMINAL_FUTURES_ROUTE in trade.routes.ts

--- packages/social-trader/src/routes/trade.routes.ts
+++ packages/social-trader/src/routes/trade.routes.ts
@@ -1,8 +1,12 @@
+import { TYPE_PARAM_NAME } from "pages/trade/binance-trade-page/binance-trade.helpers";
+
 export const TRADE = "trade";
 export const TRADE_ROUTE = `/${TRADE}`;
 
 export const TERMINAL = "terminal";
 export const TERMINAL_ROUTE = `${TRADE_ROUTE}/${TERMINAL}`;
+export const TERMINAL_SPOT_ROUTE = TERMINAL_ROUTE;
+export const TERMINAL_FUTURES_ROUTE = `${TERMINAL_ROUTE}?${TYPE_PARAM_NAME}=futures`;
 export const TERMINAL_FOLDER_ROUTE = `${TRADE_ROUTE}/${TERMINAL}/[id]`;
 export const META_TRADER_4 = "mt4";
 export const META_TRADER_4_ROUTE = `${TRADE_ROUTE}/${META_TRADER_4}`;
