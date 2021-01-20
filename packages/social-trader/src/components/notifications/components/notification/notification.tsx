dac1a209a4a69941dbda3c16c60dcb1f627a823b
mnbeh <mnbeh7@gmail.com>
2021-01-20 16:49:44 +0300

    Remade notification component

--- packages/social-trader/src/components/notifications/components/notification/notification.tsx
+++ packages/social-trader/src/components/notifications/components/notification/notification.tsx
@@ -1,22 +1,26 @@
 import AssetAvatar from "components/avatar/asset-avatar/asset-avatar";
 import ImageBaseElement from "components/avatar/image-base.element";
-import Link, { LinkProps } from "components/link/link";
+import Link from "components/link/link";
 import { useToLink } from "components/link/link.helper";
 import { Row } from "components/row/row";
 import { RowItem } from "components/row-item/row-item";
 import { Text } from "components/text/text";
 import dayjs from "dayjs";
-import { NotificationViewModel } from "gv-api-web";
+import {
+  NotificationLocationViewModel,
+  NotificationViewModel
+} from "gv-api-web";
 import NewsIcon from "media/news.svg";
 import RedUserIcon from "media/red-user.svg";
 import * as React from "react";
-import {
-  FUND_DETAILS_FOLDER_ROUTE,
-  PROGRAM_DETAILS_FOLDER_ROUTE
-} from "routes/invest.routes";
+import { DASHBOARD_ROUTE } from "routes/dashboard.routes";
+import { composePostDetailsUrl, MEDIA_ROUTE } from "routes/social.routes";
 import styled, { css } from "styled-components";
 import {
+  composeAccountDetailsUrl,
+  composeFollowDetailsUrl,
   composeFundsDetailsUrl,
+  composeManagerDetailsUrl,
   composeProgramDetailsUrl
 } from "utils/compose-url";
 import {
@@ -43,7 +47,21 @@ enum TYPE {
   PLATFORM = "Platform",
   SOCIAL = "Social",
   ASSET = "Asset",
-  TRADING_ACCOUNT = "TradingAccount"
+  TRADING_ACCOUNT = "TradingAccount",
+  USER = "User",
+  SIGNAL = "Signal",
+  PLATFORM_ASSET = "PlatformAsset"
+}
+
+enum LINKS {
+  TRADING_ACCOUNT = "TradingAccount",
+  USER = "User",
+  PROGRAM = "Program",
+  DASHBOARD = "Dashboard",
+  SOCIAL_POST = "SocialPost",
+  FOLLOW = "Follow",
+  FUND = "Fund",
+  SOCIAL_MEDIA_POST = "SocialMediaPost"
 }
 
 interface INotificationOwnProps {
@@ -51,13 +69,17 @@ interface INotificationOwnProps {
 }
 
 interface IAssetAvatarContainerProps {
-  type?: string;
+  type: string;
 }
 
 interface INotificationProps
   extends NotificationViewModel,
-  INotificationOwnProps,
-  IAssetAvatarContainerProps { }
+  INotificationOwnProps { }
+
+interface INotificationAssetAvatarProps extends INotificationProps {
+  color: string;
+  logoUrl: string;
+}
 
 const getStaticIconUrl = (type: string): string | null => {
   switch (type) {
@@ -69,6 +91,69 @@ const getStaticIconUrl = (type: string): string | null => {
     }
     default:
       return null;
+  }
+};
+
+const getColor = (type: string): string => {
+  switch (type) {
+    case TYPE.PROFILE: {
+      return "#D6CE93";
+    }
+    case TYPE.PLATFORM: {
+      return "#D8A48F";
+    }
+    case TYPE.ASSET: {
+      return "#6AB547";
+    }
+    case TYPE.SOCIAL: {
+      return "#EDD83D";
+    }
+    case TYPE.TRADING_ACCOUNT: {
+      return "#e81652";
+    }
+    case TYPE.USER: {
+      return "#4381C1";
+    }
+    case TYPE.SIGNAL: {
+      return "#FF7D00";
+    }
+    case TYPE.PLATFORM_ASSET: {
+      return "#3BF4FB";
+    }
+    default:
+      return "#FFFFFF";
+  }
+};
+
+const getLink = (location: NotificationLocationViewModel): string => {
+  const { id } = location;
+  switch (location?.location) {
+    case LINKS.TRADING_ACCOUNT: {
+      return composeAccountDetailsUrl(id);
+    }
+    case LINKS.DASHBOARD: {
+      return DASHBOARD_ROUTE;
+    }
+    case LINKS.FOLLOW: {
+      return composeFollowDetailsUrl(id);
+    }
+    case LINKS.PROGRAM: {
+      return composeProgramDetailsUrl(id);
+    }
+    case LINKS.SOCIAL_POST: {
+      return composePostDetailsUrl(id);
+    }
+    case LINKS.USER: {
+      return composeManagerDetailsUrl(id);
+    }
+    case LINKS.FUND: {
+      return composeFundsDetailsUrl(id);
+    }
+    case LINKS.SOCIAL_MEDIA_POST: {
+      return MEDIA_ROUTE;
+    }
+    default:
+      return "";
   }
 };
 
@@ -103,35 +188,20 @@ const AssetAvatarDivContainer = styled.div<IAssetAvatarContainerProps>`
   ${AssetAvatarContainerStyle}
 `;
 
-const _NotificationAssetAvatar: React.FC<INotificationProps> = ({
+const _NotificationAssetAvatar: React.FC<INotificationAssetAvatarProps> = ({
   type,
   closeNotifications,
-  platformAssetDetails,
-  userDetails,
-  assetDetails
+  color,
+  logoUrl,
+  location
 }) => {
-  const logoUrl =
-    assetDetails?.logoUrl ||
-    userDetails?.logoUrl ||
-    platformAssetDetails?.logoUrl;
   const { linkCreator } = useToLink();
-  const Tag: React.ComponentType<LinkProps | any> | string = assetDetails?.url
-    ? AssetAvatarLinkContainer
-    : AssetAvatarDivContainer;
-  const to = assetDetails?.url
-    ? linkCreator(
-      assetDetails?.assetType === "Program"
-        ? composeProgramDetailsUrl(assetDetails?.url)
-        : composeFundsDetailsUrl(assetDetails?.url),
-      assetDetails?.assetType === "Program"
-        ? PROGRAM_DETAILS_FOLDER_ROUTE
-        : FUND_DETAILS_FOLDER_ROUTE
-    )
-    : null;
+  const to = location.externalUrl || linkCreator(getLink(location));
+
   return (
-    <Tag to={to} onClick={closeNotifications} type={type}>
-      <AssetAvatar url={logoUrl} alt={type} color={assetDetails?.color} />
-    </Tag>
+    <AssetAvatarLinkContainer to={to} onClick={closeNotifications} type={type}>
+      <AssetAvatar url={logoUrl} alt={type} color={color} />
+    </AssetAvatarLinkContainer>
   );
 };
 const NotificationAssetAvatar = React.memo(_NotificationAssetAvatar);
@@ -163,18 +233,40 @@ const Content = styled.div`
 `;
 
 const _Notification: React.FC<INotificationProps> = props => {
-  const { date, text, isUnread, type, location } = props;
+  const {
+    imageUrl,
+    assetDetails,
+    userDetails,
+    platformAssetDetails,
+    date,
+    text,
+    isUnread,
+    type,
+    location
+  } = props;
+
+  const logoUrl: string =
+    assetDetails?.logoUrl ||
+    userDetails?.logoUrl ||
+    platformAssetDetails?.logoUrl ||
+    imageUrl;
   const staticIconUrl = getStaticIconUrl(type);
+  const color: string =
+    assetDetails?.color || platformAssetDetails?.color || getColor(type);
 
   return (
     <Container center={false}>
       <RowItem>
         {!location ? (
           <AssetAvatarDivContainer type={type}>
-            <ImageBaseElement src={staticIconUrl} alt={type} />
+            {staticIconUrl ? (
+              <ImageBaseElement src={staticIconUrl} alt={type} />
+            ) : (
+                <AssetAvatar url={logoUrl} color={color} alt={type} />
+              )}
           </AssetAvatarDivContainer>
         ) : (
-            <NotificationAssetAvatar {...props} type={type} />
+            <NotificationAssetAvatar {...props} logoUrl={logoUrl} color={color} />
           )}
       </RowItem>
 
