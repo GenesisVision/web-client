273468d63fe5404d3881773c31d2d10182406b56
pavelzubov <pasha224@yandex.ru>
2021-01-24 11:45:01 +0300

    Use Position type in position table

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions-row.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions-row.tsx
@@ -1,40 +1,34 @@
 import TableCell from "components/table/components/table-cell";
 import TableRow from "components/table/components/table-row";
-import { FuturesPositionInformation } from "pages/trade/binance-trade-page/trading/terminal.types";
+import { Position } from "pages/trade/binance-trade-page/trading/terminal.types";
 import React from "react";
 
 interface Props {
   marginRatio: number;
-  position: FuturesPositionInformation;
+  position: Position;
 }
 
 const _PositionsRow: React.FC<Props> = ({
   marginRatio,
-  position: {
-    liquidationPrice,
-    entryPrice,
-    leverage,
-    markPrice,
-    positionAmt,
-    symbol,
-    marginType,
-    unRealizedProfit
-  }
+  position: { entryPrice, leverage, positionAmount, symbol, unrealizedProfit }
 }) => {
   return (
     <TableRow>
       <TableCell firstOffset={false}>
         {symbol} {leverage}x
       </TableCell>
-      <TableCell>{positionAmt}</TableCell>
+      <TableCell>{positionAmount}</TableCell>
       <TableCell>{entryPrice}</TableCell>
-      <TableCell>{markPrice}</TableCell>
-      <TableCell>{liquidationPrice}</TableCell>
+      {/*<TableCell>{markPrice}</TableCell>*/}
+      <TableCell>{0}</TableCell>
+      {/*<TableCell>{liquidationPrice}</TableCell>*/}
+      <TableCell>{0}</TableCell>
       <TableCell>{marginRatio}</TableCell>
       <TableCell>
-        {(+positionAmt * +markPrice) / +leverage} ({marginType})
+        {/*{(+positionAmount * +markPrice) / +leverage} ({marginType})*/}
+        {(+positionAmount * +0) / +leverage} ({"Cross"})
       </TableCell>
-      <TableCell>{unRealizedProfit}</TableCell>
+      <TableCell>{unrealizedProfit}</TableCell>
     </TableRow>
   );
 };
--- packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions.container.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions.container.tsx
@@ -2,24 +2,16 @@ import { TerminalInfoContext } from "pages/trade/binance-trade-page/trading/cont
 import React, { useContext, useMemo } from "react";
 
 import { Positions } from "./positions";
-import { mapBinanceRawFuturesAccountPositionToFuturesPositionInformation } from "./positions.helpers";
 
 export const PositionsContainer: React.FC = () => {
   const { accountInfo } = useContext(TerminalInfoContext);
 
-  const {
-    symbol: { baseAsset, quoteAsset }
-  } = useContext(TerminalInfoContext);
-
   const list = useMemo(
     () =>
       accountInfo?.positions
-        ? accountInfo.positions
-            // .filter(({ symbol }) => symbol === getSymbol(baseAsset, quoteAsset))
-            .filter(({ positionAmount }) => positionAmount !== 0)
-            .map(
-              mapBinanceRawFuturesAccountPositionToFuturesPositionInformation
-            )
+        ? accountInfo.positions.filter(
+            ({ positionAmount }) => positionAmount !== 0
+          )
         : [],
     [accountInfo?.positions]
   );
