d5bee8aae81882bce5e5e385e5e411998d19bfbc
mnbeh <43515545+mnbeh@users.noreply.github.com>
2021-01-28 15:07:44 +0300

    Fix tooltip position (#1833)

--- packages/social-trader/src/pages/wallet/components/wallet-tables/buttons/line-wallet-button.tsx
+++ packages/social-trader/src/pages/wallet/components/wallet-tables/buttons/line-wallet-button.tsx
@@ -1,3 +1,4 @@
+import { HORIZONTAL_POPOVER_POS } from "components/popover/popover";
 import { RowItem } from "components/row-item/row-item";
 import Tooltip from "components/tooltip/tooltip";
 import { TooltipContent } from "components/tooltip/tooltip-content";
@@ -9,7 +10,10 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 export const _LineWalletButton: React.FC<Props> = ({ title, children }) => (
   <RowItem>
-    <Tooltip render={() => <TooltipContent>{title}</TooltipContent>}>
+    <Tooltip
+      horizontal={HORIZONTAL_POPOVER_POS.RIGHT}
+      render={() => <TooltipContent>{title}</TooltipContent>}
+    >
       <div>{children}</div>
     </Tooltip>
   </RowItem>
