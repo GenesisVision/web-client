af806a1c8b6d89e48050bf62cb1ba4f44f64ff66
pavelzubov <pasha224@yandex.ru>
2021-01-30 20:10:41 +0300

    Correct SecurityVerificationForm validation

--- packages/social-trader/src/pages/auth/security-verification/security-verification-form.tsx
+++ packages/social-trader/src/pages/auth/security-verification/security-verification-form.tsx
@@ -10,106 +10,107 @@ import { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { HookForm } from "utils/hook-form.helpers";
-import { object, string } from "yup";
+import { string } from "yup";
+import { convertShapeToRules } from "utils/validators/validators";
 
 enum FIELDS {
-    code = "code",
-    email = "email",
-    token = "token"
+  code = "code",
+  email = "email",
+  token = "token"
 }
 
 const _SecurityVerificationForm: React.FC<Props> = ({
-    email,
-    token,
-    errorMessage,
-    onSubmit
+  email,
+  token,
+  errorMessage,
+  onSubmit
 }) => {
-    const [t] = useTranslation();
+  const [t] = useTranslation();
 
-    const form = useForm<ISecurityVerificationFormValues>({
-        defaultValues: {
-            [FIELDS.code]: "",
-            [FIELDS.email]: email,
-            [FIELDS.token]: token
-        },
-        validationSchema: object().shape({
-            [FIELDS.code]: string()
-                .trim()
-                .matches(/^\d{6}$/, t("validations.three-factor-6digits"))
-                .required(t("validations.three-factor-required"))
-        }),
-        mode: "onChange"
-    });
-    const {
-        watch,
-        formState: { isSubmitting }
-    } = form;
-    const { code } = watch();
+  const form = useForm<ISecurityVerificationFormValues>({
+    defaultValues: {
+      [FIELDS.code]: "",
+      [FIELDS.email]: email,
+      [FIELDS.token]: token
+    },
+    mode: "onChange"
+  });
+  const {
+    watch,
+    formState: { isSubmitting }
+  } = form;
+  const { code } = watch();
 
-    const [isChecking, setIsChecking] = useIsOpen();
+  const [isChecking, setIsChecking] = useIsOpen();
 
-    useEffect(() => {
-        if (!isChecking && code.length === 6) {
-            checkThreeFactor();
-        }
-    }, [code, isChecking]);
+  useEffect(() => {
+    if (!isChecking && code.length === 6) {
+      checkThreeFactor();
+    }
+  }, [code, isChecking]);
 
-    const checkThreeFactor = useCallback(() => {
-        if (isSubmitting) return;
-        setIsChecking();
-        onSubmit({ code, email, token });
-    }, [isSubmitting, code, email, token]);
+  const checkThreeFactor = useCallback(() => {
+    if (isSubmitting) return;
+    setIsChecking();
+    onSubmit({ code, email, token });
+  }, [isSubmitting, code, email, token]);
 
-    const handleSubmit = useCallback(() => {
-        return onSubmit({ code, email, token });
-    }, [code, email, token]);
+  const handleSubmit = useCallback(() => {
+    return onSubmit({ code, email, token });
+  }, [code, email, token]);
 
-    return (
-        <HookForm form={form} onSubmit={handleSubmit}>
-            <Row>
-                <Text muted>{t("auth:security-verification.text")}</Text>
-            </Row>
-            <Row size={"xlarge"}>
-                <GVHookFormField
-                    disabled={isSubmitting}
-                    type="tel"
-                    name={FIELDS.code}
-                    label={t("auth:security-verification.input-label")}
-                    autoComplete="off"
-                    autoFocus
-                    component={SimpleTextField}
-                    format="######"
-                />
-            </Row>
-            <Row>
-                <Text muted>{t("auth:security-verification.message")}</Text>
-            </Row>
-            {errorMessage && (
-                <Row>
-                    <FormError error={errorMessage} />
-                </Row>
-            )}
-            <Row size={"large"}>
-                <SubmitButton checkSubmitted={false} isPending={isSubmitting}>
-                    {t("auth:security-verification.submit-button-label")}
-                </SubmitButton>
-            </Row>
-        </HookForm>
-    );
+  return (
+    <HookForm form={form} onSubmit={handleSubmit}>
+      <Row>
+        <Text muted>{t("auth:security-verification.text")}</Text>
+      </Row>
+      <Row size={"xlarge"}>
+        <GVHookFormField
+          disabled={isSubmitting}
+          type="tel"
+          name={FIELDS.code}
+          label={t("auth:security-verification.input-label")}
+          autoComplete="off"
+          autoFocus
+          component={SimpleTextField}
+          format="######"
+          rules={convertShapeToRules(
+            string()
+              .trim()
+              .matches(/^\d{6}$/, t("validations.three-factor-6digits"))
+              .required(t("validations.three-factor-required"))
+          )}
+        />
+      </Row>
+      <Row>
+        <Text muted>{t("auth:security-verification.message")}</Text>
+      </Row>
+      {errorMessage && (
+        <Row>
+          <FormError error={errorMessage} />
+        </Row>
+      )}
+      <Row size={"large"}>
+        <SubmitButton checkSubmitted={false} isPending={isSubmitting}>
+          {t("auth:security-verification.submit-button-label")}
+        </SubmitButton>
+      </Row>
+    </HookForm>
+  );
 };
 
 export interface ISecurityVerificationFormValues {
-    [FIELDS.code]: string;
-    [FIELDS.email]: string;
-    [FIELDS.token]: string;
+  [FIELDS.code]: string;
+  [FIELDS.email]: string;
+  [FIELDS.token]: string;
 }
 
 interface Props {
-    email: string;
-    token: string;
-    onSubmit: (values: ISecurityVerificationFormValues) => void;
-    errorMessage: string;
-    isChecking?: boolean;
+  email: string;
+  token: string;
+  onSubmit: (values: ISecurityVerificationFormValues) => void;
+  errorMessage: string;
+  isChecking?: boolean;
 }
 const SecurityVerificationForm = React.memo(_SecurityVerificationForm);
 export default SecurityVerificationForm;
