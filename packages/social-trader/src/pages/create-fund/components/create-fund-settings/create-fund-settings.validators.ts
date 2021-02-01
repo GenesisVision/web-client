ca043d1a602b60f822a50ba4c3b06660e1581138
pavelzubov <pasha224@yandex.ru>
2021-02-01 20:23:31 +0300

    Change shape to rules in assets field

--- packages/social-trader/src/pages/create-fund/components/create-fund-settings/create-fund-settings.validators.ts
+++ packages/social-trader/src/pages/create-fund/components/create-fund-settings/create-fund-settings.validators.ts
@@ -1,16 +1,12 @@
 import { FundAssetPart } from "gv-api-web";
 import { TFunction } from "i18next";
-import { array, TestFunction } from "yup";
 
-import { CREATE_FUND_FIELDS } from "./create-fund-settings";
-
-export const assetsShape = (t: TFunction) => {
-  return array()
-    .test(CREATE_FUND_FIELDS.assets, t("validations.assets-share"), ((
-      val: FundAssetPart[] = []
-    ) => {
-      return val.reduce((acc, next) => acc + next.percent, 0) === 100;
-    }) as TestFunction<any>)
-    .required(t("validations.assets-count"))
-    .min(2, t("validations.assets-count"));
-};
+export const assetsRules = (t: TFunction) => ({
+  validate: (value: FundAssetPart[]) => {
+    if (!value?.length) return t("validations.assets-count");
+    if (value.length < 2) return t("validations.assets-count");
+    if (value.reduce((acc, next) => acc + next.percent, 0) !== 100)
+      return t("validations.assets-share");
+    return true;
+  }
+});
