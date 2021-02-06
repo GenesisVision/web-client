8a97bd878798eb68fc789b2c2f5ab78b553d0b35
pavelzubov <pasha224@yandex.ru>
2021-02-06 20:11:58 +0300

    Use styled components and ClosePositionContainer in PositionsRow

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions-row.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/positions/positions-row.tsx
@@ -1,7 +1,11 @@
-import TableCell from "components/table/components/table-cell";
 import TableRow from "components/table/components/table-row";
 import { Position } from "pages/trade/binance-trade-page/trading/terminal.types";
 import React from "react";
+import { ClosePositionContainer } from "pages/trade/binance-trade-page/trading/trading-tables/positions/close-position/close-position.container";
+import {
+  CloseTableCell,
+  StyledTableCell
+} from "pages/trade/binance-trade-page/trading/trading-tables/positions/positions.styles";
 
 interface Props {
   marginRatio: number;
@@ -10,25 +14,40 @@ interface Props {
 
 const _PositionsRow: React.FC<Props> = ({
   marginRatio,
-  position: { entryPrice, leverage, positionAmount, symbol, unrealizedProfit }
+  position: {
+    positionSide,
+    entryPrice,
+    leverage,
+    positionAmount,
+    symbol,
+    unrealizedProfit
+  }
 }) => {
   return (
     <TableRow>
-      <TableCell firstOffset={false}>
+      <StyledTableCell firstOffset={false}>
         {symbol} {leverage}x
-      </TableCell>
-      <TableCell>{positionAmount}</TableCell>
-      <TableCell>{entryPrice}</TableCell>
+      </StyledTableCell>
+      <StyledTableCell>{positionAmount}</StyledTableCell>
+      <StyledTableCell>{entryPrice}</StyledTableCell>
       {/*<TableCell>{markPrice}</TableCell>*/}
-      <TableCell>{0}</TableCell>
+      <StyledTableCell>{0}</StyledTableCell>
       {/*<TableCell>{liquidationPrice}</TableCell>*/}
-      <TableCell>{0}</TableCell>
-      <TableCell>{marginRatio}</TableCell>
-      <TableCell>
+      <StyledTableCell>{0}</StyledTableCell>
+      <StyledTableCell>{marginRatio}</StyledTableCell>
+      <StyledTableCell>
         {/*{(+positionAmount * +markPrice) / +leverage} ({marginType})*/}
         {(+positionAmount * +0) / +leverage} ({"Cross"})
-      </TableCell>
-      <TableCell>{unrealizedProfit}</TableCell>
+      </StyledTableCell>
+      <StyledTableCell>{unrealizedProfit}</StyledTableCell>
+      <CloseTableCell>
+        <ClosePositionContainer
+          symbol={symbol}
+          positionSide={positionSide}
+          price={entryPrice}
+          amount={positionAmount}
+        />
+      </CloseTableCell>
     </TableRow>
   );
 };
