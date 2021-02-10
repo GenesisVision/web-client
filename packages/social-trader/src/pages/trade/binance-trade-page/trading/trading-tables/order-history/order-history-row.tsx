4f56a49da76a3002cf47b7e2334d1fbf5db44c07
pavelzubov <pasha224@yandex.ru>
2021-02-10 22:25:33 +0300

    Use setUpperFirstLetter for side in terminal tables

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/order-history/order-history-row.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/trading-tables/order-history/order-history-row.tsx
@@ -4,7 +4,7 @@ import { Text } from "components/text/text";
 import { BinanceOrderStatus } from "gv-api-web";
 import { terminalMoneyFormat } from "pages/trade/binance-trade-page/trading/components/terminal-money-format/terminal-money-format";
 import { TerminalInfoContext } from "pages/trade/binance-trade-page/trading/contexts/terminal-info.context";
-import { getSymbolFilters } from "pages/trade/binance-trade-page/trading/terminal.helpers";
+import { getSymbolFilters, setUpperFirstLetter } from "pages/trade/binance-trade-page/trading/terminal.helpers";
 import { OrderSide } from "pages/trade/binance-trade-page/trading/terminal.types";
 import React, { useContext } from "react";
 import styled from "styled-components";
@@ -54,7 +54,7 @@ const _OrderHistoryRow: React.FC<Props> = ({
       <TableCell>{type}</TableCell>
       <TableCell>
         <Text color={side.toLowerCase() === "buy" ? "green" : "red"}>
-          {side}
+          {setUpperFirstLetter(side)}
         </Text>
       </TableCell>
       <TableCell>
