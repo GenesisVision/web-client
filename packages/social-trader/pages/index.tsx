529d8165e4491b38a08c0185c9ef0b2d0ea1e880
pavelzubov <pasha224@yandex.ru>
2021-02-16 19:40:38 +0300

    Revert "Use getServerSideProps in landing page"
    This reverts commit 16ad3dfa

--- packages/social-trader/pages/index.tsx
+++ packages/social-trader/pages/index.tsx
@@ -5,11 +5,38 @@ import {
   PlatformNews,
   ProgramDetailsListItemItemsViewModel
 } from "gv-api-web";
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from "next";
 import { getAccept } from "pages/landing-page/components/cookie-message/cookie-message.helpers";
 import { LandingPage } from "pages/landing-page/landing.page";
 import { getLandingAssets, landingAssetsDefaultData } from "pages/landing-page/services/landing.service";
 import React from "react";
+
+const IndexPage: NextPage<Props> = props => {
+  return <LandingPage {...props} />;
+};
+
+IndexPage.getInitialProps = async ctx => {
+  const cookieAccept = getAccept(ctx);
+  const namespacesRequired = ["fees", "auth", "landing-page"];
+  try {
+    const { events, follows, programs, funds, news } = await getLandingAssets();
+    return {
+      cookieAccept,
+      events,
+      follows,
+      programs,
+      funds,
+      news,
+      namespacesRequired
+    };
+  } catch (e) {
+    return {
+      cookieAccept,
+      namespacesRequired,
+      ...landingAssetsDefaultData
+    };
+  }
+};
 
 interface Props {
   cookieAccept?: string;
@@ -20,32 +47,5 @@ interface Props {
   funds: FundDetailsListItemItemsViewModel;
   news: Array<PlatformNews>;
 }
-
-const IndexPage: NextPage<Props> = props => {
-  return <LandingPage {...props} />;
-};
-
-export const getServerSideProps: GetServerSideProps = async ctx => {
-  const cookieAccept = getAccept(ctx);
-  const namespacesRequired = ["fees", "auth", "landing-page"];
-  try {
-    const { events, follows, programs, funds, news } = await getLandingAssets();
-    return {
-      props: {
-        cookieAccept,
-        events,
-        follows,
-        programs,
-        funds,
-        news,
-        namespacesRequired
-      }
-    };
-  } catch (e) {
-    return {
-      props: { cookieAccept, namespacesRequired, ...landingAssetsDefaultData }
-    };
-  }
-};
 
 export default IndexPage;
