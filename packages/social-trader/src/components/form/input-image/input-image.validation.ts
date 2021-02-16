b2a8185f9fe390a1f3b68b0202542d1003704301
pavelzubov <pasha224@yandex.ru>
2021-02-16 19:58:51 +0300

    Remove returning the error text for a empty image in inputImageRules

--- packages/social-trader/src/components/form/input-image/input-image.validation.ts
+++ packages/social-trader/src/components/form/input-image/input-image.validation.ts
@@ -4,7 +4,7 @@ import { AnyObjectType } from "utils/types";
 export const inputImageRules = (t: TFunction) => ({
   validate: (data: AnyObjectType) => {
     if (!data?.image || isNaN(data.image.height) || isNaN(data.image.width))
-      return "Invalid image";
+      return;
     if (data.image.height < 300 || data.image.width < 300)
       return t("validations.resolution");
     return true;
