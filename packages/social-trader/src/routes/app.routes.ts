19dc7bca5d85ecf86f3b59d4790ae8aae86b8bab
mnbeh <mnbeh7@gmail.com>
2021-07-28 15:55:02 +0300

    Refactor menu navigation

--- packages/social-trader/src/routes/app.routes.ts
+++ packages/social-trader/src/routes/app.routes.ts
@@ -1,3 +1,5 @@
+import { GUIDES_TOTAL_PAGE_ROUTE } from "pages/guides/guides.paths";
+
 export const HOME_ROUTE = "/";
 export const SIGNUP_ROUTE = "/signup";
 export const LOGIN_ROUTE = "/login";
@@ -5,3 +7,11 @@ export const LOGIN_ROUTE = "/login";
 export const ID = "id";
 
 export const SLUG_URL_PARAM_NAME = "slugUrl";
+
+export const INFO_ROUTE = "/#info";
+export const BLOG_ROUTE = "https://blog.genesis.vision";
+export const FEES_ROUTE = "/fees";
+export const REFERRAL_PROGRAM_ROUTE = "/referral-program";
+export const FAQ_ROUTE = "/faq";
+export const GUIDES_ROUTE = GUIDES_TOTAL_PAGE_ROUTE;
+export const FEEDBACK_ROUTE = "https://feedback.genesis.vision/";
