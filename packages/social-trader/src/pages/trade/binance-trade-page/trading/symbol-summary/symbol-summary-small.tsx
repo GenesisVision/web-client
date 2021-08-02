74228d67d85a898844af11e3d2730d23bcb7987d
mnbeh <mnbeh7@gmail.com>
2021-08-02 17:41:19 +0300

    Add symbols and symbol price for terminal title

--- packages/social-trader/src/pages/trade/binance-trade-page/trading/symbol-summary/symbol-summary-small.tsx
+++ packages/social-trader/src/pages/trade/binance-trade-page/trading/symbol-summary/symbol-summary-small.tsx
@@ -3,8 +3,8 @@ import { DefaultBlock } from "components/default.block/default.block";
 import { BlurableLabeledValue } from "components/labeled-value/blurable-labeled-value";
 import { LabeledValue } from "components/labeled-value/labeled-value";
 import { ResponsiveContainer } from "components/responsive-container/responsive-container";
-import { RowItem } from "components/row-item/row-item";
 import { Row } from "components/row/row";
+import { RowItem } from "components/row-item/row-item";
 import { Text } from "components/text/text";
 import { TooltipLabel } from "components/tooltip-label/tooltip-label";
 import { withBlurLoader } from "decorators/with-blur-loader";
@@ -23,6 +23,8 @@ import React, { useContext } from "react";
 import NumberFormat from "react-number-format";
 import { diffDate } from "utils/dates";
 import { formatCurrencyValue } from "utils/formatter";
+
+import TerminalTitle from "../components/terminal-title/terminal-title";
 
 interface Props {
   data: SymbolSummaryData;
@@ -75,189 +77,193 @@ const _SymbolSummarySmallView: React.FC<Props> = ({
     </h5>
   );
   return (
-    <Center>
-      <RowItem size={"large"}>
-        <ResponsiveContainer
-          enabledScreens={["tablet", "landscape-tablet", "desktop"]}
-        >
-          <MarketWatchTooltipButton>{renderSymbol()}</MarketWatchTooltipButton>
-        </ResponsiveContainer>
-        <ResponsiveContainer enabledScreens={["large-desktop"]}>
-          {renderSymbol()}
-        </ResponsiveContainer>
-      </RowItem>
-      <RowItem>
-        <Row>
-          <h4>
-            <MonoText>
-              <TradeStatefulValue
-                value={terminalMoneyFormat({
-                  amount: lastPrice,
-                  tickSize
-                })}
-                trigger={eventTime}
-              />
-            </MonoText>
-          </h4>
-        </Row>
-        {usdRate && (
-          <Row size={"xsmall"}>
-            <Text muted>
-              $<MonoText>{formatCurrencyValue(usdRate, "USD")}</MonoText>
-            </Text>
-          </Row>
-        )}
-      </RowItem>
-      {markPrice && (
-        <>
-          <RowItem>
-            <LabeledValue
-              size={"xsmall"}
-              label={
-                <TooltipLabel
-                  labelText={"Mark Price"}
-                  tooltipContent={
-                    "The latest mark price for this contract. This is the price used for PNL and margin calculations, and may differ from the last price for the purposes of avoiding price manipulation."
-                  }
-                />
-              }
-            >
-              <Text size={"xsmall"}>
-                <MonoText>
-                  {terminalMoneyFormat({
-                    amount: markPrice.markPrice,
+    <TerminalTitle amount={lastPrice} trigger={eventTime!}>
+      <Center>
+        <RowItem size={"large"}>
+          <ResponsiveContainer
+            enabledScreens={["tablet", "landscape-tablet", "desktop"]}
+          >
+            <MarketWatchTooltipButton>
+              {renderSymbol()}
+            </MarketWatchTooltipButton>
+          </ResponsiveContainer>
+          <ResponsiveContainer enabledScreens={["large-desktop"]}>
+            {renderSymbol()}
+          </ResponsiveContainer>
+        </RowItem>
+        <RowItem>
+          <Row>
+            <h4>
+              <MonoText>
+                <TradeStatefulValue
+                  value={terminalMoneyFormat({
+                    amount: lastPrice,
                     tickSize
                   })}
-                </MonoText>
-              </Text>
-            </LabeledValue>
-          </RowItem>
-          <RowItem>
-            <LabeledValue
-              size={"xsmall"}
-              label={
-                <TooltipLabel
-                  labelText={"Funding/8h"}
-                  tooltipContent={
-                    "The payment rate exchanged between the buyer and seller for the next funding."
-                  }
+                  trigger={eventTime}
                 />
-              }
-            >
-              {serverTime && (
+              </MonoText>
+            </h4>
+          </Row>
+          {usdRate && (
+            <Row size={"xsmall"}>
+              <Text muted>
+                $<MonoText>{formatCurrencyValue(usdRate, "USD")}</MonoText>
+              </Text>
+            </Row>
+          )}
+        </RowItem>
+        {markPrice && (
+          <>
+            <RowItem>
+              <LabeledValue
+                size={"xsmall"}
+                label={
+                  <TooltipLabel
+                    labelText={"Mark Price"}
+                    tooltipContent={
+                      "The latest mark price for this contract. This is the price used for PNL and margin calculations, and may differ from the last price for the purposes of avoiding price manipulation."
+                    }
+                  />
+                }
+              >
                 <Text size={"xsmall"}>
-                  <Text wrap={false}>
-                    <MonoText>{+markPrice.fundingRate * 100} % </MonoText>
-                  </Text>
                   <MonoText>
-                    {diffDate(
-                      new Date(serverTime.date),
-                      markPrice.nextFundingTime
-                    )
-                      .utc()
-                      .format("HH:mm:ss")}
+                    {terminalMoneyFormat({
+                      amount: markPrice.markPrice,
+                      tickSize
+                    })}
                   </MonoText>
                 </Text>
-              )}
-            </LabeledValue>
-          </RowItem>
-        </>
-      )}
-      <RowItem>
-        <LabeledValue size={"xsmall"} label={"24h Change"}>
-          <MonoText>
-            <Text
-              size={"xsmall"}
-              color={+priceChangePercent > 0 ? "green" : "red"}
-            >
-              {terminalMoneyFormat({ amount: priceChange, tickSize })}{" "}
-              {terminalMoneyFormat({
-                amount: priceChangePercent,
-                digits: 2
-              })}{" "}
-              %
+              </LabeledValue>
+            </RowItem>
+            <RowItem>
+              <LabeledValue
+                size={"xsmall"}
+                label={
+                  <TooltipLabel
+                    labelText={"Funding/8h"}
+                    tooltipContent={
+                      "The payment rate exchanged between the buyer and seller for the next funding."
+                    }
+                  />
+                }
+              >
+                {serverTime && (
+                  <Text size={"xsmall"}>
+                    <Text wrap={false}>
+                      <MonoText>{+markPrice.fundingRate * 100} % </MonoText>
+                    </Text>
+                    <MonoText>
+                      {diffDate(
+                        new Date(serverTime.date),
+                        markPrice.nextFundingTime
+                      )
+                        .utc()
+                        .format("HH:mm:ss")}
+                    </MonoText>
+                  </Text>
+                )}
+              </LabeledValue>
+            </RowItem>
+          </>
+        )}
+        <RowItem>
+          <LabeledValue size={"xsmall"} label={"24h Change"}>
+            <MonoText>
+              <Text
+                size={"xsmall"}
+                color={+priceChangePercent > 0 ? "green" : "red"}
+              >
+                {terminalMoneyFormat({ amount: priceChange, tickSize })}{" "}
+                {terminalMoneyFormat({
+                  amount: priceChangePercent,
+                  digits: 2
+                })}{" "}
+                %
+              </Text>
+            </MonoText>
+          </LabeledValue>
+        </RowItem>
+        <RowItem>
+          <BlurableLabeledValue
+            size={"xsmall"}
+            isPending={!highPrice}
+            label={"24h High"}
+          >
+            <Text size={"xsmall"}>
+              <MonoText>
+                <NumberFormat
+                  value={terminalMoneyFormat({
+                    amount: highPrice,
+                    tickSize
+                  })}
+                  thousandSeparator={","}
+                  displayType="text"
+                />
+              </MonoText>
             </Text>
-          </MonoText>
-        </LabeledValue>
-      </RowItem>
-      <RowItem>
-        <BlurableLabeledValue
-          size={"xsmall"}
-          isPending={!highPrice}
-          label={"24h High"}
-        >
-          <Text size={"xsmall"}>
-            <MonoText>
-              <NumberFormat
-                value={terminalMoneyFormat({
-                  amount: highPrice,
-                  tickSize
-                })}
-                thousandSeparator={","}
-                displayType="text"
-              />
-            </MonoText>
-          </Text>
-        </BlurableLabeledValue>
-      </RowItem>
-      <RowItem>
-        <BlurableLabeledValue
-          size={"xsmall"}
-          isPending={!lowPrice}
-          label={"24h Low"}
-        >
-          <Text size={"xsmall"}>
-            <MonoText>
-              <NumberFormat
-                value={terminalMoneyFormat({
-                  amount: lowPrice,
-                  tickSize
-                })}
-                thousandSeparator={","}
-                displayType="text"
-              />
-            </MonoText>
-          </Text>
-        </BlurableLabeledValue>
-      </RowItem>
-      <RowItem>
-        <LabeledValue size={"xsmall"} label={`24h Volume (${baseAsset})`}>
-          <Text size={"xsmall"}>
-            <MonoText>
-              <NumberFormat
-                value={terminalMoneyFormat({
-                  amount: baseVolume,
-                  tickSize: stepSize
-                })}
-                thousandSeparator={","}
-                displayType="text"
-                suffix={` ${baseAsset}`}
-              />
-            </MonoText>
-          </Text>
-        </LabeledValue>
-      </RowItem>
-      <RowItem>
-        <LabeledValue size={"xsmall"} label={`24h Volume (${quoteAsset})`}>
-          <Text size={"xsmall"}>
-            <MonoText>
-              <NumberFormat
-                value={terminalMoneyFormat({
-                  amount: quoteVolume,
-                  tickSize: tickSize
-                })}
-                thousandSeparator={","}
-                displayType="text"
-                suffix={` ${quoteAsset}`}
-              />
-            </MonoText>
-          </Text>
-        </LabeledValue>
-      </RowItem>
-      <RowItem>
-        <AccountSelectorContainer currentAccount={exchangeAccountId} />
-      </RowItem>
-    </Center>
+          </BlurableLabeledValue>
+        </RowItem>
+        <RowItem>
+          <BlurableLabeledValue
+            size={"xsmall"}
+            isPending={!lowPrice}
+            label={"24h Low"}
+          >
+            <Text size={"xsmall"}>
+              <MonoText>
+                <NumberFormat
+                  value={terminalMoneyFormat({
+                    amount: lowPrice,
+                    tickSize
+                  })}
+                  thousandSeparator={","}
+                  displayType="text"
+                />
+              </MonoText>
+            </Text>
+          </BlurableLabeledValue>
+        </RowItem>
+        <RowItem>
+          <LabeledValue size={"xsmall"} label={`24h Volume (${baseAsset})`}>
+            <Text size={"xsmall"}>
+              <MonoText>
+                <NumberFormat
+                  value={terminalMoneyFormat({
+                    amount: baseVolume,
+                    tickSize: stepSize
+                  })}
+                  thousandSeparator={","}
+                  displayType="text"
+                  suffix={` ${baseAsset}`}
+                />
+              </MonoText>
+            </Text>
+          </LabeledValue>
+        </RowItem>
+        <RowItem>
+          <LabeledValue size={"xsmall"} label={`24h Volume (${quoteAsset})`}>
+            <Text size={"xsmall"}>
+              <MonoText>
+                <NumberFormat
+                  value={terminalMoneyFormat({
+                    amount: quoteVolume,
+                    tickSize: tickSize
+                  })}
+                  thousandSeparator={","}
+                  displayType="text"
+                  suffix={` ${quoteAsset}`}
+                />
+              </MonoText>
+            </Text>
+          </LabeledValue>
+        </RowItem>
+        <RowItem>
+          <AccountSelectorContainer currentAccount={exchangeAccountId} />
+        </RowItem>
+      </Center>
+    </TerminalTitle>
   );
 };
 export const SymbolSummarySmallView = withBlurLoader(
