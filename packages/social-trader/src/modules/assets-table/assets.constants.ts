65e660773c2874399eefe5a129642ed20be3cbd5
A Popova <a-nap@inbox.ru>
2021-09-10 17:30:50 +0200

    Create AssetsPage

---
+++ packages/social-trader/src/modules/assets-table/assets.constants.ts
@@ -0,0 +1,26 @@
+import { SortingColumn } from "components/table/components/filtering/filter.type";
+
+export const ASSETS_TABLE_COLUMNS: SortingColumn[] = [
+  {
+    name: "asset"
+  },
+  {
+    name: "symbol"
+  },
+  {
+    name: "price"
+  },
+  {
+    name: "change-24h"
+  },
+  {
+    name: "market-cap"
+  },
+  {
+    name: "volume-24h"
+  },
+  {
+    name: "chart"
+  }
+];
+
---
+++ packages/social-trader/src/pages/invest/assets/assets.page.tsx
@@ -0,0 +1,46 @@
+import BreadCrumbs from "components/breadcrumbs/breadcrumbs";
+import { DefaultTableBlock } from "components/default.block/default-table.block";
+import Page from "components/page/page";
+import { useTranslation } from "i18n";
+import * as React from "react";
+import { GV_ASSETS_ROUTE, INVEST_ROUTE } from "routes/invest.routes";
+import { ORGANIZATION_SCHEMA } from "utils/seo";
+import { CoinsAssetResponseItemsViewModel } from "gv-api-web";
+import AssetsTableSSR from "modules/assets-table/components/assets-table/assets-table-ssr";
+
+interface Props {
+  data: CoinsAssetResponseItemsViewModel;
+}
+
+const AssetsPage: React.FC<Props> = ({ data }) => {
+  const { t } = useTranslation();
+  const title = t("assets-page:title");
+  const description = t("assets-page:description");
+  return (
+    <Page
+      description={description}
+      showTitle
+      title={title}
+      schemas={[
+        ORGANIZATION_SCHEMA,
+        {
+          "@context": "https://schema.org",
+          "@type": "Table",
+          about: description
+        }
+      ]}
+    >
+      <BreadCrumbs
+        items={[
+          { href: INVEST_ROUTE, label: t("navigation.invest") },
+          { href: GV_ASSETS_ROUTE, label: t("navigation.assets") }
+        ]}
+      />
+      <DefaultTableBlock>
+        <AssetsTableSSR data={data} />
+      </DefaultTableBlock>
+    </Page>
+  );
+};
+
+export default AssetsPage;
