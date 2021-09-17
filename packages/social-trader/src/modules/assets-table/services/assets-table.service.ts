c17cc043c21ea657404d10ab56f2dfd0d807f27b
A Popova <a-nap@inbox.ru>
2021-09-17 13:20:12 +0200

    Add transferCoins method

--- packages/social-trader/src/modules/assets-table/services/assets-table.service.ts
+++ packages/social-trader/src/modules/assets-table/services/assets-table.service.ts
@@ -1,5 +1,5 @@
 import { ComposeFiltersAllType } from "components/table/components/filtering/filter.type";
-import { CoinsAssetResponseItemsViewModel } from "gv-api-web";
+import { CoinsAssetResponseItemsViewModel, InternalTransferRequest } from "gv-api-web";
 import { api } from "services/api-client/swagger-custom-client";
 
 export const fetchCoins = (
@@ -13,3 +13,8 @@ export const fetchPortfolioCoins = (
 ): Promise<CoinsAssetResponseItemsViewModel> => {
   return api.coins().getUserCoins(filters);
 };
+
+export const transferCoins = (body: InternalTransferRequest): Promise<any> =>
+  api.coins().transfer({
+    body
+  });
