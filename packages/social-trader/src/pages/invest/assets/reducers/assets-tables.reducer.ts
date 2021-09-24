de70cf85c9cd38144a10a384c858985b1f406189
A Popova <a-nap@inbox.ru>
2021-09-24 12:10:49 +0200

    Fix default sorting for assets page and fix prefix $ in tables

--- packages/social-trader/src/pages/invest/assets/reducers/assets-tables.reducer.ts
+++ packages/social-trader/src/pages/invest/assets/reducers/assets-tables.reducer.ts
@@ -1,34 +1,38 @@
 import { tableSelectorCreator } from "components/table/helpers/table.selector";
-import {
-  DEFAULT_PAGING
-} from "components/table/reducers/table-paging.reducer";
+import { DEFAULT_PAGING } from "components/table/reducers/table-paging.reducer";
 import tableReducerFactory from "components/table/reducers/table.reducer";
 import { RootState } from "reducers/root-reducer";
 
 import {
   ASSETS_COINS,
-  ASSETS_HISTORY, ASSETS_HISTORY_DEFAULT_FILTERS,
+  ASSETS_HISTORY,
+  ASSETS_HISTORY_DEFAULT_FILTERS,
   ASSETS_HISTORY_FILTERS,
-  ASSETS_PORTFOLIO
+  ASSETS_PORTFOLIO, ASSETS_SORTING_DEFAULT
 } from "pages/invest/assets/assets.constants";
-import { CoinsAssetItemsViewModel, CoinsHistoryEventItemsViewModel } from "gv-api-web";
+import {
+  CoinsAssetItemsViewModel,
+  CoinsHistoryEventItemsViewModel
+} from "gv-api-web";
 import { IDataModel } from "constants/constants";
 
 const defaultData: IDataModel = { items: null, total: 0 };
 
-export const assetsCoinsSelector = (state: RootState) =>
-  state.assets.coins;
+export const assetsCoinsSelector = (state: RootState) => state.assets.coins;
 
 export const assetsCoinsTableSelector = tableSelectorCreator<
   RootState,
   CoinsAssetItemsViewModel,
   CoinsAssetItemsViewModel
-  >(assetsCoinsSelector, "items", defaultData);
+>(assetsCoinsSelector, "items", defaultData);
 
-export const assetsCoinsReducer = tableReducerFactory<CoinsAssetItemsViewModel>({
-  type: ASSETS_COINS,
-  paging: { ...DEFAULT_PAGING, itemsOnPage: 12 }
-});
+export const assetsCoinsReducer = tableReducerFactory<CoinsAssetItemsViewModel>(
+  {
+    type: ASSETS_COINS,
+    sorting: ASSETS_SORTING_DEFAULT,
+    paging: { ...DEFAULT_PAGING, itemsOnPage: 12 }
+  }
+);
 
 export const assetsPortfolioSelector = (state: RootState) =>
   state.assets.portfolio;
@@ -37,25 +41,28 @@ export const assetsPortfolioTableSelector = tableSelectorCreator<
   RootState,
   CoinsAssetItemsViewModel,
   CoinsAssetItemsViewModel
-  >(assetsPortfolioSelector, "items", defaultData);
+>(assetsPortfolioSelector, "items", defaultData);
 
-export const assetsPortfolioReducer = tableReducerFactory<CoinsAssetItemsViewModel>({
-  type: ASSETS_PORTFOLIO,
-  paging: { ...DEFAULT_PAGING, itemsOnPage: 12 }
-});
+export const assetsPortfolioReducer = tableReducerFactory<CoinsAssetItemsViewModel>(
+  {
+    type: ASSETS_PORTFOLIO,
+    paging: { ...DEFAULT_PAGING, itemsOnPage: 12 }
+  }
+);
 
-export const assetsHistorySelector = (state: RootState) =>
-  state.assets.history;
+export const assetsHistorySelector = (state: RootState) => state.assets.history;
 
 export const assetsHistoryTableSelector = tableSelectorCreator<
   RootState,
   CoinsHistoryEventItemsViewModel,
   CoinsHistoryEventItemsViewModel
-  >(assetsHistorySelector, "items", defaultData);
+>(assetsHistorySelector, "items", defaultData);
 
-export const assetsHistoryReducer = tableReducerFactory<CoinsHistoryEventItemsViewModel>({
-  type: ASSETS_HISTORY,
-  paging: { ...DEFAULT_PAGING, itemsOnPage: 12 },
-  filtering: ASSETS_HISTORY_FILTERS,
-  defaultFilters: ASSETS_HISTORY_DEFAULT_FILTERS
-});
+export const assetsHistoryReducer = tableReducerFactory<CoinsHistoryEventItemsViewModel>(
+  {
+    type: ASSETS_HISTORY,
+    paging: { ...DEFAULT_PAGING, itemsOnPage: 12 },
+    filtering: ASSETS_HISTORY_FILTERS,
+    defaultFilters: ASSETS_HISTORY_DEFAULT_FILTERS
+  }
+);
