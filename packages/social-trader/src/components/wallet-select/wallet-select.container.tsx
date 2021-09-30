3b8cbbdfaf386e74c7029da36836a1c46b1a86bd
A Popova <a-nap@inbox.ru>
2021-09-30 19:51:37 +0200

    Add prop 'withUpdateButton' in Deposit container

--- packages/social-trader/src/components/wallet-select/wallet-select.container.tsx
+++ packages/social-trader/src/components/wallet-select/wallet-select.container.tsx
@@ -1,5 +1,9 @@
 import { onSelectChange } from "components/select/select.test-helpers";
-import { CommonWalletType, HookFormWalletSelect, IWalletSelectProps } from "components/wallet-select/wallet-select";
+import {
+  CommonWalletType,
+  HookFormWalletSelect,
+  IWalletSelectProps
+} from "components/wallet-select/wallet-select";
 import {
   mergeNormalizedList,
   normalizeWalletList,
@@ -8,7 +12,10 @@ import {
 } from "components/wallet-select/wallet-select.helpers";
 import { useAccountCurrency } from "hooks/account-currency.hook";
 import { fetchWalletsByCurrencyAvailableAction } from "pages/wallet/actions/wallet.actions";
-import { walletsAvailableSelector, walletsSelector } from "pages/wallet/reducers/wallet.reducers";
+import {
+  walletsAvailableSelector,
+  walletsSelector
+} from "pages/wallet/reducers/wallet.reducers";
 import React, { useCallback, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
@@ -19,6 +26,7 @@ export interface IWalletSelectContainerProps extends IWalletSelectProps {
   label?: string;
   onChange: (wallet: CommonWalletType) => void;
   filterFunc?: (wallet: any) => boolean;
+  withUpdateButton?: boolean;
 }
 
 const _WalletSelectContainer: React.FC<IWalletSelectContainerProps> = props => {
